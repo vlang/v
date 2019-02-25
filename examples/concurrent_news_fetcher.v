2d5f84d4307a4a78ea1d2f2181058e7605426ce1
Alexander Medvednikov <alexander@medvednikov.com>
2019-02-25 17:27:32 +0100

    Create concurrent_news_fetcher.v

---
+++ examples/concurrent_news_fetcher.v
@@ -0,0 +1,44 @@
+// Please share your thoughts, suggestions, questions, etc here:
+// https://github.com/vlang-io/V/issues/3
+
+// I'm very interested in your feedback.
+
+module main
+
+import http
+import json
+import runtime
+
+struct Story {
+        title string
+}
+
+// Fetches top HN stories in 8 coroutines
+func main() {
+        resp := http.get('https://hacker-news.firebaseio.com/v0/topstories.json') or {
+                panic(err)
+        }
+        ids := json.decode([]int, resp.body) or {
+                panic(err)
+        }
+        mut cursor := 0
+        for i := 0; i < 8; i++ {
+                go fn() {
+                        for cursor < ids.len {
+                                lock { // Without this lock block the program will not compile
+                                        id := ids[cursor]
+                                        cursor++
+                                }
+                                url := 'https://hacker-news.firebaseio.com/v0/item/$id.json'
+                                resp := http.get(url) or {
+                                        panic(err)
+                                }
+                                story := json.decode(Story, resp.body) or {
+                                        panic(err)
+                                }
+                                println(story.title)
+                        }
+                }()
+        }
+        runtime.wait() // Waits for all coroutines to finish
+}
