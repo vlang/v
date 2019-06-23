35cef791492cf2ba104e0d770ca39cbbbb41727c
Alexander Medvednikov <alexander@medvednikov.com>
2019-06-23 23:02:11 +0200

    http/download.v: replace C code with V + clean up

--- http/download_mac.v
+++ http/download_mac.v
@@ -6,63 +6,51 @@ module http
 
 import os
 
-struct LUEL {
-	age int
-}
-
 type downloadfn fn (written int)
+type download_finished_fn fn () 
 
 struct DownloadStruct {
+mut: 
 	stream  voidptr
 	written int
 	cb      downloadfn
 }
 
 fn download_cb(ptr voidptr, size, nmemb size_t, userp voidptr) int {
-	// # struct http__MemoryStruct *mem = (struct http__MemoryStruct *)userp;
-	data := &DownloadStruct(userp)
-	# size_t written = fwrite(ptr, size, nmemb, (FILE*)(data->stream));
-	// # printf("!!!%d\n", written);
-	# data->written += written;
-	if !isnil(data.cb) {
-		# data->cb(data->written);
-	}
-	# return written;
-	return 0
+	mut data := &DownloadStruct(userp)
+	written := C.fwrite(ptr, size, nmemb, data.stream) 
+	data.written += written 
+	#data->cb(data->written); // TODO 
+	return written 
 }
 
-fn download_file_with_progress(url, out string, cb, cb_finished voidptr) {
-	/* 
+fn download_file_with_progress(url, out string, cb, cb_finished download_finished_fn) {  
 	curl := C.curl_easy_init()
 	if isnil(curl) {
 		return
 	}
-	# FILE*        fp = fopen(out.str,"wb");
-	# curl_easy_setopt(curl, CURLOPT_URL, url.str);
+	cout := out.cstr() 
+	fp := C.fopen(cout, 'wb') 
+	C.curl_easy_setopt(curl, CURLOPT_URL, url.cstr()) 
 	C.curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, download_cb)
-	// # curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, http__download_cb);
 	data := &DownloadStruct {
-		// stream:fp
+		stream:fp
 		cb: cb
 	}
-	# data->stream = fp;
-	# curl_easy_setopt(curl, CURLOPT_WRITEDATA, data);
-	# double d = 0;
-	# curl_easy_getinfo(curl, CURLINFO_CONTENT_LENGTH_DOWNLOAD, &d);
-	# CURLcode        res = curl_easy_perform(curl);
-	println('DONE!')
-	# curl_easy_cleanup(curl);
-	# fclose(fp);
-	# void (*finished)() =cb_finished; finished();
-*/
+	C.curl_easy_setopt(curl, CURLOPT_WRITEDATA, data) 
+	mut d := 0.0 
+	C.curl_easy_getinfo(curl, CURLINFO_CONTENT_LENGTH_DOWNLOAD, &d) 
+	C.curl_easy_perform(curl) 
+	C.curl_easy_cleanup(curl) 
+	C.fclose(fp) 
+	#cb_finished(); // TODO 
 }
 
 fn download_file(url, out string) {
-	// println('\nDOWNLOAD FILE $out url=$url')
-	// -L follow redirects
-	// println('curl -L -o "$out" "$url"')
-	os.system2('curl -s -L -o "$out" "$url"')
-	// res := os.system('curl -s -L -o "$out" "$url"')
-	// println(res)
+	download_file_with_progress(url, out, empty, empty) 
 }
+
+fn empty() {
+ 
+} 
 
