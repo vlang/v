c3df224c975b73b8a594fd2fd4a72930deef08fe
musou1500 <musou1500@gmail.com>
2019-06-23 16:03:52 +0900

    add include path for cJSON

--- json/json_primitives.v
+++ json/json_primitives.v
@@ -4,8 +4,12 @@
 
 module json
 
-// #include "json/cJSON/cJSON.c"
-#include "json/cJSON/cJSON.h"
+// TODO: windows support
+#flag linux -I$HOME/code/v/thirdpaty/cJSON
+#flag mac -I$HOME/code/v/thirdpaty/cJSON
+
+// #include "cJSON.c"
+#include "cJSON.h"
 struct C.cJSON {
 	valueint    int
 	valuestring byteptr
