e8de00f68765bf5071fe29ffcc0ce2aef4a60228
chai2010 <chaishushan@gmail.com>
2019-06-23 10:21:30 +0800

    all: add copyright

--- examples/hello_world.v
+++ examples/hello_world.v
@@ -1,1 +1,5 @@
+// Copyright (c) 2019 Alexander Medvednikov. All rights reserved.
+// Use of this source code is governed by an MIT license
+// that can be found in the LICENSE file.
+
 println('Hello!')
