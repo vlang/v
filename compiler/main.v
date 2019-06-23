0c282ba5e4fa1cacf3951c39a456be8d6afc0e6f
juliancoffee <lightdarkdaughter@gmail.com>
2019-06-23 16:16:12 +0300

    In linux clang is in /usr/bin/clang, now It is fixed in main.v

--- compiler/main.v
+++ compiler/main.v
@@ -472,7 +472,12 @@ mut args := ''
 		a << '-lm -ldl -lpthread'
 	}
 	// Find clang executable
-	fast_clang := '/usr/local/Cellar/llvm/8.0.0/bin/clang'
+    $if mac {
+        fast_clang := '/usr/local/Cellar/llvm/8.0.0/bin/clang'
+    }
+    $if linux {
+        fast_clang := '/usr/bin/clang'
+    }
 	args := a.join(' ')
 	cmd := if os.file_exists(fast_clang) {
 		'$fast_clang -I. $args'
