e2853112901e0565dc262f83be6341a6cb609ce4
Aurélien Foucault <aurelien.foucault@epitech.eu>
2019-06-24 22:25:35 +0200

    Add all types of int and float to json decode/encode

--- json/json_primitives.v
+++ json/json_primitives.v
@@ -12,6 +12,7 @@ module json
 #include "cJSON.h"
 struct C.cJSON {
 	valueint    int
+	valuedouble float
 	valuestring byteptr
 }
 
@@ -21,6 +22,58 @@ fn jsdecode_int(root *C.cJSON) int {
 	}
 	return root.valueint
 }
+
+//TODO: Refactor with generics when it will be avaible
+
+fn jsdecode_i8(root *C.cJSON) i8 {
+	if isnil(root) {
+		return i8(0)
+	}
+	return i8(root.valueint)
+}
+
+fn jsdecode_i16(root *C.cJSON) i16 {
+	if isnil(root) {
+		return i16(0)
+	}
+	return i16(root.valueint)
+}
+
+fn jsdecode_i32(root *C.cJSON) i32 {
+	if isnil(root) {
+		return i32(0)
+	}
+	return i32(root.valueint)
+}
+
+fn jsdecode_i64(root *C.cJSON) i64 {
+	if isnil(root) {
+		return i64(0)
+	}
+	return i64(root.valuedouble) //i64 is double in C
+}
+
+fn jsdecode_float(root *C.cJSON) float {
+	if isnil(root) {
+		return 0
+	}
+	return root.valuedouble
+}
+
+fn jsdecode_f32(root *C.cJSON) f32 {
+	if isnil(root) {
+		return f32(0)
+	}
+	return f32(root.valuedouble)
+}
+
+fn jsdecode_f64(root *C.cJSON) f64 {
+	if isnil(root) {
+		return f64(0)
+	}
+	return f64(root.valuedouble)
+}
+
 
 fn jsdecode_string(root *C.cJSON) string {
 	if isnil(root) {
@@ -42,7 +95,35 @@ fn jsdecode_bool(root *C.cJSON) bool {
 }
 
 // ///////////////////
+//TODO: Refactor with Generics when it will be available
 fn jsencode_int(val int) *C.cJSON {
+	return C.cJSON_CreateNumber(val)
+}
+
+fn jsencode_i8(val i8) *C.cJSON {
+	return C.cJSON_CreateNumber(val)
+}
+
+fn jsencode_i16(val i16) *C.cJSON {
+	return C.cJSON_CreateNumber(val)
+}
+fn jsencode_i32(val i32) *C.cJSON {
+	return C.cJSON_CreateNumber(val)
+}
+
+fn jsencode_i64(val i64) *C.cJSON {
+	return C.cJSON_CreateNumber(val)
+}
+
+fn jsencode_float(val float) *C.cJSON {
+	return C.cJSON_CreateNumber(val)
+}
+
+fn jsencode_f32(val f32) *C.cJSON {
+	return C.cJSON_CreateNumber(val)
+}
+
+fn jsencode_f64(val f64) *C.cJSON {
 	return C.cJSON_CreateNumber(val)
 }
 
