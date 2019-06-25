cc4d60cc1c7daea5dab7d512657df8df0e0a7c9e
musou1500 <musou1500@gmail.com>
2019-06-26 00:03:18 +0900

    add `unsetenv`, `setenv`, `clearenv` to os module

--- os/os.v
+++ os/os.v
@@ -315,6 +315,18 @@ pub fn getenv(key string) string {
 	return tos2(s)
 }
 
+pub fn setenv(name string, value string, overwrite bool) int {
+  return C.setenv(name.cstr(), value.cstr(), overwrite)
+}
+
+pub fn unsetenv(name string) int {
+  return C.unsetenv(name.cstr())
+}
+
+pub fn clearenv() {
+  C.clearenv()
+}
+
 fn exit(code int) {
 	C.exit(code)
 }
