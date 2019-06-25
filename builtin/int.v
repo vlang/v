744ea78e10442c52d8d39b67f1d5884b84cd926e
Aurélien Foucault <aurelien.foucault@epitech.eu>
2019-06-25 02:39:39 +0200

    Add display of rune

--- builtin/int.v
+++ builtin/int.v
@@ -150,11 +150,38 @@ fn (a[]byte) contains(val byte) bool {
 	}
 	return false
 }
-
-/*  TODO 
-fn (c rune) str() string {
+ 
+fn (code rune)str() string {
+	// println('code = $code')
+	buffer := malloc(5)
+	# if (code <= 0x7F) {
+	// println('!!!!!!!1')
+	# buffer[0] = code;
+	# return tos(buffer, 1);
+	# }
+	# if (code <= 0x7FF) {
+	// println('!!!!!!!2')
+	# buffer[0] = 0xC0 | (code >> 6);            /* 110xxxxx */
+	# buffer[1] = 0x80 | (code & 0x3F);          /* 10xxxxxx */
+	# return tos(buffer, 2);
+	# }
+	# if (code <= 0xFFFF) {
+	// println('!!!!!!!3')
+	# buffer[0] = 0xE0 | (code >> 12);           /* 1110xxxx */
+	# buffer[1] = 0x80 | ((code >> 6) & 0x3F);   /* 10xxxxxx */
+	# buffer[2] = 0x80 | (code & 0x3F);          /* 10xxxxxx */
+	# return tos(buffer, 3);
+	# }
+	# if (code <= 0x10FFFF) {
+	# buffer[0] = 0xF0 | (code >> 18);           /* 11110xxx */
+	# buffer[1] = 0x80 | ((code >> 12) & 0x3F);  /* 10xxxxxx */
+	# buffer[2] = 0x80 | ((code >> 6) & 0x3F);   /* 10xxxxxx */
+	# buffer[3] = 0x80 | (code & 0x3F);          /* 10xxxxxx */
+	# return tos(buffer, 4);
+	# }
+	return ''
 }
-*/
+
 fn (c byte) str() string {
 	mut str := string {
 		len: 1
