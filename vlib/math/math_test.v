0ae3583bc076f261169ac4c4d207f1369d52538f
Dinar Garipov <garipov.dinar@gmail.com>
2019-06-29 21:54:19 +0200

    Rename test method names

--- vlib/math/math_test.v
+++ vlib/math/math_test.v
@@ -1,13 +1,13 @@
 import math
 
-fn test_gcd_returns_correct_values() {
+fn test_gcd() {
 	assert math.gcd(6, 9) == 3
 	assert math.gcd(6, -9) == 3
 	assert math.gcd(-6, -9) == 3
 	assert math.gcd(0, 0) == 0
 }
 
-fn test_lcm_returns_correct_values() {
+fn test_lcm() {
 	assert math.lcm(2, 3) == 6
 	assert math.lcm(-2, 3) == 6
 	assert math.lcm(-2, -3) == 6
