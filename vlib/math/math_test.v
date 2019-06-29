4c79f495206e5e2d0f93d091baf3d356ede333d0
Dinar Garipov <garipov.dinar@gmail.com>
2019-06-29 19:41:52 +0200

    Cleanup and improve tests of libs

--- vlib/math/math_test.v
+++ vlib/math/math_test.v
@@ -1,9 +1,15 @@
 import math
 
-fn test_gcd_and_lcm() {
+fn test_gcd_returns_correct_values() {
 	assert math.gcd(6, 9) == 3
 	assert math.gcd(6, -9) == 3
+	assert math.gcd(-6, -9) == 3
+	assert math.gcd(0, 0) == 0
+}
 
+fn test_lcm_returns_correct_values() {
 	assert math.lcm(2, 3) == 6
 	assert math.lcm(-2, 3) == 6
+	assert math.lcm(-2, -3) == 6
+	assert math.lcm(0, 0) == 0
 }
