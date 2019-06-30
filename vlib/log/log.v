6a2b1131d24267c4363fdf39900a6387f4197221
Zaoqi <zaomir@outlook.com>
2019-06-30 18:17:28 +0800

    Update log.v

--- vlib/log/log.v
+++ vlib/log/log.v
@@ -5,48 +5,48 @@ import termcolor
 const (
     FATAL = 1
     ERROR = 2 
-    WARN = 3
-    INFO = 4
+    WARN  = 3
+    INFO  = 4
     DEBUG = 5
 )
 
-struct Log{
+struct Log {
 mut:
     level int
 }
 
 
-pub fn (l mut Log) set_level(level int){
+pub fn (l mut Log) set_level(level int) {
     l.level = level
 }
 
-pub fn (l Log) fatal(s string){
+pub fn (l Log) fatal(s string) {
     panic(s)
 }
 
-pub fn (l Log) error(s string){
-    if l.level >= ERROR{
+pub fn (l Log) error(s string) {
+    if l.level >= ERROR {
         f := termcolor.red('E')
         println('[$f]$s')
     }
 }
 
-pub fn (l Log) warn(s string){
-    if l.level >= WARN{
+pub fn (l Log) warn(s string) {
+    if l.level >= WARN {
         f := termcolor.yellow('W')
         println('[$f]$s')
     }
 }
 
-pub fn (l Log) info(s string){
-    if l.level >= INFO{
+pub fn (l Log) info(s string) {
+    if l.level >= INFO {
         f := termcolor.white('I')
         println('[$f]$s')
     }
 }
 
-pub fn (l Log) debug(s string){
-    if l.level >= DEBUG{
+pub fn (l Log) debug(s string) {
+    if l.level >= DEBUG {
         f := termcolor.blue('D')
         println('[$f]$s')
     }
