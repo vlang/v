509dc5787cc83e2d4e29471c6140c3d3f51021fd
archanpatkar <pat@archanpatkar.com>
2019-07-02 01:41:21 +0530

    Added support for Fractions

---
+++ examples/using_constructs.v
@@ -0,0 +1,18 @@
+import math
+
+fn main() {
+	f1 := math.fraction(2,4)
+	f2 := math.fraction(5,10)
+	println(f1 + f2)
+	println(f2 - f1)
+	println(f1.multiply(f2))
+	println(f1.divide(f2))
+	println(f1.gcf())
+	println(f1.reduce())
+	println(f2.gcf())
+	println(f2.reduce())
+	println(f1.reciprocal())
+	println(f2.reciprocal())
+	println(f1.equals(f1))
+	println(f2.equals(f2))
+}
