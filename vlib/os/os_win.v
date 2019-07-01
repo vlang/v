1ca20196d0ed6375b8ed95781e5b0a84b2cd33c8
0x9ef <43169346+0x9ef@users.noreply.github.com>
2019-07-01 18:45:55 +0300

    Created os_win.v and added `get_file_handle`

---
+++ vlib/os/os_win.v
@@ -0,0 +1,15 @@
+module os
+
+// Ref - https://docs.microsoft.com/en-us/windows/desktop/winprog/windows-data-types
+// A handle to an object.
+type HANDLE voidptr // C.HANDLE
+
+pub fn get_file_handle(path string) HANDLE {
+    mode := 'r' 
+    _fh := C.fopen(path.cstr(), mode.cstr())
+    if isnil(_fh) {
+	      return HANDLE(INVALID_HANDLE_VALUE)
+    }
+    _handle := C._get_osfhandle(C._fileno(_fh)) // CreateFile? - hah, no -_-
+    return _handle
+}
