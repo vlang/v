e2c523e6f3ee1e98308ad8a18274b435faf5c950
Paparoni Alvardo <ntw1997@yandex.ru>
2019-07-05 11:02:56 +0300

    Delete INVALID_HANDLE_VALUE

--- vlib/os/const.v
+++ vlib/os/const.v
@@ -106,11 +106,6 @@ const (
 	DEFFILEMODE = (S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH) /* 0666*/
 )
 
-// Windows 
-const(
-	INVALID_HANDLE_VALUE = -1
-)
-
 const(
     STD_INPUT_HANDLE  = -10
     STD_OUTPUT_HANDLE = -11
