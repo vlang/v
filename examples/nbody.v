287310058f060ae1252b644afcd8ea633e5ef38e
vertical222 <52521763+vertical222@users.noreply.github.com>
2019-07-06 23:36:12 +0800

    As requested to optimize array

--- examples/nbody.v
+++ examples/nbody.v
@@ -3,10 +3,6 @@
 // -0.169075164
 // -0.169059907
 
-// Elapsed time: 4.91s
-// Maximum resident (kbytes): 788
-// Between Rust and Go
-
 import math
 
 const (
@@ -103,22 +99,24 @@ fn energy(sys System) f64 {
 }
 
 fn arr_momentum() []Momentum {
-    mut x := []Momentum
-    x << Momentum {0.0, 0.0, 0.0, SolarMass}
-    x << Momentum {1.66007664274403694e-03 * DaysPerYear, 7.69901118419740425e-03 * DaysPerYear, -6.90460016972063023e-05 * DaysPerYear, 9.54791938424326609e-04 * SolarMass}
-    x << Momentum {-2.76742510726862411e-03 * DaysPerYear, 4.99852801234917238e-03 * DaysPerYear, 2.30417297573763929e-05 * DaysPerYear, 2.85885980666130812e-04 * SolarMass}
-    x << Momentum {2.96460137564761618e-03 * DaysPerYear, 2.37847173959480950e-03 * DaysPerYear, -2.96589568540237556e-05 * DaysPerYear, 4.36624404335156298e-05 * SolarMass}
-    x << Momentum {2.68067772490389322e-03 * DaysPerYear, 1.62824170038242295e-03 * DaysPerYear, -9.51592254519715870e-05 * DaysPerYear, 5.15138902046611451e-05 * SolarMass}
+    mut x := [
+    Momentum {0.0, 0.0, 0.0, SolarMass},
+    Momentum {1.66007664274403694e-03 * DaysPerYear, 7.69901118419740425e-03 * DaysPerYear, -6.90460016972063023e-05 * DaysPerYear, 9.54791938424326609e-04 * SolarMass},
+    Momentum {-2.76742510726862411e-03 * DaysPerYear, 4.99852801234917238e-03 * DaysPerYear, 2.30417297573763929e-05 * DaysPerYear, 2.85885980666130812e-04 * SolarMass},
+    Momentum {2.96460137564761618e-03 * DaysPerYear, 2.37847173959480950e-03 * DaysPerYear, -2.96589568540237556e-05 * DaysPerYear, 4.36624404335156298e-05 * SolarMass},
+    Momentum {2.68067772490389322e-03 * DaysPerYear, 1.62824170038242295e-03 * DaysPerYear, -9.51592254519715870e-05 * DaysPerYear, 5.15138902046611451e-05 * SolarMass},
+    ]
     return x
 }
 
 pub fn arr_position() []Position {
-    mut x := []Position
-    x << Position {0.0, 0.0, 0.0}
-    x << Position {4.84143144246472090e+00, -1.16032004402742839e+00, -1.03622044471123109e-01}
-    x << Position {8.34336671824457987e+00, 4.12479856412430479e+00, -4.03523417114321381e-01}
-    x << Position {1.28943695621391310e+01, -1.51111514016986312e+01, -2.23307578892655734e-01} 
-    x << Position {1.53796971148509165e+01, -2.59193146099879641e+01, 1.79258772950371181e-01}
+    mut x := [
+    Position {0.0, 0.0, 0.0},
+    Position {4.84143144246472090e+00, -1.16032004402742839e+00, -1.03622044471123109e-01},
+    Position {8.34336671824457987e+00, 4.12479856412430479e+00, -4.03523417114321381e-01},
+    Position {1.28943695621391310e+01, -1.51111514016986312e+01, -2.23307578892655734e-01},
+    Position {1.53796971148509165e+01, -2.59193146099879641e+01, 1.79258772950371181e-01},
+    ]
     return x
 }
 
