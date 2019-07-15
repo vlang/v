8e7c2ea828e123c4e05e86358c47c4677d42e0f7
Joe Conigliaro <joe.conigliaro@gmail.com>
2019-07-16 01:24:22 +1000

    fix (c)

--- vlib/crypto/sha1/sha1.v
+++ vlib/crypto/sha1/sha1.v
@@ -1,6 +1,6 @@
-// Copyright 2009 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// Copyright (c) 2019 Alexander Medvednikov. All rights reserved.
+// Use of this source code is governed by an MIT license
+// that can be found in the LICENSE file.
 
 // Package sha1 implements the SHA-1 hash algorithm as defined in RFC 3174.
 //
