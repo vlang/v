251e32948e9e7197b29e82ef37d62331910854e3
Alexander Medvednikov <alexander@medvednikov.com>
2019-07-17 13:50:58 +0200

    fix Windows build: move hostname to net/

--- vlib/os/os.v
+++ vlib/os/os.v
@@ -484,17 +484,6 @@ pub fn user_os() string {
 }
 
 
-// hostname returns the host name reported by the kernel. 
-pub fn hostname() ?string {
-  	mut name := [256]byte
-	// https://www.ietf.org/rfc/rfc1035.txt
-	// The host name is returned as a null-terminated string.
-	res := C.gethostname(&name, 256)
-	if res != 0 {
-		return error('os.hostname() cannot get the host name')
-	}
-  	return tos_clone(name)
-}
 
 // home_dir returns path to user's home directory.
 pub fn home_dir() string {
