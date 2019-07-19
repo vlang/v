02b7a988fd64ffa4bcc367266b2730e211262acb
Archan Patkar <pat@archanpatkar.com>
2019-07-19 11:55:50 +0530

    checking value

--- vlib/math/stats_test.v
+++ vlib/math/stats_test.v
@@ -35,6 +35,7 @@ fn test_geometric_mean() {
 	assert o.str().eq('5.159932')
 	data = [f64(-3.0),f64(67.31),f64(4.4),f64(1.89)]
 	o = stats.geometric_mean(data)
+	println(o)
 	// Some issue with precision comparison in f64 using == operator hence serializing to string
 	assert o.str().eq('nan') || o.str().eq('NAN') || o == f64(0) // Because in math it yields a complex number
 	data = [f64(12.0),f64(7.88),f64(76.122),f64(54.83)]
