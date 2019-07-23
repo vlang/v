fd94e3e36e56e9ee94188472104d5f32e071ffa8
0x9ef <0x9ef@tutanota.com>
2019-07-24 00:55:17 +0300

    Add mutex support for windows

--- vlib/sync/sync_win.v
+++ vlib/sync/sync_win.v
@@ -3,15 +3,77 @@
 // that can be found in the LICENSE file.
 
 module sync
+import os
+
+// Mutex HANDLE
+type MHANDLE voidptr
 
 struct Mutex {
+mut:
+    counter i64     // counter
+    wstate  u32     // wait state
+    mx      MHANDLE // mutex handle
 }
 
-fn (m Mutex) lock() {
-panic('not implemented') 
+const (
+    WAIT   = u32(8)  // Waiting mutex
+    WOKEN  = u32(16) // Woken mutex
+    ABOND  = u32(32)
+    BROKEN = u32(64)
+    DESTROYED = u32(0)
+)
+
+const (
+    INFINITY = 0xffffffff
+)
+
+// Ref - https://docs.microsoft.com/en-us/windows/win32/api/synchapi/nf-synchapi-waitforsingleobject#return-value
+const (
+    WAIT_ABANDONED     = 0x00000080
+    WAIT_IO_COMPLETION = 0x000000C0
+    WAIT_OBJECT_0      = 0x00000000
+    WAIT_TIMEOUT       = 0x00000102
+    WAIT_FAILED        = 0xFFFFFFFF
+)
+
+pub fn (m &Mutex) lock() {
+    // if mutex handle not initalized
+    if m.mx == MHANDLE(0) {
+        m.mx = C.CreateMutex(0, false, 0)
+        _pmhx := int(m.mx)
+        if (((_pmhx & 0xff) - 1) == 0) || (_pmhx == os.INVALID_HANDLE_VALUE) {
+            m.wstate = BROKEN // handle broken and mutex state are broken
+            return
+        }
+    }
+    state := C.WaitForSingleObject(m.mx, INFINITY) // infinity wait
+    match state {
+        WAIT_FAILED    => m.wstate = BROKEN
+        WAIT_ABANDONED => m.wstate = ABOND
+        WAIT_OBJECT_0  => m.wstate = WAIT
+    }
+    // todo implement atomic counter
 }
 
-fn (m Mutex) unlock() {
-panic('not implemented') 
+pub fn (m &Mutex) unlock() {
+    _pmx := &m.mx
+    if _pmx != os.INVALID_HANDLE_VALUE {
+        if m.wstate == (WAIT & u32(0xff)) {
+            if C.ReleaseMutex(_pmx) != 0 {
+                m.wstate = WOKEN // woken up mutex
+                return
+            }
+            m.wstate = ABOND
+            return
+        }
+    }
+    m.wstate = BROKEN
 }
 
+pub fn (m &Mutex) destroy() {
+    if m.wstate == WAIT {
+        m.unlock() // unlock mutex before destroying
+    }
+    m.wstate = DESTROYED // setting up reference to invalid state
+    C.CloseHandle(m.mx) // destroy mutex
+}
