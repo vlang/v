298ea141c24ddf7bb72a34bd48026498a0e4488b
F1ssi0N <emily33901@gmail.com>
2019-07-23 14:09:10 +0100

    Properly.

--- compiler/msvc.v
+++ compiler/msvc.v
@@ -3,9 +3,9 @@ module main
 import os
 
 #flag -I @VROOT/thirdparty/microsoft_craziness
-#flag @VROOT/thirdparty/microsoft_craziness/microsoft_craziness.o
-#flag -l ole32
-#flag -l oleaut32
+#flag windows @VROOT/thirdparty/microsoft_craziness/microsoft_craziness.o
+#flag windows -l ole32
+#flag windows -l oleaut32
 
 // Emily: If these arent included then msvc assumes that 
 // these return int (which should be 64bit)
