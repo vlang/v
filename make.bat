736e65527d0002f178e2c4308c47774762c8065e
Chanrles WANG <changrui@users.noreply.github.com>
2019-07-25 21:27:46 +0800

    Update make.bat
    Let vlang compile itself under Windows.

--- make.bat
+++ make.bat
@@ -1,3 +1,5 @@
 curl -O https://raw.githubusercontent.com/vlang/vc/master/v.c
 gcc -std=gnu11 -DUNICODE -D_UNICODE -w -o v.exe v.c
 del v.c
+v.exe -prod -o vv.exe compiler
+vv.exe -prod -o v.exe compiler
