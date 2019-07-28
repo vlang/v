287b6787f05758f9f4ebd9ba3602e9dbebd49907
Joe Conigliaro <joe.conigliaro@gmail.com>
2019-07-28 14:17:50 +1000

    add submodule test

--- compiler/tests/module_test.v
+++ compiler/tests/module_test.v
@@ -1,14 +1,18 @@
 import os
 import time as t
+import crypto.sha256 as s2
 
 import (
 	math
 	log as l
+	crypto.sha515 as s5
 )
 
 pub fn test_modules() {
 	assert os.SUCCESS == os.SUCCESS &&
 		t.MonthDays[0] == t.MonthDays[0] &&
+		s2.Size == s2.Size &&
 		math.Pi == math.Pi &&
-		l.INFO == l.INFO
+		l.INFO == l.INFO &&
+		s5.Size == s5.Size
 }
