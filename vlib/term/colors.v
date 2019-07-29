45029f8c86a401d637440d1fe9202223e327e2e9
joe-conigliaro <joe-conigliaro@users.noreply.github.com>
2019-07-29 12:44:21 +1000

    term: move enable_term_color_win() to colors_win.v

--- vlib/term/colors.v
+++ vlib/term/colors.v
@@ -6,38 +6,12 @@ module term
 
 import os
 
-// Calling this functions enables color terminal output on windows
-// Maybe implement a way to auto run an init function when a module
-// is imported on a certain os. for example to run this?
-pub fn enable_term_color_win() {
-    $if windows {
-        mode_wanted := os.ENABLE_PROCESSED_OUTPUT | os.ENABLE_VIRTUAL_TERMINAL_PROCESSING
-        mut mode_current := 0
-        h_output := C.GetStdHandle(os.STD_OUTPUT_HANDLE)
-        if h_output == os.INVALID_HANDLE_VALUE {
-            panic('term.enable_term_color_win(): error getting output handle.')
-        }
-        if !C.GetConsoleMode(h_output, &mode_current) {
-            panic('term.enable_term_color_win(): error getting console mode.')
-        }
-        if mode_wanted == mode_current {
-            return
-        }
-        if !C.SetConsoleMode(h_output, mode_wanted) {
-            panic('term.enable_term_color_win(): error setting console mode.')
-        }
-    }
-    $else {
-        println('term.enable_term_color_win() should only be called on windows.')
-    }
+fn _format(msg, open, close string) string {
+    return '\x1b[' + open + 'm' + msg + '\x1b[' + close + 'm'
 }
 
 pub fn format(msg, open, close string) string {
-    $if windows {
-        enable_term_color_win()
-    }
-
-    return '\x1b[' + open + 'm' + msg + '\x1b[' + close + 'm'
+    return _format(msg, open, close)
 }
 
 pub fn bg_black(msg string) string {
