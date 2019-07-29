a02080d066c755b1651ee6e4f032d38add096475
Delyan Angelov <delian66@gmail.com>
2019-07-29 10:46:04 +0300

    Add missing exit(1) to example news_fetcher.v .

--- examples/news_fetcher.v
+++ examples/news_fetcher.v
@@ -35,6 +35,7 @@ fn (f mut Fetcher) fetch() {
 		f.mu.unlock()
 		resp := http.get('https://hacker-news.firebaseio.com/v0/item/${id}.json') or {
 			println('failed to fetch data from /v0/item/${id}.json')
+			exit(1)
 		}
 		story := json.decode(Story, resp) or {
 			println('failed to decode a story')
