18de4622e53fe2fc3fa3ad4afe791c4766c83f98
Joe Conigliaro <joe.conigliaro@gmail.com>
2019-07-30 09:59:59 +1000

    more

--- vlib/crypto/rand/rand_lin.v
+++ vlib/crypto/rand/rand_lin.v
@@ -1,31 +1,16 @@
 module rand
 
-const (
-	// Getrand_Max_Bytes = 33554431
-	O_RDONLY = 0x0000
-	RNDGETENTCNT = 0x01
+// #include <unistd.h>
+#include <sys/syscall.h>
+#include <linux/random.h>
+
+import const (
+	SYS_getrandom
+	GRND_NONBLOCK
 )
 
-// import const (
-// 	RNDGETENTCNT
-// 	O_RDONLY
-// )
-
-pub fn read(bytes_needed int) []byte {	
-
+pub fn read(bytes_needed int) ?[]byte {	
 	mut buffer := malloc(bytes_needed)
-	mut entropy := 0
-	fd_random := C.open('/dev/urandom', O_RDONLY)
-  	// if !C.ioctl(fd_random, RNDGETENTCNT, &entropy) {
-	// 	// not random device
-	// 	panic('not random device')
-	// }
-	// if entropy < bytes_needed * 8 {
-	// 	// not enough entropy
-	// 	panic('not enough entropy')
-	// }
-	amount := C.read(fd_random, buffer, bytes_needed) //Grab as much data as we possibly can
-	C.close(fd_random)
-
+	C.syscall(SYS_getrandom, buffer, bytes_needed, GRND_NONBLOCK)
 	return new_array_from_c_array_no_alloc_rand(bytes_needed, 1, 1, buffer)
 }
--- vlib/crypto/rand/rand_nix.v
+++ vlib/crypto/rand/rand_nix.v
@@ -1,5 +1,25 @@
 module rand
 
+const (
+	// Getrand_Max_Bytes = 33554431
+	// O_RDONLY = 0x0000
+	// RNDGETENTCNT = 0x01
+)
+
+	// mut entropy := 0
+	// fd_random := C.open('/dev/urandom', O_RDONLY)
+  	// if !C.ioctl(fd_random, RNDGETENTCNT, &entropy) {
+	// 	// not random device
+	// 	panic('not random device')
+	// }
+	// if entropy < bytes_needed * 8 {
+	// 	// not enough entropy
+	// 	panic('not enough entropy')
+	// }
+	// amount := C.read(fd_random, buffer, bytes_needed) //Grab as much data as we possibly can
+	// C.close(fd_random)
+
+
 // // Alternate pseudo-random implementation for use on
 // // systems without a reliable /dev/urandom.
 
