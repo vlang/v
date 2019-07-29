626da27ad632ac06da5b3f12c02485445410d9f4
Alexander Medvednikov <alexander@medvednikov.com>
2019-07-29 19:39:52 +0200

    better C error message

--- compiler/main.v
+++ compiler/main.v
@@ -806,10 +806,11 @@ mut args := ''
 		println('cc took $diff ms') 
 		println('=========\n')
 	}
-	// println('C OUTPUT:')
 	if res.contains('error: ') {
-		println(res)
-		panic('clang error')
+		if v.pref.is_debug { 
+			println(res)
+		} 
+		panic('C error. This should never happen. Please create a GitHub issue.')
 	}
 	// Link it if we are cross compiling and need an executable
 	if v.os == .linux && !linux_host && v.pref.build_mode != .build {
