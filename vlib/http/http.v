5c526e6cc69358cb57bf9d5768149e9da984e403
Delyan Angelov <delian66@gmail.com>
2019-07-29 11:17:03 +0300

    http: add http.request/3 returning ?Response. Use it inside http.get/1 and http.post/2 .

--- vlib/http/http.v
+++ vlib/http/http.v
@@ -28,25 +28,32 @@ pub:
 }
 
 // embed 'http'
-pub fn get(url string) ?string {
-	if url == '' {
-		return error('http: empty url')
+pub fn request(typ, url, data string) ?Response {
+	req := new_request('GET', url, '') or {
+		return error(err)
 	}
-	mut req := new_request('GET', url, '')
 	resp := req.do()
+	return resp
+}
+
+pub fn get(url string) ?string {
+	resp := request('GET', url, '') or {
+		return error(err)
+	}
 	return resp.body
 }
 
 pub fn post(url, data string) ?string {
-	if url == '' {
-		return error('http: empty url')
+	resp := request('POST', url, data) or {
+		return error(err)
 	}
-	req := new_request('POST', url, data)
-	resp := req.do()
 	return resp.body
 }
 
-pub fn new_request(typ, _url, _data string) *Request {
+pub fn new_request(typ, _url, _data string) ?Request {
+	if _url == '' {
+		return error('http: empty url')
+	}
 	mut url := _url
 	mut data := _data
 	// req.headers['User-Agent'] = 'V $VERSION'
@@ -56,7 +63,7 @@ pub fn new_request(typ, _url, _data string) *Request {
 		data = ''
 	}
 	// req.headers = new_map(0, sizeof(string))// []string{}
-	return &Request {
+	return Request {
 		typ: typ
 		url: url
 		data: data
@@ -66,12 +73,6 @@ pub fn new_request(typ, _url, _data string) *Request {
 	}
 }
 
-/* 
-fn (req &Request) do() Response {
-	mut resp := Response{}
-	return resp
-}
-*/
 fn (req mut Request) free() {
 	req.headers.free()
 }
