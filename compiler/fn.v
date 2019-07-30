73461b229ace0e6c185f346c734dcb39f40b5e15
Joe Conigliaro <joe.conigliaro@gmail.com>
2019-07-30 20:46:33 +1000

    add panic_debug

--- compiler/fn.v
+++ compiler/fn.v
@@ -752,6 +752,15 @@ fn (p mut Parser) fn_call_args(f mut Fn) *Fn {
 		p.check(.rpar)
 		return f
 	}
+	// Add debug information to panic when -debug is passed
+	if p.v.pref.is_debug && f.name == 'panic' {
+		fn_name := p.cur_fn.name.replace('${p.mod}__', '')
+		file_path := p.file_path.replace('\\', '\\\\') // esecape \
+		p.cgen.resetln(p.cgen.cur_line.replace(
+			'v_panic (',
+			'_panic_debug ($p.scanner.line_nr, tos2("$file_path"), tos2("$p.mod"), tos2("$fn_name"), '
+		))
+	}
 	// Receiver - first arg
 	for i, arg in f.args {
 		// println('$i) arg=$arg.name')
