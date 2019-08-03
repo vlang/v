d4c07d9b66a30b641c1052361a911f54522e3451
joe-conigliaro <joe-conigliaro@users.noreply.github.com>
2019-08-03 11:40:54 +1000

    hash.crc32: modify sum methods to take []byte instead of string

--- vlib/hash/crc32/crc32_test.v
+++ vlib/hash/crc32/crc32_test.v
@@ -1,10 +1,15 @@
 import hash.crc32
 
 fn test_hash_crc32() {
-	assert crc32.sum('testing crc32') == u32(1212124400)
-	assert crc32.sum('testing crc32').hex() == '0x483f8cf0'
+	b1 := 'testing crc32'.bytes()
+	sum1 := crc32.sum(b1)
+	assert sum1 == u32(1212124400)
+	assert sum1.hex() == '0x483f8cf0'
 
+	
 	c := crc32.new(crc32.IEEE)
-	assert c.checksum('testing crc32 again') == u32(1420327025)
-	assert c.checksum('testing crc32 again').hex() == '0x54a87871'
+	b2 := 'testing crc32 again'.bytes()
+	sum2 := c.checksum(b2)
+	assert sum2 == u32(1420327025)
+	assert sum2.hex() == '0x54a87871'
 }
