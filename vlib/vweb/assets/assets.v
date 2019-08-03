fade60a75be812766bfa218b0420b44b8cf5adf2
Joe Conigliaro <joe.conigliaro@gmail.com>
2019-08-03 16:59:41 +1000

    more

--- vlib/vweb/assets/assets.v
+++ vlib/vweb/assets/assets.v
@@ -41,23 +41,31 @@ pub fn (am mut AssetManager) combine_js(to_file bool) string {
 // if to_file is true combine will return the path to the combined file
 // if it is false combine will return the combined file contents
 pub fn (am mut AssetManager) combine(asset_type string, to_file bool) string {
-	mut out := ''
-	assets := am.get_assets(asset_type)
-	mut files_salt := ''
-	for asset in assets {
-		mut data := os.read_file(asset.file_path) or {
-			panic(err)
-			return ''
-		}
-		if am.minify {
-			if asset_type == 'css' {
-				// data = minify_css(data)
-			} else {
-				// data = minify_js(data)
+	cache_key := am.get_cache_key(asset_type)
+	out_file := '$am.cache_dir/${cache_key}.$asset_type'
+	// use cache 
+	if os.file_exists(out_file) {
+		if !to_file {
+			out := os.read_file(out_file) or {
+				return ''
 			}
 		}
+		return out_file
+	}
+	// rebuild
+	mut out := ''
+	for asset in am.get_assets(asset_type) {
+		mut data := os.read_file(asset.file_path) or {
+			return ''
+		}
 		out += data
-		files_salt += asset.file_path
+	}
+	if am.minify {
+		if asset_type == 'css' {
+			out = minify_css(out)
+		} else {
+			out = minify_js(out)
+		}
 	}
 	if !to_file {
 		return out
@@ -69,15 +77,26 @@ pub fn (am mut AssetManager) combine(asset_type string, to_file bool) string {
 	if !os.dir_exists(am.cache_dir) {
 		os.mkdir(am.cache_dir)
 	}
-	now := time.now().uni
-	hash := md5.sum(files_salt.bytes()).hex()
-	out_file := '$am.cache_dir/$hash-${now}.$asset_type'
 	file := os.create(out_file) or {
 		panic(err)
 	}
 	file.write(out)
 	file.close()
 	return out_file
+}
+
+fn (am mut AssetManager) get_cache_key(asset_type string) string {
+	mut files_salt := ''
+	mut latest_modified := 0
+	for asset in am.get_assets(asset_type) {
+		files_salt += asset.file_path
+		println('asset lm: ' + asset.last_modified.uni.str())
+		if asset.last_modified.uni > latest_modified {
+			latest_modified = asset.last_modified.uni
+		}
+	}
+	hash := md5.sum(files_salt.bytes()).hex()
+	return '$hash-$latest_modified'
 }
 
 pub fn (am mut AssetManager) include_css(combine bool) string {
@@ -122,6 +141,8 @@ pub fn (am mut AssetManager) add(asset_type, file string) ?bool {
 	if !os.file_exists(file) {
 		return error('vweb: cannot add asset $file, it does not exist.')
 	}
+	// lm := time.unix(os.file_last_mod_unix(file)
+	println('lm: ' + os.file_last_mod_unix(file).str())
 	asset := Asset{
 		file_path: file
 		last_modified: time.unix(os.file_last_mod_unix(file))
@@ -157,10 +178,11 @@ fn (am mut AssetManager) get_assets(asset_type string) []Asset {
 	return assets
 }
 
-pub fn minify_js() {
-
+// todo: implement advanced minification
+pub fn minify_css(css string) string {
+	return css.replace('\r\n', '').replace('\n', '')
 }
 
-pub fn minify_css() {
-
+pub fn minify_js(js string) string {
+	return js.replace('\r\n', '').replace('\n', '')
 }
