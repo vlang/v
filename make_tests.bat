3b937870d11f2d0a7a6d4642853716b3b322b00d
Emily Hudson <emily33901@gmail.com>
2019-08-04 12:02:06 +0000

    CI: on windows build both a gcc and msvc and test with both

--- make_tests.bat
+++ make_tests.bat
@@ -3,9 +3,14 @@ curl -O https://raw.githubusercontent.com/vlang/vc/master/v.c
 gcc -std=gnu11 -DUNICODE -D_UNICODE -w -o vc.exe v.c
 del v.c
 vc.exe -o v.exe compiler
+vc.exe -o v.msvc.exe compiler
 setlocal EnableDelayedExpansion
 for /r . %%x in (*_test.v) do (
   v -o test.exe -debug %%x
+  if !ERRORLEVEL! NEQ 0 goto :fail
+)
+for /r . %%x in (*_test.v) do (
+  v.msvc -o test.exe -debug %%x
   if !ERRORLEVEL! NEQ 0 goto :fail
 )
 goto :done
