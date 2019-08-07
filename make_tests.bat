5a6c9edbd8c6accb435ef93c9bd428381297c2ae
Emily Hudson <emily33901@gmail.com>
2019-08-07 13:52:27 +0000

    Add some debugging statements to make_tests.bat and build tests with -os msvc becuase Im dumb and didnt do that last time

--- make_tests.bat
+++ make_tests.bat
@@ -1,11 +1,20 @@
 @echo off
 curl -O https://raw.githubusercontent.com/vlang/vc/master/v.c
+
+echo DOWNLOAD COMPLETE
+
 gcc -std=gnu11 -DUNICODE -D_UNICODE -w -o vc.exe v.c
+
+echo BUILD COMPLETE
+
 del v.c
+
 vc.exe -o v.exe compiler
 v.exe -os msvc -o v.msvc.exe compiler
 v.msvc.exe -os msvc -o v.msvc.2.exe compiler
 v.msvc.exe -o v.gcc.exe compiler
+
+
 setlocal EnableDelayedExpansion
 for /r . %%x in (*_test.v) do (
   v -o test.exe -debug %%x
@@ -23,6 +32,24 @@ for /r . %%x in (*_test.v) do (
   v.gcc.exe -o test.exe -debug %%x
   if !ERRORLEVEL! NEQ 0 goto :fail
 )
+
+for /r . %%x in (*_test.v) do (
+  v -os msvc -o test.exe -debug %%x
+  if !ERRORLEVEL! NEQ 0 goto :fail
+)
+for /r . %%x in (*_test.v) do (
+  v.msvc.exe -os msvc -o test.exe -debug %%x
+  if !ERRORLEVEL! NEQ 0 goto :fail
+)
+for /r . %%x in (*_test.v) do (
+  v.msvc.2.exe -os msvc -o test.exe -debug %%x
+  if !ERRORLEVEL! NEQ 0 goto :fail
+)
+for /r . %%x in (*_test.v) do (
+  v.gcc.exe -os msvc -o test.exe -debug %%x
+  if !ERRORLEVEL! NEQ 0 goto :fail
+)
+
 goto :done
 
 :fail
