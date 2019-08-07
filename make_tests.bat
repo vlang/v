e1a235bf186e1c0f3a0502cb8da28cc3d0f13cf7
Emily Hudson <emily33901@gmail.com>
2019-08-07 14:48:38 +0000

    Cleanup make_tests.bat

--- make_tests.bat
+++ make_tests.bat
@@ -1,22 +1,13 @@
-REM @echo off
+@echo off
+
 curl -O https://raw.githubusercontent.com/vlang/vc/master/v.c
-
-echo DOWNLOAD COMPLETE
-
 gcc -std=gnu11 -DUNICODE -D_UNICODE -w -o vc.exe v.c
-
-echo BUILD COMPLETE
-
-dir
-
 del v.c
 
 vc.exe -o v.exe compiler
 v.exe -os msvc -o v.msvc.exe compiler
 v.msvc.exe -os msvc -o v.msvc.2.exe compiler
 v.msvc.exe -o v.gcc.exe compiler
-
-dir
 
 setlocal EnableDelayedExpansion
 for /r . %%x in (*_test.v) do (
@@ -27,14 +18,6 @@ for /r . %%x in (*_test.v) do (
   v.msvc.exe -o test.exe -debug %%x
   if !ERRORLEVEL! NEQ 0 goto :fail
 )
-for /r . %%x in (*_test.v) do (
-  v.msvc.2.exe -o test.exe -debug %%x
-  if !ERRORLEVEL! NEQ 0 goto :fail
-)
-for /r . %%x in (*_test.v) do (
-  v.gcc.exe -o test.exe -debug %%x
-  if !ERRORLEVEL! NEQ 0 goto :fail
-)
 
 for /r . %%x in (*_test.v) do (
   v -os msvc -o test.exe -debug %%x
@@ -42,14 +25,6 @@ for /r . %%x in (*_test.v) do (
 )
 for /r . %%x in (*_test.v) do (
   v.msvc.exe -os msvc -o test.exe -debug %%x
-  if !ERRORLEVEL! NEQ 0 goto :fail
-)
-for /r . %%x in (*_test.v) do (
-  v.msvc.2.exe -os msvc -o test.exe -debug %%x
-  if !ERRORLEVEL! NEQ 0 goto :fail
-)
-for /r . %%x in (*_test.v) do (
-  v.gcc.exe -os msvc -o test.exe -debug %%x
   if !ERRORLEVEL! NEQ 0 goto :fail
 )
 
