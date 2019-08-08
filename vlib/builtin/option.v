34bf7326f570fd849dd7331b7174f231f224e394
Emily Hudson <emily33901@gmail.com>
2019-08-08 15:53:54 +0000

    Temporary fix for options - stack alloc up to size bytes

--- vlib/builtin/option.v
+++ vlib/builtin/option.v
@@ -5,19 +5,18 @@
 module builtin
 
 struct Option {
-	data [500]byte 
+	data *byte
 	error string
 	ok    bool
 }
 
 // `fn foo() ?Foo { return foo }` => `fn foo() ?Foo { return opt_ok(foo); }`
 fn opt_ok(data voidptr, size int) Option {
-	if size >= 500 {
-		panic('option size too big: $size (max is 500), this is a temporary limit') 
-	} 
-	res := Option { 
+	res := Option {
 		ok: true
+		data : C.alloca(size)
 	}
+
 	C.memcpy(res.data, data, size) 
 	return res 
 }
