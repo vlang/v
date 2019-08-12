66aac75d79fc10e22b562e45ae4923e272ef1e21
Emily Hudson <emily33901@gmail.com>
2019-08-12 14:25:46 +0000

    More descriptive echos

--- make_msvc.bat
+++ make_msvc.bat
@@ -2,7 +2,7 @@
 
 set exiterror=0
 
-REM find an MSVC installation
+echo finding an MSVC installation
 
 for /f "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
   set InstallDir=%%i
@@ -16,17 +16,18 @@ if exist "%InstallDir%\Common7\Tools\vsdevcmd.bat" (
   goto :nomsvc
 )
 
-REM get ourselves a copy of v.c
+echo fetch v.c
 curl -O https://raw.githubusercontent.com/vlang/vc/master/v.c
 
+echo build v.c with msvc
 cl.exe /w /volatile:ms /D_UNICODE /DUNICODE /Fo.v.c.obj /O2 /MD v.c user32.lib kernel32.lib advapi32.lib shell32.lib /link /NOLOGO /OUT:v2.exe /INCREMENTAL:NO
 
 if %ERRORLEVEL% GEQ 1 (
    goto :compileerror
 )
 
-REM rebuild from source
-v2.exe -os msvc -o v.exe compiler
+echo rebuild from source
+v2.exe -o v.exe compiler
 
 del .v.c.obj
 del v.c
