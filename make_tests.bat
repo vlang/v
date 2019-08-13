f9255d814542e9bc1e8e53d4919233dd5791ccda
Emily Hudson <emily33901@gmail.com>
2019-08-13 18:44:35 +0000

    Add dir for debugging

--- make_tests.bat
+++ make_tests.bat
@@ -22,6 +22,8 @@ echo build vc.msvc using vc
 vc.exe -o -os msvc v.msvc.exe compiler
 if %ERRORLEVEL% NEQ 0 goto :fail
 
+dir
+
 echo build v.msvc.3 using v
 v.exe -os msvc -o v.msvc.2.exe compiler
 if %ERRORLEVEL% NEQ 0 goto :fail
