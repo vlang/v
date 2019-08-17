20dd538b65ce79ab72f4907ba3dd065e7722a03b
Alexander Medvednikov <alexander@medvednikov.com>
2019-08-17 12:51:22 +0300

    make.bat: use v_win.c

--- make.bat
+++ make.bat
@@ -1,4 +1,4 @@
-curl -O https://raw.githubusercontent.com/vlang/vc/master/v.c
+curl -O https://raw.githubusercontent.com/vlang/vc/master/v_win.c
 gcc -std=gnu11 -DUNICODE -D_UNICODE -w -o v2.exe v.c
 v2.exe -o v.exe compiler
 del v2.exe
