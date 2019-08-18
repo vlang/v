30dc219e5bec47bb350108894576e79b266dd820
Chanrles WANG <changrui@users.noreply.github.com>
2019-08-18 19:09:16 +0800

    Update make.bat
    Compile vlang itself with v.

--- make.bat
+++ make.bat
@@ -1,5 +1,7 @@
 git clone --depth 1 --quiet https://github.com/vlang/vc
-gcc -std=gnu11 -DUNICODE -D_UNICODE -w -o v2.exe vc/v_win.c
-v2.exe -o v.exe compiler
+gcc -std=gnu11 -DUNICODE -D_UNICODE -w -o v1.exe vc/v_win.c
+v1.exe -o v2.exe compiler
+v2.exe -prod -o v.exe compiler
+del v1.exe
 del v2.exe
 rd /s /q vc
