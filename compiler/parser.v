2fa41d7bd317d3e09328e646854865baea4ee33a
teggot <danylo.lapirov@anvileight.com>
2019-08-19 20:22:30 +0300

    Separate match_st and implement match_expr

--- compiler/parser.v
+++ compiler/parser.v
@@ -1148,8 +1148,10 @@ fn (p mut Parser) statement(add_semi bool) string {
 		p.if_st(false, 0)
 	case Token.key_for:
 		p.for_st()
-	case Token.key_switch, Token.key_match:
+	case Token.key_switch:
 		p.switch_statement()
+	case Token.key_match:
+		p.match_statement(false)
 	case Token.key_mut, Token.key_static:
 		p.var_decl()
 	case Token.key_return:
@@ -2384,6 +2386,9 @@ fn (p mut Parser) factor() string {
 	case Token.key_if:
 		typ = p.if_st(true, 0)
 		return typ
+	case Token.key_match:
+		typ = p.match_statement(true)
+		return typ
 	default:
 		if p.pref.is_verbose || p.pref.is_debug {
 			next := p.peek()
@@ -3289,6 +3294,187 @@ fn (p mut Parser) switch_statement() {
 		i++
 	}
 	p.returns = false // only get here when no default, so return is not guaranteed
+}
+
+// Returns typ if used as expession 
+fn (p mut Parser) match_statement(is_expr bool) string {
+	p.check(.key_match)
+	p.cgen.start_tmp()
+	typ := p.bool_expression()
+	expr := p.cgen.end_tmp()
+
+	// TODO: introduce new variable to store result. We don't need to recalculate expression
+	// tmp_var := p.get_tmp()
+	// p.gen('$typ $tmp_var = $expr;')
+
+	p.check(.lcbr)
+	mut i := 0
+	mut all_cases_return := true
+
+	// stores typ of resulting variable 
+	mut res_typ := ''
+
+	defer {
+		p.check(.rcbr)
+	}
+
+	for p.tok != .rcbr {
+		if p.tok == .key_else { 
+			p.check(.key_else) 
+			p.check(.arrow)
+
+			// unwrap match if there is only else
+			if i == 0 {
+				if is_expr {
+					// statements are dissallowed (if match is expression) so user cant declare variables there and so on
+
+					// allow braces not to confuse anyone
+					got_brace := p.tok == .lcbr
+					if got_brace {
+						p.check(.lcbr)
+					}
+
+					p.gen('( ')
+
+					res_typ = p.bool_expression()
+
+					p.gen(' )')
+
+					// allow braces not to confuse anyone
+					if got_brace {
+						p.check(.rcbr)
+					}
+
+					return res_typ
+				} else {
+					p.genln('if ($expr) {')
+					p.statements()
+					p.returns = all_cases_return && p.returns
+					return ''
+				}
+			}
+
+			if is_expr {
+				// statements are dissallowed (if match is expression) so user cant declare variables there and so on
+				p.gen(':(')
+
+				p.check_types(p.bool_expression(), res_typ)
+				
+				p.gen(strings.repeat(`)`, i+1))
+
+				return res_typ
+			} else {
+				p.genln('else  { // default:')
+				p.statements()
+				p.returns = all_cases_return && p.returns
+				return ''
+			}
+		}
+
+		if i > 0 {
+			if is_expr {
+				p.gen(': (')
+			} else {
+				p.gen('else ')
+			}
+		} else if is_expr {
+			p.gen('(')
+		}
+
+		if is_expr {
+			p.gen('(')
+		} else {
+			p.gen('if (')
+		}
+		
+		// Multiple checks separated by comma
+		mut got_comma := false
+
+		for {
+			if got_comma {
+				p.gen(') || (')
+			}
+
+			if typ == 'string' { 
+				// TODO: use tmp variable
+				// p.gen('string_eq($tmp_var, ')
+				p.gen('string_eq($expr, ')
+			}
+			else {
+				// TODO: use tmp variable
+				// p.gen('($tmp_var == ')
+				p.gen('($expr == ')
+			}
+
+			p.expected_type = typ
+			p.check_types(p.bool_expression(), typ)
+			p.expected_type = ''
+
+			if p.tok != .comma {
+				break
+			}
+			p.check(.comma)
+			got_comma = true
+		}
+		p.gen(') )')
+		
+		p.check(.arrow)
+		
+		// statements are dissallowed (if match is expression) so user cant declare variables there and so on	
+		if is_expr {
+			p.gen('? ')
+			p.gen('(')
+
+			got_brace := p.tok == .lcbr
+
+			// allow braces not to confuse anyone
+			if got_brace {
+				p.check(.lcbr)
+			}
+			
+			if i == 0 {
+				// on the first iteration we set value of res_typ
+				res_typ = p.bool_expression()
+			} else {
+				// later on we check that the value is of res_typ type
+				p.check_types(p.bool_expression(), res_typ)
+			}
+
+			// allow braces not to confuse anyone
+			if got_brace {
+				p.check(.rcbr)
+			}
+
+			p.gen(')')
+		}
+		else {
+			p.match_parse_statement_branch()
+			// p.gen(')')
+		}
+
+		all_cases_return = all_cases_return && p.returns
+		i++
+	}
+	if is_expr {
+		// we get here if no else found, ternary requires "else" branch
+		p.error('Match expession requires "else"')
+	}
+	p.returns = false // only get here when no default, so return is not guaranteed
+
+	return ''
+}
+
+fn (p mut Parser) match_parse_statement_branch(){
+	p.genln('{ ')
+	if p.tok == .lcbr {
+		p.next()
+		p.statements()
+	} else {
+		p.cur_fn.open_scope()
+		p.statement(true)
+		p.cur_fn.close_scope()
+		p.genln('} ')
+	}
 }
 
 fn (p mut Parser) assert_statement() {
--- compiler/tests/match_test.v
+++ compiler/tests/match_test.v
@@ -1,12 +1,70 @@
 fn test_match() {
-	a := 3 
-	mut b := 0 
-	match a {
-	   2 => println('two') 
-	   3 => println('three') 
-	        b = 3 
-	   4 => println('four') 
-	else => println('???') 
-	} 
-	assert b == 3 
+	// a := 3 
+	// mut b := 0 
+	// match a {
+	//    2 => println('two') 
+	//    3 => println('three') 
+	//         b = 3 
+	//    4 => println('four') 
+	// else => println('???') 
+	// } 
+	// assert b == 3 
+
+	assert match 1 {
+        1 => 2
+        2 => 3
+        3 => {
+            3
+        }
+        4 => {
+            4
+        }
+        else => 5
+    } == 2
+    assert match 3 {
+        1 => 2
+        2 => 3
+        3 => {
+            3
+        }
+        4 => {
+            4
+        }
+        else => 5
+    } == 3
+    assert match 0 {
+        1 => 2
+        2 => 3
+        3 => {
+            3
+        }
+        4 => {
+            4
+        }
+        else => 5
+    } == 5
+
+    assert match 1 {
+        else => 5
+    } == 5
+    
+    mut a := 0
+    match 0 {
+        0 => a = 1
+        1 => {
+            a = a + 2
+        }
+    }
+    assert a == 1
+    
+    a = 0
+    match 1 {
+        0 => a = 1
+        1 => {
+            a = a + 2
+            a = a + 2
+            a = a + 2
+        }
+    }
+    assert a == 6
 } 
