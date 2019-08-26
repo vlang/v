be4359c8ec9bd20911c0ded826f373f0206c64e1
Delyan Angelov <delian66@gmail.com>
2019-08-26 09:45:09 +0300

    Cleanup compiler/tests/repl/repl_test.v output.

--- compiler/tests/repl/repl_test.v
+++ compiler/tests/repl/repl_test.v
@@ -1,33 +1,34 @@
 import os
 
 fn full_path_to_v() string {
-	// vexec := os.realpath('v')
 	vname  := if os.user_os() == 'windows' { 'v.exe' } else { 'v' }
 	vexec := os.dir(os.dir(os.dir(os.dir( os.executable() )))) + os.PathSeparator + vname
 	return vexec
 }
 
 fn test_the_v_compiler_can_be_invoked() {
+	vexec := full_path_to_v()
+	println('vexecutable: $vexec')
+	/*
 	args := os.args
 	vreal  := os.realpath('v')
 	myself := os.realpath( os.executable() )
-	vexec := full_path_to_v()
+	wd := os.getwd() + os.PathSeparator
 	println('args are: $args')
 	println('vreal   : $vreal')
 	println('myself  : $myself')
-	println('vexec   : $vexec')
+	println('wd      : $wd')
+    */
 	assert vexec != ''
 
 	vcmd := '$vexec --version'
 	r := os.exec(vcmd) or { panic(err) }
-	println('"$vcmd" output: $r.output')
-	println('"$vcmd" exit_code: $r.exit_code')
+	//println('"$vcmd" exit_code: $r.exit_code | output: $r.output')
 	assert r.exit_code == 0
 
 	vcmd_error := '$vexec nonexisting.v'
 	r_error := os.exec(vcmd_error) or { panic(err) }
-	println('"$vcmd_error" output: $r_error.output')
-	println('"$vcmd_error" exit_code: $r_error.exit_code')
+	//println('"$vcmd_error" exit_code: $r_error.exit_code | output: $r_error.output')
 	assert r_error.exit_code == 1
 	assert r_error.output == '`nonexisting.v` does not exist'
 }
@@ -35,19 +36,20 @@ fn test_the_v_compiler_can_be_invoked() {
 fn test_the_v_repl() {
 	test_files := os.walk_ext('.', '.repl')
 	wd := os.getwd() + os.PathSeparator
+	vexec := full_path_to_v()
+	
 	for file in test_files {
-		content := os.read_file(file) or {
+		fcontent := os.read_file(file) or {
 			assert false
 			break
 		}
+		content := fcontent.replace('\r', '')		
+		input := content.all_before('===output===\n')
+		output := content.all_after('===output===\n')
+		
 		input_temporary_filename := 'input_temporary_filename.txt'
-		input := content.replace('\r', '').all_before('===output===\n')
-		output := content.replace('\r', '').all_after('===output===\n')
 		os.write_file(input_temporary_filename, input)
-		defer {
-			os.rm(input_temporary_filename)
-		}
-		vexec := full_path_to_v()
+		defer { os.rm(input_temporary_filename) }		
 		r := os.exec('$vexec < $input_temporary_filename') or {
 			assert false
 			break
@@ -56,8 +58,8 @@ fn test_the_v_repl() {
 		assert result == output
 		if result != output {
 			println(file)
-			println('Got : $result')
-			println('Expected : $output')
+			println('Got : |$result|')
+			println('Expected : |$output|')
 		} else {
 			println('Repl file $file is OK')
 		}
