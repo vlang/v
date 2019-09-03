7d5967779ba4785b17519ab1358ba4dbf8e07779
joe-conigliaro <joe.conigliaro@gmail.com>
2019-09-03 17:11:50 +1000

    improve rand test to actually check for difference

--- vlib/crypto/rand/rand_test.v
+++ vlib/crypto/rand/rand_test.v
@@ -5,9 +5,26 @@
 import crypto.rand
 
 fn test_crypto_rand() {
-	r := rand.read(100) or {
+	no_bytes := 100
+	min_percentage_diff := 80
+
+	r1 := rand.read(no_bytes) or {
 		assert false
 		return
 	}
-	assert r.len == 100
+	assert r1.len == no_bytes
+	r2 := rand.read(no_bytes) or {
+		assert false
+		return
+	}
+	assert r2.len == no_bytes
+	
+	mut difference := 0
+	for i, _ in r1 {
+		difference += if r1[i] == r2[i] {0} else {1}
+	}
+	
+	diff_percentage := no_bytes/difference*100
+	
+	assert diff_percentage >= min_percentage_diff
 }
