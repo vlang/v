0e16b0ed6c0eb28709252aada5d20919d4250242
keito940 <keito940@live.jp>
2019-09-08 12:15:36 +0900

    TOML&EBNF Createing.

---
+++ vlib/ebnf/ebnf.primitive.v
@@ -0,0 +1,5 @@
+module ebnf
+
+struct Scanner {
+    
+}
---
+++ vlib/toml/toml_test.v
@@ -0,0 +1,17 @@
+import toml
+
+struct Table {
+    
+}
+
+struct Array {
+
+}
+
+fn test_parse_table(){
+
+}
+
+fn test_parse_array(){
+
+}
