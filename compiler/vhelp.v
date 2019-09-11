21a4a3a9fa6b5b1a993dddf185e97f7a384f36eb
Nicolas Sauzede <nsauzede@laposte.net>
2019-09-12 00:36:32 +0200

    Update vhelp.v

--- compiler/vhelp.v
+++ compiler/vhelp.v
@@ -21,19 +21,19 @@ const (
    
 Options:  
   -                 Shorthand for `v runrepl` .
-  -h, help          Display this information.
+  -h, --help        Display this information.
   -live             Enable hot code reloading (required by functions marked with [live]).
   -os <OS>          Produce an executable for the selected OS. 
                     OS can be linux, mac, windows, msvc, etc...
                     -os msvc is useful, if you want to use the MSVC compiler on Windows.
   -prod             Build an optimized executable.
-  -v, version       Display compiler version and git hash of the compiler source.
+  -v, --version     Display compiler version and git hash of the compiler source.
   
 Debugging options:
   -cc <ccompiler>   Specify which C compiler you want to use as a C backend. 
                     The C backend compiler should be able to handle C99 compatible C code.
                     Common C compilers are gcc, clang, tcc, icc, cl ...
-  -cflags flags     Pass additional C flags to the C backend compiler.
+  -cflags <flags>   Pass additional C flags to the C backend compiler.
                     Example: -cflags `sdl2-config --cflags`
                     
   -debug            Keep the generated C file for debugging in program.tmp.c even after compilation.
@@ -46,10 +46,10 @@ Debugging options:
 Subcommands:
   up                Update V. Run `v up` at least once per day, since V development is rapid and features/bugfixes are added constantly.
   run <file.v>      Build and execute the V program in file.v . You can add arguments for the V program *after* the file name.
-  build module      Compile a module into an object file.
+  build <module>    Compile a module into an object file.
   runrepl           Run the V REPL. If V is running in a tty terminal, the REPL is interactive, otherwise it just reads from stdin.
   symlink           Useful on unix systems. Symlinks the current V executable to /usr/local/bin/v, so that V is globally available.
-  install module    Install a user module from https://vpm.vlang.io/ .
+  install <module>  Install a user module from https://vpm.vlang.io/.
   test v            Run all V test files, and compile all V examples.
 
   fmt               Run vfmt to format the source code. [wip]
