1f7d3a857ae2f417fa2a352acafcd9fec34d35e5
Joe Conigliaro <joe.conigliaro@gmail.com>
2019-09-16 14:59:02 +1000

    use emtpy struct init 0 as default

--- compiler/cheaders.v
+++ compiler/cheaders.v
@@ -39,7 +39,7 @@ CommonCHeaders = '
 #endif
 
 #define EMPTY_STRUCT_DECLARATION
-#define EMPTY_STRUCT_INITIALIZATION
+#define EMPTY_STRUCT_INITIALIZATION 0
 #define OPTION_CAST(x) (x)
 
 #ifdef _WIN32
@@ -61,11 +61,9 @@ CommonCHeaders = '
 
 // MSVC cannot parse some things properly
 #undef EMPTY_STRUCT_DECLARATION
-#undef EMPTY_STRUCT_INITIALIZATION
 #undef OPTION_CAST
 
 #define EMPTY_STRUCT_DECLARATION int ____dummy_variable
-#define EMPTY_STRUCT_INITIALIZATION 0
 #define OPTION_CAST(x)
 #endif
 
