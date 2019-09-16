1e3c5d44c20c0f73817d2eceb48d47766c587e1e
Nicolas Sauzede <nsauzede@laposte.net>
2019-09-16 03:05:30 +0200

    Add rand_r

--- vlib/rand/rand.v
+++ vlib/rand/rand.v
@@ -12,4 +12,11 @@ pub fn next(max int) int {
 	return C.rand() % max
 }
 
-fn C.rand() int 
+fn C.rand() int
+
+pub fn rand_r(seed &int) int {
+	mut rs := seed
+	ns := ( *rs * 1103515245 + 12345 )
+	*rs = ns
+	return ns & 0x7fffffff
+}
