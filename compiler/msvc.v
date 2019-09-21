4317b5c5a531bf563e42723cf3fd7e7042c37d27
luoheng <1301089462@qq.com>
2019-09-21 08:38:40 +0800

    `!`: replace `!(pointer)` with isnil(pointer)

--- compiler/msvc.v
+++ compiler/msvc.v
@@ -45,7 +45,7 @@ fn find_windows_kit_internal(key RegKey, versions []string) ?string {
 			alloc_length := (required_bytes + 2)
 
 			mut value := &u16(malloc(alloc_length))
-			if !value {
+			if isnil(value) {
 				continue
 			}
 
