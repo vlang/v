367f8d6130bc8a775eafebe54f60e6f9e91b4d5c
Alexander Medvednikov <alexander@medvednikov.com>
2019-09-27 00:53:57 +0300

    solaris: add -lm

--- compiler/cc.v
+++ compiler/cc.v
@@ -172,7 +172,7 @@ fn (v mut V) cc() {
 	// Without these libs compilation will fail on Linux
 	// || os.user_os() == 'linux'
 	if v.pref.build_mode != .build_module && (v.os == .linux || v.os == .freebsd || v.os == .openbsd ||
-		v.os == .netbsd || v.os == .dragonfly) {
+		v.os == .netbsd || v.os == .dragonfly || v.os == .solaris) {
 		a << '-lm -lpthread '
 		// -ldl is a Linux only thing. BSDs have it in libc.
 		if v.os == .linux {
