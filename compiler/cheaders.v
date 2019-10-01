f35f6083cd73ecc0b44b57bb72d59614d99d10dd
hazohelet <48541090+hazohelet@users.noreply.github.com>
2019-10-02 03:21:05 +0900

    add C default comparison operator macros for future bitwise comparisons

--- compiler/cheaders.v
+++ compiler/cheaders.v
@@ -123,6 +123,11 @@ typedef map map_string;
 #define _IN(typ, val, arr) array_##typ##_contains(arr, val)
 #define _IN_MAP(val, m) map__exists(m, val)
 #define DEFAULT_EQUAL(a, b) (a == b)
+#define DEFAULT_NOT_EQUAL(a, b) (a != b)
+#define DEFAULT_LT(a, b) (a < b)
+#define DEFAULT_LE(a, b) (a <= b)
+#define DEFAULT_GT(a, b) (a > b)
+#define DEFAULT_GE(a, b) (a >= b)
 //================================== GLOBALS =================================*/
 byteptr g_str_buf;
 int load_so(byteptr);
