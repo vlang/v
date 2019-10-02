fce4199412cbc66a538423e84b2882c5f28c105e
Delyan Angelov <delian66@gmail.com>
2019-10-03 01:28:24 +0300

    compiler: let methods return voidptr without a segfault

---
+++ compiler/tests/return_voidptr_test.v
@@ -0,0 +1,23 @@
+
+struct Zest { val int }
+    
+fn (t Zest) get_a_finger_to_the_moon() voidptr {
+	return voidptr(0)
+}
+
+fn get_the_dao_way() voidptr {
+	return voidptr(0)
+}
+        
+fn test_returning_a_void_pointer_from_a_method() {
+	t := &Zest{ val: 123 }	
+	z := voidptr(0)
+	assert z == t.get_a_finger_to_the_moon()
+	assert t.get_a_finger_to_the_moon() == 0
+}
+
+fn test_returning_a_void_pointer_from_a_function() {
+	z := voidptr(0)
+	assert z == get_the_dao_way()
+	assert get_the_dao_way() == 0
+}
