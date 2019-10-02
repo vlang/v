7d763e0776b272dce8368a4498f37ed6390ccd71
Swastik Baranwal <swstkbaranwal@gmail.com>
2019-10-02 18:16:08 +0530

    fix js build

---
+++ vlib/builtin/js/hashmap.v
@@ -0,0 +1,13 @@
+// Copyright (c) 2019 Alexander Medvednikov. All rights reserved.
+// Use of this source code is governed by an MIT license
+// that can be found in the LICENSE file.
+
+module builtin
+
+/*
+TODO: Implement hashmap for js when hashmap for V is done.
+*/
+
+fn foo() {
+
+}
