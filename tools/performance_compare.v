9a594d1cc014fc8f381c729feac096c351a5d03f
Delyan Angelov <delian66@gmail.com>
2019-10-04 13:03:49 +0300

    tools/performance_compare is now more robust. It uses the C source from the time of the v commit, instead of always the latest one. It also now clones https://github.com/vlang/vc just once per comparison, not for every build.

--- tools/performance_compare.v
+++ tools/performance_compare.v
@@ -16,6 +16,7 @@ mut:
 	workdir       string // the working folder (typically /tmp), where the tool will write
 	a             string // the full path to the 'after' folder inside workdir
 	b             string // the full path to the 'before' folder inside workdir
+	vc            string // the full path to the vc folder inside workdir. It is used during bootstrapping v from the C source.
 	commit_before string // the git commit for the 'before' state
 	commit_after  string // the git commit for the 'after' state
 }
@@ -54,7 +55,9 @@ fn (c Context) compare_versions() {
 	// Input is validated at this point...
 	//Cleanup artifacts from previous runs of this tool:
 	os.chdir( c.workdir )
-	run('rm -rf "$c.a" "$c.b" ')
+	run('rm -rf "$c.a" "$c.b" "$c.vc" ')
+	// clone the VC source *just once per comparison*, and reuse it:
+	run('git clone --quiet https://github.com/vlang/vc	\'$c.vc\' ')
 
 	println('Comparing v compiler performance of commit $c.commit_before (before) vs commit $c.commit_after (after) ...')
 	c.prepare_v( c.b , c.commit_before )
@@ -73,30 +76,68 @@ fn show_sizes_of_files(files []string) {
 	}
 }
 
+fn line_to_timestamp_and_commit(line string) (int, string) {
+	parts := line.split(' ')
+	return parts[0].int(), parts[1]
+}
+
+fn (c &Context) prepare_vc_source( cdir string, commit string ) {
+	os.chdir( cdir )
+	// Building a historic v with the latest vc is not always possible ...
+	// It is more likely, that the vc *at the time of the v commit*,
+	// or slightly before that time will be able to build the historic v:
+	vline := run('git rev-list -n1 --timestamp \'$commit\' ')
+	v_timestamp, _ := line_to_timestamp_and_commit( vline )
+	os.chdir( c.vc )
+	run('git checkout master')
+	vcbefore := run('git rev-list HEAD -n1 --timestamp --before=$v_timestamp ')
+	_, vccommit_before := line_to_timestamp_and_commit( vcbefore )
+	run('git checkout \'$vccommit_before\' ')
+	os.chdir( cdir )
+}
+
 fn (c &Context) prepare_v( cdir string, commit string ) {
+	mut cc := os.getenv('CC')
+	if cc == '' { cc = 'cc' }
+
+	mut command_for_building_v_from_c_source := '$cc -std=gnu11 -w -o cv	 $c.vc/v.c	   -lm'
+	if 'windows' == os.user_os() {
+		command_for_building_v_from_c_source  = '$cc -std=gnu11 -w -o cv.exe $c.vc/v_win.c'
+	}
+
 	println('')
+	// prepare c.vc first
+	os.chdir( c.vc )
+	run('git checkout master')
+
 	println('Cloning current v source to $cdir ...')
 	os.system('git clone --quiet \'$c.cwd\' \'$cdir\' ')
 	os.chdir( cdir )
 	os.system('git checkout --quiet \'$commit\' ')
 
+	run('git clean -f')
+	c.prepare_vc_source( cdir, commit )
+
 	println('Making v and vprod compilers in $cdir')
+	run(command_for_building_v_from_c_source)
+	run('./cv		-o v	 compiler/ ')
+	run('./cv -prod -o vprod compiler/ ')
 
-	run('make')
-	run('./v	   -o v		compiler/ ')
-	run('./v -prod -o vprod compiler/ ')
-
-	run('cp v	  v_stripped')
-	run('cp vprod vprod_stripped')
+	run('cp cv		 cv_stripped')
+	run('cp v		 v_stripped')
+	run('cp vprod	 vprod_stripped')
 	run('strip *_stripped')
 
-	run('cp v_stripped		v_stripped_upxed')
-	run('cp vprod_stripped	vprod_stripped_upxed')
+	run('cp cv_stripped		 cv_stripped_upxed')
+	run('cp v_stripped		 v_stripped_upxed')
+	run('cp vprod_stripped	 vprod_stripped_upxed')
+	run('upx -qqq --lzma cv_stripped_upxed')
 	run('upx -qqq --lzma v_stripped_upxed')
 	run('upx -qqq --lzma vprod_stripped_upxed')
 
-	show_sizes_of_files(["$cdir/v",     "$cdir/v_stripped",     "$cdir/v_stripped_upxed"])
-	show_sizes_of_files(["$cdir/vprod", "$cdir/vprod_stripped", "$cdir/vprod_stripped_upxed"])
+	show_sizes_of_files(["$cdir/cv",    "$cdir/cv_stripped",     "$cdir/cv_stripped_upxed"])
+	show_sizes_of_files(["$cdir/v",     "$cdir/v_stripped",      "$cdir/v_stripped_upxed"])
+	show_sizes_of_files(["$cdir/vprod", "$cdir/vprod_stripped",  "$cdir/vprod_stripped_upxed"])
 	println("V version is: " + run("$cdir/v --version") + " , local source commit: " + run("git rev-parse --short  --verify HEAD") )
 	println('Source lines in compiler/ ' + run('wc compiler/*.v | tail -n -1') )
 }
@@ -150,8 +191,9 @@ fn main(){
 	validate_commit_exists( context.commit_before )
 	validate_commit_exists( context.commit_after )
 
-	context.b = context.normalized_workpath_for_commit( context.commit_before )
-	context.a = context.normalized_workpath_for_commit( context.commit_after )
+	context.b  = context.normalized_workpath_for_commit( context.commit_before )
+	context.a  = context.normalized_workpath_for_commit( context.commit_after )
+	context.vc = context.normalized_workpath_for_commit( 'vc' )
 
 	if !os.is_dir( context.workdir ) {
 		msg := 'Work folder: ' + context.workdir + ' , does not exist.'
