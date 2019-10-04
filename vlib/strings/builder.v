7a4a5da8d42bca66d320d9c396f226185fdd7d00
Joe Conigliaro <joe.conigliaro@gmail.com>
2019-10-04 16:05:27 +1000

    just comment out _make for now

--- vlib/strings/builder.v
+++ vlib/strings/builder.v
@@ -12,11 +12,8 @@ pub:
 }
 
 pub fn new_builder(initial_size int) Builder {
-	$if js {
-		return Builder {}
-	}
 	return Builder {
-		buf: _make(0, initial_size, sizeof(byte))
+		// buf: _make(0, initial_size, sizeof(byte))
 	}
 }
 
