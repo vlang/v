72ae12877ef9b30b9ca4e1c92c23e7e1a486ef58
joe-conigliaro <joe-conigliaro@users.noreply.github.com>
2019-10-05 18:07:10 +1000

    remvoe dupe in main & fix js string.builder (#2233)

--- vlib/strings/builder_js.v
+++ vlib/strings/builder_js.v
@@ -5,27 +5,39 @@
 module strings
 
 struct Builder {
+mut:
+	buf []byte
 pub:
 	len int
 }
 
 pub fn new_builder(initial_size int) Builder {
 	return Builder {
+		buf: _make(0, initial_size, sizeof(byte))
 	}
 }
 
 pub fn (b mut Builder) write(s string) {
+	b.buf._push_many(s.str, s.len)
+	//b.buf << []byte(s)  // TODO
+	b.len += s.len
 }
 
 pub fn (b mut Builder) writeln(s string) {
+	b.buf._push_many(s.str, s.len)
+	//b.buf << []byte(s)  // TODO
+	b.buf << `\n`
+	b.len += s.len + 1
 }
 
 pub fn (b Builder) str() string {
-	return ''
+	return string(b.buf, b.len)
 }
 
 pub fn (b mut Builder) cut(n int) {
+	b.len -= n
 }
 
 pub fn (b mut Builder) free() {
+	free(b.buf.data)
 }
