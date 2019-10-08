8ff84781bf4a7bb21b26495a306cbc5d669edb75
Joe Conigliaro <joe.conigliaro@gmail.com>
2019-10-09 00:00:52 +1100

    use char ptr instead

--- thirdparty/vschannel/vschannel.c
+++ thirdparty/vschannel/vschannel.c
@@ -930,7 +930,7 @@ static DWORD verify_server_certificate( PCCERT_CONTEXT  pServerCert, LPWSTR host
 	CHAR *rgszUsages[] = {  szOID_PKIX_KP_SERVER_AUTH,
 							szOID_SERVER_GATED_CRYPTO,
 							szOID_SGC_NETSCAPE };
-	DWORD cUsages = sizeof(rgszUsages) / sizeof(LPSTR);
+	DWORD cUsages = sizeof(rgszUsages) / sizeof(CHAR*);
 
 	PWSTR   pwszServerName = NULL;
 	DWORD   cchServerName;
