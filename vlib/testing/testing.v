ede9bfd8faf7639f2b36da11412e652ac91fb2e0
Michiel Vlootman <michiel@vlootman.com>
2019-10-10 16:44:06 +0200

    only support asserts for int (default) and string. is vs is_string

--- vlib/testing/testing.v
+++ vlib/testing/testing.v
@@ -1,26 +1,25 @@
 module testing
-/*
-asserts for V's basic types:
 
-testing.is(expected, actual, reason) bool               // tests equality
-testing.is_not(expected, actual, reason) bool           // tests inequality
-testing.is_match(expected, actual, reason) bool         // partial matching string | only on string types
+// is compares int for equality, printing the values if they differ 
+pub fn is(expected int, actual int, reason string) bool {
+        if expected != actual {
+                print_output(expected.str(), actual.str(), reason)
+                return false
+        }
+        return true
+}
 
-implement for all specific types (until we have generics)
-bool
-string
-i8    i16  int  i64
-byte  u16  u32  u64
-f32 f64
+// is compares int for inequality, printing the values if they differ 
+pub fn is_not(expected int, actual int, reason string) bool {
+        if expected == actual {
+                print_output('not `$expected`', actual.str(), reason)
+                return false
+        }
+        return true
+}
 
-// maybe add (convenience functions) 
-testing.is_before(expected, actual, reason) bool   // equal types:  time  [synonymn for is_less_than and is_greater_than]
-testing.is_after(expected, actual, reason) bool   // equal types:  time
-...
-
-*/
-
-fn is(expected string, actual string, reason string) bool {
+// is_string compares string for equality, printing the values if they differ 
+pub fn is_string(expected string, actual string, reason string) bool {
         if expected != actual {
                 print_output(expected, actual, reason)
                 return false
@@ -28,7 +27,8 @@ fn is(expected string, actual string, reason string) bool {
         return true
 }
 
-fn is_not(expected string, actual string, reason string) bool {
+// is_not_string compares string for inequality, printing the values if they differ 
+pub fn is_not_string(expected string, actual string, reason string) bool {
         if expected == actual {
                 print_output('not `$expected`', actual, reason)
                 return false
@@ -36,7 +36,8 @@ fn is_not(expected string, actual string, reason string) bool {
         return true
 }
 
-fn is_match(expected string, actual string, reason string) bool {
+// is_match checks if the expected string is found in the actual, printing the values if they differ 
+pub fn is_match(expected string, actual string, reason string) bool {
         if !actual.contains(expected) {
                 print_output('`$expected` to be in the actual string', actual, reason)
                 return false
--- vlib/testing/testing_test.v
+++ vlib/testing/testing_test.v
@@ -1,22 +1,42 @@
 import testing
 
+fn test_is_same_int(){
+        ok_assert := testing.is(int(42), int(42), "should match same int")
+        assert ok_assert == true
+}
+
+fn test_is_different_int(){
+        ok_assert := testing.is(int(42), int(12), "should match same int")
+        assert ok_assert == false
+}
+
+fn test_is_not_same_int(){
+        ok_assert := testing.is_not(42, 42, "should not match same int")
+        assert ok_assert == false
+}
+
+fn test_is_not_different_int(){
+        ok_assert := testing.is_not(42, 43, "should not match same int")
+        assert ok_assert == true
+}
+
 fn test_is_same_string(){
-        ok_assert := testing.is("a", "a", "should match same string")
+        ok_assert := testing.is_string("a", "a", "should match same string")
         assert ok_assert == true
 }
 
 fn test_is_different_string(){
-        ok_assert := testing.is("a", "b", "should match same string")
+        ok_assert := testing.is_string("a", "b", "should match same string")
         assert ok_assert == false
 }
 
 fn test_is_not_same_string(){
-        ok_assert := testing.is_not("a", "a", "should not match same string")
+        ok_assert := testing.is_not_string("a", "a", "should not match same string")
         assert ok_assert == false
 }
 
 fn test_is_not_different_string(){
-        ok_assert := testing.is_not("a", "b", "should not match same string")
+        ok_assert := testing.is_not_string("a", "b", "should not match same string")
         assert ok_assert == true
 }
 
@@ -29,29 +49,3 @@ fn test_is_match_on_nonmatch_string(){
         ok_assert := testing.is_match("wolf", "The quick brown fox jumps over the lazy dog", "should match substring")
         assert ok_assert == false
 }
-
-// // OUTPUTS
-// Testing...
-// -------------------------------------------------
-// compilation took: 883ms
-// running tests in: /home/michiel/v/src/github.com/mvlootman/v/vlib/testing/testing_test.v
-//    OK     0 ms |  1 assert  | test_is_same_string()
-// expected: a
-// actual  : b
-// reason  : should match same string
-//    OK     0 ms |  1 assert  | test_is_different_string()
-// expected: not `a`
-// actual  : a
-// reason  : should not match same string
-//    OK     0 ms |  1 assert  | test_is_not_same_string()
-//    OK     0 ms |  1 assert  | test_is_not_different_string()
-//    OK     0 ms |  1 assert  | test_is_match_on_matched_string()
-// expected: `wolf` to be in the actual string
-// actual  : The quick brown fox jumps over the lazy dog
-// reason  : should match substring
-//    OK     0 ms |  1 assert  | test_is_match_on_nonmatch_string()
-//           0 ms | <=== total time spent running V tests in "testing_test.v" 
-//  ok, fail, total =     6,     0,     6
-// ----------------------------------------------------------------------------
-//    887 ms | <=== total time spent running V _test.v files 
-//  ok, fail, total =     1,     0,     1
