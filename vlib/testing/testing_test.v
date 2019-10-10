6e5a457834f0c695d477a3c5a587925f464732f8
Michiel Vlootman <michiel@vlootman.com>
2019-10-10 12:10:50 +0200

    add output as comment

--- vlib/testing/testing_test.v
+++ vlib/testing/testing_test.v
@@ -30,3 +30,28 @@ fn test_is_match_on_nonmatch_string(){
         assert ok_assert == false
 }
 
+// // OUTPUTS
+// Testing...
+// -------------------------------------------------
+// compilation took: 883ms
+// running tests in: /home/michiel/v/src/github.com/mvlootman/v/vlib/testing/testing_test.v
+//    OK     0 ms |  1 assert  | test_is_same_string()
+// expected: a
+// actual  : b
+// reason  : should match same string
+//    OK     0 ms |  1 assert  | test_is_different_string()
+// expected: not `a`
+// actual  : a
+// reason  : should not match same string
+//    OK     0 ms |  1 assert  | test_is_not_same_string()
+//    OK     0 ms |  1 assert  | test_is_not_different_string()
+//    OK     0 ms |  1 assert  | test_is_match_on_matched_string()
+// expected: `wolf` to be in the actual string
+// actual  : The quick brown fox jumps over the lazy dog
+// reason  : should match substring
+//    OK     0 ms |  1 assert  | test_is_match_on_nonmatch_string()
+//           0 ms | <=== total time spent running V tests in "testing_test.v" 
+//  ok, fail, total =     6,     0,     6
+// ----------------------------------------------------------------------------
+//    887 ms | <=== total time spent running V _test.v files 
+//  ok, fail, total =     1,     0,     1
