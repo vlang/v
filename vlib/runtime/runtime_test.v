91bc4eeb30c2830f426ebe6b714ccd4eede4a993
Nicolas Sauzede <nsauzede@laposte.net>
2019-10-11 16:12:00 +0200

    Add windows support for nr_cpus()

--- vlib/runtime/runtime_test.v
+++ vlib/runtime/runtime_test.v
@@ -1,9 +1,16 @@
 import runtime
 
 fn test_nr_cpus() {
+	mut do_test := false
 	$if linux {
+		do_test = true
+	}
+	$if windows {
+		do_test = true
+	}
+	if do_test {
 		nr_cpus := runtime.nr_cpus()
 		println(nr_cpus)
-		assert nr_cpus >= 0
+		assert nr_cpus > 0
 	}
-}	
+}
