9193242a7652aafd2925d40a1c0a96ee400b6497
Alexander Medvednikov <alexander@medvednikov.com>
2019-10-11 14:16:02 +0300

    fix runtime module test

--- vlib/runtime/runtime.v
+++ vlib/runtime/runtime.v
@@ -1,13 +1,1 @@
-// Copyright (c) 2019 Alexander Medvednikov. All rights reserved.
-// Use of this source code is governed by an MIT license
-// that can be found in the LICENSE file.
-
 module runtime
-
-#include <sys/sysinfo.h>
-
-fn C.get_nprocs() int
-
-pub fn nr_cpus() int {
-	return C.get_nprocs()
-}
