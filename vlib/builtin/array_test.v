942c56ca954654c5e03c441103a069a8773466c2
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2019-10-11 09:12:40 +0800

    array: add reduce() method

--- vlib/builtin/array_test.v
+++ vlib/builtin/array_test.v
@@ -314,3 +314,27 @@ fn test_filter() {
 	assert d[0] == 'is'
 	assert d[1] == 'awesome'
 }
+
+fn sum(prev int, curr int) int {
+	return prev + curr
+}
+
+fn sub(prev int, curr int) int {
+	return prev - curr
+}
+
+fn test_reduce() {
+	a := [1, 2, 3, 4, 5]
+	b := a.reduce(sum, 0)
+	c := a.reduce(sum, 5)
+	d := a.reduce(sum, -1)
+	assert b == 15
+	assert c == 20
+	assert d == 14
+
+	e := [1, 2, 3]
+	f := e.reduce(sub, 0)
+	g := e.reduce(sub, -1)
+	assert f == -6
+	assert g == -7
+}
