d0a87d8a0e000981a9a20ddcf74b126cef1ce994
Alexander Medvednikov <alexander@medvednikov.com>
2019-10-12 00:54:25 +0300

    make $if work with includes: skip the body if the target is different

--- vlib/runtime/runtime_test.v
+++ vlib/runtime/runtime_test.v
@@ -2,8 +2,8 @@ import runtime
 
 fn test_nr_cpus() {
 	$if linux {
-	nr_cpus := runtime.nr_cpus()
-	println(nr_cpus)
-	assert nr_cpus > 0
+		nr_cpus := runtime.nr_cpus()
+		println(nr_cpus)
+		assert nr_cpus > 0
 	}
 }	
