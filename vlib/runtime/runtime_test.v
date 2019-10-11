04b9d572c8ac4e7f5a71140752f37231446e228c
Nicolas Sauzede <nsauzede@laposte.net>
2019-10-11 22:46:17 +0200

    Remove silly prints

--- vlib/runtime/runtime_test.v
+++ vlib/runtime/runtime_test.v
@@ -1,16 +1,12 @@
 import runtime
 
 fn test_nr_cpus() {
-	mut do_test := false
 	$if linux {
-		do_test = true
+		nr_cpus := runtime.nr_cpus()
+		assert nr_cpus > 0
 	}
 	$if windows {
-		do_test = true
-	}
-	if do_test {
 		nr_cpus := runtime.nr_cpus()
-		println(nr_cpus)
 		assert nr_cpus > 0
 	}
 }
--- vlib/runtime/runtime_win.v
+++ vlib/runtime/runtime_win.v
@@ -6,10 +6,8 @@ fn C.GetCurrentProcessorNumber() u32
 
 pub fn nr_cpus() int {
 	mut nr := int(C.GetCurrentProcessorNumber())
-	println(nr)
 	if nr == 0 {
 		nr = os.getenv('NUMBER_OF_PROCESSORS').int()
 	}
-	println(nr)
 	return nr
 }
