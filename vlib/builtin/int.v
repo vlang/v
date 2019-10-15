10ed65bc1236fa529a218e1fc2659928d6b2cc09
Alexander Medvednikov <alexander@medvednikov.com>
2019-10-15 03:45:11 +0300

    fix float.eq()

--- vlib/builtin/int.v
+++ vlib/builtin/int.v
@@ -43,10 +43,10 @@ pub fn (a f32) eq(b f32) bool {
 		$if tinyc {
 			return a -b <= 0.01
 		} $else {
-			return C.fabs(a - b) <= C.DBL_EPSILON	
+			return C.fabsf(a - b) <= C.FLT_EPSILON	
 		}
 	} $else {
-		return C.fabs(a - b) <= C.DBL_EPSILON	
+		return C.fabsf(a - b) <= C.FLT_EPSILON	
 	}
 }
 pub fn (a f64) eqbit(b f64) bool {
