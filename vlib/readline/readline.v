3d59ddfc09d16a23a6e58464594d3fdd578a9189
Henrixounez <30901439+Henrixounez@users.noreply.github.com>
2019-10-15 08:04:22 +0200

    readline: basic mac and windows version using os.getline

---
+++ vlib/readline/readline.v
@@ -0,0 +1,42 @@
+// Copyright (c) 2019 Alexander Medvednikov. All rights reserved.
+// Use of this source code is governed by an MIT license
+// that can be found in the LICENSE file.
+
+// Will serve as more advanced input method
+// Based on the work of https://github.com/AmokHuginnsson/replxx
+
+module readline
+
+// Linux
+// Used to change the terminal options
+struct Termios {
+mut:
+  c_iflag int
+  c_oflag int
+  c_cflag int
+  c_lflag int
+  c_cc [12]int //NCCS == 12. Cant use the defined value here
+}
+
+// Linux
+// Used to collect the screen information
+struct Winsize {
+  ws_row u16
+  ws_col u16
+  ws_xpixel u16
+  ws_ypixel u16
+}
+
+struct Readline {
+mut:
+  is_raw bool
+  orig_termios Termios // Linux
+  current ustring // Line being edited
+  cursor int // Cursor position
+  overwrite bool
+  cursor_row_offset int
+  prompt string
+  previous_lines []ustring
+  search_index int
+  is_tty bool
+}
