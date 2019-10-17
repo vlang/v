5971ff1c730ff903a63068e3b8ffc7bf0271fd50
Joe Conigliaro <joe.conigliaro@gmail.com>
2019-10-17 23:10:41 +1100

    mant int_size 32

--- vlib/strconv/atoi.v
+++ vlib/strconv/atoi.v
@@ -8,7 +8,8 @@ module strconv
 
 const(
     // int_size is the size in bits of an int or uint value.
-    int_size = 32 << (~u32(0) >> 63)
+    // int_size = 32 << (~u32(0) >> 63)
+	int_size = 32
     max_u64 = u64(u64(1<<64) - 1)
 )
 
