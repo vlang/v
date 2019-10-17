f49ab846c184b47ed766482dece2f574e5b753d3
Joe Conigliaro <joe.conigliaro@gmail.com>
2019-10-18 02:07:57 +1100

    remove extraneous cast

--- vlib/strconv/atoi.v
+++ vlib/strconv/atoi.v
@@ -12,7 +12,7 @@ const(
     // int_size = 32 << (~u32(0) >> 63)
     // max_u64 = u64(u64(1 << 63) - 1)
 	int_size = 32
-	max_u64  = u64(C.UINT64_MAX)         
+	max_u64  = u64(C.UINT64_MAX) // use this untill we add support
 )
 
 fn byte_to_lower(c byte) byte {
