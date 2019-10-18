e37d97444dc434a33845c9e7ed84d92befc90dca
Joe Conigliaro <joe.conigliaro@gmail.com>
2019-10-19 06:52:40 +1100

    fix blank ident & add cflag error

--- vlib/compiler/comptime.v
+++ vlib/compiler/comptime.v
@@ -178,13 +178,18 @@ fn (p mut Parser) chash() {
 	hash := p.lit.trim_space()
 	// println('chsh() file=$p.file  hash="$hash"')
 	p.next()
+	if !p.first_pass() {
+		return
+	}
 	if hash.starts_with('flag ') {
 		mut flag := hash.right(5)
 		// expand `@VROOT` `@VMOD` to absolute path
 		flag = flag.replace('@VROOT', p.vroot)
 		flag = flag.replace('@VMOD', v_modules_path)
 		//p.log('adding flag "$flag"')
-		p.table.parse_cflag(flag, p.mod)
+		_ = p.table.parse_cflag(flag, p.mod) or {
+			p.error_with_token_index(err, p.cur_tok_index()-1)
+		}
 		return
 	}
 	if hash.starts_with('include') {
