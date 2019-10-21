20df46a3f215e6c3f09b7c8e4f5adb2887a76d03
amalshaji <amalshajid@gmail.com>
2019-10-21 14:03:56 +0530

    Added Fibonacci using memoization and Factorial using recursion

---
+++ examples/factorial.v
@@ -0,0 +1,22 @@
+import os
+
+fn fact(x int) int {
+    if x == 1 {
+        return 1
+    } else {
+        return x * fact (x - 1)
+    }
+}
+
+fn main() {
+    if os.args.len != 2 {
+        println('usage: factorial [rank]')
+
+        // Exit
+        return
+    }
+
+    stop := os.args[1].int()
+    nfacto := fact(stop)
+    println(facto)
+}
--- examples/fibonacci.v
+++ examples/fibonacci.v
@@ -1,32 +1,34 @@
-// This program displays the fibonacci sequence
-
 import os
 
+struct Fib {
+        nums []int
+}
+
+fn (fib mut Fib) calc(n int) int {
+        if n <= 1 {
+                return n
+        }
+        if fib.nums[n] != 0 {
+                return fib.nums[n]
+        }
+        fib.nums[n] = fib.calc(n - 1) + fib.calc(n - 2)
+        return fib.nums[n]
+}
+
 fn main() {
-	// Check for user input
-	if os.args.len != 2 {
-		println('usage: fibonacci [rank]')
+		if os.args.len != 2 {
+			println('usage: fibonacci [rank]')
 
-		// Exit
-		return
-	}
-	
-	// Parse first argument and cast it to int
-	stop := os.args[1].int()
+			//Exit
+			return
+		}
 
-	// Three consecutive terms of the sequence
-	mut a := 0
-	mut b := 0
-	mut c := 1
+		max := os.args[1].int()
 
-	for i := 0; i < stop; i++ {
-		// Set a and b to the next term
-		a = b
-		b = c
-		// Compute the new term
-		c = a + b
-
-		// Print the new term
-		println(c)
-	}
+        mut fib := Fib {
+                nums: [0].repeat(max)
+        }
+        for i := 0; i < max; i++ {
+                println(fib.calc(i))
+        }
 }
