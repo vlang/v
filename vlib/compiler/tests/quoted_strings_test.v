5f4c6e280d593e872f36d4d15bfae8e6dda284d2
Delyan Angelov <delian66@gmail.com>
2019-10-22 11:33:00 +0300

    Cleanup formatting.

--- vlib/compiler/tests/quoted_strings_test.v
+++ vlib/compiler/tests/quoted_strings_test.v
@@ -1,9 +1,9 @@
 
 fn test_simple_quoted_strings(){
 	s := 'testing'
-	raw_s_a		  := r'testing'
-	raw_s_q		  := r"testing"
-	quoted_s	  :=  "testing"
+	raw_s_a       := r'testing'
+	raw_s_q       := r"testing"
+	quoted_s      :=  "testing"
 	///////////////////////////////////////
 	assert raw_s_a == raw_s_q
 	assert s == quoted_s
@@ -12,14 +12,14 @@ fn test_simple_quoted_strings(){
 }
 
 fn test_quoted_apostrophes(){
-	s		  := r"this 'should' be quoted"
+	s         := r"this 'should' be quoted"
 	quoted_s  :=  "this 'should' be quoted"
 	assert s == quoted_s
 }
 
 fn test_simple_interpolated_variables(){
 	s := 'abc'
-	quoted_s	  := "$s"
+	quoted_s      := "$s"
 	apostrophed_s := '$s'
 	assert s == quoted_s
 	assert s == apostrophed_s
@@ -27,8 +27,8 @@ fn test_simple_interpolated_variables(){
 
 fn test_interpolated_variables_in_curly_braces_with_format_params(){
 	s := 'abc'
-	preformatted_string := '|		abc|'
-	quoted_s	  := " ${s:10s} "
+	preformatted_string := '|       abc|'
+	quoted_s      := " ${s:10s} "
 	apostrophed_s := ' ${s:10s} '
 	assert preformatted_string == quoted_s
 	assert preformatted_string == apostrophed_s
@@ -38,16 +38,17 @@ fn test_interpolated_variables_in_curly_braces_with_format_params(){
 
 /* TODO: V error: C error
 fn test_apostrophed_quotes(){
-	s			  := r'this "should" be quoted'
+	s             := r'this "should" be quoted'
 	apostrophed_s :=  'this "should" be quoted'
 	assert s == apostrophed_s
 }
 */
 
-/* TODO: expected `|` but got `:`
+
+/* TODO: compiler error: expected `|` but got `:`
 fn test_interpolated_variables_in_curly_braces(){
 	s := 'abc'
-	quoted_s	  := "${s}"
+	quoted_s      := "${s}"
 	apostrophed_s := '$s'
 	assert s == quoted_s
 	assert s == apostrophed_s
