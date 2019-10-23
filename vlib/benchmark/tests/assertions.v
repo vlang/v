dde267e9934ae41829cf66bc1aca12c1fbfffa3f
Delyan Angelov <delian66@gmail.com>
2019-10-23 21:01:51 +0300

    Fix look&feel of the enhanced assertion.

--- vlib/benchmark/tests/assertions.v
+++ vlib/benchmark/tests/assertions.v
@@ -1,6 +1,7 @@
 module main
 
 import os
+import term
 
 ////////////////////////////////////////////////////////////////////
 /// This file will get compiled as part of the main program,
@@ -12,16 +13,20 @@ import os
 ////////////////////////////////////////////////////////////////////
 
 fn cb_assertion_failed(filename string, line int, sourceline string, funcname string){
+	color_on := term.can_show_color_on_stderr()
 	use_relative_paths := match os.getenv('VERROR_PATHS') {
 		'absolute' { false }
 		else { true }
 	}
 	final_filename := if use_relative_paths { filename } else { os.realpath( filename ) }
 	final_funcname := funcname.replace('main__','')
+
+	mut fail_message := 'FAILED assertion'
+	if color_on { fail_message = term.bold(term.red(fail_message)) }
 	
-	println('$final_filename:$line: assertion failure')
-	println('Func: $final_funcname')
-	println('Line: $sourceline')
+	eprintln('$final_filename:$line: $fail_message')
+	eprintln('Function: $final_funcname')
+	eprintln('Source  : $sourceline')
 }
 
 fn cb_assertion_ok(filename string, line int, sourceline string, funcname string){
