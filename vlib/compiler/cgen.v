1a2db54bc18a960ca7d7395deef2f104668c68f5
joe-conigliaro <joe-conigliaro@users.noreply.github.com>
2019-10-24 21:23:11 +1100

    compiler: cgen - fix typo. dependency was spelled wrong

--- vlib/compiler/cgen.v
+++ vlib/compiler/cgen.v
@@ -361,7 +361,7 @@ fn sort_structs(types []Type) []Type {
 	// sort graph
 	dep_graph_sorted := dep_graph.resolve()
 	if !dep_graph_sorted.acyclic {
-		verror('cgen.sort_structs(): the following structs form a dependancy cycle:\n' +
+		verror('cgen.sort_structs(): the following structs form a dependency cycle:\n' +
 			dep_graph_sorted.display_cycles() +
 			'\nyou can solve this by making one or both of the dependant struct fields references, eg: field &MyStruct' +
 			'\nif you feel this is an error, please create a new issue here: https://github.com/vlang/v/issues and tag @joe-conigliaro')
