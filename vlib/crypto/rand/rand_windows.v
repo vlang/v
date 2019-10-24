fa575625f94d8ce207fa7ac716d66fb829920689
Joe Conigliaro <joe.conigliaro@gmail.com>
2019-10-24 22:09:32 +1100

    add back windows consts

--- vlib/crypto/rand/rand_windows.v
+++ vlib/crypto/rand/rand_windows.v
@@ -8,11 +8,16 @@ module rand
 #flag windows -lbcrypt
 #include <bcrypt.h>
 
+const (
+	STATUS_SUCCESS                  = 0x00000000
+	BCRYPT_USE_SYSTEM_PREFERRED_RNG = 0x00000002
+)
+
 pub fn read(bytes_needed int) ?[]byte {	
 	mut buffer := malloc(bytes_needed)
 	// use BCRYPT_USE_SYSTEM_PREFERRED_RNG because we passed null as algo
-	status := C.BCryptGenRandom(0, buffer, bytes_needed, C.BCRYPT_USE_SYSTEM_PREFERRED_RNG)
-	if status != C.STATUS_SUCCESS {
+	status := C.BCryptGenRandom(0, buffer, bytes_needed, BCRYPT_USE_SYSTEM_PREFERRED_RNG)
+	if status != STATUS_SUCCESS {
 		return read_error
 	}
 	return c_array_to_bytes_tmp(bytes_needed, buffer)
