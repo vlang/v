4892cefde77bb58c1d86361976830fd4717fbd11
Ahgm <31023168+ahgm@users.noreply.github.com>
2019-11-03 00:29:06 +0330

    fixed "C:\v\tools\vrepl.v" could not be compiled.
    err:"V panic: V tool "C:\v\tools\vrepl.v" could not be compiled.
    print_backtrace_skipping_top_frames is not implemented on this platform for now..."
    it was my problem but temporarily i solved it better a better way

--- vlib/compiler/vtools.v
+++ vlib/compiler/vtools.v
@@ -31,7 +31,11 @@ pub fn launch_tool(tname string){
 	}
 	
 	if tool_should_be_recompiled {
-		compilation_command := '$vexe $tool_source'
+		mut ccompiler := ''
+		if (os.user_os() == 'windows' ) {
+				ccompiler = '-cc msvc'
+		}
+		compilation_command := '$vexe $ccompiler $tool_source'
 		//println('Compiling $tname with: "$compilation_command"')
 		tool_compilation := os.exec(compilation_command) or { panic(err) }
 		if tool_compilation.exit_code != 0 {
