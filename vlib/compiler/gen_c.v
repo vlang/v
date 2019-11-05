07e3699700b7d7d241f23bfd4f5e4d2c2859f537
Alexander Medvednikov <alexander@medvednikov.com>
2019-11-05 18:56:19 +0300

    parser: do not allow number => bool casts

--- vlib/compiler/gen_c.v
+++ vlib/compiler/gen_c.v
@@ -516,6 +516,9 @@ fn (p mut Parser) cast(typ string) {
 		p.cgen.set_placeholder(pos, '*($typ*)(')
 	}
 	else {
+		if (is_number_type(expr_typ) || is_float_type(expr_typ))  && typ == 'bool' {
+			p.error('cannot cast a number to `bool`')
+		}	
 		p.cgen.set_placeholder(pos, '($typ)(')
 	}
 	p.check(.rpar)
