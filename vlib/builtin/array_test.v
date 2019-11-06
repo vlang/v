f6d06fcda271a7929a6198701b203637d0a973d1
Alexander Medvednikov <alexander@medvednikov.com>
2019-11-06 06:57:04 +0300

    update tests and examples after the mutability fix

--- vlib/builtin/array_test.v
+++ vlib/builtin/array_test.v
@@ -262,8 +262,8 @@ fn test_push_many() {
 }
 
 fn test_reverse() {
-  mut a := [1, 2, 3, 4]
-	mut b := ['test', 'array', 'reverse']
+	a := [1, 2, 3, 4]
+	b := ['test', 'array', 'reverse']
 	c := a.reverse()
 	d := b.reverse()
 	for i, _  in c {
