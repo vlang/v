a44ba0b8a2de6a93773baa403b1681a757a819b3
Delyan Angelov <delian66@gmail.com>
2019-11-07 21:04:18 +0200

    bignum module wrapper for kokke/tiny-bignum-c

--- vlib/os/os_test.v
+++ vlib/os/os_test.v
@@ -1,5 +1,9 @@
 import os
 
+fn test_aaa_setup(){
+	cleanup_leftovers() assert true
+}
+
 fn test_setenv() {
   os.setenv('foo', 'bar', true)
   assert os.getenv('foo') == 'bar'
@@ -114,12 +118,11 @@ fn test_walk() {
 }
 
 fn test_cp() {
-  $if windows {
-    old_file_name := './example.txt'
-    new_file_name := './new_example.txt'
-
+    old_file_name := 'cp_example.txt'
+    new_file_name := 'cp_new_example.txt'
+    
     os.write_file(old_file_name, 'Test data 1 2 3, V is awesome #$%^[]!~⭐')
-    result := os.cp(old_file_name, new_file_name) or { panic('$err: errcode: $errcode') }
+    os.cp(old_file_name, new_file_name) or { panic('$err: errcode: $errcode') }
 
     old_file := os.read_file(old_file_name) or { panic(err) }
     new_file := os.read_file(new_file_name) or { panic(err) }
@@ -127,13 +130,11 @@ fn test_cp() {
 
     os.rm(old_file_name)
     os.rm(new_file_name)
-  }
 }
 
 fn test_cp_r() {
   //fileX -> dir/fileX
- // TODO clean up the files
- /*
+  // NB: clean up of the files happens inside the cleanup_leftovers function
   os.write_file('ex1.txt', 'wow!')
   os.mkdir('ex')
   os.cp_r('ex1.txt', 'ex', false) or { panic(err) }
@@ -148,7 +149,6 @@ fn test_cp_r() {
   assert old2 == new2
   //recurring on dir -> local dir
   os.cp_r('ex', './', true) or { panic(err) }
- */
 }
 
 //fn test_fork() {
@@ -173,3 +173,25 @@ fn test_cp_r() {
 //    println(cpid)
 //  }
 //}
+
+fn test_zzz_cleanup(){
+	cleanup_leftovers() assert true
+}
+
+
+// this function is called by both test_aaa_setup & test_zzz_cleanup
+// it ensures that os tests do not polute the filesystem with leftover
+// files so that they can be run several times in a row.
+fn cleanup_leftovers(){
+	// possible leftovers from test_cp
+	os.rm('cp_example.txt')
+	os.rm('cp_new_example.txt')
+	
+	// possible leftovers from test_cp_r
+	os.rm('ex/ex2/ex2.txt')
+	os.rm('ex/ex2')
+	os.rm('ex/ex1.txt')
+	os.rm('ex')  
+	os.rm('ex2/ex2.txt')
+	os.rm('ex2')  
+}
