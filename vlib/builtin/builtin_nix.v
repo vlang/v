c3056ccc8e6b262e5ed9b0d2ddcb5c8eeba0a1f1
Delyan Angelov <delian66@gmail.com>
2019-11-10 08:54:11 +0200

    Add a builtin_nix.v for symmetry and to fix osx and linux compilation.

---
+++ vlib/builtin/builtin_nix.v
@@ -0,0 +1,13 @@
+// Copyright (c) 2019 Alexander Medvednikov. All rights reserved.
+// Use of this source code is governed by an MIT license
+// that can be found in the LICENSE file.
+
+module builtin
+
+fn print_backtrace_skipping_top_frames_msvc(skipframes int) {
+	println('not implemented, see builtin_windows.v')
+}
+
+fn print_backtrace_skipping_top_frames_mingw(skipframes int) {
+	println('not implemented, see builtin_windows.v')
+}
