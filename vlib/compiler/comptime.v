861f2d4bc063248dea410b776107057bb6864fba
Alexander Medvednikov <alexander@medvednikov.com>
2019-11-13 21:14:37 +0300

    parser: skip $if windows etc blocks on a different os

--- vlib/compiler/comptime.v
+++ vlib/compiler/comptime.v
@@ -31,12 +31,15 @@ fn (p mut Parser) comp_time() {
 			}
 			p.check(.lcbr)
 			os := os_from_string(name)
-			if false && p.fileis('runtime.v') && os != p.os {
+			if (!not && os != p.os) || (not && os == p.os) {
 				// `$if os {` for a different target, skip everything inside
 				// to avoid compilation errors (like including <windows.h>
 				// on non-Windows systems)
 				mut stack := 1
 				for {
+					if p.tok == .key_return {
+						p.returns = true
+					}	
 					if p.tok == .lcbr {
 						stack++
 					} else if p.tok == .rcbr {
