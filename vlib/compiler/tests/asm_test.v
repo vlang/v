6c51d0bb7827e1c1344cf520131e2327f059d606
Alexander Medvednikov <alexander@medvednikov.com>
2019-11-17 01:07:21 +0300

    another inline asm test + disable on msvc

--- vlib/compiler/tests/asm_test.v
+++ vlib/compiler/tests/asm_test.v
@@ -1,14 +1,28 @@
 fn test_inline_asm() {
+	$if !windows {
+	$if !tinyc {
 	a := 10
 	b := 0
 	unsafe {
-		asm ("movl %1, %%eax;"
-      "movl %%eax, %0;"
-     :"=r"(b)
-     :"r"(a)
-     :"%eax"
-     )
+		asm (
+			"movl %1, %%eax;"
+			"movl %%eax, %0;"
+			:"=r"(b)
+			:"r"(a)
+			:"%eax"
+		)
 	}
 	assert a == 10
 	assert b == 10
+	//
+	e := 0
+	unsafe {
+		asm(
+			"movl $5, %0"
+			:"=a"(e)
+		)
+	}
+	assert e == 5
+	}
+	}
 }
