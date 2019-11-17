dfd196b4e8fdb63d7898cde6db3bd3f6c392825d
timbasel <tim@basel-berlin.de>
2019-11-17 10:59:04 +0100

    add float type with minor refactoring + expand example

--- examples/cli.v
+++ examples/cli.v
@@ -22,17 +22,26 @@ fn main() {
 		name: 'language',
 		abbrev: 'l',
 	})
+	greet_cmd.add_flag(cli.Flag{
+		flag: .int,
+		name: 'times',
+		value: '3',
+	})
 
 	cmd.add_command(greet_cmd)
 	cmd.parse(os.args)
 }
 
 fn greet_func(cmd cli.Command, args []string) {
-	language := cmd.flags.get_string('language') or { panic('failed to get language flag') }
-	match language {
-		'english' { println('Hello World') }
-		'german' { println('Hallo Welt') }
-		'dutch' { println('Hallo Wereld') }
-		else { println('unsupported language') }
+	language := cmd.flags.get_string('language') or { panic('failed to get \'language\' flag: $err') }
+	times := cmd.flags.get_int('times') or { panic('failed to get \'times\' flag: $err') }
+
+	for i := 0; i < times; i++ {
+		match language {
+			'english' { println('Hello World') }
+			'german' { println('Hallo Welt') }
+			'dutch' { println('Hallo Wereld') }
+			else { println('unsupported language') }
+		}
 	}
 }
--- vlib/cli/flag.v
+++ vlib/cli/flag.v
@@ -3,6 +3,7 @@ module cli
 pub enum FlagType {
 	bool
 	int
+	float
 	string
 }
 
@@ -17,30 +18,37 @@ mut:
 	value string
 }
 
+// check if first arg matches flag
 fn (flag mut Flag) matches(args []string) bool {
 	return 
 		(flag.name != '' && args[0].starts_with('--${flag.name}')) ||
 		(flag.abbrev != '' && args[0].starts_with('-${flag.abbrev}'))
 }
 
+// parse flag value from arguments and return arguments with all consumed element removed
 fn (flag mut Flag) parse(args []string) ?[]string {
 	if flag.matches(args) {
-		match flag.flag {
-			.bool { 
-				new_args := flag.parse_bool(args) or { return error(err) } 
-				return new_args 
-			}
-			.int { 
-				new_args := flag.parse_int(args) or { return error(err) } 
-				return new_args 
-			}
-			.string { 
-				new_args := flag.parse_string(args) or { return error(err) } 
-				return new_args 
-			}
+		if flag.flag == .bool {
+			new_args := flag.parse_bool(args) or { return error(err) }
+			return new_args
+		} else {
+			new_args := flag.parse_raw(args) or { return error(err) }
+			return new_args
 		}
+	} else {
+		return args
 	}
-	return args
+}
+
+fn (flag mut Flag) parse_raw(args []string) ?[]string {
+	if args[0].len > flag.name.len && args[0].contains('=') {
+		flag.value = args[0].split('=')[1]
+		return args.right(1)
+	} else if args.len >= 2 {
+		flag.value = args[1]
+		return args.right(2)
+	}
+	return error('missing argument for ${flag.name}')
 }
 
 fn (flag mut Flag) parse_bool(args []string) ?[]string {
@@ -56,30 +64,7 @@ fn (flag mut Flag) parse_bool(args []string) ?[]string {
 	}
 }
 
-fn (flag mut Flag) parse_int(args []string) ?[]string {
-	if args[0].len > flag.name.len && args[0].contains('=') {
-		flag.value = args[0].split('=')[1]
-		return args.right(1)
-	} else if args.len >= 2 {
-		flag.value = args[1]
-		return args.right(2)
-	}
-	return error('missing argument for ${flag.name}')
-}
-
-fn (flag mut Flag) parse_string(args []string) ?[]string {
-	if args[0].len > flag.name.len && args[0].contains('=') {
-		flag.value = args[0].split('=')[1]
-		return args.right(1)
-	} else if args.len >= 2 {
-		flag.value = args[1]
-		return args.right(2)
-	} 
-	return error('missing argument for ${flag.name}')
-}
-
-
-pub fn (flags []Flag) get_string(name string) ?string {
+fn (flags []Flag) get_raw(name string) ?string {
 	for flag in flags {
 		if flag.name == name {
 			return flag.value
@@ -89,11 +74,20 @@ pub fn (flags []Flag) get_string(name string) ?string {
 }
 
 pub fn (flags []Flag) get_bool(name string) ?bool {
-	value := flags.get_string(name) or { return error(err) }
+	value := flags.get_raw(name) or { return error(err) }
 	return value == 'true'
 }
 
 pub fn (flags []Flag) get_int(name string) ?int {
-	value := flags.get_string(name) or { return error(err) }
+	value := flags.get_raw(name) or { return error(err) }
 	return value.int()
+}
+
+pub fn (flags []Flag) get_float(name string) ?f32 {
+	value := flags.get_raw(name) or { return error(err) }
+	return value.f32()
+}
+
+pub fn (flags []Flag) get_string(name string) ?string {
+	return flags.get_raw(name)
 }
