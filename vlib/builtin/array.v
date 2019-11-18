66a9e0ddff9a97f1b6cd0af0884e454001c05011
Alexander Medvednikov <alexander@medvednikov.com>
2019-11-19 00:27:27 +0300

    array: fix sort() for ints

--- vlib/builtin/array.v
+++ vlib/builtin/array.v
@@ -349,11 +349,11 @@ pub fn copy(dst, src []byte) int {
 }
 
 // Private function. Comparator for int type.
-fn compare_ints(a, b int) int {
-	if a < b {
+fn compare_ints(a, b &int) int {
+	if *a < *b {
 		return -1
 	}
-	if a > b {
+	if *a > *b {
 		return 1
 	}
 	return 0
