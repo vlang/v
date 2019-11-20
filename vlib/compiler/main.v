65e478ce97db6ad4f914d0788089990fece2cfe6
Delyan Angelov <delian66@gmail.com>
2019-11-21 01:56:16 +0200

    Cleanup duplicate code in create_symlink .

--- vlib/compiler/main.v
+++ vlib/compiler/main.v
@@ -1038,30 +1038,18 @@ pub fn create_symlink() {
 	$if windows { return }
 	vexe := os.executable()
 	link_path := '/usr/local/bin/v'
-	if vexe[0..2] == './' {
-		vexe_full_path := os.getwd() + vexe
-		ret := os.system('ln -sf $vexe_full_path $link_path')
-		if ret == 0 {
-			println('symlink "$link_path" has been created')
-		} else {
-			println('failed to create symlink "$link_path", '+
-				'make sure you run with sudo')
-		}
+	ret := os.system('ln -sf $vexe $link_path')
+	if ret == 0 {
+		println('Symlink "$link_path" has been created')
 	} else {
-		ret := os.system('ln -sf $vexe $link_path')
-		if ret == 0 {
-			println('symlink "$link_path" has been created')
-		} else {
-			println('failed to create symlink "$link_path", '+
-				'make sure you run with sudo')
-		}
+		println('Failed to create symlink "$link_path". Try again with sudo.')
 	}
 }
 
 pub fn vexe_path() string {
 	vexe := os.getenv('VEXE')
 	if '' != vexe {	return vexe	}
-	return os.executable()
+	return os.realpath(os.executable())
 }
 
 pub fn verror(s string) {
