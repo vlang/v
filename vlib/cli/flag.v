597a6fead268df0b56d99635b190ffac4cb9b9a1
Tim Basel <timbasel@users.noreply.github.com>
2019-11-21 13:03:12 +0100

    vlib: cli module

---
+++ vlib/cli/flag.v
@@ -0,0 +1,110 @@
+module cli
+
+pub enum FlagType {
+	bool
+	int
+	float
+	string
+}
+
+pub struct Flag {
+pub mut:
+	flag FlagType
+	name string
+	abbrev string
+	description string
+	global bool
+	required bool
+
+	value string
+}
+
+pub fn (flags []Flag) get_bool(name string) ?bool {
+	flag := flags.get(name) or { return error(err) }
+	if flag.flag != .bool { return error('invalid flag type') }
+	return flag.value == 'true'
+}
+
+pub fn (flags []Flag) get_int(name string) ?int {
+	flag := flags.get(name) or { return error(err) }
+	if flag.flag != .int { return error('invalid flag type') }
+	return flag.value.int()
+}
+
+pub fn (flags []Flag) get_float(name string) ?f32 {
+	flag := flags.get(name) or { return error(err) }
+	if flag.flag != .float { return error('invalid flag type') }
+	return flag.value.f32()
+}
+
+pub fn (flags []Flag) get_string(name string) ?string {
+	flag := flags.get(name) or { return error(err) }
+	if flag.flag != .string { return error('invalid flag type') }
+	return flag.value
+}
+
+// parse flag value from arguments and return arguments with all consumed element removed
+fn (flag mut Flag) parse(args []string) ?[]string {
+	if flag.matches(args) {
+		if flag.flag == .bool {
+			new_args := flag.parse_bool(args) or { return error(err) }
+			return new_args
+		} else {
+			new_args := flag.parse_raw(args) or { return error(err) }
+			return new_args
+		}
+	} else {
+		return args
+	}
+}
+
+// check if first arg matches flag
+fn (flag mut Flag) matches(args []string) bool {
+	return 
+		(flag.name != '' && args[0].starts_with('--${flag.name}')) ||
+		(flag.abbrev != '' && args[0].starts_with('-${flag.abbrev}'))
+}
+
+fn (flag mut Flag) parse_raw(args []string) ?[]string {
+	if args[0].len > flag.name.len && args[0].contains('=') {
+		println('1')
+		flag.value = args[0].split('=')[1]
+		return args.right(1)
+	} else if args.len >= 2 {
+		flag.value = args[1]
+		return args.right(2)
+	}
+	return error('missing argument for ${flag.name}')
+}
+
+fn (flag mut Flag) parse_bool(args []string) ?[]string {
+	if args[0].len > flag.name.len && args[0].contains('=') {
+		flag.value = args[0].split('=')[1]
+		return args.right(1)
+	} else if args.len >= 2 { 
+		if args[1] in ['true', 'false'] {
+			flag.value = args[1]
+			return args.right(2)
+		} 
+	} 
+	flag.value = 'true'
+	return args.right(1)
+}
+
+fn (flags []Flag) get(name string) ?Flag {
+	for flag in flags {
+		if flag.name == name {
+			return flag
+		}
+	}
+	return error('flag ${name} not found.')
+}
+
+fn (flags []Flag) contains(name string) bool {
+	for flag in flags {
+		if flag.name == name || flag.abbrev == name {
+			return true
+		}
+	}
+	return false
+}
---
+++ vlib/cli/flag_test.v
@@ -0,0 +1,56 @@
+import cli
+
+fn test_if_string_flag_parses() {
+	mut flag := cli.Flag{
+		flag: .string,
+		name: 'flag',
+	}
+
+	flag.parse(['--flag', 'value']) or { panic(err) }
+	assert flag.value == 'value'
+
+	flag.parse(['--flag=value']) or { panic(err) }
+	assert flag.value == 'value'
+}
+
+fn test_if_bool_flag_parses() {
+	mut flag := cli.Flag{
+		flag: .bool,
+		name: 'flag',
+	}
+
+	flag.parse(['--flag']) or { panic(err) }
+	assert flag.value == 'true'
+
+	flag.parse(['--flag', 'true']) or { panic(err) }
+	assert flag.value == 'true'
+
+	flag.parse(['--flag=true']) or { panic(err) }
+	assert flag.value == 'true'
+}
+
+fn test_if_int_flag_parses() {
+	mut flag := cli.Flag{
+		flag: .int,
+		name: 'flag',
+	}
+
+	flag.parse(['--flag', '42']) or { panic(err) }
+	assert flag.value.int() == 42
+
+	flag.parse(['--flag=42']) or { panic(err) }
+	assert flag.value.int() == 42
+}
+
+fn test_if_float_flag_parses() {
+	mut flag := cli.Flag{
+		flag: .float,
+		name: 'flag',
+	}
+
+	flag.parse(['--flag', '3.14159']) or { panic(err) }
+	assert flag.value.f32() == 3.14159
+
+	flag.parse(['--flag=3.14159']) or { panic(err) }
+	assert flag.value.f32() == 3.14159
+}
