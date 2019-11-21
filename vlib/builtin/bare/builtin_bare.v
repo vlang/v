ecd3d3be2ca4c99b3c914888ff83f881adf8e6c0
Dwight Schauer <dwight.schauer@teegra.net>
2019-11-21 07:12:15 -0600

    Added print and println, completed panic and panic_debug with those

--- vlib/builtin/bare/builtin_bare.v
+++ vlib/builtin/bare/builtin_bare.v
@@ -1,13 +1,33 @@
 module builtin
 
-//pub fn syscall5(number, arg1, arg2, arg3, arg4, arg5 voidptr) voidptr
-//pub fn syscall6(number, arg1, arg2, arg3, arg4, arg5, arg6 voidptr) voidptr
+pub fn print(s string) {
+	write(1, s.str, u64(s.len))
+}
+
+pub fn println(s string) {
+	print(s)
+	write(1, "\n".str, 1)
+}
+
+// replaces panic when -debug arg is passed
+fn panic_debug(line_no int, file,  mod, fn_name, s string) {
+	println('================ V panic ================')
+	print('   module: ')
+	println('mod')
+	print(' function: ')
+	print(fn_name)
+	println('()')
+	println('     file: ')
+	println(file)
+	//println('     line: ${line_no}')
+	print('  message: ')
+	println(s)
+	println('=========================================')
+	exit(1)
+}
 
 pub fn panic(s string) {
-	//write(1, s.str, s.len)
+	print('V panic: ')
+	println(s)
+	exit(1)
 }
-
-pub fn panic_debug(s string) {
-	//write(1, s.str, s.len)
-}
-
