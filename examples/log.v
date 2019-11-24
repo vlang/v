d573216f3579be23bdb04d901f0b0811c892d4a8
_leif <zmeriksen@gmail.com>
2019-11-24 15:17:23 -0600

    Update log.v

--- examples/log.v
+++ examples/log.v
@@ -1,12 +1,12 @@
 import log
 
 fn main() {
-	mut l := Log { LogLevel.info, 'info', true }
+	mut l := log.Log { log.LogLevel.info, 'info', true }
 	l.info('info')
 	l.warn('warn')
 	l.error('error')
 	l.debug('no debug')
-	l.set_level(DEBUG)
+	l.set_level(log.DEBUG)
 	l.debug('debug')
 	l.fatal('fatal')
 }
