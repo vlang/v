6375cc8043a84be5b3ad9eb33101415972bcf636
Delyan Angelov <delian66@gmail.com>
2019-11-25 19:04:21 +0000

    Remove scaffolding printlns .

--- vlib/compiler/vtools.v
+++ vlib/compiler/vtools.v
@@ -4,20 +4,14 @@ import os
 
 pub fn launch_tool(tname string){
 	vexe := vexe_path()
-	println('launch_tool $tname | vexe: $vexe')
 	vroot := os.dir(vexe)
-	println('launch_tool $tname | vroot: $vroot')
 	mut oargs := os.args
-	println('launch_tool $tname | oargs: ')	 println(oargs)
 	oargs[0] = '"$vexe"' // make it more explicit
 	tool_exe := os.realpath('$vroot/tools/$tname')
 	tool_source := os.realpath('$vroot/tools/${tname}.v')
-				 println('launch_tool $tname | tool_exe: $tool_exe ')
-				 println('launch_tool $tname | tool_source: $tool_source ')
 	//////////////////////////////////////////////////////
 	tool_args := oargs.join(' ')
 	tool_command := '"$tool_exe" $tool_args'
-	println('Launching: "$tool_command" ...')
 	
 	mut tool_should_be_recompiled := false
 	if !os.file_exists( tool_exe ) {
@@ -40,9 +34,6 @@ pub fn launch_tool(tname string){
 		//println('Compiling $tname with: "$compilation_command"')
 		tool_compilation := os.exec(compilation_command) or { panic(err) }
 		if tool_compilation.exit_code != 0 {
-						    println('>> FAILING vexe: "$vexe" ')
-						    println('>> FAILING vroot: "$vroot" ')
-						    println('>> FAILING compilation_command: "$compilation_command" ')
 			panic('V tool "$tool_source" could not be compiled\n' + tool_compilation.output)
 		}
 	}
