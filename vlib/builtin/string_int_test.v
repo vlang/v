fef0d1e33cd70f63410e0323197a52d4d730ed64
Dwight Schauer <dwight.schauer@teegra.net>
2019-11-27 17:53:32 -0600

    Allow string.int() to stop (and not error) on invalid or non digit characters. Resolves #2901

--- vlib/builtin/string_int_test.v
+++ vlib/builtin/string_int_test.v
@@ -1,4 +1,6 @@
-fn test_flexible_atoi() {
+fn test_common_atoi() {
+	assert "70zzz".int() == 70
+	assert "2901issue".int() == 2901
 	assert '234232w'.int() == 234232
 	assert '-9009x'.int() == -9009
 	assert '0y'.int() == 0
