b21f102b6b7fd0c27ff23d9314ba8d4adce437f7
Delyan Angelov <delian66@gmail.com>
2019-11-29 23:25:21 +0200

    Fix utf8 tests.

--- vlib/encoding/utf8/utf8_util_test.v
+++ vlib/encoding/utf8/utf8_util_test.v
@@ -1,7 +1,9 @@
+import encoding.utf8
+
 fn test_utf8_util() {
 	src:="ăĂ ôÔ testo æ€”"
-	upper:=src.utf8_to_upper()
-	lower:=src.utf8_to_lower()
+	upper:=utf8.to_upper(src)
+	lower:=utf8.to_lower(src)
 	assert upper=="ĂĂ ÔÔ TESTO Æ€”"
 	assert lower=="ăă ôô testo æ€”"
 }
