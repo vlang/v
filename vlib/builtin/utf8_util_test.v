52425c14d9d64d3abbd957f91d0a8c7945161a66
penguindark <dariodeledda@hotmail.com>
2019-11-29 01:15:08 +0000

    fix on test assert error

--- vlib/builtin/utf8_util_test.v
+++ vlib/builtin/utf8_util_test.v
@@ -3,5 +3,5 @@ fn test_utf8_util() {
 	upper:=src.utf8_to_upper()
 	lower:=src.utf8_to_lower()
 	assert upper=="ĂĂ ÔÔ TESTO Æ€”"
-	assert lower=="ăă ôô TESTO æ€”"
+	assert lower=="ăă ôô testo æ€”"
 }
