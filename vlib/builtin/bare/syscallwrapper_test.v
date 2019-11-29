a8e85436396fd6943d384675dad7c9b36107752b
Dwight Schauer <dwight.schauer@teegra.net>
2019-11-28 22:27:01 -0600

    Now deriving v path from vexe

--- vlib/builtin/bare/syscallwrapper_test.v
+++ vlib/builtin/bare/syscallwrapper_test.v
@@ -1,14 +1,19 @@
 import os
 
 fn test_syscallwrappers() {
-	if true { return }
-	dot_checks := "vlib/builtin/bare/.checks"
-	assert os.dir_exists(dot_checks)
-	os.chdir(dot_checks)
-	checks_v := "checks.v"
-	assert os.file_exists(checks_v)
-	rc := os.exec("v run $checks_v") or { panic(err) }
-	assert rc.exit_code == 0
-	assert !rc.output.contains("V panic: An assertion failed.")
-	assert !rc.output.contains("failed")
+	vexe := os.getenv("VEXE")
+	vn := vexe.len - 1
+	if vn > 1 {
+		dot_checks := vexe.substr(0,vn) + "vlib/builtin/bare/.checks"
+		assert os.dir_exists(dot_checks)
+		os.chdir(dot_checks)
+		checks_v := "checks.v"
+		assert os.file_exists(checks_v)
+		rc := os.exec("v run $checks_v") or { panic(err) }
+		assert rc.exit_code == 0
+		assert !rc.output.contains("V panic: An assertion failed.")
+		assert !rc.output.contains("failed")
+	} else {
+		panic("Can't find v")
+	}
 }
