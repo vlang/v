69b82909afa24f8b7dacbff366f126fa8cf76af7
RONTheCookie <ronthecookie0101@gmail.com>
2019-11-29 09:07:06 +0200

    Now running with some refactoring and request parsing

--- vlib/http/http_server.v
+++ vlib/http/http_server.v
@@ -10,8 +10,19 @@ pub struct HttpServer {
 }
 
 pub struct ServerRequest {
-	dummy string
+	path string
+	method string
+	headers map[string]string
+	text ?string // Only on POST
 }
+
+const (
+	allowed_methods = ['GET', 'POST']
+	default_headers = {
+		'User-Agent': 'vlang',
+		'Content-Type': 'text/plain'
+	}
+)
 
 
 // create an http server, beware: the handler callback runs on a different system thread.
@@ -50,27 +61,72 @@ pub fn (s HttpServer) free() ?bool {
 	return true
 }
 
-// handle connections, this runs in a different system thread.
-fn (s HttpServer) handle_conn(conn net.Socket) ?bool {
-	mut headers := map[string]string
-	headers['User-Agent'] = 'vlang'
-	headers['Content-Type'] = 'text/plain'
-	res := Response {
-		text: 'Not Found',
-		status_code: 404,
-		headers: headers
+fn get_response_status_message(code int) string {
+	status := match code {
+		100 { 'Continue' }
+		101 { 'Switching Protocols' }
+		200 { 'OK' }
+		201 { 'Created' }
+		202 { 'Accepted' }
+		203 { 'Non-Authoritive Information' }
+		204 { 'No Content' }
+		205 { 'Reset Content' }
+		206 { 'Partial Content' }
+		300 { 'Multiple Choices' }
+		301 { 'Moved Permanently' }
+		400 { 'Bad Request' }
+		401 { 'Unauthorized' }
+		403 { 'Forbidden' }
+		404 { 'Not Found' }
+		405 { 'Method Not Allowed' }
+		408 { 'Request Timeout' }
+		500 { 'Internal Server Error' }
+		501 { 'Not Implemented' }
+		502 { 'Bad Gateway' }
+		else { 'Internal Server Error' }
 	}
-	req := ServerRequest {
-		dummy: 'test'
+
+	return status
+}
+fn get_status_res(code int) Response {
+	return Response {
+		text: get_response_status_message(code),
+		status_code: code,
+		headers: default_headers
 	}
-	handler := s.handler
-	new_res := handler(req, res)
-	serialized_headers := serialize_headers(res.headers)
-	conn.write('HTTP/1.1 $new_res.status_code\r\n$serialized_headers\r\n$new_res.text') or {
+}
+
+fn (r Response) send(conn net.Socket) ?bool {
+	serialized_headers := serialize_headers(r.headers)
+	conn.write('HTTP/1.1 $r.status_code\r\n$serialized_headers\r\n$r.text') or {
 		return error(err)
 	}
 	conn.close() or {
 		return error(err)
+	}
+	return true
+}
+// handle connections, this runs in a different system thread.
+fn (s HttpServer) handle_conn(conn net.Socket) ?bool {
+	res := get_status_res(404)
+
+	meta_line := conn.read_line().split(' ')
+	if (meta_line.len != 3) {
+		get_status_res(400).send(conn) or {
+			return error('couldnt send response')
+		}
+		return error('invalid response')
+	}
+	req := ServerRequest {
+		method: meta_line[0],
+		path: meta_line[1],
+		headers: default_headers
+	}
+	// TODO: impl allowed_methods
+	handler := s.handler
+	new_res := handler(req, res)
+	new_res.send(conn) or {
+		return error('couldnt send response')
 	}
 	return true
 }
