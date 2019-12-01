854309a7d8b1a192499b0e312c0d2950d4bef431
bogen85 <52499484+bogen85@users.noreply.github.com>
2019-12-01 02:27:36 -0600

    freestanding: add array support (stack only for now)

--- vlib/builtin/bare/mm_bare.v
+++ vlib/builtin/bare/mm_bare.v
@@ -1,7 +1,5 @@
 module builtin
 
-//__global buffer [128]byte
-
 pub fn mm_pages(size u64) u32 {
 	pages := (u64(size+u64(4))+u64(linux_mem.page_size))/u64(linux_mem.page_size)
 	return u32(pages)
@@ -13,16 +11,6 @@ pub fn mm_alloc(size u64) (byteptr, errno) {
 
 	mem_prot := mm_prot(int(mm_prot.prot_read) | int(mm_prot.prot_write))
 	mem_flags := map_flags(int(map_flags.map_private) | int(map_flags.map_anonymous))
-
-	//mps := i64_tos(buffer,80,i64(mem_prot),16)
-	//println(mps)
-	//mfs := i64_tos(buffer,80,i64(mem_flags),16)
-	//println(mfs)
-
-	//print("pages in: ")
-	//ps := i64_tos(buffer,80,i64(pages),16)
-	//println(ps)
-
 
 	a, e := sys_mmap(0, n_bytes, mem_prot, mem_flags, -1, 0)
 	if e == .enoerror {
@@ -37,20 +25,14 @@ pub fn mm_free(addr byteptr) errno {
 	ap := intptr(addr-4)
 	size := u64(*ap) * u64(linux_mem.page_size)
 
-	//aps := i64_tos(buffer,80,i64(addr),16)
-	//println(aps)
+	return sys_munmap(ap, size)
+}
 
-	//ss := i64_tos(buffer,80,i64(size),16)
-	//print("size out: ")
-	//println(ss)
-
-	//pas := i64_tos(buffer,80,i64(ap),16)
-	//println(pas)
-
-	e := sys_munmap(ap, size)
-
-	//es := i64_tos(buffer,80,i64(e),16)
-	//println(es)
-
-	return e
+pub fn mem_copy(dest0 voidptr, src0 voidptr, n int) voidptr {
+	mut dest := byteptr(dest0)
+	src := byteptr(src0)
+	for i in 0..n {
+		dest[i] = src[i]
+	}
+	return dest0
 }
--- vlib/builtin/bare/syscallwrapper_test.v
+++ vlib/builtin/bare/syscallwrapper_test.v
@@ -1,20 +1,25 @@
 import os
 
 fn test_syscallwrappers() {
-	if true {return}
-	vexe := os.getenv("VEXE")
-	vn := vexe.len - 1
-	if vn > 1 {
-		dot_checks := vexe.substr(0,vn) + "vlib/builtin/bare/.checks"
-		assert os.dir_exists(dot_checks)
-		os.chdir(dot_checks)
-		checks_v := "checks.v"
-		assert os.file_exists(checks_v)
-		rc := os.exec("v run $checks_v") or { panic(err) }
-		assert !rc.output.contains("V panic: An assertion failed.")
-		assert !rc.output.contains("failed")
-		assert rc.exit_code == 0
-	} else {
-		panic("Can't find v")
+	if true { return }
+	$if linux {
+		$if x64 {
+			exe := os.executable()
+			vdir := os.dir(exe)
+			if vdir.len > 1 {
+				dot_checks := vdir + "/.checks"
+				assert os.dir_exists(dot_checks)
+
+				os.chdir(dot_checks)
+				checks_v := "checks.v"
+				assert os.file_exists(checks_v)
+				rc := os.exec("v run $checks_v") or { panic(err) }
+				assert !rc.output.contains("V panic: An assertion failed.")
+				assert !rc.output.contains("failed")
+				assert rc.exit_code == 0
+			} else {
+				panic("Can't find test directory")
+			}
+		}
 	}
 }
