e0b8918c79c44ed35bb517d9565d91d1a565e225
Delyan Angelov <delian66@gmail.com>
2019-12-24 16:46:31 +0200

    Run 'v fmt' over tools/vtest-fmt.v and tools/vtest-compiler.v .

--- tools/vtest-fmt.v
+++ tools/vtest-fmt.v
@@ -18,44 +18,43 @@ fn v_test_formatting(vargs string) {
 	// Removing the binaries below is needed, since the building tools step
 	// rebuilds all the tools without the special option needed by vfmt
 	// by simply compiling each of them with `v tools/{toolname}.v`
-	//	os.rm('tools/vfmt')
-	//	os.rm('tools/vfmt.exe')	
-
+	// os.rm('tools/vfmt')
+	// os.rm('tools/vfmt.exe')
 	mut files_able_to_be_formatted := []string
 	all_test_files := os.walk_ext('.', '.v')
-	known_failing_exceptions := [
-    './examples/vweb/vweb_example.v',
-    './tools/gen_vc.v',
-    './tutorials/code/blog/article.v',
-    './tutorials/code/blog/blog.v',
-    './vlib/arrays/arrays.v',
-		'./vlib/arrays/arrays_test.v',
-    './vlib/builtin/js/hashmap.v',
-		'./vlib/compiler/tests/fn_variadic_test.v',
-		'./vlib/compiler/tests/generic_test.v',
-    './vlib/compiler/preludes/live_main.v',
-    './vlib/compiler/preludes/live_shared.v',
-    './vlib/compiler/preludes/tests_assertions.v',
-    './vlib/compiler/preludes/tests_with_stats.v',
-    './vlib/crypto/aes/aes.v',
-    './vlib/crypto/aes/aes_cbc.v',
-    './vlib/crypto/aes/block_generic.v',
-    './vlib/crypto/aes/const.v',
-    './vlib/crypto/aes/cypher_generic.v',
-    './vlib/crypto/rc4/rc4.v',
-		'./vlib/eventbus/eventbus_test.v',
-    './vlib/flag/flag.v',
-    './vlib/os/bare/bare_example_linux.v',
-    './vlib/szip/szip.v',
-    './vlib/ui/examples/users_gui/users.v',
-    './vlib/vweb/assets/assets.v',
-    './vlib/vweb/vweb.v',
+	known_failing_exceptions := ['./examples/vweb/vweb_example.v',
+	'./tools/gen_vc.v',
+	'./tutorials/code/blog/article.v',
+	'./tutorials/code/blog/blog.v',
+	'./vlib/arrays/arrays.v',
+	'./vlib/arrays/arrays_test.v',
+	'./vlib/builtin/js/hashmap.v',
+	'./vlib/compiler/tests/fn_variadic_test.v',
+	'./vlib/compiler/tests/generic_test.v',
+	'./vlib/compiler/preludes/live_main.v',
+	'./vlib/compiler/preludes/live_shared.v',
+	'./vlib/compiler/preludes/tests_assertions.v',
+	'./vlib/compiler/preludes/tests_with_stats.v',
+	'./vlib/crypto/aes/aes.v',
+	'./vlib/crypto/aes/aes_cbc.v',
+	'./vlib/crypto/aes/block_generic.v',
+	'./vlib/crypto/aes/const.v',
+	'./vlib/crypto/aes/cypher_generic.v',
+	'./vlib/crypto/rc4/rc4.v',
+	'./vlib/eventbus/eventbus_test.v',
+	'./vlib/flag/flag.v',
+	'./vlib/os/bare/bare_example_linux.v',
+	'./vlib/szip/szip.v',
+	'./vlib/ui/examples/users_gui/users.v',
+	'./vlib/vweb/assets/assets.v',
+	'./vlib/vweb/vweb.v',
 	]
 	for tfile in all_test_files {
-		if tfile in known_failing_exceptions { continue }
+		if tfile in known_failing_exceptions {
+			continue
+		}
 		files_able_to_be_formatted << tfile
 	}
-	
 	eprintln('Run "v fmt" over all .v files')
 	mut vfmt_test_session := testing.new_test_session('$vargs fmt')
 	vfmt_test_session.files << files_able_to_be_formatted
