f04c487cb5f7dd868d20f563fc7225449a3c52ec
Delyan Angelov <delian66@gmail.com>
2019-12-27 16:37:16 +0200

    Reformatted vlib/os2/os2_darwin.v .

--- vlib/os2/os2_darwin.v
+++ vlib/os2/os2_darwin.v
@@ -1,24 +1,31 @@
 module os2
 
 #include <fcntl.h>
-
 struct File {
 	fd int
-}	
+}
 
 fn C.perror(charptr)
+
+
 fn C.open(byteptr, int, int) int
+
+
 fn C.write(voidptr, byteptr, int) int
+
+
 fn C.close(int) int
 
+
 pub fn create(path string) ?File {
-  fd := C.open(path.str, C.O_CREAT | C.O_TRUNC | C.O_WRONLY, 0644)//511
+	fd := C.open(path.str, C.O_CREAT | C.O_TRUNC | C.O_WRONLY, 0644) // 511
 	if fd == -1 {
 		return error('failed to create "$path":')
-		//os.print_c_errno()
+		// os.print_c_errno()
 	}
-	return File{fd}
-}	
+	return File{
+		fd}
+}
 
 pub fn (f File) writeln(s string) {
 	ss := s + '\n'
@@ -31,4 +38,3 @@ pub fn (f File) writeln(s string) {
 pub fn (f File) close() {
 	C.close(f.fd)
 }
-
