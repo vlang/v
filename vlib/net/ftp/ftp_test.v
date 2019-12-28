3d99392107b2f07744858d32f489aa22c0f75215
Delyan Angelov <delian66@gmail.com>
2019-12-28 13:14:14 +0200

    Reformat vlib/net/ftp/ftp_test.v with vfmt.

--- vlib/net/ftp/ftp_test.v
+++ vlib/net/ftp/ftp_test.v
@@ -2,43 +2,37 @@ module main
 
 import net.ftp
 
-fn test_client(){
+fn test_client() {
 	make_client_connection()
 	assert true
 }
 
-
 // NB: this function makes network calls to external servers,
 // that is why it is not a very good idea to run it in CI.
 // If you want to run it manually, use `v -d network vlib/net/ftp/ftp_test.v`
-[if network]
+[ifnetwork]
 fn make_client_connection() {
 	mut ftp := ftp.new()
 	defer {
 		ftp.close()
 	}
-
 	assert ftp.connect('ftp.redhat.com')
-	assert ftp.login('ftp','ftp')
-
+	assert ftp.login('ftp', 'ftp')
 	pwd := ftp.pwd()
-  assert pwd.len > 0
-
+	assert pwd.len > 0
 	ftp.cd('/')
-	dir_list1 := ftp.dir() or {
+	dir_list1 := ftp.dir()  or {
 		assert false
 		return
 	}
 	assert dir_list1.len > 0
-
 	ftp.cd('/suse/linux/enterprise/11Server/en/SAT-TOOLS/SRPMS/')
-	dir_list2 := ftp.dir() or {
+	dir_list2 := ftp.dir()  or {
 		assert false
 		return
 	}
 	assert dir_list2.len > 0
-
-	blob := ftp.get('katello-host-tools-3.3.5-8.sles11_4sat.src.rpm') or {
+	blob := ftp.get('katello-host-tools-3.3.5-8.sles11_4sat.src.rpm')  or {
 		assert false
 		return
 	}
