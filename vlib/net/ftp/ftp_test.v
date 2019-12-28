44a9949dcb54ee6c524f957f04fc62d4d3093558
Delyan Angelov <delian66@gmail.com>
2019-12-28 13:17:19 +0200

    Fix ')  or {' formatting manually too.

--- vlib/net/ftp/ftp_test.v
+++ vlib/net/ftp/ftp_test.v
@@ -21,18 +21,18 @@ fn make_client_connection() {
 	pwd := ftp.pwd()
 	assert pwd.len > 0
 	ftp.cd('/')
-	dir_list1 := ftp.dir()  or {
+	dir_list1 := ftp.dir() or {
 		assert false
 		return
 	}
 	assert dir_list1.len > 0
 	ftp.cd('/suse/linux/enterprise/11Server/en/SAT-TOOLS/SRPMS/')
-	dir_list2 := ftp.dir()  or {
+	dir_list2 := ftp.dir() or {
 		assert false
 		return
 	}
 	assert dir_list2.len > 0
-	blob := ftp.get('katello-host-tools-3.3.5-8.sles11_4sat.src.rpm')  or {
+	blob := ftp.get('katello-host-tools-3.3.5-8.sles11_4sat.src.rpm') or {
 		assert false
 		return
 	}
