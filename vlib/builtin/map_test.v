f7a7c8397217eaf9a574c2effc79351d0c7fec4e
ka-weihe <k@weihe.dk>
2019-12-29 02:21:46 +0100

    Remove faulty test

--- vlib/builtin/map_test.v
+++ vlib/builtin/map_test.v
@@ -23,14 +23,11 @@ fn test_map() {
 	assert m.size == 2
 	assert 'hi' in m
 	mut sum := 0
-	mut key_sum := ''
 	// Test `for in`
 	for key, val in m {
 		sum += val
-		key_sum += key
 	}
 	assert sum == 80 + 101
-	assert key_sum == 'hihello'
 	// Test `.keys()`
 	keys := m.keys()
 	assert keys.len == 2
