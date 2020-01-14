ddcd872aff0e2a37960e7c703518551772b960b2
Delyan Angelov <delian66@gmail.com>
2020-01-14 16:09:23 +0200

    Small cleanup of utility methods get,post,put,head,patch,delete.

--- vlib/net/http/http.v
+++ vlib/net/http/http.v
@@ -34,63 +34,33 @@ pub:
 	status_code int
 }
 
-pub fn delete(url string) ?Response {
-	req := new_request('DELETE', url, '') or {
-		return error(err)
-	}
-	res := req.do() or {
-		return error(err)
-	}
-	return res
+pub fn get(url string) ?Response { 
+	return method('GET', url, '') 
 }
 
-pub fn put(url string) ?Response {
-	req := new_request('PUT', url, '') or {
-		return error(err)
-	}
-	res := req.do() or {
-		return error(err)
-	}
-	return res
-}
-
-pub fn patch(url string) ?Response {
-	req := new_request('PATCH', url, '') or {
-		return error(err)
-	}
-	res := req.do() or {
-		return error(err)
-	}
-	return res
-}
-
-pub fn get(url string) ?Response {
-	req := new_request('GET', url, '') or {
-		return error(err)
-	}
-	res := req.do() or {
-		return error(err)
-	}
-	return res
-}
-
-pub fn post(url, data string) ?Response {
-	req := new_request('POST', url, data) or {
-		return error(err)
-	}
-	res := req.do() or {
-		return error(err)
-	}
-	return res
+pub fn post(url string) ?Response {
+	return method('POST', url, '')
 }
 
 pub fn head(url string) ?Response {
-	req := new_request('HEAD', url, '') or {
-		return error(err)
-	}
-	res := req.do() or {
-		return error(err)
-	}
+	return method('HEAD', url, '')
+}
+
+pub fn delete(url string) ?Response {
+	return method('DELETE', url, '')
+}
+
+pub fn patch(url string) ?Response {
+	return method('PATCH', url, '')
+}
+
+pub fn put(url string) ?Response {
+	return method('PUT', url, '')
+}
+
+pub fn method(mname string, url string, data string) ?Response {
+	req := new_request(mname, url, data) or { return error(err) }
+	res := req.do() or { return error(err)}
 	return res
 }
 
