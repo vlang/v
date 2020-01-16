f85c1cfae8ab2f297ccca4af7b21ef3f2810bb65
Delyan Angelov <delian66@gmail.com>
2020-01-16 13:34:36 +0200

    Make network tests run only when the network is present (i.e. when compiled with -d network).

--- vlib/net/http/http_test.v
+++ vlib/net/http/http_test.v
@@ -1,95 +1,38 @@
 import net.http
-import json
 
-struct HttpbinResponseBody {
-	args    map[string]string
-	data    string
-	files   map[string]string
-	form    map[string]string
-	headers map[string]string
-	json    ?map[string]string
-	origin  string
-	url     string
+fn test_http_get() {
+	$if !network ? { return }
+	assert http.get_text('https://vlang.io/version') == '0.1.5'
+	println('http ok')
 }
 
-fn http_fetch_mock(_methods []string, _config http.FetchConfig) ?[]http.Response {
-	url := 'https://httpbin.org/'
-	methods := if _methods.len == 0 { ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'] } else { _methods }
-	mut config := _config
-	mut result := []http.Response
-	// Note: httpbin doesn't support head
-	for method in methods {
-		lmethod := method.to_lower()
-		config.method = method
-		res := http.fetch(url + lmethod, config) or {
-			return error(err)
-		}
-		// TODO
-		// body := json.decode(HttpbinResponseBody,res.text) or {
-		// return error(err)
-		// }
-		result << res
-	}
-	return result
-}
-
-fn test_http_fetch_bare() {
-	responses := http_fetch_mock([], http.FetchConfig{}) or {
-		panic(err)
-	}
-	for response in responses {
-		assert response.status_code == 200
+fn test_http_get_from_vlang_utc_now() {
+	$if !network ? { return }
+	urls := ['http://vlang.io/utc_now', 'https://vlang.io/utc_now']
+	for url in urls {
+		println('Test getting current time from $url by http.get')
+		res := http.get(url) or { panic(err) }
+		assert 200 == res.status_code
+		assert res.text.len > 0
+		assert res.text.int() > 1566403696
+		println('Current time is: ${res.text.int()}')
 	}
 }
 
-fn test_http_fetch_with_data() {
-	responses := http_fetch_mock(['POST', 'PUT', 'PATCH', 'DELETE'], {
-		data: 'hello world'
-	}) or {
-		panic(err)
-	}
-	for response in responses {
-		payload := json.decode(HttpbinResponseBody,response.text) or {
-			panic(err)
-		}
-		assert payload.data == 'hello world'
-	}
-}
-
-fn test_http_fetch_with_params() {
-	responses := http_fetch_mock([], {
-		params: {
-			'a': 'b',
-			'c': 'd'
-		}
-	}) or {
-		panic(err)
-	}
-	for response in responses {
-		// payload := json.decode(HttpbinResponseBody,response.text) or {
-		// panic(err)
-		// }
-		assert response.status_code == 200
-		// TODO
-		// assert payload.args['a'] == 'b'
-		// assert payload.args['c'] == 'd'
-	}
-}
-
-fn test_http_fetch_with_headers() {
-	responses := http_fetch_mock([], {
-		headers: {
-			'Test-Header': 'hello world'
-		}
-	}) or {
-		panic(err)
-	}
-	for response in responses {
-		// payload := json.decode(HttpbinResponseBody,response.text) or {
-		// panic(err)
-		// }
-		assert response.status_code == 200
-		// TODO
-		// assert payload.headers['Test-Header'] == 'hello world'
+fn test_public_servers() {
+	$if !network ? { return }
+	urls := [
+		'http://github.com/robots.txt',
+		'http://google.com/robots.txt',
+		'http://yahoo.com/robots.txt',
+		'https://github.com/robots.txt',
+		'https://google.com/robots.txt',
+		'https://yahoo.com/robots.txt',
+	]
+	for url in urls {
+		println('Testing http.get on public url: $url ')
+		res :=  http.get( url ) or { panic(err) }
+		assert 200 == res.status_code
+		assert res.text.len > 0
 	}
 }
