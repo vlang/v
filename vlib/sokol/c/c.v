vlang/v

commit 0502a54a360fc888c116f83cb3d756a201be88d9

new gg/freetype modules with sokol/fontstash backends

vlib/sokol/c/c.v
@@ -13,12 +13,12 @@ pub const (
 #flag windows -lgdi32
 
 // METAL
-// #flag -DSOKOL_METAL
-// #flag darwin -framework Metal -framework Cocoa -framework MetalKit -framework QuartzCore
+#flag darwin -DSOKOL_METAL
+#flag darwin -framework Metal -framework Cocoa -framework MetalKit -framework QuartzCore
 
 // OPENGL
-#flag -DSOKOL_GLCORE33
-#flag darwin -framework OpenGL -framework Cocoa -framework QuartzCore
+#flag linux -DSOKOL_GLCORE33
+//#flag darwin -framework OpenGL -framework Cocoa -framework QuartzCore
 
 
 // for simplicity, all header includes are here because import order matters and we dont have any way

examples/gg2.v
@@ -1,0 +1,50 @@
+module main
+
+import gg2 // as gg
+import gx
+
+const (
+	win_width = 600
+	win_height = 300
+)
+
+struct App {
+mut:
+	gg &gg2.GG
+}
+
+fn main() {
+	mut app := &App{}
+	app.gg = gg2.new_context({
+		bg_color: gx.white
+		width: win_width
+		height: win_height
+		use_ortho: true // This is needed for 2D drawing
+		create_window: true
+		window_title: 'Empty window'
+		frame_fn: frame
+		user_data: app
+		font_path: 'examples/tetris/RobotoMono-Regular.ttf'
+	})
+	app.gg.run()
+}
+
+fn frame(user_data voidptr) {
+	mut app := &App(user_data)
+	mut gg := app.gg
+	gg.begin()
+	if gg.fons == 0 {
+		gg.init_font()
+	}
+	app.draw()
+	C.sfons_flush(gg.fons)
+	gg.end()
+}
+
+fn (app &App) draw() {
+	app.gg.draw_text_def(200,20, 'hello world!')
+	app.gg.draw_text_def(300,300, 'привет')
+	app.gg.draw_rect(10, 10, 100, 30, gx.blue)
+	app.gg.draw_empty_rect(10, 150, 80, 40, gx.green)
+}
+

examples/sokol_examples/fonts.v
@@ -52,7 +52,7 @@ fn init(user_data voidptr) {
 	})
 	s := &C.sgl_desc_t{}
 	C.sgl_setup(s)
-	state.fons = C.sfons_create(512, 512, 1)
+	state.fons = sfons.create(512, 512, 1)
 	// or use DroidSerif-Regular.ttf
 	if bytes := os.read_bytes(os.resource_abs_path('assets/ProggyTiny.ttf')) {
 		println('loaded font: $bytes.len')

vlib/builtin/array_test.v
@@ -561,3 +561,12 @@ fn test_push_many_self() {
         assert actual_arr[i] == expected_arr[i]
     }
 }
+
+fn test_for() {
+	nums := [1,2,3]
+	mut sum := 0
+	for num <- nums {
+		sum += num
+	}
+	assert sum == 6
+}

vlib/compiler/aparser.v
@@ -446,7 +446,7 @@ fn (p mut Parser) parse(pass Pass) {
 	}
 	p.fgen_nl()
 	p.builtin_mod = p.mod == 'builtin'
-	p.can_chash = p.mod in ['ui', 'uiold', 'darwin', 'clipboard', 'webview'] // TODO tmp remove
+	p.can_chash = p.mod in ['gg2', 'ui', 'uiold', 'darwin', 'clipboard', 'webview'] // TODO tmp remove
 	// Import pass - the first and the smallest pass that only analyzes imports
 	// if we are a building module get the full module name from v.mod
 	fq_mod := if p.pref.build_mode == .build_module && p.v.mod.ends_with(p.mod) { p.v.mod }
@@ -544,10 +544,9 @@ fn (p mut Parser) parse(pass Pass) {
 			}
 			.key_global {
 				if !p.pref.translated && !p.pref.is_live && !p.builtin_mod && !p.pref.building_v &&
-					p.mod != 'ui' && p.mod != 'uiold' && !os.getwd().contains('/volt') &&
+					p.mod != 'ui' && p.mod != 'gg2' && p.mod != 'uiold' && !os.getwd().contains('/volt') &&
 					!p.pref.enable_globals {
 					p.error('use `v --enable-globals ...` to enable globals')
-					// p.error('__global is only allowed in translated code')
 				}
 				p.next()
 				p.fspace()

vlib/compiler/for.v
@@ -128,10 +128,11 @@ fn (p mut Parser) for_st() {
 		}
 	}
 	// `for val in vals`
-	else if p.peek() == .key_in {
+	else if p.peek() == .key_in || p.peek() == .left_arrow {
 		val := p.check_name()
 		p.fspace()
-		p.check(.key_in)
+		//p.check(.key_in)
+		p.next()
 		p.fspace()
 		tmp := p.get_tmp()
 		mut typ,expr := p.tmp_expr()

vlib/compiler/scanner.v
@@ -549,6 +549,11 @@ fn (s mut Scanner) scan() ScanRes {
 				s.pos++
 				return scan_res(.left_shift, '')
 			}
+			else if nextc == `-` {
+				s.pos++
+				println("GOT ARR")
+				return scan_res(.left_arrow, '')
+			}
 			else {
 				return scan_res(.lt, '')
 			}

vlib/compiler/table.v
@@ -705,6 +705,9 @@ fn (p mut Parser) check_types2(got_, expected_ string, throw bool) bool {
 	if got.starts_with('fn ') && (expected.ends_with('fn') || expected.ends_with('Fn')) {
 		return true
 	}
+	if got.starts_with('fn ') && expected.starts_with('fn ') {
+		return true
+	}
 	// Allow pointer arithmetic
 	if expected == 'void*' && got == 'int' {
 		return true

commit 82da001fafdfc3814e207dd897919ce4d90927a5

sokol: fix issue causing win test to fail introduced in 0502a54

vlib/sokol/c/c.v
@@ -18,6 +18,7 @@ pub const (
 
 // OPENGL
 #flag linux -DSOKOL_GLCORE33
+#flag windows -DSOKOL_GLCORE33
 //#flag darwin -framework OpenGL -framework Cocoa -framework QuartzCore
 
 