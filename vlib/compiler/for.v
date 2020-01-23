f3f50349b27766c8dcaa8c4e645cc6648ee28b6f
Henrixounez <30901439+Henrixounez@users.noreply.github.com>
2020-01-23 21:06:57 +0100

    compiler: fix check restricted names in for..in statement

--- vlib/compiler/for.v
+++ vlib/compiler/for.v
@@ -129,6 +129,7 @@ fn (p mut Parser) for_st() {
 	}
 	// `for val in vals`
 	else if p.peek() == .key_in || p.peek() == .left_arrow {
+		p.check_not_reserved()
 		val := p.check_name()
 		p.fspace()
 		//p.check(.key_in)
