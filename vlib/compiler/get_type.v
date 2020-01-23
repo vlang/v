cd04d301fa5d3d91f719becbe573eac17fde0424
joe-conigliaro <joe.conigliaro@gmail.com>
2020-01-23 14:23:05 +1100

    fix module alias resolution

--- vlib/compiler/get_type.v
+++ vlib/compiler/get_type.v
@@ -147,8 +147,10 @@ fn (p mut Parser) get_type2() Type {
 			// try resolve full submodule
 			if !p.builtin_mod && p.import_table.known_alias(typ) {
 				mod := p.import_table.resolve_alias(typ)
-				if mod.contains('.') {
-					typ = mod_gen_name(mod)
+				typ = if mod.contains('.') {
+					mod_gen_name(mod)
+				} else {
+					mod
 				}
 			}
 			p.next()
