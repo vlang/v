953eb04de91dfad0536b1dfa976325f1706e6c68
joe-conigliaro <joe.conigliaro@gmail.com>
2020-01-24 22:11:42 +1100

    add mul to bits

--- vlib/math/bits/bits.v
+++ vlib/math/bits/bits.v
@@ -294,3 +294,24 @@ pub fn len64(x u64) int {
 	return n + int(len8_tab[y])
 }
 
+// Mul64 returns the 128-bit product of x and y: (hi, lo) = x * y
+// with the product bits' upper half returned in hi and the lower
+// half returned in lo.
+//
+// This function's execution time does not depend on the inputs.
+[inline]
+pub fn mul64(x, y u64) (u64, u64) {
+	mask32 := u32(1<<32) - 1
+	x0 := x & mask32
+	x1 := x >> 32
+	y0 := y & mask32
+	y1 := y >> 32
+	w0 := x0 * y0
+	t := x1*y0 + (w0>>32)
+	mut w1 := t & mask32
+	w2 := t >> 32
+	w1 += x0 * y1
+	hi := x1*y1 + w2 + (w1>>32)
+	lo := x * y
+	return hi,lo
+}
