7abc4fc48d2ea08fad0cec82e3cb4d66bc66cd53
joe-conigliaro <joe.conigliaro@gmail.com>
2020-01-24 22:31:12 +1100

    implement wyhash v4

---
+++ vlib/hash/wyhash/wyhash.v
@@ -0,0 +1,87 @@
+// Copyright (c) 2019 Alexander Medvednikov. All rights reserved.
+// Use of this source code is governed by an MIT license
+// that can be found in the LICENSE file.
+//
+// This is an implementation of wyhash v4
+// from https://github.com/wangyi-fudan/wyhash
+module wyhash
+
+import (
+	encoding.binary
+	math.bits
+)
+
+const (
+	wyp0 = 0xa0761d6478bd642f
+	wyp1 = 0xe7037ed1a0b428db
+	wyp2 = 0x8ebc6af09c88c6e3
+	wyp3 = 0x589965cc75374cc3
+	wyp4 = 0x1d8e4e27c47d124f
+)
+
+pub fn sum64(seed u64, key []byte) u64 {
+	return wyhash64(key, u64(key.len), seed)
+}
+
+fn wyhash64(key []byte, len, seed_ u64) u64 {
+	if len == 0 {
+		return 0
+	}
+	mut p := key
+	mut seed := seed_
+	mut i := len & 63
+	if i < 4 {
+		seed = wymum(wyr3(p, i) ^ seed ^ wyp0, seed ^ wyp1)
+	}
+	else if i <= 8 {
+		seed = wymum(wyr4(p) ^ seed ^ wyp0, wyr4(p[i - 4..]) ^ seed ^ wyp1)
+	}
+	else if i <= 16 {
+		seed = wymum(wyr8(p) ^ seed ^ wyp0, wyr8(p[i - 8..]) ^ seed ^ wyp1)
+	}
+	else if i <= 24 {
+		seed = wymum(wyr8(p) ^ seed ^ wyp0, wyr8(p[8..]) ^ seed ^ wyp1) ^ wymum(wyr8(p[i - 8..]) ^ seed ^ wyp2, seed ^ wyp3)
+	}
+	else if i <= 32 {
+		seed = wymum(wyr8(p) ^ seed ^ wyp0, wyr8(p[8..]) ^ seed ^ wyp1) ^ wymum(wyr8(p[16..]) ^ seed ^ wyp2, wyr8(p[i - 8..]) ^ seed ^ wyp3)
+	}
+	else {
+		seed = wymum(wyr8(p) ^ seed ^ wyp0, wyr8(p[8..]) ^ seed ^ wyp1) ^ wymum(wyr8(p[16..]) ^ seed ^ wyp2, wyr8(p[24..]) ^ seed ^ wyp3) ^ wymum(wyr8(p[i - 32..]) ^ seed ^ wyp1, wyr8(p[i - 24..]) ^ seed ^ wyp2) ^ wymum(wyr8(p[i - 16..]) ^ seed ^ wyp3, wyr8(p[i - 8..]) ^ seed ^ wyp0)
+	}
+	if i == len {
+		return wymum(seed, len ^ wyp4)
+	}
+	mut see1 := seed
+	mut see2 := seed
+	mut see3 := seed
+	p = p[i..]
+	for i = len - i; i >= 64; i -= 64 {
+		seed = wymum(wyr8(p) ^ seed ^ wyp0, wyr8(p[8..]) ^ seed ^ wyp1)
+		see1 = wymum(wyr8(p[16..]) ^ see1 ^ wyp2, wyr8(p[24..]) ^ see1 ^ wyp3)
+		see2 = wymum(wyr8(p[32..]) ^ see2 ^ wyp1, wyr8(p[40..]) ^ see2 ^ wyp2)
+		see3 = wymum(wyr8(p[48..]) ^ see3 ^ wyp3, wyr8(p[56..]) ^ see3 ^ wyp0)
+		p = p[64..]
+	}
+	return wymum(seed ^ see1 ^ see2, see3 ^ len ^ wyp4)
+}
+
+[inline]
+fn wymum(a, b u64) u64 {
+	hi,lo := bits.mul64(a, b)
+	return hi ^ lo
+}
+
+[inline]
+fn wyr3(p []byte, k u64) u64 {
+	return (u64(p[0])<<16) | (u64(p[k>>1])<<8) | u64(p[k - 1])
+}
+
+[inline]
+fn wyr4(p []byte) u64 {
+	return u64(binary.little_endian_u32(p))
+}
+
+[inline]
+fn wyr8(p []byte) u64 {
+	return binary.little_endian_u64(p)
+}
--- vlib/math/bits/bits.v
+++ vlib/math/bits/bits.v
@@ -300,18 +300,18 @@ pub fn len64(x u64) int {
 //
 // This function's execution time does not depend on the inputs.
 [inline]
-pub fn mul64(x, y u64) (u64, u64) {
+pub fn mul64(x, y u64) (u64,u64) {
 	mask32 := u32(1<<32) - 1
 	x0 := x & mask32
-	x1 := x >> 32
+	x1 := x>>32
 	y0 := y & mask32
-	y1 := y >> 32
+	y1 := y>>32
 	w0 := x0 * y0
-	t := x1*y0 + (w0>>32)
+	t := x1 * y0 + (w0>>32)
 	mut w1 := t & mask32
-	w2 := t >> 32
+	w2 := t>>32
 	w1 += x0 * y1
-	hi := x1*y1 + w2 + (w1>>32)
+	hi := x1 * y1 + w2 + (w1>>32)
 	lo := x * y
 	return hi,lo
 }
