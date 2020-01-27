ec977d2e48612ad04bc721ad733dc5694c505581
Leah Lundqvist <hampus0505@gmail.com>
2020-01-27 20:31:27 +0100

    Argument now takes angles instead of rads

--- vlib/gg/gg.v
+++ vlib/gg/gg.v
@@ -421,16 +421,18 @@ pub fn (ctx &GG) draw_arc(x, y, r, start_angle, end_angle f32, segments int, col
 	C.glDeleteBuffers(1, &ctx.vbo)
 	ctx.shader.use()
 	ctx.shader.set_color('color', color)
-	angle_increment := end_angle / segments
+	start_rads := start_angle * 0.0174533
+	end_rads := end_angle * 0.0174533
+	rad_increment := end_rads / segments
 	mut vertices := []f32
 	mut i := 0
 	for i < segments {
-		theta := f32(i) * angle_increment - start_angle
+		theta := f32(i) * rad_increment - start_rads 
 		vertices << [x + f32(math.cos(theta)) * r, y + f32(math.sin(theta)) * r]
 		i++
 	}
 	// Add the last vertex at the final arc angle.
-	vertices << [x + f32(math.cos(end_angle)) * r, y + f32(math.sin(end_angle)) * r]
+	vertices << [x + f32(math.cos(end_rads)) * r, y + f32(math.sin(end_rads)) * r]
 	gl.bind_vao(ctx.vao)
 	gl.set_vbo(ctx.vbo, vertices, C.GL_STATIC_DRAW)
 	gl.vertex_attrib_pointer(0, 2, C.GL_FLOAT, false, 2, 0)
