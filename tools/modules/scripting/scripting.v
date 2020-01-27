vlang/v

commit 760448afa4ec5beb6ed4cc551402904b3099b1e6

Use os.rmdir_recursive instead of scripting.run('rm -rf')

tools/oldv.v
@@ -99,8 +99,8 @@ fn main() {
 		context.cc = ecc
 	}
 	if context.cleanup {
-		scripting.run('rm -rf $context.path_v')
-		scripting.run('rm -rf $context.path_vc')
+		os.rmdir_recursive(context.path_v)
+		os.rmdir_recursive(context.path_vc)
 	}
 	
 	context.compile_oldv_if_needed()

commit 130c3c22ca593d43138a4937a27f3b68e5a4d927

tools/oldv: fix default behaviour (regression caused by 760448a).

tools/modules/scripting/scripting.v
@@ -39,6 +39,17 @@ pub fn chdir(path string) {
 	os.chdir(path)
 }
 
+pub fn rmrf(path string) {
+	verbose_trace(@FN, 'rm -rf $path')
+	if os.exists(path) {
+		if os.is_dir(path) {
+			os.rmdir_recursive(path)
+		}else{
+			os.rm(path)
+		}
+	}
+}
+
 pub fn run(cmd string) string {
 	verbose_trace(@FN, cmd)
 	x := os.exec(cmd) or {

tools/oldv.v
@@ -1,6 +1,5 @@
 import (
 	os
-	os.cmdline
 	flag
 	filepath
 	scripting
@@ -100,8 +99,8 @@ fn main() {
 		context.cc = ecc
 	}
 	if context.cleanup {
-		os.rmdir_recursive(context.path_v)
-		os.rmdir_recursive(context.path_vc)
+		scripting.rmrf(context.path_v)
+		scripting.rmrf(context.path_vc)
 	}
 	
 	context.compile_oldv_if_needed()