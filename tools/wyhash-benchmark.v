1b8b1ac00966e86450a11cc41dd10aaa9c3b12ff
joe-conigliaro <joe.conigliaro@gmail.com>
2020-01-29 04:11:34 +1100

    remove unused var & tidy bench tool

--- tools/wyhash-benchmark.v
+++ tools/wyhash-benchmark.v
@@ -9,47 +9,47 @@ import (
 
 fn main() {
 	sample_size := 10000000
-	min_word_len := 20
-	max_word_len := 40
+	min_str_len := 20
+	max_str_len := 40
+	println('Generating $sample_size strings between $min_str_len - $max_str_len chars long...')
 	mut bytepile := []byte
-	for _ in 0 .. sample_size * max_word_len {
+	for _ in 0 .. sample_size * max_str_len {
 		bytepile << byte(40 + rand.next(125 - 40))
 	}
-	mut positions := []int
+	mut str_lens := []int
 	for _ in 0 .. sample_size {
-		positions << min_word_len + rand.next(max_word_len - min_word_len)
+		str_lens << min_str_len + rand.next(max_str_len - min_str_len)
 	}
-	println('Performing $sample_size hashes, word size between $min_word_len - $max_word_len chars.')
+	println('Hashing each of the generated strings...')
 	t0 := time.ticks()
 	mut start_pos := 0
-	for i, len in positions {
+	for len in str_lens {
 		end_pos := start_pos + len
-		word := string(bytepile[start_pos..end_pos],len)
-		_ = wyhash.wyhash_c(&word.str, u64(word.len), 1)
+		str := string(bytepile[start_pos..end_pos],len)
+		_ = wyhash.wyhash_c(&str.str, u64(str.len), 1)
 		start_pos = end_pos
 	}
 	t1 := time.ticks()
 	d1 := t1 - t0
 	println(' * wyhash4 C: ${d1}ms')
 	start_pos = 0
-	for i, len in positions {
+	for len in str_lens {
 		end_pos := start_pos + len
-		word := string(bytepile[start_pos..end_pos],len)
-		_ = wyhash.sum64_string(word, 1)
+		str := string(bytepile[start_pos..end_pos],len)
+		_ = wyhash.sum64_string(str, 1)
 		start_pos = end_pos
 	}
 	t2 := time.ticks()
 	d2 := t2 - t1
 	println(' * wyhash4: ${d2}ms')
 	start_pos = 0
-	for i, len in positions {
+	for len in str_lens {
 		end_pos := start_pos + len
-		word := string(bytepile[start_pos..end_pos],len)
-		_ = fnv1a.sum64_string(word)
+		str := string(bytepile[start_pos..end_pos],len)
+		_ = fnv1a.sum64_string(str)
 		start_pos = end_pos
 	}
 	t3 := time.ticks()
 	d3 := t3 - t2
 	println(' * fnv1a64: ${d3}ms')
-	println('done.')
 }
