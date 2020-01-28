e4ee10ed6ea6649b8ee248048a7672f2770f6ad4
Delyan Angelov <delian66@gmail.com>
2020-01-28 11:18:20 +0200

    Use stty again to debug.

--- vlib/term/misc_nix.v
+++ vlib/term/misc_nix.v
@@ -1,5 +1,7 @@
 module term
 
+import os
+
 #include <sys/ioctl.h>
 
 pub struct C.winsize {
@@ -16,5 +18,14 @@ pub fn get_terminal_size() (int, int) {
 	w := C.winsize{}
 	C.ioctl(0, C.TIOCGWINSZ, &w)
   eprintln('ws_row: $w.ws_row | ws_col: $w.ws_col | ws_xpixel: $w.ws_xpixel | ws_ypixel: $w.ws_ypixel')
+  if cols := os.exec('tput cols') {
+     eprintln('tput cols: $cols.output')
+  }
+  if lines := os.exec('tput lines') {
+     eprintln('tput lines: $lines.output')
+  }
+  if stty := os.exec('stty -a') {
+     eprintln('stty: $stty.output')
+  }
 	return int(w.ws_col), int(w.ws_row)
 }
