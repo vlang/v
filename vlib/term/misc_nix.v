f3bc196d2e20a9df6fe5f4962d8a93b9452b68a4
Delyan Angelov <delian66@gmail.com>
2020-01-28 12:12:03 +0200

    Fix by checking if the stdout is a real terminal.

--- vlib/term/misc_nix.v
+++ vlib/term/misc_nix.v
@@ -3,29 +3,22 @@ module term
 import os
 
 #include <sys/ioctl.h>
-
 pub struct C.winsize {
 pub:
-	ws_row u16
-	ws_col u16
-  ws_xpixel u16
-  ws_ypixel u16
+	ws_row    u16
+	ws_col    u16
+	ws_xpixel u16
+	ws_ypixel u16
 }
 
 fn C.ioctl(fd int, request u64, arg voidptr) int
 
-pub fn get_terminal_size() (int, int) {
+
+pub fn get_terminal_size() (int,int) {
+	if is_atty(1) <= 0 || os.getenv('TERM') == 'dumb' {
+		return 80,25
+	}
 	w := C.winsize{}
 	C.ioctl(0, C.TIOCGWINSZ, &w)
-  eprintln('ws_row: $w.ws_row | ws_col: $w.ws_col | ws_xpixel: $w.ws_xpixel | ws_ypixel: $w.ws_ypixel')
-  if cols := os.exec('tput cols') {
-     eprintln('tput cols: $cols.output')
-  }
-  if lines := os.exec('tput lines') {
-     eprintln('tput lines: $lines.output')
-  }
-  if stty := os.exec('stty -a') {
-     eprintln('stty: $stty.output')
-  }
-	return int(w.ws_col), int(w.ws_row)
+	return int(w.ws_col),int(w.ws_row)
 }
