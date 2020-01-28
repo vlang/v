9ac0c54eb0e336ac88f470357da962d878c6b67d
Delyan Angelov <delian66@gmail.com>
2020-01-28 21:25:17 +0200

    term: fix too long h_divider lines in CI

--- vlib/term/misc.v
+++ vlib/term/misc.v
@@ -1,15 +1,12 @@
 module term
-
 // h_divider will return a horizontal divider line with a dynamic width,
 // that depends on the current terminal settings
 pub fn h_divider(divider string) string {
 	mut cols := 76
-	term_cols, _ := get_terminal_size()
-
+	term_cols,_ := get_terminal_size()
 	if term_cols > 0 {
 		cols = term_cols
 	}
-
 	result := divider.repeat(1 + (cols / divider.len))
 	return result[0..cols]
 }
--- vlib/term/misc_nix.v
+++ vlib/term/misc_nix.v
@@ -1,20 +1,24 @@
 module term
 
-#include <sys/ioctl.h>
+import os
 
-struct C.winsize{
-	pub:
-	ws_row int
-	ws_col int
+#include <sys/ioctl.h>
+pub struct C.winsize {
+pub:
+	ws_row    u16
+	ws_col    u16
+	ws_xpixel u16
+	ws_ypixel u16
 }
 
-fn C.ioctl() int
+fn C.ioctl(fd int, request u64, arg voidptr) int
 
-pub fn get_terminal_size() (int, int) {
-	// TODO: check for resize events
 
-	mut w := C.winsize{}
+pub fn get_terminal_size() (int,int) {
+	if is_atty(1) <= 0 || os.getenv('TERM') == 'dumb' {
+		return 80,25
+	}
+	w := C.winsize{}
 	C.ioctl(0, C.TIOCGWINSZ, &w)
-
-	return w.ws_col, w.ws_row
+	return int(w.ws_col),int(w.ws_row)
 }
