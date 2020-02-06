6c3613407dce5aa044f7679b5e9170f7b04f46bf
Alexander Medvednikov <alexander@medvednikov.com>
2020-02-06 10:41:10 +0100

    vweb: fix header.html

--- vlib/vweb/tmpl/tmpl.v
+++ vlib/vweb/tmpl/tmpl.v
@@ -17,7 +17,7 @@ pub fn compile_template(path string) string {
 		panic('html failed')
 	}
 	mut header := ''
-	if os.exists('header.html') {
+	if os.exists('header.html') && html.contains('@header') {
 		h := os.read_file('header.html')or{
 			panic('reading file header.html failed')
 		}
