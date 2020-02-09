5d79934110e1393b88d8f971d88550427acfebea
joe-conigliaro <joe.conigliaro@gmail.com>
2020-02-09 16:08:49 +1100

    no cast

--- vlib/math/bits/bits.v
+++ vlib/math/bits/bits.v
@@ -28,8 +28,7 @@ const (
 const (
 	// save importing math mod just for these
 	max_u32 = 4294967295
-	// max_u64 = 18446744073709551615
-	max_u64 = C.UINT64_MAX // OSX test
+	max_u64 = 18446744073709551615
 )
 
 // --- LeadingZeros ---
