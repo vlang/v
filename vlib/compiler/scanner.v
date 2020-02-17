80861f2219705b4101293c4ccb0aa033d090d4a1
penguindark <57967770+penguindark@users.noreply.github.com>
2020-02-17 02:35:01 +0100

    scanner: exponent without sign

--- vlib/compiler/scanner.v
+++ vlib/compiler/scanner.v
@@ -213,8 +213,13 @@ fn (s mut Scanner) ident_dec_number() string {
 	}
 	// scan exponential part
 	mut has_exponential_part := false
-	if s.expect('e+', s.pos) || s.expect('e-', s.pos) {
-		exp_start_pos := s.pos += 2
+	if s.expect('e', s.pos) || s.expect('E', s.pos) {
+		exp_start_pos := (s.pos++)
+
+		if s.text[s.pos] in [`-`, `+`] {
+			s.pos++
+		}
+
 		for s.pos < s.text.len && s.text[s.pos].is_digit() {
 			s.pos++
 		}
