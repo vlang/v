9b271d172847a69fa936ecc9eaa6adf9a8c8d0f5
joe-conigliaro <joe.conigliaro@gmail.com>
2020-02-19 16:06:16 +1100

    v2: remove left over testing printlns

--- vlib/v/table/modules.v
+++ vlib/v/table/modules.v
@@ -6,11 +6,9 @@ import os
 // this is not optimal
 pub fn (table &Table) qualify_module(mod string, file_path string) string {
 	for m in table.imports {
-		println('TABLE IMPORT: $m')
 		if m.contains('.') && m.contains(mod) {
 			m_parts := m.split('.')
 			m_path := m_parts.join(os.path_separator)
-			println('IMPORT B: $m - $m_path')
 			if mod == m_parts[m_parts.len - 1] && file_path.contains(m_path) {
 				return m
 			}
