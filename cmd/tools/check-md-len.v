883feb92c7f829263531ead50bae93a53c327e71
Delyan Angelov <delian66@gmail.com>
2020-02-20 13:17:25 +0200

    Cleanup code formatting.

--- cmd/tools/check-md-len.v
+++ cmd/tools/check-md-len.v
@@ -9,25 +9,23 @@ const (
 fn main() {
 	files_paths := os.args[1..]
 	mut errors := 0
-
 	for file_path in files_paths {
 		real_path := os.realpath(file_path)
-		lines := os.read_lines(real_path) or { continue }
-		mut line_num := 1
-
-		for line in lines {
+		lines := os.read_lines(real_path) or {
+			continue
+		}
+		for i, line in lines {
 			if line.len > too_long_line_length {
-				eprintln('$real_path:$line_num:${line.len+1}: line too long')
+				eprintln('$real_path:${i+1}:${line.len+1}: line too long')
 				errors++
 			}
-			line_num++
 		}
 	}
-
 	// TODO: uncomment this AFTER doc/docs.md line lengths are fixed
 	/*	
 	if errors > 0 {
 		exit(1)
 	}
 	*/
+
 }
