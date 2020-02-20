e3a07fb477733cefe9535c3ac95b3af6256e68bb
Delyan Angelov <delian66@gmail.com>
2020-02-20 12:35:00 +0200

    Better integration with IDEs.

--- cmd/tools/check-md-len.v
+++ cmd/tools/check-md-len.v
@@ -2,6 +2,10 @@ module main
 
 import os
 
+const (
+	too_long_line_length = 100
+)
+
 fn main() {
 	files_paths := os.args[1..]
 	mut errors := 0
@@ -12,8 +16,8 @@ fn main() {
 		mut line_num := 1
 
 		for line in lines {
-			if line.len > 100 {
-				eprintln('$real_path:$line_num with $line.len column width')
+			if line.len > too_long_line_length {
+				eprintln('$real_path:$line_num:${line.len+1}: line too long')
 				errors++
 			}
 			line_num++
