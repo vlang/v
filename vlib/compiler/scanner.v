661ada8d247bc7cb6a2c611700f0c9a77cd3954b
SleepyRoy <47302112+SleepyRoy@users.noreply.github.com>
2020-02-26 01:15:54 +0800

    more (1)

--- vlib/compiler/scanner.v
+++ vlib/compiler/scanner.v
@@ -98,11 +98,7 @@ fn scan_res(tok TokenKind, lit string) ScanRes {
 fn (s mut Scanner) ident_name() string {
 	start := s.pos
 	s.pos++
-	for s.pos < s.text.len {
-		c := s.text[s.pos]
-		if !is_name_char(c) && !c.is_digit() {
-			break
-		}
+	for s.pos < s.text.len && (is_name_char(s.text[s.pos]) || s.text[s.pos].is_digit()) {
 		s.pos++
 	}
 	name := s.text[start..s.pos]
