17fd0b1a9dec90b8f14eb0fd7769164edc34494b
SleepyRoy <47302112+SleepyRoy@users.noreply.github.com>
2020-02-25 15:22:34 +0800

    v/scanner (v2)

--- vlib/v/scanner/scanner.v
+++ vlib/v/scanner/scanner.v
@@ -131,6 +131,8 @@ fn filter_num_sep(txt byteptr, start int, end int) string {
 }
 
 fn (s mut Scanner) ident_bin_number() string {
+	mut has_wrong_digit := false
+	mut first_wrong_digit := `\0`
 	start_pos := s.pos
 	s.pos += 2 // skip '0b'
 	for {
@@ -138,13 +140,20 @@ fn (s mut Scanner) ident_bin_number() string {
 			break
 		}
 		c := s.text[s.pos]
-		if !c.is_bin_digit() && c != num_sep {
+		if !c.is_digit() && !c.is_letter() && c != num_sep {
 			break
+		}
+		if !has_wrong_digit && !c.is_bin_digit() {
+			has_wrong_digit = true
+			first_wrong_digit = c
 		}
 		s.pos++
 	}
 	if start_pos + 2 == s.pos {
 		s.error('number part of this binary is not provided')
+	}
+	else if has_wrong_digit {
+		s.error('this binary number has unsuitable digit `${first_wrong_digit.str()}`')
 	}
 	number := filter_num_sep(s.text.str, start_pos, s.pos)
 	s.pos--
@@ -152,6 +161,8 @@ fn (s mut Scanner) ident_bin_number() string {
 }
 
 fn (s mut Scanner) ident_hex_number() string {
+	mut has_wrong_digit := false
+	mut first_wrong_digit := `\0`
 	start_pos := s.pos
 	s.pos += 2 // skip '0x'
 	for {
@@ -159,13 +170,20 @@ fn (s mut Scanner) ident_hex_number() string {
 			break
 		}
 		c := s.text[s.pos]
-		if !c.is_hex_digit() && c != num_sep {
+		if !c.is_digit() && !c.is_letter() && c != num_sep {
 			break
+		}
+		if !has_wrong_digit && !c.is_hex_digit() {
+			has_wrong_digit = true
+			first_wrong_digit = c
 		}
 		s.pos++
 	}
 	if start_pos + 2 == s.pos {
 		s.error('number part of this hexadecimal is not provided')
+	}
+	else if has_wrong_digit {
+		s.error('this hexadecimal number has unsuitable digit `${first_wrong_digit.str()}`')
 	}
 	number := filter_num_sep(s.text.str, start_pos, s.pos)
 	s.pos--
@@ -173,6 +191,8 @@ fn (s mut Scanner) ident_hex_number() string {
 }
 
 fn (s mut Scanner) ident_oct_number() string {
+	mut has_wrong_digit := false
+	mut first_wrong_digit := `\0`
 	start_pos := s.pos
 	s.pos += 2 // skip '0o'
 	for {
@@ -180,13 +200,20 @@ fn (s mut Scanner) ident_oct_number() string {
 			break
 		}
 		c := s.text[s.pos]
-		if !c.is_oct_digit() && c != num_sep {
+		if !c.is_digit() && !c.is_letter() && c != num_sep {
 			break
+		}
+		if !has_wrong_digit && !c.is_oct_digit() {
+			has_wrong_digit = true
+			first_wrong_digit = c
 		}
 		s.pos++
 	}
 	if start_pos + 2 == s.pos {
 		s.error('number part of this octal is not provided')
+	}
+	else if has_wrong_digit {
+		s.error('this octal number has unsuitable digit `${first_wrong_digit.str()}`')
 	}
 	number := filter_num_sep(s.text.str, start_pos, s.pos)
 	s.pos--
