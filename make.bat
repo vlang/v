0d79e1a2d26dfa7693d5c7fea3abaecb3a9176e6
Luther Wen Xu <lutherchanpublic+git@gmail.com>
2020-02-25 17:51:52 +0800

    ci: Fix MSVC build by removing duplicate flag

--- make.bat
+++ make.bat
@@ -77,8 +77,8 @@ if %ERRORLEVEL% NEQ 0 (
 )
 
 echo rebuild from source (twice, in case of C definitions changes)
-v2.exe -cc msvc -o v3.exe cmd/v
-v3.exe -cc msvc -o v -prod cmd/v
+v2.exe -o v3.exe cmd/v
+v3.exe -o v -prod cmd/v
 if %ERRORLEVEL% NEQ 0 (
     echo V failed to build itself with error %ERRORLEVEL%
     goto :compileerror
