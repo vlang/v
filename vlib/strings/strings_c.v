f4b4a6c1501deb1fecd19136ba4efa4ae5fa3b16
Delyan Angelov <delian66@gmail.com>
2020-02-26 13:11:02 +0200

    Add doc comments for the strings.repeat and strings.repeat_string functions.

--- vlib/strings/strings_c.v
+++ vlib/strings/strings_c.v
@@ -1,5 +1,6 @@
 module strings
 
+// strings.repeat - fill a string with `n` repetitions of the character `c`
 pub fn repeat(c byte, n int) string {
 	if n <= 0 {
 		return ''
@@ -11,7 +12,9 @@ pub fn repeat(c byte, n int) string {
 	return string( bytes, n )
 }
 
-
+// strings.repeat_string - gives you `n` repetitions of the substring `s`
+// NB: strings.repeat, that repeats a single byte, is between 2x
+// and 24x faster than strings.repeat_string called for a 1 char string.
 pub fn repeat_string(s string, n int) string {
 	if n <= 0 || s.len == 0 {
 		return ''
