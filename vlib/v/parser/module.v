06fc8a830bb8b30470efab91b8f0f4dcc930894e
Alexander Medvednikov <alexander@medvednikov.com>
2020-02-27 03:30:17 +0100

    v2: fix a bug with `mod.const`

--- vlib/v/parser/module.v
+++ vlib/v/parser/module.v
@@ -2,16 +2,17 @@
 // Use of this source code is governed by an MIT license
 // that can be found in the LICENSE file.
 module parser
-
 // return true if file being parsed imports `mod`
 pub fn (p &Parser) known_import(mod string) bool {
 	return mod in p.imports
 }
 
 fn (p &Parser) prepend_mod(name string) string {
+	if p.expr_mod != '' {
+		return p.expr_mod + '.' + name
+	}
 	if p.builtin_mod || p.mod == 'main' {
 		return name
 	}
 	return '${p.mod}.$name'
 }
-
