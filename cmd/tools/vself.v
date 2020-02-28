05fc424f91c275598b9f195f4d0dad0914ee10b6
yuyi <yuyi98@163.com>
2020-02-28 19:44:08 +0800

    optimize file processing

--- cmd/tools/vself.v
+++ cmd/tools/vself.v
@@ -11,27 +11,26 @@ fn main() {
 	vroot := filepath.dir(pref.vexe_path())
 	os.chdir(vroot)
 
+	s2 := os.exec('v -o v2 cmd/v') or {
+		panic(err)
+	}
+	println(s2.output)
+
 	$if windows {
 		bak_file := 'v_old.exe'
 		if os.exists(bak_file) {
 			os.rm(bak_file)
 		}
 		os.mv('v.exe', bak_file)
+		os.mv('v2.exe', 'v.exe')
 
-		s2 := os.exec('v_old -o v cmd/v') or {
-			panic(err)
-		}
-		println(s2.output)
+
 	} $else {
 		bak_file := 'v_old'
 		if os.exists(bak_file) {
 			os.rm(bak_file)
 		}
 		os.mv('v', bak_file)
-
-		s2 := os.exec('v_old -o v cmd/v') or {
-			panic(err)
-		}
-		println(s2.output)
+		os.mv('v2', 'v')
 	}
 }
