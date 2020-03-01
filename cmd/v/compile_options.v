041a9dc4440e20c60f64d0f75f76295e87c1779c
danieldaeschle <daniel.daeschle@gmail.com>
2020-03-01 12:55:57 +0100

    cmd: store outfile of run command in temp dir

--- cmd/v/compile_options.v
+++ cmd/v/compile_options.v
@@ -39,6 +39,7 @@ pub fn new_v(args []string) &compiler.V {
 	mut out_name := cmdline.option(args, '-o', '')
 	mut dir := args[args.len-1]//.last()
 	if 'run' in args {
+		out_name = '$os.tmpdir()/v-build'
 		args_after_run := cmdline.only_non_options( cmdline.options_after(args,['run']) )
 		dir = if args_after_run.len>0 { args_after_run[0] } else { '' }
 	}
