e792c396e29b30af6bead2091cbb04e8696f21c0
Luther Wen Xu <lutherchanpublic+git@gmail.com>
2020-03-05 21:12:57 +0800

    vlib/v/builder: Replace is_verbose with verbosity

--- vlib/v/builder/builder.v
+++ vlib/v/builder/builder.v
@@ -208,11 +208,11 @@ pub fn (b &Builder) find_module_path(mod string) ?string {
 	mod_path := module_path(mod)
 	for search_path in b.module_search_paths {
 		try_path := filepath.join(search_path,mod_path)
-		if b.pref.is_verbose {
+		if b.pref.verbosity.is_higher_or_equal(.level_three) {
 			println('  >> trying to find $mod in $try_path ..')
 		}
 		if os.is_dir(try_path) {
-			if b.pref.is_verbose {
+			if b.pref.verbosity.is_higher_or_equal(.level_three) {
 				println('  << found $try_path .')
 			}
 			return try_path
