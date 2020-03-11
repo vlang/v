7c8729c13565067671959f3f67b51a042814897c
Delyan Angelov <delian66@gmail.com>
2020-03-11 16:00:20 +0200

    builtin: implement []int.str() .

--- vlib/builtin/array.v
+++ vlib/builtin/array.v
@@ -372,6 +372,21 @@ pub fn (a []string) str() string {
 	return sb.str()
 }
 
+// []int.str returns a string representation of the array of ints
+// "[ 1, 2, 3 ]"
+pub fn (a []int) str() string {
+	mut sb := strings.new_builder(a.len * 13)
+	sb.write('[')
+	for i in 0..a.len {
+		sb.write(a[i].str())
+		if i < a.len - 1 {
+			sb.write(', ')
+		}
+	}
+	sb.write(']')
+	return sb.str()
+}
+
 // []bool.str returns a string representation of the array of bools
 // "[true, true, false]"
 pub fn (a []bool) str() string {
