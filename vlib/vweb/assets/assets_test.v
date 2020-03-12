54400a424cc85173ad4af4fb28b94bb09527cba0
Ian M. Jones <ian@ianmjones.com>
2020-03-12 23:58:52 +0000

    vweb.assets: fix tests when run from outside of module path.

--- vlib/vweb/assets/assets_test.v
+++ vlib/vweb/assets/assets_test.v
@@ -12,6 +12,40 @@ fn clean_cache_dir(dir string) {
 	}
 }
 
+fn base_cache_dir() string {
+	return os.join_path(os.temp_dir(), 'assets_test_cache')
+}
+
+fn cache_dir(test_name string) string {
+	return os.join_path(base_cache_dir(), test_name)
+}
+
+fn get_test_file_path(file string) string {
+	path := os.join_path(base_cache_dir(), file)
+
+	if ! os.is_dir(base_cache_dir()) {
+		os.mkdir_all(base_cache_dir())
+	}
+
+	if ! os.exists(path) {
+		os.write_file(path, get_test_file_contents(file))
+	}
+
+	return path
+}
+
+fn get_test_file_contents(file string) string {
+	contents := match file {
+		'test1.js' { '{"one": 1}\n' }
+		'test2.js' { '{"two": 2}\n' }
+		'test1.css' { '.one {\n\tcolor: #336699;\n}\n' }
+		'test2.css' { '.two {\n\tcolor: #996633;\n}\n' }
+		else { 'wibble\n' }
+	}
+
+	return contents
+}
+
 fn test_set_cache() {
 	mut am := assets.new_manager()
 	am.cache_dir = 'cache'
@@ -25,32 +59,32 @@ fn test_set_minify() {
 fn test_add() {
 	mut am := assets.new_manager()
 	assert am.add('css', 'testx.css') == false
-	assert am.add('css', 'test1.css') == true
-	assert am.add('js', 'test1.js') == true
-	// assert am.add('css', 'test2.js') == false // TODO: test extension on add
+	assert am.add('css', get_test_file_path('test1.css')) == true
+	assert am.add('js', get_test_file_path('test1.js')) == true
+	// assert am.add('css', get_test_file_path('test2.js')) == false // TODO: test extension on add
 }
 
 fn test_add_css() {
 	mut am := assets.new_manager()
 	assert am.add_css('testx.css') == false
-	assert am.add_css('test1.css') == true
-	// assert am.add_css('test1.js') == false // TODO: test extension on add
+	assert am.add_css(get_test_file_path('test1.css')) == true
+	// assert am.add_css(get_test_file_path('test1.js')) == false // TODO: test extension on add
 }
 
 fn test_add_js() {
 	mut am := assets.new_manager()
 	assert am.add_js('testx.js') == false
-	assert am.add_js('test1.js') == true
-	// assert am.add_js('test1.css') == false // TODO: test extension on add
+	assert am.add_css(get_test_file_path('test1.js')) == true
+	// assert am.add_css(get_test_file_path('test1.css')) == false // TODO: test extension on add
 }
 
 fn test_combine_css() {
 	mut am := assets.new_manager()
-	am.cache_dir = 'combine_css_cache'
+	am.cache_dir = cache_dir('test_combine_css')
 	clean_cache_dir(am.cache_dir)
 
-	am.add_css('test1.css')
-	am.add_css('test2.css')
+	am.add_css(get_test_file_path('test1.css'))
+	am.add_css(get_test_file_path('test2.css'))
 
 	// TODO: How do I test non-minified, is there a "here doc" format that keeps formatting?
 	am.minify = true
@@ -70,11 +104,11 @@ fn test_combine_css() {
 
 fn test_combine_js() {
 	mut am := assets.new_manager()
-	am.cache_dir = 'combine_js_cache'
+	am.cache_dir = cache_dir('test_combine_js')
 	clean_cache_dir(am.cache_dir)
 
-	am.add_js('test1.js')
-	am.add_js('test2.js')
+	am.add_js(get_test_file_path('test1.js'))
+	am.add_js(get_test_file_path('test2.js'))
 
 	expected1 := '{"one": 1}'
 	expected2 := '{"two": 2}'
@@ -103,18 +137,20 @@ fn test_combine_js() {
 
 fn test_include_css() {
 	mut am := assets.new_manager()
-	am.add_css('test1.css')
-	expected := '<link rel="stylesheet" href="test1.css">'
+	file1 := get_test_file_path('test1.css')
+	am.add_css(file1)
+	expected := '<link rel="stylesheet" href="$file1">'
 	actual := am.include_css(false)
 	// assert actual == expected // TODO: Why does this not pass, file/line ending?
 	assert actual.contains(expected)
 
 	// Two lines of output.
-	am.add_css('test2.css')
-	am.cache_dir = 'include_css_cache'
+	file2 := get_test_file_path('test2.css')
+	am.add_css(file2)
+	am.cache_dir = cache_dir('test_include_css')
 	clean_cache_dir(am.cache_dir)
 
-	expected2 := expected + '\n<link rel="stylesheet" href="test2.css">'
+	expected2 := expected + '\n<link rel="stylesheet" href="$file2">'
 	actual2 := am.include_css(false)
 	// assert actual2 == expected2 // TODO: Why does this not pass, file/line ending?
 	assert actual2.contains(expected2)
@@ -135,18 +171,20 @@ fn test_include_css() {
 
 fn test_include_js() {
 	mut am := assets.new_manager()
-	am.add_js('test1.js')
-	expected := '<script type="text/javascript" src="test1.js"></script>'
+	file1 := get_test_file_path('test1.js')
+	am.add_js(file1)
+	expected := '<script type="text/javascript" src="$file1"></script>'
 	actual := am.include_js(false)
 	// assert actual == expected // TODO: Why does this not pass, file/line ending?
 	assert actual.contains(expected)
 
 	// Two lines of output.
-	am.add_js('test2.js')
-	am.cache_dir = 'include_js_cache'
+	file2 := get_test_file_path('test2.js')
+	am.add_js(file2)
+	am.cache_dir = cache_dir('test_include_js')
 	clean_cache_dir(am.cache_dir)
 
-	expected2 := expected + '\n<script type="text/javascript" src="test2.js"></script>'
+	expected2 := expected + '\n<script type="text/javascript" src="$file2"></script>'
 	actual2 := am.include_js(false)
 	// assert actual2 == expected2 // TODO: Why does this not pass, file/line ending?
 	assert actual2.contains(expected2)
