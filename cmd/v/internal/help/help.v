2d3d475560c54174ab653786232ff29f020d7249
Luther Wen Xu <lutherchanpublic+git@gmail.com>
2020-03-13 23:03:31 +0800

    cmd/v: Added `help` module

---
+++ cmd/v/internal/help/help.v
@@ -0,0 +1,31 @@
+module help
+
+import (
+	os
+	v.pref
+)
+
+const (
+	unknown_topic = 'V Error: Unknown help topic provided. Use `v help` for usage information.'
+)
+
+pub fn print_and_exit(topic string) {
+	vexe := pref.vexe_path()
+	vroot := os.dir(vexe)
+
+	bytes := []byte(topic)
+	for b in bytes {
+		if (b >= `a` && b <= `z`) || b == `-` {
+			continue
+		}
+		println(unknown_topic)
+		exit(1)
+	}
+	target_topic := os.join_path(vroot, 'cmd', 'v', 'internal', 'help', '${topic}.txt')
+	content := os.read_file(target_topic) or {
+		println(unknown_topic)
+		exit(1)
+	}
+	println(content)
+	exit(0)
+}
