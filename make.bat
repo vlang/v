08da7163a90db9ed2364af1da88a11aacec6ec12
yuyi <yuyi98@163.com>
2020-03-16 17:25:47 +0800

    optimize make.bat

--- make.bat
+++ make.bat
@@ -15,7 +15,6 @@ REM option to force msvc or gcc
 if "%~1"=="-gcc" goto :gccstrap
 if "%~1"=="-msvc" goto :msvcstrap
 
-
 :gccstrap
 
 echo Attempting to build v.c with GCC...
@@ -28,26 +27,24 @@ if not exist "%gccpath%" (
     goto:msvcstrap
 )
 
-gcc -std=c99 -municode -w -o v2.exe vc\v_win.c
+gcc -std=c99 -municode -w -o v.exe vc\v_win.c
 if %ERRORLEVEL% NEQ 0 (
     echo gcc failed to compile - Create an issue at 'https://github.com/vlang'
+	rd /s /q vc
     exit /b 1
 )
 
 echo Now using V to build V...
-v2.exe -o v3.exe cmd/v
-v3.exe -o v.exe -prod cmd/v
+v self
 if %ERRORLEVEL% NEQ 0 (
     echo v.exe failed to compile itself - Create an issue at 'https://github.com/vlang'
+	rd /s /q vc
     exit /b 1
 )
 
-del v2.exe
-del v3.exe
 rd /s /q vc
 
 goto :success
-
 
 :msvcstrap
 echo Attempting to build v.c  with MSVC...
@@ -70,22 +67,19 @@ if exist "%InstallDir%\Common7\Tools\vsdevcmd.bat" (
 
 set ObjFile=.v.c.obj
 
-cl.exe /nologo /w /volatile:ms /Fo%ObjFile% /O2 /MD /D_VBOOTSTRAP vc\v_win.c user32.lib kernel32.lib advapi32.lib shell32.lib /link /NOLOGO /OUT:v2.exe /INCREMENTAL:NO
+cl.exe /nologo /w /volatile:ms /Fo%ObjFile% /O2 /MD /D_VBOOTSTRAP vc\v_win.c user32.lib kernel32.lib advapi32.lib shell32.lib /link /NOLOGO /OUT:v.exe /INCREMENTAL:NO
 if %ERRORLEVEL% NEQ 0 (
     echo cl.exe failed to build V
     goto :compileerror
 )
 
 echo rebuild from source (twice, in case of C definitions changes)
-v2.exe -o v3.exe cmd/v
-v3.exe -o v -prod cmd/v
+v self
 if %ERRORLEVEL% NEQ 0 (
     echo V failed to build itself with error %ERRORLEVEL%
     goto :compileerror
 )
 
-del v2.exe
-del v3.exe
 rd /s /q vc
 del v.pdb
 del v3.ilk
@@ -107,6 +101,12 @@ goto :error
 
 :compileerror
 echo Failed to compile - Create an issue at 'https://github.com/vlang' and tag '@emily33901'!
+rd /s /q vc
+del v.pdb
+del v3.ilk
+del v3.pdb
+del vc140.pdb
+del %ObjFile%
 goto :error
 
 :error
