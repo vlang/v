aab4102acabddfdd1373e6e2030b1ce3a1de95ab
SleepyRoy <47302112+SleepyRoy@users.noreply.github.com>
2020-03-17 16:52:24 +0800

    fix test

--- vlib/compiler/tests/type_alias_test.v
+++ vlib/compiler/tests/type_alias_test.v
@@ -10,8 +10,8 @@ fn test_type_alias() {
 	f1 := Myf32(1.0)
 	assert f1 + f32(3.14) == f32(4.14)
 	
-	f2 := Myf64(-10.4)
-	assert f2 + 0.5 == 9.9  
+	f2 := Myf64(10.4)
+	assert f2 + 0.5 == 10.9  
 
 
 
