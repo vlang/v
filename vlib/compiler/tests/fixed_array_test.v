3dea6ff47b23cf63b7e2d9a901b7356dcebcd046
SleepyRoy <47302112+SleepyRoy@users.noreply.github.com>
2020-03-17 16:26:01 +0800

    fix test (3)

--- vlib/compiler/tests/fixed_array_test.v
+++ vlib/compiler/tests/fixed_array_test.v
@@ -1,7 +1,7 @@
 
 fn test_fixed_array_can_be_assigned(){
 	x := 2.32
-	mut v := [8]f32
+	mut v := [8]f64
 	v = [1.0, x, 3.0,4.0,5.0,6.0,7.0,8.0]!!
 	assert v[1] == x
 }
@@ -15,7 +15,7 @@ fn test_fixed_array_can_be_used_in_declaration(){
 
 struct Context {
 	pub mut:
-	vb [8]f32
+	vb [8]f64
 }
 fn test_fixed_array_can_be_assigned_to_a_struct_field(){
 	mut ctx := Context{}
