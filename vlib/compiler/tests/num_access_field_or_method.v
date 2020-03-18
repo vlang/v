e2257b9bd1cd9e752f8dfc00f68b014354eab2ac
SleepyRoy <47302112+SleepyRoy@users.noreply.github.com>
2020-03-19 02:27:48 +0800

    fix test

--- vlib/compiler/tests/num_access_field_or_method.v
+++ vlib/compiler/tests/num_access_field_or_method.v
@@ -1,23 +1,23 @@
-fn int_access_field_or_method() {
-    x1 := 1234.str()
+fn int_lit_call_method() {
+	x1 := (1234).str()
     assert x1 == '1234'
-    x2 := -0xffff.str()
+	x2 := (-0xffff).str()
     assert x2 == '-65535'
     x3 := 0b1001001.str()
     assert x3 == '73'
     x4 := 0o653262.str()
     assert x4 == '218802'
-    x5 := 0.str()
+	x5 := (0).str()
     assert x5 == '0'
 }
 
-fn float_access_field_or_method() {
-    x1 := -123.66.str()
+fn float_lit_call_method() {
+	x1 := (-123.66).str()
     assert x1 == '-1.2366e+02'
-    x2 := 12.5e-2.str()
+	x2 := (12.5e-2).str()
     assert x2 == '1.25e-01'
-    x3 := .789.str()
+	x3 := (.789).str()
     assert x3 == '7.89e-01'
-    x4 := .003e2.str()
+	x4 := (.003e2).str()
     assert x4 == '3.e-01'
 }
