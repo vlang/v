59f40aca265b9dfaee9539751f38d1e934af35d6
Delyan Angelov <delian66@gmail.com>
2020-03-20 22:50:08 +0200

    Fix live_test.v and make it report more details to diagnose problems more easily.

--- vlib/compiler/tests/live_test.v
+++ vlib/compiler/tests/live_test.v
@@ -2,21 +2,25 @@ import os
 import time
 
 const (
-        vexe = os.getenv('VEXE')
-        source_file = os.join_path(os.cache_dir(), 'generated_live_program.v')
-        output_file = os.join_path(os.cache_dir(), 'generated_live_program.output.txt')
-        live_program_source = "
+	vexe = os.getenv('VEXE')
+	source_file = os.join_path(os.cache_dir(), 'generated_live_program.v')
+	output_file = os.join_path(os.cache_dir(), 'generated_live_program.output.txt')
+	live_program_source = "
 module main
 import time
+
 [live]
 fn pmessage() {
-        println('ORIGINAL')
+	println('ORIGINAL')
 }
+
 fn main() {
-        for i := 0; i<6*100; i++ {
-                pmessage()
-                time.sleep_ms(10)
-        }
+	println('START')
+	for i := 0; i<6*100; i++ {
+		pmessage()
+		time.sleep_ms(10)
+	}
+	println('END')
 }
 "
 )
@@ -24,57 +28,61 @@ fn main() {
 //
 
 fn testsuite_begin(){
-        os.write_file(source_file,live_program_source)
+	os.write_file(source_file, live_program_source)
 }
 
 fn testsuite_end(){
-        os.rm( source_file )
-        eprintln('source: $source_file')
-        eprintln('output: $output_file')
-        output_lines := os.read_lines( output_file ) or {
-                return
-        }
-        mut histogram := map[string]int
-        for line in output_lines {
-                histogram[line] = histogram[line] + 1
-        }
-        for k,v in histogram {
-                eprintln('> found ${k} $v times.')
-        }
-        assert histogram['ORIGINAL'] > 0
-        assert histogram['CHANGED'] + histogram['ANOTHER'] > 0
+	os.rm( source_file )
+	eprintln('source: $source_file')
+	eprintln('output: $output_file')
+	output_lines := os.read_lines( output_file ) or {
+		return
+	}
+	mut histogram := map[string]int
+	for line in output_lines {
+		histogram[line] = histogram[line] + 1
+	}
+	for k,v in histogram {
+		println('> found ${k} $v times.')
+	}
+	println('---------------------------------------------------------------------------')
+	println('---------------------------------------------------------------------------')
+	assert histogram['START'] > 0
+	assert histogram['END'] > 0
+	assert histogram['CHANGED'] + histogram['ANOTHER'] > 0
+	assert histogram['ORIGINAL'] > 0
 }
 
 fn change_source(new string){
-        time.sleep_ms(250)
-        eprintln('> change ORIGINAL to: $new')
-        os.write_file(source_file,live_program_source.replace('ORIGINAL', new))
-        time.sleep_ms(1000)
-        eprintln('> done.')
+	time.sleep_ms(250)
+	eprintln('> change ORIGINAL to: $new')
+	os.write_file(source_file,live_program_source.replace('ORIGINAL', new))
+	time.sleep_ms(1000)
+	eprintln('> done.')
 }
 
 //
 
 fn test_live_program_can_be_compiled(){
-        cmd := '$vexe -live run $source_file > $output_file &'
-        eprintln('Compiling and running with: $cmd')
-        res := os.system(cmd)
-        eprintln('... running in the background')
-        time.sleep_ms(2000)
-        assert res == 0
+	cmd := '$vexe -live run $source_file > $output_file &'
+	eprintln('Compiling and running with: $cmd')
+	res := os.system(cmd)
+	eprintln('... running in the background')
+	time.sleep_ms(3000)
+	assert res == 0
 }
 
 fn test_live_program_can_be_changed_1(){
-        change_source('CHANGED')
-        assert true
+	change_source('CHANGED')
+	assert true
 }
 
 fn test_live_program_can_be_changed_2(){
-        change_source('ANOTHER')
-        assert true
+	change_source('ANOTHER')
+	assert true
 }
 
 fn test_live_program_has_ended(){
 	time.sleep_ms(10*1000)
-	 assert true
+	assert true
 }
