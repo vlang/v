85e411bd7c266ac18843affb92e99b4e50b4c704
Alexander Medvednikov <alexander@medvednikov.com>
2020-03-21 20:10:30 +0100

    fix valgrind test

--- vlib/v/tests/valgrind/valgrind_test.v
+++ vlib/v/tests/valgrind/valgrind_test.v
@@ -4,12 +4,16 @@ import term
 fn test_all() {
 	$if !linux {
 		println('Valgrind tests can only be run on Linux.')
-		exit(1)
+		exit(0)
 	}
 	exe := os.executable()
 	dir := os.dir(exe)
-	files := os.ls('$dir/vlib/v/tests/valgrind/') or {
+	println(dir)
+	println(111)
+	// files := os.ls('$dir/vlib/v/tests/valgrind/') or {
+	files := os.ls(dir) or {
 		panic(err)
 	}
-	println(files)
+	tests := files.filter(it.ends_with('.vv'))
+	println(tests)
 }
