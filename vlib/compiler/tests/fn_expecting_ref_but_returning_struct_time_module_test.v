2efc37947d925c71778c755c4d24d0552afdfee6
yuyi <yuyi98@163.com>
2020-03-23 02:43:59 +0800

    time: calc_unix => unix_time

--- vlib/compiler/tests/fn_expecting_ref_but_returning_struct_time_module_test.v
+++ vlib/compiler/tests/fn_expecting_ref_but_returning_struct_time_module_test.v
@@ -2,7 +2,7 @@ import time.misc as tmisc
 // using a manual temporary intermediate variable should always work:
 fn test_call_fn_that_requires_reference_with_function_that_returns_a_struct_manual() {
 	t1 := tmisc.random()
-	t2 := t1.calc_unix()
+	t2 := t1.unix_time()
 	println('res: $t2')
 	assert true
 }
@@ -11,7 +11,7 @@ fn test_call_fn_that_requires_reference_with_function_that_returns_a_struct_manu
 // TODO: Fix this.
 // v should produce temporary intermediate variables in chained calls:
 fn test_call_fn_that_requires_reference_with_function_that_returns_a_struct_chained(){
-	res := (tmisc.random().calc_unix())
+	res := (tmisc.random().unix_time())
 	println('res: $res')
 	assert true
 }
