839399b1127dd0b6937b0055df4c2b8de6c9d369
Delyan Angelov <delian66@gmail.com>
2020-03-26 16:49:01 +0200

    run v fmt over vlib/compiler/tests/string_interpolation_test.v .

--- vlib/compiler/tests/string_interpolation_test.v
+++ vlib/compiler/tests/string_interpolation_test.v
@@ -1,4 +1,4 @@
-fn test_simple_string_interpolation(){
+fn test_simple_string_interpolation() {
 	a := 'Hello'
 	b := 'World'
 	res := '$a $b'
@@ -16,17 +16,14 @@ fn test_mixed_string_interpolation() {
 	assert s3 == 'a: 7 | b: abc'
 }
 
-
 fn test_formatted_string_interpolation() {
 	x := 'abc'
-	axb  := 'a:$x:b'
+	axb := 'a:$x:b'
 	assert axb == 'a:abc:b'
-	 
 	x_10 := 'a:${x:10s}:b'
 	x10_ := 'a:${x:-10s}:b'
 	assert x_10 == 'a:       abc:b'
 	assert x10_ == 'a:abc       :b'
-	 
 	i := 23
 	si_right := '${i:10d}'
 	si__left := '${i:-10d}'
@@ -35,30 +32,26 @@ fn test_formatted_string_interpolation() {
 }
 
 fn test_excape_dollar_in_string() {
-  i := 42
-
-  assert '($i)' == '(42)'
-  assert '(\$i)'.contains('i') && !'(\$i)'.contains('42')
-  assert !'(\\$i)'.contains('i') && '(\\$i)'.contains('42') && '(\\$i)'.contains('\\')
-  assert '(\\\$i)'.contains('i') && !'(\\\$i)'.contains('42') && '(\\$i)'.contains('\\')
-  assert !'(\\\\$i)'.contains('i') && '(\\\\$i)'.contains('42') && '(\\\\$i)'.contains('\\\\')
-
-  assert '(${i})' == '(42)'
-  assert '(\${i})'.contains('i') && !'(\${i})'.contains('42')
-  assert !'(\\${i})'.contains('i') && '(\\${i})'.contains('42') && '(\\${i})'.contains('\\')
-  assert '(\\\${i})'.contains('i') && !'(\\\${i})'.contains('42') && '(\\${i})'.contains('\\')
-  assert !'(\\\\${i})'.contains('i') && '(\\\\${i})'.contains('42') && '(\\\\${i})'.contains('\\\\')
-  assert i==42
+	i := 42
+	assert '($i)' == '(42)'
+	assert '(\$i)'.contains('i') && !'(\$i)'.contains('42')
+	assert !'(\\$i)'.contains('i') && '(\\$i)'.contains('42') && '(\\$i)'.contains('\\')
+	assert '(\\\$i)'.contains('i') && !'(\\\$i)'.contains('42') && '(\\$i)'.contains('\\')
+	assert !'(\\\\$i)'.contains('i') && '(\\\\$i)'.contains('42') && '(\\\\$i)'.contains('\\\\')
+	assert '(${i})' == '(42)'
+	assert '(\${i})'.contains('i') && !'(\${i})'.contains('42')
+	assert !'(\\${i})'.contains('i') && '(\\${i})'.contains('42') && '(\\${i})'.contains('\\')
+	assert '(\\\${i})'.contains('i') && !'(\\\${i})'.contains('42') && '(\\${i})'.contains('\\')
+	assert !'(\\\\${i})'.contains('i') && '(\\\\${i})'.contains('42') && '(\\\\${i})'.contains('\\\\')
+	assert i == 42
 }
 
 fn test_implicit_str() {
-  i := 42
-  assert 'int $i' == 'int 42'
-  assert '$i' == '42'
-
-  check := '$i' == '42'
-  assert check
-
-  text := '$i' + '42'
-  assert text == '4242'
+	i := 42
+	assert 'int $i' == 'int 42'
+	assert '$i' == '42'
+	check := '$i' == '42'
+	assert check
+	text := '$i' + '42'
+	assert text == '4242'
 }
