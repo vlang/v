ddba541535a4df4146d1031a6fb844a96101c019
Delyan Angelov <delian66@gmail.com>
2020-03-28 09:35:10 +0200

    Reindent files with v fmt.

--- vlib/math/utilities/utilities.v
+++ vlib/math/utilities/utilities.v
@@ -1,9 +1,8 @@
 module utilities
 
 import math
-
 // copysign returns a value with the magnitude of x and the sign of y
 pub fn copysign(x, y f64) f64 {
-	sign := u64(1) << 63	 
-	return math.f64_from_bits( (math.f64_bits(x) & ~sign) | (math.f64_bits(y)&sign) )
+	sign := u64(1)<<63
+	return math.f64_from_bits((math.f64_bits(x) & ~sign) | (math.f64_bits(y) & sign))
 }
