5889c6ef5c6574f8714518da1675b624bc6a50d5
Delyan Angelov <delian66@gmail.com>
2020-03-28 09:36:53 +0200

    Reformat with v -2 fmt.

--- vlib/math/utilities/utilities.v
+++ vlib/math/utilities/utilities.v
@@ -1,8 +1,9 @@
 module utilities
 
 import math
+
 // copysign returns a value with the magnitude of x and the sign of y
 pub fn copysign(x, y f64) f64 {
-	sign := u64(1)<<63
+	sign := u64(1) << 63
 	return math.f64_from_bits((math.f64_bits(x) & ~sign) | (math.f64_bits(y) & sign))
 }
