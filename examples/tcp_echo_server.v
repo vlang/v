c675d173ddd28199707a6327748b1b7993c94b73
Delyan Angelov <delian66@gmail.com>
2020-03-28 10:40:28 +0200

    Add comments about client usage to examples/tcp_echo_server.v .

--- examples/tcp_echo_server.v
+++ examples/tcp_echo_server.v
@@ -1,5 +1,11 @@
 import net
 
+// This file shows how a basic TCP echo server can be implemented using
+// the `net` module. You can connect to the server by using netcat
+// or telnet, in separate shells, for example:
+// `nc 127.0.0.1 12345`
+// `telnet 127.0.0.1 12345`
+
 fn handle_connection(con net.Socket) {
 	if _ := con.send_string("Welcome to V's TCP Echo server.\n") {
 		for	 {
