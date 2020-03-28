a14d9971dff9459696d93ddc4eb367613348d4a0
Delyan Angelov <delian66@gmail.com>
2020-03-28 09:32:53 +0200

    Use bit manipulation for the implementation of copysign instead of calling math.atan2 and math.abs .

--- vlib/math/utilities/utilities.v
+++ vlib/math/utilities/utilities.v
@@ -1,11 +1,9 @@
 module utilities
+
 import math
 
-pub fn copysign(x f64, y f64) f64 {
-
-	if(y > 0. || (y == 0. && math.atan2(y, -1.) > 0.)) {
-		return math.abs(x)
-        } else {
-            return -math.abs(x)
-        }
+// copysign returns a value with the magnitude of x and the sign of y
+pub fn copysign(x, y f64) f64 {
+	sign := u64(1) << 63	 
+	return math.f64_from_bits( (math.f64_bits(x) & ~sign) | (math.f64_bits(y)&sign) )
 }
