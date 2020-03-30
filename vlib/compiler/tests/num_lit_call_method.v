f817e1f18fe93fc5a94516e5dcd4718a66da5641
SleepyRoy <47302112+SleepyRoy@users.noreply.github.com>
2020-03-30 23:19:18 +0800

    Update num_lit_call_method.v

--- vlib/compiler/tests/num_lit_call_method.v
+++ vlib/compiler/tests/num_lit_call_method.v
@@ -20,4 +20,7 @@ fn float_lit_call_method() {
     assert x3 == '7.89e-01'
 	x4 := .003e2.str()
     assert x4 == '3.e-01'
+	x5 := 2.e-3.str() 
+	assert x5 == '2.e-03'
+	// x6 := 5..str()    Syntax `5.` is allowed, but do not call method on it (it's parsed as a range). Use 5.0.str() instead.
 }
