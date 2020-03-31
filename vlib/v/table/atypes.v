f8169fee487ee2eae15870706ba9390b6b1e2cb1
joe-conigliaro <joe.conigliaro@gmail.com>
2020-03-31 16:17:26 +1100

    table: add i8 to number_type_idxs

--- vlib/v/table/atypes.v
+++ vlib/v/table/atypes.v
@@ -175,7 +175,7 @@ pub const (
 )
 
 pub const (
-	number_type_idxs = [int_type_idx, byte_type_idx, u16_type_idx, i16_type_idx, i64_type_idx, u32_type_idx, u64_type_idx, f32_type_idx, f64_type_idx]
+	number_type_idxs = [i8_type_idx, i16_type_idx, int_type_idx, i64_type_idx, byte_type_idx, u16_type_idx, u32_type_idx, u64_type_idx, f32_type_idx, f64_type_idx]
 	pointer_type_idxs = [voidptr_type_idx, byteptr_type_idx, charptr_type_idx]
 )
 
