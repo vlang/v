81c897c526bc26bb5ee0c6d697b268d8967ddf52
Kris Cherven <50562493+krischerven@users.noreply.github.com>
2020-04-08 17:43:25 -0400

    initial tests

--- vlib/v/tests/array_equality_test.v
+++ vlib/v/tests/array_equality_test.v
@@ -1,4 +1,34 @@
+struct Tester {
+	b bool
+}
+
 fn test_array_equality() {
 	strlist := ["a", "b", "c"]
 	assert strlist == ["a", "b", "c"]
+	assert strlist != ["a", "c", "b"]
+	assert strlist != ["b", "c", "a"]
+	assert strlist != ["b", "a", "c"]
+	assert strlist != ["c", "b", "a"]
+	assert strlist != ["c", "a", "b"]
+	boollist := [true, true, false]
+	assert boollist == [true, true, false]
+	assert boollist != [true, false, false]
+	assert boollist != [false, true, true]
+	assert boollist != [false, false, true]
+	assert boollist != [false, false, false]
+	assert boollist != [false, true, false]
+	intlist := [1, 2, 3]
+	assert intlist == [1, 2, 3]
+	assert intlist != [1, 3, 2]
+	assert intlist != [2, 3, 1]
+	assert intlist != [2, 1, 3]
+	assert intlist != [3, 2, 1]
+	assert intlist != [3, 1, 2]
+	t := Tester{true}
+	f := Tester{false}
+	testerlist := [t, f]
+	assert testerlist == [t, f]
+	assert testerlist != [t, t]
+	assert testerlist != [f, f]
+	assert testerlist != [f, t]
 }
