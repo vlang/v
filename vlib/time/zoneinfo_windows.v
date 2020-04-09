3fe28cc5a41f70ee9cae5afe7f6e52c51488ca09
Keito Nagai <keito940@live.jp>
2020-04-09 19:30:20 +0900

    Duration API Implement.

--- vlib/time/zoneinfo_windows.v
+++ vlib/time/zoneinfo_windows.v
@@ -3,18 +3,28 @@ module time
 #flag -lws2_32
 #include <Timezoneapi.h>
 
+// Windows's TimeZone API.
 struct TimeZoneInfomation{
-	Bias 		 int
-	StandardName [32]u16
+	Bias 		 u32
+	StandardBias u32
+	DaylightBias u32
 	StandardDate C.SYSTEMTIME
-	StandardBias int
-	DaylightName [32]u16
 	DaylightName C.SYSTEMTIME
-	DaylightBias int
 }
 
-fn SetTimeZoneInfomation(TIME_ZONE_INFORMATION *C.lpTimeZoneInfomation) bool
-fn GetTimeZoneInfomation(LPTIME_ZONE_INFORMATION *C.lpTimeZoneInfomation) 
+struct C.SYSTEMTIME {
+	wYear u16
+	wMonth u16
+	wDayOfWeek u16
+	wDay u16
+	wHour u16 
+	wMinute u16
+	wSecond u16
+	wMillseconds u16
+}
+
+fn C.SetTimeZoneInfomation(TIME_ZONE_INFORMATION *C.lpTimeZoneInfomation)
+fn C.GetTimeZoneInfomation(LPTIME_ZONE_INFORMATION *C.lpTimeZoneInfomation) 
 
 pub fn() get_system_timezone(){
 	
