56c3a8b9a29169b73ce411c5e4e7d0b1fdff88aa
Alexey <alex.esprit@gmail.com>
2020-04-09 13:17:01 +0300

    base64: fix build

--- cmd/tools/vtest-fixed.v
+++ cmd/tools/vtest-fixed.v
@@ -14,8 +14,6 @@ const (
 		'vlib/crypto/aes/aes_test.v',
 		'vlib/crypto/rand/rand_test.v',
 		'vlib/crypto/rc4/rc4_test.v',
-		'vlib/encoding/base64/base64_memory_test.v',
-		'vlib/encoding/base64/base64_test.v',
 		'vlib/encoding/utf8/utf8_util_test.v',
 		'vlib/eventbus/eventbus_test.v',
 		'vlib/flag/flag_test.v',
