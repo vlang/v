eaada8871e23e581a1dfbbf201f51c6cd9e02d6d
yuyi <yuyi98@163.com>
2020-04-09 09:51:48 +0800

    math: fix factorial_test.v error

--- vlib/math/factorial/factorial.v
+++ vlib/math/factorial/factorial.v
@@ -13,13 +13,13 @@ import math
 pub fn factorial(n f64) f64 {
 	// For a large postive argument (n >= FACTORIALS.len) return max_f64
 
-	if n >= FACTORIALS.len {
+	if n >= factorials_table.len {
 			return math.max_f64
 	}
 
 	// Otherwise return n!.
 	if n == f64(i64(n)) && n >= 0.0 {
-		return FACTORIALS[i64(n)]
+		return factorials_table[i64(n)]
 	}
 
 	return math.gamma(n + 1.0)
@@ -37,8 +37,8 @@ pub fn log_factorial(n f64) f64 {
 
 	if n != f64(i64(n)) {
 		return math.log_gamma(n+1)
-	} else if n < LOG_FACTORIALS.len {
-                return LOG_FACTORIALS[i64(n)]
+	} else if n < log_factorials_table.len {
+                return log_factorials_table[i64(n)]
         }
 
 	// Otherwise return asymptotic expansion of ln(n!).
@@ -51,9 +51,9 @@ fn log_factorial_asymptotic_expansion(n int) f64 {
         mut term := []f64
         xx := f64((n + 1) * (n + 1))
         mut xj := f64(n + 1)
-        
+
         log_factorial := log_sqrt_2pi - xj + (xj - 0.5) * math.log(xj)
-        
+
         mut i := 0
 
         for i = 0; i < m; i++ {
