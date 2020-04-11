bd00f5eaa652c43fb8a434b0dacb10645166f541
Kris Cherven <50562493+krischerven@users.noreply.github.com>
2020-04-11 18:00:20 -0400

    old way is more readable

--- vlib/v/tests/operator_overloading_with_string_interpolation_test.v
+++ vlib/v/tests/operator_overloading_with_string_interpolation_test.v
@@ -30,21 +30,27 @@ fn (a Vec) %(b Vec) Vec {
 fn test_operator_overloading_with_string_interpolation() {
 	a := Vec{2, 3}
 	b := Vec{4, 5}
+
 	c := a + b
 	assert a.x + b.x == c.x
 	assert a.y + b.y == c.y
+
 	d := a - b
 	assert a.x - b.x == d.x
 	assert a.y - b.y == d.y
+
 	e := a * b
 	assert a.x * b.x == e.x
 	assert a.y * b.y == e.y
+
 	f := a / b
 	assert a.x / b.x == f.x
 	assert a.y / b.y == f.y
+
 	g := a % b
 	assert a.x % b.x == g.x
 	assert a.y % b.y == g.y
+
 	assert c.str() == '{6, 8}'
 	assert d.str() == '{-2, -2}'
 	assert e.str() == '{8, 15}'
