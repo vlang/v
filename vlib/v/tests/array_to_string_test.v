6896a5a146fed9ddea41af0327e76880f0b81162
yuyi <yuyi98@163.com>
2020-04-13 23:29:42 +0800

    add test_interpolation_array_to_string

--- vlib/v/tests/array_to_string_test.v
+++ vlib/v/tests/array_to_string_test.v
@@ -2,6 +2,9 @@ fn test_array_to_string_conversion() {
 	expected := '["1", "2", "3", "4"]'
 	arr := ['1', '2', '3', '4']
 	assert arr.str() == expected
+}
+
+fn test_interpolation_array_to_string() {
 	a := ['1', '2', '3']
 	assert '$a' == '["1", "2", "3"]'
 	b := ['a', 'b']
