a726d1a43e1edd22156c78c6375134566ce31fc3
danieldaeschle <daniel.daeschle@gmail.com>
2020-04-13 10:02:34 +0200

    cgen: fix match expr multiline error

--- vlib/v/gen/cgen.v
+++ vlib/v/gen/cgen.v
@@ -324,11 +324,11 @@ pub fn (g mut Gen) reset_tmp_count() {
 
 fn (g mut Gen) stmts(stmts []ast.Stmt) {
 	g.indent++
-	for stmt in stmts {
+	for i, stmt in stmts {
 		g.stmt(stmt)
-		// if !g.inside_ternary {
-		// g.writeln('')
-		// }
+		if g.inside_ternary && i < stmts.len - 1 {
+			g.write(', ')
+		}
 	}
 	g.indent--
 }
