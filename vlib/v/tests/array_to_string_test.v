6184adfec67684216c3bc4c1b5b7f0d04f29e14c
yuyi <yuyi98@163.com>
2020-04-13 23:13:48 +0800

    move test from string_test.v to array_to_string_test.v

--- vlib/v/tests/array_to_string_test.v
+++ vlib/v/tests/array_to_string_test.v
@@ -2,4 +2,8 @@ fn test_array_to_string_conversion() {
 	expected := '["1", "2", "3", "4"]'
 	arr := ['1', '2', '3', '4']
 	assert arr.str() == expected
+	a := ['1', '2', '3']
+	assert '$a' == '["1", "2", "3"]'
+	b := ['a', 'b']
+	assert '$b' == '["a", "b"]'
 }
