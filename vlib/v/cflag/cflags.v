fb6a4ce6227cd040fd5baffa67004ef144bf0600
Kris Cherven <50562493+krischerven@users.noreply.github.com>
2020-04-17 15:01:59 -0400

    move misplaced code

--- vlib/v/cflag/cflags.v
+++ vlib/v/cflag/cflags.v
@@ -17,41 +17,6 @@ pub fn (c &CFlag) str() string {
 	return 'CFlag{ name: "$c.name" value: "$c.value" mod: "$c.mod" os: "$c.os" }'
 }
 
-// get flags for current os
-fn (v &Builder) get_os_cflags() []CFlag {
-	mut flags := []CFlag
-	mut ctimedefines := []string
-	if v.pref.compile_defines.len > 0 {
-		ctimedefines << v.pref.compile_defines
-	}
-	for flag in v.table.cflags {
-		if flag.os == '' || (flag.os == 'linux' && v.pref.os == .linux) || (flag.os == 'darwin' &&
-			v.pref.os == .mac) || (flag.os == 'freebsd' && v.pref.os == .freebsd) || (flag.os == 'windows' &&
-			v.pref.os == .windows) || (flag.os == 'mingw' && v.pref.os == .windows && v.pref.ccompiler !=
-			'msvc') || (flag.os == 'solaris' && v.pref.os == .solaris) {
-			flags << flag
-		}
-		if flag.os in ctimedefines {
-			flags << flag
-		}
-	}
-	return flags
-}
-
-fn (v &Builder) get_rest_of_module_cflags(c &CFlag) []CFlag {
-	mut flags := []CFlag
-	cflags := v.get_os_cflags()
-	for flag in cflags {
-		if c.mod == flag.mod {
-			if c.name == flag.name && c.value == flag.value && c.os == flag.os {
-				continue
-			}
-			flags << flag
-		}
-	}
-	return flags
-}
-
 // format flag
 fn (cf &CFlag) format() string {
 	mut value := cf.value
