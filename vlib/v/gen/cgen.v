08fac28c520c1446dfc6aa1897fbbb780e731ff7
Uwe Krüger <45282134+UweKrueger@users.noreply.github.com>
2020-04-22 01:48:53 +0200

    cgen: fix string interpolation for non-standard integer types

--- vlib/v/gen/cgen.v
+++ vlib/v/gen/cgen.v
@@ -2284,8 +2284,18 @@ fn (mut g Gen) string_inter_literal(node ast.StringInterLiteral) {
 			g.write('%g')
 		} else if sym.kind == .struct_ && !sym.has_method('str') {
 			g.write('%.*s')
+		} else if node.expr_types[i] == table.i16_type {
+		        g.write('%"PRId16"')
+		} else if node.expr_types[i] == table.u16_type {
+		        g.write('%"PRIu16"')
+		} else if node.expr_types[i] == table.u32_type {
+		        g.write('%"PRIu32"')
+		} else if node.expr_types[i] == table.i64_type {
+		        g.write('%"PRId64"')
+		} else if node.expr_types[i] == table.u64_type {
+		        g.write('%"PRIu64"')
 		} else {
-			g.write('%d')
+			g.write('%"PRId32"')
 		}
 	}
 	g.write('", ')
