b09dd46277e23fafdcc34168c1cf935d5313712e
yuyi <yuyi98@163.com>
2020-04-28 11:31:16 +0800

    cgen: fix `map` `fixed-array` .str() error

--- vlib/v/gen/fn.v
+++ vlib/v/gen/fn.v
@@ -243,9 +243,17 @@ fn (mut g Gen) method_call(node ast.CallExpr) {
 		g.gen_filter(node)
 		return
 	}
-	if node.name == 'str' && !typ_sym.has_method('str') {
-		g.gen_str_for_type(node.receiver_type)
+	if node.name == 'str' {
+		typ := node.receiver_type
+		mut styp := g.typ(typ)
+		if typ.is_ptr() {
+			styp = styp.replace('*', '')
+		}
+		g.gen_str_for_type_with_styp(typ, styp)
 	}
+	// if node.name == 'str' && !typ_sym.has_method('str') {
+	// 	g.gen_str_for_type(node.receiver_type)
+	// }
 	// TODO performance, detect `array` method differently
 	if typ_sym.kind == .array && node.name in ['repeat', 'sort_with_compare', 'free', 'push_many',
 		'trim',
