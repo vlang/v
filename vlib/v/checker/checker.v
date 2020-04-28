f2060d431ec1c3e07d614afd7b70a2214c26b648
yuyi <yuyi98@163.com>
2020-04-28 15:52:01 +0800

    checker: check division by zero

--- vlib/v/checker/checker.v
+++ vlib/v/checker/checker.v
@@ -355,6 +355,10 @@ pub fn (mut c Checker) infix_expr(infix_expr mut ast.InfixExpr) table.Type {
 			return table.bool_type
 		}
 		.plus, .minus, .mul, .div {
+			if infix_expr.op == .div && infix_expr.right is ast.IntegerLiteral &&
+				 infix_expr.right.str().int() == 0 {
+				c.error('division by zero', infix_expr.right.position())
+			}
 			if left.kind in [.array, .array_fixed, .map, .struct_] && !left.has_method(infix_expr.op.str()) {
 				c.error('mismatched types `$left.name` and `$right.name`', infix_expr.left.position())
 			} else if right.kind in [.array, .array_fixed, .map, .struct_] && !right.has_method(infix_expr.op.str()) {
