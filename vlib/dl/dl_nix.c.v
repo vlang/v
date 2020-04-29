eb5aa852509529f26f3bec7696a83178e67ef551
alexesprit <alex.esprit@gmail.com>
2020-04-29 20:10:26 +0300

    dl: add docs

--- vlib/dl/dl_nix.c.v
+++ vlib/dl/dl_nix.c.v
@@ -11,14 +11,17 @@ pub const (
 	DL_EXT = '.so'
 )
 
+// open loads the dynamic shared object.
 pub fn open(filename string, flags int) voidptr {
 	return C.dlopen(filename.str, flags)
 }
 
+// close frees a given shared object.
 pub fn close(handle voidptr) bool {
 	return C.dlclose(handle) == 0
 }
 
+// sym returns an address of a symbol in a given shared object.
 pub fn sym(handle voidptr, symbol string) voidptr {
 	return C.dlsym(handle, symbol.str)
 }
--- vlib/dl/dl_windows.c.v
+++ vlib/dl/dl_windows.c.v
@@ -9,15 +9,18 @@ fn C.LoadLibrary(libfilename C.LPCWSTR) voidptr
 fn C.GetProcAddress(handle voidptr, procname C.LPCSTR) voidptr
 fn C.FreeLibrary(handle voidptr) bool
 
+// open loads a given module into the address space of the calling process.
 pub fn open(filename string, flags int) voidptr {
 	res := C.LoadLibrary(filename.to_wide())
 	return res
 }
 
+// close frees the loaded a given module.
 pub fn close(handle voidptr) bool {
 	return C.FreeLibrary(handle)
 }
 
+// sym returns an address of an exported function or variable from a given module.
 pub fn sym(handle voidptr, symbol string) voidptr {
 	return C.GetProcAddress(handle, symbol.str)
 }
