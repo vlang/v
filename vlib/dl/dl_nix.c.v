54d1a29267425194ea80c376f109c29912702abf
Alexey <alex.esprit@gmail.com>
2020-04-29 22:01:19 +0300

    dl: improve module

--- vlib/dl/dl_nix.c.v
+++ vlib/dl/dl_nix.c.v
@@ -2,18 +2,28 @@ module dl
 
 #include <dlfcn.h>
 
-fn C.dlopen(filename charptr, flags int) voidptr
-fn C.dlsym(handle voidptr, symbol charptr) voidptr
-
 pub const (
 	RTLD_NOW = C.RTLD_NOW
-	DL_EXT = '.so'
+	DL_EXT   = '.so'
 )
 
+fn C.dlopen(filename charptr, flags int) voidptr
+
+fn C.dlsym(handle voidptr, symbol charptr) voidptr
+
+fn C.dlclose(handle voidptr) int
+
+// open loads the dynamic shared object.
 pub fn open(filename string, flags int) voidptr {
 	return C.dlopen(filename.str, flags)
 }
 
+// close frees a given shared object.
+pub fn close(handle voidptr) bool {
+	return C.dlclose(handle) == 0
+}
+
+// sym returns an address of a symbol in a given shared object.
 pub fn sym(handle voidptr, symbol string) voidptr {
 	return C.dlsym(handle, symbol.str)
 }
