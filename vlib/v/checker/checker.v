dc3e6a0997dc6cf5fbabba54b51ddf61a3063a91
yuyi <yuyi98@163.com>
2020-04-29 15:50:35 +0800

    int change to comparison strings

--- vlib/v/checker/checker.v
+++ vlib/v/checker/checker.v
@@ -356,7 +356,7 @@ pub fn (mut c Checker) infix_expr(infix_expr mut ast.InfixExpr) table.Type {
 		}
 		.plus, .minus, .mul, .div {
 			if infix_expr.op == .div &&
-				(infix_expr.right is ast.IntegerLiteral && infix_expr.right.str().int() == 0 ||
+				(infix_expr.right is ast.IntegerLiteral && infix_expr.right.str() == '0' ||
 				infix_expr.right is ast.FloatLiteral && infix_expr.right.str().f64() == 0.0) {
 				c.error('division by zero', infix_expr.right.position())
 			}
