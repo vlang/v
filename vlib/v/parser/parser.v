b21be7f670b384282ce1d121f58aae9b0d2252d6
yuyi <yuyi98@163.com>
2020-05-02 00:18:58 +0800

    parser: multiple names error

--- vlib/v/parser/parser.v
+++ vlib/v/parser/parser.v
@@ -472,6 +472,8 @@ pub fn (mut p Parser) stmt() ast.Stmt {
 				return ast.GotoLabel{
 					name: name
 				}
+			} else if p.tok.kind == .name && p.peek_tok.kind == .name {
+				p.error_with_pos('unexpected name `$p.peek_tok.lit`', p.peek_tok.position())
 			}
 			epos := p.tok.position()
 			expr := p.expr(0)
