d07d314fc43d0b33eb4cabeb502672a43a8be8aa
Elias Aebi <eyelash@users.noreply.github.com>
2020-05-10 15:35:48 +0200

    scanner: add a colon to error messages

--- vlib/v/scanner/scanner.v
+++ vlib/v/scanner/scanner.v
@@ -1023,7 +1023,7 @@ pub fn (s &Scanner) error(msg string) {
 		line_nr: s.line_nr
 		pos: s.pos
 	}
-	eprintln(util.formatted_error('error', msg, s.file_path, pos))
+	eprintln(util.formatted_error('error:', msg, s.file_path, pos))
 	exit(1)
 }
 
