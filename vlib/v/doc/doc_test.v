b9f4e2f424c06341130edd2b5df88291183fed65
Ned Palacios <npdoesmc@gmail.com>
2020-05-15 20:08:55 +0800

    v.doc: disable tests for now

--- vlib/v/doc/doc_test.v
+++ vlib/v/doc/doc_test.v
@@ -2,9 +2,9 @@ import v.table
 import v.doc
 import v.pref
 
-fn test_vdoc() {
-	mut prefs := &pref.Preferences{}
-	prefs.fill_with_defaults()
-	table := table.new_table()
-	println(doc.doc('net', table, prefs))
-}
+// fn test_vdoc() {
+// 	mut prefs := &pref.Preferences{}
+// 	prefs.fill_with_defaults()
+// 	table := table.new_table()
+// 	println(doc.doc('net', table, prefs))
+// }
