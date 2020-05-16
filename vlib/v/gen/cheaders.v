789b75df92f879adfe3b0388281f8e2bf3030f14
Delyan Angelov <delian66@gmail.com>
2020-05-16 15:50:59 +0300

    cheaders: fix _MOV macro in the C case

--- vlib/v/gen/cheaders.v
+++ vlib/v/gen/cheaders.v
@@ -66,9 +66,12 @@ const (
 typedef int (*qsort_callback_func)(const void*, const void*);
 #include <stdio.h>  // TODO remove all these includes, define all function signatures and types manually
 #include <stdlib.h>
+
 #ifdef __cplusplus
-#include <utility>
-#define _MOV std::move
+#   include <utility>
+#   define _MOV std::move
+#else
+#   define _MOV
 #endif
 
 #ifndef _WIN32
