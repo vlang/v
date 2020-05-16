9b6ee8e77d1ecef09f8c10ca74b2d23171ad01c2
yuyi <yuyi98@163.com>
2020-05-16 21:21:37 +0800

    cgen: array_init_with_default

--- vlib/v/tests/array_init_test.v
+++ vlib/v/tests/array_init_test.v
@@ -14,3 +14,11 @@ fn test_array_init() {
 	d << 'bbb'
 	'$d, $d.len, $d.cap' == "['aaa', 'bbb'], 2, 3"
 }
+
+fn test_array_init_with_default() {
+	a := []int{len: 4, init: 2}
+	assert '$a' == '[2, 2, 2, 2]'
+
+	b := []string{len: 3, init: 'abc'}
+	assert '$b' == "['abc', 'abc', 'abc']"
+}
