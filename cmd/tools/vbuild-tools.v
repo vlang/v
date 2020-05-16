dfad0ed3f67fc31e8787ee667ad249779772493f
Delyan Angelov <delian66@gmail.com>
2020-05-17 00:14:06 +0300

    CI: add `v build-tools` task to the tcc job

--- cmd/tools/vbuild-tools.v
+++ cmd/tools/vbuild-tools.v
@@ -6,7 +6,11 @@ import testing
 fn main() {
 	args := os.args
 	args_string := args[1..].join(' ')
-	if testing.v_build_failing(args_string.all_before('build-tools'), 'cmd/tools') {
+	// TODO: fix cmd/tools/gen_vc.v / vweb too
+	skips := [
+		'cmd/tools/gen_vc.v'
+	]
+	if testing.v_build_failing_skipped(args_string.all_before('build-tools'), 'cmd/tools', skips) {
 		exit(1)
 	}
 }
