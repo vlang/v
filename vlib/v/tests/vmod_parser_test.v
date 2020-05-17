c25ab991339a6d754a19309368071149947e95ac
Ned Palacios <npdoesmc@gmail.com>
2020-05-17 13:12:31 +0800

    tests: add vmod_parser_test

---
+++ vlib/v/tests/vmod_parser_test.v
@@ -0,0 +1,39 @@
+import vmod
+
+fn test_from_file() {
+	data := vmod.from_file('./v.mod') or {
+		panic(err)
+	}
+
+	assert data.name == 'V'
+	assert data.description == 'The V programming language.'
+	assert data.version == '0.1.27'
+	assert data.dependencies.len == 0
+}
+
+fn test_decode() {
+	content := '
+	  Module {
+		name: \'foobar\',
+		description: \'Just a sample module\'
+		version: \'0.2.0\',
+		repo_url: \'https://gitlab.com\',
+		author: \'Fooz Bar\',
+		license: \'GPL-2.0\',
+		dependencies: [\'hello\']
+	  }
+	'
+
+	data := vmod.decode(content) or {
+		println(err)
+		exit(1)
+	}
+
+	assert data.name == 'foobar'
+	assert data.version == '0.2.0'
+	assert data.description == 'Just a sample module'
+	assert data.repo_url == 'https://gitlab.com'
+	assert data.author == 'Fooz Bar'
+	assert data.license == 'GPL-2.0'
+	assert data.dependencies[0] == 'hello'
+}
