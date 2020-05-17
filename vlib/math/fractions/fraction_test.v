b138cadbcb21f99f0f7c48c891fa41d3bf6b309c
Hungry Blue Dev <46458389+hungrybluedev@users.noreply.github.com>
2020-05-17 14:30:29 +0530

    math.fractions: add approximation.v and tests

--- vlib/math/fractions/fraction_test.v
+++ vlib/math/fractions/fraction_test.v
@@ -1,3 +1,6 @@
+// Copyright (c) 2019-2020 Alexander Medvednikov. All rights reserved.
+// Use of this source code is governed by an MIT license
+// that can be found in the LICENSE file.
 import math.fractions
 
 // (Old) results are verified using https://www.calculatorsoup.com/calculators/math/fractions.php
