1367a85e2fd015a30b0db2273d2d9aff0ab10533
Enzo Baldisserri <enzo.baldisserri@outlook.fr>
2020-05-23 14:21:59 +0200

    tests: add more tests

--- vlib/v/tests/if_expression_test.v
+++ vlib/v/tests/if_expression_test.v
@@ -44,6 +44,71 @@ fn test_if_expression_with_function_assign() {
 	assert a == 0
 }
 
+fn test_infix_expr() {
+	a := 1 + if true {
+		noop()
+		2
+	} else {
+		3
+	}
+	assert a == 3
+	assert 3.3 + if false {
+		0.0
+	} else {
+		noop()
+		1.1
+	} == 4.4
+	b := 'Hello ' + if false {
+		noop()
+		'Everybody'
+	} else {
+		'world'
+	}
+	assert b == 'Hello world'
+	assert 'N' + if true {
+		noop()
+		'ah'
+	} else {
+		'!'
+	} == 'Nah'
+	c := if true {
+		noop()
+		1
+	} else {
+		2
+	} + 3
+	assert c == 4
+	assert if true {
+		noop()
+		1
+	} else {
+		2
+	} + 3 == 4
+	d := if true {
+		noop()
+		1
+	} else {
+		2
+	} - if false {
+		noop()
+		2
+	} else {
+		0
+	}
+	assert d == 1
+	assert if true {
+		noop()
+		1
+	} else {
+		2
+	} - if false {
+		noop()
+		2
+	} else {
+		0
+	} == 1
+}
+
 fn get_bool_str(b bool) string {
 	return b.str()
 }
@@ -57,6 +122,13 @@ fn test_if_expression_mutate_var() {
 		true
 	}
 	assert r == false
+	assert 1 + if true {
+		println('two')
+		2
+	} else {
+		println('three')
+		3
+	} == 3
 	// test in function call
 	assert get_bool_str(b && if true {
 		b = true
@@ -78,6 +150,97 @@ fn test_if_expression_mutate_var() {
 		true
 	} else {
 		false
+	}
+	mut d := false
+	_ := false && if true {
+		d = true
+		false
+	} else {
+		false
+	}
+	assert d == false
+}
+
+fn test_loop_in_if_expr() {
+	a := if true {
+		mut i := 0
+		for _ in 1 .. 3 {
+			i++
+		}
+		i
+	} else {
+		3
+	}
+	assert a == 2
+}
+
+fn test_anon_fn_in_if_expr() {
+	a := if true {
+		f := fn (i int) int {
+			return i + 3
+		}
+		g := f(6)
+		g + 1
+	} else {
+		0
+	}
+	assert a == 10
+	b := if true {
+		f := fn (i int) int {
+			return i + 3
+		}
+		f(7)
+	} else {
+		0
+	}
+	assert b == 10
+}
+
+fn ret_none() ?int {
+	return none
+}
+
+fn test_opt_call_in_if_expr() {
+	a := if true {
+		b := ret_none() or {
+			2
+		}
+		b
+	} else {
+		0
+	}
+	assert a == 2
+}
+
+fn test_opt_call_as_last_stmt_in_if_expr() {
+	a := if false {
+		0
+	} else {
+		ret_none() or {
+			1
+		}
+	}
+	assert a == 1
+}
+
+fn err_call(err bool) ?int {
+	if err {
+		return error('Nah')
+	}
+	return 0
+}
+
+fn propagate_opt(err bool) ?int {
+	a := if true { err_call(err)? } else { 1 }
+	return a
+}
+
+fn test_opt_propagate_in_if_expr() {
+	propagate_opt(false) or {
+		assert false
+	}
+	if _ := propagate_opt(true) {
+		assert false
 	}
 }
 
@@ -109,6 +272,9 @@ fn test_complex_nested_if_expressions() {
 		mut d := false
 		if h == 2 {
 			d = g + 4 == 5
+		}
+		for _ in 1 .. 3 {
+			d = !d
 		}
 		if d {
 			if true {
