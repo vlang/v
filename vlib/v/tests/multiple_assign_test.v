9cbd9db4e7291da985fb1d68022db0e0d29abe0a
yuyi <yuyi98@163.com>
2020-05-28 07:22:09 +0800

    cgen: fix swap assign of prefix and postfix expr

--- vlib/v/tests/multiple_assign_test.v
+++ vlib/v/tests/multiple_assign_test.v
@@ -34,3 +34,33 @@ fn test_multiple_assign_infix_expr() {
 	assert b == 22
 	assert c == 22
 }
+
+fn test_multiple_assign_prefix_expr() {
+	mut a := 11
+	mut b := 22
+	mut c := 33
+	a, b, c = -b, -c, -a
+	assert a == -22
+	assert b == -33
+	assert c == -11
+}
+
+fn test_multiple_assign_postfix_expr() {
+	mut a := 11
+	mut b := 22
+	mut c := 33
+	a, b, c = b++, c++, a--
+	assert a == 22
+	assert b == 33
+	assert c == 11
+}
+
+fn test_multiple_assign_complex_expr() {
+	mut a := 11
+	mut b := 22
+	mut c := 33
+	a, b, c = -b + 1, -c * 2, a++
+	assert a == -21
+	assert b == -66
+	assert c == 11
+}
