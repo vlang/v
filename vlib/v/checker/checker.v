42e3cf5473c4c277c8fad45c0701e4e5c26ba4ff
pancake <pancake@nopcode.org>
2020-05-30 19:12:12 +0200

    meaningful error when trying to return on interfaces

--- vlib/v/checker/checker.v
+++ vlib/v/checker/checker.v
@@ -1232,6 +1232,9 @@ pub fn (mut c Checker) return_stmt(mut return_stmt ast.Return) {
 			if is_generic {
 				exp_typ_sym = c.table.get_type_symbol(c.cur_generic_type)
 			}
+			if exp_typ_sym.kind == .interface_ {
+				c.error('returning structs as interfaces is not yet supported', pos)
+			}
 			c.error('cannot use `$got_typ_sym.name` as type `$exp_typ_sym.name` in return argument',
 				pos)
 		}
