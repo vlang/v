aeb7e8ef40d6e125e326530d41f8faa2529136a3
Ned Palacios <npdoesmc@gmail.com>
2020-05-31 21:20:30 +0800

    vdoc: save space. save the environment

--- cmd/tools/vdoc.v
+++ cmd/tools/vdoc.v
@@ -39,11 +39,9 @@ fn open_url(url string) {
 	$if windows {
 		os.system('start $url')
 	}
-
 	$if macos {
 		os.system('open $url')
 	}
-
 	$if linux {
 		os.system('xdg-open $url')
 	}
@@ -57,30 +55,24 @@ fn (mut cfg DocConfig) serve_html() {
 	}
 	println('Serving docs on: http://localhost:8046')
 	open_url('http://localhost:8046')
-
 	for {
 		con := server.accept() or {
 			server.close() or { }
 			panic(err)
 		}
-
 		s := con.read_line()
 		first_line := s.all_before('\n')
 		mut filename := def_name
-
 		if first_line.len != 0 {
 			data := first_line.split(' ')
 			url := urllib.parse(data[1]) or { return }
 			filename = if url.path == '/' { def_name } else { url.path.trim_left('/') }
 		}
-
 		html := docs[filename]
-
 		con.write('HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n$html') or {
 			con.close() or { return }
 			return
 		}
-
 		con.close() or { return }
 	}
 }
@@ -171,7 +163,6 @@ fn (cfg DocConfig) gen_html(idx int) string {
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet">')
 	// write css
 	hw.write(r'<style>/*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */html{line-height:1.15;-webkit-text-size-adjust:100%}body{margin:0}main{display:block}h1{font-size:2em;margin:.67em 0}hr{box-sizing:content-box;height:0;overflow:visible}pre{font-family:monospace,monospace;font-size:1em}a{background-color:transparent}abbr[title]{border-bottom:none;text-decoration:underline;text-decoration:underline dotted}b,strong{font-weight:bolder}code,kbd,samp{font-family:monospace,monospace;font-size:1em}small{font-size:80%}sub,sup{font-size:75%;line-height:0;position:relative;vertical-align:baseline}sub{bottom:-.25em}sup{top:-.5em}img{border-style:none}button,input,optgroup,select,textarea{font-family:inherit;font-size:100%;line-height:1.15;margin:0}button,input{overflow:visible}button,select{text-transform:none}[type=button],[type=reset],[type=submit],button{-webkit-appearance:button}[type=button]::-moz-focus-inner,[type=reset]::-moz-focus-inner,[type=submit]::-moz-focus-inner,button::-moz-focus-inner{border-style:none;padding:0}[type=button]:-moz-focusring,[type=reset]:-moz-focusring,[type=submit]:-moz-focusring,button:-moz-focusring{outline:1px dotted ButtonText}fieldset{padding:.35em .75em .625em}legend{box-sizing:border-box;color:inherit;display:table;max-width:100%;padding:0;white-space:normal}progress{vertical-align:baseline}textarea{overflow:auto}[type=checkbox],[type=radio]{box-sizing:border-box;padding:0}[type=number]::-webkit-inner-spin-button,[type=number]::-webkit-outer-spin-button{height:auto}[type=search]{-webkit-appearance:textfield;outline-offset:-2px}[type=search]::-webkit-search-decoration{-webkit-appearance:none}::-webkit-file-upload-button{-webkit-appearance:button;font:inherit}details{display:block}summary{display:list-item}[hidden],template{display:none}blockquote,dd,dl,figure,h1,h2,h3,h4,h5,h6,hr,p,pre{margin:0}button{background-color:transparent;background-image:none;padding:0}button:focus{outline:1px dotted;outline:5px auto -webkit-focus-ring-color}fieldset,ol,ul{margin:0;padding:0}ol,ul{list-style:none}html{font-family:Inter,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;line-height:1.5}*,:after,:before{box-sizing:border-box;border:0 solid #e2e8f0}hr{border-top-width:1px}img{border-style:solid}textarea{resize:vertical}input::placeholder,textarea::placeholder{color:#a0aec0}[role=button],button{cursor:pointer}table{border-collapse:collapse}h1,h2,h3,h4,h5,h6{font-size:inherit;font-weight:inherit}a{color:inherit;text-decoration:inherit}button,input,optgroup,select,textarea{padding:0;line-height:inherit;color:inherit}code,kbd,pre,samp{font-family:SFMono-Regular,Menlo,Monaco,Consolas,Liberation Mono,Courier New,monospace}audio,canvas,embed,iframe,img,object,svg,video{display:block;vertical-align:middle}img,video{max-width:100%;height:auto}.container{width:100%}.flex{display:flex}.hidden{display:none}.font-bold{font-weight:700}.resize{resize:both}.text-gray-600{--text-opacity:1;color:#718096;color:rgba(113,128,150,var(--text-opacity))}.doc-container{display:flex;flex-direction:column-reverse;min-height:100vh;margin-top:10%}.doc-nav{position:fixed;left:0;top:0;right:0;display:flex;flex-direction:column;--bg-opacity:1;background-color:#2b6cb0;background-color:rgba(43,108,176,var(--bg-opacity));--text-opacity:1;color:#fff;color:rgba(255,255,255,var(--text-opacity));padding:1rem}.doc-nav .heading{display:flex;justify-content:space-between;align-items:center}.doc-nav .heading h2{font-size:1.875rem;font-weight:500}.doc-nav .heading .version{font-size:1.5rem;font-weight:500;margin-right:auto;--text-opacity:1;color:#bee3f8;color:rgba(190,227,248,var(--text-opacity))}.doc-nav nav{flex-direction:column;--text-opacity:1;color:#fff;color:rgba(255,255,255,var(--text-opacity));padding-left:2rem;padding-right:2rem;padding-bottom:3rem;margin-top:.5rem;margin-left:-2rem;margin-right:-2rem;height:100%;overflow-y:auto;max-height:100vh}.doc-nav nav .section{text-decoration:none;display:flex;flex-direction:row;align-items:center;justify-content:space-between;font-size:1.25rem;font-weight:500;margin-bottom:1rem}.doc-nav nav .section svg{width:2rem}.doc-nav nav a{font-size:1.125rem;word-break:break-all}.doc-nav nav a:hover{text-decoration:underline;--text-opacity:1;color:#bee3f8;color:rgba(190,227,248,var(--text-opacity))}.doc-nav nav ul{display:flex;flex-direction:column}.doc-nav nav ul li:not(:last-child){margin-bottom:.5rem}.doc-nav nav ul li ul{padding-left:.75rem;border-left-width:2px;margin-top:1rem;--border-opacity:1;border-color:#2c5282;border-color:rgba(44,82,130,var(--border-opacity))}.doc-content{--bg-opacity:1;background-color:#fff;background-color:rgba(255,255,255,var(--bg-opacity));width:100%;padding:2rem}.doc-content .doc-node{padding-bottom:2rem}.doc-content p,.doc-content ul li{font-size:1.125rem;margin-bottom:1rem}.doc-content ul li p{margin-bottom:0}.doc-content p,.doc-content p img{display:inline-block}.doc-content code{--bg-opacity:1;background-color:#e2e8f0;background-color:rgba(226,232,240,var(--bg-opacity));padding:.25rem;border-radius:.25rem}.doc-content a{--text-opacity:1;color:#2b6cb0;color:rgba(43,108,176,var(--text-opacity));text-decoration:underline}.doc-content code.code-snippet,.doc-content pre code{margin-bottom:2rem;margin-top:.5rem;white-space:pre;overflow-x:auto;display:inline-block;padding:1rem;width:100%;border-radius:.5rem;-webkit-overflow-scrolling:touch}.doc-content ul{list-style-type:disc;list-style-position:inside}.doc-content h1{font-size:2.25rem}.doc-content h2{font-size:1.875rem}.doc-content h3{font-size:1.5rem}.doc-content h4{font-size:1.25rem}.doc-content h5{font-size:1.125rem}.doc-content h6{font-size:1rem}.doc-content h1,.doc-content h2,.doc-content h3,.doc-content h4,.doc-content h5,.doc-content h6{font-weight:700;border-bottom-width:2px;padding-bottom:.5rem;margin-bottom:1rem}.doc-content h1 a,.doc-content h2 a,.doc-content h3 a,.doc-content h4 a,.doc-content h5 a,.doc-content h6 a{display:none;text-decoration:none;--text-opacity:1;color:#a0aec0;color:rgba(160,174,192,var(--text-opacity))}.doc-content h1:hover a,.doc-content h2:hover a,.doc-content h3:hover a,.doc-content h4:hover a,.doc-content h5:hover a,.doc-content h6:hover a{display:inline-block}.doc-toc{padding:2rem;font-size:1.25rem;width:100%;max-height:100vh;height:100%}.doc-toc .section{--text-opacity:1;color:#718096;color:rgba(113,128,150,var(--text-opacity));text-decoration:none;display:flex;flex-direction:row-reverse;align-items:center;justify-content:flex-end;font-size:1.25rem;font-weight:500;margin-bottom:1rem}.doc-toc .section svg{margin-right:.5rem;width:1.3rem}.doc-toc a{--text-opacity:1;color:#2b6cb0;color:rgba(43,108,176,var(--text-opacity));text-decoration:none}.doc-toc a:hover{text-decoration:underline}.doc-toc ul{display:flex;flex-direction:column;overflow-y:auto;height:100%;padding-bottom:2rem}.doc-toc ul li:not(:last-child){margin-bottom:.5rem}.doc-toc ul li ul{padding-left:.75rem;margin-top:1rem;border-left-width:2px;padding-bottom:0;--border-opacity:1;border-color:#cbd5e0;border-color:rgba(203,213,224,var(--border-opacity));overflow-y:visible}@media (min-width:640px){.container{max-width:640px}}@media (min-width:768px){.container{max-width:768px}}@media (min-width:1024px){.container{max-width:1024px}.doc-container{flex-direction:row;margin-top:0}.doc-nav{bottom:0;flex-direction:column;width:16.666667%;height:100vh;padding-left:2rem;padding-right:2rem}.doc-nav .heading{align-items:flex-end}.doc-nav .heading h2{font-size:2.25rem;margin-bottom:-.25rem}.doc-nav .heading .version{margin-right:0}.doc-nav nav{margin-top:2rem;margin-bottom:-3rem}.doc-content{width:50%;margin-left:16.666667%}.doc-content h1{font-size:3rem}.doc-content h2{font-size:2.25rem}.doc-content h3{font-size:1.875rem}.doc-content h4{font-size:1.5rem}.doc-content h5{font-size:1.25rem}.doc-content h6,.doc-toc{font-size:1.125rem}.doc-toc{position:fixed;width:16.666667%;margin-top:0;left:66.66667%}.lg\:flex{display:flex}.lg\:hidden{display:none}}@media (min-width:1280px){.container{max-width:1280px}}</style></head><body>')
-
 	version := if cfg.manifest.version.len != 0 { '<span class="version">${cfg.manifest.version}</span>' } else { '' }
 	repo_link := if cfg.manifest.repo_url.len != 0 {
 		'<a href="${cfg.manifest.repo_url}" class="section">
@@ -195,7 +186,6 @@ fn (cfg DocConfig) gen_html(idx int) string {
 				Contents
 				<svg fill="currentColor" width="13%" viewBox="0 0 20 20"><path d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd" fill-rule="evenodd"></path></svg>
 			</p><ul>')
-
 	if cfg.is_multi && cfg.docs.len > 1 {
 		for doc in cfg.docs {
 			class_css := if doc.head.name == dcs.head.name { 'class="font-bold" ' } else { '' }
@@ -205,7 +195,6 @@ fn (cfg DocConfig) gen_html(idx int) string {
 	} else {
 		hw.writeln(toc.str())
 	}
-
 	hw.write('</ul>\n</nav>\n</header>')
 	hw.write('<div class="doc-container">\n<div class="doc-content">\n')
 	hw.write(doc_node_html(dcs.head, '', true))
@@ -222,11 +211,9 @@ fn (cfg DocConfig) gen_html(idx int) string {
 		}
 	}
 	hw.write('\n</div>\n')
-
 	if cfg.is_multi && cfg.docs.len > 1 && dcs.head.name != 'README' {
 		hw.write('<div class="doc-toc">\n<p class="section">Contents</p>\n<ul>\n${toc.str()}</ul>\n</div>')
 	}
-
 	hw.write('</div>
 		<script>
 			var toggle = document.getElementById("toggleMenu");
@@ -245,10 +232,8 @@ fn (cfg DocConfig) gen_html(idx int) string {
 fn (cfg DocConfig) gen_plaintext(idx int) string {
 	dcs := cfg.docs[idx]
 	mut pw := strings.new_builder(200)
-
 	head_lines := '='.repeat(dcs.head.content.len)
 	pw.writeln('${dcs.head.content}\n$head_lines\n')
-
 	for cn in dcs.contents {
 		pw.writeln(cn.content)
 		if cn.comment.len > 0 {
@@ -258,7 +243,6 @@ fn (cfg DocConfig) gen_plaintext(idx int) string {
 			pw.writeln('Location: ${cn.file_path}:${cn.pos.line}:${cn.pos.col}\n\n')
 		}
 	}
-
 	pw.writeln('Generated on $dcs.time_generated')
 	return pw.str()
 }
@@ -267,12 +251,10 @@ fn (cfg DocConfig) gen_markdown(idx int, with_toc bool) string {
 	dcs := cfg.docs[idx]
 	mut hw := strings.new_builder(200)
 	mut cw := strings.new_builder(200)
-
 	hw.writeln('# ${dcs.head.content}\n${dcs.head.comment}\n')
 	if with_toc {
 		hw.writeln('## Contents')
 	}
-
 	for cn in dcs.contents {
 		name := cn.name.all_after(dcs.head.name+'.')
 
@@ -283,7 +265,6 @@ fn (cfg DocConfig) gen_markdown(idx int, with_toc bool) string {
 		cw.writeln('```v\n${cn.content}\n```${cn.comment}\n')
 		cw.writeln('[\[Return to contents\]](#Contents)\n')
 	}
-
 	cw.writeln('#### Generated by vdoc. Last generated: ${dcs.time_generated.str()}')
 	return hw.str() + '\n' + cw.str()
 }
@@ -427,16 +408,13 @@ fn lookup_module(mod string) ?string {
 fn get_modules_list(path string) []string {
 	files := os.walk_ext(path, 'v')
 	mut dirs := []string{}
-
 	for file in files {
 		if 'test' in file || 'bare' in file || 'uiold' in file || 'vweb' in file { continue }
 		dirname := os.base_dir(file)
 		if dirname in dirs { continue }
 		dirs << dirname
 	}
-
 	dirs.sort()
-
 	return dirs
 }
 
