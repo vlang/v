7704fc4449511a289ff979c4f3f04dbe845a4e05
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2020-06-01 11:53:32 +0800

    mysql: Add some docs

--- vlib/mysql/mysql.v
+++ vlib/mysql/mysql.v
@@ -4,6 +4,7 @@ module mysql
 #flag linux -I/usr/include/mysql
 #include <mysql.h>
 
+// TODO: Documentation
 pub struct Connection {
 mut:
 	conn     &C.MYSQL = C.mysql_init(0)
@@ -16,6 +17,7 @@ pub mut:
 	flag     int
 }
 
+// connect Connects to a MySQL server
 pub fn (mut conn Connection) connect() ?bool {
 	init := conn.conn
 	conn.conn = C.mysql_real_connect(
@@ -34,6 +36,7 @@ pub fn (mut conn Connection) connect() ?bool {
 	return true
 }
 
+// query Performs a query on the database
 pub fn (conn Connection) query(q string) ?Result {
 	if C.mysql_query(conn.conn, q.str) != 0 {
 		return error_with_code(get_error_msg(conn.conn), get_errno(conn.conn))
@@ -42,6 +45,7 @@ pub fn (conn Connection) query(q string) ?Result {
 	return Result{res}
 }
 
+// select_db Selects the default database for database queries
 pub fn (conn Connection) select_db(dbname string) ?bool {
 	if C.mysql_select_db(conn.conn, dbname.str) != 0 {
 		return error_with_code(get_error_msg(conn.conn), get_errno(conn.conn))
@@ -49,6 +53,9 @@ pub fn (conn Connection) select_db(dbname string) ?bool {
 	return true
 }
 
+// change_user Changes the user of the specified database connection.
+// If desired, the empty string value can be passed to the `dbname` parameter
+// resulting in only changing the user and not selecting a database
 pub fn (conn Connection) change_user(username, password, dbname string) ?bool {
 	mut ret := true
 	if dbname != '' {
@@ -62,15 +69,18 @@ pub fn (conn Connection) change_user(username, password, dbname string) ?bool {
 	return ret
 }
 
+// affected_rows Returns the number of rows changed/deleted/inserted
+// by the last UPDATE, DELETE, or INSERT query
 pub fn (conn Connection) affected_rows() u64 {
 	return C.mysql_affected_rows(conn.conn)
 }
 
+// autocommit Turns on or off auto-committing database modifications
 pub fn (conn Connection) autocommit(mode bool) {
 	C.mysql_autocommit(conn.conn, mode)
 }
 
-
+// escape_string Creates a legal SQL string for use in an SQL statement
 pub fn (conn Connection) escape_string(s string) string {
     len := C.strlen(s.str)
     to := malloc(2 * len + 1)
@@ -80,10 +90,15 @@ pub fn (conn Connection) escape_string(s string) string {
     return string(to)
 }
 
+// set_option Can be used to set extra connect options and affect behavior for a connection.
+// This function may be called multiple times to set several options.
+// To retrieve option values, use `get_option()`
 pub fn (conn Connection) set_option(option_type int, val voidptr) {
 	C.mysql_options(conn.conn, option_type, val)
 }
 
+// get_option Returns the current value of an option settable `set_option`.
+// The value should be treated as read only
 pub fn (conn Connection) get_option(option_type int) ?voidptr {
 	ret := voidptr(0)
 	if C.mysql_get_option(conn.conn, option_type, &ret) != 0 {
@@ -92,6 +107,8 @@ pub fn (conn Connection) get_option(option_type int) ?voidptr {
 	return ret
 }
 
+// refresh Flushes tables or caches, or resets replication server information.
+// The connected user must have the `RELOAD` privilege
 pub fn (conn Connection) refresh(options u32) ?bool {
 	if C.mysql_refresh(conn.conn, options) != 0 {
 		return error_with_code(get_error_msg(conn.conn), get_errno(conn.conn))
@@ -99,6 +116,7 @@ pub fn (conn Connection) refresh(options u32) ?bool {
 	return true
 }
 
+// reset_connection Resets the connection to clear the session state
 pub fn (conn Connection) reset_connection() ?bool {
 	if C.mysql_reset_connection(conn.conn) != 0 {
 		return error_with_code(get_error_msg(conn.conn), get_errno(conn.conn))
@@ -106,6 +124,7 @@ pub fn (conn Connection) reset_connection() ?bool {
 	return true
 }
 
+// ping Pings a server connection, or tries to reconnect if the connection has gone down
 pub fn (conn Connection) ping() ?bool {
 	if C.mysql_ping(conn.conn) != 0 {
 		return error_with_code(get_error_msg(conn.conn), get_errno(conn.conn))
@@ -113,37 +132,48 @@ pub fn (conn Connection) ping() ?bool {
 	return true
 }
 
+// close Closes a previously opened database connection
 pub fn (conn Connection) close() {
 	C.mysql_close(conn.conn)
 }
 
-/* MYSQL INFO & VERSION */
+/* -------------------------- MYSQL INFO & VERSION -------------------------- */
 
+// info Returns information about the most recently executed query
 pub fn (conn Connection) info() string {
 	return string(C.mysql_info(conn.conn))
 }
 
+// get_host_info Returns a string describing the connection
 pub fn (conn Connection) get_host_info() string {
 	return string(C.mysql_get_host_info(conn.conn))
 }
 
+// get_server_info Returns the server version number as a string
 pub fn (conn Connection) get_server_info() string {
 	return string(C.mysql_get_server_info(conn.conn))
 }
 
+// get_server_version Returns the server version number as an integer
 pub fn (conn Connection) get_server_version() u64 {
 	return C.mysql_get_server_version(conn.conn)
 }
 
-pub fn get_client_version() u64 {
-	return C.mysql_get_client_version()
-}
+/* --------------------------------- CLIENT --------------------------------- */
 
+// get_client_info Returns client version information as a string
 pub fn get_client_info() string {
 	return string(C.mysql_get_client_info())
 }
 
-/* MYSQL DEBUG */
+// get_client_version Returns client version information as an integer
+pub fn get_client_version() u64 {
+	return C.mysql_get_client_version()
+}
+
+/* ------------------------------- MYSQL DEBUG ------------------------------ */
+
+// dump_debug_info Causes the server to write debug information to the log
 pub fn (conn Connection) dump_debug_info() ?bool {
 	if C.mysql_dump_debug_info(conn.conn) != 0 {
 		return error_with_code(get_error_msg(conn.conn), get_errno(conn.conn))
@@ -151,6 +181,8 @@ pub fn (conn Connection) dump_debug_info() ?bool {
 	return true
 }
 
+// debug Does a `DBUG_PUSH` with the given string.
+// See https://dev.mysql.com/doc/refman/5.7/en/mysql-debug.html
 pub fn debug(debug string) {
 	C.mysql_debug(debug.str)
 }
