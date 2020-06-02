eacd6bad4f4a6cfce82118a50cc0aea2fdec6ae6
louis <dev@louis-schmieder.de>
2020-06-02 23:12:39 +0200

    Added firsts documentations in gl module & clean up uncommented and solved stuff in gl module

--- vlib/gl/gl.v
+++ vlib/gl/gl.v
@@ -63,10 +63,10 @@ fn C.glGetUniformLocation() int
 fn C.glGetAttribLocation() int
 fn C.glBindAttribLocation()
 
-//Uniform functions
 fn C.glUniform1f()
 
 
+// Initializes glad, which is needed to use other functions.
 pub fn init_glad() {
 	ok := C.gladLoadGL()
 	if ok == 0 {
@@ -75,82 +75,115 @@ pub fn init_glad() {
 	}
 }
 
-pub fn viewport(a, b, c, d int) {
-	C.glViewport(a, b, c, d)
+// Declare the area, which should be rendered in the window.
+// Should be used on resizing the window.
+pub fn viewport(x, y, width, height int) {
+	C.glViewport(x, y, width, height)
 }
 
+// Sets the color, which will be the background, where you can draw.
+// Need only set once and can be changed over the time.
 pub fn clear_color(r, g, b, a int) {
 	C.glClearColor(f32(r)/255.0, f32(g)/255.0, f32(b)/255.0, f32(a)/255.0)
 }
 
+// Clear the bits of the last frame (ColorBufferBit, DepthBufferBit, StencilBufferBit) to store new data.
+// Should be done every frame.
 pub fn clear() {
 	C.glClear(C.GL_COLOR_BUFFER_BIT | C.GL_DEPTH_BUFFER_BIT | C.GL_STENCIL_BUFFER_BIT)
 }
 
+// Create a shader in OpenGL and return the id of it. 
+// Can be used then to store data in it.
 pub fn create_shader(typ int) int {
 	return C.glCreateShader(typ)
 }
 
+// Create a program in OpenGL and returns the id.
+// A shader can attached to a program, which is required to use them.
 pub fn create_program() int {
 	return C.glCreateProgram()
 }
 
+// Delete a program by id.
+// Cleanup method, should be used after the main game loop
 pub fn delete_program(program int) {
 	C.glDeleteProgram(program)
 }
 
-pub fn shader_source(shader, a int, source string, b int) {
-	C.glShaderSource(shader, a, &source.str, b)
+// Adds shader source code to the shader via the shaderID.
+// Could be also used to load multiple sources into a shader.
+// To just add one source code to one shader use: `gl.shader_source(shader, 1, src, 0)`
+pub fn shader_source(shader, count int, source string, length int) {
+	C.glShaderSource(shader, count, &source.str, length)
 }
 
+// Compiles the source code in the shader.
+// OpenGL compiles the source code while runtime.
 pub fn compile_shader(shader int) {
 	C.glCompileShader(shader)
 }
 
+// Returns the compile status of the shader compilation.
+// Can be used to check the compilation and see for errors in the shader code via `gl.shader_info_log()`
 pub fn shader_compile_status(shader int) int {
 	success := 0
 	C.glGetShaderiv(shader, C.GL_COMPILE_STATUS, &success)
 	return success
 }
 
+// Attachs a shader to a program.
+// Required for drawing things on the screen
 pub fn attach_shader(program, shader int) {
-	// fn (s Shader) attach(program int) {
 	C.glAttachShader(program, shader)
 }
 
+// Detachs a shader from a program
+// Cleanup method for the end
 pub fn detach_shader(program, shader int) {
 	C.glDetachShader(program, shader)
 }
 
+// Links a program.
+// This let OpenGL know, which program it has to use.
 pub fn link_program(program int) {
 	C.glLinkProgram(program)
 }
 
+// Returns the link status of linking the program
 pub fn get_program_link_status(program int) int {
 	success := 0
 	C.glGetProgramiv(program, C.GL_LINK_STATUS, &success)
 	return success
 }
 
+// Checks that the shaders in the program can be executed
 pub fn validate_program(program int) {
 	C.glValidateProgram(program)
 }
 
+// Deletes a shader via the shaderID
+// Cleanup method
 pub fn delete_shader(shader int) {
 	C.glDeleteShader(shader)
 }
 
+// Returns a shader info log. 
+// Can be used to print compilation errors etc.
 pub fn shader_info_log(shader int) string {
 	info_log := [512]byte
 	C.glGetShaderInfoLog(shader, 512, 0, info_log)
 	return tos_clone(info_log)
 }
 
+// Returns a program info log. 
+// Can be used to print linking errors etc.
 pub fn get_program_info_log(program int) string {
 	info_log := [1024]byte
 	C.glGetProgramInfoLog(program, 1024, 0, info_log)
 	return tos_clone(info_log)
 }
+
 
 pub fn bind_vao(vao u32) {
 	C.glBindVertexArray(vao)
@@ -199,7 +232,6 @@ pub fn set_vbo(vbo u32, vertices []f32, draw_typ int) {
 
 pub fn set_ebo(ebo u32, indices []int, draw_typ int) {
 	gl.bind_buffer(C.GL_ELEMENT_ARRAY_BUFFER, ebo)
-	// gl.buffer_data_int(GL_ELEMENT_ARRAY_BUFFER, indices, draw_typ)
 	gl.buffer_data_int(C.GL_ELEMENT_ARRAY_BUFFER, indices, draw_typ)
 }
 
@@ -210,11 +242,6 @@ pub fn delete_buffer(vbo u32) {
 pub fn delete_vao(vao u32) {
 	C.glDeleteVertexArrays(1, vao)
 }
-
-// /////////////////////
-// fn gen_vertex_arrays(a int, vao uint) {
-// # glGenVertexArrays(a, &VAO);
-// }
 
 // gets the uniform location for key
 pub fn get_uniform_location(program int, key string) int {
