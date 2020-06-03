7a6d1dc8f4fc07fece330b278f43b2bcc4d76177
danieldaeschle <daniel.daeschle@gmail.com>
2020-06-03 22:04:03 +0200

    vdoc: keep dark mode on site refresh

--- cmd/tools/vdoc-resources/doc.js
+++ cmd/tools/vdoc-resources/doc.js
@@ -1,10 +1,20 @@
-var toggle = document.getElementById("toggle-menu");
-toggle.addEventListener("click", function(ev) {
-    document.querySelectorAll(".doc-nav").forEach(function(el) {
-        el.classList.toggle("hidden");
+(function() {
+    var toggle = document.getElementById("toggle-menu");
+    toggle.addEventListener("click", function(ev) {
+        document.querySelectorAll(".doc-nav").forEach(function(el) {
+            el.classList.toggle("hidden");
+        });
+        document.querySelectorAll(".doc-nav .content").forEach(function(el) {
+            el.classList.toggle("hidden");
+            el.classList.toggle("show");
+        });
     });
-    document.querySelectorAll(".doc-nav .content").forEach(function(el) {
-        el.classList.toggle("hidden");
-        el.classList.toggle("show");
+
+    var darkModeSwitch = document.getElementById('dark-mode-switch');
+    darkModeSwitch.addEventListener('change', function(event) {
+        localStorage.setItem('darkmode', +event.target.checked);
     });
-});
+    var darkMode = localStorage.getItem('darkmode') || false;
+    darkModeSwitch.checked = darkMode;
+})();
+
