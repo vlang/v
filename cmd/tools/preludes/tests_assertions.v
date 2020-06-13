65176423b7742a287a475473a63877dd4c9d476e
Adam Stankiewicz <sheerun@sher.pl>
2020-06-13 14:30:20 +0200

    test: run all tests even if some fail

--- cmd/tools/preludes/tests_assertions.v
+++ cmd/tools/preludes/tests_assertions.v
@@ -20,6 +20,7 @@ fn cb_assertion_failed(i &VAssertMetaInfo) {
 	}
 	final_filename := if use_relative_paths { i.fpath } else { os.real_path(i.fpath) }
 	final_funcname := i.fn_name.replace('main__', '').replace('__', '.')
+	eprintln('')
 	eprintln('$final_filename:${i.line_nr+1}: failed assert in ${final_funcname}')
 	eprintln('Source  : ${i.src}')
 	if i.op != 'call' {
