dbd1420b88fcf1a71a4d65283218690f0afd1875
spaceface777 <spaceface7777@gmail.com>
2020-06-20 12:35:10 +0200

    jsgen: add simple tests for the js backend

---
+++ vlib/v/gen/js/jsgen_test.v
@@ -0,0 +1,36 @@
+import os
+
+const (
+	test_dir   = 'vlib/v/gen/js/tests/'
+	output_dir = '_js_tests/'
+	v_options  = '-b js -w'
+)
+
+fn test_example_compilation() {
+	vexe := os.getenv('VEXE')
+	os.chdir(os.dir(vexe))
+
+	os.mkdir_all(output_dir)
+
+	files := find_test_files()
+	for file in files {
+		path := os.join_path(test_dir, file)
+		println('Testing $file')
+
+		v_code := os.system('$vexe $v_options -o ${output_dir}${file}.js $path')
+		if v_code != 0 { assert false } // Compilation failed
+
+		js_code := os.system('node ${output_dir}${file}.js')
+		if js_code != 0 { assert false } // Running failed
+	}
+
+	os.rmdir_all(output_dir)
+}
+
+fn find_test_files() []string {
+	files := os.ls(test_dir) or { panic(err) }
+	// The life example never exits, so tests would hang with it, skip
+	mut tests := files.filter(it.ends_with('.v')).filter(it != 'life.v')
+	tests.sort()
+	return tests
+}
