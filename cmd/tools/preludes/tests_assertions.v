c2925e1bb5cd114fbb2f44e3b6d3c436023e2bbd
Delyan Angelov <delian66@gmail.com>
2020-06-20 15:23:53 +0300

    improve failed assertion output

--- cmd/tools/preludes/tests_assertions.v
+++ cmd/tools/preludes/tests_assertions.v
@@ -1,6 +1,7 @@
 module main
 
 import os
+import term
 // //////////////////////////////////////////////////////////////////
 // / This file will get compiled as part of the main program,
 // / for a _test.v file.
@@ -11,7 +12,7 @@ import os
 // //////////////////////////////////////////////////////////////////
 // TODO copy pasta builtin.v fn ___print_assert_failure
 fn cb_assertion_failed(i &VAssertMetaInfo) {
-	// color_on := term.can_show_color_on_stderr()
+	use_color := term.can_show_color_on_stderr()
 	use_relative_paths := match os.getenv('VERROR_PATHS') {
 		'absolute' {
 			false
@@ -20,18 +21,22 @@ fn cb_assertion_failed(i &VAssertMetaInfo) {
 		}
 	}
 	final_filename := if use_relative_paths { i.fpath } else { os.real_path(i.fpath) }
-	final_funcname := i.fn_name.replace('main__', '').replace('__', '.')
+	final_funcname := i.fn_name.replace('main.', '').replace('__', '.')
+	final_src := if use_color { term.bold(i.src) } else { i.src }
 	eprintln('')
-	eprintln('$final_filename:${i.line_nr+1}: failed assert in ${final_funcname}')
-	eprintln('Source  : ${i.src}')
+	eprintln('$final_filename:${i.line_nr+1}: failed assert in function ${final_funcname}')
+	eprintln('Source  : `${final_src}`')
 	if i.op.len > 0 && i.op != 'call' {
-		eprintln('   left value: ${i.llabel} = ${i.lvalue}')
-		if i.rlabel == i.rvalue {
-			eprintln('  right value: $i.rlabel')
+		mut slvalue := '${i.lvalue}'
+		mut srvalue := '${i.rvalue}'
+		lpostfix := if slvalue == i.llabel { '.' } else { '<= `${i.llabel}`' }
+		rpostfix := if srvalue == i.rlabel { '.' } else { '<= `${i.rlabel}`' }
+		if use_color {
+			slvalue = term.bold(term.yellow(slvalue))
+			srvalue = term.bold(term.yellow(srvalue))
 		}
-		else {
-			eprintln('  right value: ${i.rlabel} = ${i.rvalue}')
-		}
+		eprintln('	 left value: ${slvalue} ${lpostfix}')
+		eprintln('	right value: ${srvalue} ${rpostfix}')
 	}
 }
 
