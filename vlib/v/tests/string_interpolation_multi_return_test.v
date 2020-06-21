7f45fa93aeda118fb86451356bfebe4b2b9f910b
yuyi <yuyi98@163.com>
2020-06-21 21:59:01 +0800

    change tests

--- vlib/v/tests/string_interpolation_multi_return_test.v
+++ vlib/v/tests/string_interpolation_multi_return_test.v
@@ -1,22 +1,27 @@
-fn test_multi_return_interpolation() {
-	s1 := '${return_multi_1()}'
-	assert s1 == "(1, 'aaa')"
 
-	s2 := '${return_multi_2()}'
-	assert s2 == "('aaa', true, [1, 2, 3])"
+struct RetTest {}
 
-	s3 := '${return_multi_3()}'
-	assert s3 == "(2.2, false, ['a', 'b', 'c'])"
-}
-
-fn return_multi_1() (int, string) {
+fn (r RetTest) return_multi_1() (int, string) {
 	return 1, 'aaa'
 }
 
-fn return_multi_2() (string, bool, []int) {
+fn (r RetTest) return_multi_2() (string, bool, []int) {
 	return 'aaa', true, [1,2,3]
 }
 
-fn return_multi_3() (f32, bool, []string) {
+fn (r RetTest) return_multi_3() (f32, bool, []string) {
 	return 2.2, false, ['a', 'b', 'c']
+}
+
+fn test_multi_return_interpolation() {
+	r := RetTest{}
+
+	s1 := '${r.return_multi_1()}'
+	assert s1 == "(1, 'aaa')"
+
+	s2 := '${r.return_multi_2()}'
+	assert s2 == "('aaa', true, [1, 2, 3])"
+
+	s3 := '${r.return_multi_3()}'
+	assert s3 == "(2.2, false, ['a', 'b', 'c'])"
 }
