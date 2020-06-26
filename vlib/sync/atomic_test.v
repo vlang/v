341be097b82fa589a2fdefba7a5c8199bd51bb8a
helto4real <tomas.hellstrom@yahoo.se>
2020-06-26 19:39:49 +0200

    Fix mut for waitgroup too

--- vlib/sync/atomic_test.v
+++ vlib/sync/atomic_test.v
@@ -11,7 +11,7 @@ fn test_count_100_milion_should_result_100_million() {
 	mut counter := &Counter{}
 	wg.add(10)
 	for i := 0; i < 10; i++ {
-		go count_ten_million(mut counter, wg)
+		go count_ten_million(mut counter, mut wg)
 	}
 	wg.wait()
 	assert counter.counter == 10000000
@@ -23,7 +23,7 @@ fn test_count_100_milion_should_fail_100_million_without_sync() {
 	mut counter := &Counter{}
 	wg.add(10)
 	for i := 0; i < 10; i++ {
-		go count_ten_million_without_sync(mut counter, wg)
+		go count_ten_million_without_sync(mut counter, mut wg)
 	}
 	wg.wait()
 	assert counter.counter != 10000000
@@ -78,7 +78,7 @@ fn test_count_minus_greater_than_one_i64() {
 }
 
 // count_ten_million counts the common counter 10 million times in thread-safe way
-fn count_ten_million(mut counter Counter, group &sync.WaitGroup) {
+fn count_ten_million(mut counter Counter, mut group sync.WaitGroup) {
 	for i := 0; i < 1000000; i++ {
 		sync.add_u64(&counter.counter, 1)
 	}
@@ -86,7 +86,7 @@ fn count_ten_million(mut counter Counter, group &sync.WaitGroup) {
 }
 
 // count_ten_million_without_sync counts the common counter 10 million times in none thread-safe way
-fn count_ten_million_without_sync(mut counter Counter, group &sync.WaitGroup) {
+fn count_ten_million_without_sync(mut counter Counter, mut group sync.WaitGroup) {
 	for i := 0; i < 1000000; i++ {
 		counter.counter++
 	}
