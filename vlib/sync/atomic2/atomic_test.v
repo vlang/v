ef6ac5880c5ff8e4da5a1b37173508c32cedf5ab
helto4real <tomas.hellstrom@yahoo.se>
2020-06-26 23:05:02 +0200

    removed anti-tests

--- vlib/sync/atomic2/atomic_test.v
+++ vlib/sync/atomic2/atomic_test.v
@@ -19,16 +19,16 @@ fn test_count_100_milion_should_result_100_million() {
 }
 
 // This test just to make sure that we have an anti-test to prove it works
-fn test_count_100_milion_should_fail_100_million_without_sync() {
-	mut wg := sync.new_waitgroup()
-	mut counter := &Counter{}
-	wg.add(10)
-	for i := 0; i < 10; i++ {
-		go count_ten_million_without_sync(mut counter, mut wg)
-	}
-	wg.wait()
-	assert counter.counter != 10000000
-}
+// fn test_count_100_milion_should_fail_100_million_without_sync() {
+// 	mut wg := sync.new_waitgroup()
+// 	mut counter := &Counter{}
+// 	wg.add(10)
+// 	for i := 0; i < 10; i++ {
+// 		go count_ten_million_without_sync(mut counter, mut wg)
+// 	}
+// 	wg.wait()
+// 	assert counter.counter != 10000000
+// }
 
 fn test_count_plus_one_u64() {
 	mut c := u64(0)
@@ -86,10 +86,10 @@ fn count_ten_million(mut counter Counter, mut group sync.WaitGroup) {
 	group.done()
 }
 
-// count_ten_million_without_sync counts the common counter 10 million times in none thread-safe way
-fn count_ten_million_without_sync(mut counter Counter, mut group sync.WaitGroup) {
-	for i := 0; i < 1000000; i++ {
-		counter.counter++
-	}
-	group.done()
-}
+// // count_ten_million_without_sync counts the common counter 10 million times in none thread-safe way
+// fn count_ten_million_without_sync(mut counter Counter, mut group sync.WaitGroup) {
+// 	for i := 0; i < 1000000; i++ {
+// 		counter.counter++
+// 	}
+// 	group.done()
+// }
