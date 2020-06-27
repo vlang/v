2669610be90f95fe158c5c0fcca25bf0ed277530
Delyan Angelov <delian66@gmail.com>
2020-06-27 21:44:14 +0300

    sqlite: fix windows-install-sqlite.bat

--- .github/workflows/windows-install-sqlite.bat
+++ .github/workflows/windows-install-sqlite.bat
@@ -2,9 +2,11 @@
 
 curl -L https://www.sqlite.org/2020/sqlite-amalgamation-3320300.zip -o sqlite-amalgamation-3320300.zip
 
-unzip sqlite-amalgamation-3320300.zip -d thirdparty/
+unzip sqlite-amalgamation-3320300.zip -d thirdparty\
 
-del thirdparty/sqlite-amalgamation-3320300/shell.c
+del thirdparty\sqlite-amalgamation-3320300\shell.c
 
-move /y thirdparty/sqlite-amalgamation-3320300  thirdparty/sqlite
+move /y thirdparty\sqlite-amalgamation-3320300  thirdparty\sqlite
+
+dir thirdparty\sqlite
 
