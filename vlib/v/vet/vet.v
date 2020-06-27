2bfe8e5153aea2f77b046c82682c991a3df59d67
Alexander Medvednikov <alexander@vlang.io>
2020-06-27 14:50:04 +0200

    vet: a basic version that just checks indentation for now

---
+++ vlib/v/vet/vet.v
@@ -0,0 +1,9 @@
+// Copyright (c) 2019-2020 Alexander Medvednikov. All rights reserved.
+// Use of this source code is governed by an MIT license that can be found in the LICENSE file.
+module vet
+
+import v.ast
+import v.table
+
+pub fn vet(file ast.File, table &table.Table, is_debug bool) {
+}
