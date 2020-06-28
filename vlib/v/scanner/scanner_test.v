a16fe004faba0588a15ceddf48e460ee9ec1ba70
Delyan Angelov <delian66@gmail.com>
2020-06-28 21:26:10 +0300

    Extract failing scanner_at_literals_test.v from failing scanner_test.v

--- vlib/v/scanner/scanner_test.v
+++ vlib/v/scanner/scanner_test.v
@@ -5,45 +5,6 @@ module scanner
 
 import v.token
 
-
-struct TestStruct {
-	test	string
-}
-
-fn (mut t TestStruct) test_struct() {
-	assert @STRUCT == 'TestStruct'
-}
-
-fn (mut t TestStruct) test_struct_w_return() string {
-	assert @STRUCT == 'TestStruct'
-	return t.test
-}
-
-fn (mut t TestStruct) test_struct_w_high_order(cb fn(int)string) string {
-	assert @STRUCT == 'TestStruct'
-	return 'test'+cb(2)
-}
-
-struct TestFn { }
-
-fn (mut t TestFn) tst_1() {
-	assert @FN == 'tst_1'
-}
-
-fn (mut t TestFn) tst_2(cb fn(int)) {
-	assert @FN == 'tst_2'
-	cb(1)
-}
-
-fn fn_name_mod_level() {
-	assert @FN == 'fn_name_mod_level'
-}
-
-fn fn_name_mod_level_high_order(cb fn(int)) {
-	assert @FN == 'fn_name_mod_level_high_order'
-	cb(1)
-}
-
 fn scan_kinds(text string) []token.Kind {
 	mut scanner := new_scanner(text, .skip_comments, false)
 	mut token_kinds := []token.Kind{}
@@ -66,14 +27,18 @@ fn test_scan() {
 	assert token_kinds[3] == .plus
 	assert token_kinds[4] == .number
 	assert token_kinds[5] == .rpar
-	// test number costants input format
+}
+
+fn test_number_constant_input_format() {
 	mut c := 0xa0
 	assert c == 0xa0
 	c = 0b1001
 	assert c == 9
 	c = 1000000
 	assert c == 1000000
-	// test float conversion and reading
+}
+
+fn test_float_conversion_and_reading() {
 	d := 23000000e-3
 	assert int(d) == 23000
 	mut e := 1.2E3 * -1e-1
@@ -83,92 +48,62 @@ fn test_scan() {
 	assert 1.23e+10 == 1.23e10
 	assert 1.23e+10 == 1.23e0010
 	assert (-1.23e+10) == (1.23e0010 * -1.0)
-
-	// Test @FN
-	assert @FN == 'test_scan'
-
-	fn_name_mod_level()
-	fn_name_mod_level_high_order(fn(i int){
-		t := i + 1
-		assert t == 2
-	})
-
-	mut tfn := TestFn{}
-	tfn.tst_1()
-	tfn.tst_2(fn(i int){
-		t := i + 1
-		assert t == 2
-	})
-
-	// Test @MOD
-	assert @MOD == 'scanner'
-
-	// Test @STRUCT
-	assert @STRUCT == ''
-
-	mut ts := TestStruct { test: "test" }
-	ts.test_struct()
-	r1 := ts.test_struct_w_return()
-	r2 := ts.test_struct_w_high_order(fn(i int)string{
-		assert @STRUCT == ''
-		return i.str()
-	})
-	assert r1 == 'test'
-	assert r2 == 'test2'
-
 }
 
-fn test_vmod_file() {
-	content := @VMOD_FILE
-	assert content.len > 0
-	assert content.contains('Module {')
-	assert content.contains('name:')
-	assert content.contains('version:')
-	assert content.contains('description:')
-}
-
-fn test_reference() {
-	mut result := scan_kinds('true && false')
+fn test_reference_bools() {
+	result := scan_kinds('true && false')
 	assert result.len == 3
 	assert result[0] == .key_true
 	assert result[1] == .and
 	assert result[2] == .key_false
+}
 
-	result = scan_kinds('&foo')
+fn test_reference_var() {
+	result := scan_kinds('&foo')
 	assert result.len == 2
 	assert result[0] == .amp
 	assert result[1] == .name
+}
 
-	result = scan_kinds('[]&foo')
+fn test_array_of_references() {
+	result := scan_kinds('[]&foo')
 	assert result.len == 4
 	assert result[0] == .lsbr
 	assert result[1] == .rsbr
 	assert result[2] == .amp
 	assert result[3] == .name
+}
 
-	result = scan_kinds('&[]&foo')
+fn test_ref_array_of_references() {
+	result := scan_kinds('&[]&foo')
 	assert result.len == 5
 	assert result[0] == .amp
 	assert result[1] == .lsbr
 	assert result[2] == .rsbr
 	assert result[3] == .amp
 	assert result[4] == .name
+}
 
-	result = scan_kinds('&&foo')
+fn test_ref_ref_foo() {
+	result := scan_kinds('&&foo')
 	assert result.len == 3
 	assert result[0] == .amp
 	assert result[1] == .amp
 	assert result[2] == .name
+}    
 
-	result = scan_kinds('[]&&foo')
+fn test_array_of_ref_ref_foo() {
+	result := scan_kinds('[]&&foo')
 	assert result.len == 5
 	assert result[0] == .lsbr
 	assert result[1] == .rsbr
 	assert result[2] == .amp
 	assert result[3] == .amp
 	assert result[4] == .name
+}
 
-	result = scan_kinds('&&[]&&foo')
+fn test_ref_ref_array_ref_ref_foo() {
+	result := scan_kinds('&&[]&&foo')
 	assert result.len == 7
 	assert result[0] == .amp
 	assert result[1] == .amp
