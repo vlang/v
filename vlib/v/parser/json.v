2236589b3b5d660b164c46574ba81d3deca1530c
Ned Palacios <npdoesmc@gmail.com>
2020-06-30 21:42:59 +0800

    json: update fns on json.v

--- vlib/v/parser/json.v
+++ vlib/v/parser/json.v
@@ -4,39 +4,40 @@
 module parser
 
 import strings
-import v.ast
 import v.scanner
 import v.table
 
 fn (mut p Parser) gen_json_for_type(typ table.Type) {
-	mut enc_dec := strings.new_builder(100)
-	sym := p.table.get_type_symbol(typ)
-	if is_js_prim(sym.name) || sym.kind == .enum_{
-		return
-	}
-	if sym.kind == .array {
-		// return
-	}
 	if typ in p.json_types {
 		return
 	}
+	typ_str := p.table.type_to_str(typ)
+	if is_prim(typ_str.all_after('[]')) || is_js_prim(typ_str.all_after('[]')) {
+		return
+	}
+	sym := p.table.get_type_symbol(typ)
+	if sym.kind == .enum_ {
+		return
+	}
+	mut enc_dec := strings.new_builder(100)
 	p.json_types << typ
 	if !sym.has_method('from_json') {
-		enc_dec.str(p.gen_from_json(typ, sym))
+		enc_dec.writeln(p.gen_from_json(typ, sym))
 	}
 	if !sym.has_method('to_json') {
-		enc_dec.str(p.gen_to_json(typ, sym))
+		enc_dec.writeln(p.gen_to_json(typ, sym))
 	}
 	p.scanner.codegen(enc_dec.str())
 }
 
-fn gen_json_value_cast(f_name string, var_name string, sym table.TypeSymbol) {
-	typ_name := p.table.typ_to_str(sym.name)
-	if sym.is_numeric() || sym.kind == .enum_ {
-		cast_typ := '${var_name}.' + if sym.is_int() { 'as_int()' } else { 'as_float()' }
-		exp_typ_kind := if sym.is_int() { token.Kind.int } else { token.Kind.f64 }
+fn (p Parser) gen_json_value_cast(var_name string, typ table.Type) string {
+	sym := p.table.get_type_symbol(typ)
+	typ_name := p.table.type_to_str(typ)
+	if typ.is_number() || sym.kind == .enum_ {
+		cast_typ := '${var_name}.' + if sym.is_int() || sym.kind == .enum_ { 'as_int()' } else { 'as_float()' }
+		exp_typ_kind := if sym.is_int() { table.Kind.int } else { table.Kind.f64 }
 		if sym.kind != exp_typ_kind {
-			return '$typ_name($cast_typ)'
+			return '${typ_name}(' + cast_typ + ')'
 		}
 		return cast_typ
 	}
@@ -55,175 +56,184 @@ fn gen_json_value_cast(f_name string, var_name string, sym table.TypeSymbol) {
 			return '${var_name}.as_bool()'
 		}
 		else {
-			return '$typ_name(${var_name}.as_string())'
+			return typ_name + '(' + var_name + '.as_string())'
 		}
 	}
 }
 
 fn (mut p Parser) gen_from_json(typ table.Type, sym table.TypeSymbol) string {
-	mut enc := strings.new_builder(200)
-	pubfn := if p.mod == 'main' { 'fn' } else { 'pub fn' }
-	typ_name := p.table.typ_to_str(sym.name)
-
-	enc.writeln('
-// autogen JSON from_json $typ_name
-$pubfn (mut t $typ_name) from_json(a json.Any) {
-	')
+	mut dec := strings.new_builder(200)
+	pub_fn := if p.mod == 'main' { 'fn' } else { 'pub fn' }
+	typ_name := p.table.type_to_str(typ)
+	mut defs := strings.new_builder(200)
+	defs.writeln('
+$pub_fn (mut t $typ_name) from_json(a json.Any) {')
 	match sym.kind {
-		.struct_, .map {
-			enc.writeln('   obj := a.as_map()')
-			if sym.kind == .struct_ {
-				enc.writeln('    for k, v in obj {\n        match k {')
-				info := sym.info as table.Struct
-				for field in info.fields {
-					if 'skip' in field.attrs {
-						continue
-					}
-					mut name := field.name
-					for attr in field.attrs {
-						if attr.starts_with('json:') {
-							name = attr.all_after('json:')
-							break
-						}
-					}
-					enc.writeln("            '$name' {")
-					field_typ := field.typ
-					table.value_type(typ)
-					enc.writeln("            }")
+		.struct_ {
+			defs.writeln('obj := a.as_map()')
+			dec.writeln('final := &$typ_name{')
+			info := sym.info as table.Struct
+			for field in info.fields {
+				if 'skip' in field.attrs {
+					continue
 				}
-				enc.writeln("            else {}")
-				enc.writeln('        }\n    }')
+				field_name := field.name
+				mut name := field_name
+				for attr in field.attrs {
+					if attr.starts_with('json:') {
+						name = attr.all_after('json:')
+						break
+					}
+				}
+				// todo arrays
+				dec.write("$field_name: ")
+				field_typ := field.typ
+				field_typ_str := p.table.type_to_str(field_typ)
+				field_sym := p.table.get_type_symbol(field_typ)
+				if field_sym.kind in [.struct_, .array] {
+					if field_sym.kind == .array && (is_prim(field_typ_str.all_after('[]')) || is_js_prim(field_typ_str.all_after('[]'))) {
+						value_typ := p.table.value_type(field_typ)
+						cast_array_item_typ := p.gen_json_value_cast('it', value_typ)
+						dec.writeln('obj["$name"].as_array().map($cast_array_item_typ)')
+					} else if field_typ !in p.json_types {
+						p.gen_json_for_type(field_typ)
+						field_var_name := typ_name.to_lower() +'_'+field_name
+						defs.writeln('mut $field_var_name := ${field_typ_str}{}')
+						defs.writeln('${field_var_name}.from_json(obj["$name"])')
+						dec.writeln('$field_var_name')
+					}
+				} else if field_sym.kind == .enum_ {
+					field_var_name := typ_name.to_lower() +'_'+field_name
+					defs.writeln('$field_var_name := obj["$name"].as_int()')
+					dec.writeln('${field_typ_str}($field_var_name)')
+				} else if 'raw' in field.attrs {
+					dec.writeln('obj["$name"].str()')
+				} else if (is_prim(field_typ_str) || is_js_prim(field_typ_str)) || field_sym.kind == .enum_ {
+					dec.writeln(p.gen_json_value_cast('obj["$name"]', field_typ))
+				}
 			}
+			dec.writeln('}')
+			dec.writeln('t = final')
+		}
+		.map {
+			dec.writeln('t = a.as_map()')
 		}
 		.array {
-			enc.writeln('   obj := a.as_array()')
-		}
-		.string, .ustring {
-			enc.writeln('   t = a.as_string()')
-		}
-		.bool {
-			enc.writeln('   obj := a.as_bool()')
+			dec.writeln('arr := a.as_array()')
+			dec.writeln('for v in arr {')
+			value_typ := p.table.value_type(typ)
+			value_typ_str := p.table.type_to_str(value_typ)
+			if is_prim(value_typ_str) {
+				dec.write('t << ')
+				dec.writeln(p.gen_json_value_cast('v', value_typ))
+			} else {
+				if value_typ !in p.json_types {
+					p.gen_json_for_type(value_typ)
+				}
+				dec.writeln('mut item := ${value_typ_str}{}')
+				dec.writeln('item.from_json(v)')
+				dec.writeln('t << item')
+			}
+			dec.write('}')
 		}
 		else {
-			if sym.is_int() || sym.kind == .enum_ {
-				enc.write('   t = ')
-				if sym.kind != .i32 {
-					enc.writeln('$typ_name(a.as_int())')
-				} else {
-					enc.writeln('a.as_int()')
-				}
-			} else if sym.is_float() {
-				enc.write('   t = ')
-				if sym.kind != .f64 {
-					enc.writeln('$typ_name(a.as_float())')
-				} else {
-					enc.writeln('a.as_int()')
-				}
+			dec.write('t = ')
+			if is_prim(typ_name) || is_js_prim(typ_name) || sym.kind == .enum_ {
+				dec.writeln(p.gen_json_value_cast('a', typ))
 			} else {
 				// todo
 				panic('json: unsupported type $typ_name')
 			}
 		}
 	}
-	enc.writeln('}')
-	return enc.str()
+	dec.writeln('}')
+	op := defs.str() + dec.str()
+	return op
 }
 
-fn (mut p Parser) gen_to_json(typ table.Type, sym table.TypeSymbol) {
-		// Code gen encoder
-	// encode_TYPE funcs receive an object to encode
-	enc_fn_name := js_enc_name(sym.name)
-	enc_fn_dec := 'cJSON* ${enc_fn_name}($styp val)'
-	g.json_forward_decls.writeln('$enc_fn_dec;\n')
+fn (mut p Parser) gen_to_json(typ table.Type, sym table.TypeSymbol) string {
+	mut enc := strings.new_builder(200)
+	pub_fn := if p.mod == 'main' { 'fn' } else { 'pub fn' }
+	typ_name := p.table.type_to_str(typ)
 	enc.writeln('
-$enc_fn_dec {
-\tcJSON *o = cJSON_CreateObject();')
-	if sym.kind == .array {
-		// Handle arrays
-		value_type := g.table.value_type(typ)
-		g.gen_json_for_type(value_type)
-		dec.writeln(g.decode_array(value_type))
-		enc.writeln(g.encode_array(value_type))
-		// enc += g.encode_array(t)
-	} else {
-		// Structs. Range through fields
-		if !(sym.info is table.Struct) {
-			verror('json: $sym.name is not struct')
-		}
-		info := sym.info as table.Struct
-		for field in info.fields {
-			if 'skip' in field.attrs {
-				continue
-			}
-			mut name := field.name
-			for attr in field.attrs {
-				if attr.starts_with('json:') {
-					name = attr.all_after('json:')
-					break
+$pub_fn (t $typ_name) to_json() string {')
+	match sym.kind {
+		.struct_ {
+			enc.writeln('mut obj := map[string]json.Any')
+			info := sym.info as table.Struct
+			for field in info.fields {
+				if 'skip' in field.attrs {
+					continue
 				}
-			}
-			field_type := g.typ(field.typ)
-			if 'raw' in field.attrs {
-				dec.writeln(' res . ${c_name(field.name)} = tos2(cJSON_PrintUnformatted(' + 'js_get(root, "$name")));')
-			} else {
-				// Now generate decoders for all field types in this struct
-				// need to do it here so that these functions are generated first
-				g.gen_json_for_type(field.typ)
-				dec_name := js_dec_name(field_type)
-				if is_js_prim(field_type) {
-					dec.writeln(' res . ${c_name(field.name)} = $dec_name (js_get(root, "$name"));')
-				} else if g.table.get_type_symbol(field.typ).kind == .enum_ {
-					dec.writeln(' res . ${c_name(field.name)} = json__decode_u64(js_get(root, "$name"));')
+				mut name := field.name
+				for attr in field.attrs {
+					if attr.starts_with('json:') {
+						name = attr.all_after('json:')
+						break
+					}
+				}
+				field_typ := field.typ
+				field_name := field.name
+				field_sym := p.table.get_type_symbol(field_typ)
+				field_typ_str := p.table.type_to_str(field_typ)
+				enc.write("obj['$name'] = ")
+				if is_js_prim(field_typ_str) {
+					enc.writeln('t.${field_name}')
+				} else if is_prim(field_typ_str) || field_sym.kind == .enum_ {
+					if field_sym.is_int() || field_sym.kind == .enum_ { 
+						enc.writeln('int(t.${field_name})')
+					} else { 
+						enc.writeln('f64(t.${field_name})')
+					}
+				} else if is_js_prim(field_typ_str.all_after('[]')) || is_prim(field_typ_str.all_after('[]')) {
+					enc.writeln('t.${field_name}.map(json.Any(it))') 
 				} else {
-					// dec.writeln(' $dec_name (js_get(root, "$name"), & (res . $field.name));')
-					dec.writeln('  res . ${c_name(field.name)} = *($field_type*) $dec_name (js_get(root,"$name")).data;')
+					if field_typ !in p.json_types {
+						p.gen_json_for_type(field_typ)
+					}
+					enc.writeln("t.${field_name}.to_json()")
 				}
 			}
-
-			mut enc_name := js_enc_name(field_type)
-			if g.table.get_type_symbol(field.typ).kind == .enum_ {
-				enc.writeln('\tcJSON_AddItemToObject(o, "$name", json__encode_u64(val.${c_name(field.name)}));')
-				
+		}
+		.array {
+			value_typ := p.table.value_type(typ)
+			value_typ_str := p.table.type_to_str(value_typ)
+			value_sym := p.table.get_type_symbol(value_typ)
+			enc.write('obj := t.map(')
+			if is_js_prim(value_typ_str) {
+				enc.write('json.Any(it)')
+			} else if is_prim(value_typ_str) {
+				enc.write('json.Any(')
+				if value_sym.is_int() { 
+					enc.write('it')
+				} else if value_sym.kind == .enum_ {
+					enc.write('int(it)')
+				} else { 
+					enc.write('f64(it)')
+				}
+				enc.write(')')
 			} else {
-				enc.writeln('\tcJSON_AddItemToObject(o, "$name", ${enc_name}(val.${c_name(field.name)}));')
+				if value_typ !in p.json_types {
+					p.gen_json_for_type(value_typ)
+				}
+				enc.write("it.to_json()")
 			}
+			enc.writeln(')')
+		}
+		else {
+			enc.writeln('obj := json.Any(t)')
 		}
 	}
+	enc.writeln('return obj.str() \n}')
+	op := enc.str()
+	println(op)
+	return op
+}
+
+fn is_prim(typ string) bool {
+	return typ in ['f32', 'i8', 'i16', 'i64', 'u16', 'u32', 'u64', 'byte']
 }
 
 fn is_js_prim(typ string) bool {
-	return typ in ['int', 'string', 'bool', 'f32', 'f64', 'i8', 'i16', 'i64', 'u16', 'u32', 'u64', 'byte']
-}
-
-fn (mut p Parser) decode_array(value_type table.Type) string {
-	styp := g.typ(value_type)
-	fn_name := js_dec_name(styp)
-	// If we have `[]Profile`, have to register a Profile en(de)coder first
-	g.gen_json_for_type(value_type)
-	mut s := ''
-	if is_js_prim(styp) {
-		s = '$styp val = ${fn_name}(jsval); '
-	} else {
-		s = '\t$styp val = *($styp*) ${fn_name}(jsval).data; '
-	}
-	return '
-res = __new_array(0, 0, sizeof($styp));
-const cJSON *jsval = NULL;
-cJSON_ArrayForEach(jsval, root)
-{
-$s
-  array_push(&res, &val);
-}
-'
-}
-
-fn (mut g Gen) encode_array(value_type table.Type) string {
-	styp := g.typ(value_type)
-	fn_name := js_enc_name(styp)
-	return '
-o = cJSON_CreateArray();
-for (int i = 0; i < val.len; i++){
-  cJSON_AddItemToArray(o, $fn_name (  (($styp*)val.data)[i]  ));
-}
-'
+	return typ in ['string', 'int', 'f64', 'any_int', 'any_float', 'bool', 'Null', '[]Any', 'map[string]Any']
 }
