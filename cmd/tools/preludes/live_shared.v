8edbd6a3e280e7e883ab1c95f7122d8d2336a77a
Uwe Krüger <45282134+UweKrueger@users.noreply.github.com>
2020-06-30 20:30:35 +0200

    rename live.shared -> live.sharedlib to avoid conflict with keyword `shared`

--- cmd/tools/preludes/live_shared.v
+++ cmd/tools/preludes/live_shared.v
@@ -2,4 +2,4 @@ module main
 
 // This prelude is loaded in every v program compiled with -live,
 // but only for the shared library.
-import live.shared
+import live.sharedlib
---
+++ vlib/live/sharedlib/live_sharedlib.v
@@ -0,0 +1,3 @@
+module sharedlib
+
+import live
