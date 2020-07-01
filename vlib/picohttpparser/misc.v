e7339fec15e2fcd3fa127a24823583fc3c58c30f
Nick Treleaven <n@trelsoft.com>
2020-07-01 13:50:17 +0100

    all: disallow pointer arithmetic for AssignStmt, PostfixExpr outside unsafe{} (#5581)

--- vlib/picohttpparser/misc.v
+++ vlib/picohttpparser/misc.v
@@ -1,12 +1,6 @@
 module picohttpparser
 
-[inline]
-fn cpy_str(dst byteptr, src string) int {
-	C.memcpy(dst, src.str, src.len)
-	return src.len
-}
-
-[inline]
+[inline] [unsafe_fn]
 fn cpy(dst, src byteptr, len int) int {
 	C.memcpy(dst, src, len)
 	return len
