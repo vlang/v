e9e437ce0502a4c39532fa25dbbb4dc3d66d05cc
Alexander Medvednikov <alexander@vlang.io>
2020-07-03 22:30:34 +0200

    repl: show short version

--- cmd/tools/vrepl.v
+++ cmd/tools/vrepl.v
@@ -76,7 +76,7 @@ fn (r &Repl) current_source_code(should_add_temp_lines bool) string {
 }
 
 fn repl_help() {
-	println(util.full_v_version(true))
+	println(util.full_v_version(false))
 	println('
   help                   Displays this information.
   list                   Show the program so far.
@@ -87,7 +87,7 @@ fn repl_help() {
 }
 
 fn run_repl(workdir string, vrepl_prefix string) {
-	println(util.full_v_version(true))
+	println(util.full_v_version(false))
 	println('Use Ctrl-C or `exit` to exit')
 
 	file := os.join_path(workdir, '.${vrepl_prefix}vrepl.v')
