f3e906db522616dc550b95d0e66e46739d3c86eb
Uwe Krüger <45282134+UweKrueger@users.noreply.github.com>
2020-07-05 20:57:42 +0200

    Revert "minor fix"
    This reverts commit afcd3cda502d299439e80b5c3a2705980f7df766.

--- vlib/v/gen/cheaders.v
+++ vlib/v/gen/cheaders.v
@@ -195,7 +195,7 @@ $c_common_macros
 #pragma comment(lib, "Dbghelp.lib")
 
 extern wchar_t **_wenviron;
-#elif defined(__TINYC__)
+#elif !defined(SRWLOCK_INIT)
 // these seem to be missing on Windows tcc
 typedef struct SRWLOCK { void* SRWLOCK; } SRWLOCK;
 void InitializeSRWLock(void*);
