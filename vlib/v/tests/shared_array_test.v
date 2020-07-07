e6bacca74442cc60544f55defd76cde4d000f40f
Uwe Krüger <45282134+UweKrueger@users.noreply.github.com>
2020-07-07 13:45:21 +0200

    cgen: add support for shared arrays

---
+++ vlib/v/tests/shared_array_test.v
@@ -0,0 +1,41 @@
+import sync
+import time
+
+fn incr(shared foo []int, index int) {
+    for _ in 0..100000 {
+		lock foo {
+			foo[index] = foo[index] + 1
+		}
+    }
+	lock foo {
+		foo[2]++
+	}
+}
+
+fn test_shared_array(){
+    shared foo := &[10, 20, 0]
+    go incr(shared foo, 0)
+    go incr(shared foo, 1)
+    go incr(shared foo, 0)
+    go incr(shared foo, 1)
+	for _ in 0..50000 {
+		lock foo {
+			foo[0] -= 2
+			foo[1] += 3
+		}
+	}
+	mut finished_threads := 0
+	for {
+		rlock foo {
+			finished_threads = foo[2]
+		}
+		if finished_threads == 4 {
+			break
+		}
+		time.sleep_ms(100)
+	}
+	rlock foo {
+		assert foo[0] == 100010
+		assert foo[1] == 350020
+	}
+}
