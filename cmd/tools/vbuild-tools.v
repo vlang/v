1d24fecc1b7c16045588590ddd589636d8777af4
Delyan Angelov <delian66@gmail.com>
2020-07-08 14:29:55 +0300

    cmd: fix building gen_vc.v and vcreate.v by `v build-tools`

--- cmd/tools/vbuild-tools.v
+++ cmd/tools/vbuild-tools.v
@@ -7,10 +7,7 @@ import v.util
 fn main() {
 	args := os.args
 	args_string := args[1..].join(' ')
-	// TODO: fix cmd/tools/gen_vc.v / vweb too
-	skips := [
-		'cmd/tools/gen_vc.v'
-	]
+	skips := []string{}
 	util.ensure_modules_for_all_tools_are_installed('-v' in args)
 	if testing.v_build_failing_skipped(args_string.all_before('build-tools'), 'cmd/tools', skips) {
 		exit(1)
