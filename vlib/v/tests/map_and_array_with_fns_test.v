889eabfff0e20e1b08d77bebdc85883756cb4ac4
Ruofan XU <47302112+SleepyRoy@users.noreply.github.com>
2020-07-20 22:50:16 +0800

    Update map_and_array_with_fns_test.v

--- vlib/v/tests/map_and_array_with_fns_test.v
+++ vlib/v/tests/map_and_array_with_fns_test.v
@@ -6,9 +6,10 @@ fn foo2(a string) int {
 	return 20 + a.len
 }
 
-fn test_array_of_fns() {
+fn test_array_with_fns() {
 	mut a := [foo, foo2]
 	assert a.len == 2
+	assert foo in a
 	f0 := a[0]
 	assert f0('xx') == 12
 	f1 := a[1]
@@ -19,12 +20,13 @@ fn test_array_of_fns() {
 	f3 := a[1]
 	assert f3('aaaaa') == 15
 	mut b := [foo]
+	assert foo2 !in b
 	b[0] = a[0]
 	f4 := b[0]
 	assert f4('bbbbbb') == 26
 }
 
-fn test_map_of_fns() {
+fn test_map_with_fns() {
 	mut a := {'one':foo, 'two':foo2}
 	assert a.len == 2
 	f0 := a['one']
