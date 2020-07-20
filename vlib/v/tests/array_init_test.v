8356911f4756ee887102ff4b70d90779c750ab55
Ruofan XU <47302112+SleepyRoy@users.noreply.github.com>
2020-07-20 19:30:32 +0800

    Update array_init_test.v

--- vlib/v/tests/array_init_test.v
+++ vlib/v/tests/array_init_test.v
@@ -194,30 +194,3 @@ fn test_array_init_direct_call() {
 	assert []int{len: 2, init: 0}.len == 2
 	assert []int{len: 3, init: 1}.map(it*2) == [2,2,2]
 }
-
-
-fn foo(a string) int {
-	return 10 + a.len
-}
-
-fn foo2(a string) int {
-	return 20 + a.len
-}
-
-fn test_array_of_fns_init() {
-	a := [foo, foo2]
-	assert a.len == 2
-	f0 := a[0]
-	assert f0('xx') == 12
-	f1 := a[1]
-	assert f1('yyy') == 23
-	a[0], a[1] = a[1], a[0]
-	f2 := a[0]
-	assert f2('zzzz') == 24
-	f3 := a[1]
-	assert f3('aaaaa') == 15
-	mut b := [foo]
-	b[0] = a[1]
-	f4 := b[0]
-	assert f4('bbbbbb') == 15
-}
