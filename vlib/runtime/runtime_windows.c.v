0eb0604df5200278f1dadf87e32d951a7cb26c5c
Carlos Esquerdo <esquerbatua@gmail.com>
2020-07-20 11:51:05 +0200

    Simplify runtime

--- vlib/runtime/runtime_windows.c.v
+++ vlib/runtime/runtime_windows.c.v
@@ -2,15 +2,10 @@ module runtime
 
 import os
 
-fn nr_cpus_win() int {
+pub fn nr_cpus() int {
 	mut nr := int(C.GetCurrentProcessorNumber())
 	if nr == 0 {
 		nr = os.getenv('NUMBER_OF_PROCESSORS').int()
 	}
 	return nr
-}
-
-fn nr_cpus_nix() int {
-	eprintln('nr_cpus_nix should be callable only for nix platforms')
-	return 1
 }
