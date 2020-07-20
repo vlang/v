6127dc81ed43c6c151dfe4d330f5ed3239a9be77
Ruofan XU <47302112+SleepyRoy@users.noreply.github.com>
2020-07-20 21:10:21 +0800

    Add files via upload

---
+++ vlib/v/tests/map_and_array_with_fns_test.v
@@ -0,0 +1,27 @@
+fn foo(a string) int {
+	return 10 + a.len
+}
+
+fn foo2(a string) int {
+	return 20 + a.len
+}
+
+fn test_array_of_fns_init() {
+	mut a := [foo, foo2]
+	assert a.len == 2
+	f0 := a[0]
+	assert f0('xx') == 12
+	f1 := a[1]
+	assert f1('yyy') == 23
+	a[0], a[1] = a[1], a[0]
+	f2 := a[0]
+	assert f2('zzzz') == 24
+	f3 := a[1]
+	assert f3('aaaaa') == 15
+	mut b := [foo]
+	b[0] = a[0]
+	f4 := b[0]
+	assert f4('bbbbbb') == 26
+}
+
+
