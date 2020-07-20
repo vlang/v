40afde42cfbf77a79892da09921f59b97e0ff57d
Carlos Esquerdo <esquerbatua@gmail.com>
2020-07-20 21:10:09 +0200

    Updated url

--- vlib/net/websocket/ws_test.v
+++ vlib/net/websocket/ws_test.v
@@ -2,5 +2,5 @@ module websocket
 
 // TODO This only checks that the client compiles, do real tests
 fn test_compile() {
-	new('http://examples.com')
+	new('ws://echo.websocket.org')//wss://echo.websocket.org
 }
