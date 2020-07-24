921b5cddd0a8c48652881b7d0f8662a2f460f847
Delyan Angelov <delian66@gmail.com>
2020-07-24 08:45:28 +0300

    live: fix compilation

--- vlib/live/common.v
+++ vlib/live/common.v
@@ -60,7 +60,7 @@ pub fn info() &LiveReloadInfo {
 	// started, and the structure LiveReloadInfo will not get updated.
 	// All its fields will be 0, but still safe to access.
 	mut x := &LiveReloadInfo{}
-	p := &u64(&C.g_live_info)
+	mut p := &u64(&C.g_live_info)
 	unsafe { *p = &u64(x) }        
 	return x
 }
