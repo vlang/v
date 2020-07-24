049c060e93ec98f06eaf9f6c3d5de139da08d257
LouisSchmieder <dev@louis-schmieder.de>
2020-07-24 13:50:47 +0200

    update tests

--- vlib/v/tests/comptime_for_test.v
+++ vlib/v/tests/comptime_for_test.v
@@ -1,4 +1,15 @@
 struct App {
+	a string
+	b string
+mut:
+	c int
+	d f32
+pub:
+	e f32
+	f u64
+pub mut:
+	g string
+	h byte
 }
 
 ['foo/bar/three']
@@ -18,17 +29,39 @@ fn (mut app App) int_method2() int {
 
 fn test_comptime_for() {
 	methods := ['run', 'method2', 'int_method1', 'int_method2']
-	$for method in App.methods {
-		println(attrs)
-		println(ret_type)
-		assert method in methods
+	$for method in App(methods) {
+		println(method.attrs)
+		println(method.ret_type)
+		assert method.name in methods
 	}
 }
 
 fn test_comptime_for_with_if() {
 	methods := ['int_method1', 'int_method2']
-	$for method in App if int {
-		println(attrs)
-		assert method in methods
+	$for method in App(methods) {
+		$if ret_type is int {
+			println(method.attrs)
+			assert method.name in methods
+		}
+	}
+}
+
+fn test_comptime_for_fields() {
+	$for field in App(fields) {
+		$if ret_type is string {
+			assert field.name in ['a', 'b', 'g']
+		}
+		$if ret_type is f32 {
+			assert field.name in ['d', 'e']
+		}
+		if field.is_mut {
+			assert field.name in ['c', 'd', 'g', 'h']
+		}
+		if field.is_pub {
+			assert field.name in ['e', 'f', 'g', 'h']
+		}
+		if field.is_pub && field.is_mut {
+			assert field.name in ['g', 'h']
+		}
 	}
 }
