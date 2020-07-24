d57b8b69f0a42046f0dcc47f3798d71613ed77f7
LouisSchmieder <dev@louis-schmieder.de>
2020-07-24 05:34:55 +0200

    fix test

--- vlib/v/tests/comptime_for_test.v
+++ vlib/v/tests/comptime_for_test.v
@@ -9,12 +9,18 @@ fn (mut app App) run() {
 fn (mut app App) method2() {
 }
 
-fn (mut app App) int_method1() int {}
-fn (mut app App) int_method2() int {}
+fn (mut app App) int_method1() int {
+	return 0
+}
+fn (mut app App) int_method2() int {
+	return 1
+}
 
 fn test_comptime_for() {
 	methods := ['run', 'method2', 'int_method1', 'int_method2']
 	$for method in App {
+		println(attrs)
+		println(ret_type)
 		assert method in methods
 	}
 }
@@ -22,6 +28,8 @@ fn test_comptime_for() {
 fn test_comptime_for_with_if() {
 	methods := ['int_method1', 'int_method2']
 	$for method in App if int {
+		println(attrs)
+		println(ret_type)
 		assert method in methods
 	}
 }
