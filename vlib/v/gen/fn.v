8a08d9ce5240a0597200c415f62913b329b0caa4
Ruofan XU <47302112+SleepyRoy@users.noreply.github.com>
2020-07-27 05:47:16 +0800

    Update fn.v

--- vlib/v/gen/fn.v
+++ vlib/v/gen/fn.v
@@ -391,7 +391,7 @@ fn (mut g Gen) method_call(node ast.CallExpr) {
 	// g.write('/*${g.typ(node.receiver_type)}*/')
 	// g.write('/*expr_type=${g.typ(node.left_type)} rec type=${g.typ(node.receiver_type)}*/')
 	// }
-	if !node.receiver_type.is_ptr() && node.left_type.is_ptr() && node.name == 'str' && !g.should_write_aster_for(node.left) {
+	if !node.receiver_type.is_ptr() && node.left_type.is_ptr() && node.name == 'str' && !g.should_write_asterisk_for(node.left) {
 		g.write('ptr_str(')
 	} else {
 		g.write('${name}(')
