2d0dd8ea18c237ad7104f842a9e252f9fa24eb8e
LouisSchmieder <dev@louis-schmieder.de>
2020-08-06 16:16:06 +0200

    move module

--- vlib/io/netinstream.v
+++ vlib/io/netinstream.v
@@ -1,4 +1,4 @@
-module stream
+module io
 
 import net
 import encoding.binary
@@ -15,7 +15,7 @@ pub fn new_net_input_stream(sock &net.Socket) &NetInputStream {
 }
 
 pub fn (mut nis NetInputStream) get_int() int {
-	return int(nis.get_bytes(sizeof(int)).data)
+	return int(binary.big_endian_u32(nis.get_bytes(sizeof(int))))
 }
 
 pub fn (mut nis NetInputStream) get_ints(l u32) []int {
@@ -24,7 +24,7 @@ pub fn (mut nis NetInputStream) get_ints(l u32) []int {
 	for i in 0 .. l {
 		offs := int(u32(i) * sizeof(int))
 		b := bytes[offs..int(u32(offs) + sizeof(int))]
-		ints << int(binary.little_endian_u32(b))
+		ints << int(binary.big_endian_u32(b))
 	}
 	return ints
 }
@@ -43,7 +43,7 @@ pub fn (mut nis NetInputStream) get_i8s(l u32) []i8 {
 }
 
 pub fn (mut nis NetInputStream) get_i16() i16 {
-	return i16(nis.get_bytes(sizeof(i16)).data)
+	return i16(binary.big_endian_u16(nis.get_bytes(sizeof(i16))))
 }
 
 pub fn (mut nis NetInputStream) get_i16s(l u32) []i16 {
@@ -52,13 +52,14 @@ pub fn (mut nis NetInputStream) get_i16s(l u32) []i16 {
 	for i in 0 .. l {
 		offs := int(u32(i) * sizeof(i16))
 		b := bytes[offs..int(u32(offs) + sizeof(i16))]
-		i16s << i16(binary.little_endian_u16(b))
+		i16s << i16(binary.big_endian_u16(b))
 	}
 	return i16s
 }
 
 pub fn (mut nis NetInputStream) get_i64() i64 {
-	return i64(nis.get_bytes(sizeof(i64)).data)
+	return i64(binary.big_endian_u64(nis.get_bytes(sizeof(i64))))
+
 }
 
 pub fn (mut nis NetInputStream) get_i64s(l u32) []i64 {
@@ -67,7 +68,7 @@ pub fn (mut nis NetInputStream) get_i64s(l u32) []i64 {
 	for i in 0 .. l {
 		offs := int(u32(i) * sizeof(i64))
 		b := bytes[offs..int(u32(offs) + sizeof(i64))]
-		i64s << i64(binary.little_endian_u64(b))
+		i64s << i64(binary.big_endian_u64(b))
 	}
 	return i64s
 }
@@ -80,67 +81,66 @@ pub fn (mut nis NetInputStream) get_byte() byte {
 }
 
 pub fn (mut nis NetInputStream) get_bytes(l u32) []byte {
-	mut buf := byteptr(0)
-	unsafe {
-		buf = malloc(int(l))
+	mut bytes := []byte{len: int(l), cap: int(l)}
+	for i in 0..l {
+		bytes[i] = nis.get_byte()
 	}
-	nis.sock.cread(buf, int(l))
-	println(string(buf).bytes().hex())
-	return string(buf).bytes()
+	return bytes
 }
 
 pub fn (mut nis NetInputStream) get_u16() u16 {
-	return u16(nis.get_bytes(sizeof(u16)).data)
+	return binary.big_endian_u16(nis.get_bytes(sizeof(u16)))
 }
 
 pub fn (mut nis NetInputStream) get_u16s(l u32) []u16 {
 	bytes := nis.get_bytes(sizeof(u16) * l)
-	println(bytes.len)
 	mut u16s := []u16{}
 	for i in 0 .. l {
 		offs := int(u32(i) * sizeof(u16))
 		b := bytes[offs..int(u32(offs) + sizeof(u16))]
-		u16s << binary.little_endian_u16(b)
+		u16s << binary.big_endian_u16(b)
 
 	}
 	return u16s
 }
 
 pub fn (mut nis NetInputStream) get_u32() u32 {
-	return u32(nis.get_bytes(sizeof(u32)).data)
+	return binary.big_endian_u32(nis.get_bytes(sizeof(u32)))
 }
 
 pub fn (mut nis NetInputStream) get_u32s(l u32) []u32 {
 	bytes := nis.get_bytes(sizeof(u32) * l)
-	println(bytes.len)
 	mut u32s := []u32{}
 	for i in 0 .. l {
-		println(i)
 		offs := int(u32(i) * sizeof(u32))
 		b := bytes[offs..int(u32(offs) + sizeof(u32))]
-		u32s <<  binary.little_endian_u32(b)
+		u32s <<  binary.big_endian_u32(b)
 	}
 	return u32s
 }
 
 pub fn (mut nis NetInputStream) get_u64() u64 {
-	return u64(nis.get_bytes(sizeof(u64)).data)
+	return binary.big_endian_u64(nis.get_bytes(sizeof(u64)))
 }
 
 pub fn (mut nis NetInputStream) get_u64s(l u32) []u64 {
 	bytes := nis.get_bytes(sizeof(u64) * l)
-	println(bytes.len)
 	mut u64s := []u64{}
 	for i in 0 .. l {
 		offs := int(u32(i) * sizeof(u64))
 		b := bytes[offs..int(u32(offs) + sizeof(u64))]
-		u64s <<  binary.little_endian_u64(b)
+		u64s <<  binary.big_endian_u64(b)
 	}
 	return u64s
 }
-
+/*
 pub fn (mut nis NetInputStream) get_f32() f32 {
-	return f32(nis.get_int())
+	bytes := nis.get_bytes(sizeof(f32))
+	println(bytes.hex())
+	f := &f32(bytes.data)
+	unsafe {
+		return f[0]
+	}
 }
 
 pub fn (mut nis NetInputStream) get_f32s(l u32) []f32 {
@@ -173,7 +173,7 @@ pub fn (mut nis NetInputStream) get_f64s(l u32) []f64 {
 		}
 	}
 	return f64s
-}
+}*/
 
 pub fn (mut nis NetInputStream) get_string(l u32) string {
 	return string(nis.get_bytes(l))
@@ -183,6 +183,7 @@ pub fn (mut nis NetInputStream) skip(l u32) {
 	nis.get_bytes(l)
 }
 
-pub fn (mut nis NetInputStream) get_struct<T>() T {
-	return T(nis.get_bytes(sizeof(T)).data)
+// TODO make it generic
+pub fn (mut nis NetInputStream) get_struct(l u32) voidptr {
+	return voidptr(nis.get_bytes(l).data)
 }
--- vlib/io/netoutstream.v
+++ vlib/io/netoutstream.v
@@ -1,4 +1,4 @@
-module stream
+module io
 
 import net
 import encoding.binary
@@ -15,15 +15,23 @@ pub fn new_net_output_stream(sock &net.Socket) &NetOutputStream {
 }
 
 pub fn (mut nos NetOutputStream) write_int(d int) ? {
-	nos.sock.send(byteptr(d), int(sizeof(int)))?
+	mut bytes := []byte{len: int(sizeof(int))}
+	binary.big_endian_put_u32(mut bytes, u32(d))
+	nos.sock.send(bytes.data, bytes.len)?
 }
 
 pub fn (mut nos NetOutputStream) write_ints(d []int) ? {
-	nos.sock.send(d.data, int(sizeof(int) * u32(d.len)))?
+	mut bytes := []byte{}
+	for u in d {
+		mut tmp := []byte{len: int(sizeof(int))}
+		binary.big_endian_put_u32(mut tmp, u32(u))
+		bytes << tmp
+	}
+	nos.sock.send(bytes.data, bytes.len)?
 }
 
 pub fn (mut nos NetOutputStream) write_i8(d i8) ? {
-	nos.sock.send(byteptr(d), int(sizeof(i8)))?
+	nos.sock.send([byte(d)].data, int(sizeof(i8)))?
 }
 
 pub fn (mut nos NetOutputStream) write_i8s(d []i8) ? {
@@ -31,23 +39,39 @@ pub fn (mut nos NetOutputStream) write_i8s(d []i8) ? {
 }
 
 pub fn (mut nos NetOutputStream) write_i16(d i16) ? {
-	nos.sock.send(byteptr(d), int(sizeof(i16)))?
+	mut bytes := []byte{len: int(sizeof(i16))}
+	binary.big_endian_put_u16(mut bytes, u16(d))
+	nos.sock.send(bytes.data, bytes.len)?
 }
 
 pub fn (mut nos NetOutputStream) write_i16s(d []i16) ? {
-	nos.sock.send(d.data, int(sizeof(i16) * u32(d.len)))?
+	mut bytes := []byte{}
+	for u in d {
+		mut tmp := []byte{len: int(sizeof(i16))}
+		binary.big_endian_put_u16(mut tmp, u16(u))
+		bytes << tmp
+	}
+	nos.sock.send(bytes.data, bytes.len)?
 }
 
 pub fn (mut nos NetOutputStream) write_i64(d i64) ? {
-	nos.sock.send(byteptr(d), int(sizeof(i64)))?
+	mut bytes := []byte{len: int(sizeof(i64))}
+	binary.big_endian_put_u64(mut bytes, u64(d))
+	nos.sock.send(bytes.data, bytes.len)?
 }
 
 pub fn (mut nos NetOutputStream) write_i64s(d []i64) ? {
-	nos.sock.send(d.data, int(sizeof(i64) * u32(d.len)))?
+	mut bytes := []byte{}
+	for u in d {
+		mut tmp := []byte{len: int(sizeof(i64))}
+		binary.big_endian_put_u64(mut tmp, u64(u))
+		bytes << tmp
+	}
+	nos.sock.send(bytes.data, bytes.len)?
 }
 
 pub fn (mut nos NetOutputStream) write_byte(d byte) ? {
-	nos.sock.send(byteptr(d), int(sizeof(byte)))?
+	nos.sock.send([d].data, int(sizeof(byte)))?
 }
 
 pub fn (mut nos NetOutputStream) write_bytes(d []byte) ? {
@@ -55,7 +79,9 @@ pub fn (mut nos NetOutputStream) write_bytes(d []byte) ? {
 }
 
 pub fn (mut nos NetOutputStream) write_u16(d u16) ? {
-	nos.sock.send(byteptr(d), int(sizeof(u16)))?
+	mut bytes := []byte{len: int(sizeof(u16))}
+	binary.big_endian_put_u16(mut bytes, d)
+	nos.sock.send(bytes.data, bytes.len)?
 }
 
 pub fn (mut nos NetOutputStream) write_u16s(d []u16) ? {
@@ -65,18 +91,13 @@ pub fn (mut nos NetOutputStream) write_u16s(d []u16) ? {
 		binary.big_endian_put_u16(mut tmp, u)
 		bytes << tmp
 	}
-	for b in bytes {
-		print(int(b))
-		print(' ')
-	}
-	println('')
-	println(bytes.len)
-	println(bytes.hex())
 	nos.sock.send(bytes.data, bytes.len)?
 }
 
 pub fn (mut nos NetOutputStream) write_u32(d u32) ? {
-	nos.sock.send(byteptr(d), int(sizeof(u32)))?
+	mut bytes := []byte{len: int(sizeof(u32))}
+	binary.big_endian_put_u32(mut bytes, d)
+	nos.sock.send(bytes.data, bytes.len)?
 }
 
 pub fn (mut nos NetOutputStream) write_u32s(d []u32) ? {
@@ -90,7 +111,9 @@ pub fn (mut nos NetOutputStream) write_u32s(d []u32) ? {
 }
 
 pub fn (mut nos NetOutputStream) write_u64(d u64) ? {
-	nos.sock.send(byteptr(d), int(sizeof(u64)))?
+	mut bytes := []byte{len: int(sizeof(u64))}
+	binary.big_endian_put_u64(mut bytes, d)
+	nos.sock.send(bytes.data, bytes.len)?
 }
 
 pub fn (mut nos NetOutputStream) write_u64s(d []u64) ? {
@@ -102,9 +125,13 @@ pub fn (mut nos NetOutputStream) write_u64s(d []u64) ? {
 	}
 	nos.sock.send(bytes.data, int(sizeof(u64) * u32(d.len)))?
 }
-
+/*
 pub fn (mut nos NetOutputStream) write_f32(d f32) ? {
-	nos.sock.send(byteptr(int(d)), int(sizeof(f32)))?
+	a := &d
+	mut bytes := []byte{len: int(sizeof(f32))}
+	binary.big_endian_put_u32(mut bytes, u32(d))
+	println(bytes.hex())
+	nos.sock.send(bytes.data, bytes.len)?
 }
 
 pub fn (mut nos NetOutputStream) write_f32s(d []f32) ? {
@@ -118,11 +145,13 @@ pub fn (mut nos NetOutputStream) write_f64(d f64) ? {
 pub fn (mut nos NetOutputStream) write_f64s(d []f64) ? {
 	nos.sock.send(d.data, int(sizeof(f64) * u32(d.len)))?
 }
+*/
 
 pub fn (mut nos NetOutputStream) write_string(d string) ? {
 	nos.write_bytes(d.bytes())?
 }
 
-pub fn (mut nos NetOutputStream) write_struct<T>(d T) ? {
-	nos.sock.send(byteptr(d), int(sizeof(T)))?
+// TODO make it generic
+pub fn (mut nos NetOutputStream) write_struct(d voidptr, l u32) ? {
+	nos.sock.send(byteptr(d), int(l))?
 }
