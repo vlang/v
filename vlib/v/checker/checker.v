4c03558d53d2fbaba4236c3db0cdd3fbf2bf6ee5
Nick Treleaven <n@trelsoft.com>
2020-08-16 11:54:24 +0100

    Error casting struct pointer to struct

--- vlib/v/checker/checker.v
+++ vlib/v/checker/checker.v
@@ -2333,7 +2333,7 @@ pub fn (mut c Checker) expr(node ast.Expr) table.Type {
 				type_name := c.table.type_to_str(node.expr_type)
 				c.error('cannot cast type `$type_name` to `byte`', node.pos)
 			} else if to_type_sym.kind == .struct_ && !node.typ.is_ptr() && !(to_type_sym.info as table.Struct).is_typedef {
-				if from_type_sym.kind == .struct_ {
+				if from_type_sym.kind == .struct_ && !node.expr_type.is_ptr() {
 					from_type_info := from_type_sym.info as table.Struct
 					to_type_info := to_type_sym.info as table.Struct
 					if !c.check_struct_signature(from_type_info, to_type_info) {
