baab9dddcf25825a4907a252b9e5a6922093b295
Lukas Neubert <lukas.neubert3@gmail.com>
2020-08-19 14:16:22 +0200

    run fmt

--- examples/cli.v
+++ examples/cli.v
@@ -5,45 +5,52 @@ import os
 
 fn main() {
 	mut cmd := cli.Command{
-		name: 'cli',
-		description: 'An example of the cli library.',
-		version: '1.0.0',
+		name: 'cli'
+		description: 'An example of the cli library.'
+		version: '1.0.0'
 	}
-
 	mut greet_cmd := cli.Command{
-		name: 'greet',
-		description: 'Prints greeting in different languages.',
-		pre_execute: greet_pre_func,
-		execute: greet_func,
-		post_execute: greet_post_func,
+		name: 'greet'
+		description: 'Prints greeting in different languages.'
+		pre_execute: greet_pre_func
+		execute: greet_func
+		post_execute: greet_post_func
 	}
 	greet_cmd.add_flag(cli.Flag{
-		flag: .string,
-		required: true,
-		name: 'language',
-		abbrev: 'l',
+		flag: .string
+		required: true
+		name: 'language'
+		abbrev: 'l'
 		description: 'Language of the message.'
 	})
 	greet_cmd.add_flag(cli.Flag{
-		flag: .int,
-		name: 'times',
-		value: '3',
+		flag: .int
+		name: 'times'
+		value: '3'
 		description: 'Number of times the message gets printed.'
 	})
-
 	cmd.add_command(greet_cmd)
 	cmd.parse(os.args)
 }
 
 fn greet_func(cmd cli.Command) {
-	language := cmd.flags.get_string('language') or { panic('Failed to get \'language\' flag: $err') }
-	times := cmd.flags.get_int('times') or { panic('Failed to get \'times\' flag: $err') }
-
-	for _ in 0..times {
+	language := cmd.flags.get_string('language') or {
+		panic("Failed to get \'language\' flag: $err")
+	}
+	times := cmd.flags.get_int('times') or {
+		panic("Failed to get \'times\' flag: $err")
+	}
+	for _ in 0 .. times {
 		match language {
-			'english' { println('Hello World') }
-			'german' { println('Hallo Welt') }
-			'dutch' { println('Hallo Wereld') }
+			'english' {
+				println('Hello World')
+			}
+			'german' {
+				println('Hallo Welt')
+			}
+			'dutch' {
+				println('Hallo Wereld')
+			}
 			else {
 				println('Unsupported language')
 				println('Supported are `englisch`, `german` and `dutch`.')
