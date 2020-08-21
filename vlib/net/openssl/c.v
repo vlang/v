b91724d3b0ed49609a940c55625353006de5a20d
helto4real <tomas.hellstrom@yahoo.se>
2020-08-21 19:46:57 +0200

    revert ssl flags

--- vlib/net/openssl/c.v
+++ vlib/net/openssl/c.v
@@ -6,9 +6,6 @@ module openssl
 // the next flag is harmless, since it will still use the
 // (older) system openssl.
 #flag linux -I/usr/local/include/openssl -L/usr/local/lib
-#flag darwin -I/usr/local/include/openssl -L/usr/local/lib
-
-#flag windows -l libssl -l libcrypto
 #flag -l ssl -l crypto
 // MacPorts
 #flag darwin -I/opt/local/include
