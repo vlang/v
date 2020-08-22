0bdb096bfa59b74230c6c0899e49f65eb568fe7a
Delyan Angelov <delian66@gmail.com>
2020-08-22 15:29:29 +0300

    x.net: use port 45123 in tcp_test.v, preventing sporadic `socket error 98` conflicts with websocket_test.v

--- vlib/x/net/tcp_test.v
+++ vlib/x/net/tcp_test.v
@@ -1,5 +1,9 @@
-import x.net as net
+import x.net
 import time
+
+const (
+	test_port = 45123
+)
 
 fn handle_conn(_c net.TcpConn) {
 	mut c := _c
@@ -8,12 +12,11 @@ fn handle_conn(_c net.TcpConn) {
 	c.set_read_timeout(10 * time.second)
 	c.set_write_timeout(10 * time.second)
 	for {
-		buf := []byte{ len: 100, init: 0 }
+		buf := []byte{len: 100, init: 0}
 		read := c.read_into(mut buf) or {
 			println('Server: connection dropped')
 			return
 		}
-
 		c.write(buf[..read]) or {
 			println('Server: connection dropped')
 			return
@@ -23,51 +26,44 @@ fn handle_conn(_c net.TcpConn) {
 
 fn echo_server(l net.TcpListener) ? {
 	for {
-		new_conn := l.accept() or { continue }
+		new_conn := l.accept() or {
+			continue
+		}
 		go handle_conn(new_conn)
 	}
-
 	return none
 }
 
 fn echo() ? {
-	mut c := net.dial_tcp('127.0.0.1:30000')?
-	defer { c.close() or {} }
-	
+	mut c := net.dial_tcp('127.0.0.1:$test_port')?
+	defer {
+		c.close() or { }
+	}
 	// arbitrary timeouts to ensure that it doesnt
 	// instantly throw its hands in the air and give up
 	c.set_read_timeout(10 * time.second)
 	c.set_write_timeout(10 * time.second)
-
 	data := 'Hello from vlib/net!'
 	c.write_string(data)?
-
-	buf := []byte{ len: 100, init: 0 }
+	buf := []byte{len: 100, init: 0}
 	read := c.read_into(mut buf)?
-
 	assert read == data.len
-
 	for i := 0; i < read; i++ {
 		assert buf[i] == data[i]
 	}
-
-	println('Got "${buf.bytestr()}"')
-
+	println('Got "$buf.bytestr()"')
 	return none
 }
 
 fn test_tcp() {
-	l := net.listen_tcp(30000) or {
+	l := net.listen_tcp(test_port) or {
 		panic(err)
 	}
-
 	go echo_server(l)
 	echo() or {
 		panic(err)
 	}
-
-	l.close() or {
-	}
+	l.close() or { }
 }
 
 fn main() {
