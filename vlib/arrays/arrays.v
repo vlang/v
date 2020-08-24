f5c1db59077c9420bcede1cbc303852c4d40da29
Maciej Obarski <mobarski@gmail.com>
2020-08-24 23:42:48 +0200

    arrays: new module

---
+++ vlib/arrays/arrays.v
@@ -0,0 +1,91 @@
+module arrays
+import rand
+
+// min returns the minimum
+[direct_array_access]
+pub fn min<T>(a []T) T {
+	if a.len==0 { panic('.min called on an empty array') } // TODO
+	mut val := a[0]
+	for i in 0..a.len {
+		if a[i] < val {
+			val = a[i]
+		}
+	}
+	return val
+}
+
+// max returns the maximum
+[direct_array_access]
+pub fn max<T>(a []T) T {
+	if a.len==0 { panic('.max called on an empty array') } // TODO
+	mut val := a[0]
+	for i in 0..a.len {
+		if a[i] > val {
+			val = a[i]
+		}
+	}
+	return val
+}
+
+// argmax returns the index of the first minimum
+[direct_array_access]
+pub fn argmin<T>(a []T) int {
+	if a.len==0 { panic('.argmin called on an empty array') } // TODO
+	mut idx := 0
+	mut val := a[0]
+	for i in 0..a.len {
+		if a[i] < val {
+			val = a[i]
+			idx = i
+		}
+	}
+	return idx
+}
+
+// argmax returns the index of the first maximum
+[direct_array_access]
+pub fn argmax<T>(a []T) int {
+	if a.len==0 { panic('.argmax called on an empty array') } // TODO
+	mut idx := 0
+	mut val := a[0]
+	for i in 0..a.len {
+		if a[i] > val {
+			val = a[i]
+			idx = i
+		}
+	}
+	return idx
+}
+
+// shuffle randomizes the items of an array in place
+[direct_array_access]
+pub fn shuffle<T>(mut a []T) {
+	for i in 0..a.len-1 {
+		x := rand.int_in_range(i,a.len)
+		if i != x {
+			a[i],a[x] = a[x],a[i] // swap
+		}
+	}
+}
+
+// all checks if all array items are equal to given value
+[direct_array_access]
+pub fn all<T>(a []T, value T) bool {
+	for i in 0..a.len {
+		if a[i] != value { return false }
+	}
+	return true
+}
+
+
+/*
+// replace values given in old_value with new_value
+[direct_array_access]
+pub fn replace<T>(mut a[]T, old_value T, new_value T) {
+	for i in 0..a.len {
+		if a[i] == old_value {
+			a[i] = new_value
+		}
+	}
+}
+*/
---
+++ vlib/arrays/arrays_test.v
@@ -0,0 +1,92 @@
+module arrays
+
+
+fn test_min() {
+	a := [8, 2, 6, 4]
+	assert min<int>(a)==2
+	
+	b := [f32(5.1), 3.1, 1.1, 9.1]
+	assert min<f32>(b) == f32(1.1)
+	
+	c := [byte(4), 9, 3, 1]
+	assert min<byte>(c) == byte(1)
+}
+
+
+fn test_max() {
+	a := [8, 2, 6, 4]
+	assert max<int>(a)==8
+	
+	b := [f32(5.1), 3.1, 1.1, 9.1]
+	assert max<f32>(b) == f32(9.1)
+	
+	c := [byte(4), 9, 3, 1]
+	assert max<byte>(c) == byte(9)
+}
+
+
+fn test_argmin() {
+	a := [8, 2, 6, 4]
+	assert argmin<int>(a)==1
+	
+	b := [f32(5.1), 3.1, 1.1, 9.1]
+	assert argmin<f32>(b) == 2
+	
+	c := [byte(4), 9, 3, 1]
+	assert argmin<byte>(c) == 3
+}
+
+
+fn test_argmax() {
+	a := [8, 2, 6, 4]
+	assert argmax<int>(a)==0
+	
+	b := [f32(5.1), 3.1, 1.1, 9.1]
+	assert argmax<f32>(b) == 3
+	
+	c := [byte(4), 9, 3, 1]
+	assert argmax<byte>(c) == 1
+}
+
+
+fn test_shuffle() {
+	a := [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+	mut b := a.clone()
+	mut c := a.clone()
+	shuffle<int>(mut b)
+	shuffle<int>(mut c)
+	assert a != b // false negative chance: 1/20!
+	assert a != c // false negative chance: 1/20!
+	assert b != c // false negative chance: 1/20!
+	mut d := a.clone()
+	shuffle<int>(mut d[..15])
+	assert d[..15] != a[..15] // false negative chance: 1/15!
+	assert d[15..] == a[15..]
+}
+
+
+fn test_all() {
+	a := [1,1,1,1]
+	assert all<int>(a, 1) == true
+	assert all<int>(a, 2) == false
+	b := [1,1,2,2]
+	assert all<int>(b, 1) == false
+	assert all<int>(b, 2) == false
+	c := [f32(1.1), 1.1, 1.1]
+	assert all<f32>(c, 1.1) == true
+	assert all<f32>(c, 1.2) == false
+	d := [f32(1.1), 1.1, 2.2]
+	assert all<f32>(d, 1.1) == false
+	assert all<f32>(d, 2.2) == false
+}
+
+
+/*
+fn test_replace() {
+	mut a := [1,1,2,2,3,3]
+	replace<int>(mut a, 2, 1)
+	assert a == [1,1,1,1,3,3]
+	replace<int>(mut a, 0, 1)
+	assert a == [1,1,1,1,3,3]
+}
+*/
