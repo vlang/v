60abb7ffa3ceec0f161e091ae36967f65965ef3c
Nick Treleaven <n@trelsoft.com>
2020-08-24 18:34:47 +0100

    Also ignore vlib/regex postfix warnings

--- vlib/v/parser/pratt.v
+++ vlib/v/parser/pratt.v
@@ -262,7 +262,7 @@ pub fn (mut p Parser) expr(precedence int) ast.Expr {
 		} else if p.tok.kind in [.inc, .dec] {
 			// Postfix
 			if p.peek_tok.kind in [.comma, .rpar, .rsbr] &&
-				p.mod !in ['builtin', 'strconv'] { // temp
+				p.mod !in ['builtin', 'regex', 'strconv'] { // temp
 				p.warn_with_pos('`$p.tok.kind` operator can only be used as a statement', p.peek_tok.position())
 			}
 			node = ast.PostfixExpr{
