4e9c2c1adf6f893ff42551910336a47761733711
Uwe Krüger <45282134+UweKrueger@users.noreply.github.com>
2020-09-11 23:45:00 +0200

    add testcases for `try_pop()` and `try_push()`

---
+++ vlib/sync/channel_try_buf_test.v
@@ -0,0 +1,17 @@
+fn test_channel_try_buffered() {
+	ch := chan int{cap: 5}
+	for z in 2..13 {
+		if ch.try_push(z) == .not_ready {
+			assert z == 7
+			break
+		}
+	}
+	mut obj := int(0)
+	for ch.try_pop(obj) == .success {
+		println(obj)
+	}
+	assert obj == 6
+	ch <- 17
+	obj = <-ch
+	assert obj == 17
+}
