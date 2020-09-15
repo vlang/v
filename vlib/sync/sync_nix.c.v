fa718a23f61fbd48b574f85614b13c5b4735e511
spaceface777 <spaceface7777@gmail.com>
2020-09-15 20:01:27 +0200

    bring back `dispatch.h` on macos

--- vlib/sync/sync_nix.c.v
+++ vlib/sync/sync_nix.c.v
@@ -6,6 +6,9 @@ module sync
 import time
 
 #flag -lpthread
+$if macos {
+	#include <dispatch/dispatch.h>
+}
 #include <semaphore.h>
 
 // [init_with=new_mutex] // TODO: implement support for this struct attribute, and disallow Mutex{} from outside the sync.new_mutex() function.
