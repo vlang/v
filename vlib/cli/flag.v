3ef00eea63f6de94eb6efbe43f5fab26dd891c39
Lukas Neubert <serkonda@protonmail.com>
2020-09-16 22:01:44 +0200

    cli: improvements to help and error messages (#6375)

--- vlib/cli/flag.v
+++ vlib/cli/flag.v
@@ -26,13 +26,13 @@ pub fn (flags []Flag) get_all_found() []Flag {
 
 pub fn (flag Flag) get_bool() ?bool {
 	if flag.flag != .bool {
-		return error('Invalid flag type')
+		return error('$flag.name: Invalid flag type `$flag.flag`, expected `bool`')
 	}
 	return flag.value == 'true'
 }
 
 pub fn (flags []Flag) get_bool(name string) ?bool {
-	flag := flags.get(name)?
+	flag := flags.get(name) ?
 	return flag.get_bool()
 }
 
@@ -45,13 +45,13 @@ pub fn (flags []Flag) get_bool_or(name string, or_value bool) bool {
 
 pub fn (flag Flag) get_int() ?int {
 	if flag.flag != .int {
-		return error('Invalid flag type')
+		return error('$flag.name: Invalid flag type `$flag.flag`, expected `int`')
 	}
 	return flag.value.int()
 }
 
 pub fn (flags []Flag) get_int(name string) ?int {
-	flag := flags.get(name)?
+	flag := flags.get(name) ?
 	return flag.get_int()
 }
 
@@ -64,13 +64,13 @@ pub fn (flags []Flag) get_int_or(name string, or_value int) int {
 
 pub fn (flag Flag) get_float() ?f64 {
 	if flag.flag != .float {
-		return error('Invalid flag type')
+		return error('$flag.name: Invalid flag type `$flag.flag`, expected `float`')
 	}
 	return flag.value.f64()
 }
 
 pub fn (flags []Flag) get_float(name string) ?f64 {
-	flag := flags.get(name)?
+	flag := flags.get(name) ?
 	return flag.get_float()
 }
 
@@ -83,13 +83,13 @@ pub fn (flags []Flag) get_float_or(name string, or_value f64) f64 {
 
 pub fn (flag Flag) get_string() ?string {
 	if flag.flag != .string {
-		return error('Invalid flag type')
+		return error('$flag.name: Invalid flag type `$flag.flag`, expected `string`')
 	}
 	return flag.value
 }
 
 pub fn (flags []Flag) get_string(name string) ?string {
-	flag := flags.get(name)?
+	flag := flags.get(name) ?
 	return flag.get_string()
 }
 
@@ -104,10 +104,10 @@ pub fn (flags []Flag) get_string_or(name, or_value string) string {
 fn (mut flag Flag) parse(args []string, with_abbrev bool) ?[]string {
 	if flag.matches(args, with_abbrev) {
 		if flag.flag == .bool {
-			new_args := flag.parse_bool(args)?
+			new_args := flag.parse_bool(args) ?
 			return new_args
 		} else {
-			new_args := flag.parse_raw(args)?
+			new_args := flag.parse_raw(args) ?
 			return new_args
 		}
 	} else {
@@ -159,7 +159,7 @@ fn (flags []Flag) get(name string) ?Flag {
 			return flag
 		}
 	}
-	return error('Flag `$name` not found')
+	return error('Flag `$name` not found in $flags')
 }
 
 fn (flags []Flag) contains(name string) bool {
