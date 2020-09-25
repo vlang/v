b999d01de7ac47e7440352f9c5f5c6a71efe6c33
Delyan Angelov <delian66@gmail.com>
2020-09-25 22:02:29 +0300

    runtime: use GetCurrentProcessorNumber for runtime.nr_cpus()

--- vlib/runtime/runtime_windows.c.v
+++ vlib/runtime/runtime_windows.c.v
@@ -2,8 +2,16 @@ module runtime
 
 import os
 
+[typedef]
+struct C.SYSTEM_INFO {
+    dwNumberOfProcessors u32
+}
+fn C.GetSystemInfo(&C.SYSTEM_INFO)
+
 pub fn nr_cpus() int {
-	mut nr := int(C.GetCurrentProcessorNumber())
+	sinfo := C.SYSTEM_INFO{}
+	C.GetSystemInfo(&sinfo)
+	mut nr := int(sinfo.dwNumberOfProcessors)
 	if nr == 0 {
 		nr = os.getenv('NUMBER_OF_PROCESSORS').int()
 	}
