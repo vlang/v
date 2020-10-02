c2b1f676a664c7a525baadce8318effb6ed15707
kalapalo <kalapal0@protonmail.ch>
2020-10-02 08:55:06 -0400

    cgen: fix as cast when sumtype is function result

--- vlib/v/gen/cgen.v
+++ vlib/v/gen/cgen.v
@@ -5044,11 +5044,13 @@ fn (mut g Gen) as_cast(node ast.AsCast) {
 		g.write('.obj')
 		*/
 		dot := if node.expr_type.is_ptr() { '->' } else { '.' }
-		g.write('/* as */ ($styp*)__as_cast(')
+		g.write('/* as */ ($styp*)__as_cast((')
 		g.expr(node.expr)
+		g.write(')')
 		g.write(dot)
-		g.write('_object, ')
+		g.write('_object, (')
 		g.expr(node.expr)
+		g.write(')')
 		g.write(dot)
 		g.write('typ, /*expected:*/$node.typ)')
 	}
