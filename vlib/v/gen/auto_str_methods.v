a98a972d7c7d87adf24d134540d27294fe27cf85
danieldaeschle <daniel.daeschle@gmail.com>
2020-10-02 09:03:51 +0200

    gen: fix sumtype custom str

--- vlib/v/gen/auto_str_methods.v
+++ vlib/v/gen/auto_str_methods.v
@@ -297,7 +297,7 @@ fn (mut g Gen) gen_str_for_struct(info table.Struct, styp, str_fn_name string) {
 	for field in info.fields {
 		sym := g.table.get_type_symbol(field.typ)
 		if !sym.has_method('str') {
-			mut typ := field.typ 
+			mut typ := field.typ
 			if typ.is_ptr() {
 				typ = typ.deref()
 			}
@@ -383,15 +383,12 @@ fn struct_auto_str_func(sym table.TypeSymbol, field_type table.Type, fn_name, fi
 		}
 		if has_custom_str {
 			return '${fn_name}($obj)'
-		} else {
-			return 'indent_${fn_name}($obj, indent_count + 1)'
 		}
-	} else if sym.kind in [.array, .array_fixed, .map] {
+		return 'indent_${fn_name}($obj, indent_count + 1)'
+	} else if sym.kind in [.array, .array_fixed, .map, .sum_type] {
 		if has_custom_str {
 			return '${fn_name}(it->${c_name(field_name)})'
 		}
-		return 'indent_${fn_name}(it->${c_name(field_name)}, indent_count + 1)'
-	} else if sym.kind == .sum_type {
 		return 'indent_${fn_name}(it->${c_name(field_name)}, indent_count + 1)'
 	} else {
 		mut method_str := 'it->${c_name(field_name)}'
