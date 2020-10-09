e556e69938445f29d0f5bef7b807fbcb491c5856
rvajustin <rvajustin82@gmail.com>
2020-10-09 10:07:56 -0400

    feat: add http status codes

---
+++ vlib/net/http/status_test.v
@@ -0,0 +1,49 @@
+module http
+
+fn test_str() {
+	code := Status.bad_gateway
+	actual := code.str()
+	assert actual == 'Bad Gateway'
+}
+
+fn test_int() {
+	code := Status.see_other
+	actual := code.int()
+	assert actual == 303
+}
+
+fn is_valid_positive() {
+	code := Status.unassigned
+	actual := code.is_valid()
+	assert !actual
+}
+
+fn is_valid_negative() {
+	code := Status.gateway_timeout
+	actual := code.is_valid()
+	assert actual
+}
+
+fn is_error_positive() {
+	code := Status.too_many_requests
+	actual := code.is_valid()
+	assert !actual
+}
+
+fn is_error_negative() {
+	code := Status.cont
+	actual := code.is_valid()
+	assert actual
+}
+
+fn is_success_positive() {
+	code := Status.accepted
+	actual := code.is_valid()
+	assert actual
+}
+
+fn is_success_negative() {
+	code := Status.forbidden
+	actual := code.is_valid()
+	assert !actual
+}
