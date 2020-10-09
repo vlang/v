74b9ce8dc38a6c2641ad2cd1d627d783b27e88fb
rvajustin <rvajustin82@gmail.com>
2020-10-09 10:46:43 -0400

    chore: added integer mapping to status enum

--- vlib/net/http/status.v
+++ vlib/net/http/status.v
@@ -7,73 +7,73 @@ module http
 // available at:
 // https://www.iana.org/assignments/http-status-codes/http-status-codes.xhtml
 pub enum Status {
-	unknown
-	unassigned
-	cont
-	switching_protocols
-	processing
-	checkpoint_draft
-	ok
-	created
-	accepted
-	non_authoritative_information
-	no_content
-	reset_content
-	partial_content
-	multi_status
-	already_reported
-	im_used
-	multiple_choices
-	moved_permanently
-	found
-	see_other
-	not_modified
-	use_proxy
-	switch_proxy
-	temporary_redirect
-	permanent_redirect
-	bad_request
-	unauthorized
-	payment_required
-	forbidden
-	not_found
-	method_not_allowed
-	not_acceptable
-	proxy_authentication_required
-	request_timeout
-	conflict
-	gone
-	length_required
-	precondition_failed
-	request_entity_too_large
-	request_uri_too_long
-	unsupported_media_type
-	requested_range_not_satisfiable
-	expectation_failed
-	im_a_teapot
-	misdirected_request
-	unprocessable_entity
-	locked
-	failed_dependency
-	unordered_collection
-	upgrade_required
-	precondition_required
-	too_many_requests
-	request_header_fields_too_large
-	unavailable_for_legal_reasons
-	client_closed_request
-	internal_server_error
-	not_implemented
-	bad_gateway
-	service_unavailable
-	gateway_timeout
-	http_version_not_supported
-	variant_also_negotiates
-	insufficient_storage
-	loop_detected
-	bandwidth_limit_exceeded
-	not_extended
-	network_authentication_required
+	unknown = -1
+	unassigned = 0
+	cont = 100
+	switching_protocols = 101
+	processing = 102
+	checkpoint_draft = 103
+	request_uri_too_long = 122
+	ok = 200
+	created = 201
+	accepted = 202
+	non_authoritative_information = 203
+	no_content = 204
+	reset_content = 205
+	partial_content = 206
+	multi_status = 207
+	already_reported = 208
+	im_used = 226
+	multiple_choices = 300
+	moved_permanently = 301
+	found = 302
+	see_other = 303
+	not_modified = 304
+	use_proxy = 305
+	switch_proxy = 306
+	temporary_redirect = 307
+	permanent_redirect = 308
+	bad_request = 400
+	unauthorized = 401
+	payment_required = 402
+	forbidden = 403
+	not_found = 404
+	method_not_allowed = 405
+	not_acceptable = 406
+	proxy_authentication_required = 407
+	request_timeout = 408
+	conflict = 409
+	gone = 410
+	length_required = 411
+	precondition_failed = 412
+	request_entity_too_large = 413
+	request_uri_too_long = 414
+	unsupported_media_type = 415
+	requested_range_not_satisfiable = 416
+	expectation_failed = 417
+	im_a_teapot = 418
+	unprocessable_entity = 422
+	locked = 423
+	failed_dependency = 424
+	unordered_collection = 425
+	upgrade_required = 426
+	precondition_required = 428
+	too_many_requests = 429
+	request_header_fields_too_large = 431
+	unavailable_for_legal_reasons = 451
+	client_closed_request = 499
+	internal_server_error = 500
+	not_implemented = 501
+	bad_gateway = 502
+	service_unavailable = 503
+	gateway_timeout = 504
+	http_version_not_supported = 505
+	variant_also_negotiates = 506
+	insufficient_storage = 507
+	loop_detected = 508
+	bandwidth_limit_exceeded = 509
+	not_extended = 510
+	network_authentication_required = 511
 }
 
 pub fn status_from_int(code int) Status {
@@ -228,73 +228,8 @@ pub fn (code Status) str() string {
 // int converts an assigned and known Status to its integral equivalent.
 // if a Status is unknown or unassigned, this method will return zero
 pub fn (code Status) int() int {
-	return match code {
-		.cont { 100 }
-		.switching_protocols { 101 }
-		.processing { 102 }
-		.checkpoint_draft { 103 }
-		.ok { 200 }
-		.created { 201 }
-		.accepted { 202 }
-		.non_authoritative_information { 203 }
-		.no_content { 204 }
-		.reset_content { 205 }
-		.partial_content { 206 }
-		.multi_status { 207 }
-		.already_reported { 208 }
-		.im_used { 226 }
-		.multiple_choices { 300 }
-		.moved_permanently { 301 }
-		.found { 302 }
-		.see_other { 303 }
-		.not_modified { 304 }
-		.use_proxy { 305 }
-		.switch_proxy { 306 }
-		.temporary_redirect { 307 }
-		.permanent_redirect { 308 }
-		.bad_request { 400 }
-		.unauthorized { 401 }
-		.payment_required { 402 }
-		.forbidden { 403 }
-		.not_found { 404 }
-		.method_not_allowed { 405 }
-		.not_acceptable { 406 }
-		.proxy_authentication_required { 407 }
-		.request_timeout { 408 }
-		.conflict { 409 }
-		.gone { 410 }
-		.length_required { 411 }
-		.precondition_failed { 412 }
-		.request_entity_too_large { 413 }
-		.request_uri_too_long { 414 }
-		.unsupported_media_type { 415 }
-		.requested_range_not_satisfiable { 416 }
-		.expectation_failed { 417 }
-		.im_a_teapot { 418 }
-		.unprocessable_entity { 422 }
-		.locked { 423 }
-		.failed_dependency { 424 }
-		.unordered_collection { 425 }
-		.upgrade_required { 426 }
-		.precondition_required { 428 }
-		.too_many_requests { 429 }
-		.request_header_fields_too_large { 431 }
-		.unavailable_for_legal_reasons { 451 }
-		.client_closed_request { 499 }
-		.internal_server_error { 500 }
-		.not_implemented { 501 }
-		.bad_gateway { 502 }
-		.service_unavailable { 503 }
-		.gateway_timeout { 504 }
-		.http_version_not_supported { 505 }
-		.variant_also_negotiates { 506 }
-		.insufficient_storage { 507 }
-		.loop_detected { 508 }
-		.bandwidth_limit_exceeded { 509 }
-		.not_extended { 510 }
-		.network_authentication_required { 511 }
-		else { 0 }
-	}
+	if (code in [.unknown, .unassigned]) { return 0 }
+	return int(code)
 }
 
 // is_valid returns true if the status code is assigned and known
