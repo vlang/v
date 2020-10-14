46f7e99d3c87669a3c06acc149f0e82d2b1b4ca8
Delyan Angelov <delian66@gmail.com>
2020-10-14 17:21:43 +0300

    crypto.hmac: implement hmac.equal/2 too

--- vlib/crypto/hmac/hmac.v
+++ vlib/crypto/hmac/hmac.v
@@ -2,6 +2,8 @@
 // implementation based on https://tools.ietf.org/html/rfc2104
 module hmac
 
+import crypto.internal.subtle
+
 const (
 	ipad = []byte{len: 256, init: 0x36} // TODO is 256 enough??
 	opad = []byte{len: 256, init: 0x5C}
@@ -32,4 +34,11 @@ pub fn new(key, data []byte, hash_func fn (bytes []byte) []byte, blocksize int) 
 	outer << inner_hash
 	digest := hash_func(outer)
 	return digest
+}
+
+// equal compares 2 MACs for equality, without leaking timing info
+// NB: if the lengths of the 2 MACs are different, probably a completely different
+// hash function was used to generate them => no useful timing information.
+pub fn equal(mac1, mac2 []byte) bool {
+	return subtle.constant_time_compare(mac1, mac2) == 1
 }
