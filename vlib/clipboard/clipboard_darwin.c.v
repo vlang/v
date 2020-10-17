909725df000a6ceb232aeddcf2a0c27a0df57291
Lukas Neubert <serkonda@protonmail.com>
2020-10-17 17:47:32 +0200

    clipboard_darwin: fix usage of # as comment

--- vlib/clipboard/clipboard_darwin.c.v
+++ vlib/clipboard/clipboard_darwin.c.v
@@ -14,7 +14,7 @@ mut:
 
 fn new_clipboard() &Clipboard{
 	pb := voidptr(0)
-	#pb = [NSPasteboard generalPasteboard];
+	// pb = [NSPasteboard generalPasteboard];
 	cb := &Clipboard{
 		pb: pb
 	}
@@ -27,17 +27,17 @@ fn (cb &Clipboard) check_availability() bool {
 
 fn (mut cb Clipboard) clear(){
 	cb.foo = 0
-	#[cb->pb clearContents];
+	// [cb->pb clearContents];
 }
 
 fn (mut cb Clipboard) free(){
 	cb.foo = 0
-	//nothing to free
+	// nothing to free
 }
 
 fn (cb &Clipboard) has_ownership() bool {
 	if cb.last_cb_serial == 0 {return false}
-	#return [cb->pb changeCount] == cb->last_cb_serial;
+	// return [cb->pb changeCount] == cb->last_cb_serial;
 	return false
 }
 
@@ -45,31 +45,31 @@ fn C.OSAtomicCompareAndSwapLong()
 
 fn (mut cb Clipboard) set_text(text string) bool {
 	cb.foo = 0
-	#NSString *ns_clip;
+	// NSString *ns_clip;
 	ret := false
 
-	#ns_clip = [[ NSString alloc ] initWithBytesNoCopy:text.str length:text.len encoding:NSUTF8StringEncoding freeWhenDone: false];
-	#[cb->pb declareTypes:[NSArray arrayWithObject:NSStringPboardType] owner:nil];
-	#ret = [cb->pb setString:ns_clip forType:NSStringPboardType];
-	#[ns_clip release];
+	// ns_clip = [[ NSString alloc ] initWithBytesNoCopy:text.str length:text.len encoding:NSUTF8StringEncoding freeWhenDone: false];
+	// [cb->pb declareTypes:[NSArray arrayWithObject:NSStringPboardType] owner:nil];
+	// ret = [cb->pb setString:ns_clip forType:NSStringPboardType];
+	// [ns_clip release];
 
 	mut serial := 0
-	#serial = [cb->pb changeCount];
+	// serial = [cb->pb changeCount];
 	C.OSAtomicCompareAndSwapLong(cb.last_cb_serial, serial, &cb.last_cb_serial)
 	return ret
 }
 
 fn (mut cb Clipboard) get_text() string {
 	cb.foo = 0
-	#NSString *ns_clip;
+	// NSString *ns_clip;
 	utf8_clip := byteptr(0)
 
-	#ns_clip = [cb->pb stringForType:NSStringPboardType]; //NSPasteboardTypeString
-	#if (ns_clip == nil) {
-	#	return tos3(""); //in case clipboard is empty
-	#}
+	// ns_clip = [cb->pb stringForType:NSStringPboardType]; // NSPasteboardTypeString
+	// if (ns_clip == nil) {
+		// return tos3(""); // in case clipboard is empty
+	// }
 
-	#utf8_clip = [ns_clip UTF8String];
+	// utf8_clip = [ns_clip UTF8String];
 	return unsafe { utf8_clip.vstring() }
 }
 
