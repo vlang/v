873d4f2fd08b1bd26b96d42f17b6bbfec872bf03
Delyan Angelov <delian66@gmail.com>
2020-10-18 19:00:32 +0300

    tests: implement compact progress mode (the old one is available with -expand)

--- vlib/v/pref/pref.v
+++ vlib/v/pref/pref.v
@@ -155,6 +155,9 @@ pub fn parse_args(args []string) (&Preferences, string) {
 					command_pos = i
 				}
 			}
+			'-expand' {
+				// processed by testing tools
+			}
 			'-silent' {
 				res.output_mode = .silent
 			}
