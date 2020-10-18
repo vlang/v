347496de0dc27173b2743b061f5963ed0e45206c
Alexander Medvednikov <alexander@vlang.io>
2020-10-18 21:22:37 +0200

    checker: handle bad `$if` values

--- vlib/gg/gg.v
+++ vlib/gg/gg.v
@@ -308,7 +308,10 @@ pub fn (ctx &Context) draw_circle_line(x f32, y f32, r int, segments int, c gx.C
 	sgl.end()
 }
 
-pub fn (ctx &Context) draw_circle(x f32, y f32, r int, segments int, c gx.Color) {
+pub fn (ctx &Context) draw_circle(x f32, y f32, r int, c gx.Color) {
+	ctx.draw_circle_with_segments(x,y,r,10, c)
+}
+pub fn (ctx &Context) draw_circle_with_segments(x f32, y f32, r int, segments int, c gx.Color) {
 	if c.a != 255 {
 		sgl.load_pipeline(ctx.timage_pip)
 	}
