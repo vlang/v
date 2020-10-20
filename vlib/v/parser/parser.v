7a1488a6b63caa8b43f055e534d86b3a8f8faefc
lmp <dev.larpon@gmail.com>
2020-10-20 15:12:16 +0200

    parser: avoid duplicate module names in table.imports and ast_imports

--- vlib/v/parser/parser.v
+++ vlib/v/parser/parser.v
@@ -1523,10 +1523,10 @@ fn (mut p Parser) import_stmt() ast.Import {
 		}
 	}
 	p.imports[mod_alias] = mod_name
-	// if mod_name !in p.table.imports {
-	p.table.imports << mod_name
-	p.ast_imports << node
-	// }
+	if mod_name !in p.table.imports {
+		p.table.imports << mod_name
+		p.ast_imports << node
+	}
 	return node
 }
 
