5497dc6585d9d93c5a83f3b21085137390c21360
Lukas Neubert <serkonda@protonmail.com>
2020-10-20 10:48:41 +0200

    check-md: improve warning messages

--- cmd/tools/check-md.v
+++ cmd/tools/check-md.v
@@ -13,15 +13,21 @@ fn main() {
 	for file_path in files_paths {
 		real_path := os.real_path(file_path)
 		lines := os.read_lines(real_path) or {
+			println('"$file_path" does not exist')
+			warnings++
 			continue
 		}
 		for i, line in lines {
 			if line.len > too_long_line_length {
-				if line.starts_with('|') || line.contains('https') {
-					println('$file_path:${i + 1}:${line.len + 1}: warning')
+				linetrace_msg := '$file_path:${i + 1}:${line.len + 1}: '
+				if line.starts_with('|') {
+					println(linetrace_msg + 'long table (warn)')
+					warnings++
+				} else if line.contains('https') {
+					println(linetrace_msg + 'long link (warn)')
 					warnings++
 				} else {
-					eprintln('$file_path:${i + 1}:${line.len + 1}: line too long')
+					eprintln(linetrace_msg + 'line too long')
 					errors++
 				}
 			}
