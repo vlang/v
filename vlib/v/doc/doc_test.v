8ecb70a350d98bfde9b3cbca1eeb542ae794356b
Ned Palacios <npdoesmc@gmail.com>
2020-10-21 15:52:45 +0800

    vdoc: fmt doc_test

--- vlib/v/doc/doc_test.v
+++ vlib/v/doc/doc_test.v
@@ -5,7 +5,6 @@ import v.parser
 // fn test_generate_with_pos() {}
 // fn test_generate() {}
 // fn test_generate_from_ast() {}
-
 fn test_generate_from_mod() {
 	nested_mod_name := 'net.http.chunked'
 	nested_mod_doc := doc.generate_from_mod(nested_mod_name, false, true) or {
@@ -13,7 +12,6 @@ fn test_generate_from_mod() {
 		assert false
 		doc.Doc{}
 	}
-
 	assert nested_mod_doc.head.name == nested_mod_name
 	assert nested_mod_doc.head.content == 'module $nested_mod_name'
 	assert nested_mod_doc.contents.len == 3
