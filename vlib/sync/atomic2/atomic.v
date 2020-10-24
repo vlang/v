89765305a377741b494ba73551163d7fafa226d9
Larpon <Larpon@users.noreply.github.com>
2020-10-24 12:49:57 +0200

    tcc: fix 'tcc: error: library `atomic` not found' on older Ubuntu distros (#6672)

--- vlib/sync/atomic2/atomic.v
+++ vlib/sync/atomic2/atomic.v
@@ -20,7 +20,7 @@ further tested.
 $if linux {
 	$if tinyc {
 		// most Linux distributions have /usr/lib/libatomic.so, but Ubuntu uses gcc version specific dir
-		#flag -L/usr/lib/gcc/x86_64-linux-gnu/8 -L/usr/lib/gcc/x86_64-linux-gnu/9 -latomic
+		#flag -L/usr/lib/gcc/x86_64-linux-gnu/6 -L/usr/lib/gcc/x86_64-linux-gnu/7 -L/usr/lib/gcc/x86_64-linux-gnu/8 -L/usr/lib/gcc/x86_64-linux-gnu/9 -latomic
 	}
 }
 
