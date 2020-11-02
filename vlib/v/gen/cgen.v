ec510ce0710bdff9b7e3d1955781cc1dffe4898c
Nick Treleaven <n@trelsoft.com>
2020-11-02 21:59:48 +0000

    cgen: fix typeof(expr).name for generic type, pointers, etc (#6712)

--- vlib/v/gen/cgen.v
+++ vlib/v/gen/cgen.v
@@ -2386,17 +2386,19 @@ fn (mut g Gen) expr(node ast.Expr) {
 
 // typeof(expr).name
 fn (mut g Gen) typeof_name(node ast.TypeOf) {
-	sym := g.table.get_type_symbol(node.expr_type)
-	if sym.kind == .array {
-		info := sym.info as table.Array
-		mut s := g.table.get_type_name(info.elem_type)
-		s = util.strip_main_name(s)
-		g.write('tos_lit("[]$s")')
-	} else if sym.kind == .sum_type {
-		// new typeof() must be known at compile-time
-		g.write('tos_lit("${util.strip_main_name(sym.name)}")')
-	} else {
+	mut typ := node.expr_type
+	if typ.has_flag(.generic) {
+		typ = g.cur_generic_type
+	}
+	sym := g.table.get_type_symbol(typ)
+	// TODO: fix table.type_to_str and use instead
+	if sym.kind == .function {
 		g.typeof_expr(node)
+	} else {
+		// Note: typeof() must be known at compile-time
+		// sum types should not be handled dynamically
+		s := g.table.type_to_str(typ)
+		g.write('tos_lit("${util.strip_main_name(s)}")')
 	}
 }
 
