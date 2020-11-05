d34c5b767b924121b8430d821fb8a7f93a6b60ab
Delyan Angelov <delian66@gmail.com>
2020-11-05 20:15:26 +0200

    vfmt: keep the source fn type alias names, instead of the expanded fn declarations

--- vlib/v/table/atypes.v
+++ vlib/v/table/atypes.v
@@ -839,10 +839,12 @@ pub fn (table &Table) type_to_str(t Type) string {
 			}
 		}
 		.function {
-			info := sym.info as FnType
-			res = table.fn_signature(info.func, {
-				type_only: true
-			})
+			if !table.is_fmt {
+				info := sym.info as FnType
+				res = table.fn_signature(info.func, {
+					type_only: true
+				})
+			}
 		}
 		.map {
 			if int(t) == map_type_idx {
