80c4c10514903dadcce654a097d969c2ed153fc1
Delyan Angelov <delian66@gmail.com>
2020-11-07 21:19:46 +0200

    checker: add a check for `ptr = Abc{}`

--- vlib/os/os_c.v
+++ vlib/os/os_c.v
@@ -2,27 +2,41 @@ module os
 
 #include <sys/stat.h> // #include <signal.h>
 #include <errno.h>
+struct C.dirent {
+	d_name [256]char
+}
 
-fn C.readdir(voidptr) C.dirent
+fn C.readdir(voidptr) &C.dirent
+
 fn C.getpid() int
-fn C.readlink() int
-fn C.getline(voidptr, voidptr, voidptr) int
-fn C.ftell(fp voidptr) int
-fn C.sigaction(int, voidptr, int)
-fn C.open(charptr, int, int) int
-fn C.fdopen(int, string) voidptr
-fn C.CopyFile(&u32, &u32, int) int
-fn C.fork() int
-fn C.wait() int
-//fn C.proc_pidpath(int, byteptr, int) int
 
+fn C.readlink() int
+
+fn C.getline(voidptr, voidptr, voidptr) int
+
+fn C.ftell(fp voidptr) int
+
+fn C.sigaction(int, voidptr, int)
+
+fn C.open(charptr, int, int) int
+
+fn C.fdopen(int, string) voidptr
+
+fn C.CopyFile(&u32, &u32, int) int
+
+fn C.fork() int
+
+fn C.wait() int
+
+// fn C.proc_pidpath(int, byteptr, int) int
 struct C.stat {
 	st_size  int
 	st_mode  u32
 	st_mtime int
 }
 
-struct C.DIR {}
+struct C.DIR {
+}
 
 struct C.sigaction {
 mut:
