41155936c1dc48d8e8c43da910932ac21d3d5044
pancake <pancake@nowsecure.com>
2020-11-08 09:14:24 +0100

    vweb: check function and route parameter count (#6761)

--- vlib/v/checker/checker.v
+++ vlib/v/checker/checker.v
@@ -31,7 +31,7 @@ pub struct Checker {
 	pref              &pref.Preferences // Preferences shared from V struct
 pub mut:
 	table             &table.Table
-	file              ast.File
+	file              &ast.File = 0
 	nr_errors         int
 	nr_warnings       int
 	errors            []errors.Error
@@ -61,6 +61,7 @@ mut:
 	error_details     []string
 	generic_funcs     []&ast.FnDecl
 	vmod_file_content string // needed for @VMOD_FILE, contents of the file, *NOT its path*
+	vweb_gen_types    []table.Type // vweb route checks
 }
 
 pub fn new_checker(table &table.Table, pref &pref.Preferences) Checker {
@@ -71,7 +72,7 @@ pub fn new_checker(table &table.Table, pref &pref.Preferences) Checker {
 	}
 }
 
-pub fn (mut c Checker) check(ast_file ast.File) {
+pub fn (mut c Checker) check(ast_file &ast.File) {
 	c.file = ast_file
 	for i, ast_import in ast_file.imports {
 		for j in 0 .. i {
@@ -112,7 +113,7 @@ pub fn (mut c Checker) check_scope_vars(sc &ast.Scope) {
 }
 
 // not used right now
-pub fn (mut c Checker) check2(ast_file ast.File) []errors.Error {
+pub fn (mut c Checker) check2(ast_file &ast.File) []errors.Error {
 	c.file = ast_file
 	for stmt in ast_file.stmts {
 		c.stmt(stmt)
@@ -147,6 +148,7 @@ pub fn (mut c Checker) check_files(ast_files []ast.File) {
 			has_main_fn = true
 		}
 	}
+	c.verify_all_vweb_routes()
 	// Make sure fn main is defined in non lib builds
 	if c.pref.build_mode == .build_module || c.pref.is_test {
 		return
@@ -4207,17 +4209,17 @@ fn (mut c Checker) post_process_generic_fns() {
 	// Loop thru each generic function concrete type.
 	// Check each specific fn instantiation.
 	for i in 0 .. c.generic_funcs.len {
-		mut node := c.generic_funcs[i]
 		if c.table.fn_gen_types.len == 0 {
 			// no concrete types, so just skip:
 			continue
 		}
-		// eprintln('>> post_process_generic_fns $c.file.path | $node.name , c.table.fn_gen_types.len: $c.table.fn_gen_types.len')
+		mut node := c.generic_funcs[i]
 		for gen_type in c.table.fn_gen_types[node.name] {
 			c.cur_generic_type = gen_type
-			// sym:=c.table.get_type_symbol(gen_type)
-			// println('\ncalling check for $node.name for type $sym.source_name')
 			c.fn_decl(mut node)
+			if node.name in ['vweb.run_app', 'vweb.run'] {
+				c.vweb_gen_types << gen_type
+			}
 		}
 		c.cur_generic_type = 0
 		c.generic_funcs[i] = 0
@@ -4266,6 +4268,8 @@ fn (mut c Checker) fn_decl(mut node ast.FnDecl) {
 			c.error('cannot define new methods on non-local `$sym.source_name` (' +
 				'current module is `$c.mod`, `$sym.source_name` is from `$sym.mod`)', node.pos)
 		}
+		// needed for proper error reporting during vweb route checking
+		sym.methods[node.method_idx].source_fn = voidptr(node)
 	}
 	if node.language == .v {
 		// Make sure all types are valid
@@ -4322,6 +4326,7 @@ fn (mut c Checker) fn_decl(mut node ast.FnDecl) {
 		c.error('missing return at end of function `$node.name`', node.pos)
 	}
 	c.returns = false
+	node.source_file = c.file
 }
 
 fn has_top_return(stmts []ast.Stmt) bool {
@@ -4341,4 +4346,46 @@ fn has_top_return(stmts []ast.Stmt) bool {
 		}
 	}
 	return false
+}
+
+fn (mut c Checker) verify_vweb_params_for_method(m table.Fn) (bool, int, int) {
+	margs := m.params.len - 1 // first arg is the receiver/this
+	if m.attrs.len == 0 {
+		// allow non custom routed methods, with 1:1 mapping
+		return true, -1, margs
+	}
+	mut route_attributes := 0
+	for a in m.attrs {
+		if a.name.starts_with('/') {
+			route_attributes += a.name.count(':')
+		}
+	}
+	return route_attributes == margs, route_attributes, margs
+}
+
+fn (mut c Checker) verify_all_vweb_routes() {
+	if c.vweb_gen_types.len == 0 {
+		return
+	}
+	typ_vweb_result := c.table.find_type_idx('vweb.Result')
+	for vgt in c.vweb_gen_types {
+		sym_app := c.table.get_type_symbol(vgt)
+		for m in sym_app.methods {
+			if m.return_type_source_name == 'vweb.Result' {
+				is_ok, nroute_attributes, nargs := c.verify_vweb_params_for_method(m)
+				if !is_ok {
+					f := &ast.FnDecl(m.source_fn)
+					if isnil(f) {
+						continue
+					}
+					if f.return_type == typ_vweb_result &&
+						f.receiver.typ == m.params[0].typ && f.name == m.name {
+						c.file = f.source_file // setup of file path for the warning
+						c.warn('mismatched parameters count between vweb method `${sym_app.name}.$m.name` ($nargs) and route attribute $m.attrs ($nroute_attributes)',
+							f.pos)
+					}
+				}
+			}
+		}
+	}
 }
