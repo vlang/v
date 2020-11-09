f042ef8500da2b2c36442ca054e77b3d79e9ba21
spaceface777 <spaceface7777@gmail.com>
2020-11-09 19:21:54 +0100

    checker: fix `&` on pointers

--- vlib/v/checker/checker.v
+++ vlib/v/checker/checker.v
@@ -2865,6 +2865,8 @@ pub fn (mut c Checker) expr(node ast.Expr) table.Type {
 					}
 				}
 				return right_type.to_ptr()
+			} else if node.op == .amp && node.right !is ast.CastExpr {
+				return right_type.to_ptr()
 			}
 			if node.op == .mul {
 				if right_type.is_ptr() {
