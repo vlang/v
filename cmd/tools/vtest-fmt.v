ebfd259333d7e85567bd5dbdbfaae9f2f65adb50
Delyan Angelov <delian66@gmail.com>
2020-11-11 17:48:42 +0200

    vfmt: support `struct Repo <T, U> { }`

--- cmd/tools/vtest-fmt.v
+++ cmd/tools/vtest-fmt.v
@@ -7,7 +7,6 @@ import v.util
 // os.v - // embeded comments, mib := [1/* CTL_KERN */, 14/* KERN_PROC */, 12/* KERN_PROC_PATHNAME */, -1] => comment the rest of the line
 const (
 	known_failing_exceptions = [
-		'vlib/v/tests/generics_test.v', // struct Repo<T, U> { => struct Repo {
 		'vlib/crypto/aes/aes.v', // pub fn (c &AesCipher) encrypt(mut dst, mut src []byte) {
 		'vlib/crypto/aes/block_generic.v', // fn expand_key_generic(key []byte, mut enc, mut dec []u32) {
 		'vlib/crypto/aes/const.v', // multiple narrow columns of []string turned to 1 long single column, otherwise works
