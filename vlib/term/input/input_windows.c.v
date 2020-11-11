ec36dacd71e4dc46babf459650de5fa06dbba7fb
spaceface777 <spaceface7777@gmail.com>
2020-11-11 19:11:17 +0100

    just remove the test
    it's not like it tests much anyways, and isn't worth the trouble of fixing windows CI

--- vlib/term/input/input_windows.c.v
+++ vlib/term/input/input_windows.c.v
@@ -8,8 +8,3 @@ pub fn init(cfg Config) &Context {
 pub fn (mut ctx Context) run() {
 	panic("term.input: error: Windows support isn't implemented yet")
 }
-
-// TODO: remove, only here so that the test compiles
-fn escape_end(buf string) int {
-	return 0
-}
