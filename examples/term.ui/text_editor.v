b7c892a71885542f90795be8c3048152186f8c17
spaceface777 <spaceface7777@gmail.com>
2020-11-15 10:24:56 +0100

    examples/text_editor: implement ctrl+arrows to move by word

--- examples/term.ui/text_editor.v
+++ examples/term.ui/text_editor.v
@@ -359,6 +359,23 @@ fn (mut b Buffer) move_cursor(amount int, movement Movement) {
 	}
 }
 
+fn (mut b Buffer) move_to_word(movement Movement) {
+	a := if movement == .left { -1 } else { 1 }
+	line := b.cur_line()
+	mut x, mut y := b.cursor.pos_x, b.cursor.pos_y
+	// first, move past all non-`a-zA-Z0-9_` characters
+	for ; x+a >= 0 && x+a < line.len && !(line[x+a].is_letter() || line[x+a].is_digit() || line[x+a] == `_`); x += a {}
+	// then, move past all the letters and numbers
+	for ; x+a >= 0 && x+a < line.len && (line[x+a].is_letter() || line[x+a].is_digit() || line[x+a] == `_`); x += a {}
+	if x + a >= 0 && x + a < line.len {
+		x += a
+	} else {
+		y += a
+		x = 0
+	}
+	b.cursor.set(x, y)
+}
+
 fn imax(x int, y int) int {
 	return if x < y { y } else { x }
 }
@@ -462,10 +479,18 @@ fn event(e &tui.Event, x voidptr) {
 				buffer.del(1)
 			}
 			.left {
-				buffer.move_cursor(1, .left)
+				if e.modifiers == tui.ctrl {
+					buffer.move_to_word(.left)
+				} else if e.modifiers == 0 {
+					buffer.move_cursor(1, .left)
+				}
 			}
 			.right {
-				buffer.move_cursor(1, .right)
+				if e.modifiers == tui.ctrl {
+					buffer.move_to_word(.right)
+				} else if e.modifiers == 0 {
+					buffer.move_cursor(1, .right)
+				}
 			}
 			.up {
 				buffer.move_cursor(1, .up)
