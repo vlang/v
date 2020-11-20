d31c14aee8a35f91433a2b5247f6ac28fb78ffde
Alexander Medvednikov <alexander@vlang.io>
2020-11-20 03:28:28 +0100

    clipboard: fix set_text()

--- vlib/clipboard/clipboard_darwin.m
+++ vlib/clipboard/clipboard_darwin.m
@@ -10,3 +10,14 @@ char* darwin_get_pasteboard_text(void* pb) {
 	}
 	return [ns_clip UTF8String];
 }
+
+bool darwin_set_pasteboard_text(void* _pb, string text) {
+	NSPasteboard* pb = (__bridge NSPasteboard*) _pb;
+	NSString *ns_clip = [[ NSString alloc ] initWithBytesNoCopy:text.str length:text.len encoding:NSUTF8StringEncoding freeWhenDone: false];
+	[pb declareTypes:[NSArray arrayWithObject:NSStringPboardType] owner:nil];
+	bool ret = [pb setString:ns_clip forType:NSStringPboardType];
+	[ns_clip release];
+	int serial = [pb changeCount];
+	//OSAtomicCompareAndSwapLong(cb.last_cb_serial, serial, &cb.last_cb_serial);
+	return ret;
+}
