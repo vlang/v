96b73acad7467bce9b516289f4dc7e5944218318
Leah Lundqvist <hampus0505@gmail.com>
2020-11-24 22:52:23 +0100

    jsgen: optional "or blocks" (#6938)

---
+++ vlib/v/gen/js/tests/optional.v
@@ -0,0 +1,31 @@
+module main
+
+fn main() {
+	try_propagation() or { println("captured: $err") }
+}
+
+fn try_propagation() ? {
+	try_numbers()?
+}
+
+fn try_numbers() ? {
+	for x in 1 .. 10 {
+		y := error_if_even(x) or { x + 1 }
+		println("$x rounded to $y")
+		error_if_prime(y)?
+	}
+}
+
+fn error_if_even(num int) ?int {
+	if num % 2 == 0 { return error('number is even') }
+	return num
+}
+
+fn error_if_prime(num int) ?int {
+	for i in 2..num {
+		if num % i == 0 {
+			return error('$num is prime')
+		}
+	}
+	return num
+}
