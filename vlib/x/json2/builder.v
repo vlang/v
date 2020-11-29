6c634086b091fe1cb4dc1da3203719e13a2db7d2
Ned Palacios <npdoesmc@gmail.com>
2020-11-29 21:54:45 +0800

    json2: decode fn returns `?T`; add new tests (#6933)

--- vlib/x/json2/builder.v
+++ vlib/x/json2/builder.v
@@ -9,56 +9,69 @@ pub fn (mut obj map[string]Any) insert_str(key string, val string) {
 	fi = val
 	obj[key] = fi
 }
+
 // Inserts an int into the map.
 pub fn (mut obj map[string]Any) insert_int(key string, val int) {
 	obj[key] = Any(val)
 }
+
 // Inserts a float into the map.
 pub fn (mut obj map[string]Any) insert_f(key string, val f64) {
 	obj[key] = Any(val)
 }
+
 // Inserts a null into the map.
 pub fn (mut obj map[string]Any) insert_null(key string) {
 	obj[key] = Any(Null{})
 }
+
 // Inserts a bool into the map.
 pub fn (mut obj map[string]Any) insert_bool(key string, val bool) {
 	obj[key] = Any(val)
 }
+
 // Inserts a map into the map.
 pub fn (mut obj map[string]Any) insert_map(key string, val map[string]Any) {
 	obj[key] = Any(val)
 }
+
 // Inserts an array into the map.
 pub fn (mut obj map[string]Any) insert_arr(key string, val []Any) {
 	obj[key] = Any(val)
 }
+
 // Inserts a string into the array.
 pub fn (mut arr []Any) insert_str(val string) {
 	mut fi := Any{}
 	fi = val
 	arr << fi
 }
+
 // Inserts an int into the array.
 pub fn (mut arr []Any) insert_int(val int) {
 	arr << Any(val)
 }
+
 // Inserts a float into the array.
 pub fn (mut arr []Any) insert_f(val f64) {
 	arr << Any(val)
 }
+
 // Inserts a null into the array.
 pub fn (mut arr []Any) insert_null() {
 	arr << Any(Null{})
 }
+
 // Inserts a bool into the array.
 pub fn (mut arr []Any) insert_bool(val bool) {
 	arr << Any(val)
 }
+
 // Inserts a map into the array.
 pub fn (mut arr []Any) insert_map(val map[string]Any) {
 	arr << Any(val)
 }
+
 // Inserts an array into the array.
 pub fn (mut arr []Any) insert_arr(val []Any) {
 	arr << Any(val)
--- vlib/x/json2/json2.v
+++ vlib/x/json2/json2.v
@@ -3,6 +3,10 @@
 // that can be found in the LICENSE file.
 module json2
 
+pub const (
+	null = Null{}
+)
+
 pub interface Serializable {
 	from_json(f Any)
 	to_json() string
@@ -19,110 +23,73 @@ pub fn fast_raw_decode(src string) ?Any {
 	mut p := new_parser(src, false)
 	return p.decode()
 }
-// A generic function that decodes a JSON string into the target type.
-//
-// TODO: decode must return an optional generics
-pub fn decode<T>(src string) T {
-	res := raw_decode(src) or {
-		panic(err)
-	}
+
+// decode is a generic function that decodes a JSON string into the target type.
+pub fn decode<T>(src string) ?T {
+	res := raw_decode(src) ?
 	mut typ := T{}
 	typ.from_json(res)
 	return typ
 }
-// TODO: decode must return an optional generics
-// pub fn decode2<T>(src string) ?T {
-// 	res := raw_decode(src)?
 
-// 	match typeof(T) {
-// 		'string' {
-// 			return res.str()
-// 		}
-// 		'int' {
-// 			return res.int()
-// 		}
-// 		'f64' {
-// 			return res.f64()
-// 		}
-// 		else {
-// 			mut typ := T{}
-// 			typ.from_json(res)
-// 			return typ
-// 		}
-// 	}
-// }
-// A generic function that encodes a type into a JSON string.
+// encode is a generic function that encodes a type into a JSON string.
 pub fn encode<T>(typ T) string {
-	// if typeof(typ) in ['string', 'int', 'f64'] {
-	// 	return Any(typ).str()
-	// }
-
 	return typ.to_json()
 }
-// A simple function that returns `Null` struct. For use on constructing an `Any` object.
-pub fn null() Null {
-	return Null{}
-}
-// Use `Any` as a map.
+
+// as_map uses `Any` as a map.
 pub fn (f Any) as_map() map[string]Any {
 	if f is map[string]Any {
 		return f
 	} else if f is []Any {
-		mut mp := map[string]Any
+		mut mp := map[string]Any{}
 		for i, fi in f {
 			mp['$i'] = fi
 		}
 		return mp
 	}
-	return { '0': f }
+	return {
+		'0': f
+	}
 }
 
-// Use `Any` as an integer.
+// int uses `Any` as an integer.
 pub fn (f Any) int() int {
 	match f {
-		int  { return f }
-		i64  { return int(f) }
-		f64  { return f.str().int() }
-		f32  { return f.str().int() }
-		bool { return int(f) }
+		int { return f }
+		i64, f32, f64, bool { return int(f) }
 		else { return 0 }
 	}
 }
 
-// Use `Any` as a 64-bit integer.
+// i64 uses `Any` as a 64-bit integer.
 pub fn (f Any) i64() i64 {
 	match f {
-		int  { return f }
-		i64  { return int(f) }
-		f64  { return f.str().i64() }
-		f32  { return f.str().i64() }
-		bool { return int(f) }
+		i64 { return f }
+		int, f32, f64, bool { return i64(f) }
 		else { return 0 }
 	}
 }
 
-// Use `Any` as a 32-bit float.
+// f32 uses `Any` as a 32-bit float.
 pub fn (f Any) f32() f32 {
 	match f {
-		int { return f }
-		i64 { return f.str().f32() }
-		f64 { return f.str().f32() }
 		f32 { return f }
+		int, i64, f64 { return f32(f) }
 		else { return 0.0 }
 	}
 }
 
-// Use `Any` as a float.
+// f64 uses `Any` as a float.
 pub fn (f Any) f64() f64 {
 	match f {
-		int { return f }
-		i64 { return f }
 		f64 { return f }
-		f32 { return f.str().f64() }
+		int, i64, f32 { return f64(f) }
 		else { return 0.0 }
 	}
 }
-// Use `Any` as an array.
+
+// arr uses `Any` as an array.
 pub fn (f Any) arr() []Any {
 	if f is []Any {
 		return f
@@ -136,7 +103,7 @@ pub fn (f Any) arr() []Any {
 	return [f]
 }
 
-// Use `Any` as a bool
+// bool uses `Any` as a bool
 pub fn (f Any) bool() bool {
 	match f {
 		bool { return f }
