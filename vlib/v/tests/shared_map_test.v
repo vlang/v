827c10d636a66ca48c5a469733f54d81513b990f
Uwe Krüger <45282134+UweKrueger@users.noreply.github.com>
2020-11-30 13:25:14 +0100

    cgen: add support for shared maps (#7042)

---
+++ vlib/v/tests/shared_map_test.v
@@ -0,0 +1,36 @@
+import sync
+
+fn incr(shared foo map[string]int, key string, sem sync.Semaphore) {
+	for _ in 0 .. 100000 {
+		lock foo {
+			foo[key] = foo[key] + 1
+		}
+	}
+	sem.post()
+}
+
+fn test_shared_array() {
+	shared foo := &{'p': 10, 'q': 20}
+	sem := sync.new_semaphore()
+	go incr(shared foo, 'p', sem)
+	go incr(shared foo, 'q', sem)
+	go incr(shared foo, 'p', sem)
+	go incr(shared foo, 'q', sem)
+	for _ in 0 .. 50000 {
+		lock foo {
+			unsafe {
+				foo['p'] = foo['p'] - 2
+				foo['q'] = foo['q'] + 3
+			}
+		}
+	}
+	for _ in 0..4 {
+		sem.wait()
+	}
+	rlock foo {
+		fp := unsafe { foo['p'] }
+		fq := unsafe { foo['q'] }
+		assert fp == 100010
+		assert fq == 350020
+	}
+}
