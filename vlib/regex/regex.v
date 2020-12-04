08b166a7acaa102e3092bb93f509170cb4f8fdc7
penguindark <dariodeledda@hotmail.com>
2020-12-04 22:29:45 +0000

    first attempt to fix regex

--- vlib/regex/regex.v
+++ vlib/regex/regex.v
@@ -616,7 +616,7 @@ fn (mut re RE) parse_char_class(in_txt string, in_i int) (int, int, rune) {
 				}
 			}
 			if status == .in_bsls {
-				println("CC bsls not found [${ch:c}]")
+				//println("CC bsls not found [${ch:c}]")
 				status = .in_char
 			}else {
 				continue
@@ -1490,6 +1490,8 @@ pub fn (mut re RE) match_base(in_txt byteptr, in_txt_len int ) (int,int) {
 	mut ch       := rune(0)           // examinated char
 	mut char_len := 0                 // utf8 examinated char len
 	mut m_state  := Match_state.start // start point for the matcher FSM
+	mut src_end  := false
+	mut last_fnd_pc := -1
 
 	mut pc    := -1                   // program counter
 	mut state := StateObj{}           // actual state
@@ -1599,13 +1601,18 @@ pub fn (mut re RE) match_base(in_txt byteptr, in_txt_len int ) (int,int) {
 			dbg_line++
 		}
 		//******************************************
-
+/*		if ist == ist_prog_end {
+			println("HERE")
+			break
+		}
+*/
 		// we're out of text, manage it
 		if i >= in_txt_len || m_state == .new_line {
+			src_end = true
 
 			// manage groups
 			if group_index >= 0 && state.match_index >= 0 {
-				//println("End text with open groups!")
+				println("End text with open groups!")
 				// close the groups
 				for group_index >= 0 {
 					tmp_pc := group_data[group_index]
@@ -1644,11 +1651,29 @@ pub fn (mut re RE) match_base(in_txt byteptr, in_txt_len int ) (int,int) {
 				}
 			}
 
-			// manage ist_dot_char
+			if pc == -1 {
+				pc = last_fnd_pc
+			}
+			println("Finished text!!")
+			println("Instruction: ${ist:08x} pc: $pc")
+			println("min_rep: ${re.prog[pc].rep_min} max_rep: ${re.prog[pc].rep_max} rep: ${re.prog[pc].rep}")
+			
+			// program end
+			if ist == ist_prog_end {
+				println("Program end on end of text!")
+				return first_match,i
+			}
 
-			m_state = .end
-			break
-			//return no_match_found,0
+			// if we go out of text and we are the last instruction .* check
+			if (re.prog[pc+1].ist == ist_prog_end) && 
+			(re.prog[pc].rep >= re.prog[pc].rep_min && re.prog[pc].rep <= re.prog[pc].rep_max) {
+				println("Ok .* rep match!")
+				return first_match,i
+			}		
+			
+			//m_state = .end
+			//break
+			return no_match_found,0
 		}
 
 		// starting and init
@@ -1697,7 +1722,8 @@ pub fn (mut re RE) match_base(in_txt byteptr, in_txt_len int ) (int,int) {
 		if m_state == .stop {
 
 			// we are in search mode, don't exit until the end
-			if re.flag & f_src != 0 && ist != ist_prog_end {
+			if ((re.flag & f_src) != 0) && (ist != ist_prog_end) {
+				last_fnd_pc = pc
 				pc = -1
 				i += char_len
 				m_state = .ist_next
@@ -1741,9 +1767,10 @@ pub fn (mut re RE) match_base(in_txt byteptr, in_txt_len int ) (int,int) {
 				// we have a DOT MATCH on going
 				//println("ist_prog_end l_ist: ${l_ist:08x}", l_ist)
 				if re.state_stack_index>=0 && l_ist == ist_dot_char {
+					i = in_txt_len // dario
 					m_state = .stop
 					continue
-				}
+				}				
 
 				re.state_stack_index = -1
 				m_state = .stop
@@ -1832,7 +1859,7 @@ pub fn (mut re RE) match_base(in_txt byteptr, in_txt_len int ) (int,int) {
 					first_match = i
 				}
 				state.match_index = i
-				re.prog[pc].rep++
+				re.prog[pc].rep++ // increase repetitions
 
 				//if re.prog[pc].rep >= re.prog[pc].rep_min && re.prog[pc].rep <= re.prog[pc].rep_max {
 				if re.prog[pc].rep >= 0 && re.prog[pc].rep <= re.prog[pc].rep_max {
@@ -1857,12 +1884,15 @@ pub fn (mut re RE) match_base(in_txt byteptr, in_txt_len int ) (int,int) {
 					re.state_stack[re.state_stack_index].i  = i + char_len
 				}
 
+				//i += char_len // next char
+/*
 				// manage * and {0,} quantifier
-				if re.prog[pc].rep_min > 0 {
-					i += char_len // next char
-					l_ist = u32(ist_dot_char)
+				if re.prog[pc].rep_max == max_quantifier {
+					println("manage .*")
+					m_state = .ist_load
+					continue
 				}
-
+*/
 				m_state = .ist_next
 				continue
 
@@ -2163,14 +2193,53 @@ pub fn (mut re RE) match_base(in_txt byteptr, in_txt_len int ) (int,int) {
 		return err_internal_error, i
 	}
 
+	//println("Check end of text!")
 	// Check the results
 	if state.match_index >= 0 {
 		if group_index < 0 {
-			//println("OK match,natural end [$first_match,$i]")
-			return first_match, i
+			
+			if re.prog[pc].ist == ist_prog_end {
+				println("program ended!!")
+				
+				if (re.flag & f_src) != 0 {
+					println("find return")
+					return first_match, i
+				} else {
+					return 0, i
+				}
+			}
+			
+			println("No Group here, natural end [$first_match,$i] state: ${state_str(m_state)} ist: $ist pgr_end: $re.prog.len")
+				
+			if re.prog[pc+1].ist == ist_prog_end || re.prog[pc].ist == ist_prog_end{
+				rep := re.prog[pc].rep
+				println("rep: $rep re.prog[pc].rep_min: ${re.prog[pc].rep_min} re.prog[pc].rep_max: ${re.prog[pc].rep_max}")
+				if rep >= re.prog[pc].rep_min && rep <= re.prog[pc].rep_max {
+					return first_match, i
+				}
+				//println("Program not finished! ")
+				return no_match_found, 0
+			}
+			if src_end {
+				println("program end")
+				return first_match, i
+			}
+			print("No match found!!")
+			return no_match_found, 0
+/*
+			if ist < (ist_prog_end-1) {
+				return no_match_found, 0
+			}
+*/
+//			return first_match, i
+
 		} else {
+			println("Group match! OK")
+			println("first_match: $first_match, i: $i")
+
 			//println("Skip last group")
-			return first_match,group_stack[group_index--]
+			return first_match,i
+			//return first_match,group_stack[group_index--]
 		}
 	}
 	//println("no_match_found, natural end")
@@ -2224,7 +2293,14 @@ fn impl_new_regex_by_size(mult int) RE {
 //
 
 pub fn (mut re RE) match_string(in_txt string) (int,int) {
-	start, end := re.match_base(in_txt.str,in_txt.len)
+
+	tmp_str := in_txt + "\uFFFF"
+	start, mut end := re.match_base(tmp_str.str, tmp_str.len)
+	if end > in_txt.len {
+		end = in_txt.len
+	}
+
+//	start, end := re.match_base(in_txt.str, in_txt.len)
 	if start >= 0 && end > start {
 		if (re.flag & f_ms) != 0 && start > 0 {
 			return no_match_found, 0
@@ -2247,9 +2323,20 @@ pub fn (mut re RE) match_string(in_txt string) (int,int) {
 // find try to find the first match in the input string
 pub fn (mut re RE) find(in_txt string) (int,int) {
 	old_flag := re.flag
+	tmp_str := in_txt + "\uFFFF"
+	re.flag |= f_src  // enable search mode
+	start, mut end := re.match_base(tmp_str.str, tmp_str.len)
+	if end > in_txt.len {
+		end = in_txt.len
+	}
+	re.flag = old_flag
+/*
+	old_flag := re.flag
 	re.flag |= f_src  // enable search mode
 	start, end := re.match_base(in_txt.str, in_txt.len)
+	//print("Find [$start,$end] '${in_txt[start..end]}'")
 	re.flag = old_flag
+*/
 	if start >= 0 && end > start {
 		return start, end
 	}
--- vlib/regex/regex_test.v
+++ vlib/regex/regex_test.v
@@ -38,7 +38,7 @@ match_test_suite = [
 	TestItem{"this these those ",r"(th[eio]se? ?)+",0,17},
 	TestItem{"this these those ",r"(th[eio]se? )+",0,17},
 	TestItem{"this,these,those. over",r"(th[eio]se?[,. ])+",0,17},
-	TestItem{"soday,this,these,those. over",r"(th[eio]se?[,. ])+",6,23},
+	TestItem{"soday,this,these,those. over",r".+(th[eio]se?[,. ])+",0,23},
 
 	TestItem{"cpapaz",r"(c(pa)+z)",0,6},
 	TestItem{"this is a cpapaz over",r"(c(pa)+z)",10,16},
@@ -60,7 +60,7 @@ match_test_suite = [
 	TestItem{"soday,this,these,those. over",r".*,(th[eio]se?[,. ])+",0,23},
 	TestItem{"soday,this,these,thesa.thesi over",r".*,(th[ei]se?[,. ])+(thes[ai][,. ])+",0,29},
 	TestItem{"cpapaz ole. pippo,",r".*(c(pa)+z)(\s+\a+[\.,]?)+",0,18},
-	TestItem{"cpapaz ole. pippo",r".*(c(pa)+z)(\s+\a+[\.,]?)+",0,17},
+	TestItem{"cpapaz ole. pippo",r"(c(pa)+z)(\s+\a+[\.,]?)+",0,17},
 	TestItem{"cpapaz ole. pippo, 852",r".*(c(pa)+z)(\s+\a+[\.,]?)+",0,18},
 	TestItem{"123cpapaz ole. pippo",r".*(c(pa)+z)(\s+\a+[\.,]?)+",0,20},
 	TestItem{"...cpapaz ole. pippo",r".*(c(pa)+z)(\s+\a+[\.,]?)+",0,20},
@@ -73,7 +73,6 @@ match_test_suite = [
 	TestItem{" abb",r"\s(.*)",0,4},
 
 	TestItem{"/home/us_er/pippo/info-01.txt", r"(/?[-\w_]+)*\.txt$",0,29}
-
 
 	// negative
 	TestItem{"zthis ciao",r"((t[hieo]+se?)\s*)+",-1,0},
@@ -88,6 +87,25 @@ match_test_suite = [
 	// check unicode
 	TestItem{"this is a Ⅰ Ⅱ Ⅲ Ⅳ Ⅴ Ⅵ test",r".*a [Ⅰ-Ⅵ ]+",0,34},
 	TestItem{"123Ⅰ Ⅱ Ⅲ Ⅳ Ⅴ Ⅵ test",r"[Ⅰ-Ⅴ\s]+",3,23},
+
+	// new edge cases
+	TestItem{"12345678", r"[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]",-1,0},
+	TestItem{"12345678", r"[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]",0,8},
+	TestItem{"123456789", r"^[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]$",0,9}
+	TestItem{"12345678", r"^\d{8}$",0,8},
+	TestItem{"12345678", r"^\d{7}$",-1,0},
+	TestItem{"12345678", r"^\d{9}$",-1,0},
+	
+	TestItem{"eth", r"(oth)|(eth)",0,3},
+	TestItem{"et", r"(oth)|(eth)",-1,0},
+	TestItem{"et", r".*(oth)|(eth)",-1,0},
+	TestItem{"peoth", r".*(ith)|(eth)",-1,0},
+
+	TestItem{"poth", r"(eth)|(oth)",1,4},
+	TestItem{"poth", r"(oth)|(eth)",1,4},
+	TestItem{"poth", r".(oth)|(eth)$",0,4},
+	TestItem{"poth", r"^.(oth)|(eth)$",0,4},
+	TestItem{"poth", r"^\w+$",0,4},
 ]
 )
 
@@ -150,30 +168,31 @@ const (
 cgroups_test_suite = [
 	TestItemCGroup{
 		"http://www.ciao.mondo/hello/pippo12_/pera.html",
-		r"(?P<format>https?)|(?:ftps?)://(?P<token>[\w_]+.)+",0,46,
-		[8, 0, 0, 4, 1, 7, 11, 1, 11, 16, 1, 16, 22, 1, 22, 28, 1, 28, 37, 1, 37, 42, 1, 42, 46],
+		r"(?P<format>https?)|(?:ftps?)://(?P<token>[\w_]+[\.|/])+",0,42,
+		[7, 0, 0, 4, 1, 7, 11, 1, 11, 16, 1, 16, 22, 1, 22, 28, 1, 28, 37, 1, 37, 42],
 		{'format':int(0),'token':1}
 	},
 	TestItemCGroup{
 		"http://www.ciao.mondo/hello/pippo12_/pera.html",
 		r"(?P<format>https?)|(?P<format>ftps?)://(?P<token>[\w_]+.)+",0,46,
-		[8, 0, 0, 4, 1, 7, 11, 1, 11, 16, 1, 16, 22, 1, 22, 28, 1, 28, 37, 1, 37, 42, 1, 42, 46],
+		[2, 0, 0, 4, 1, 7, 10],
 		{'format':int(0),'token':1}
 	},
 	TestItemCGroup{
 		"http://www.ciao.mondo/hello/pippo12_/pera.html",
-		r"(?P<format>https?)|(?P<format>ftps?)://([\w_]+.)+",0,46,
-		[8, 0, 0, 4, 1, 7, 11, 1, 11, 16, 1, 16, 22, 1, 22, 28, 1, 28, 37, 1, 37, 42, 1, 42, 46],
+		r"(?P<format>https?)|(?P<format>ftps?)://([\w_]+\.)+",0,16,
+		[3, 0, 0, 4, 1, 7, 11, 1, 11, 16],
 		{'format':int(0)}
 	},
 ]
 )
 
 fn test_regex(){
+
 	// check capturing groups
 	for c,to in cgroups_test_suite {
 		// debug print
-		//println("#$c [$to.src] q[$to.q] ($to.s, $to.e)")
+		println("#$c [$to.src] q[$to.q] ($to.s, $to.e)")
 
 		mut re := regex.regex_opt(to.q) or {
 			eprintln('err: $err')
@@ -191,16 +210,16 @@ fn test_regex(){
 		}
 
 		if start != to.s || end != to.e {
-			println("#$c [$to.src] q[$to.q] res[$tmp_str] $start, $end")
+			//println("#$c [$to.src] q[$to.q] res[$tmp_str] $start, $end")
 			println("ERROR!")
-			//C.printf("ERROR!! res:(%d, %d) refh:(%d, %d)\n",start, end, to.s, to.e)
+			C.printf("ERROR!! res:(%d, %d) refh:(%d, %d)\n",start, end, to.s, to.e)
 			assert false
 			continue
 		}
 
 		// check cgroups
 		if re.group_csave.len == 0 || re.group_csave[0] != to.cg[0] {
-			println("Capturing group len error!")
+			println("Capturing group len error! ${re.group_csave[0]}")
 			assert false
 			continue
 		}
@@ -289,7 +308,7 @@ fn test_regex(){
 
 			if start != to.s || end != to.e {
 				err_str := re.get_parse_error_string(start)
-				println("ERROR : $err_str")
+				println("ERROR : $err_str start: ${start} end: ${end}")
 				assert false
 			} else {
 				//tmp_str := text[start..end]
