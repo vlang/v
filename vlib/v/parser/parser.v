8f8bfc7ff44f962b41705926dd3ba4456b8eb4d5
Daniel Däschle <daniel.daeschle@gmail.com>
2020-12-10 18:16:47 +0100

    simplify code in for loop

--- vlib/v/parser/parser.v
+++ vlib/v/parser/parser.v
@@ -352,12 +352,8 @@ pub fn (mut p Parser) parse_block_no_scope(is_top_level bool) []ast.Stmt {
 	mut stmts := []ast.Stmt{}
 	if p.tok.kind != .rcbr {
 		mut c := 0
-		for p.tok.kind != .eof {
+		for p.tok.kind !in [.eof, .rcbr] {
 			stmts << p.stmt(is_top_level)
-			// p.warn('after stmt(): tok=$p.tok.str()')
-			if p.tok.kind in [.eof, .rcbr] {
-				break
-			}
 			c++
 			if c % 100000 == 0 {
 				eprintln('parsed $c statements so far from fn $p.cur_fn_name ...')
