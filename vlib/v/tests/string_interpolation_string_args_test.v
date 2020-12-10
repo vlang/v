ca39a9a1a9d7e82d0e9220acaf91f43ab391acc3
yuyi <yuyi98@163.com>
2020-12-10 17:20:42 +0800

    test: add more embed string interpolation tests (#7229)

--- vlib/v/tests/string_interpolation_string_args_test.v
+++ vlib/v/tests/string_interpolation_string_args_test.v
@@ -4,6 +4,11 @@ fn show_info(a string) string {
 
 fn test_interpolation_string_args() {
 	assert '${show_info("abc")}' == 'abc'
-	assert '${show_info("bac")}' == 'bac'
+	assert '${show_info('abc')}' == 'abc'
+
+	assert '1_${show_info("aaa")} 2_${show_info("bbb")}' == '1_aaa 2_bbb'
+	assert '1_${show_info('aaa')} 2_${show_info('bbb')}' == '1_aaa 2_bbb'
+
 	assert '${"aaa"}' == 'aaa'
+	assert '${'aaa'}' == 'aaa'
 }
