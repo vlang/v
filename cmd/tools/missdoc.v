99de52e7b8629f9fe762b3acd2cbaa26b4cb30f3
lmp <dev.larpon@gmail.com>
2020-12-11 15:57:07 +0100

    missdoc: add missing license information

--- cmd/tools/missdoc.v
+++ cmd/tools/missdoc.v
@@ -1,4 +1,6 @@
 // Copyright (c) 2020 Lars Pontoppidan. All rights reserved.
+// Use of this source code is governed by an MIT license
+// that can be found in the LICENSE file.
 import os
 import flag
 
