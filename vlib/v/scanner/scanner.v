11808f9fa317cf87ae6056275535c578bab9b0ab
Daniel Däschle <daniel.daeschle@gmail.com>
2020-12-12 04:06:09 +0100

    parser: fix silent errors (#7271)

--- vlib/v/scanner/scanner.v
+++ vlib/v/scanner/scanner.v
@@ -890,10 +890,12 @@ fn (mut s Scanner) text_scan() token.Token {
 				if nextc == `=` {
 					s.pos++
 					return s.new_token(.ne, '', 2)
-				} else if nextc == `i` && s.text[s.pos + 2] == `n` && s.text[s.pos + 3].is_space() {
+				} else if s.text.len > s.pos + 3 &&
+					nextc == `i` && s.text[s.pos + 2] == `n` && s.text[s.pos + 3].is_space() {
 					s.pos += 2
 					return s.new_token(.not_in, '', 3)
-				} else if nextc == `i` && s.text[s.pos + 2] == `s` && s.text[s.pos + 3].is_space() {
+				} else if s.text.len > s.pos + 3 &&
+					nextc == `i` && s.text[s.pos + 2] == `s` && s.text[s.pos + 3].is_space() {
 					s.pos += 2
 					return s.new_token(.not_is, '', 3)
 				} else {
@@ -943,7 +945,7 @@ fn (mut s Scanner) text_scan() token.Token {
 					start := s.pos + 2
 					mut nest_count := 1
 					// Skip comment
-					for nest_count > 0 {
+					for nest_count > 0 && s.pos < s.text.len - 1 {
 						s.pos++
 						if s.pos >= s.text.len {
 							s.line_nr--
