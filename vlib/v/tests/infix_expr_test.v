5cfcb7eb7955123f57457eb3b19fa50059630133
Enzo Baldisserri <enzo.baldisserri@outlook.fr>
2020-12-13 21:54:33 +0100

    gen: add tests for <= and >=

--- vlib/v/tests/infix_expr_test.v
+++ vlib/v/tests/infix_expr_test.v
@@ -6,9 +6,17 @@ fn test_cmp_signed_and_u32() {
 	// >
 	assert !(int(1) > u32(1))
 	assert int(1) > u32(0)
+	// >=
+	assert !(int(0) >= u32(1))
+	assert int(1) >= u32(1)
+	assert int(1) >= u32(0)
 	// <
 	assert !(int(1) < u32(1))
 	assert int(0) < u32(1)
+	// <=
+	assert int(0) <= u32(1)
+	assert int(1) <= u32(1)
+	assert !(int(1) <= u32(0))
 }
 
 fn test_cmp_u32_and_signed() {
@@ -19,9 +27,17 @@ fn test_cmp_u32_and_signed() {
 	// >
 	assert !(u32(1) > int(1))
 	assert u32(1) > int(0)
+	// >=
+	assert u32(1) >= int(0)
+	assert u32(1) >= int(1)
+	assert !(u32(0) >= int(1))
 	// <
 	assert !(u32(1) < int(1))
 	assert u32(0) < int(1)
+	// <=
+	assert u32(0) <= int(1)
+	assert u32(1) <= int(1)
+	assert !(u32(1) <= int(0))
 }
 
 fn test_cmp_signed_and_u64() {
@@ -32,9 +48,17 @@ fn test_cmp_signed_and_u64() {
 	// >
 	assert !(int(1) > u64(1))
 	assert int(1) > u64(0)
+	// >=
+	assert !(int(0) >= u64(1))
+	assert int(1) >= u64(1)
+	assert int(1) >= u64(0)
 	// <
 	assert !(int(1) < u64(1))
 	assert int(0) < u64(1)
+	// <=
+	assert int(0) <= u64(1)
+	assert int(1) <= u64(1)
+	assert !(int(1) <= u64(0))
 }
 
 fn test_cmp_u64_and_signed() {
@@ -45,7 +69,15 @@ fn test_cmp_u64_and_signed() {
 	// >
 	assert !(u64(1) > int(1))
 	assert u64(1) > int(0)
+	// >=
+	assert u64(1) >= int(0)
+	assert u64(1) >= int(1)
+	assert !(u64(0) >= int(1))
 	// <
 	assert !(u64(1) < int(1))
 	assert u64(0) < int(1)
+	// <=
+	assert u64(0) <= int(1)
+	assert u64(1) <= int(1)
+	assert !(u64(1) <= int(0))
 }
