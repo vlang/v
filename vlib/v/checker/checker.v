6149c627946554b54e91b1df4ffa6a4d18581e86
Delta456 <swstkbaranwal@gmail.com>
2020-12-13 22:28:30 +0530

    fix

--- vlib/v/checker/checker.v
+++ vlib/v/checker/checker.v
@@ -3168,7 +3168,7 @@ pub fn (mut c Checker) cast_expr(mut node ast.CastExpr) table.Type {
 	} else if node.expr_type == table.none_type {
 		type_name := c.table.type_to_str(node.typ)
 		c.error('cannot cast `none` to `$type_name`', node.pos)
-	} else if from_type_sym.kind == .struct_ && !node.expr_type.is_ptr() {
+	} else if from_type_sym.kind == .struct_ && !node.expr_type.is_ptr() && to_type_sym.kind !in [.sum_type, .interface_] {
 		type_name := c.table.type_to_str(node.typ)
 		c.error('cannot cast `struct` to `$type_name`', node.pos)
 	}
