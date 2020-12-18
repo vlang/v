170f79539043c98436376da4088159380f090baf
Uwe Krüger <45282134+UweKrueger@users.noreply.github.com>
2020-12-18 21:32:35 +0100

    add testcase for concurrent `select`s

---
+++ vlib/sync/channel_select_6_test.v
@@ -0,0 +1,53 @@
+fn do_rec(chi chan int, chf chan f64, sumchi chan i64, sumchf chan f64) {
+	mut sum := i64(0)
+	mut sumf := 0.0
+	for j in 0 .. 20000 {
+		println('iteration $j')
+		select {
+			i := <-chi {
+				sum += i
+			}
+			f := <-chf {
+				sumf += f
+			}
+		}
+	}
+	sumchi <- sum
+	sumchf <- sumf
+}
+
+fn do_send_int(ch chan int) {
+	for i in 0 .. 10000 {
+		ch <- i
+		println('sent int $i')
+	}
+}
+
+fn do_send_f64(ch chan f64) {
+	for i in 0 .. 10000 {
+		ch <- f64(i)
+		println('sent f64 $i')
+	}
+}
+
+fn test_select() {
+	chi := chan int{cap: 0}
+	chf := chan f64{}
+	chsumi := chan i64{}
+	chsumf := chan f64{}
+	go do_send_int(chi)
+	go do_send_f64(chf)
+	go do_rec(chi, chf, chsumi, chsumf)
+	go do_send_int(chi)
+	go do_send_f64(chf)
+	go do_rec(chi, chf, chsumi, chsumf)
+	mut sumi := <-chsumi
+	sumi += <-chsumi
+	mut sumf := <-chsumf
+	sumf += <-chsumf
+	// Use Gauß' formula
+	expected_sumi :=  2 * i64(10000) * (10000 - 1) / 2
+	expected_sumf :=  2 * f64(10000) * (10000 - 1) / 2
+	assert sumi == expected_sumi
+	assert sumf == expected_sumf
+}
