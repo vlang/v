4bfb11e675de2b19aec02c47e8a3ff72e12b53a8
Lukas Neubert <serkonda@protonmail.com>
2020-12-20 18:26:58 +0100

    checker: error if the same type is used more than once in a sumtype

--- vlib/v/checker/checker.v
+++ vlib/v/checker/checker.v
@@ -338,16 +338,20 @@ pub fn (mut c Checker) type_decl(node ast.TypeDecl) {
 		}
 		ast.SumTypeDecl {
 			c.check_valid_pascal_case(node.name, 'sum type', node.pos)
+			mut found_sym_names := []string{}
 			for variant in node.variants {
 				if variant.typ.is_ptr() {
 					c.error('sum type cannot hold a reference type', variant.pos)
 				}
 				mut sym := c.table.get_type_symbol(variant.typ)
-				if sym.kind == .placeholder {
+				if sym.name in found_sym_names {
+					c.error('sum type cannot hold the type `$sym.name` multiple times', node.pos)
+				} else if sym.kind == .placeholder {
 					c.error("type `$sym.name` doesn't exist", node.pos)
 				} else if sym.kind == .interface_ {
 					c.error('sum type cannot hold an interface', node.pos)
 				}
+				found_sym_names << sym.name
 			}
 		}
 	}
