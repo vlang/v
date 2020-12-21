fe58eab36b4035e2416e03fe5feca1c7938e01e1
Lukas Neubert <serkonda@protonmail.com>
2020-12-21 19:44:06 +0100

    implement requested changes

--- vlib/v/checker/checker.v
+++ vlib/v/checker/checker.v
@@ -338,21 +338,21 @@ pub fn (mut c Checker) type_decl(node ast.TypeDecl) {
 		}
 		ast.SumTypeDecl {
 			c.check_valid_pascal_case(node.name, 'sum type', node.pos)
-			mut found_sym_names := []string{}
+			mut names_used := []string{}
 			for variant in node.variants {
 				if variant.typ.is_ptr() {
 					c.error('sum type cannot hold a reference type', variant.pos)
 				}
 				mut sym := c.table.get_type_symbol(variant.typ)
-				if sym.name in found_sym_names {
-					c.error('sum type cannot hold the type `$sym.name` more than once',
+				if sym.name in names_used {
+					c.error('sum type $node.name cannot hold the type `$sym.name` more than once',
 						variant.pos)
 				} else if sym.kind == .placeholder {
 					c.error("type `$sym.name` doesn't exist", node.pos)
 				} else if sym.kind == .interface_ {
 					c.error('sum type cannot hold an interface', node.pos)
 				}
-				found_sym_names << sym.name
+				names_used << sym.name
 			}
 		}
 	}
