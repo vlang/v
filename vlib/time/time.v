c69947160b453aa40a46eb880e31be94d6b83ff8
zakuro <z@kuro.red>
2020-12-21 17:27:06 +0900

    time: change type of duration constants from i64 to Duration (#7435)

--- vlib/time/time.v
+++ vlib/time/time.v
@@ -380,11 +380,11 @@ pub type Duration = i64
 
 pub const (
 	nanosecond  = Duration(1)
-	microsecond = Duration(1000) * nanosecond
-	millisecond = Duration(1000) * microsecond
-	second      = Duration(1000) * millisecond
-	minute      = Duration(60) * second
-	hour        = Duration(60) * minute
+	microsecond = Duration(1000 * nanosecond)
+	millisecond = Duration(1000 * microsecond)
+	second      = Duration(1000 * millisecond)
+	minute      = Duration(60 * second)
+	hour        = Duration(60 * minute)
 	infinite    = Duration(-1)
 )
 
