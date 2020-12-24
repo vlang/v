c65483357547f36ed6d433998678e3c21fdaae42
Nick Treleaven <n@trelsoft.com>
2020-12-24 11:10:03 +0000

    list supported types when creating a map with an unsupported key type

--- vlib/v/parser/parse_type.v
+++ vlib/v/parser/parse_type.v
@@ -55,7 +55,8 @@ pub fn (mut p Parser) parse_map_type() table.Type {
 	if !(key_type in [table.string_type_idx, table.voidptr_type_idx] ||
 		(key_type.is_int() && !key_type.is_ptr())) {
 		s := p.table.type_to_str(key_type)
-		p.error_with_pos('maps do not support `$s` keys for now', p.tok.position())
+		p.error_with_pos('maps only support string, integer, rune or voidptr keys for now (not `$s`)',
+			p.tok.position())
 		return 0
 	}
 	if key_type != table.string_type_idx {
