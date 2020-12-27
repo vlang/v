9e6575eaab5e471019ff3af9f0a633182c1d6f4d
Larpon <Larpon@users.noreply.github.com>
2020-12-27 19:14:43 +0100

    runtime: document all functions (#7612)

--- vlib/runtime/runtime.v
+++ vlib/runtime/runtime.v
@@ -6,14 +6,10 @@ module runtime
 
 import os
 
-//$if linux {
-fn C.sysconf(name int) i64
-//}
-
-//$if windows {
-fn C.GetCurrentProcessorNumber() u32
-//}
-
+// nr_jobs returns the same as `nr_cpus` with the difference that if an
+// environment variable `VJOBS` is set, and has a value > 0,
+// then `nr_jobs` will return that number instead.
+// This is useful for runtime tweaking of e.g. threaded or concurrent code.
 pub fn nr_jobs() int {
 	mut cpus := nr_cpus()
 	// allow for overrides, for example using `VJOBS=32 ./v test .`
@@ -24,21 +20,25 @@ pub fn nr_jobs() int {
 	return cpus
 }
 
+// is_32bit returns true if the current executable is running on a 32 bit system.
 pub fn is_32bit() bool {
 	$if x32 { return true }
 	return false
 }
 
+// is_64bit returns true if the current executable is running on a 64 bit system.
 pub fn is_64bit() bool {
 	$if x64 { return true }
 	return false
 }
 
+// is_little_endian returns true if the current executable is running on a little-endian system.
 pub fn is_little_endian() bool {
 	$if little_endian { return true }
 	return false
 }
 
+// is_big_endian returns true if the current executable is running on a big-endian system.
 pub fn is_big_endian() bool {
 	$if big_endian { return true }
 	return false
