08f15eb33b93a661ca04f0009a1af91fbf0f2da8
Delyan Angelov <delian66@gmail.com>
2020-12-28 12:03:50 +0200

    move test_eq() to a separate array_eq_test.v

--- vlib/builtin/array_test.v
+++ vlib/builtin/array_test.v
@@ -691,14 +691,6 @@ fn test_array_str() {
 	// assert numbers2.str() == '[[1, 2, 3], [4, 5, 6]]'
 }
 
-fn test_eq() {
-	assert [5, 6, 7] != [6, 7]
-	assert [`a`, `b`] == [`a`, `b`]
-	assert [User{age: 22, name: 'bob'}] == [User{age: 22, name: 'bob'}]
-	assert [{'bob': 22}, {'tom': 33}] == [{'bob': 22}, {'tom': 33}]
-	assert [[1, 2, 3], [4]] == [[1, 2, 3], [4]]
-}
-
 struct User {
 	age  int
 	name string
