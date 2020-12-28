3a50afce61d1c8523737accbb5c54e6cd457c94b
ka-weihe <k@weihe.dk>
2020-12-28 22:37:07 +0100

    sanitize tests

--- cmd/tools/vtest-fixed.v
+++ cmd/tools/vtest-fixed.v
@@ -5,6 +5,20 @@ import testing
 import v.pref
 
 const (
+	skip_with_sanitize  = [
+		// 'vlib/x/websocket/websocket_test.v',
+		// 'vlib/encoding/csv/reader_test.v',
+		// 'vlib/net/tcp_test.v',
+		// 'vlib/net/tcp_simple_client_server_test.v',
+		// 'vlib/net/udp_test.v',
+		// 'vlib/net/http/cookie_test.v',
+		// 'vlib/net/http/http_test.v',
+		// 'vlib/net/http/status_test.v',
+		// 'vlib/orm/orm_test.v',
+		// 'vlib/sqlite/sqlite_test.v',
+		// 'vlib/vweb/tests/vweb_test.v',
+		// 'vlib/x/websocket/websocket_test.v'
+	]
 	skip_test_files     = [
 		'vlib/net/http/http_httpbin_test.v',
 	]
@@ -53,6 +67,15 @@ fn main() {
 	mut tsession := testing.new_test_session(cmd_prefix)
 	tsession.files << all_test_files
 	tsession.skip_files << skip_test_files
+	mut sanitize := false
+	for arg in args {
+		if '-fsanitize' in arg {
+			sanitize = true
+		}
+	}
+	if sanitize {
+		tsession.skip_files << skip_with_sanitize
+	}
 	//
 	if os.getenv('V_CI_MUSL').len > 0 {
 		tsession.skip_files << skip_on_musl
