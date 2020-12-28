550d4ea7bf5424c37b84b1c84c006b900b98dc64
ka-weihe <k@weihe.dk>
2020-12-29 00:37:17 +0100

    add 3 more jobs

--- cmd/tools/vtest-fixed.v
+++ cmd/tools/vtest-fixed.v
@@ -5,8 +5,8 @@ import testing
 import v.pref
 
 const (
-	skip_with_sanitize  = [
-		'vlib/x/websocket/websocket_test.v'
+	skip_with_fsanitize_memory  = [
+		'vlib/x/websocket/websocket_test.v',
 		'vlib/encoding/csv/reader_test.v',
 		'vlib/net/tcp_test.v',
 		'vlib/net/tcp_simple_client_server_test.v',
@@ -17,8 +17,41 @@ const (
 		'vlib/orm/orm_test.v',
 		'vlib/sqlite/sqlite_test.v',
 		'vlib/vweb/tests/vweb_test.v',
-		'vlib/x/websocket/websocket_test.v',
-		'vlib/v/tests/unsafe_test.v'
+		'vlib/x/websocket/websocket_test.v'
+	]
+	skip_with_fsanitize_address  = [
+		'vlib/encoding/base64/base64_test.v',
+		'vlib/encoding/csv/reader_test.v',
+		'vlib/flag/flag_test.v',
+		'vlib/io/util/util_test.v',
+		'vlib/io/reader_test.v',
+		'vlib/json/json_test.v',
+		'vlib/net/http/cookie_test.v',
+		'vlib/os/inode_test.v',
+		'vlib/os/os_test.v',
+		'vlib/regex/regex_test.v',
+		'vlib/semver/semver_test.v',
+		'vlib/sync/channel_opt_propagate_test.v',
+		'vlib/time/parse_test.v',
+		'vlib/v/fmt/fmt_keep_test.v',
+		'vlib/v/fmt/fmt_test.v',
+		'vlib/v/tests/array_init_test.v',
+		'vlib/v/doc/doc_test.v',
+		'vlib/v/tests/const_test.v',
+		'vlib/v/tests/fn_multiple_returns_test.v',
+		'vlib/v/tests/inout/compiler_test.v',
+		'vlib/v/tests/option_default_values_test.v',
+		'vlib/v/tests/option_test.v',
+		'vlib/v/tests/ptr_arithmetic_test.v',
+		'vlib/v/tests/str_gen_test.v',
+		'vlib/v/tests/unsafe_test.v',
+		'vlib/v/tests/vmod_parser_test.v',
+		'vlib/v/vcache/vcache_test.v',
+		'vlib/x/json2/decoder_test.v',
+		'vlib/x/websocket/websocket_test.v'
+	]
+	skip_with_fsanitize_undefined  = [
+		'vlib/encoding/csv/reader_test.v'
 	]
 	skip_test_files     = [
 		'vlib/net/http/http_httpbin_test.v',
@@ -68,14 +101,28 @@ fn main() {
 	mut tsession := testing.new_test_session(cmd_prefix)
 	tsession.files << all_test_files
 	tsession.skip_files << skip_test_files
-	mut sanitize := false
+	mut sanitize_memory := false
+	mut sanitize_address := false
+	mut sanitize_undefined := false
 	for arg in args {
-		if '-fsanitize' in arg {
-			sanitize = true
+		if '-fsanitize=memory' in arg {
+			sanitize_memory = true
+		}
+		if '-fsanitize=address' in arg {
+			sanitize_address = true
+		}
+		if '-fsanitize=undefined' in arg {
+			sanitize_address = true
 		}
 	}
-	if sanitize {
-		tsession.skip_files << skip_with_sanitize
+	if sanitize_memory {
+		tsession.skip_files << skip_with_fsanitize_memory
+	}
+	if sanitize_address {
+		tsession.skip_files << skip_with_fsanitize_address
+	}
+	if sanitize_undefined {
+		tsession.skip_files << skip_with_fsanitize_undefined
 	}
 	//
 	if os.getenv('V_CI_MUSL').len > 0 {
