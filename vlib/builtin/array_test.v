f45a2fece5dfee815708a89a6f66b56802c98f70
yuyi98 <yuyi98@163.com>
2020-12-28 10:41:33 +0800

    cgen: fix gen_array_equality_fn()

--- vlib/builtin/array_test.v
+++ vlib/builtin/array_test.v
@@ -692,10 +692,11 @@ fn test_array_str() {
 }
 
 fn test_eq() {
-	/*
-	assert [5, 6, 7].eq([6, 7]) == false
-	assert [`a`, `b`].eq([`a`, `b`]) == true
-	*/
+	assert [5, 6, 7] != [6, 7]
+	assert [`a`, `b`] == [`a`, `b`]
+	assert [User{age: 22, name: 'bob'}] == [User{age: 22, name: 'bob'}]
+	assert [{'bob': 22}, {'tom': 33}] == [{'bob': 22}, {'tom': 33}]
+	assert [[1, 2, 3], [4]] == [[1, 2, 3], [4]]
 }
 
 struct User {
