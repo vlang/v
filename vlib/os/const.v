428891b4c2e0c32be8bf83b4cb0d06f72b94941a
clubby789 <jamie@hill-daniel.co.uk>
2020-12-31 21:30:22 +0000

    Move constants to os-specific file

--- vlib/os/const.v
+++ vlib/os/const.v
@@ -2,6 +2,7 @@ module os
 
 // (Must be realized in Syscall) (Must be specified)
 // ref: http://www.ccfit.nsu.ru/~deviv/courses/unix/unix/ng7c229.html
+/*
 const (
 	s_ifmt  = 0xF000 // type of file
 	s_ifdir = 0x4000 // directory
@@ -19,6 +20,7 @@ const (
 	s_iwoth = 0o0002 // Write by others
 	s_ixoth = 0o0001 // Execute by others
 )
+*/
 
 const (
 	std_input_handle  = -10
--- vlib/os/os_nix.c.v
+++ vlib/os/os_nix.c.v
@@ -17,6 +17,25 @@ const (
 	stderr_value = 2
 )
 
+pub const (
+    s_ifmt  = 0xF000 // type of file
+    s_ifdir = 0x4000 // directory
+    s_iflnk = 0xa000 // link
+    s_isuid = 0o4000 // SUID
+    s_isgid = 0o2000 // SGID
+    s_isvtx = 0o1000 // Sticky
+    s_irusr = 0o0400 // Read by owner
+    s_iwusr = 0o0200 // Write by owner
+    s_ixusr = 0o0100 // Execute by owner
+    s_irgrp = 0o0040 // Read by group
+    s_iwgrp = 0o0020 // Write by group
+    s_ixgrp = 0o0010 // Execute by group
+    s_iroth = 0o0004 // Read by others
+    s_iwoth = 0o0002 // Write by others
+    s_ixoth = 0o0001 // Execute by others
+)
+
+
 struct C.utsname {
 mut:
 	sysname  charptr
