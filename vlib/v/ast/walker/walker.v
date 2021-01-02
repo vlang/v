8d3ace17d9fe2ddb1b6b757f8dbadf1661fe6b4e
Ned Palacios <npdoesmc@gmail.com>
2021-01-02 09:52:29 +0800

    ast/walker: move fn type to a separate type

--- vlib/v/ast/walker/walker.v
+++ vlib/v/ast/walker/walker.v
@@ -6,21 +6,23 @@ pub interface Visitor {
 	visit(node ast.Node) ?Visitor
 }
 
+pub type InspectorFn = fn (node ast.Node, data voidptr) bool
+
 struct Inspector {
-	i_fn fn (node ast.Node, data voidptr) bool
+	inspector_callback InspectorFn
 mut:
 	data voidptr
 }
 
 pub fn (i &Inspector) visit(node ast.Node) ?Visitor {
-	if i.i_fn(node, i.data) {
+	if i.inspector_callback(node, i.data) {
 		return &i
 	}
 	return none
 }
 
-pub fn inspect(node ast.Node, data voidptr, i_fn fn (node ast.Node, data voidptr) bool) {
-	walk(Inspector{i_fn, data}, node)
+pub fn inspect(node ast.Node, data voidptr, inspector_callback InspectorFn) {
+	walk(Inspector{inspector_callback, data}, node)
 }
 
 pub fn walk(visitor Visitor, node ast.Node) {
