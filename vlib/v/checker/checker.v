8f3ad2ab2c53a88c6fe5426433ed1d084c115753
Daniel Däschle <daniel.daeschle@gmail.com>
2021-01-05 14:44:30 +0100

    fix error message

--- vlib/v/checker/checker.v
+++ vlib/v/checker/checker.v
@@ -3222,7 +3222,7 @@ pub fn (mut c Checker) expr(node ast.Expr) table.Type {
 					return c.comptime_fields_type[expr_name]
 				}
 			}
-			c.error('compile time field access can only be used when iteration over `T.fields`',
+			c.error('compile time field access can only be used when iterating over `T.fields`',
 				node.field_expr.position())
 			return table.void_type
 		}
