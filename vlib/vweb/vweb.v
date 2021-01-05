b4c0735ae9ff5d1316d59cbeee87e52fd8f397be
Daniel Däschle <daniel.daeschle@gmail.com>
2021-01-05 22:56:48 +0100

    vweb: fix array.last bug

--- vlib/vweb/vweb.v
+++ vlib/vweb/vweb.v
@@ -447,7 +447,7 @@ fn handle_conn<T>(mut conn net.TcpConn, mut app T) {
 							req_method << route_words[0]
 						}
 						if url_words.len == route_words.len ||
-							(url_words.len >= route_words.len - 1 && route_words.last().ends_with('...')) {
+							(url_words.len >= route_words.len - 1 && route_words.len > 0 && route_words.last().ends_with('...')) {
 							if req_method.len > 0 {
 								if req_method_str.to_lower()[1..] !in req_method {
 									continue
