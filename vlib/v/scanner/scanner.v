eaba21d81ae4821790a1b7d1c70c4a9081db5585
Lukas Neubert <serkonda@protonmail.com>
2021-01-05 15:14:16 +0100

    fmt: keep the shebang header in vsh files (#7893)

--- vlib/v/scanner/scanner.v
+++ vlib/v/scanner/scanner.v
@@ -820,9 +820,9 @@ fn (mut s Scanner) text_scan() token.Token {
 				s.ignore_line()
 				if nextc == `!` {
 					// treat shebang line (#!) as a comment
-					s.line_comment = s.text[start + 1..s.pos].trim_space()
+					comment := s.text[start - 1..s.pos].trim_space()
 					// s.fgenln('// shebang line "$s.line_comment"')
-					continue
+					return s.new_token(.comment, comment, comment.len + 2)
 				}
 				hash := s.text[start..s.pos].trim_space()
 				return s.new_token(.hash, hash, hash.len)
