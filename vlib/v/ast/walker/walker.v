e840005807a6b3a23777b602f2f121fe59ea5374
Ned Palacios <npdoesmc@gmail.com>
2021-01-06 17:18:15 +0800

    v.ast.walker: add docs

--- vlib/v/ast/walker/walker.v
+++ vlib/v/ast/walker/walker.v
@@ -2,6 +2,7 @@ module walker
 
 import v.ast
 
+// Visitor's visit method is invoked by the walker in each node it encounters.
 pub interface Visitor {
 	visit(node ast.Node) ?
 }
@@ -21,10 +22,12 @@ pub fn (i &Inspector) visit(node ast.Node) ? {
 	return none
 }
 
+// inspect traverses and checks the AST node on a depth-first order and based on the data given
 pub fn inspect(node ast.Node, data voidptr, inspector_callback InspectorFn) {
 	walk(Inspector{inspector_callback, data}, node)
 }
 
+// walk traverses the AST using the given visitor
 pub fn walk(visitor Visitor, node ast.Node) {
 	visitor.visit(node) or { return }
 	children := node.children()
