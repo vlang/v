c3c961216f4a548d427ebba20e43cbbbf9f75681
helto4real <tomas.hellstrom@yahoo.se>
2021-01-06 14:20:19 +0100

    websocket server broadcast plus examples

--- vlib/x/websocket/websocket_client.v
+++ vlib/x/websocket/websocket_client.v
@@ -333,11 +333,9 @@ pub fn (mut ws Client) close(code int, message string) ? {
 			close_frame[i + 2] = message[i]
 		}
 		ws.send_control_frame(.close, 'CLOSE', close_frame) ?
-		ws.send_close_event(code, message)
 		unsafe { close_frame.free() }
 	} else {
 		ws.send_control_frame(.close, 'CLOSE', []) ?
-		ws.send_close_event(code, '')
 	}
 	ws.fragments = []
 }
