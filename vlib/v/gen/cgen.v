4f7cf59bcda21cd18a811bc1420a41aa583bd009
BigBlack <840206@qq.com>
2021-01-11 15:28:30 +0800

    fix fixed_array slice ommit high_value

--- vlib/v/gen/cgen.v
+++ vlib/v/gen/cgen.v
@@ -3870,6 +3870,10 @@ fn (mut g Gen) index_expr(node ast.IndexExpr) {
 			g.write(', ')
 			if node.index.has_high {
 				g.expr(node.index.high)
+			} else if sym.kind == .array_fixed {
+				g.write('_ARR_LEN(')
+				g.expr(node.left)
+				g.write(')')
 			} else {
 				g.expr(node.left)
 				g.write('.len')
