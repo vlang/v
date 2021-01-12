656beaaf1522023c1c6760d8a35388560a62ffe1
Delta456 <swstkbaranwal@gmail.com>
2021-01-12 19:34:41 +0530

    fmt

--- vlib/v/gen/cgen.v
+++ vlib/v/gen/cgen.v
@@ -3247,8 +3247,7 @@ fn (mut g Gen) infix_expr(node ast.InfixExpr) {
 			g.typ((left_sym.info as table.Alias).parent_type).split('__').last()[0].is_capital()
 		// Do not generate operator overloading with these `right_sym.kind`.
 		e := right_sym.kind !in [.voidptr, .int_literal, .int]
-		if node.op in
-			[.plus, .minus, .mul, .div, .mod, .lt, .gt, .eq, .ne, .le, .ge] &&
+		if node.op in [.plus, .minus, .mul, .div, .mod, .lt, .gt, .eq, .ne, .le, .ge] &&
 			((a && b && e) || c || d) {
 			// Overloaded operators
 			g.write(g.typ(if !d {
