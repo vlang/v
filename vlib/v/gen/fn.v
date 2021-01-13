6ac664dce9d8163d20a23409dd273514bfd6b99f
Ruofan XU <47302112+SleepyRoy@users.noreply.github.com>
2021-01-13 18:24:33 +0800

    Update fn.v

--- vlib/v/gen/fn.v
+++ vlib/v/gen/fn.v
@@ -240,8 +240,9 @@ fn (mut g Gen) fn_args(args []table.Param, is_variadic bool) ([]string, []string
 				g.definitions.write(')')
 			}
 		} else {
-			s := if arg_type_sym.kind ==
-				.array_fixed { arg_type_name.trim('*') } else { arg_type_name } + ' ' + caname
+			//TODO: combine two lines into one once ternary in expression is fixed
+			mut s := if arg_type_sym.kind == .array_fixed { arg_type_name.trim('*') } else { arg_type_name } 
+			s += ' ' + caname
 			g.write(s)
 			g.definitions.write(s)
 			fargs << caname
