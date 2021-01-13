0c5cb41792972d94873623a5d4603687d434a9c6
lmp <dev.larpon@gmail.com>
2021-01-13 17:21:03 +0100

    tests: re-add embed_file test

---
+++ vlib/v/tests/embed_file/embed_file_test.v
@@ -0,0 +1,25 @@
+import os
+
+const (
+	const_file = $embed_file('v.png')
+)
+
+fn test_embed_file() {
+	mut file := const_file
+	mut out := os.join_path(os.temp_dir(), 'const_v_out.png')
+	mut fw := os.create(out) or { panic('failed to create file $out') }
+	fw.write_bytes(file.data(), file.len)
+	fw.close()
+
+	mut size := os.file_size(out)
+	assert size == file.len
+
+	file = $embed_file('v.png')
+	out = os.join_path(os.temp_dir(), 'v_out.png')
+	fw = os.create(out) or { panic('failed to create file $out') }
+	fw.write_bytes(file.data(), file.len)
+	fw.close()
+
+	size = os.file_size(out)
+	assert size == file.len
+}
