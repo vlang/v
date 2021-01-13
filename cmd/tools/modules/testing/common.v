b467431786da43bd8601d6ee2a2bf3302a3c23cf
Delyan Angelov <delian66@gmail.com>
2021-01-13 10:51:14 +0200

    re-add a newline to the fail output, if it ends in one

--- cmd/tools/modules/testing/common.v
+++ cmd/tools/modules/testing/common.v
@@ -276,7 +276,8 @@ fn worker_trunner(mut p sync.PoolProcessor, idx int, thread_id int) voidptr {
 			ts.failed = true
 			ts.benchmark.fail()
 			tls_bench.fail()
-			ts.append_message(.fail, tls_bench.step_message_fail('$relative_file\n$r.output.trim_space()'))
+			ending_newline := if r.output.ends_with('\n') { '\n' } else { '' }
+			ts.append_message(.fail, tls_bench.step_message_fail('$relative_file\n$r.output.trim_space()$ending_newline'))
 		} else {
 			ts.benchmark.ok()
 			tls_bench.ok()
