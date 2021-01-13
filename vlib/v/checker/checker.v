243353e4fadda6e3aadba1f55dc065ad4a8e57dc
Nick Treleaven <n@trelsoft.com>
2021-01-13 13:51:15 +0000

    checker: improve errors for interface method compatibility
    Make Table.is_same_method_as return error message.

--- vlib/v/checker/checker.v
+++ vlib/v/checker/checker.v
@@ -1849,10 +1849,12 @@ fn (mut c Checker) type_implements(typ table.Type, inter_typ table.Type, pos tok
 	styp := c.table.type_to_str(typ)
 	for imethod in inter_sym.methods {
 		if method := typ_sym.find_method(imethod.name) {
-			if !imethod.is_same_method_as(method) {
+			msg := c.table.is_same_method_as(imethod, method)
+			if msg.len > 0 {
 				sig := c.table.fn_signature(imethod, skip_receiver: true)
-				c.error('`$styp` incorrectly implements method `$imethod.name` of interface `$inter_sym.name`, expected `$sig`',
+				c.error('`$styp` incorrectly implements method `$imethod.name` of interface `$inter_sym.name`: $msg',
 					pos)
+				c.add_error_detail('expected `$sig`')
 				return false
 			}
 			continue
