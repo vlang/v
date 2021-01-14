da682f7678f95f9f8c44355bac2b532448b5876d
Delyan Angelov <delian66@gmail.com>
2021-01-14 15:51:10 +0200

    final windows fix ...

--- vlib/v/gen/embed.v
+++ vlib/v/gen/embed.v
@@ -6,15 +6,15 @@ import v.ast
 // gen_embed_file_struct generates C code for `$embed_file('...')` calls.
 fn (mut g Gen) gen_embed_file_init(node ast.ComptimeCall) {
 	g.writeln('(embed__EmbeddedData){')
-	g.writeln('\t.path = _SLIT("$node.embed_file.rpath"),')
-	g.writeln('\t.apath = _SLIT("$node.embed_file.apath"),')
+	g.writeln('\t.path = ${ctoslit(node.embed_file.rpath)},')
+	g.writeln('\t.apath = ${ctoslit(node.embed_file.apath)},')
 	file_size := os.file_size(node.embed_file.apath)
 	if file_size > 5242880 {
 		eprintln('Warning: embedding of files >= ~5MB is currently not supported')
 	}
 	if g.pref.is_prod {
 		// Use function generated in Gen.gen_embedded_data()
-		g.writeln('\t.compressed = _v_embed_locate_data(_SLIT("$node.embed_file.apath")),')
+		g.writeln('\t.compressed = _v_embed_locate_data(${ctoslit(node.embed_file.apath)}),')
 	}
 	g.writeln('\t.len = $file_size')
 	g.writeln('} // $' + 'embed_file("$node.embed_file.apath")')
@@ -55,7 +55,7 @@ fn (mut g Gen) gen_embedded_data() {
 	g.embedded_data.writeln('}')
 	g.embedded_data.writeln('_v_embedded_data[] = {')
 	for i, emfile in g.embedded_files {
-		g.embedded_data.writeln('\t{_SLIT("$emfile.rpath"), _v_embed_blob_$i},')
+		g.embedded_data.writeln('\t{${ctoslit(emfile.rpath)}}, _v_embed_blob_$i},')
 	}
 	g.embedded_data.writeln('\t{_SLIT(""), NULL}')
 	g.embedded_data.writeln('};')
@@ -65,7 +65,7 @@ fn (mut g Gen) gen_embedded_data() {
 // function to locate embedded data by a vstring
 byteptr _v_embed_locate_data(string id) {
 	const struct _v_embed *ve;
-	for (ve = _v_embedded_data; !string_eq(ve->id,_SLIT("")) && ve->data != NULL; ve++) {
+	for (ve = _v_embedded_data; !string_eq(ve->id, _SLIT("")) && ve->data != NULL; ve++) {
 		if (string_eq(ve->id, id)) {
 			return (byteptr) ve->data;
 		}
