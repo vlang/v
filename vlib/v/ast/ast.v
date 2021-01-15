4da648a989fcf42226f99cbb8ff02635ec4de48b
Nick Treleaven <n@trelsoft.com>
2021-01-15 18:06:53 +0000

    vfmt (wrongly changing alignment)

--- vlib/v/ast/ast.v
+++ vlib/v/ast/ast.v
@@ -190,10 +190,10 @@ pub mut:
 
 pub struct StructDecl {
 pub:
-	pos          token.Position
-	name         string
-	gen_types    []table.Type
-	is_pub       bool
+	pos       token.Position
+	name      string
+	gen_types []table.Type
+	is_pub    bool
 	// _pos fields for vfmt
 	mut_pos      int // mut:
 	pub_pos      int // pub:
