6c87c25ef1db1f6d6ba002aab873f2c5525794f6
yuyi <yuyi98@163.com>
2021-01-18 13:05:38 +0800

    cgen: fix `for _,mut j in arr` for fixed arrays (fix #8163) (#8167)

--- vlib/v/tests/for_in_mut_val_test.v
+++ vlib/v/tests/for_in_mut_val_test.v
@@ -1,12 +1,25 @@
-fn foo(mut arr []int) {
+fn foo1(mut arr []int) {
 	for _, mut j in arr {
 		j *= 2
 	}
 }
 
-fn test_for_in_mut_val() {
+fn test_for_in_mut_val_of_array() {
 	mut arr := [1, 2, 3]
-	foo(mut arr)
+	foo1(mut arr)
 	println(arr)
 	assert arr == [2, 4, 6]
+}
+
+fn foo2(mut arr [3]int) {
+	for _, mut j in arr {
+		j *= 2
+	}
+}
+
+fn test_for_in_mut_val_of_fixed_array() {
+	mut arr := [1,2,3]!
+	foo2(mut arr)
+	println(arr)
+	assert arr == [2, 4, 6]!
 }
