5b29c0d9fda75947223fc3d517ac8b6d10438b8e
Uwe Krüger <45282134+UweKrueger@users.noreply.github.com>
2021-01-19 00:38:06 +0100

    add positive tests, too

--- vlib/v/tests/array_map_or_test.v
+++ vlib/v/tests/array_map_or_test.v
@@ -4,8 +4,12 @@ fn test_array_or() {
 	el := m[4] or {
 		testvar = -43
 	}
+	good := m[1] or {
+		testvar = 11
+	}
 	assert testvar == -43
 	assert el == 0
+	assert good == 4
 }
 
 fn test_map_or() {
@@ -14,6 +18,10 @@ fn test_map_or() {
 	el := m['pp'] or {
 		testvar = 7931
 	}
+	good := m['kl'] or {
+		testvar = -45
+	}
 	assert testvar == 7931
 	assert el == 0
+	assert good == 5
 }
