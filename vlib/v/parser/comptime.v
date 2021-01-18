29556d4759655f1e2b1f1f643ea651141481269a
LouisSchmieder <dev@louis-schmieder.de>
2021-01-18 22:25:46 +0100

    remove absolute path

--- vlib/v/parser/comptime.v
+++ vlib/v/parser/comptime.v
@@ -112,7 +112,6 @@ fn (mut p Parser) comp_call() ast.ComptimeCall {
 	dir := os.dir(p.scanner.file_path)
 	mut path := os.join_path(dir, fn_path.join('/'))
 	path += '.html'
-	path = os.resource_abs_path(path)
 	if !is_html {
 		path = tmpl_path
 	}
@@ -121,7 +120,6 @@ fn (mut p Parser) comp_call() ast.ComptimeCall {
 		if is_html {
 			path = os.join_path(dir, 'templates', fn_path.join('/'))
 			path += '.html'
-			path = os.resource_abs_path(path)
 		}
 		if !os.exists(path) {
 			if is_html {
