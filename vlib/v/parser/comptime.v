205121d770143068cdd5eb31125fbdf3877ad998
LouisSchmieder <dev@louis-schmieder.de>
2021-01-19 14:09:41 +0100

    remove tests

--- vlib/v/parser/comptime.v
+++ vlib/v/parser/comptime.v
@@ -112,7 +112,6 @@ fn (mut p Parser) comp_call() ast.ComptimeCall {
 	dir := os.dir(p.scanner.file_path)
 	mut path := os.join_path(dir, fn_path.join('/'))
 	path += '.html'
-	path = os.real_path(path)
 	if !is_html {
 		path = tmpl_path
 	}
@@ -121,8 +120,6 @@ fn (mut p Parser) comp_call() ast.ComptimeCall {
 		if is_html {
 			path = os.join_path(dir, 'templates', fn_path.join('/'))
 			path += '.html'
-			path = os.real_path(path)
-			eprintln(path)
 		}
 		if !os.exists(path) {
 			if is_html {
