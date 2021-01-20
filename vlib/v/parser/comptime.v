8e74caad041aece1de13c848c1447e50471dba03
LouisSchmieder <dev@louis-schmieder.de>
2021-01-20 11:32:11 +0100

    remove debug msg

--- vlib/v/parser/comptime.v
+++ vlib/v/parser/comptime.v
@@ -122,7 +122,6 @@ fn (mut p Parser) comp_call() ast.ComptimeCall {
 			path = os.join_path(dir, 'templates', fn_path.join('/'))
 			path += '.html'
 			path = os.real_path(path)
-			eprintln(path)
 		}
 		if !os.exists(path) {
 			if is_html {
