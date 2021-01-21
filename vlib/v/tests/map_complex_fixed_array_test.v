723d88631ed85268fe31ccd9c8cdbfada1decf56
yuyi98 <yuyi98@163.com>
2021-01-22 07:28:29 +0800

    modify indent of test

--- vlib/v/tests/map_complex_fixed_array_test.v
+++ vlib/v/tests/map_complex_fixed_array_test.v
@@ -13,7 +13,7 @@ fn test_innermost_value_of_map_fixed_array() {
 	mut m := map[string][1][2]map[string]int
 	m['foo'] = [[{'bar': 1}, {'baz': 3}]!]!
 	println(m['foo'][0][0]['bar'])
-    println(m['foo'][0][0]['bar'] == 1)
+	println(m['foo'][0][0]['bar'] == 1)
 	assert m['foo'][0][0]['bar'] == 1
 	assert '${m['foo'][0][0]['bar']}' == '1'
 }
