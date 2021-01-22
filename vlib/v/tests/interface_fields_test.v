99212341d731339be9daf37c8c9ad3cfbd405281
spaceface <spaceface7777@gmail.com>
2021-01-22 20:10:17 +0100

    split up test file

--- vlib/v/tests/interface_fields_test.v
+++ vlib/v/tests/interface_fields_test.v
@@ -15,16 +15,21 @@ mut:
 	breed    string
 }
 
-fn use_interface(mut a Animal) {
+fn use_interface(a Animal) {
 	assert a.breed in ['Persian', 'Labrador']
 	if a is Cat {
 		assert a.breed == 'Persian'
-		a.breed = 'Siamese'
 	} else {
 		assert a.breed == 'Labrador'
+	}
+}
+
+fn mutate_interface(mut a Animal) {
+	if a is Cat {
+		a.breed = 'Siamese'
+	} else {
 		a.breed = 'Golden Retriever'
 	}
-	assert a.breed in ['Siamese', 'Golden Retriever']
 	if a is Cat {
 		assert a.breed == 'Siamese'
 	} else {
@@ -37,8 +42,10 @@ fn use_interface(mut a Animal) {
 fn test_interface_fields() {
 	mut c := Cat{ breed: 'Persian'}
 	mut d := Dog{ breed: 'Labrador'}
-	use_interface(mut c)
-	use_interface(mut d)
+	use_interface(c)
+	use_interface(d)
+	mutate_interface(mut c)
+	mutate_interface(mut d)
 	assert c.breed == 'what??'
 	assert d.breed == 'what??'
 }
