8a59ffb4b72bbaf53ab192b9f9fdc824a66a972b
Swastik Baranwal <swstkbaranwal@gmail.com>
2021-01-23 21:29:39 +0530

    cgen: allow `==` to be user defined if `!=` is auto generated and vice versa (#8286)

---
+++ vlib/v/tests/struct_eq_op_only_test.v
@@ -0,0 +1,15 @@
+struct User {
+	name string
+	num int
+}
+
+fn (u User) == (u1 User) bool {
+	return u.name == u1.name
+}
+
+fn test_eq_op() {
+	u1 := User{'Joe', 23}
+	u2 := User{'Joe', 24}
+	assert u1 == u2
+	assert (u1 != u2) == false
+}
