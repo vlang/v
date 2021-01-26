cf074ea48c46d9e5033e200816d36773b14f24d1
shadowninja55 <shadowninja1050@gmail.com>
2021-01-26 15:23:19 -0500

    added fireworks example

---
+++ examples/fireworks/objects/constants.v
@@ -0,0 +1,12 @@
+module objects
+
+const (
+	width           = 800
+	height          = 800
+	gravity         = Vector{0, -0.03}
+	age_rate        = 1
+	offspring_count = 100
+	rocket_radius   = 5
+	particle_radius = 2.5
+	drag            = 0.98
+)
