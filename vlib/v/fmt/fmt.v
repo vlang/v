eca72c7a32522f9c4e8539a1821991b7e0d21aca
Lukas Neubert <serkonda@protonmail.com>
2021-01-27 18:47:16 +0100

    fmt: better ternary concat wrapping
    - set buffering onyl to true if the op is one that has custom wrapping

--- vlib/v/fmt/fmt.v
+++ vlib/v/fmt/fmt.v
@@ -1449,7 +1449,7 @@ pub fn (mut f Fmt) lock_expr(lex ast.LockExpr) {
 
 pub fn (mut f Fmt) infix_expr(node ast.InfixExpr) {
 	buffering_save := f.buffering
-	if !f.buffering {
+	if !f.buffering && node.op in [.logical_or, .and, .plus] {
 		f.buffering = true
 	}
 	if node.op == .left_shift {
@@ -1587,8 +1587,9 @@ pub fn (mut f Fmt) if_expr(it ast.IfExpr) {
 				f.write(' ')
 			}
 		}
-		// When a single line if is really long, write it again as multiline
-		if single_line && f.line_len > fmt.max_len.last() {
+		// When a single line if is really long, write it again as multiline,
+		// except it is part of an InfixExpr.
+		if single_line && f.line_len > fmt.max_len.last() && !f.buffering {
 			single_line = false
 			f.single_line_if = false
 			f.out.go_back(f.line_len - if_start)
