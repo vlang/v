89bf48e3bac7697a4254d84105b00fbd976aeeac
Delyan Angelov <delian66@gmail.com>
2021-01-31 10:43:49 +0200

    ci: fix `v -W build-examples`

--- cmd/tools/vtest-all.v
+++ cmd/tools/vtest-all.v
@@ -70,11 +70,11 @@ fn get_all_commands() []Command {
 		okmsg: 'There are no _test.v file regressions.'
 	}
 	res << Command{
-		line: '$vexe $vargs -progress build-tools'
+		line: '$vexe $vargs -progress -W build-tools'
 		okmsg: 'All tools can be compiled.'
 	}
 	res << Command{
-		line: '$vexe $vargs -progress build-examples'
+		line: '$vexe $vargs -progress -W build-examples'
 		okmsg: 'All examples can be compiled.'
 	}
 	res << Command{
