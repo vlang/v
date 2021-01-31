ae60ea4ed5131497e1179a787140fdf5f08c9771
Ruofan XU <47302112+SleepyRoy@users.noreply.github.com>
2021-01-31 18:20:15 +0800

    tests: add regression tests for typeof map and array of fns (#8458)

--- vlib/v/tests/map_and_array_with_fns_test.v
+++ vlib/v/tests/map_and_array_with_fns_test.v
@@ -57,3 +57,17 @@ fn test_map_with_fns() {
 		assert func('ccccccc', '') == 27
 	}
 }
+
+fn foo3(a string) int {
+	return 10 + a.len
+}
+
+fn test_map_and_array_with_fns_typeof_and_direct_call() {
+	a := [foo3]
+	assert typeof(a).name == '[]fn (string) int'
+	assert a[0]('hello') == 15
+	b := {'one': foo3}
+	assert typeof(b).name == 'map[string]fn (string) int'
+	// TODO: enable this
+	// assert b['one']('hi') == 12
+}
