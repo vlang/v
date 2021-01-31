44b0573a2046f92b098f9765d5a51b5576d222b9
yuyi98 <yuyi98@163.com>
2021-01-31 09:12:07 +0800

    add another test

--- vlib/v/tests/fn_mut_args_test.v
+++ vlib/v/tests/fn_mut_args_test.v
@@ -4,9 +4,20 @@ fn func(mut a []int) {
 	assert '$a' == '[1, 2, 3, 4]'
 }
 
-fn test_fn_mut_args() {
+fn test_fn_mut_args_of_array() {
 	mut a := [1, 2, 3]
 	func(mut a)
 	println('inside main: $a')
 	assert '$a' == '[1, 2, 3, 4]'
+}
+
+fn init_map(mut n map[string]int) {
+	n = {'one': 1}
+}
+
+fn test_fn_mut_args_of_map() {
+	mut m := map[string]int{}
+	init_map(mut m)
+	println(m)
+	assert m == {'one': 1}
 }
