51fba5002f5cfa6d6378ccbfeff417a79b0adae2
Delyan Angelov <delian66@gmail.com>
2021-02-01 08:04:58 +0200

    cleanup examples/gg/polygons.v

--- examples/gg/polygons.v
+++ examples/gg/polygons.v
@@ -2,17 +2,10 @@ module main
 
 import gg
 import gx
-import os
-
-const (
-	win_width  = 600
-	win_height = 300
-)
 
 struct App {
 mut:
-	gg    &gg.Context
-	image gg.Image
+	gg &gg.Context
 }
 
 fn main() {
@@ -20,39 +13,22 @@ fn main() {
 		gg: 0
 	}
 	app.gg = gg.new_context(
-		bg_color: gx.white
-		width: win_width
-		height: win_height
-		use_ortho: true // This is needed for 2D drawing
-		create_window: true
+		bg_color: gx.rgb(174, 198, 255)
+		width: 600
+		height: 400
 		window_title: 'Polygons'
 		frame_fn: frame
 		user_data: app
-		init_fn: init_images
 	)
-	app.image = app.gg.create_image(os.resource_abs_path('logo.png'))
 	app.gg.run()
 }
 
-fn init_images(mut app App) {
-	// app.image = gg.create_image('logo.png')
-}
-
-fn frame(app &App) {
+fn frame(mut app App) {
 	app.gg.begin()
-	app.draw()
+	app.gg.draw_convex_poly([f32(100.0), 200.0, 300.0, 200.0, 100.0], [f32(100.0), 100.0, 200.0,
+		300.0, 300.0], gx.blue)
+	app.gg.draw_empty_poly([f32(50.0), 70.0, 90.0, 70.0], [f32(50.0), 60.0, 80.0, 110.0],
+		gx.black)
+	app.gg.draw_triangle(450, 142, 530, 280, 370, 280, gx.red)
 	app.gg.end()
-}
-
-fn (app &App) draw() {
-	// app.gg.draw_text_def(200,20, 'hello world!')
-	// app.gg.draw_text_def(300,300, 'привет')
-	// app.gg.draw_rect(10, 10, 100, 30, gx.blue)
-	// app.gg.draw_empty_rect(110, 150, 80, 40, gx.black)
-	// app.gg.draw_image(230, 30, app.image.width, app.image.height, app.image)
-	app.gg.draw_convex_poly([f32(100.0), f32(200.0), f32(300.0), f32(200.0), f32(100.0)],
-		[f32(100.0), f32(100.0), f32(200.0), f32(300.0), f32(300.0)], gx.blue)
-	app.gg.draw_empty_poly([f32(50.0), f32(70.0), f32(90.0), f32(70.0)], [f32(50.0), f32(60.0),
-		f32(80.0), f32(110.0)], gx.black)
-	// app.gg.draw_triangle(10, 20, 30, 10, 10, 10, gx.blue)
 }
