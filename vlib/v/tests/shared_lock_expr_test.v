9a6b2523d712bd43541aa4eaff22ef2ecb778b17
Uwe Krüger <45282134+UweKrueger@users.noreply.github.com>
2021-02-03 21:09:21 +0100

    use both `lock` and `rlock` in expression test case

--- vlib/v/tests/shared_lock_expr_test.v
+++ vlib/v/tests/shared_lock_expr_test.v
@@ -7,7 +7,7 @@ fn test_lock_expr() {
 	shared xx := St{ i: 173 }
 	shared y := St{ i: -57 }
 	mut m := 0
-	m = rlock y { y.i }
+	m = lock y { y.i }
 	n := rlock xx { xx.i }
 	assert m == -57
 	assert n == 173
