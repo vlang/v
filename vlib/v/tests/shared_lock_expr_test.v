35d79bcb35b8df5e85d5b0a9f0ee77b5db7f7ead
Uwe Krüger <45282134+UweKrueger@users.noreply.github.com>
2021-02-03 21:08:41 +0100

    add test case for `lock` expression

---
+++ vlib/v/tests/shared_lock_expr_test.v
@@ -0,0 +1,14 @@
+struct St {
+mut:
+	i int
+}
+
+fn test_lock_expr() {
+	shared xx := St{ i: 173 }
+	shared y := St{ i: -57 }
+	mut m := 0
+	m = rlock y { y.i }
+	n := rlock xx { xx.i }
+	assert m == -57
+	assert n == 173
+}
