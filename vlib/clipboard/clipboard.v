4c2bfa5d0051b31c8f8c2a1cb2c16798db4a12f2
Rafik El Hadi Houari <rehhouari@gmail.com>
2021-02-03 03:14:19 +0100

    use constants instead of calling the functions multiple times

--- vlib/clipboard/clipboard.v
+++ vlib/clipboard/clipboard.v
@@ -8,11 +8,17 @@ pub fn new() &Clipboard {
 	return new_clipboard()
 }
 
+const wayland_display = os.getenv('WAYLAND_DISPLAY') != ''
+
+const has_wl_clipboard = os.exists_in_system_path('wl-copy') && os.exists_in_system_path('wl-paste')
+
+const is_termux = os.exists_in_system_path('termux-clipboard-set')
+
 // copy copies `text` into the clipboard.
 pub fn (mut cb Clipboard) copy(text string) bool {
 	// WayLand workaround using wl-clipboard.
-	if os.getenv('WAYLAND_DISPLAY') != '' {
-		if os.exists_in_system_path('wl-copy') && os.exists_in_system_path('wl-paste') {
+	if clipboard.wayland_display {
+		if clipboard.has_wl_clipboard {
 			success := os.system('wl-copy $text') == 0
 			cb.is_owner = success
 			return success
@@ -21,7 +27,7 @@ pub fn (mut cb Clipboard) copy(text string) bool {
 		}
 	}
 	// Termux clipboard support
-	if os.exists_in_system_path('termux-clipboard-set') && os.exists_in_system_path('termux-clipboard-get') {
+	if clipboard.is_termux {
 		success := os.system('termux-clipboard-set $text') == 0
 		cb.is_owner = success
 		return success
@@ -33,20 +39,16 @@ pub fn (mut cb Clipboard) copy(text string) bool {
 // paste returns current entry as a `string` from the clipboard.
 pub fn (mut cb Clipboard) paste() string {
 	// WayLand workaround using wl-clipboard.
-	if os.getenv('WAYLAND_DISPLAY') != '' {
-		if os.exists_in_system_path('wl-copy') && os.exists_in_system_path('wl-paste') {
-			result := os.exec('wl-paste --no-newline') or {
-				return ''
-			}
+	if clipboard.wayland_display {
+		if clipboard.has_wl_clipboard {
+			result := os.exec('wl-paste --no-newline') or { return '' }
 			return result.output
 		}
-	} else if os.exists_in_system_path('termux-clipboard-set') && os.exists_in_system_path('termux-clipboard-get') {
-			// Termux clipboard support
-			result := os.exec('termux-clipboard-get') or {
-				return ''
-			}
-			return result.output
-	} 
+	} else if clipboard.is_termux {
+		// Termux clipboard support
+		result := os.exec('termux-clipboard-get') or { return '' }
+		return result.output
+	}
 	return cb.get_text()
 }
 
@@ -54,12 +56,12 @@ pub fn (mut cb Clipboard) paste() string {
 pub fn (mut cb Clipboard) clear_all() {
 	// wayland and termux implementation have different ways to clear clipboard
 	// check for them before
-	if os.getenv('WAYLAND_DISPLAY') != '' {
-		if os.exists_in_system_path('wl-copy') && os.exists_in_system_path('wl-paste') {
-			 os.system('wl-copy -c')
-			 cb.is_owner = false
+	if clipboard.wayland_display {
+		if clipboard.has_wl_clipboard {
+			os.system('wl-copy -c')
+			cb.is_owner = false
 		}
-	} else if os.exists_in_system_path('termux-clipboard-set') && os.exists_in_system_path('termux-clipboard-get') {
+	} else if clipboard.is_termux {
 		os.system('termux-clipboard-set  ""')
 		cb.is_owner = false
 	} else {
@@ -70,7 +72,9 @@ pub fn (mut cb Clipboard) clear_all() {
 // destroy destroys the clipboard and free it's resources.
 pub fn (mut cb Clipboard) destroy() {
 	// nothing to destroy in case of wayland and termux
-	if os.getenv('WAYLAND_DISPLAY') != '' || os.exists_in_system_path('termux-clipboard-set') { return }
+	if clipboard.wayland_display || clipboard.is_termux {
+		return
+	}
 	cb.free()
 }
 
@@ -81,10 +85,12 @@ pub fn (cb Clipboard) check_ownership() bool {
 
 // is_available returns `true` if the clipboard is available for use.
 pub fn (cb &Clipboard) is_available() bool {
-	if os.getenv('WAYLAND_DISPLAY') != '' {
-		return (os.exists_in_system_path('wl-copy') && os.exists_in_system_path('wl-paste'))
+	if clipboard.wayland_display {
+		return (clipboard.has_wl_clipboard)
 	}
-	if os.exists_in_system_path('termux-clipboard-set') { return true }
+	if clipboard.is_termux {
+		return true
+	}
 
 	return cb.check_availability()
 }
