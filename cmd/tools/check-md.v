0d8a2caa17082350661c1cef74760d894516ff53
Lukas Neubert <serkonda@protonmail.com>
2021-02-03 19:28:51 +0100

    improve file ignores

--- cmd/tools/check-md.v
+++ cmd/tools/check-md.v
@@ -110,10 +110,7 @@ fn md_file_paths(dir string) []string {
 	mut files_to_check := []string{}
 	md_files := os.walk_ext(dir, '.md')
 	for file in md_files {
-		if file.starts_with('./thirdparty') {
-			continue
-		}
-		if file.contains('CHANGELOG') {
+		if file.contains_any_substr(['/thirdparty/', 'CHANGELOG']) {
 			continue
 		}
 		files_to_check << file
