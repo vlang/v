cfb3bb50510f0862b1841611d4b5a588ff09d823
Delyan Angelov <delian66@gmail.com>
2021-02-04 08:29:15 +0200

    fix tests, improve error output further

--- vlib/v/builder/builder.v
+++ vlib/v/builder/builder.v
@@ -2,6 +2,7 @@ module builder
 
 import os
 import v.ast
+import v.token
 import v.table
 import v.pref
 import v.util
@@ -96,9 +97,8 @@ pub fn (mut b Builder) parse_imports() {
 		ast_file := b.parsed_files[i]
 		for imp in ast_file.imports {
 			mod := imp.mod
-			fileline := '\n$ast_file.path:$imp.pos.line_nr'
 			if mod == 'builtin' {
-				verror('$fileline: cannot import module "builtin"')
+				error_with_pos('cannot import module "builtin"', ast_file.path, imp.pos)
 				break
 			}
 			if mod in done_imports {
@@ -107,20 +107,23 @@ pub fn (mut b Builder) parse_imports() {
 			import_path := b.find_module_path(mod, ast_file.path) or {
 				// v.parsers[i].error_with_token_index('cannot import module "$mod" (not found)', v.parsers[i].import_table.get_import_tok_idx(mod))
 				// break
-				verror('$fileline: cannot import module "$mod" (not found)')
+				error_with_pos('cannot import module "$mod" (not found)', ast_file.path,
+					imp.pos)
 				break
 			}
 			v_files := b.v_files_from_dir(import_path)
 			if v_files.len == 0 {
 				// v.parsers[i].error_with_token_index('cannot import module "$mod" (no .v files in "$import_path")', v.parsers[i].import_table.get_import_tok_idx(mod))
-				verror('$fileline: cannot import module "$mod" (no .v files in "$import_path")')
+				error_with_pos('cannot import module "$mod" (no .v files in "$import_path")',
+					ast_file.path, imp.pos)
 			}
 			// Add all imports referenced by these libs
 			parsed_files := parser.parse_files(v_files, b.table, b.pref, b.global_scope)
 			for file in parsed_files {
 				if file.mod.name != mod {
 					// v.parsers[pidx].error_with_token_index('bad module definition: ${v.parsers[pidx].file_path} imports module "$mod" but $file is defined as module `$p_mod`', 1
-					verror('$fileline: bad module definition: $ast_file.path imports module "$mod" but $file.path is defined as module `$file.mod.name`')
+					error_with_pos('bad module definition: $ast_file.path imports module "$mod" but $file.path is defined as module `$file.mod.name`',
+						ast_file.path, imp.pos)
 				}
 			}
 			b.parsed_files << parsed_files
@@ -389,6 +392,12 @@ struct FunctionRedefinition {
 	fline   int
 	fheader string
 	f       ast.FnDecl
+}
+
+fn error_with_pos(s string, fpath string, pos token.Position) {
+	ferror := util.formatted_error('builder error:', s, fpath, pos)
+	eprintln(ferror)
+	exit(1)
 }
 
 fn verror(s string) {
