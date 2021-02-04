1e9ec6a126ab9a6c2a4b074c1d4c623f1633d52d
Delyan Angelov <delian66@gmail.com>
2021-02-04 16:00:08 +0200

    vfmt: exit(1) after vfmt-ing a file with a syntax error

--- cmd/tools/vfmt.v
+++ cmd/tools/vfmt.v
@@ -155,6 +155,7 @@ fn main() {
 		if foptions.is_c {
 			exit(2)
 		}
+		exit(1)
 	}
 }
 
@@ -276,7 +277,7 @@ fn (foptions &FormatOptions) post_process_file(file string, formatted_file_path 
 }
 
 fn (f FormatOptions) str() string {
-	return
+	return 
 		'FormatOptions{ is_l: $f.is_l, is_w: $f.is_w, is_diff: $f.is_diff, is_verbose: $f.is_verbose,' +
 		' is_all: $f.is_all, is_worker: $f.is_worker, is_debug: $f.is_debug, is_noerror: $f.is_noerror,' +
 		' is_verify: $f.is_verify" }'
