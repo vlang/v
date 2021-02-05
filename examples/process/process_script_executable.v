cb00ea6429ee90274adc9cffc76a6b173d1007d0
kristof <kristof@incubaid.com>
2021-02-05 13:39:22 +0200

    more process examples

--- examples/process/process_script_executable.v
+++ examples/process/process_script_executable.v
@@ -1,17 +1,16 @@
 module main
 import os
 
-//a test where we execute a bash script but work around where we put script in bash inside bash
+//test where we try to run script directly as script which is executable
 
 fn exec(path string, redirect bool) {
+	// mut args := []string
 	mut line := ""
 	mut line_err := ""
-	mut cmd := os.new_process("/bin/bash")
-
+	
+	mut cmd := os.new_process(path)
 	if redirect{
-		cmd.set_args(['-c', '/bin/bash /tmp/test.sh 2>&1'])
-	}else{
-		cmd.set_args([path])
+		cmd.set_args(['2>&1'])
 	}
 	
 	cmd.set_redirect_stdio()
@@ -42,14 +41,15 @@ fn exec(path string, redirect bool) {
 fn main() {
 
 	script:="
-echo line 1
+#!/bin/bash
+echo 1
 #will use some stderr now
-echo redirect 1 to 2  1>&2
-echo line 3
+echo 2 1>&2
+echo 3
 "
 
 	os.write_file("/tmp/test.sh",script) or {panic(err)}
-	// os.chmod("/tmp/test.sh",0o700) //make executable
+	os.chmod("/tmp/test.sh",0o700) 
 
 	//this will work because stderr/stdout are smaller than 4096 chars, once larger there can be deadlocks
 	//in other words this can never work reliably without being able to check if there is data on stderr or stdout
