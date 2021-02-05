b12cab01fa0f34106b89a274af1b342f4e340457
kristof <kristof@incubaid.com>
2021-02-05 12:00:47 +0200

    process examples

---
+++ examples/process/execve.v
@@ -0,0 +1,25 @@
+module main
+import time
+import os
+
+fn exec(args []string) {
+	mut out := ""
+	mut line := ""
+	mut line_err := ""
+
+	os.execve("/bin/bash", args, []) or {
+		// eprintln(err)
+		panic(err)
+	}
+}
+
+
+fn main(){
+
+	// exec(["-c","find /"]) //works
+	exec(["-c","find /tmp/"]) //here it works as well
+	
+	// exec(["-c","find","/tmp/"])  // does not work I guess is normal
+
+
+}
