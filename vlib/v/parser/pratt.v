ff1aa0645519a3375c06950fc50ab92c4b549282
Nick Treleaven <n@trelsoft.com>
2021-02-07 22:10:16 +0000

    parser: require ++/-- to be on the same line as the previous token (#8621)

--- vlib/v/parser/pratt.v
+++ vlib/v/parser/pratt.v
@@ -377,6 +377,10 @@ pub fn (mut p Parser) expr_with_left(left ast.Expr, precedence int, is_stmt_iden
 				p.warn_with_pos('`$p.tok.kind` operator can only be used as a statement',
 					p.peek_tok.position())
 			}
+			if p.tok.kind in [.inc, .dec] && p.prev_tok.line_nr != p.tok.line_nr {
+				p.error_with_pos('$p.tok must be on the same line as the previous token',
+					p.tok.position())
+			}
 			node = ast.PostfixExpr{
 				op: p.tok.kind
 				expr: node
