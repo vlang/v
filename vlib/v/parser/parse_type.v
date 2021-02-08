09cff699193537889b4255ef3d931cb4067abd43
Swastik Baranwal <swstkbaranwal@gmail.com>
2021-02-08 23:21:05 +0530

    all: allow using aliases as keys in map (#8589)

--- vlib/v/parser/parse_type.v
+++ vlib/v/parser/parse_type.v
@@ -73,12 +73,18 @@ pub fn (mut p Parser) parse_map_type() table.Type {
 	}
 	p.check(.lsbr)
 	key_type := p.parse_type()
+	is_alias := p.table.get_type_symbol(key_type).kind == .alias
 	if key_type.idx() == 0 {
 		// error is reported in parse_type
 		return 0
 	}
-	if !(key_type in [table.string_type_idx, table.voidptr_type_idx]
+	if is_alias && !(key_type in [table.string_type_idx, table.voidptr_type_idx]
 		|| ((key_type.is_int() || key_type.is_float()) && !key_type.is_ptr())) {
+		p.error('cannot use the alias type as the parent type is unsupported')
+		return 0
+	}
+	if !(key_type in [table.string_type_idx, table.voidptr_type_idx]
+		|| ((key_type.is_int() || key_type.is_float() || is_alias) && !key_type.is_ptr())) {
 		s := p.table.type_to_str(key_type)
 		p.error_with_pos('maps only support string, integer, float, rune or voidptr keys for now (not `$s`)',
 			p.tok.position())
--- vlib/v/table/types.v
+++ vlib/v/table/types.v
@@ -550,7 +550,7 @@ pub fn (t &TypeSymbol) is_pointer() bool {
 
 [inline]
 pub fn (t &TypeSymbol) is_int() bool {
-	return t.kind in [.i8, .i16, .int, .i64, .byte, .u16, .u32, .u64, .int_literal]
+	return t.kind in [.i8, .i16, .int, .i64, .byte, .u16, .u32, .u64, .int_literal, .rune]
 }
 
 [inline]
