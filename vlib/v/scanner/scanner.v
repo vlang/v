a2d862360cef1256d12dbfcf8487b4d89024e8a7
Lukas Neubert <serkonda@protonmail.com>
2021-02-13 20:04:30 +0100

    format

--- vlib/v/scanner/scanner.v
+++ vlib/v/scanner/scanner.v
@@ -609,7 +609,7 @@ fn (mut s Scanner) text_scan() token.Token {
 			// end of `$expr`
 			// allow `'$a.b'` and `'$a.c()'`
 			if s.is_inter_start && next_char == `\\`
-				&& s.look_ahead(2) !in [`x`, `n`, `r`, `\\`, `t`, `e`, `"`, `'`] {
+				&& s.look_ahead(2) !in [`x`, `n`, `r`, `\\`, `t`, `e`, `"`, `\'`] {
 				s.warn('unknown escape sequence \\${s.look_ahead(2)}')
 			}
 			if s.is_inter_start && next_char == `(` {
