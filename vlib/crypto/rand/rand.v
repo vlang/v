33d80748468ebf850acd6b392291964ffb949e16
Nick Treleaven <n@trelsoft.com>
2021-02-17 19:47:19 +0000

    crypto.rand: use byteptr.vbytes instead of c_array_to_bytes_tmp (#8786)

--- vlib/crypto/rand/rand.v
+++ vlib/crypto/rand/rand.v
@@ -7,20 +7,3 @@ module rand
 const (
 	read_error = error('crypto.rand.read() error reading random bytes')
 )
-
-// NOTE: temp until we have []bytes(buff)
-fn c_array_to_bytes_tmp(len int, buffer voidptr) []byte {
-
-	mut arr := []byte{len:len, cap:1}
-	arr.data = buffer
-	/*
-
-	arr = array {
-		len: len
-		cap: 1
-		element_size: 1
-		data: buffer
-	}
-	*/
-	return arr
-}
