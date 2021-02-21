84b435cff3c2b1531fdcfc22a2d1add6c1a1873d
LouisSchmieder <dev@louis-schmieder.de>
2021-02-21 11:32:24 +0100

    fix examples

--- examples/vweb/vweb_assets/vweb_assets.v
+++ examples/vweb/vweb_assets/vweb_assets.v
@@ -23,7 +23,7 @@ pub fn (mut app App) init_once() {
 	// app.handle_static('assets')
 	// This would make available all known static mime types from current
 	// directory and below.
-	app.handle_static('.')
+	app.handle_static('.', false)
 }
 
 pub fn (mut app App) init() {
