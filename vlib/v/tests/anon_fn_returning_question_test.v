33d7883e9f5a6ec41559f4aba6eea9ce1a4d4a6c
Lukas Neubert <serkonda@protonmail.com>
2021-02-24 21:21:44 +0100

    1

--- vlib/v/tests/anon_fn_returning_question_test.v
+++ vlib/v/tests/anon_fn_returning_question_test.v
@@ -7,11 +7,11 @@ fn test_calling_an_anon_function_returning_question() {
 fn create_and_call_anon_function() ? {
 	x := fn (a string, b int) ? {
 		println('test')
-        // NB: the anon function does NOT return explicitly,
-        // so V should generate an implicit "OK" value and
-        // return it. Previously, it created an implicit optional
-        // filled with 0s => .ok was false, and that was treated
-        // as a failure, triggering or blocks.
+		// NB: the anon function does NOT return explicitly,
+		// so V should generate an implicit "OK" value and
+		// return it. Previously, it created an implicit optional
+		// filled with 0s => .ok was false, and that was treated
+		// as a failure, triggering or blocks.
 	}
 	should_not_call_block(x) ?
 	assert true
--- vlib/v/tests/array_append_short_struct_test.v
+++ vlib/v/tests/array_append_short_struct_test.v
@@ -1,5 +1,5 @@
 struct Page {
-    contents int
+	contents int
 }
 
 fn test_array_append_short_struct() {
@@ -8,7 +8,9 @@ fn test_array_append_short_struct() {
 		contents: 3
 	}
 	println(pages)
-	assert pages == [Page{contents: 3}]
+	assert pages == [Page{
+		contents: 3
+	}]
 }
 
 struct Container {
@@ -18,8 +20,12 @@ pub mut:
 
 fn test_array_insert_or_prepend_short_struct() {
 	mut a := []Container{}
-	a.prepend({name: 'a'})
-	a.insert(0, {name: 'b'})
+	a.prepend(name: 'a')
+	a.insert(0, name: 'b')
 	println(a)
-	assert a == [Container{name: 'b'}, Container{name: 'a'}]
+	assert a == [Container{
+		name: 'b'
+	}, Container{
+		name: 'a'
+	}]
 }
