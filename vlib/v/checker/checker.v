df8a16e599edf96acd658de6e1b0a573f5935825
Nick Treleaven <n@trelsoft.com>
2021-02-26 18:38:41 +0000

    checker: check `init` fn has no params or return type

--- vlib/v/checker/checker.v
+++ vlib/v/checker/checker.v
@@ -5778,6 +5778,14 @@ fn (mut c Checker) fn_decl(mut node ast.FnDecl) {
 			c.ensure_type_exists(arg.typ, node.pos) or { return }
 		}
 	}
+	if node.language == .v && node.name.after_char(`.`) == 'init' && !node.is_method {
+		if node.params.len > 0 {
+			c.error('fn `init` cannot have parameters', node.pos)
+		}
+		if node.return_type != table.void_type {
+			c.error('fn `init` cannot have a return type', node.pos)
+		}
+	}
 	if node.return_type != table.Type(0) {
 		c.ensure_type_exists(node.return_type, node.pos) or { return }
 		if node.language == .v && node.is_method && node.name == 'str' {
