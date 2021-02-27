a1244a9f5f62dfe2c6cbaa0459fdfacf29a6220e
Nick Treleaven <n@trelsoft.com>
2021-02-27 09:11:20 +0000

    checker: check that the module `init` fn, should have no params, and no return type (#8988)

--- vlib/v/checker/checker.v
+++ vlib/v/checker/checker.v
@@ -5797,6 +5797,15 @@ fn (mut c Checker) fn_decl(mut node ast.FnDecl) {
 			c.ensure_type_exists(arg.typ, node.pos) or { return }
 		}
 	}
+	if node.language == .v && node.name.after_char(`.`) == 'init' && !node.is_method
+		&& node.params.len == 0 {
+		if node.is_pub {
+			c.error('fn `init` must not be public', node.pos)
+		}
+		if node.return_type != table.void_type {
+			c.error('fn `init` cannot have a return type', node.pos)
+		}
+	}
 	if node.return_type != table.Type(0) {
 		c.ensure_type_exists(node.return_type, node.pos) or { return }
 		if node.language == .v && node.is_method && node.name == 'str' {
