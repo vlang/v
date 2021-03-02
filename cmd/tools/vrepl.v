0cbb1b3d4e8bd4dbeb3b599b4ae7109d7c1962f4
Delta456 <swstkbaranwal@gmail.com>
2021-03-02 19:45:52 +0530

    more changes

--- cmd/tools/vrepl.v
+++ cmd/tools/vrepl.v
@@ -23,17 +23,19 @@ mut:
 	functions_name []string // all the user function names
 	lines          []string // all the other lines/statements
 	temp_lines     []string // all the temporary expressions/printlns
+	vstartup_lines []string // lines in the `VSTARTUP` file
 }
 
 const is_stdin_a_pipe = (is_atty(0) == 0)
 
 const vexe = os.getenv('VEXE')
-const startup = os.getenv('VSTARTUP')
+const vstartup = os.getenv('VSTARTUP')
 
 fn new_repl() Repl {
 	return Repl{
 		readline: readline.Readline{}
 		modules: ['os', 'time', 'math']
+		vstartup_lines: os.read_file(vstartup) or { '' }.trim_right('\n\r').split_into_lines()
 	}
 }
 
@@ -79,11 +81,12 @@ fn (r &Repl) current_source_code(should_add_temp_lines bool, not_add_print bool)
 	for mod in r.modules {
 		all_lines << 'import $mod\n'
 	}
-	if startup != '' {
-		l := os.read_file(startup) or { '' }
-		mut lines := l.trim_right('\n\r').split_into_lines()
-		if not_add_print {
-			lines = lines.filter(!it.starts_with('print'))
+	if vstartup != '' {
+		mut lines := []string{}
+		if !not_add_print {
+			lines = r.vstartup_lines.filter(!it.starts_with('print'))
+		} else {
+			lines = r.vstartup_lines	
 		}
 		all_lines << lines
 	}
@@ -114,8 +117,9 @@ fn run_repl(workdir string, vrepl_prefix string) {
 		println('Use Ctrl-C or `exit` to exit, or `help` to see other available commands')
 	}
 
-	if startup != '' {
-		result := repl_run_vfile(startup) or { os.Result{output: 'file not found'} }
+	if vstartup != '' {
+		result := repl_run_vfile(vstartup) or { os.Result{output: '$vstartup file not found'} }
+		print('\n')
 		print_output(result)
 	}
 	
@@ -186,7 +190,7 @@ fn run_repl(workdir string, vrepl_prefix string) {
 			continue
 		}
 		if r.line == 'list' {
-			source_code := r.current_source_code(true, false)
+			source_code := r.current_source_code(true, true)
 			println('//////////////////////////////////////////////////////////////////////////////////////')
 			println(source_code)
 			println('//////////////////////////////////////////////////////////////////////////////////////')
@@ -199,7 +203,7 @@ fn run_repl(workdir string, vrepl_prefix string) {
 			r.line = 'println(' + r.line[1..] + ')'
 		}
 		if r.line.starts_with('print') {
-			source_code := r.current_source_code(false, true) + '\n$r.line\n'
+			source_code := r.current_source_code(false, false) + '\n$r.line\n'
 			os.write_file(file, source_code) or { panic(err) }
 			s := repl_run_vfile(file) or { return }
 			print_output(s)
@@ -254,10 +258,10 @@ fn run_repl(workdir string, vrepl_prefix string) {
 			if temp_line.starts_with('import ') {
 				mod := r.line.fields()[1]
 				if mod !in r.modules {
-					temp_source_code = '$temp_line\n' + r.current_source_code(false, false)
+					temp_source_code = '$temp_line\n' + r.current_source_code(false, true)
 				}
 			} else if temp_line.starts_with('#include ') {
-				temp_source_code = '$temp_line\n' + r.current_source_code(false, true)
+				temp_source_code = '$temp_line\n' + r.current_source_code(false, false)
 			} else {
 				for i, l in r.lines {
 					if (l.starts_with('for ') || l.starts_with('if ')) && l.contains('println') {
@@ -265,7 +269,7 @@ fn run_repl(workdir string, vrepl_prefix string) {
 						break
 					}
 				}
-				temp_source_code = r.current_source_code(true, true) + '\n$temp_line\n'
+				temp_source_code = r.current_source_code(true, false) + '\n$temp_line\n'
 			}
 			os.write_file(temp_file, temp_source_code) or { panic(err) }
 			s := repl_run_vfile(temp_file) or { return }
