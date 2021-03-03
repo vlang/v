40ba86f5220501d0cc2b788a53d34ad2929b5560
James Addison <64211121+jaddison06@users.noreply.github.com>
2021-03-03 18:15:49 +0000

    Update termios_nix.c.v

--- vlib/term/ui/termios_nix.c.v
+++ vlib/term/ui/termios_nix.c.v
@@ -78,6 +78,9 @@ fn (mut ctx Context) termios_setup() ? {
 
 	if ctx.cfg.hide_cursor {
 		ctx.hide_cursor()
+		ctx.flush() Member
+
+these should have a ctx.flush() call after them - otherwise they w
 	}
 
 	if ctx.cfg.window_title != '' {
