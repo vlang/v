97f9abcf82d05132f01bbdb90299434b6bb4344c
yuyi <yuyi98@163.com>
2021-03-03 16:12:08 +0800

    cgen: fix anon_fn in containers (#9077)

--- vlib/v/tests/anon_fn_in_containers_test.v
+++ vlib/v/tests/anon_fn_in_containers_test.v
@@ -11,3 +11,27 @@ fn test_anon_fn_in_map() {
 	}
 	assert woop['shat']() == 'shoopity shoop'
 }
+
+fn test_anon_fn_in_array() {
+	mut woop := [fn() string {
+		return 'whoopity whoop'
+	}]
+	assert woop[0]() == 'whoopity whoop'
+
+	woop[0] = fn() string {
+		return 'shoopity shoop'
+	}
+	assert woop[0]() == 'shoopity shoop'
+}
+
+fn test_anon_fn_in_fixed_array() {
+	mut woop := [fn() string {
+		return 'whoopity whoop'
+	}]!
+	assert woop[0]() == 'whoopity whoop'
+
+	woop[0] = fn() string {
+		return 'shoopity shoop'
+	}
+	assert woop[0]() == 'shoopity shoop'
+}
