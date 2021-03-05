4050e54676e252e1233d459868cbec208889f210
Joe Conigliaro <joe@vlang.io>
2021-03-05 18:38:04 +1100

    use _FILETIME

--- vlib/time/time_windows.c.v
+++ vlib/time/time_windows.c.v
@@ -15,7 +15,7 @@ struct C.tm {
 	tm_sec  int
 }
 
-struct C.FILETIME {
+struct C._FILETIME {
 }
 
 struct SystemTime {
@@ -29,9 +29,9 @@ struct SystemTime {
 	millisecond u16
 }
 
-fn C.GetSystemTimeAsFileTime(lpSystemTimeAsFileTime C.FILETIME)
+fn C.GetSystemTimeAsFileTime(lpSystemTimeAsFileTime C._FILETIME)
 
-fn C.FileTimeToSystemTime(lpFileTime &C.FILETIME, lpSystemTime C.LPSYSTEMTIME)
+fn C.FileTimeToSystemTime(lpFileTime &C._FILETIME, lpSystemTime C.LPSYSTEMTIME)
 
 fn C.SystemTimeToTzSpecificLocalTime(lpTimeZoneInformation &C.TIME_ZONE_INFORMATION, lpUniversalTime &SystemTime, lpLocalTime C.LPSYSTEMTIME)
 
@@ -124,7 +124,7 @@ pub fn (t Time) local() Time {
 // GetSystemTimeAsFileTime is used and converted to local time. It can resolve time
 // down to millisecond. Other more precice methods can be implemented in the future
 fn win_now() Time {
-	ft_utc := C.FILETIME{}
+	ft_utc := C._FILETIME{}
 	C.GetSystemTimeAsFileTime(&ft_utc)
 	st_utc := SystemTime{}
 	C.FileTimeToSystemTime(&ft_utc, &st_utc)
@@ -147,7 +147,7 @@ fn win_now() Time {
 // GetSystemTimeAsFileTime is used. It can resolve time down to millisecond
 // other more precice methods can be implemented in the future
 fn win_utc() Time {
-	ft_utc := C.FILETIME{}
+	ft_utc := C._FILETIME{}
 	C.GetSystemTimeAsFileTime(&ft_utc)
 	st_utc := SystemTime{}
 	C.FileTimeToSystemTime(&ft_utc, &st_utc)
