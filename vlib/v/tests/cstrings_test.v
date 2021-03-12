298fa89d170c5e893f14757f27d2e43e75fca9b4
zakuro <z@kuro.red>
2021-03-12 19:37:23 +0900

    rename

--- vlib/v/tests/cstrings_test.v
+++ vlib/v/tests/cstrings_test.v
@@ -14,14 +14,3 @@ fn test_cstring_with_zeros() {
 	h := s.bytes().hex()
 	assert h == '00757365726e616d650070617373776f7264'
 }
-
-fn test_raw_string() {
-	assert r'\n\u00c0' == '\\n\\u00c0'
-}
-
-fn test_escape() {
-	assert '\x20' == ' '
-	assert '\u0020' == ' '
-	// assert '\u00c4' == 'Ä'
-	assert '\r\n'.bytes() == [byte(0x0d), 0x0a]
-}
