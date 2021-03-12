9498014daa6e56d0b6d258703989bfe6bdd0b41f
Lukas Neubert <serkonda@protonmail.com>
2021-03-12 18:12:46 +0100

    use correct command in v remove err msg

--- cmd/tools/vpm.v
+++ cmd/tools/vpm.v
@@ -313,7 +313,7 @@ fn vpm_remove(module_names []string) {
 		exit(0)
 	}
 	if module_names.len == 0 {
-		println('  v update requires *at least one* module name')
+		println('´v remove´ requires *at least one* module name.')
 		exit(2)
 	}
 	for name in module_names {
