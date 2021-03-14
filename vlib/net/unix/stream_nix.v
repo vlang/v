6a7a4ea3cf7f9e60d2626ebd0c0fd8333a11ddcc
Nick Treleaven <n@trelsoft.com>
2021-03-14 13:47:18 +0000

    more fixes

--- vlib/net/unix/stream_nix.v
+++ vlib/net/unix/stream_nix.v
@@ -64,7 +64,7 @@ fn (mut s StreamSocket) connect(a string) ? {
 	mut addr := C.sockaddr_un{}
 	unsafe { C.memset(&addr, 0, sizeof(C.sockaddr_un)) }
 	addr.sun_family = C.AF_UNIX
-	unsafe { C.strncpy(addr.sun_path, a.str, max_sun_path) }
+	unsafe { C.strncpy(&addr.sun_path[0], a.str, max_sun_path) }
 	size := C.SUN_LEN(&addr)
 	sockaddr := unsafe { &C.sockaddr(&addr) }
 	res := C.connect(s.handle, sockaddr, size)
@@ -97,7 +97,7 @@ pub fn listen_stream(sock string) ?&StreamListener {
 	mut addr := C.sockaddr_un{}
 	unsafe { C.memset(&addr, 0, sizeof(C.sockaddr_un)) }
 	addr.sun_family = C.AF_UNIX
-	unsafe { C.strncpy(addr.sun_path, sock.str, max_sun_path) }
+	unsafe { C.strncpy(&addr.sun_path[0], sock.str, max_sun_path) }
 	size := C.SUN_LEN(&addr)
 	sockaddr := unsafe { &C.sockaddr(&addr) }
 	net.socket_error(C.bind(s.handle, sockaddr, size)) ?
