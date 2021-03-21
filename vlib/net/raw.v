e90db70612b328e98406e3d7467aadfcbe9a62b0
Sugate <51022808+Joffref@users.noreply.github.com>
2021-03-21 01:04:18 +0100

    Update raw.v

--- vlib/net/raw.v
+++ vlib/net/raw.v
@@ -4,35 +4,35 @@ import time
 import encoding
 
 enum RawProto {
-    IPPROTO_IP = 0,          
-    IPPROTO_HOPOPTS = 0,   
-    IPPROTO_ICMP = 1,           
-    IPPROTO_IGMP = 2,           
-    IPPROTO_IPIP = 4,           
-    IPPROTO_TCP = 6,           
-    IPPROTO_EGP = 8,           
-    IPPROTO_PUP = 12,           
-    IPPROTO_UDP = 17,          
-    IPPROTO_IDP = 22,         
-    IPPROTO_TP = 29,          
-    IPPROTO_DCCP = 33,           
-    IPPROTO_IPV6 = 41,    
-    IPPROTO_ROUTING = 43,
-    IPPROTO_FRAGMENT = 44, 
-    IPPROTO_RSVP = 46,         
-    IPPROTO_GRE = 47,          
-    IPPROTO_ESP = 50, 
-    IPPROTO_AH = 51,       
-    IPPROTO_ICMPV6 = 58,  
-    IPPROTO_NONE = 59,  
-    IPPROTO_DSTOPTS = 60, 
-    IPPROTO_MTP = 92,           
-    IPPROTO_ENCAP = 98,          
-    IPPROTO_PIM = 103,           
-    IPPROTO_COMP = 108,           
-    IPPROTO_SCTP = 132,         
-    IPPROTO_UDPLITE = 136, 
-    IPPROTO_RAW = 255,           
+    IPPROTO_IP = 0          
+    IPPROTO_HOPOPTS = 0   
+    IPPROTO_ICMP = 1           
+    IPPROTO_IGMP = 2           
+    IPPROTO_IPIP = 4           
+    IPPROTO_TCP = 6           
+    IPPROTO_EGP = 8
+    IPPROTO_PUP = 12           
+    IPPROTO_UDP = 17          
+    IPPROTO_IDP = 22         
+    IPPROTO_TP = 29          
+    IPPROTO_DCCP = 33           
+    IPPROTO_IPV6 = 41    
+    IPPROTO_ROUTING = 43
+    IPPROTO_FRAGMENT = 44 
+    IPPROTO_RSVP = 46         
+    IPPROTO_GRE = 47          
+    IPPROTO_ESP = 50 
+    IPPROTO_AH = 51       
+    IPPROTO_ICMPV6 = 58  
+    IPPROTO_NONE = 59  
+    IPPROTO_DSTOPTS = 60 
+    IPPROTO_MTP = 92           
+    IPPROTO_ENCAP = 98          
+    IPPROTO_PIM = 103          
+    IPPROTO_COMP = 108           
+    IPPROTO_SCTP = 132         
+    IPPROTO_UDPLITE = 136 
+    IPPROTO_RAW = 255           
 }
 
 const (
