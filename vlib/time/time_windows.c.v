753a85d809d7490ebddef5f14a85f8e8f6162112
Eko <Ekopalypse@gmail.com>
2021-03-24 12:02:33 +0100

    fix time offset on windows

--- vlib/time/time_windows.c.v
+++ vlib/time/time_windows.c.v
@@ -114,8 +114,10 @@ pub fn (t Time) local() Time {
 		hour: st_local.hour
 		minute: st_local.minute
 		second: st_local.second // These are the same
-		microsecond: t.microsecond
-		unix: t.unix
+		// microsecond: t.microsecond
+		microsecond: st_local.millisecond * 1000
+		// unix: t.unix
+		unix: u64(st_local.unix_time())
 	}
 	return t_local
 }
