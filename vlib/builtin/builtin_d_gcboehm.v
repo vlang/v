cf0bbaf0717e48ffdb4dd302fd40ac9b755facc0
Uwe Krüger <45282134+UweKrueger@users.noreply.github.com>
2021-03-25 12:30:50 +0100

    add some comments for `GC_*` functions

--- vlib/builtin/builtin_d_gcboehm.v
+++ vlib/builtin/builtin_d_gcboehm.v
@@ -14,22 +14,29 @@ $if macos {
 
 #flag -lgc
 
+// replacements for `malloc()/calloc()`, `realloc()` and `free()`
+// for use with Boehm-GC
+// Do not use them manually. They are automatically chosen when
+// compiled with `-gc boehm` or `-gc boehm_leak`.
 fn C.GC_MALLOC(n size_t) voidptr
 
 fn C.GC_REALLOC(ptr voidptr, n size_t) voidptr
 
 fn C.GC_FREE(ptr voidptr)
 
-fn C.GC_set_find_leak(int)
-
+// explicitely perform garbage collection now! Garbage collections
+// are done automatically when needed, so this function is hardly needed 
 fn C.GC_gcollect()
 
+// functions to temporarily suspend/resume garbage collection
 fn C.GC_disable()
-
-fn C.GC_is_disabled() int
 
 fn C.GC_enable()
 
+// returns non-zero if GC is disabled
+fn C.GC_is_disabled() int
+
+// for leak detection it is advisable to do explicit garbage collections
 fn gcboehm_check_leaks() {
 	$if gcboehm_leak ? {
 		C.GC_gcollect()
