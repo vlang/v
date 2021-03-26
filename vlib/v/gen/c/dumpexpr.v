91ea76797ab0ebac8c287d892995ca8562eb5747
zakuro <z@kuro.red>
2021-03-26 15:35:12 +0900

    cgen: fix a c error, when using dump(ptr) (#9468)

--- vlib/v/gen/c/dumpexpr.v
+++ vlib/v/gen/c/dumpexpr.v
@@ -1,6 +1,7 @@
 module c
 
 import v.ast
+import v.table
 
 fn (mut g Gen) dump_expr(node ast.DumpExpr) {
 	sexpr := ctoslit(node.expr.str())
@@ -10,28 +11,41 @@ fn (mut g Gen) dump_expr(node ast.DumpExpr) {
 		g.expr(node.expr)
 		return
 	}
-	g.write(' _v_dump_expr_${node.cname}(${ctoslit(fpath)}, $line, $sexpr, ')
+	dump_fn_name := '_v_dump_expr_$node.cname' + (if node.expr_type.is_ptr() { '_ptr' } else { '' })
+	g.write(' ${dump_fn_name}(${ctoslit(fpath)}, $line, $sexpr, ')
 	g.expr(node.expr)
 	g.write(' )')
 }
 
 fn (mut g Gen) dump_expr_definitions() {
 	if g.pref.build_mode == .build_module {
-		for _, cname in g.table.dumps {
-			g.definitions.writeln('$cname _v_dump_expr_${cname}(string fpath, int line, string sexpr, $cname x);')
+		for dump_type, cname in g.table.dumps {
+			is_ptr := table.Type(dump_type).is_ptr()
+			ptr_suffix := if is_ptr { '*' } else { '' }
+			dump_fn_name := '_v_dump_expr_$cname' + (if is_ptr { '_ptr' } else { '' })
+			g.definitions.writeln('$cname$ptr_suffix ${dump_fn_name}(string fpath, int line, string sexpr, $cname$ptr_suffix x) {')
 		}
 	} else {
 		for dump_type, cname in g.table.dumps {
 			to_string_fn_name := g.gen_str_for_type(dump_type)
-			g.definitions.writeln('$cname _v_dump_expr_${cname}(string fpath, int line, string sexpr, $cname x) {')
+			is_ptr := table.Type(dump_type).is_ptr()
+			ptr_astarisk := if is_ptr { '*' } else { '' }
+			dump_fn_name := '_v_dump_expr_$cname' + (if is_ptr { '_ptr' } else { '' })
+			g.definitions.writeln('$cname$ptr_astarisk ${dump_fn_name}(string fpath, int line, string sexpr, $cname$ptr_astarisk x) {')
 			g.definitions.writeln('\teprint(${ctoslit('[')});')
 			g.definitions.writeln('\teprint(fpath);')
 			g.definitions.writeln('\teprint(${ctoslit(':')});')
 			g.definitions.writeln('\teprint(int_str(line));')
 			g.definitions.writeln('\teprint(${ctoslit('] ')});')
+
 			g.definitions.writeln('\teprint(sexpr);')
 			g.definitions.writeln('\teprint(${ctoslit(': ')});')
-			g.definitions.writeln('\teprintln(${to_string_fn_name}(x));')
+
+			if is_ptr {
+				g.definitions.writeln('\teprint(${ctoslit('&')});')
+			}
+			g.definitions.writeln('\teprintln(${to_string_fn_name}(${ptr_astarisk}x));')
+
 			g.definitions.writeln('\treturn x;')
 			g.definitions.writeln('}')
 		}
