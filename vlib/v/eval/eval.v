c7f719ff83d4bd6c344cb543e99478365abf8414
Theoo3 <theor17@madronaschool.com>
2021-03-27 14:29:00 -0700

    fix2

--- vlib/v/eval/eval.v
+++ vlib/v/eval/eval.v
@@ -29,10 +29,7 @@ pub mut:
 	returning              bool
 	return_values          []Object
 	cur_mod                string
-<<<<<<< HEAD
 	cur_file               string
-=======
->>>>>>> 2b2b5cc64... interpret hello world with builtin
 	back_trace             []string
 }
 
