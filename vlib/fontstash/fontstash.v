44b9e251bd6462d7a4fe93de1cbab49ad72382f0
Uwe Krüger <45282134+UweKrueger@users.noreply.github.com>
2021-03-28 19:26:03 +0200

    fix fontstash

--- vlib/fontstash/fontstash.v
+++ vlib/fontstash/fontstash.v
@@ -2,6 +2,11 @@ module fontstash
 
 #flag -I @VROOT/thirdparty/fontstash
 #define FONTSTASH_IMPLEMENTATION
+$if gcboehm ? {
+	#define FONTSTASH_MALLOC GC_MALLOC
+	#define FONTSTASH_REALLOC GC_REALLOC
+	#define FONTSTASH_FREE GC_FREE
+}
 #include "fontstash.h"
 #flag -I /usr/local/Cellar/freetype/2.10.2/include/freetype2
 //#flag -lfreetype
