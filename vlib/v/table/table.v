7936ec99c08e41c273ef6e41bf94006e152a777b
yuyi98 <yuyi98@163.com>
2021-03-30 23:01:07 +0800

    generics: fix generic_struct_insts_to_concrete

--- vlib/v/table/table.v
+++ vlib/v/table/table.v
@@ -1091,27 +1091,22 @@ pub fn (mut t Table) generic_struct_insts_to_concrete() {
 			}
 			mut parent_info := parent.info as Struct
 			mut fields := parent_info.fields.clone()
-			for i, _ in fields {
-				mut field := fields[i]
-				if field.typ.has_flag(.generic) {
-					if parent_info.generic_types.len != info.generic_types.len {
-						// TODO: proper error
-						panic('generic template mismatch')
+			if parent_info.generic_types.len == info.generic_types.len {
+				for i, _ in fields {
+					mut field := fields[i]
+					if t_typ := t.resolve_generic_by_types(field.typ, parent_info.generic_types,
+						info.generic_types)
+					{
+						field.typ = t_typ
 					}
-					for j, gp in parent_info.generic_types {
-						if gp == field.typ {
-							field.typ = info.generic_types[j].derive(field.typ).clear_flag(.generic)
-							break
-						}
-					}
+					fields[i] = field
 				}
-				fields[i] = field
+				parent_info.generic_types = []
+				parent_info.fields = fields
+				typ.is_public = true
+				typ.kind = .struct_
+				typ.info = parent_info
 			}
-			parent_info.generic_types = []
-			parent_info.fields = fields
-			typ.is_public = true
-			typ.kind = .struct_
-			typ.info = parent_info
 		}
 	}
 }
