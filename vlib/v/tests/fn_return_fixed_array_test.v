ff2fbc5495b3c7265b2135fd7cf593b1b8da13ad
yuyi98 <yuyi98@163.com>
2021-04-01 16:09:32 +0800

    format fn_return_fixed_array_test.v

--- vlib/v/tests/fn_return_fixed_array_test.v
+++ vlib/v/tests/fn_return_fixed_array_test.v
@@ -1,13 +1,13 @@
 fn test_fn_return_fixed_array() {
-    x := get3()
-    dump(x)
+	x := get3()
+	dump(x)
 	assert '$x' == '[1, 2, 3]'
 }
 
 fn get3() [3]int {
-    mut res := [3]int{}
-    res[0] = 1
-    res[1] = 2
-    res[2] = 3
-    return res
+	mut res := [3]int{}
+	res[0] = 1
+	res[1] = 2
+	res[2] = 3
+	return res
 }
