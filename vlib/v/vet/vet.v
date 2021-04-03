646d46b4dc73c7516f9bb1bad197d3ffe0a8c2ba
Lukas Neubert <serkonda@protonmail.com>
2021-04-03 18:30:15 +0200

    vet: turn warnings into errors with -W flag (#9575)

--- vlib/v/vet/vet.v
+++ vlib/v/vet/vet.v
@@ -16,12 +16,13 @@ pub enum FixKind {
 }
 
 pub struct Error {
+pub mut:
+	kind ErrorKind [required]
 pub:
 	// General message
 	message   string         [required]
 	details   string // Details about how to resolve or fix the situation
 	file_path string // file where the error have origin
 	pos       token.Position // position in the file
-	kind      ErrorKind      [required]
 	fix       FixKind        [required]
 }
