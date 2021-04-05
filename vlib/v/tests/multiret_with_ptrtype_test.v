d82a0c16373bac645c8ff74125c7b2dedb3430bf
Delyan Angelov <delian66@gmail.com>
2021-04-05 08:31:20 +0300

    ci: fix multiret_with_ptrtype_test.v

--- vlib/v/tests/multiret_with_ptrtype_test.v
+++ vlib/v/tests/multiret_with_ptrtype_test.v
@@ -2,15 +2,15 @@ fn multi_voidptr_ret() (voidptr, bool) {
 	return voidptr(0), true
 }
 
-fn multi_byteptr_ret() (byteptr, bool) {
-	return byteptr(0), true
+fn multi_byteptr_ret() (&byte, bool) {
+	return &byte(0), true
 }
 
 fn test_multi_ptrtype_ret() {
 	a, b := multi_voidptr_ret()
-	assert a == voidptr(0)
+	assert u64(a) == 0
 	assert b == true
 	c, d := multi_byteptr_ret()
-	assert c == byteptr(0)
+	assert u64(c) == 0
 	assert d == true
 }
