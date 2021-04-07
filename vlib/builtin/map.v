bca940919255e2583b6aa16b0b24370dfdeed2df
ka-weihe <k@weihe.dk>
2021-04-07 14:12:12 +0200

    map: wrap up delete fix (#9626)

--- vlib/builtin/map.v
+++ vlib/builtin/map.v
@@ -637,16 +637,9 @@ fn (mut d DenseArray) delete(i int) {
 	}
 }
 
-// delete this
-pub fn (mut m map) delete(key string) {
-	unsafe {
-		m.delete_1(&key)
-	}
-}
-
 // Removes the mapping of a particular key from the map.
 [unsafe]
-pub fn (mut m map) delete_1(key voidptr) {
+pub fn (mut m map) delete(key voidptr) {
 	mut index, mut meta := m.key_to_index(key)
 	index, meta = m.meta_less(index, meta)
 	// Perform backwards shifting
--- vlib/v/gen/c/fn.v
+++ vlib/v/gen/c/fn.v
@@ -551,11 +551,16 @@ fn (mut g Gen) method_call(node ast.CallExpr) {
 		}
 	}
 
-	if left_sym.kind == .map && node.name == 'delete_1' {
+	if left_sym.kind == .map && node.name == 'delete' {
 		left_info := left_sym.info as ast.Map
 		elem_type_str := g.typ(left_info.key_type)
-		g.write('map_delete_1(&')
-		g.expr(node.left)
+		g.write('map_delete(')
+		if node.left_type.is_ptr() {
+			g.expr(node.left)
+		} else {
+			g.write('&')
+			g.expr(node.left)
+		}
 		g.write(', &($elem_type_str[]){')
 		g.expr(node.args[0].expr)
 		g.write('})')
