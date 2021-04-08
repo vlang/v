23dd9a4d3051dc25761a66a89a167452bce80bdd
Carlos Esquerdo Bernat <esquerbatua@gmail.com>
2021-04-08 13:02:49 +0200

    Renamed file to submodules_test.v

--- vlib/v/tests/modules/submodules/submodules_test.v
+++ vlib/v/tests/modules/submodules/submodules_test.v
@@ -2,20 +2,20 @@ import submodules
 import submodules.test
 import submodules.test.test2
 
-fn main() {
+fn main_test() {
 	println('main')
 	main := submodules.Main{
 		a: 'main'
 	}
-	println(main)
+	assert 'main' == main.a
 
 	test := test.Test{
 		a: 'test'
 	}
-	println(test)
+	assert 'test' == test.a
 
 	test2 := test2.Test{
 		a: 'test2'
 	}
-	println(test2)
+	assert 'test2' == test2.a
 }
