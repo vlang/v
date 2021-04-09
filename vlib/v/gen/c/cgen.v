b346dd94643094548feb4ed1ddf4c60d96a8834a
Uwe Krüger <45282134+UweKrueger@users.noreply.github.com>
2021-04-09 17:18:36 +0200

    cgen: fix blank identifier in IfGard (`if _ := <-ch { ... }`, etc) (#9650)

--- vlib/v/gen/c/cgen.v
+++ vlib/v/gen/c/cgen.v
@@ -4576,10 +4576,15 @@ fn (mut g Gen) if_expr(node ast.IfExpr) {
 						g.expr(branch.cond.expr)
 						g.writeln(', ${var_name}.state == 0) {')
 					}
-					if branch.cond.var_name != '_' {
+					if short_opt || branch.cond.var_name != '_' {
 						base_type := g.base_type(branch.cond.expr_type)
 						if short_opt {
-							g.write('\t$base_type $branch.cond.var_name = ')
+							cond_var_name := if branch.cond.var_name == '_' {
+								'_dummy_${g.tmp_count + 1}'
+							} else {
+								branch.cond.var_name
+							}
+							g.write('\t$base_type $cond_var_name = ')
 							g.expr(branch.cond.expr)
 							g.writeln(';')
 						} else {
