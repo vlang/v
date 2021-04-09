a27f894ab68064a461beab7831b69b5319f4200e
Uwe Krüger <45282134+UweKrueger@users.noreply.github.com>
2021-04-09 21:09:33 +0200

    check send failure, too, in test cases

--- vlib/sync/channel_close_test.v
+++ vlib/sync/channel_close_test.v
@@ -74,25 +74,35 @@ fn test_channel_close_unbuffered() {
 
 fn test_channel_send_close_buffered() {
 	ch := chan int{cap: 1}
-	go fn (ch chan int) {
+	t := go fn (ch chan int) {
 		ch <- 31
-		ch <- 17 or {}
+		mut x := 45
+		ch <- 17 or {
+			x = -133
+		}
+		assert x == -133
 	}(ch)
 	time.sleep(100 * time.millisecond)
 	ch.close()
 	mut r := <-ch
 	r = <-ch or { 23 }
 	assert r == 23
+	t.wait()
 }
 
 fn test_channel_send_close_unbuffered() {
 	time.sleep(1 * time.second)
 	ch := chan int{}
-	go fn (ch chan int) {
-		ch <- 177 or {}
+	t := go fn (ch chan int) {
+		mut x := 31
+		ch <- 177 or {
+			x = -71
+		}
+		assert x == -71
 	}(ch)
 	time.sleep(100 * time.millisecond)
 	ch.close()
 	r := <-ch or { 238 }
 	assert r == 238
+	t.wait()
 }
