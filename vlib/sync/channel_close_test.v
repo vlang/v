442c782c79e80da7ad1e4ada233f4fc03d515cab
Uwe Krüger <45282134+UweKrueger@users.noreply.github.com>
2021-04-09 20:44:53 +0200

    add test case to expose the problem

--- vlib/sync/channel_close_test.v
+++ vlib/sync/channel_close_test.v
@@ -1,4 +1,4 @@
-import sync
+import time
 
 fn do_rec(ch chan int, resch chan i64) {
 	mut sum := i64(0)
@@ -70,4 +70,29 @@ fn test_channel_close_unbuffered() {
 	mut sum := i64(0)
 	sum += <-resch
 	assert sum == i64(8000) * (8000 - 1) / 2
+}
+
+fn test_channel_send_close_buffered() {
+	ch := chan int{cap: 1}
+	go fn (ch chan int) {
+		ch <- 31
+		ch <- 17 or {}
+	}(ch)
+	time.sleep(100 * time.millisecond)
+	ch.close()
+	mut r := <-ch
+	r = <-ch or { 23 }
+	assert r == 23
+}
+
+fn test_channel_send_close_unbuffered() {
+	time.sleep(1 * time.second)
+	ch := chan int{}
+	go fn (ch chan int) {
+		ch <- 177 or {}
+	}(ch)
+	time.sleep(100 * time.millisecond)
+	ch.close()
+	r := <-ch or { 238 }
+	assert r == 238
 }
