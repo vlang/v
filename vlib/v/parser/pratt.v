bde3125ee9a0abd5123cd91a5377286fdffa84c8
Uwe Krüger <45282134+UweKrueger@users.noreply.github.com>
2021-04-16 15:04:55 +0200

    do not assume prefix `-` if there is no space before

--- vlib/v/parser/pratt.v
+++ vlib/v/parser/pratt.v
@@ -328,7 +328,8 @@ pub fn (mut p Parser) expr(precedence int) ast.Expr {
 		}
 	}
 	if inside_array_lit {
-		if p.tok.kind == .minus && p.tok.pos + 1 == p.peek_tok.pos {
+		if p.tok.kind == .minus && p.tok.pos + 1 == p.peek_tok.pos
+			&& p.prev_tok.pos + p.prev_tok.len + 1 != p.peek_tok.pos {
 			return node
 		}
 	}
