3abe860f3ace592df8690ab108932bbe50332146
Enzo Baldisserri <enzo.baldisserri@outlook.fr>
2021-04-19 20:14:53 +0200

    format files

--- cmd/tools/vbug.v
+++ cmd/tools/vbug.v
@@ -91,7 +91,7 @@ fn open_browser(uri string) ? {
 		handle := dl.open_opt('shell32', dl.rtld_now) ?
 		// https://docs.microsoft.com/en-us/windows/win32/api/shellapi/nf-shellapi-shellexecutew
 		func := ShellExecuteWin(dl.sym_opt(handle, 'ShellExecuteW') ?)
-		func(C.NULL, "open".to_wide(), uri.to_wide(), C.NULL, C.NULL, C.SW_SHOWNORMAL)
+		func(C.NULL, 'open'.to_wide(), uri.to_wide(), C.NULL, C.NULL, C.SW_SHOWNORMAL)
 		dl.close(handle)
 	} $else {
 		return error('unsupported platform')
