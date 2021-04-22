b951d679ca3935f15b90e62b5fa7b8c0f08d3447
pancake <pancake@nowsecure.com>
2021-04-22 14:44:25 +0200

    x64: handle -arch amd64/arm64 and -os for raw/linux/macos options (#9844)

--- vlib/v/builder/x64.v
+++ vlib/v/builder/x64.v
@@ -7,10 +7,8 @@ import v.gen.x64
 import v.markused
 
 pub fn (mut b Builder) build_x64(v_files []string, out_file string) {
-	$if !linux {
-		println('v -x64 can only generate Linux binaries for now')
-		println('You are not on a Linux system, so you will not ' +
-			'be able to run the resulting executable')
+	$if !linux && !macos {
+		eprintln('Warning: v -x64 can only generate macOS and Linux binaries for now')
 	}
 	util.timing_start('PARSE')
 	b.parsed_files = parser.parse_files(v_files, b.table, b.pref, b.global_scope)
