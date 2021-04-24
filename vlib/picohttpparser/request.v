dee4904beee60b553b789b290e39d3ef2d299661
Anton Zavodchikov <terisbackno@gmail.com>
2021-04-24 15:21:30 +0500

    pico: small refactor (#9857)

--- vlib/picohttpparser/request.v
+++ vlib/picohttpparser/request.v
@@ -1,6 +1,8 @@
 module picohttpparser
 
 pub struct Request {
+mut:
+	prev_len    int
 pub mut:
 	method      string
 	path        string
@@ -17,7 +19,7 @@ pub fn (mut r Request) parse_request(s string, max_headers int) int {
 	num_headers := size_t(max_headers)
 
 	pret := C.phr_parse_request(s.str, s.len, PPchar(&r.method.str), &method_len, PPchar(&r.path.str),
-		&path_len, &minor_version, &r.headers[0], &num_headers, 0)
+		&path_len, &minor_version, &r.headers[0], &num_headers, r.prev_len)
 	if pret > 0 {
 		unsafe {
 			r.method = tos(r.method.str, int(method_len))
@@ -25,6 +27,7 @@ pub fn (mut r Request) parse_request(s string, max_headers int) int {
 		}
 		r.num_headers = u64(num_headers)
 	}
+	r.prev_len = s.len
 	return pret
 }
 
