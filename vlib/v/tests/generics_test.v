9a4e701d909c7ff87c59330cb027cf0e5fdb3ec6
yuyi98 <yuyi98@163.com>
2021-04-30 18:05:02 +0800

    fix test using '>>'

--- vlib/v/tests/generics_test.v
+++ vlib/v/tests/generics_test.v
@@ -493,7 +493,7 @@ fn test_generic_detection() {
 	// generic
 	assert multi_generic_args<int, string>(0, 's')
 	assert multi_generic_args<Foo1, Foo2>(Foo1{}, Foo2{})
-	assert multi_generic_args<Foo<int>, Foo<int>>(Foo<int>{}, Foo<int>{})
+	assert multi_generic_args<Foo<int>, Foo<int> >(Foo<int>{}, Foo<int>{})
 	// TODO: assert multi_generic_args<Foo<int>, Foo<int>>(Foo1{}, Foo2{})
 	assert multi_generic_args<simplemodule.Data, int>(simplemodule.Data{}, 0)
 	assert multi_generic_args<int, simplemodule.Data>(0, simplemodule.Data{})
