0255be4aee39e2b438a6bf654150999d2d7db8f1
Leigh McCulloch <leigh@mcchouse.com>
2021-05-04 07:02:58 +0000

    io: add MultiWriter

---
+++ vlib/io/multi_writer.v
@@ -0,0 +1,21 @@
+module io
+
+// MultiWriter writes to all its writers.
+pub struct MultiWriter {
+pub:
+	writers []Writer
+}
+
+// Write writes to all writers of the MultiWriter. Returns the length of bytes
+// written. If any writer fails to write the full length an error is returned
+// and writing to other writers stops. If any writer returns an error the error
+// is returned immediately and writing to other writers stops.
+pub fn (m MultiWriter) write(buf []byte) ?int {
+	for w in m.writers {
+		n := w.write(buf) ?
+		if n != buf.len {
+			return error('io: incomplete write to writer of MultiWriter')
+		}
+	}
+	return buf.len
+}
