5d3d2adc80f90b361c40bd9b384465788d1026b5
Cabral <17774851+cabralski@users.noreply.github.com>
2021-05-05 12:27:00 -0300

    remove buggy stuff from the past sign implementation

--- vlib/math/math.v
+++ vlib/math/math.v
@@ -117,12 +117,6 @@ pub fn min(a f64, b f64) f64 {
 // sign returns the corresponding sign [-1, 0, 1] of the provided number.
 pub fn sign(n f64) int {
 	return copysign(1, n)
-		return 1
-	}
-	if n < 0 {
-		return -1
-	}
-	return 0
 }
 
 // radians convert from radians to degrees.
