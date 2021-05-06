cade2862ecc241b69f3807a40718e91e5526257e
Leigh McCulloch <leigh@mcchouse.com>
2021-05-06 14:55:16 +0000

    io: add new_multiwriter

--- vlib/io/multi_writer.v
+++ vlib/io/multi_writer.v
@@ -1,5 +1,17 @@
 module io
 
+// new_multiwriter returns a Writer that writes to all writers. The write
+// function of the returned Writer writes to all writers of the MultiWriter,
+// returns the length of bytes written, and iif any writer fails to write the
+// full length an error is returned and writing to other writers stops, and if
+// any writer returns an error the error is returned immediately and writing to
+// other writers stops.
+pub fn new_multiwriter(writers []Writer) MultiWriter {
+	return MultiWriter{
+		writers: writers
+	}
+}
+
 // MultiWriter writes to all its writers.
 pub struct MultiWriter {
 pub:
