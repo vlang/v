1cbe07cf7f371436c1f372c8d5bba7dfbded780d
Delyan Angelov <delian66@gmail.com>
2021-05-07 20:03:16 +0300

    rename math.isign to math.signi as suggested

--- vlib/math/math.v
+++ vlib/math/math.v
@@ -125,9 +125,9 @@ pub fn sign(n f64) f64 {
 	return copysign(1.0, n)
 }
 
-// isign returns the corresponding sign [-1, 1] of the provided number.
+// signi returns the corresponding sign [-1, 1] of the provided number.
 [inline]
-pub fn isign(n f64) int {
+pub fn signi(n f64) int {
 	return int(copysign(1.0, n))
 }
 
