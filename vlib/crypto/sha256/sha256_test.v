d8d6e9b9019cb41ce820e75b8505ed745c67fc55
Delyan Angelov <delian66@gmail.com>
2021-05-08 17:38:05 +0300

    ci: run vfmt over sha256_test.v

--- vlib/crypto/sha256/sha256_test.v
+++ vlib/crypto/sha256/sha256_test.v
@@ -12,6 +12,5 @@ fn test_crypto_sha256_writer() {
 	digest.write('This is a'.bytes()) or { assert false }
 	digest.write(' sha256 checksum.'.bytes()) or { assert false }
 	sum := digest.sum([])
-	assert sum.hex() ==
-		'dc7163299659529eae29683eb1ffec50d6c8fc7275ecb10c145fde0e125b8727'
+	assert sum.hex() == 'dc7163299659529eae29683eb1ffec50d6c8fc7275ecb10c145fde0e125b8727'
 }
