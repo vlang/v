a477ea17595f63f3dc1c12e7af08506cadc81519
Leigh McCulloch <leigh@mcchouse.com>
2021-05-08 04:32:07 +0000

    crypto/sha256: make digest sum public

--- vlib/crypto/sha256/sha256_test.v
+++ vlib/crypto/sha256/sha256_test.v
@@ -8,3 +8,12 @@ fn test_crypto_sha256() {
 	assert sha256.sum('This is a sha256 checksum.'.bytes()).hex() ==
 		'dc7163299659529eae29683eb1ffec50d6c8fc7275ecb10c145fde0e125b8727'
 }
+
+fn test_crypto_sha256_writer() {
+	mut digest := sha256.new()
+	digest.write('This is a'.bytes()) or { assert false }
+	digest.write(' sha256 checksum.'.bytes()) or { assert false }
+	sum := digest.sum([])
+	assert sum.hex() ==
+		'dc7163299659529eae29683eb1ffec50d6c8fc7275ecb10c145fde0e125b8727'
+}
