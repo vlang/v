f09d8d4963772eacdddacc0181fcc8718da9738d
yuyi98 <yuyi98@163.com>
2021-05-13 20:19:20 +0800

    modify test and fix generic struct to string in other PR

--- vlib/v/tests/generic_fn_infer_struct_test.v
+++ vlib/v/tests/generic_fn_infer_struct_test.v
@@ -12,13 +12,8 @@ fn test_generics_fn_infer_struct() {
 	assert ret1.contains('Node<int>{')
 	assert ret1.contains('data: 0')
 
-	ret2 := foo(Node<f64>{})
+	ret2 := foo(Node<byte>{})
 	println(ret2)
-	assert ret2.contains('Node<f64>{')
+	assert ret2.contains('Node<byte>{')
 	assert ret2.contains('data: 0')
-
-	ret3 := foo(Node<byte>{})
-	println(ret3)
-	assert ret3.contains('Node<byte>{')
-	assert ret3.contains('data: 0')
 }
