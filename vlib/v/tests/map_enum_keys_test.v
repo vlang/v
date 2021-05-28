49c9db49c0f9340ac6b217490ee17404adaeee88
Lukas Neubert <serkonda@protonmail.com>
2021-05-28 13:43:15 +0200

    parser: support imported enums as map keys

--- vlib/v/tests/map_enum_keys_test.v
+++ vlib/v/tests/map_enum_keys_test.v
@@ -1,3 +1,5 @@
+import gg { MouseButton }
+
 enum Token {
 	aa = 2
 	bb
@@ -10,8 +12,25 @@ fn test_map_with_enum_keys() {
 	m[Token.bb] = 'def'
 	assert m[Token.aa] == 'abc'
 	assert m[.bb] == 'def'
-	//
 	s := '$m'
 	assert s == "{aa: 'abc', bb: 'def'}"
 	println(m)
+}
+
+fn test_map_with_imported_enum_keys() {
+	mut km := map[gg.KeyCode]string{}
+	km[.minus] = '-'
+	km[gg.KeyCode.comma] = ','
+	assert km[.invalid] == ''
+	assert gg.KeyCode.comma in km
+	assert km[.minus] == '-'
+}
+
+fn test_map_with_selective_imported_enum_keys() {
+	mut bm := map[MouseButton]string{}
+	bm[.left] = 'lb'
+	bm[MouseButton.middle] = 'mb'
+	assert bm[.left] == 'lb'
+	bm.delete(MouseButton.left)
+	assert MouseButton.left !in bm
 }
