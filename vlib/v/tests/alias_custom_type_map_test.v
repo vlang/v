681ff9329ce538d7d405f917c47024e7953b3635
Lukas Neubert <serkonda@protonmail.com>
2021-05-30 17:52:51 +0200

    fmt

--- vlib/v/tests/alias_custom_type_map_test.v
+++ vlib/v/tests/alias_custom_type_map_test.v
@@ -2,7 +2,7 @@ import geometry
 
 fn test_aliases_map_init() {
 	a := geometry.ShapeMap(map{
-		geometry.Shape.circle: "Shape is a circle."
+		geometry.Shape.circle: 'Shape is a circle.'
 	})
-	assert a[geometry.Shape.circle] == "Shape is a circle."
+	assert a[geometry.Shape.circle] == 'Shape is a circle.'
 }
