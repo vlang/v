8990114b4b830030060d326a9cd79ad3d8067ccf
youyuanwu <48816116+youyuanwu@users.noreply.github.com>
2021-05-31 04:08:31 -0700

    vlib: add a vlib/mssql module (#10280)

---
+++ vlib/mssql/config.v
@@ -0,0 +1,13 @@
+module mssql
+
+pub struct Config {
+pub:
+	driver string
+	server string
+	uid    string
+	pwd    string
+}
+
+pub fn (cfg Config) get_conn_str() string {
+	return 'Driver=$cfg.driver;Server=$cfg.server;UID=$cfg.uid;PWD=$cfg.pwd'
+}
