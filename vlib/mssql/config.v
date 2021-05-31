8b761ec15c396e3a136c5145ed5e1f19fb72461b
Youyuan Wu <youyuanwu@outlook.com>
2021-05-31 16:39:51 -0700

    fix formating again

--- vlib/mssql/config.v
+++ vlib/mssql/config.v
@@ -8,13 +8,13 @@ pub:
 	pwd    string
 	// if dbname empty, conn str will not contain Database info,
 	// and it is up to the server to choose which db to connect to.
-	dbname string 
+	dbname string
 }
 
 pub fn (cfg Config) get_conn_str() string {
 	mut str := 'Driver=$cfg.driver;Server=$cfg.server;UID=$cfg.uid;PWD=$cfg.pwd'
-	if cfg.dbname != ""{
-		str += ';Database=${cfg.dbname}'
+	if cfg.dbname != '' {
+		str += ';Database=$cfg.dbname'
 	}
 	return str
 }
