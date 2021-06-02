927cb07a2649e10879b49cdae85d7ff5b089e977
Miccah Castorina <m.castorina93@gmail.com>
2021-06-02 11:23:13 -0500

    Change panic to return error in tests

--- vlib/net/http/response_test.v
+++ vlib/net/http/response_test.v
@@ -2,22 +2,22 @@ module http
 
 fn test_parse_response_line() ? {
 	if _ := parse_response_line('hello world') {
-		panic('should be error')
+		return error('should be error')
 	}
 	mut version, mut status := parse_response_line('http/1.1 200 OK') or {
-		panic('should not error')
+		return error('should not error')
 	}
 	assert version == .v1_1
 	assert status == .ok
 
 	version, status = parse_response_line('http/1.1 200 any string') or {
-		panic('should not error')
+		return error('should not error')
 	}
 	assert version == .v1_1
 	assert status == .ok
 
 	// TODO: should this not parse?
-	version, status = parse_response_line('abc 123') or { panic('should not error') }
+	version, status = parse_response_line('abc 123') or { return error('should not error') }
 	assert version == .unknown
 	assert status == .unassigned
 }
