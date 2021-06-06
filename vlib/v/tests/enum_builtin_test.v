a643b44630d1cbd53ba06fcdf09a60385aa3d61b
Lukas Neubert <serkonda@protonmail.com>
2021-06-06 11:23:26 +0200

    fmt

--- vlib/v/tests/enum_builtin_test.v
+++ vlib/v/tests/enum_builtin_test.v
@@ -1,8 +1,8 @@
 import os
 
 const (
-	vdir = @VEXEROOT
-	builtin_path = '$vdir/vlib/builtin/tmp_my_enum.v'
+	vdir            = @VEXEROOT
+	builtin_path    = '$vdir/vlib/builtin/tmp_my_enum.v'
 	builtin_content = 'module builtin
 pub enum BuiltinTestEnum{
     v0 = 0
@@ -14,7 +14,7 @@ pub fn (x BuiltinTestEnum) some_method() string {
         else { return "v_t" }
     }
 }'
-	genc_path = '$vdir/vlib/v/gen/c/tmp_builtin_enum_usage.v'
+	genc_path    = '$vdir/vlib/v/gen/c/tmp_builtin_enum_usage.v'
 	genc_content = 'module c
 pub fn builtin_enum_usage() {
     x := BuiltinTestEnum.v0
@@ -28,8 +28,8 @@ fn testsuite_end() {
 }
 
 fn test_enum_method() {
-	os.write_file(builtin_path, builtin_content) or {panic(err)}
-	os.write_file(genc_path, genc_content) or {panic(err)}
+	os.write_file(builtin_path, builtin_content) or { panic(err) }
+	os.write_file(genc_path, genc_content) or { panic(err) }
 	res := os.execute('v self')
 	assert res.exit_code == 0
 }
