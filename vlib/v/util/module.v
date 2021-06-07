44a1f60a9daf621143580764d8d6129e8e2bb3e7
Yuriy Zymlex <Zymlex@users.noreply.github.com>
2021-06-07 18:51:25 +1000

    fix2

--- vlib/v/util/module.v
+++ vlib/v/util/module.v
@@ -102,10 +102,10 @@ pub fn mod_path_to_full_name(pref &pref.Preferences, mod string, path string) ?s
 				for j := try_path_parts.len; j > 0; j-- {
 					parent := try_path_parts[0..j].join(os.path_separator)
 
-					pref_path := os.dir(os.real_path(pref.path)) + os.path_separator
+					pref_path := os.dir(os.real_path(pref.path))
 					println('~~~ pref_path: $pref_path ~~~')
-					if parent.starts_with(pref_path) {
-						mod_full_name := parent.all_after(pref_path).replace(os.path_separator,
+					if parent != pref_path && parent.starts_with(pref_path + os.path_separator) {
+						mod_full_name := parent.all_after(pref_path + os.path_separator).replace(os.path_separator,
 							'.')
 						println('~~~ mod_full_name: $mod_full_name ~~~')
 						return mod_full_name
