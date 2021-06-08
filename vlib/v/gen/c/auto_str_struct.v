f9c4365dc751fab8ffe7664aac901d09c69fbf4a
Delyan Angelov <delian66@gmail.com>
2021-06-08 16:54:18 +0300

    v.gen.c: free `indents` in autogenerated .str() methods

--- vlib/v/gen/c/auto_str_struct.v
+++ vlib/v/gen/c/auto_str_struct.v
@@ -81,86 +81,86 @@ fn (mut g Gen) gen_str_for_struct(info ast.Struct, styp string, str_fn_name stri
 	}
 	clean_struct_v_type_name = util.strip_main_name(clean_struct_v_type_name)
 	// generate ident / indent length = 4 spaces
-	g.auto_str_funcs.writeln('\tstring indents = _SLIT("");')
-	g.auto_str_funcs.writeln('\tfor (int i = 0; i < indent_count; ++i) {')
-	g.auto_str_funcs.writeln('\t\tindents = string__plus(indents, _SLIT("    "));')
-	g.auto_str_funcs.writeln('\t}')
 	if info.fields.len == 0 {
-		g.auto_str_funcs.write_string('\treturn _SLIT("$clean_struct_v_type_name{}");')
-	} else {
-		g.auto_str_funcs.write_string('\treturn str_intp( ${info.fields.len * 4 + 3}, _MOV((StrIntpData[]){\n')
-		g.auto_str_funcs.write_string('\t\t{_SLIT("$clean_struct_v_type_name{\\n"), 0, {.d_c=0}},\n')
-
-		for i, field in info.fields {
-			mut ptr_amp := if field.typ.is_ptr() { '&' } else { '' }
-			base_fmt := g.type_to_fmt1(g.unwrap_generic(field.typ))
-
-			// manage prefix and quote symbol for the filed
-			mut quote_str := ''
-			mut prefix := ''
-			sym := g.table.get_type_symbol(g.unwrap_generic(field.typ))
-			if sym.kind == .string {
-				quote_str = "'"
-			} else if field.typ in ast.charptr_types {
-				quote_str = '\\"'
-				prefix = 'C'
-			}
-
-			// first fields doesn't need \n
-			if i == 0 {
-				g.auto_str_funcs.write_string('\t\t{_SLIT0, $si_s_code, {.d_s=indents}}, {_SLIT("    $field.name: $ptr_amp$prefix"), 0, {.d_c=0}}, ')
-			} else {
-				g.auto_str_funcs.write_string('\t\t{_SLIT("\\n"), $si_s_code, {.d_s=indents}}, {_SLIT("    $field.name: $ptr_amp$prefix"), 0, {.d_c=0}}, ')
-			}
-
-			// custom methods management
-			has_custom_str := sym.has_method('str')
-			mut field_styp := g.typ(field.typ).replace('*', '')
-			field_styp_fn_name := if has_custom_str {
-				'${field_styp}_str'
-			} else {
-				fnames2strfunc[field_styp]
-			}
-
-			// manage the fact hat with float we use always the g representation
-			if sym.kind !in [.f32, .f64] {
-				g.auto_str_funcs.write_string('{_SLIT("$quote_str"), ${int(base_fmt)}, {.${data_str(base_fmt)}=')
-			} else {
-				g_fmt := '0x' + (u32(base_fmt) | u32(0x7F) << 9).hex()
-				g.auto_str_funcs.write_string('{_SLIT("$quote_str"), $g_fmt, {.${data_str(base_fmt)}=')
-			}
-
-			mut func := struct_auto_str_func1(sym, field.typ, field_styp_fn_name, field.name)
-
-			// manage reference types can be "nil"
-			if field.typ.is_ptr() && !(field.typ in ast.charptr_types
-				|| field.typ in ast.byteptr_types
-				|| field.typ == ast.voidptr_type_idx) {
-				g.auto_str_funcs.write_string('isnil(it.${c_name(field.name)})')
-				g.auto_str_funcs.write_string(' ? _SLIT("nil") : ')
-				// struct, floats and ints have a special case through the _str function
-				if sym.kind != .struct_ && !field.typ.is_int_valptr()
-					&& !field.typ.is_float_valptr() {
-					g.auto_str_funcs.write_string('*')
-				}
-			}
-			// handle circular ref type of struct to the struct itself
-			if styp == field_styp {
-				g.auto_str_funcs.write_string('_SLIT("<circular>")')
-			} else {
-				// manage C charptr
-				if field.typ in ast.charptr_types {
-					g.auto_str_funcs.write_string('tos2((byteptr)$func)')
-				} else {
-					g.auto_str_funcs.write_string(func)
-				}
-			}
-
-			g.auto_str_funcs.write_string('}}, {_SLIT("$quote_str"), 0, {.d_c=0}},\n')
-		}
-		g.auto_str_funcs.write_string('\t\t{_SLIT("\\n"), $si_s_code, {.d_s=indents}}, {_SLIT("}"), 0, {.d_c=0}},\n')
-		g.auto_str_funcs.write_string('\t}));\n')
+		g.auto_str_funcs.writeln('\treturn _SLIT("$clean_struct_v_type_name{}");')
+		g.auto_str_funcs.writeln('}')
+		g.auto_str_funcs.writeln('')
+		return
 	}
+
+	g.auto_str_funcs.writeln('\tstring indents = string_repeat(_SLIT("    "), indent_count);')
+	g.auto_str_funcs.writeln('\tstring res = str_intp( ${info.fields.len * 4 + 3}, _MOV((StrIntpData[]){')
+	g.auto_str_funcs.writeln('\t\t{_SLIT("$clean_struct_v_type_name{\\n"), 0, {.d_c=0}},')
+
+	for i, field in info.fields {
+		mut ptr_amp := if field.typ.is_ptr() { '&' } else { '' }
+		base_fmt := g.type_to_fmt1(g.unwrap_generic(field.typ))
+
+		// manage prefix and quote symbol for the filed
+		mut quote_str := ''
+		mut prefix := ''
+		sym := g.table.get_type_symbol(g.unwrap_generic(field.typ))
+		if sym.kind == .string {
+			quote_str = "'"
+		} else if field.typ in ast.charptr_types {
+			quote_str = '\\"'
+			prefix = 'C'
+		}
+
+		// first fields doesn't need \n
+		if i == 0 {
+			g.auto_str_funcs.write_string('\t\t{_SLIT0, $si_s_code, {.d_s=indents}}, {_SLIT("    $field.name: $ptr_amp$prefix"), 0, {.d_c=0}}, ')
+		} else {
+			g.auto_str_funcs.write_string('\t\t{_SLIT("\\n"), $si_s_code, {.d_s=indents}}, {_SLIT("    $field.name: $ptr_amp$prefix"), 0, {.d_c=0}}, ')
+		}
+
+		// custom methods management
+		has_custom_str := sym.has_method('str')
+		mut field_styp := g.typ(field.typ).replace('*', '')
+		field_styp_fn_name := if has_custom_str {
+			'${field_styp}_str'
+		} else {
+			fnames2strfunc[field_styp]
+		}
+
+		// manage the fact hat with float we use always the g representation
+		if sym.kind !in [.f32, .f64] {
+			g.auto_str_funcs.write_string('{_SLIT("$quote_str"), ${int(base_fmt)}, {.${data_str(base_fmt)}=')
+		} else {
+			g_fmt := '0x' + (u32(base_fmt) | u32(0x7F) << 9).hex()
+			g.auto_str_funcs.write_string('{_SLIT("$quote_str"), $g_fmt, {.${data_str(base_fmt)}=')
+		}
+
+		mut func := struct_auto_str_func1(sym, field.typ, field_styp_fn_name, field.name)
+
+		// manage reference types can be "nil"
+		if field.typ.is_ptr() && !(field.typ in ast.charptr_types
+			|| field.typ in ast.byteptr_types || field.typ == ast.voidptr_type_idx) {
+			g.auto_str_funcs.write_string('isnil(it.${c_name(field.name)})')
+			g.auto_str_funcs.write_string(' ? _SLIT("nil") : ')
+			// struct, floats and ints have a special case through the _str function
+			if sym.kind != .struct_ && !field.typ.is_int_valptr() && !field.typ.is_float_valptr() {
+				g.auto_str_funcs.write_string('*')
+			}
+		}
+		// handle circular ref type of struct to the struct itself
+		if styp == field_styp {
+			g.auto_str_funcs.write_string('_SLIT("<circular>")')
+		} else {
+			// manage C charptr
+			if field.typ in ast.charptr_types {
+				g.auto_str_funcs.write_string('tos2((byteptr)$func)')
+			} else {
+				g.auto_str_funcs.write_string(func)
+			}
+		}
+
+		g.auto_str_funcs.writeln('}}, {_SLIT("$quote_str"), 0, {.d_c=0}},')
+	}
+	g.auto_str_funcs.writeln('\t\t{_SLIT("\\n"), $si_s_code, {.d_s=indents}}, {_SLIT("}"), 0, {.d_c=0}},')
+	g.auto_str_funcs.writeln('\t}));')
+	g.auto_str_funcs.writeln('\tstring_free(&indents);')
+	g.auto_str_funcs.writeln('\treturn res;')
 	g.auto_str_funcs.writeln('}')
 }
 
