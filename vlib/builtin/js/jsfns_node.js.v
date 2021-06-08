612bf60033ab49d66ae722eda7b0109041c43fa2
Andreas Heissenberger <andreas@heissenberger.at>
2021-06-09 01:14:44 +0200

    add function to access node process properties

--- vlib/builtin/js/jsfns_node.js.v
+++ vlib/builtin/js/jsfns_node.js.v
@@ -7,6 +7,23 @@
 
 module builtin
 
+pub struct JS.node_process {
+pub:
+	arch     string
+	argsv    []string
+	env      []string
+	platform string
+	version  string
+	// TODO: add all properties
+}
+
+// hack to access  process properties
+pub fn js_node_process() JS.node_process {
+	#return process
+
+	return JS.node_process{}
+}
+
 fn JS.process.exit(int)
 fn JS.process.stdout.write(string) bool
 fn JS.process.stdout.writeln(string) bool
