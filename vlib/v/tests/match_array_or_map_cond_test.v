cdcdac05b18ade21d7e738a7e206ff789d74bf05
yuyi98 <yuyi98@163.com>
2021-06-10 22:58:52 +0800

    cgen: fix match array or map cond

---
+++ vlib/v/tests/match_array_or_map_cond_test.v
@@ -0,0 +1,37 @@
+fn test_match_array_or_map_cond() {
+	// array
+	y1 := []byte{}
+	x1 := []byte{}
+	ret1 := match x1 {
+		y1 { true }
+		else { false }
+	}
+	println('ret1 is $ret1')
+	assert ret1
+
+	// fixed array
+	y2 := [1, 2, 3]!
+	x2 := [1, 2, 3]!
+	ret2 := match x2 {
+		y2 { true }
+		else { false }
+	}
+	println('ret2 is $ret2')
+	assert ret2
+
+	// map
+	y3 := map{
+		1: 11
+		2: 22
+	}
+	x3 := map{
+		1: 11
+		2: 22
+	}
+	ret3 := match x3 {
+		y3 { true }
+		else { false }
+	}
+	println('ret3 is $ret3')
+	assert ret3
+}
