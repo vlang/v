7d47488a399db1dfbe8608388f069bfe7a2b8720
Uwe Krüger <45282134+UweKrueger@users.noreply.github.com>
2021-06-12 15:48:53 +0200

    sync/chan: make channel status buffer 'noscan'

--- vlib/sync/channels.v
+++ vlib/sync/channels.v
@@ -118,7 +118,7 @@ fn new_channel_st(n u32, st u32) &Channel {
 	wsem := if n > 0 { n } else { 1 }
 	rsem := if n > 0 { u32(0) } else { 1 }
 	rbuf := if n > 0 { unsafe { malloc(int(n * st)) } } else { &byte(0) }
-	sbuf := if n > 0 { vcalloc(int(n * 2)) } else { &byte(0) }
+	sbuf := if n > 0 { vcalloc_noscan(int(n * 2)) } else { &byte(0) }
 	mut ch := &Channel{
 		objsize: st
 		cap: n
