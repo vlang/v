d213122b5937c230c7e88f64b001cced54a87f70
Delyan Angelov <delian66@gmail.com>
2021-06-12 15:08:50 +0300

    rm leftover test.v file

--- test.v
+++
@@ -1,8 +0,0 @@
-import net.http
-
-const url = "https://www.janusfiresystems.com/downloads/PDF/DS1002_Lv%20Series_FM-200_System.pdf"
-
-fn main() {
-  resp := http.get(url)?
-  println(resp.status_code)
-}
