c2ffabbf8dd616c0861afc7ce578412239f8081f
Miccah Castorina <m.castorina93@gmail.com>
2021-06-13 11:48:44 -0500

    Remove DefaultNotifier and implement only the new() method

--- vlib/os/notify/backend_default.c.v
+++ vlib/os/notify/backend_default.c.v
@@ -1,24 +1,6 @@
 module notify
 
 // Implement the API
-pub struct DefaultNotifier {}
-
-pub fn (d DefaultNotifier) add(fd int, events FdEventType, conf ...FdConfigFlags) ? {
-	panic('unsupported')
-}
-
-pub fn (d DefaultNotifier) remove(fd int) ? {
-	panic('unsupported')
-}
-
-pub fn (d DefaultNotifier) wait(timeout time.Duration) []FdEvent {
-	panic('unsupported')
-}
-
-pub fn (d DefaultNotifier) close() ? {
-	panic('unsupported')
-}
-
-pub fn new() ?DefaultNotifier {
+pub fn new() ?FdNotifier {
 	panic('unsupported')
 }
