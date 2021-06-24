df027594d7d7ada742d2198ab837abf309520e7f
shadowninja55 <shadowninja1050@gmail.com>
2021-06-24 16:39:08 -0400

    change checker message

--- vlib/v/checker/checker.v
+++ vlib/v/checker/checker.v
@@ -2195,7 +2195,7 @@ fn (mut c Checker) array_builtin_method_call(mut call_expr ast.CallExpr, left_ty
 			ast.f64_type, ast.char_type, ast.bool_type, ast.float_literal_type, ast.int_literal_type,
 			ast.size_t_type_idx,
 		] {
-			c.error('a default comparison function for `.sort()` cannot be generated for type `${c.table.type_to_str(elem_typ)}`',
+			c.error('custom sorting condition must be supplied for type `${c.table.type_to_str(elem_typ)}`',
 				call_expr.pos)
 		}
 	} else if method_name == 'wait' {
