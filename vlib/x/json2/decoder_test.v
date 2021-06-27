ebace0f108829266adb190c3841ab439ec5cbd95
Ned Palacios <npdoesmc@gmail.com>
2021-06-27 10:30:13 +0800

    x.json2: simplify nested level detection code

--- vlib/x/json2/decoder_test.v
+++ vlib/x/json2/decoder_test.v
@@ -1,56 +1,36 @@
 module json2
 
-import os
-
 fn test_raw_decode_string() {
-	str := raw_decode('"Hello!"') or {
-		assert false
-		Any(null)
-	}
+	str := raw_decode('"Hello!"') ?
 	assert str.str() == 'Hello!'
 }
 
 fn test_raw_decode_number() {
-	num := raw_decode('123') or {
-		assert false
-		Any(null)
-	}
+	num := raw_decode('123') ?
 	assert num.int() == 123
 }
 
 fn test_raw_decode_array() {
-	raw_arr := raw_decode('["Foo", 1]') or {
-		assert false
-		Any(null)
-	}
+	raw_arr := raw_decode('["Foo", 1]') ?
 	arr := raw_arr.arr()
 	assert arr[0].str() == 'Foo'
 	assert arr[1].int() == 1
 }
 
 fn test_raw_decode_bool() {
-	bol := raw_decode('false') or {
-		assert false
-		Any(null)
-	}
+	bol := raw_decode('false') ?
 	assert bol.bool() == false
 }
 
 fn test_raw_decode_map() {
-	raw_mp := raw_decode('{"name":"Bob","age":20}') or {
-		assert false
-		Any(null)
-	}
+	raw_mp := raw_decode('{"name":"Bob","age":20}') ?
 	mp := raw_mp.as_map()
 	assert mp['name'].str() == 'Bob'
 	assert mp['age'].int() == 20
 }
 
 fn test_raw_decode_null() {
-	nul := raw_decode('null') or {
-		assert false
-		Any(null)
-	}
+	nul := raw_decode('null') ?
 	assert nul is Null
 }
 
@@ -63,26 +43,19 @@ fn test_raw_decode_invalid() {
 }
 
 fn test_raw_decode_string_with_dollarsign() {
-	str := raw_decode(r'"Hello $world"') or {
-		assert false
-		Any(null)
-	}
+	str := raw_decode(r'"Hello $world"') ?
 	assert str.str() == r'Hello $world'
 }
 
 fn test_raw_decode_map_with_whitespaces() {
-	raw_mp := raw_decode(' \n\t{"name":"Bob","age":20}\n\t') or {
-		eprintln(err.msg)
-		assert false
-		Any(null)
-	}
+	raw_mp := raw_decode(' \n\t{"name":"Bob","age":20}\n\t') ?
 	mp := raw_mp.as_map()
 	assert mp['name'].str() == 'Bob'
 	assert mp['age'].int() == 20
 }
 
-fn test_nested_array_map() ? {
-	mut parser := new_parser('[[],[[]],[[[[]]]]]', false)
+fn test_nested_array_object() ? {
+	mut parser := new_parser(r'[[[[[],[],[]]]],{"Test":{}},[[]]]', false)
 	decoded := parser.decode() ?
 	assert parser.n_level == 0
 }
