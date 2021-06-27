5254e6b51d267c4cb27cee2654f88a35fe8f4cd2
Ned Palacios <npdoesmc@gmail.com>
2021-06-27 13:47:36 +0800

    x.json2: fix decoder test fns not having an option return type

--- vlib/x/json2/decoder_test.v
+++ vlib/x/json2/decoder_test.v
@@ -1,40 +1,40 @@
 module json2
 
-fn test_raw_decode_string() {
+fn test_raw_decode_string() ? {
 	str := raw_decode('"Hello!"') ?
 	assert str.str() == 'Hello!'
 }
 
-fn test_raw_decode_number() {
+fn test_raw_decode_number() ? {
 	num := raw_decode('123') ?
 	assert num.int() == 123
 }
 
-fn test_raw_decode_array() {
+fn test_raw_decode_array() ? {
 	raw_arr := raw_decode('["Foo", 1]') ?
 	arr := raw_arr.arr()
 	assert arr[0].str() == 'Foo'
 	assert arr[1].int() == 1
 }
 
-fn test_raw_decode_bool() {
+fn test_raw_decode_bool() ? {
 	bol := raw_decode('false') ?
 	assert bol.bool() == false
 }
 
-fn test_raw_decode_map() {
+fn test_raw_decode_map() ? {
 	raw_mp := raw_decode('{"name":"Bob","age":20}') ?
 	mp := raw_mp.as_map()
 	assert mp['name'].str() == 'Bob'
 	assert mp['age'].int() == 20
 }
 
-fn test_raw_decode_null() {
+fn test_raw_decode_null() ? {
 	nul := raw_decode('null') ?
 	assert nul is Null
 }
 
-fn test_raw_decode_invalid() {
+fn test_raw_decode_invalid() ? {
 	raw_decode('1z') or {
 		assert err.msg == '[x.json2] invalid token `z` (0:17)'
 		return
@@ -42,12 +42,12 @@ fn test_raw_decode_invalid() {
 	assert false
 }
 
-fn test_raw_decode_string_with_dollarsign() {
+fn test_raw_decode_string_with_dollarsign() ? {
 	str := raw_decode(r'"Hello $world"') ?
 	assert str.str() == r'Hello $world'
 }
 
-fn test_raw_decode_map_with_whitespaces() {
+fn test_raw_decode_map_with_whitespaces() ? {
 	raw_mp := raw_decode(' \n\t{"name":"Bob","age":20}\n\t') ?
 	mp := raw_mp.as_map()
 	assert mp['name'].str() == 'Bob'
