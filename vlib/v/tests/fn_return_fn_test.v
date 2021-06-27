54a40407ea147ba541b8ff360c12426644844782
shadowninja55 <shadowninja1050@gmail.com>
2021-06-27 11:44:49 -0400

    improve fn return fn tests

--- vlib/v/tests/fn_return_fn_test.v
+++ vlib/v/tests/fn_return_fn_test.v
@@ -1,4 +1,15 @@
-fn test_doubler() {
+fn make_squarer() fn(int) int {
+	return fn(n int) int {
+		return n * n
+	}
+}
+
+fn test_fn_return_fn() {
+	squarer := make_squarer()
+	assert squarer(10) == 100
+}
+
+fn test_anon_fn_return_fn() {
 	make_doubler := fn() fn(int) int {
 		return fn(n int) int {
 			return n + n
