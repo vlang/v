15806990f881d8187f6e5c8e9c6d3627e6837849
Delyan Angelov <delian66@gmail.com>
2021-06-29 17:42:29 +0300

    add more regression tests for os.glob

--- vlib/os/glob_test.v
+++ vlib/os/glob_test.v
@@ -8,7 +8,6 @@ fn test_glob_can_find_v_files_3_levels_deep() ? {
 	assert 'vlib/v/ast/table.v' in matches
 	assert 'vlib/v/token/token.v' in matches
 	for f in matches {
-		// println(f)
 		if !f.starts_with('vlib/v/') {
 			assert false
 		}
@@ -21,11 +20,19 @@ fn test_glob_can_find_files_in_current_folder() ? {
 	matches := os.glob('*') ?
 	assert 'README.md' in matches
 	assert 'v.mod' in matches
-	assert 'cmd' in matches
-	assert 'vlib' in matches
+	assert 'cmd' + os.path_separator in matches
+	assert 'vlib' + os.path_separator in matches
 	for f in matches {
 		assert !f.ends_with('.v')
 	}
+}
+
+fn test_glob_can_be_used_with_multiple_patterns() ? {
+	os.chdir(@VMODROOT)
+	matches := os.glob('*', 'cmd/tools/*') ?
+	assert 'README.md' in matches
+	assert 'Makefile' in matches
+	assert 'cmd/tools/test_if_v_test_system_works.v' in matches
 }
 
 fn test_glob_star() ? {
