9e8101d3ee0c1d5c4a62a9316e028642e37f9239
Flinner <flinner.hlqn@aleeas.com>
2021-07-01 14:52:07 +0300

    refactor: tests for `strconv.atoi`

--- vlib/strconv/atoi_test.v
+++ vlib/strconv/atoi_test.v
@@ -22,6 +22,12 @@ fn test_atoi() {
 	} else {
 		assert true
 	}
+	if x := strconv.atoi('string_longer_than_10_chars') {
+		println(x)
+		assert false
+	} else {
+		assert true
+	}
 	if x := strconv.atoi('') {
 		println(x)
 		assert false
@@ -30,23 +36,33 @@ fn test_atoi() {
 	}
 }
 
-fn test_parse_int() {
+fn test_parse_int() ? {
 	// Different bases
-	assert strconv.parse_int('16', 16, 0) == 0x16
-	assert strconv.parse_int('16', 8, 0) == 0o16
-	assert strconv.parse_int('11', 2, 0) == 3
+	assert strconv.parse_int('16', 16, 0) ? == 0x16
+	assert strconv.parse_int('16', 8, 0) ? == 0o16
+	assert strconv.parse_int('11', 2, 0) ? == 3
 	// Different bit sizes
-	assert strconv.parse_int('127', 10, 8) == 127
-	assert strconv.parse_int('128', 10, 8) == 127
-	assert strconv.parse_int('32767', 10, 16) == 32767
-	assert strconv.parse_int('32768', 10, 16) == 32767
-	assert strconv.parse_int('2147483647', 10, 32) == 2147483647
-	assert strconv.parse_int('2147483648', 10, 32) == 2147483647
-	assert strconv.parse_int('9223372036854775807', 10, 64) == 9223372036854775807
-	assert strconv.parse_int('9223372036854775808', 10, 64) == 9223372036854775807
+	assert strconv.parse_int('127', 10, 8) ? == 127
+	assert strconv.parse_int('128', 10, 8) ? == 127
+	assert strconv.parse_int('32767', 10, 16) ? == 32767
+	assert strconv.parse_int('32768', 10, 16) ? == 32767
+	assert strconv.parse_int('2147483647', 10, 32) ? == 2147483647
+	assert strconv.parse_int('2147483648', 10, 32) ? == 2147483647
+	assert strconv.parse_int('9223372036854775807', 10, 64) ? == 9223372036854775807
+	assert strconv.parse_int('9223372036854775808', 10, 64) ? == 9223372036854775807
 	// Invalid bit sizes
-	assert strconv.parse_int('123', 10, 65) == 0
-	assert strconv.parse_int('123', 10, -1) == 0
+	if x := strconv.parse_int('123', 10, -1) {
+		println(x)
+		assert false
+	} else {
+		assert true
+	}
+	if x := strconv.parse_int('123', 10, 65) {
+		println(x)
+		assert false
+	} else {
+		assert true
+	}
 }
 
 fn test_common_parse_uint2() {
