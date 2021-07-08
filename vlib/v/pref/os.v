8b92ccd08c4765f868628f962c18865df49391ef
pancake <pancake@nopcode.org>
2021-07-08 19:26:46 +0200

    pref: add enums for SerenityOS

--- vlib/v/pref/os.v
+++ vlib/v/pref/os.v
@@ -16,6 +16,7 @@ pub enum OS {
 	js // TODO
 	android
 	solaris
+	serenity
 	haiku
 	raw
 	all
@@ -34,6 +35,7 @@ pub fn os_from_string(os_str string) ?OS {
 		'dragonfly' { return .dragonfly }
 		'js' { return .js }
 		'solaris' { return .solaris }
+		'serenity' { return .serenity }
 		'android' { return .android }
 		'haiku' { return .haiku }
 		'raw' { return .raw }
@@ -57,6 +59,7 @@ pub fn (o OS) str() string {
 		.js { return 'JavaScript' }
 		.android { return 'Android' }
 		.solaris { return 'Solaris' }
+		.serenity { return 'serenity' }
 		.haiku { return 'Haiku' }
 		.raw { return 'Raw' }
 		.all { return 'all' }
@@ -88,6 +91,13 @@ pub fn get_host_os() OS {
 	$if dragonfly {
 		return .dragonfly
 	}
+	/*
+	// TODO: uncomment after regenerating vc.c
+	$if serenity {
+		return .serenity
+	}
+	*/
+
 	$if solaris {
 		return .solaris
 	}
