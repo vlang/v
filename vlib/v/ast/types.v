vlang/v

commit 974737f26bf722c902f9d2a4fe8344c0129f70b2

vfmt: keep `module mymodule ... x := []thread MyStruct{}` as it is

vlib/v/ast/types.v
@@ -865,6 +865,15 @@ pub fn (mytable &Table) type_to_code(t Type) string {
 pub fn (t &Table) type_to_str_using_aliases(typ Type, import_aliases map[string]string) string {
 	sym := t.get_type_symbol(typ)
 	mut res := sym.name
+	// Note, that the duplication of code in some of the match branches here
+	// is VERY deliberate. DO NOT be tempted to use `else {}` instead, because
+	// that strongly reduces the usefullness of the exhaustive checking that
+	// match does.
+	//    Using else{} here led to subtle bugs in vfmt discovered *months*
+	// after the original code was written.
+	//    It is important that each case here is handled *explicitly* and
+	// *clearly*, and that when a new kind is added, it should also be handled
+	// explicitly.
 	match sym.kind {
 		.int_literal, .float_literal {
 			res = sym.name
@@ -989,7 +998,11 @@ pub fn (t &Table) type_to_str_using_aliases(typ Type, import_aliases map[string]
 			}
 			return 'void'
 		}
-		else {
+		.thread {
+			res = 'thread ' +
+				t.type_to_str_using_aliases(sym.thread_info().return_type, import_aliases)
+		}
+		.alias, .any, .sum_type, .interface_, .size_t, .aggregate, .placeholder, .enum_ {
 			res = t.shorten_user_defined_typenames(res, import_aliases)
 		}
 	}

vlib/v/fmt/tests/thread_in_a_module_keep.vv
@@ -1,0 +1,17 @@
+module my_module
+
+import another
+import other as ooo
+
+pub fn do_something() {
+	mut simples := []MyStruct{}
+	//
+	mut threads := []thread MyStruct{}
+	threads.wait()
+	//
+	mut another_threads := []thread another.MyStruct{}
+	another_threads.wait()
+	//
+	mut other_threads := []thread ooo.MyStruct{}
+	other_threads.wait()
+}

commit b5f1cf80a60f6d48c5cc64304f9a9de7a11b70be

ci: fix regression bug in formatting of go_array_wait_test.v (caused by 974737f)

vlib/v/ast/types.v
@@ -999,8 +999,10 @@ pub fn (t &Table) type_to_str_using_aliases(typ Type, import_aliases map[string]
 			return 'void'
 		}
 		.thread {
-			res = 'thread ' +
-				t.type_to_str_using_aliases(sym.thread_info().return_type, import_aliases)
+			rtype := sym.thread_info().return_type
+			if rtype != 1 {
+				res = 'thread ' + t.type_to_str_using_aliases(rtype, import_aliases)
+			}
 		}
 		.alias, .any, .sum_type, .interface_, .size_t, .aggregate, .placeholder, .enum_ {
 			res = t.shorten_user_defined_typenames(res, import_aliases)