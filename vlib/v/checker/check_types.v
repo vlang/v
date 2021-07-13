e86e094f8c08b91be8f38b100e72a97410f9b6de
shadowninja55 <shadowninja1050@gmail.com>
2021-07-13 14:18:08 -0400

    fmt check_types.v

--- vlib/v/checker/check_types.v
+++ vlib/v/checker/check_types.v
@@ -523,13 +523,13 @@ pub fn (mut c Checker) string_lit(mut node ast.StringLiteral) ast.Type {
 								first_digit := node.val[idx - 5] - 48
 								second_digit := node.val[idx - 4] - 48
 								if first_digit > 1 {
-									c.error(hex_lit_overflow_message, end_pos)
+									c.error(checker.hex_lit_overflow_message, end_pos)
 								} else if first_digit == 1 && second_digit > 0 {
-									c.error(hex_lit_overflow_message, end_pos)
+									c.error(checker.hex_lit_overflow_message, end_pos)
 								}
 							}
 							else {
-								c.error(hex_lit_overflow_message, end_pos)
+								c.error(checker.hex_lit_overflow_message, end_pos)
 							}
 						}
 						idx++
