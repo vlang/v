1484bdd19b7d33668659352cfa290de65a8f1dc4
Raúl Hernández <me@spaceface.dev>
2021-07-14 18:50:44 +0200

    [review] generic_interface_test: add a different struct implementing the interface

--- vlib/v/tests/generic_interface_test.v
+++ vlib/v/tests/generic_interface_test.v
@@ -10,6 +10,17 @@ fn (a Animal<T>) get<T>() T {
 	return a.metadata
 }
 
+// different struct implementing the same interface:
+struct Mineral<T> {
+	value T
+}
+
+fn (m Mineral<T>) get<T>() T {
+	return m.value
+}
+
+////
+
 fn extract<T>(xs []Gettable<T>) []T {
 	return xs.map(it.get())
 }
@@ -21,19 +32,22 @@ fn extract_basic<T>(xs Gettable<T>) T {
 fn test_extract() {
 	a := Animal<int>{123}
 	b := Animal<int>{456}
+	c := Mineral<int>{789}
 
-	arr := [Gettable<int>(a), Gettable<int>(b)]
+	arr := [Gettable<int>(a), Gettable<int>(b), Gettable<int>(c)]
 	assert typeof(arr).name == '[]Gettable<int>'
 
 	x := extract<int>(arr)
-	assert x == [123, 456]
+	assert x == [123, 456, 789]
 }
 
 fn test_extract_basic() {
 	a := Animal<int>{123}
 	b := Animal<int>{456}
+	c := Mineral<int>{789}
 
 	aa := extract_basic(a)
 	bb := extract_basic(b)
-	assert '$aa | $bb' == '123 | 456'
+	cc := extract_basic(c)
+	assert '$aa | $bb | $cc' == '123 | 456 | 789'
 }
