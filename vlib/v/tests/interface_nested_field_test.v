123816c9d9770882b9ab2df1f4123716720085be
Enzo Baldisserri <enzo.baldisserri@outlook.fr>
2021-07-19 14:26:23 +0200

    add tests

--- vlib/v/tests/interface_nested_field_test.v
+++ vlib/v/tests/interface_nested_field_test.v
@@ -37,3 +37,29 @@ fn test_interface_nested_field() {
 	assert ret[0] == 'bar'
 	assert ret[1] == 'subbar'
 }
+
+interface Root {
+	v View
+}
+
+struct MyRoot {
+	v View
+}
+
+interface View {
+	render() int
+}
+
+struct MyView {}
+
+fn (m MyView) render() int {
+	return 24
+}
+
+fn receive_root(r Root) int {
+	return r.v.render()
+}
+
+fn test_nested_interface_fields() {
+	assert receive_root(MyRoot{MyView{}}) == 24
+}
