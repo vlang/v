18605973bae58f09564f90ec372f1933f5debcfc
Louis Schmieder <dev@louis-schmieder.de>
2021-07-24 15:03:54 +0200

    just use one type array

--- vlib/pg/orm.v
+++ vlib/pg/orm.v
@@ -62,7 +62,7 @@ pub fn (db DB) drop(table string) ? {
 // utils
 
 fn pg_stmt_worker(db DB, query string, data orm.QueryData, where orm.QueryData) ?[]Row {
-	mut param_types := []Oid{}
+	mut param_types := []u32{}
 	mut param_vals := []&char{}
 	mut param_lens := []int{}
 	mut param_formats := []int{}
@@ -72,105 +72,100 @@ fn pg_stmt_worker(db DB, query string, data orm.QueryData, where orm.QueryData) 
 	pg_stmt_binder(mut param_types, mut param_vals, mut param_lens, mut param_formats,
 		where)
 
-	mut u32_param_types := []u32{}
-	for oid in param_types {
-		u32_param_types << u32(oid)
-	}
-
-	res := C.PQexecParams(db.conn, query.str, param_vals.len, u32_param_types.data, param_vals.data,
+	res := C.PQexecParams(db.conn, query.str, param_vals.len, param_types.data, param_vals.data,
 		param_lens.data, param_formats.data, 0)
 	return db.handle_error_or_result(res, 'orm_stmt_worker')
 }
 
-fn pg_stmt_binder(mut types []Oid, mut vals []&char, mut lens []int, mut formats []int, d orm.QueryData) {
+fn pg_stmt_binder(mut types []u32, mut vals []&char, mut lens []int, mut formats []int, d orm.QueryData) {
 	for data in d.data {
 		pg_stmt_match(mut types, mut vals, mut lens, mut formats, data)
 	}
 }
 
-fn pg_stmt_match(mut types []Oid, mut vals []&char, mut lens []int, mut formats []int, data orm.Primitive) {
+fn pg_stmt_match(mut types []u32, mut vals []&char, mut lens []int, mut formats []int, data orm.Primitive) {
 	d := data
 	match data {
 		bool {
-			types << .t_bool
+			types << u32(Oid.t_bool)
 			vals << &char(&(d as bool))
 			lens << int(sizeof(bool))
 			formats << 1
 		}
 		byte {
-			types << .t_char
+			types << u32(Oid.t_char)
 			vals << &char(&(d as byte))
 			lens << int(sizeof(byte))
 			formats << 1
 		}
 		u16 {
-			types << .t_int2
+			types << u32(Oid.t_int2)
 			num := conv.htn16(&data)
 			vals << &char(&num)
 			lens << int(sizeof(u16))
 			formats << 1
 		}
 		u32 {
-			types << .t_int4
+			types << u32(Oid.t_int4)
 			num := conv.htn32(&data)
 			vals << &char(&num)
 			lens << int(sizeof(u32))
 			formats << 1
 		}
 		u64 {
-			types << .t_int8
+			types << u32(Oid.t_int8)
 			num := conv.htn64(&data)
 			vals << &char(&num)
 			lens << int(sizeof(u64))
 			formats << 1
 		}
 		i8 {
-			types << .t_char
+			types << u32(Oid.t_char)
 			vals << &char(&(d as i8))
 			lens << int(sizeof(i8))
 			formats << 1
 		}
 		i16 {
-			types << .t_int2
+			types << u32(Oid.t_int2)
 			num := conv.htn16(unsafe { &u16(&data) })
 			vals << &char(&num)
 			lens << int(sizeof(i16))
 			formats << 1
 		}
 		int {
-			types << .t_int4
+			types << u32(Oid.t_int4)
 			num := conv.htn32(unsafe { &u32(&data) })
 			vals << &char(&num)
 			lens << int(sizeof(int))
 			formats << 1
 		}
 		i64 {
-			types << .t_int8
+			types << u32(Oid.t_int8)
 			num := conv.htn64(unsafe { &u64(&data) })
 			vals << &char(&num)
 			lens << int(sizeof(i64))
 			formats << 1
 		}
 		f32 {
-			types << .t_float4
+			types << u32(Oid.t_float4)
 			vals << &char(unsafe { &f32(&(d as f32)) })
 			lens << int(sizeof(f32))
 			formats << 1
 		}
 		f64 {
-			types << .t_float8
+			types << u32(Oid.t_float8)
 			vals << &char(unsafe { &f64(&(d as f64)) })
 			lens << int(sizeof(f64))
 			formats << 1
 		}
 		string {
-			types << .t_text
+			types << u32(Oid.t_text)
 			vals << data.str
 			lens << data.len
 			formats << 0
 		}
 		time.Time {
-			types << .t_int4
+			types << u32(Oid.t_int4)
 			vals << &char(&int(data.unix))
 			lens << int(sizeof(u32))
 			formats << 1
