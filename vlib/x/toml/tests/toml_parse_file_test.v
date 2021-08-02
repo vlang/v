53f4bb38b989b328ea88be6a957e82f5c12240ce
lmp <dev.larpon@gmail.com>
2021-08-02 14:00:24 +0200

    toml: add value extraction, add more tests

--- vlib/x/toml/tests/toml_parse_file_test.v
+++ vlib/x/toml/tests/toml_parse_file_test.v
@@ -1,12 +1,9 @@
 import os
 import x.toml
 
-fn test_parse_file() {
-	out_path := os.join_path(os.temp_dir(), 'v_toml_tests')
-	test_file := os.join_path(out_path, 'toml_parse_file_test_1.toml')
-	// Text from the eaxmple in the README.md:
-	// https://github.com/toml-lang/toml/blob/3b11f6921da7b6f5db37af039aa021fee450c091/README.md#Example
-	toml_str := '# Test TOML file
+// TODO whole text from the example in the README.md:
+// https://github.com/toml-lang/toml/blob/3b11f6921da7b6f5db37af039aa021fee450c091/README.md#Example
+const toml_text = '# Test TOML file
 
 title = "TOML Example"
 
@@ -19,9 +16,27 @@ server = "192.168.1.1"
 ports = [ 8000, 8001, 8002 ]
 connection_max = 5000
 enabled = true'
+
+fn test_parse_file() {
+	out_path := os.join_path(os.temp_dir(), 'v_toml_tests')
+	test_file := os.join_path(out_path, 'toml_parse_file_test_1.toml')
 	os.mkdir_all(out_path) or { assert false }
-	os.write_file(test_file, toml_str) or { assert false }
-	ast_root := toml.parse_file(test_file)
-	// dump(ast_root)
+	os.write_file(test_file, toml_text) or { assert false }
+	toml_doc := toml.parse_file(test_file)
+
+	assert toml_doc.value('title') as string == 'TOML Example'
+
+	// dump(toml_doc.ast)
 	// assert false
+}
+
+fn test_parse_text() {
+	toml_doc := toml.parse_text(toml_text)
+	assert toml_doc.value('title') as string == 'TOML Example'
+}
+
+fn test_i64() {
+	toml_txt := 'integer = 120'
+	toml_doc := toml.parse_text(toml_txt)
+	assert toml_doc.value('integer') as i64 == 120
 }
--- vlib/x/toml/toml.v
+++ vlib/x/toml/toml.v
@@ -3,11 +3,13 @@
 // that can be found in the LICENSE file.
 module toml
 
-// import os
+import os
 import x.toml.ast
 import x.toml.input
 import x.toml.scanner
 import x.toml.parser
+
+pub type Any = []Any | bool | f64 | i64 | map[string]Any | string // TODO add more builtin types - or use json2.Any + Date etc. ??
 
 // Config is used to configure the toml parser.
 // Only one of the fields `text` or `file_path`, is allowed to be set at time of configuration.
@@ -18,18 +20,94 @@ pub:
 	parse_comments bool
 }
 
+// Doc is a representation of a TOML document.
+// A document can be constructed from a `string` buffer or from a file path
+pub struct Doc {
+pub:
+	ast &ast.Root
+}
+
 // parse_file parses the TOML file in `path`.
-// on successful parsing parse_file returns an `&ast.Root` node.
-pub fn parse_file(path string) &ast.Root {
-	in_config := input.Config{
+pub fn parse_file(path string) Doc {
+	input_config := input.Config{
 		file_path: path
 	}
 	scanner_config := scanner.Config{
-		input: in_config
+		input: input_config
 	}
 	parser_config := parser.Config{
 		scanner: scanner.new_scanner(scanner_config)
 	}
 	mut p := parser.new_parser(parser_config)
-	return p.parse()
+	return Doc{
+		ast: p.parse()
+	}
+}
+
+// parse_text parses the TOML document provided in `text`.
+pub fn parse_text(text string) Doc {
+	input_config := input.Config{
+		text: text
+	}
+	scanner_config := scanner.Config{
+		input: input_config
+	}
+	parser_config := parser.Config{
+		scanner: scanner.new_scanner(scanner_config)
+	}
+	mut p := parser.new_parser(parser_config)
+	return Doc{
+		ast: p.parse()
+	}
+}
+
+// parse is a convenience function that parses the TOML document provided in `input`.
+// parse automatically try to determine if type of `input` is a file or text.
+// For explicit parsing of input see `parse_file` or `parse_text`.
+pub fn parse(toml string) Doc {
+	mut input_config := input.Config{}
+	if os.is_file(toml) {
+		input_config = input.Config{
+			file_path: toml
+		}
+	} else {
+		input_config = input.Config{
+			text: toml
+		}
+	}
+
+	scanner_config := scanner.Config{
+		input: input_config
+	}
+	parser_config := parser.Config{
+		scanner: scanner.new_scanner(scanner_config)
+	}
+	mut p := parser.new_parser(parser_config)
+	return Doc{
+		ast: p.parse()
+	}
+}
+
+// value queries a value from the TOML document.
+pub fn (d Doc) value(key string) Any {
+	values := d.ast.table as map[string]ast.Value
+	value := values[key]
+
+	// `match` isn't very good for these types of constructs...
+	if value is ast.Quoted {
+		return Any((value as ast.Quoted).text)
+	} else if value is ast.Number {
+		str := (value as ast.Number).text
+		if str.contains('.') {
+			return Any(str.f64())
+		}
+		return Any(str.i64())
+	}
+	/*
+	TODO else if value is map[string]ast.Value {
+		return value(???)
+	}*/
+	// TODO add more types
+	panic(@MOD + '.' + @STRUCT + '.' + @FN + ' can\'t convert "$value"')
+	return Any('')
 }
