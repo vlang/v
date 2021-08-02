1945becbb14887f6ff6cabc41841feb03be5f808
lmp <dev.larpon@gmail.com>
2021-08-02 14:12:55 +0200

    toml: rename test file

--- vlib/x/toml/tests/toml_test.v
+++ vlib/x/toml/tests/toml_test.v
@@ -1,7 +1,7 @@
 import os
 import x.toml
 
-// TODO whole text from the example in the README.md:
+// TODO Goal: Complete text from the example in the README.md:
 // https://github.com/toml-lang/toml/blob/3b11f6921da7b6f5db37af039aa021fee450c091/README.md#Example
 const toml_text = '# Test TOML file
 
@@ -24,7 +24,9 @@ fn test_parse_file() {
 	os.write_file(test_file, toml_text) or { assert false }
 	toml_doc := toml.parse_file(test_file)
 
-	assert toml_doc.value('title') as string == 'TOML Example'
+	title := toml_doc.value('title')
+	assert title == toml.Any('TOML Example')
+	assert title as string == 'TOML Example'
 
 	// dump(toml_doc.ast)
 	// assert false
@@ -32,11 +34,17 @@ fn test_parse_file() {
 
 fn test_parse_text() {
 	toml_doc := toml.parse_text(toml_text)
-	assert toml_doc.value('title') as string == 'TOML Example'
+	title := toml_doc.value('title')
+	assert title == toml.Any('TOML Example')
+	assert title as string == 'TOML Example'
 }
 
 fn test_i64() {
-	toml_txt := 'integer = 120'
+	toml_txt := 'i64 = 120'
 	toml_doc := toml.parse_text(toml_txt)
-	assert toml_doc.value('integer') as i64 == 120
+
+	value := toml_doc.value('i64')
+	assert value == toml.Any(i64(120))
+	assert value as i64 == 120
 }
+
