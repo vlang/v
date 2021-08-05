c7582f1fbb45ba1879ceac5f40d06b4b2ee2fa83
Enzo Baldisserri <enzo.baldisserri@outlook.fr>
2021-08-05 23:59:53 +0200

    format files

--- vlib/v/tests/option_struct_init_test.v
+++ vlib/v/tests/option_struct_init_test.v
@@ -4,18 +4,19 @@ struct Foo {
 	c int
 }
 
-fn add(mut i &int) ?int {
-	i++
-	return i
+struct Holder {
+mut:
+	i int
+}
+
+fn add(mut h Holder) ?int {
+	h.i++
+	return h.i
 }
 
 fn test_struct_init_with_multiple_optionals() {
-	mut i := 0
-	foo := Foo{
-		add(mut &i) or { 0 }
-		add(mut &i) or { 0 }
-		add(mut &i) or { 0 }
-	}
+	mut h := Holder{}
+	foo := Foo{add(mut h) or { 0 }, add(mut h) or { 0 }, add(mut h) or { 0 }}
 
 	assert foo == Foo{1, 2, 3}
 }
