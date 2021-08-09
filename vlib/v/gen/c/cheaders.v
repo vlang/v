d9b58ac1057a40069bf10932be8a95e71104718b
crthpl <theo@reinsberg.info>
2021-08-09 07:37:01 -0700

    make v_segmentation_fault_handler pure V

--- vlib/v/gen/c/cheaders.v
+++ vlib/v/gen/c/cheaders.v
@@ -501,18 +501,6 @@ static void* g_live_info = NULL;
 #endif
 '
 
-const segfault_handler = '
-#if __STDC_HOSTED__ == 1
-	void v_segmentation_fault_handler(int sig) {
-		void *array[40];
-		size_t size = backtrace(array, 40);
-		fprintf(stderr, "Error: signal %d:", sig);
-		backtrace_symbols_fd(array, size, STDERR_FILENO);
-		exit(1);
-	}
-#endif
-'
-
 const c_builtin_types = '
 //================================== builtin types ================================*/
 typedef int64_t i64;
