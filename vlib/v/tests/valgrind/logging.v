969c9031904b23ecc5768a0d21b5356c35848c53
Delyan Angelov <delian66@gmail.com>
2021-08-10 17:51:36 +0300

    add the loop from original sample program, but with a shorter time.sleep for each iteration

--- vlib/v/tests/valgrind/logging.v
+++ vlib/v/tests/valgrind/logging.v
@@ -1,4 +1,5 @@
 import log
+import time
 
 fn main() {
 	mut l := log.Log{}
@@ -10,4 +11,8 @@ fn main() {
 	l.warn('warn')
 	l.error('an error')
 	l.debug('some debug info')
+	for i := 0; i < 100; i++ {
+		l.info('123456')
+		time.sleep(1 * time.millisecond)
+	}
 }
