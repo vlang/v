691be21ccedf8b7d6ee368b3865e883ead08b8b6
Leo Developer <leodeveloper@protonmail.com>
2021-08-17 19:27:33 +0200

    fix horizontal rule

--- vlib/v/doc/utils.v
+++ vlib/v/doc/utils.v
@@ -92,7 +92,7 @@ pub fn merge_doc_comments(comments []DocComment) string {
 
 		mut is_horizontalrule := false
 		line_no_spaces := line_trimmed.replace(' ', '')
-		for char in ['+', '-', '*', '_', '~'] {
+		for char in ['-', '=', '*', '_', '~'] {
 			if line_no_spaces.starts_with(char.repeat(3))
 				&& line_no_spaces.count(char) == line_no_spaces.len {
 				is_horizontalrule = true
