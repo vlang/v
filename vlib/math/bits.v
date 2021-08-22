d6e886033fca15db5335070fc57928aacef31db0
ulises-jeremias <ulisescf.24@gmail.com>
2021-08-22 01:10:34 -0300

    Moved bits from C to V backend

--- vlib/math/bits.v
+++ vlib/math/bits.v
@@ -15,6 +15,17 @@ const (
 	frac_mask = ((u64(1) << u64(shift)) - u64(1))
 )
 
+// inf returns positive infinity if sign >= 0, negative infinity if sign < 0.
+pub fn inf(sign int) f64 {
+	v := if sign >= 0 { uvinf } else { uvneginf }
+	return f64_from_bits(v)
+}
+
+// nan returns an IEEE 754 ``not-a-number'' value.
+pub fn nan() f64 {
+	return f64_from_bits(uvnan)
+}
+
 // is_nan reports whether f is an IEEE 754 ``not-a-number'' value.
 pub fn is_nan(f f64) bool {
 	// IEEE 754 says that only NaNs satisfy f != f.
