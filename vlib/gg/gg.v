352df873d5d526b02ed5ac53fbf42d496e13ae77
Benjamin Stigsen <benjamin.stigsen@gmail.com>
2021-08-22 23:37:20 +0200

    gg: add scaling to draw_empty_poly() (#11272)

--- vlib/gg/gg.v
+++ vlib/gg/gg.v
@@ -668,9 +668,9 @@ pub fn (ctx &Context) draw_empty_poly(points []f32, c gx.Color) {
 
 	sgl.begin_line_strip()
 	for i in 0 .. len {
-		sgl.v2f(points[2 * i], points[2 * i + 1])
+		sgl.v2f(points[2 * i] * ctx.scale, points[2 * i + 1] * ctx.scale)
 	}
-	sgl.v2f(points[0], points[1])
+	sgl.v2f(points[0] * ctx.scale, points[1] * ctx.scale)
 	sgl.end()
 }
 
