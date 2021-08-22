363cffe77fc06e5f8a7b1c109682355fcc745f6b
Ulises Jeremias Cornejo Fandos <ulisescf.24@gmail.com>
2021-08-22 10:24:33 -0300

    Updated abs to don't override negative 0.0

--- vlib/math/abs.v
+++ vlib/math/abs.v
@@ -3,16 +3,16 @@ module math
 // Returns the absolute value.
 [inline]
 pub fn abs(x f64) f64 {
-	if x < 0.0 {
-		return -x
+	if x > 0.0 {
+		return x
 	}
-	return x
+	return -x
 }
 
 [inline]
 pub fn fabs(x f64) f64 {
-	if x < 0.0 {
-		return -x
+	if x > 0.0 {
+		return x
 	}
-	return x
+	return -x
 }
