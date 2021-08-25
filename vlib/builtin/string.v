183b718d8c67ba6dbd15ce2d9e2061d3851b7b4f
playX <adel.prokurov@gmail.com>
2021-08-25 11:21:05 +0300

    js: Add IError interface

--- vlib/builtin/string.v
+++ vlib/builtin/string.v
@@ -359,6 +359,7 @@ pub fn (s string) replace_each(vals []string) string {
 		with := vals[rep_i + 1]
 		for {
 			idx = s_.index_after(rep, idx)
+			
 			if idx == -1 {
 				break
 			}
@@ -366,6 +367,7 @@ pub fn (s string) replace_each(vals []string) string {
 			for i in 0 .. rep.len {
 				unsafe {
 					s_.str[idx + i] = 127
+					
 				}
 			}
 			// We need to remember both the position in the string,
@@ -374,6 +376,7 @@ pub fn (s string) replace_each(vals []string) string {
 				idx: idx
 				val_idx: rep_i
 			}
+			
 			idx += rep.len
 			new_len += with.len - rep.len
 		}
@@ -396,6 +399,7 @@ pub fn (s string) replace_each(vals []string) string {
 			for j in 0 .. with.len {
 				unsafe {
 					b[b_i] = with[j]
+					
 				}
 				b_i++
 			}
