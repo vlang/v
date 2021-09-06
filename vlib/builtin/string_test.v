cb83cbd195740ed9a8b5d855669d400615eb7a8d
Tanguy SALMON <tanguy.salmon@gmail.com>
2021-09-06 15:18:43 +0700

    Fix fmt in test file

--- vlib/builtin/string_test.v
+++ vlib/builtin/string_test.v
@@ -913,6 +913,6 @@ fn test_string_to_rune() {
 
 fn test_index_any() {
 	x := 'abcdefghij'
-	assert x.index_any("ef") == 4
-	assert x.index_any("fe") == 4
+	assert x.index_any('ef') == 4
+	assert x.index_any('fe') == 4
 }
