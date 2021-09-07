6468eb9b07a6be66069fd207df153e01adf9bf68
Enzo Baldisserri <enzo.baldisserri@outlook.fr>
2021-09-08 00:00:20 +0200

    fmt files

--- vlib/v/tests/c_function_mut_param/code_test.v
+++ vlib/v/tests/c_function_mut_param/code_test.v
@@ -2,10 +2,10 @@ module main
 
 #include "@VMODROOT/code.c"
 
-fn C.mut_arg(key byteptr, mut val &size_t)
+fn C.mut_arg(key &byte, mut val &size_t)
 
 fn test_c_function_mut_param() {
-	key := byteptr(1)
+	key := &byte(1)
 	mut val := size_t(1)
 	C.mut_arg(key, mut &val)
 	assert val == size_t(5)
