3fcd64f6078bcd5705758a4df0f7e3f33b4783cb
crthpl <theo@reinsberg.info>
2021-09-07 16:10:37 -0700

    fix warnings and fix g++ compilation

--- vlib/sync/sync_default.c.v
+++ vlib/sync/sync_default.c.v
@@ -32,6 +32,18 @@ fn C.sem_trywait(voidptr) int
 fn C.sem_timedwait(voidptr, voidptr) int
 fn C.sem_destroy(voidptr) int
 
+[typedef]
+struct C.pthread_mutex_t {}
+
+[typedef]
+struct C.pthread_rwlock_t {}
+
+[typedef]
+struct C.pthread_rwlockattr_t {}
+
+[typedef]
+struct C.sem_t {}
+
 // [init_with=new_mutex] // TODO: implement support for this struct attribute, and disallow Mutex{} from outside the sync.new_mutex() function.
 [heap]
 pub struct Mutex {
