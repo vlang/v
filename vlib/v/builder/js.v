337622b8ff6eb7db4cdab8237d05d0700a2f796f
playX <adel.prokurov@gmail.com>
2021-09-08 10:39:18 +0300

    vfmt

--- vlib/v/builder/js.v
+++ vlib/v/builder/js.v
@@ -25,7 +25,6 @@ pub fn (mut b Builder) build_js(v_files []string, out_file string) {
 }
 
 pub fn (mut b Builder) compile_js() {
-
 	mut files := b.get_builtin_files()
 	files << b.get_user_files()
 	b.set_module_lookup_paths()
