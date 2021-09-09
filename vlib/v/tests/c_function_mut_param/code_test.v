07a8c8318ffa5547d2af995cd101141e69feccfe
Delyan Angelov <delian66@gmail.com>
2021-09-09 17:11:16 +0300

    update test code to use usize instead of size_t

--- vlib/v/tests/c_function_mut_param/code_test.v
+++ vlib/v/tests/c_function_mut_param/code_test.v
@@ -2,11 +2,11 @@ module main
 
 #include "@VMODROOT/code.c"
 
-fn C.mut_arg(key &byte, mut val size_t)
+fn C.mut_arg(key &byte, mut val usize)
 
 fn test_c_function_mut_param() {
 	key := &byte(1)
-	mut val := size_t(1)
+	mut val := usize(1)
 	C.mut_arg(key, mut &val)
-	assert val == size_t(5)
+	assert val == usize(5)
 }
