12ec900d209f36bd5d77e3894bfca9c14d70a07b
Alexander Medvednikov <alexander@vlang.io>
2021-09-14 01:02:20 +0300

    checker: fix the void type check

--- vlib/v/checker/checker.v
+++ vlib/v/checker/checker.v
@@ -3901,11 +3901,12 @@ pub fn (mut c Checker) assign_stmt(mut node ast.AssignStmt) {
 	}
 	if node.left.len != right_len {
 		if right_first is ast.CallExpr {
-			if node.left_types.len > 0 && node.left_types[0] != ast.void_type {
+			if node.left_types.len > 0 && node.left_types[0] == ast.void_type {
 				// If it's a void type, it's an unknown variable, already had an error earlier.
-				c.error('assignment mismatch: $node.left.len variable(s) but `${right_first.name}()` returns $right_len value(s)',
-					node.pos)
+				return
 			}
+			c.error('assignment mismatch: $node.left.len variable(s) but `${right_first.name}()` returns $right_len value(s)',
+				node.pos)
 		} else {
 			c.error('assignment mismatch: $node.left.len variable(s) $right_len value(s)',
 				node.pos)
