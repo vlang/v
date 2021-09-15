6e894f7d5ea7c8e957d371c92d9415415d1df6de
lmp <dev.larpon@gmail.com>
2021-09-15 13:04:15 +0200

    examples: add simple toml example

---
+++ examples/toml.v
@@ -0,0 +1,21 @@
+import x.toml
+
+const toml_text = '# Test TOML file
+
+title = "TOML in V example"
+
+[v]
+name = "V"
+open_sourced = 2019-06-22T20:20:28
+
+[network]
+ip = "192.168.1.1"
+ports = [ 8000, 8001, 8002 ]
+connection_max = 5000
+enabled = true'
+
+fn main() {
+	doc := toml.parse(toml_text)
+	title := doc.value('title').string()
+	println(title)
+}
