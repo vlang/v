6a99b65c0caa18e332ab1ee846144456f6d03c18
yuyi98 <yuyi98@163.com>
2021-09-25 16:01:10 +0800

    fix another case

--- vlib/v/tests/if_expr_with_nested_array_call_test.v
+++ vlib/v/tests/if_expr_with_nested_array_call_test.v
@@ -1,6 +1,14 @@
-fn test_if_expr_with_nested_array_call() {
+fn test_if_expr_with_nested_array_call1() {
 	arr := ['']
 	if arr.len == 1 || (arr[1] == '' && arr.all(it[0].is_letter())) {
+		println('yes')
+		assert true
+	}
+}
+
+fn test_if_expr_with_nested_array_call2() {
+	arr := ['abc']
+	if (arr.len == 1 && arr.all(it.len > 0 && it.bytes().any(it == `b`))) || arr[1] == '' {
 		println('yes')
 		assert true
 	}
