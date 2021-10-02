0916806350c88355ac80d856520b4f7f72eed600
Delyan Angelov <delian66@gmail.com>
2021-10-02 18:20:24 +0300

    ci: do not use htonll and ntohll on windows, use the portable versions instead.

--- vlib/net/conv/conv.c.v
+++ vlib/net/conv/conv.c.v
@@ -19,3 +19,42 @@ pub fn nth32(host u32) u32 {
 pub fn nth16(host u16) u16 {
 	return C.ntohs(host)
 }
+
+//******************************
+
+struct Bytes {
+mut:
+	first u32
+	last  u32
+}
+
+union LongLong {
+	Bytes
+	ll u64
+}
+
+// host to net 64 (htonll)
+pub fn htn64(host u64) u64 {
+	mut ll := LongLong{
+		ll: host
+	}
+
+	unsafe {
+		ll.first = htn32(ll.first)
+		ll.last = htn32(ll.last)
+	}
+	return unsafe { ll.ll }
+}
+
+// net to host 64 (ntohll)
+pub fn nth64(net u64) u64 {
+	mut ll := LongLong{
+		ll: net
+	}
+
+	unsafe {
+		ll.first = nth32(ll.first)
+		ll.last = nth32(ll.last)
+	}
+	return unsafe { ll.ll }
+}
