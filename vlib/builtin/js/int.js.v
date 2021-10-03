2f7d15d489f894dcdbfc369b7c7a1d9dd12840d2
playX <adel.prokurov@gmail.com>
2021-10-03 07:35:31 +0300

    js: More integer methods, add int_test.js.v

--- vlib/builtin/js/int.js.v
+++ vlib/builtin/js/int.js.v
@@ -1,5 +1,26 @@
 module builtin
 
+pub fn (i i8) str() string {
+	mut res := ''
+	#res.str = i.val.toString()
+
+	return res
+}
+
+pub fn (i i16) str() string {
+	mut res := ''
+	#res.str = i.val.toString()
+
+	return res
+}
+
+pub fn (i u16) str() string {
+	mut res := ''
+	#res.str = i.val.toString()
+
+	return res
+}
+
 pub fn (i int) str() string {
 	mut res := ''
 	#res = new string( i )
@@ -47,4 +68,82 @@ pub fn (i int_literal) str() string {
 	#res.str = i.val.toString()
 
 	return res
+}
+
+pub fn (x u64) hex() string {
+	res := ''
+	#res.str = x.val.toString(16)
+
+	return res
+}
+
+pub fn (x i64) hex() string {
+	res := ''
+	#res.str = x.val.toString(16)
+
+	return res
+}
+
+pub fn (x u32) hex() string {
+	res := ''
+	#res.str = x.val.toString(16)
+
+	return res
+}
+
+pub fn (x u16) hex() string {
+	res := ''
+	#res.str = x.val.toString(16)
+
+	return res
+}
+
+pub fn (x i8) hex() string {
+	res := ''
+	#res.str = x.val.toString(16)
+
+	return res
+}
+
+pub fn (x i16) hex() string {
+	res := ''
+	#res.str = x.val.toString(16)
+
+	return res
+}
+
+pub fn (x int) hex() string {
+	res := ''
+	#res.str = x.val.toString(16)
+
+	return res
+}
+
+pub fn (x int_literal) hex() string {
+	res := ''
+	#res.str = x.val.toString(16)
+
+	return res
+}
+
+pub fn (x byte) hex() string {
+	res := ''
+	#res.str = x.val.toString(16)
+
+	return res
+}
+
+// hex returns a string with the hexadecimal representation
+// of the byte elements of the array.
+pub fn (b []byte) hex() string {
+	mut hex := ''
+	for i in b {
+		n0 := i >> 4
+		println(n0)
+		#hex.str += n0.val < 10 ? String.fromCharCode(n0.val) : String.fromCharCode(n0.val + 87)
+
+		n1 := i & 0xF
+		#hex.str += n1.val < 10 ? String.fromCharCode(n1.val) : String.fromCharCode(n1.val + 87)
+	}
+	return hex
 }
