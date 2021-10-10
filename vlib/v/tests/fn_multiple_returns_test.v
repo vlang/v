7a08827e35aee6a34d5df0c03a87f16d9123934b
05st <05st@users.noreply.github.com>
2021-10-10 11:29:01 -0500

    move test, try to fix msvc error

--- vlib/v/tests/fn_multiple_returns_test.v
+++ vlib/v/tests/fn_multiple_returns_test.v
@@ -82,3 +82,29 @@ fn test_multi_values() {
 	assert x == 'abc'
 	assert y == 'def'
 }
+
+fn match_expr(x bool) (int, int) {
+	return match x {
+		true { 1, 1 }
+		else { 0, 0 }
+	}
+}
+
+fn if_expr(x bool) (int, int) {
+	return if x { 3, 3 } else { 2, 2 }
+}
+
+fn test_multi_return_if_match_expr() {
+	a, b := match_expr(true)
+	c, d := match_expr(false)
+	x, y := if_expr(true)
+	z, w := if_expr(false)
+	assert a == 1
+	assert b == 1
+	assert c == 0
+	assert d == 0
+	assert x == 3
+	assert y == 3
+	assert z == 2
+	assert w == 2
+}
