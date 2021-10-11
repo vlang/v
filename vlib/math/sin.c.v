35b301f73c7069a096ab821fc744eb21769b3f99
Ulises Jeremias Cornejo Fandos <ulisesjcf_24@outlook.com>
2021-10-11 08:20:07 -0300

    math: add some benchmark tests (#12142)

--- vlib/math/sin.c.v
+++ vlib/math/sin.c.v
@@ -1,29 +1,13 @@
 module math
 
-fn C.cos(x f64) f64
-
 fn C.cosf(x f32) f32
 
-fn C.sin(x f64) f64
-
 fn C.sinf(x f32) f32
-
-// cos calculates cosine.
-[inline]
-pub fn cos(a f64) f64 {
-	return C.cos(a)
-}
 
 // cosf calculates cosine. (float32)
 [inline]
 pub fn cosf(a f32) f32 {
 	return C.cosf(a)
-}
-
-// sin calculates sine.
-[inline]
-pub fn sin(a f64) f64 {
-	return C.sin(a)
 }
 
 // sinf calculates sine. (float32)
