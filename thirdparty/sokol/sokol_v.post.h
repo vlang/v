7ace7b9d628b79c322bb2cbcf131723dc489fb21
lmp <dev.larpon@gmail.com>
2021-10-13 10:25:51 +0200

    sokol: fix disaster search-replace

--- thirdparty/sokol/sokol_v.post.h
+++ thirdparty/sokol/sokol_v.post.h
@@ -14,15 +14,15 @@
 	void v_sapp_screenshot(const char * filename) {
 		int window_width = sapp_width();
 		int window_height = sapp_height();
-		const int num_pixels = window_width * windowHeight * 3;
+		const int num_pixels = window_width * window_height * 3;
 		unsigned char pixels[num_pixels];
 
 		glPixelStorei(GL_PACK_ALIGNMENT, 1);
 		glReadBuffer(GL_FRONT);
-		glReadPixels(0, 0, window_width, windowHeight, GL_BGR_EXT, GL_UNSIGNED_BYTE, pixels);
+		glReadPixels(0, 0, window_width, window_height, GL_BGR_EXT, GL_UNSIGNED_BYTE, pixels);
 
 		FILE *outputFile = fopen(filename, "w");
-		short header[] = {0, 2, 0, 0, 0, 0, (short) window_width, (short) windowHeight, 24};
+		short header[] = {0, 2, 0, 0, 0, 0, (short) window_width, (short) window_height, 24};
 
 		fwrite(&header, sizeof(header), 1, outputFile);
 		fwrite(pixels, num_pixels, 1, outputFile);
