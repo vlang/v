664d64809ac0365bf476480f4cd48990f560c8f7
lmp <dev.larpon@gmail.com>
2021-10-13 10:24:44 +0200

    sokol: fix up screenshot C code

--- thirdparty/sokol/sokol_v.post.h
+++ thirdparty/sokol/sokol_v.post.h
@@ -12,20 +12,20 @@
 	}
 #elif defined(SOKOL_GLCORE33) || defined(SOKOL_GLES2) || defined(SOKOL_GLES3)
 	void v_sapp_screenshot(const char * filename) {
-		int windowWidth = sapp_width();
-		int windowHeight = sapp_height();
-		const int numberOfPixels = windowWidth * windowHeight * 3;
-		unsigned char pixels[numberOfPixels];
+		int window_width = sapp_width();
+		int window_height = sapp_height();
+		const int num_pixels = window_width * windowHeight * 3;
+		unsigned char pixels[num_pixels];
 
 		glPixelStorei(GL_PACK_ALIGNMENT, 1);
 		glReadBuffer(GL_FRONT);
-		glReadPixels(0, 0, windowWidth, windowHeight, GL_BGR_EXT, GL_UNSIGNED_BYTE, pixels);
+		glReadPixels(0, 0, window_width, windowHeight, GL_BGR_EXT, GL_UNSIGNED_BYTE, pixels);
 
 		FILE *outputFile = fopen(filename, "w");
-		short header[] = {0, 2, 0, 0, 0, 0, (short) windowWidth, (short) windowHeight, 24};
+		short header[] = {0, 2, 0, 0, 0, 0, (short) window_width, (short) windowHeight, 24};
 
 		fwrite(&header, sizeof(header), 1, outputFile);
-		fwrite(pixels, numberOfPixels, 1, outputFile);
+		fwrite(pixels, num_pixels, 1, outputFile);
 		fclose(outputFile);
 	}
 #endif
