dadff95eb4e3854f851c4e7177d6910dc6173eb2
Jorjick <68968657+Jerrody@users.noreply.github.com>
2021-10-25 12:08:46 +0300

    Refactored for the more good readability.
    Please, stop using massively **else if** when exists a match pattern!

--- examples/regex/regex_with_memoization.v
+++ examples/regex/regex_with_memoization.v
@@ -13,96 +13,113 @@ fn regex_match_core(src string, pat string, src_pos int, pat_pos int, mut memo [
 	}
 	mut spos := src_pos
 	mut ppos := pat_pos
-	if spos >= src.len && ppos >= pat.len {
-		memo[src_pos][pat_pos] = 1
-		return true
-	} else if spos < src.len && ppos >= pat.len {
-		memo[src_pos][pat_pos] = 0
-		return false
-	} else if spos >= src.len && ppos < pat.len {
-		if pat[ppos] == `\\` {
-			ppos++
+
+	match true {
+		spos >= src.len && ppos >= pat.len {
+			memo[src_pos][pat_pos] = 1
+			return true
 		}
-		res := ppos + 1 < pat.len && pat[ppos + 1] in [`*`, `?`]
-			&& regex_match_core(src, pat, spos, ppos + 2, mut memo)
-		memo[src_pos][pat_pos] = if res { 1 } else { 0 }
-		return res
-	} else {
-		first_is_bslash := pat[ppos] == `\\`
-		if first_is_bslash {
-			ppos++
+		spos < src.len && ppos >= pat.len {
+			memo[src_pos][pat_pos] = 0
+			return false
 		}
-		first_bslash_and_match := first_is_bslash && ppos < pat.len
-			&& (((pat[ppos] == `d` && src[spos].is_digit())
-			|| (pat[ppos] == `D` && !src[spos].is_digit())
-			|| (pat[ppos] == `s` && src[spos].is_space())
-			|| (pat[ppos] == `S` && !src[spos].is_space())
-			|| (pat[ppos] == `w` && (src[spos].is_digit() || src[spos].is_letter()
-			|| src[spos] == `_`)) || (pat[ppos] == `W` && !(src[spos].is_digit()
-			|| src[spos].is_letter() || src[spos] == `_`)))
-			|| (pat[ppos] in [`d`, `D`, `s`, `S`, `w`, `W`] && ppos + 1 < pat.len
-			&& pat[ppos + 1] in [`*`, `?`, `+`])
-			|| (pat[ppos] !in [`d`, `D`, `s`, `S`, `w`, `W`] && src[spos] == pat[ppos]))
-		if ppos + 1 < pat.len {
-			match pat[ppos + 1] {
-				`*` {
-					if first_bslash_and_match {
-						res := regex_match_core(src, pat, spos + 1, ppos - 1, mut memo)
-							|| regex_match_core(src, pat, spos, ppos + 2, mut memo)
-						memo[src_pos][pat_pos] = if res { 1 } else { 0 }
-						return res
-					} else if src[spos] == pat[ppos] || pat[ppos] == `.` {
-						res := regex_match_core(src, pat, spos + 1, ppos, mut memo)
-							|| regex_match_core(src, pat, spos, ppos + 2, mut memo)
-						memo[src_pos][pat_pos] = if res { 1 } else { 0 }
-						return res
-					} else {
-						res := regex_match_core(src, pat, spos, ppos + 2, mut memo)
-						memo[src_pos][pat_pos] = if res { 1 } else { 0 }
-						return res
-					}
-				}
-				`+` {
-					if first_bslash_and_match {
-						res := regex_match_core(src, pat, spos + 1, ppos - 1, mut memo)
-							|| regex_match_core(src, pat, spos + 1, ppos + 2, mut memo)
-						memo[src_pos][pat_pos] = if res { 1 } else { 0 }
-						return res
-					} else if src[spos] == pat[ppos] || pat[ppos] == `.` {
-						res := regex_match_core(src, pat, spos + 1, ppos, mut memo)
-							|| regex_match_core(src, pat, spos + 1, ppos + 2, mut memo)
-						memo[src_pos][pat_pos] = if res { 1 } else { 0 }
-						return res
-					} else {
-						memo[src_pos][pat_pos] = 0
-						return false
-					}
-				}
-				`?` {
-					if first_bslash_and_match || src[spos] == pat[ppos] || pat[ppos] == `.` {
-						res := regex_match_core(src, pat, spos + 1, ppos + 2, mut memo)
-							|| regex_match_core(src, pat, spos, ppos + 2, mut memo)
-						memo[src_pos][pat_pos] = if res { 1 } else { 0 }
-						return res
-					} else {
-						res := regex_match_core(src, pat, spos, ppos + 2, mut memo)
-						memo[src_pos][pat_pos] = if res { 1 } else { 0 }
-						return res
-					}
-				}
-				else {}
+		spos >= src.len && ppos < pat.len {
+			if pat[ppos] == `\\` {
+				ppos++
 			}
-		}
-		if first_is_bslash {
-			res := first_bslash_and_match
-				&& regex_match_core(src, pat, spos + 1, ppos + 1, mut memo)
+			res := ppos + 1 < pat.len && pat[ppos + 1] in [`*`, `?`]
+				&& regex_match_core(src, pat, spos, ppos + 2, mut memo)
 			memo[src_pos][pat_pos] = if res { 1 } else { 0 }
 			return res
-		} else {
-			res := (src[spos] == pat[ppos] || pat[ppos] == `.`) && pat[ppos] != `\\`
-				&& regex_match_core(src, pat, spos + 1, ppos + 1, mut memo)
-			memo[src_pos][pat_pos] = if res { 1 } else { 0 }
-			return res
+		} 
+		else {
+			first_is_bslash := pat[ppos] == `\\`
+			if first_is_bslash {
+				ppos++
+			}
+
+			first_bslash_and_match := first_is_bslash && ppos < pat.len
+				&& (((pat[ppos] == `d` && src[spos].is_digit())
+				|| (pat[ppos] == `D` && !src[spos].is_digit())
+				|| (pat[ppos] == `s` && src[spos].is_space())
+				|| (pat[ppos] == `S` && !src[spos].is_space())
+				|| (pat[ppos] == `w` && (src[spos].is_digit() || src[spos].is_letter()
+				|| src[spos] == `_`)) || (pat[ppos] == `W` && !(src[spos].is_digit()
+				|| src[spos].is_letter() || src[spos] == `_`)))
+				|| (pat[ppos] in [`d`, `D`, `s`, `S`, `w`, `W`] && ppos + 1 < pat.len
+				&& pat[ppos + 1] in [`*`, `?`, `+`])
+				|| (pat[ppos] !in [`d`, `D`, `s`, `S`, `w`, `W`] && src[spos] == pat[ppos]))
+
+			if ppos + 1 < pat.len {
+				match pat[ppos + 1] {
+					`*` {
+						match true {
+							first_bslash_and_match {
+								res := regex_match_core(src, pat, spos + 1, ppos - 1, mut memo)
+									|| regex_match_core(src, pat, spos, ppos + 2, mut memo)
+								memo[src_pos][pat_pos] = if res { 1 } else { 0 }
+								return res
+							}
+							src[spos] == pat[ppos] || pat[ppos] == `.` {
+								res := regex_match_core(src, pat, spos + 1, ppos, mut memo)
+									|| regex_match_core(src, pat, spos, ppos + 2, mut memo)
+								memo[src_pos][pat_pos] = if res { 1 } else { 0 }
+								return res
+							}
+							else {
+								res := regex_match_core(src, pat, spos, ppos + 2, mut memo)
+								memo[src_pos][pat_pos] = if res { 1 } else { 0 }
+								return res
+							}
+						}
+					}
+					`+` {
+						match true {
+							first_bslash_and_match {
+								res := regex_match_core(src, pat, spos + 1, ppos - 1, mut memo)
+									|| regex_match_core(src, pat, spos + 1, ppos + 2, mut memo)
+								memo[src_pos][pat_pos] = if res { 1 } else { 0 }
+								return res
+							}
+							src[spos] == pat[ppos] || pat[ppos] == `.` {
+								res := regex_match_core(src, pat, spos + 1, ppos, mut memo)
+									|| regex_match_core(src, pat, spos + 1, ppos + 2, mut memo)
+								memo[src_pos][pat_pos] = if res { 1 } else { 0 }
+								return res
+							} 
+							else {
+								memo[src_pos][pat_pos] = 0
+								return false
+							}
+						}
+					}
+					`?` {
+						if first_bslash_and_match || src[spos] == pat[ppos] || pat[ppos] == `.` {
+							res := regex_match_core(src, pat, spos + 1, ppos + 2, mut memo)
+								|| regex_match_core(src, pat, spos, ppos + 2, mut memo)
+							memo[src_pos][pat_pos] = if res { 1 } else { 0 }
+							return res
+						} else {
+							res := regex_match_core(src, pat, spos, ppos + 2, mut memo)
+							memo[src_pos][pat_pos] = if res { 1 } else { 0 }
+							return res
+						}
+					}
+					else {}
+				}
+			}
+
+			if first_is_bslash {
+				res := first_bslash_and_match
+					&& regex_match_core(src, pat, spos + 1, ppos + 1, mut memo)
+				memo[src_pos][pat_pos] = if res { 1 } else { 0 }
+				return res
+			} else {
+				res := (src[spos] == pat[ppos] || pat[ppos] == `.`) && pat[ppos] != `\\`
+					&& regex_match_core(src, pat, spos + 1, ppos + 1, mut memo)
+				memo[src_pos][pat_pos] = if res { 1 } else { 0 }
+				return res
+			}
 		}
 	}
 }
