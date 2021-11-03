4f21a002106d641c5b5bfa75928d61cf7ea2fa87
lmp <dev.larpon@gmail.com>
2021-11-03 11:49:19 +0100

    toml: add extra array of tables test

---
+++ vlib/toml/tests/array_of_tables_1_level_test.v
@@ -0,0 +1,28 @@
+import os
+import toml
+
+const (
+	toml_table_text = '
+[[products]]
+name = "Hammer"
+sku = 738594937
+
+[[products]]  # empty table within the array
+
+[[products]]
+name = "Nail"
+sku = 284758393
+
+color = "gray"'
+)
+
+fn test_tables() {
+	mut toml_doc := toml.parse(toml_table_text) or { panic(err) }
+
+	toml_json := toml_doc.to_json()
+
+	eprintln(toml_json)
+	assert toml_json == os.read_file(
+		os.real_path(os.join_path(os.dir(@FILE), 'testdata', os.file_name(@FILE).all_before_last('.'))) +
+		'.out') or { panic(err) }
+}
