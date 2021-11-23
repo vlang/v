e840b2888eabd760ff53c2650be8c598b0173735
lmp <dev.larpon@gmail.com>
2021-11-23 14:32:49 +0100

    toml: fix implicit array allocation

--- vlib/toml/tests/burntsushi.toml-test_test.v
+++ vlib/toml/tests/burntsushi.toml-test_test.v
@@ -19,8 +19,6 @@ const (
 		'integer/long.toml', // TODO awaits BUG fix with strconv.parse_int('-9223372036854775808')
 		// Float
 		'float/inf-and-nan.toml',
-		// Table
-		'table/array-implicit.toml',
 		// Date-time
 		'datetime/milliseconds.toml',
 		// Key
