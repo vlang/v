fcec600df70ff7fc997c08938aae7e2fe76c2550
pancake <pancake@nopcode.org>
2021-11-26 02:48:39 +0100

    native: Initial working hello world compilation for w64

--- vlib/v/gen/native/macho.v
+++ vlib/v/gen/native/macho.v
@@ -395,9 +395,18 @@ fn (mut g Gen) write_symbol(s Symbol) {
 fn (mut g Gen) sym_string_table() int {
 	begin := g.buf.len
 	g.zeroes(1)
-	at := i64(0x100000000)
+	/*
+	for s in g.strs {
+		g.patch_string(s)
+	}
+	*/
 	for i, s in g.strings {
-		g.write64_at(at + g.buf.len, int(g.str_pos[i]))
+		pos := g.buf.len - int(g.str_pos[i])
+		if g.pref.os == .windows {
+			g.write32_at(int(g.str_pos[i]), pos - 4) // 0x402028 + pos)
+		} else {
+			g.write64_at(pos, int(g.str_pos[i]))
+		}
 		g.write_string(s)
 		g.write8(0)
 	}
