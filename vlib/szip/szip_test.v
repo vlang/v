969e0dce206a94db087d94b1360829bdcafcd008
Delyan Angelov <delian66@gmail.com>
2021-11-28 15:30:26 +0200

    szip: cleanup after tests

--- vlib/szip/szip_test.v
+++ vlib/szip/szip_test.v
@@ -10,9 +10,21 @@ const (
 	fpath2       = os.join_path(test_path, fname2)
 )
 
-fn test_szip_create_temp_files() ? {
+fn cleanup() {
 	os.chdir(os.temp_dir()) or {}
 	os.rmdir_all(test_path) or {}
+	os.rm(test_out_zip) or {}
+}
+
+fn testsuite_begin() ? {
+	cleanup()
+}
+
+fn testsuite_end() ? {
+	cleanup()
+}
+
+fn test_szip_create_temp_files() ? {
 	os.mkdir(test_path) ?
 	os.write_file(fpath1, 'file one') ?
 	os.write_file(fpath2, 'file two') ?
