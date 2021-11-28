135114feeacf8659579d04e72fc84301778553aa
KyleLin921021 <43753315+KyleLin921021@users.noreply.github.com>
2021-11-29 00:56:32 +0800

    add stricter test for raw string

--- vlib/v/tests/raw_string_test.v
+++ vlib/v/tests/raw_string_test.v
@@ -6,3 +6,17 @@ fn test_raw_string_not_escaped_by_transformer() {
 	assert r'a\nb' + r'a\nb' == r'a\nba\nb'
 	assert 'a\nb' + r'a\nb' == 'a\nba\\nb'
 }
+
+fn test_many_pluses() {
+	a := r'x\n'
+	b := 'x\n'
+	c := a + b
+	d := c + c
+	e := d + d
+	result := r'x\nx
+x\nx
+x\nx
+x\nx
+'
+	assert e == result
+}
