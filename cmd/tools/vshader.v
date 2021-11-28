6f874eec39d9e761afd43f6f40880b85dfabc838
Delyan Angelov <delian66@gmail.com>
2021-11-28 15:54:44 +0200

    small cleanup

--- cmd/tools/vshader.v
+++ cmd/tools/vshader.v
@@ -90,7 +90,7 @@ fn main() {
 		show_help: fp.bool('help', `h`, false, 'Show this help text.')
 		force_update: fp.bool('force-update', `u`, false, 'Force update of the sokol-shdc tool.')
 		verbose: fp.bool('verbose', `v`, false, 'Be verbose about the tools progress.')
-		slangs: fp.string_multi('slang', `l`, 'Shader dialects to generate code for. Default is all. Available: $supported_slangs')
+		slangs: fp.string_multi('slang', `l`, 'Shader dialects to generate code for. Default is all.\n                            Available dialects: $supported_slangs')
 	}
 	if opt.show_help {
 		println(fp.usage())
