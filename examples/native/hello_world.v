cf99a0a5fbb280e3943f9dc5d770074e0fadb2c3
pancake <pancake@nopcode.org>
2021-11-30 00:58:14 +0100

    native: implement for-c and for-in range loops
    * Support increment and decrement loops
    * Handle for-in: int..int  and int..var
    * Honor var (int, bool) assign statements
    * Add missing insn printlns for v -verbose
    * Add tests with different constructions

--- examples/native/hello_world.v
+++ examples/native/hello_world.v
@@ -6,11 +6,14 @@
 
 fn main() {
 	println('native test')
-	// i := 0
-	// for i < 5 {
+	mut i := 0
+	for i < 5 {
+		println('Hello world from V native machine code generator!')
+		i++
+	}
+	// not implemented for the native backend yet
 	for _ in 1 .. 5 {
 		println('Hello world from V native machine code generator!')
-		// i++
 	}
 	/*
 	println('Hello again!')
