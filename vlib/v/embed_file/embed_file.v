f58f9feea29611f77925fe99c4582268bb84adb9
Leo Developer <leodeveloper@protonmail.com>
2021-11-29 12:29:24 +0100

    make apath private again

--- vlib/v/embed_file/embed_file.v
+++ vlib/v/embed_file/embed_file.v
@@ -5,15 +5,15 @@ import os
 // https://github.com/vlang/rfcs/blob/master/embedding_resources.md
 // EmbedFileData encapsulates functionality for the `$embed_file()` compile time call.
 pub struct EmbedFileData {
+	apath string
 mut:
 	compressed        &byte
 	uncompressed      &byte
 	free_compressed   bool
 	free_uncompressed bool
 pub:
-	len   int
-	path  string
-	apath string
+	len  int
+	path string
 }
 
 pub fn (ed EmbedFileData) str() string {
