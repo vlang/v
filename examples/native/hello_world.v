7f91ffb34535aad2783c6cbee98012b0034caf19
Delyan Angelov <delian66@gmail.com>
2021-11-30 10:50:25 +0200

    cleanup examples/native/hello_world.v

--- examples/native/hello_world.v
+++ examples/native/hello_world.v
@@ -1,23 +1,18 @@
-// fn println(s string) { }
-
-// fn test_fn() {
-// println('test fn')
-//}
+fn test_fn() {
+	println('test fn')
+}
 
 fn main() {
 	println('native test')
 	mut i := 0
 	for i < 5 {
-		println('Hello world from V native machine code generator!')
+		println('Hello world from V native machine code generator, in a while style for loop!')
 		i++
 	}
-	// not implemented for the native backend yet
 	for _ in 1 .. 5 {
-		println('Hello world from V native machine code generator!')
+		println('Hello world from V native machine code generator, in a range loop!')
 	}
-	/*
 	println('Hello again!')
-	//test_fn()
+	test_fn()
 	println('done')
-	*/
 }
