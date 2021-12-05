790caa1895cfd40b277515070dc6a5ba720d4088
penguindark <dariodeledda@hotmail.com>
2021-12-05 19:02:15 +0000

    removed explicit path separator from assets path

--- examples/sokol/freetype_raven.v
+++ examples/sokol/freetype_raven.v
@@ -99,7 +99,7 @@ fn init(user_data voidptr) {
 	C.sgl_setup(s)
 	state.fons = sfons.create(512, 512, 1)
 	// or use DroidSerif-Regular.ttf
-	if bytes := os.read_bytes(os.resource_abs_path('../assets/fonts/RobotoMono-Regular.ttf')) {
+	if bytes := os.read_bytes(os.resource_abs_path(os.join_path('..', 'assets', 'fonts', 'RobotoMono-Regular.ttf'))) {
 		println('loaded font: $bytes.len')
 		state.font_normal = C.fonsAddFontMem(state.fons, c'sans', bytes.data, bytes.len,
 			false)
