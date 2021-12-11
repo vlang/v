bf835d47d8b710c4a7197b1ea20c9e4f96fb3845
Delyan Angelov <delian66@gmail.com>
2021-12-12 01:20:21 +0200

    tools: fix `v build-examples` on windows

--- cmd/tools/vbuild-examples.v
+++ cmd/tools/vbuild-examples.v
@@ -12,18 +12,18 @@ const efolders = [
 fn main() {
 	args_string := os.args[1..].join(' ')
 	params := args_string.all_before('build-examples')
-	skip_prefixes := efolders.map(os.join_path(vroot, it))
+	skip_prefixes := efolders.map(os.real_path(os.join_path_single(vroot, it)))
 	res := testing.v_build_failing_skipped(params, 'examples', skip_prefixes, fn (mut session testing.TestSession) {
 		for x in efolders {
 			pathsegments := x.split_any('/')
-			session.add(os.join_path(vroot, ...pathsegments))
+			session.add(os.real_path(os.join_path(vroot, ...pathsegments)))
 		}
 	})
 	if res {
 		exit(1)
 	}
-	if testing.v_build_failing_skipped(params + '-live', os.join_path('examples', 'hot_reload'),
-		skip_prefixes, fn (mut session testing.TestSession) {})
+	if testing.v_build_failing_skipped(params + '-live', os.join_path_single('examples',
+		'hot_reload'), skip_prefixes, fn (mut session testing.TestSession) {})
 	{
 		exit(1)
 	}
