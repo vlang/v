f7aa8fd5a06569d847544a36c54be3f0c2ae9d56
Leo Developer <leodeveloper@protonmail.com>
2021-12-11 23:37:27 +0100

    checker: fix x.${field.name} not working outside of a $if

--- vlib/v/checker/if.v
+++ vlib/v/checker/if.v
@@ -123,9 +123,6 @@ pub fn (mut c Checker) if_expr(mut node ast.IfExpr) ast.Type {
 			} else if !is_comptime_type_is_expr {
 				node.branches[i].stmts = []
 			}
-			if comptime_field_name.len > 0 {
-				c.comptime_fields_type.delete(comptime_field_name)
-			}
 			c.skip_flags = cur_skip_flags
 			if c.fn_level == 0 && c.pref.output_cross_c && c.ct_cond_stack.len > 0 {
 				c.ct_cond_stack.delete_last()
