4e90f5e0bbe09b3442750c6aec5a9b83bed33007
playX <adel.prokurov@gmail.com>
2021-12-15 09:22:42 +0300

    js: Add support for ES5 for map type

--- vlib/builtin/js/map.js.v
+++ vlib/builtin/js/map.js.v
@@ -6,15 +6,47 @@ pub:
 	len int
 }
 
+fn (mut m map) internal_set(key JS.Any, val JS.Any) {
+	//$if es5 {
+	#if ('$toJS' in key) key = key.$toJS();
+	#if (!(key in m.val.map)) m.val.length++;
+	#m.val.map[key] = val
+	/*} $else {
+		# if ('$toJS' in key) key = key.$toJS();
+		# m.val.m.set(key,val);
+	}*/
+	_ := key
+	_ := val
+}
+
+fn (mut m map) internal_get(key JS.Any) JS.Any {
+	mut val := JS.Any(voidptr(0))
+	//$if es5 {
+	#if ('$toJS' in key) key = key.$toJS();
+	#val =  m.val.map[key]
+	/*} $else {
+		# if ('$toJS' in key) key = key.$toJS();
+		# val = m.val.m.get(key)
+	}*/
+	_ := key
+	return val
+}
+
 // Removes the mapping of a particular key from the map.
 [unsafe]
-pub fn (mut m map) delete(key voidptr) {
-	#m.map.delete(key)
+pub fn (mut m map) delete(key JS.Any) {
+	#let k = '$toJS' in key ? key.$toJS() : key;
+
+	#if (delete m.val.map[k]) { m.val.length--; };
+
+	_ := key
 }
 
 pub fn (m &map) free() {}
 
-#map.prototype[Symbol.iterator] = function () { return this.map[Symbol.iterator](); }
+$if !es5 {
+	#map.prototype[Symbol.iterator] = function () { return this.map[Symbol.iterator](); }
+}
 //#Object.defineProperty(map.prototype,"len",{get: function() { return this.map.size; }})
 #map.prototype.toString = function () {
 #function fmtKey(key) { return typeof key == 'string' ? '\'' + key + '\'' : key}
