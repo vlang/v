351d1a1b161cb403e1badbce5fefddfa143f589a
yuyi98 <yuyi98@163.com>
2021-12-18 11:05:35 +0800

    cgen: fix generic array of interface method call

---
+++ vlib/v/tests/generics_array_of_interface_method_call_test.v
@@ -0,0 +1,35 @@
+struct Array<T> {
+pub mut:
+	elements []T
+}
+
+struct String {
+	str string
+}
+
+interface IObject {
+	equals(IObject) bool
+}
+
+pub fn (s1 String) equals(s2 IObject) bool {
+	if s2 is String {
+		return s1.str == s2.str
+	}
+	return false
+}
+
+pub fn (mut m Array<T>) contains(e T) bool {
+	for mut element in m.elements {
+		if element.equals(e) {
+			return true
+		}
+	}
+	return false
+}
+
+fn test_generic_array_of_interface_method_call() {
+	s := String{'hello'}
+	mut a := Array<IObject>{[s]}
+	ret := a.contains(IObject(s))
+	println(ret)
+}
