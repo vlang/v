0b61447ff27eb2d6f6b8ece845c20af07310cd9b
Jeff Mikels <jeffweb@mikels.cc>
2022-01-05 11:41:28 -0500

    fixed arrays.binary_search comment on the algorithm

--- vlib/arrays/arrays.v
+++ vlib/arrays/arrays.v
@@ -351,7 +351,7 @@ pub fn upper_bound<T>(arr []T, val T) ?T {
 
 // binary search, requires `arr` to be sorted, returns index of found item or error.
 // Binary searches on sorted lists can be faster than other array searches because at maximum
-// the algorithm only has to traverse half the list.
+// the algorithm only has to traverse log N elements
 // Example: arrays.binary_search([1, 2, 3, 4], 4) ? => 3
 pub fn binary_search<T>(arr []T, target T) ?int {
 	mut left := 0
