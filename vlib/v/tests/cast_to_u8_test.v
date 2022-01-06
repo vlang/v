1c382ad2b4c82f11963a006215ac8b71d524d2a7
yuyi <yuyi98@163.com>
2022-01-07 00:36:31 +0800

    checker: fix error for cast to u8 (#13058)

---
+++ vlib/v/tests/cast_to_u8_test.v
@@ -0,0 +1,16 @@
+module main
+
+fn test_cast_to_alias() {
+	r1 := u8(byte(1))
+	println(r1)
+	assert '$r1' == '1'
+
+
+	r2 := u8(byte(true))
+	println(r2)
+	assert '$r2' == '1'
+
+	r3 := u8(true)
+	println(r3)
+	assert '$r3' == '1'
+}
