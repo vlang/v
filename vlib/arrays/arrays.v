5e96c0238076aa9451fec9b5ca72277ef45fb9dc
Jeff Mikels <jeffweb@mikels.cc>
2022-01-07 09:48:06 -0500

    regressing the addition of arrays.zip and getting back to documentation

--- vlib/arrays/arrays.v
+++ vlib/arrays/arrays.v
@@ -107,7 +107,11 @@ pub fn merge<T>(a []T, b []T) []T {
 }
 
 // group n arrays into a single array of arrays with n elements
-// (an error will be generated if the type annotation is omitted)
+//
+// This function is analagous to the "zip" function of other languages.
+// To fully interleave two arrays, follow this function with a call to `flatten`.
+//
+// NOTE: An error will be generated if the type annotation is omitted.
 // Example: arrays.group<int>([1,2,3],[4,5,6]) // => [[1, 4], [2, 5], [3, 6]]
 pub fn group<T>(lists ...[]T) [][]T {
 	mut length := if lists.len > 0 { lists[0].len } else { 0 }
@@ -291,41 +295,18 @@ pub fn group_by<K, V>(list []V, grouping_op fn (v V) K) map[K][]V {
 
 // concatenate an array with an arbitrary number of additional values
 //
-// NOTE: if you have two arrays, you should simply use the `<<` operator directly
+// NOTE: if you have two arrays, you should simply use the `<<` operator directly,
+// but this function is retained for functional programming paradigms.
+//
 // Example: arrays.concat([1, 2, 3], 4, 5, 6) == [1, 2, 3, 4, 5, 6] // => true
 // Example: arrays.concat([1, 2, 3], ...[4, 5, 6]) == [1, 2, 3, 4, 5, 6] // => true
 // Example: arr << [4, 5, 6] // does what you need if arr is mutable
-[deprecated]
 pub fn concat<T>(a []T, b ...T) []T {
 	mut m := []T{cap: a.len + b.len}
 
 	m << a
 	m << b
 
-	return m
-}
-
-// zip returns a new array by interleaving the source arrays.
-//
-// NOTE: The two arrays do not need to be equal sizes
-// Example: arrays.zip([1, 3, 5, 7], [2, 4, 6, 8, 10]) // => [1, 2, 3, 4, 5, 6, 7, 8, 10]
-pub fn zip<T>(a []T, b []T) []T {
-	mut m := []T{cap: a.len + b.len}
-	mut i := 0
-	for i < m.cap {
-		// short-circuit the rest of the loop as soon as we can
-		if i >= a.len {
-			m << b[i..]
-			break
-		}
-		if i >= b.len {
-			m << a[i..]
-			break
-		}
-		m << a[i]
-		m << b[i]
-		i++
-	}
 	return m
 }
 
