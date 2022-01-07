7276705684ee8614cfb2cbfe8cda94a938c78d2c
Larpon <Larpon@users.noreply.github.com>
2022-01-07 12:38:21 +0100

    checker: add support for apk comptime if (#13079)

--- vlib/v/checker/checker.v
+++ vlib/v/checker/checker.v
@@ -24,7 +24,7 @@ const (
 	valid_comptime_if_compilers      = ['gcc', 'tinyc', 'clang', 'mingw', 'msvc', 'cplusplus']
 	valid_comptime_if_platforms      = ['amd64', 'i386', 'aarch64', 'arm64', 'arm32', 'rv64', 'rv32']
 	valid_comptime_if_cpu_features   = ['x64', 'x32', 'little_endian', 'big_endian']
-	valid_comptime_if_other          = ['js', 'debug', 'prod', 'test', 'glibc', 'prealloc',
+	valid_comptime_if_other          = ['apk', 'js', 'debug', 'prod', 'test', 'glibc', 'prealloc',
 		'no_bounds_checking', 'freestanding', 'threads', 'js_node', 'js_browser', 'js_freestanding',
 		'interpreter', 'es5']
 	valid_comptime_not_user_defined  = all_valid_comptime_idents()
