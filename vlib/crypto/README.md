83e9585d06d86019b54a4dd117422ba9c2506a57
688862 <96077242+688862@users.noreply.github.com>
2022-01-08 23:08:46 +0800

    crypto: crypto.aes CBC mode moves to crypto.cipher (#13084)

--- vlib/crypto/README.md
+++ vlib/crypto/README.md
@@ -32,12 +32,12 @@ fn main() {
 
 	println('performing encryption')
 	mut encrypted := []byte{len: aes.block_size}
-	cipher.encrypt(mut encrypted, mut data)
+	cipher.encrypt(mut encrypted, data)
 	println(encrypted)
 
 	println('performing decryption')
 	mut decrypted := []byte{len: aes.block_size}
-	cipher.decrypt(mut decrypted, mut encrypted)
+	cipher.decrypt(mut decrypted, encrypted)
 	println(decrypted)
 
 	assert decrypted == data
--- vlib/crypto/aes/aes_test.v
+++ vlib/crypto/aes/aes_test.v
@@ -3,25 +3,16 @@
 // that can be found in the LICENSE file.
 import crypto.aes
 
-fn test_crypto_aes() {
-	// TEST CBC
+fn test_aes() {
 	key := '6368616e676520746869732070617373'.bytes()
 	mut ciphertext := '73c86d43a9d700a253a96c85b0f6b03ac9792e0e757f869cca306bd3cba1c62b'.bytes()
 	block := aes.new_cipher(key)
-	// The IV needs to be unique, but not secure. Therefore it's common to
-	// include it at the beginning of the ciphertext.
-	if ciphertext.len < aes.block_size {
-		panic('ciphertext too short')
-	}
-	iv := ciphertext[..aes.block_size]
-	ciphertext = ciphertext[aes.block_size..]
-	// CBC mode always works in whole blocks.
-	if ciphertext.len % aes.block_size != 0 {
-		panic('ciphertext is not a multiple of the block size')
-	}
-	mode := aes.new_cbc(block, iv)
-	cipher_clone := ciphertext.clone()
-	mode.encrypt_blocks(mut ciphertext, cipher_clone)
-	assert ciphertext.hex() == 'c210459b514668ddc44674885e4979215265a6c44431a248421254ef357a8c2a308a8bddf5623af9df91737562041cf1'
-	println('ok')
+
+	block.encrypt(mut ciphertext, ciphertext.clone())
+	assert ciphertext.hex() == '05d1737fe0a7c12088ff4c94d62ccbd9353361393663383562306636623033616339373932653065373537663836396363613330366264336362613163363262'
+
+	block.decrypt(mut ciphertext, ciphertext.clone())
+
+	assert ciphertext.bytestr() == '73c86d43a9d700a253a96c85b0f6b03ac9792e0e757f869cca306bd3cba1c62b'
+	println('test_aes ok')
 }
