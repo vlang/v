ef562c92a5f7a45820e38093d7f6f626804b8a26
yuyi <yuyi98@163.com>
2022-01-18 22:16:15 +0800

    cgen: fix assigning a new value to the mut sumtype receiver (#13204)

---
+++ vlib/v/tests/mut_receiver_of_sumtype_test.v
@@ -0,0 +1,17 @@
+pub type Foo = Bar | Baz
+
+fn (mut f Foo) set_baz() {
+	f = Baz{}
+}
+
+pub struct Bar {}
+
+pub struct Baz {}
+
+fn test_mut_receiver_of_sumtype() {
+	mut x := Foo(Bar{})
+	x.set_baz()
+
+	println(x)
+	assert '$x' == 'Foo(Baz{})'
+}
