f7cc318da4e57d7523b83def8f1c93ab70ccb475
Delyan Angelov <delian66@gmail.com>
2022-01-28 17:48:53 +0200

    add `[deprecated_after: 2022-02-11]` tag, to turn errors into notices for now

--- vlib/strings/builder.c.v
+++ vlib/strings/builder.c.v
@@ -50,6 +50,7 @@ pub fn (mut b Builder) write_runes(runes []rune) {
 
 // write_b appends a single `data` byte to the accumulated buffer
 [deprecated: 'Use write_byte() instead']
+[deprecated_after: '2022-02-11']
 pub fn (mut b Builder) write_b(data byte) {
 	b << data
 }
