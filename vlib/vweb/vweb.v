5e6941cc73a9f55988dead0deb42dbd05daff1e8
Delyan Angelov <delian66@gmail.com>
2022-02-05 11:33:33 +0200

    vfmt vweb.v

--- vlib/vweb/vweb.v
+++ vlib/vweb/vweb.v
@@ -644,7 +644,7 @@ pub fn (mut ctx Context) mount_static_folder_at(directory_path string, mount_pat
 		return false
 	}
 	dir_path := directory_path.trim_right('/')
-	
+
 	trim_mount_path := mount_path.trim_left('/').trim_right('/')
 	ctx.scan_static_directory(dir_path, '/' + trim_mount_path)
 	return true
