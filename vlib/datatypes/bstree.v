319c73f7f993750f2f2e3b777a3d71a0b5faa80c
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-02-15 22:51:56 +0100

    datatypes: implement the insert and contains operation
    In addition, introduce the unit test in the operation
    
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- vlib/datatypes/bstree.v
+++ vlib/datatypes/bstree.v
@@ -1,26 +1,48 @@
 module datatypes
 
+[heap]
 struct BTreeNode<T> {
 mut:
+	// Mark a node as ready to be walked
 	is_init bool
-	value   T
-	parent  &BTreeNode<T> = 0
-	left    &BTreeNode<T> = 0
-	right   &BTreeNode<T> = 0
+	// Value of the node
+	value T
+	// The parent of the node
+	parent &BTreeNode<T> = 0
+	// The left side with value less than the
+	// value of this node
+	left &BTreeNode<T> = 0
+	// The right side with value grater than the
+	// value of thiss node
+	right &BTreeNode<T> = 0
 }
 
+// Create new root bst node
 fn new_root_node<T>(value T) &BTreeNode<T> {
 	return &BTreeNode<T>{
 		is_init: true
 		value: value
+		parent: new_none_node<T>(true)
+		left: new_none_node<T>(false)
+		right: new_none_node<T>(false)
 	}
 }
 
-fn new_node<T>(parent BTreeNode<T>, value T) &BTreeNode<T> {
+// Create a new generic bst node, this help to create
+// node during the walking tree
+fn new_node<T>(parent &BTreeNode<T>, value T) &BTreeNode<T> {
 	return &BTreeNode<T>{
 		is_init: true
 		value: value
-		parent: &parent
+		parent: parent
+	}
+}
+
+// Create a dummy node
+// FIXME: adding the init parameter as optional
+fn new_none_node<T>(init bool) &BTreeNode<T> {
+	return &BTreeNode<T>{
+		is_init: false
 	}
 }
 
@@ -36,33 +58,24 @@ mut:
 
 // Insert an element in order inside the data structure
 pub fn (mut bst BSTree<T>) insert(value T) bool {
-	if !bst.root.is_init {
+	if bst.is_empty() {
 		bst.root = new_root_node(value)
 		return true
 	}
-
-	if bst.root.value < value {
-		return bst.insert_helper(mut bst.root.right, value)
-	} else if bst.root.value > value {
-		return bst.insert_helper(mut bst.root.left, value)
-	}
-	// we don't accept duplicate for the moment
-	return false
+	return bst.insert_helper(mut bst.root, value)
 }
 
+// Helper function that give the possibility to walk the tree and make
+// the insert operation in the correct position.
 fn (mut bst BSTree<T>) insert_helper(mut node BTreeNode<T>, value T) bool {
-	if !node.is_init {
-		return false
-	}
-
 	if node.value < value {
-		if node.right.is_init {
+		if node.right != 0 && node.right.is_init {
 			return bst.insert_helper(mut node.right, value)
 		}
 		node.right = new_node(node, value)
 		return true
 	} else if node.value > value {
-		if node.left.is_init {
+		if node.left != 0 && node.left.is_init {
 			return bst.insert_helper(mut node.left, value)
 		}
 		node.left = new_node(node, value)
@@ -73,12 +86,32 @@ fn (mut bst BSTree<T>) insert_helper(mut node BTreeNode<T>, value T) bool {
 
 // Check if an element with a given `value` is inside the data structure
 pub fn (bst &BSTree<T>) contains(value T) bool {
-	return false
+	return bst.contains_helper(bst.root, value)
+}
+
+// Helper function to walk the tree, and check the result
+// FIXME: the walk function can be generilized with a general function as params?
+fn (bst &BSTree<T>) contains_helper(node &BTreeNode<T>, value T) bool {
+	if node == 0 || !node.is_init {
+		return false
+	}
+	if node.value < value {
+		return bst.contains_helper(node.right, value)
+	} else if node.value > value {
+		return bst.contains_helper(node.left, value)
+	}
+	assert node.value == value
+	return true
 }
 
 // Remove the element with the value from the data structure
 pub fn (mut bst BSTree<T>) remove(value T) bool {
 	return false
+}
+
+// Check if the tree is empty
+pub fn (bst &BSTree<T>) is_empty() bool {
+	return bst.root == 0
 }
 
 /*
