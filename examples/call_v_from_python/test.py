ce938089c305845308da3f5ab74933b36366ccf9
Mihai Galos <mihaigalos@gmail.com>
2022-02-15 18:22:54 +0100

    Add call V from Python with double parameters

--- examples/call_v_from_python/test.py
+++ examples/call_v_from_python/test.py
@@ -6,3 +6,6 @@ if os.name=="nt":
   so_file="./test.dll"
 my_functions = CDLL(so_file)
 print(my_functions.square(10))
+
+my_functions.sqrt_of_sum_of_squares.restype = c_double
+print(my_functions.sqrt_of_sum_of_squares(c_double(1.1), c_double(2.2)))
