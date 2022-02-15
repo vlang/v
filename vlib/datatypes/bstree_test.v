45410b1ffe7534e79bddc2e6db75ad1f7a426374
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-02-15 22:51:56 +0100

    datatypes: implement the insert and contains operation
    In addition, introduce the unit test in the operation
    
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- vlib/datatypes/bstree_test.v
+++ vlib/datatypes/bstree_test.v
@@ -3,4 +3,17 @@ module datatypes
 fn test_insert_into_bst_one() {
 	mut bst := BSTree<int>{}
 	assert bst.insert(10) == true
+	assert bst.contains(10) == true
+}
+
+fn test_insert_into_bst_two() {
+	mut bst := BSTree<int>{}
+	assert bst.insert(10)
+	assert bst.insert(20)
+	assert bst.insert(9)
+
+	assert bst.contains(9)
+	assert bst.contains(10)
+	assert bst.contains(20)
+	assert bst.contains(11) == false
 }
