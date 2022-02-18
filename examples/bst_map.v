11e55d9e6a43a01b89d39363f380ea4f4a49c4e0
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-02-18 23:18:31 +0100

    bst: adding iteration example
    This is very bad for array of bigger size, beause required to load the tree in an array and this required O(N*N) space.
    
    The iterator patter will improve this solution.
    
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- examples/bst_map.v
+++ examples/bst_map.v
@@ -18,4 +18,11 @@ fn main() {
 	mut bst := datatypes.BSTree<KeyVal>{}
 	bst.insert(KeyVal{ key: 1, val: 12 })
 	println(bst.in_order_traversal())
+
+	bst.insert(KeyVal{key: 2, val: 34})
+	bst.insert(KeyVal{key:-2, val: 203})
+
+	for elem in bst.in_order_traversal() {
+		println(elem.val)
+	}
 }
