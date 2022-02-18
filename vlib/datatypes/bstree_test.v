002f62f03197f3c4f38318455804d656e7736cb2
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-02-18 15:10:02 +0100

    bst: adding in order traversal API
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- vlib/datatypes/bstree_test.v
+++ vlib/datatypes/bstree_test.v
@@ -17,3 +17,13 @@ fn test_insert_into_bst_two() {
 	assert bst.contains(20)
 	assert bst.contains(11) == false
 }
+
+fn test_in_order_bst_visit_one() {
+	mut bst := BSTree<int>{}
+	assert bst.insert(10)
+	assert bst.insert(20)
+	assert bst.insert(21)
+	assert bst.insert(1)
+
+	assert bst.in_order_traversals() == [1, 10, 20, 21]
+}
