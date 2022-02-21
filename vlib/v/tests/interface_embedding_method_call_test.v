07e9ed1a1dc2807147702dbd08fdc8e1e2d3d5e0
yuyi <yuyi98@163.com>
2022-02-21 23:49:38 +0800

    cgen: fix interface embedding method call (#13553)

---
+++ vlib/v/tests/interface_embedding_method_call_test.v
@@ -0,0 +1,59 @@
+fn test_interface_embedding_method_call() {
+	mut window := &Window{}
+	btn := &Button{}
+	window.initables << btn
+	window.run()
+}
+
+[heap]
+pub struct Window {
+mut:
+	initables []Initable
+	popview   PopView = DummyPopup{}
+}
+
+interface Initable {
+mut:
+	init(&Window)
+}
+
+interface Drawable {
+	draw()
+}
+
+pub fn (mut window Window) run() {
+	for mut i in window.initables {
+		i.init(window)
+	}
+	for wd in window.initables {
+		if wd is Drawable {
+			d := wd as Drawable
+			d.draw()
+		}
+	}
+	window.popview.draw()
+}
+
+struct DummyPopup {}
+
+fn (d DummyPopup) draw() {}
+
+interface PopView {
+	Drawable
+}
+
+[heap]
+pub struct Button {
+mut:
+	window &Window = voidptr(0)
+}
+
+pub fn (mut b Button) init(window &Window) {
+	b.window = window
+}
+
+pub fn (b Button) draw() {
+	g := b.window.initables
+	println(g.len)
+	assert g.len == 1
+}
