ee1de066788b3e769a0c6765abce682baff2e02d
gcxfd <i@rmw.link>
2022-02-22 16:34:38 +0800

    net: extract a common Socket struct, reuse it by embedding in TcpSocket & UdpSocket (#13559)

---
+++ vlib/net/socket.v
@@ -0,0 +1,11 @@
+module net
+
+pub struct Socket {
+	pub:
+	handle int
+}
+
+// address gets the address of a socket
+pub fn (s &Socket) address() ?Addr {
+	return addr_from_socket_handle(s.handle)
+}
