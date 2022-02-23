ba9a5412d1861ef94d01de0f06e6675d84c4e52b
bstnbuck <59334447+bstnbuck@users.noreply.github.com>
2022-02-23 11:52:47 +0100

    simplify go version, make it work
    to proof correctness check out Go test: https://go.dev/play/p/KQEz5B8Cb6I

--- vlib/crypto/cipher/ctr.v
+++ vlib/crypto/cipher/ctr.v
@@ -16,28 +16,21 @@ import crypto.internal.subtle
 struct Ctr {
 mut:
 	b        Block
-	ctr      []byte
+	next     []byte
 	out      []byte
 	out_used int
 }
 
-const stream_buffer_size = 512
-
-// new_ctr returns a Ctr which encrypts/decrypts using the given Block in
-// counter mode. The length of iv must be the same as the Block's block size.
 pub fn new_ctr(b Block, iv []byte) Ctr {
-	if iv.len != b.block_size {
-		panic('cipher.new_ctr: IV length must equal block size')
-	}
-	mut buf_size := cipher.stream_buffer_size
-	if buf_size < b.block_size {
-		buf_size = b.block_size
+	block_size := b.block_size
+	if iv.len != block_size {
+		panic('cipher.new_cfb: IV length must be equal block size')
 	}
 	return Ctr{
 		b: b
-		ctr: iv.clone()
 		out: []byte{len: b.block_size}
-		out_used: 0
+		next: iv.clone()
+		out_used: block_size
 	}
 }
 
@@ -45,39 +38,28 @@ pub fn (x &Ctr) xor_key_stream(mut dst_ []byte, src_ []byte) {
 	unsafe {
 		mut dst := *dst_
 		mut src := src_
-
 		if dst.len < src.len {
 			panic('crypto.cipher.xor_key_stream: output smaller than input')
 		}
+
 		if subtle.inexact_overlap(dst[..src.len], src) {
 			panic('crypto.cipher.xor_key_stream: invalid buffer overlap')
 		}
 
 		for src.len > 0 {
-			if x.out_used >= x.out.len - x.b.block_size {
-				mut remain := x.out.len - x.out_used
-				copy(x.out, x.out[x.out_used..])
-
-				x.out = x.out[..x.out.cap]
-				bs := x.b.block_size
-
-				for remain <= x.out.len - bs {
-					x.b.encrypt(mut x.out[remain..], x.ctr)
-					remain += bs
-
-					// increment counter
-					for i := x.ctr.len - 1; i >= 0; i-- {
-						x.ctr[i]++
-						if x.ctr[i] != 0 {
-							break
-						}
-					}
-				}
-				x.out = x.out[..remain]
+			if x.out_used == x.out.len {
+				x.b.encrypt(mut x.out, x.next)
 				x.out_used = 0
 			}
 
 			n := xor_bytes(mut dst, src, x.out[x.out_used..])
+
+			for i := x.next.len - 1; i >= 0; i-- {
+				x.next[i]++
+				if x.next[i] != 0 {
+					break
+				}
+			}
 
 			dst = dst[n..]
 			src = src[n..]
