958f3399185ad565a26770f34e9e5a38d0a930b1
lmp <dev.larpon@gmail.com>
2022-02-24 13:53:31 +0100

    fmt: add old behaviour if no comments are present

--- vlib/v/fmt/fmt.v
+++ vlib/v/fmt/fmt.v
@@ -845,6 +845,8 @@ pub fn (mut f Fmt) const_decl(node ast.ConstDecl) {
 		if node.is_block {
 			f.writeln('')
 		} else {
+			// Write out single line comments after const expr if present
+			// E.g.: `const x = 1 // <comment>`
 			if node.end_comments.len > 0 && node.end_comments[0].text.contains('\n') {
 				f.writeln('\n')
 			}
@@ -855,6 +857,9 @@ pub fn (mut f Fmt) const_decl(node ast.ConstDecl) {
 
 	if node.is_block {
 		f.comments_after_last_field(node.end_comments)
+	} else if node.end_comments.len == 0 {
+		// If no single line comments after the const expr is present
+		f.writeln('')
 	}
 	if node.is_block {
 		f.indent--
