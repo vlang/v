1bbcf12846be23d35b0d39b9867ce77a2726b45c
Subhomoy Haldar <hungrybluedev@gmail.com>
2022-02-26 01:36:45 +0530

    add buffering to wyrand

--- vlib/rand/wyrand/wyrand.v
+++ vlib/rand/wyrand/wyrand.v
@@ -4,7 +4,6 @@
 module wyrand
 
 import rand.seed
-import rand.constants
 import hash
 
 // Redefinition of some constants that we will need for pseudorandom number generation.
@@ -16,9 +15,9 @@ const (
 // WyRandRNG is a RNG based on the WyHash hashing algorithm.
 pub struct WyRandRNG {
 mut:
-	state     u64 = seed.time_seed_64()
-	has_extra bool
-	extra     u32
+	state      u64 = seed.time_seed_64()
+	bytes_left int
+	buffer     u64
 }
 
 // seed sets the seed, needs only two `u32`s in little-endian format as [lower, higher].
@@ -28,27 +27,136 @@ pub fn (mut rng WyRandRNG) seed(seed_data []u32) {
 		exit(1)
 	}
 	rng.state = seed_data[0] | (u64(seed_data[1]) << 32)
-	rng.has_extra = false
+	rng.bytes_left = 0
+	rng.buffer = 0
 }
 
-// u32 updates the PRNG state and returns the next pseudorandom `u32`.
+// byte returns a uniformly distributed pseudorandom 8-bit unsigned positive `byte`.
+[inline]
+pub fn (mut rng WyRandRNG) byte() byte {
+	// Can we extract a value from the buffer?
+	if rng.bytes_left >= 1 {
+		rng.bytes_left -= 1
+		value := byte(rng.buffer)
+		rng.buffer >>= 8
+		return value
+	}
+	// Add a new value to the buffer
+	rng.buffer = rng.internal_u64()
+	rng.bytes_left = 7
+	value := byte(rng.buffer)
+	rng.buffer >>= 8
+	return value
+}
+
+// bytes returns a buffer of `bytes_needed` random bytes.
+[inline]
+pub fn (mut rng WyRandRNG) bytes(bytes_needed int) ?[]byte {
+	if bytes_needed < 0 {
+		return error('can not read < 0 random bytes')
+	}
+	mut res := []byte{len: bytes_needed}
+
+	rng.read(mut res)
+
+	return res
+}
+
+// read fills up the buffer with random bytes.
+pub fn (mut rng WyRandRNG) read(mut buf []byte) {
+	mut bytes_needed := buf.len
+	mut index := 0
+
+	for _ in 0 .. rng.bytes_left {
+		buf[index] = rng.byte()
+		bytes_needed--
+		index++
+	}
+
+	for bytes_needed >= 8 {
+		mut full_value := rng.u64()
+		for _ in 0 .. 8 {
+			buf[index] = byte(full_value)
+			full_value >>= 8
+			index++
+		}
+		bytes_needed -= 8
+	}
+
+	for bytes_needed > 0 {
+		buf[index] += rng.byte()
+		index++
+		bytes_needed--
+	}
+}
+
+[inline]
+fn (mut rng WyRandRNG) step_by(amount int) u64 {
+	next_number := rng.internal_u64()
+
+	bits_left := rng.bytes_left * 8
+	bits_needed := amount - bits_left
+
+	old_value := rng.buffer & ((u64(1) << bits_left) - 1)
+	new_value := next_number & ((u64(1) << bits_needed) - 1)
+	value := old_value | (new_value << bits_left)
+
+	rng.buffer = next_number >> bits_needed
+	rng.bytes_left = 8 - (bits_needed / 8)
+
+	return value
+}
+
+// u16 returns a pseudorandom 16bit int in range `[0, 2¹⁶)`.
+[inline]
+pub fn (mut rng WyRandRNG) u16() u16 {
+	// Can we take a whole u16 out of the buffer?
+	if rng.bytes_left >= 2 {
+		rng.bytes_left -= 2
+		value := u16(rng.buffer)
+		rng.buffer >>= 16
+		return value
+	}
+	if rng.bytes_left > 0 {
+		return u16(rng.step_by(16))
+	}
+	ans := rng.internal_u64()
+	rng.buffer = ans >> 16
+	rng.bytes_left = 6
+	return u16(ans)
+}
+
+// u32 returns a pseudorandom 32bit int in range `[0, 2³²)`.
 [inline]
 pub fn (mut rng WyRandRNG) u32() u32 {
-	if rng.has_extra {
-		rng.has_extra = false
-		return rng.extra
+	// Can we take a whole u32 out of the buffer?
+	if rng.bytes_left >= 4 {
+		rng.bytes_left -= 4
+		value := u32(rng.buffer)
+		rng.buffer >>= 32
+		return value
 	}
-	full_value := rng.u64()
-	lower := u32(full_value & constants.lower_mask)
-	upper := u32(full_value >> 32)
-	rng.extra = upper
-	rng.has_extra = true
-	return lower
+	if rng.bytes_left > 0 {
+		return u32(rng.step_by(32))
+	}
+	// We're out so we start fresh.
+	ans := rng.internal_u64()
+	rng.buffer = ans >> 32
+	rng.bytes_left = 4
+	return u32(ans)
 }
 
-// u64 updates the PRNG state and returns the next pseudorandom `u64`.
+// u64 returns a pseudorandom 64bit int in range `[0, 2⁶⁴)`.
 [inline]
 pub fn (mut rng WyRandRNG) u64() u64 {
+	if rng.bytes_left > 0 {
+		return rng.step_by(64)
+	}
+	return rng.internal_u64()
+}
+
+[inline]
+fn (mut rng WyRandRNG) internal_u64() u64 {
 	unsafe {
 		mut seed1 := rng.state
 		seed1 += wyrand.wyp0
