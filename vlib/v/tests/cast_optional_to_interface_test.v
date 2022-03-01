7f8f571ed23e81a07fc7f44eef18f5e1d8d77e65
yuyi <yuyi98@163.com>
2022-03-02 01:21:55 +0800

    cgen: fix error for optional cast to interface (#13623)

---
+++ vlib/v/tests/cast_optional_to_interface_test.v
@@ -0,0 +1,40 @@
+[heap]
+struct Package {
+	str string
+}
+
+interface Parser {
+	main &Package
+}
+
+struct ParserV1 {
+mut:
+	main &Package
+}
+
+fn new_0_parser() ?ParserV1 {
+	return ParserV1{
+		main: &Package{
+			str: 'test'
+		}
+	}
+}
+
+fn new_parser() ?Parser {
+	return Parser(new_0_parser() ?)
+}
+
+struct Engine {
+	parser Parser
+}
+
+fn test_cast_optional_to_interface() ? {
+	parser := new_parser() ?
+	assert parser.main.str == 'test'
+	eprintln(voidptr(parser.main))
+	e := Engine{
+		parser: parser
+	}
+	assert e.parser.main.str == 'test'
+	eprintln(voidptr(e.parser.main))
+}
