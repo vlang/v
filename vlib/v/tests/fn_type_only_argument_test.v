723aca395005d29d52a59bff220b4e3bb3f2cc19
Delyan Angelov <delian66@gmail.com>
2022-03-10 22:35:11 +0200

    change test to use `time` instead of `gg`, so it can be run everywhere

--- vlib/v/tests/fn_type_only_argument_test.v
+++ vlib/v/tests/fn_type_only_argument_test.v
@@ -1,28 +1,16 @@
 module main
 
-import gg
-import gx
+import time
 
 struct Game {
-	update fn (mut gg.Context) = fn (mut gg gg.Context) {}
-	draw   fn (mut gg.Context) = fn (mut gg gg.Context) {}
+	update fn (mut time.Time) = fn (mut time time.Time) {}
+	draw   fn (mut time.Time) = fn (mut time time.Time) {}
 mut:
-	gg gg.Context
+	time time.Time
 }
 
 fn test_fn_type_only_argument() {
 	mut game := Game{}
-	game.gg = gg.new_context(
-		bg_color: gx.rgb(230, 230, 240)
-		frame_fn: frame
-	)
+	game.time = time.Time{}
 	assert true
-}
-
-fn frame(mut game Game) {
-	game.gg.begin()
-	game.gg.draw_triangle_filled(450, 142, 530, 280, 370, 280, gx.red)
-	game.update(mut game.gg)
-	game.draw(mut game.gg)
-	game.gg.end()
 }
