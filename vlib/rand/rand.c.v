807cd408f7b06ef359909d8c8d030c6f1f291a47
Subhomoy Haldar <hungrybluedev@gmail.com>
2022-03-12 01:09:12 +0530

    decrease gap between PRNG interface and rand module functions

--- vlib/rand/rand.c.v
+++ vlib/rand/rand.c.v
@@ -7,6 +7,10 @@ const clock_seq_hi_and_reserved_valid_values = [`8`, `9`, `a`, `b`]
 // uuid_v4 generates a random (v4) UUID
 // See https://en.wikipedia.org/wiki/Universally_unique_identifier#Version_4_(random)
 pub fn uuid_v4() string {
+	return internal_uuid_v4(mut default_rng)
+}
+
+fn internal_uuid_v4(mut rng PRNG) string {
 	buflen := 36
 	mut buf := unsafe { malloc_noscan(37) }
 	mut i_buf := 0
@@ -14,7 +18,7 @@ pub fn uuid_v4() string {
 	mut d := byte(0)
 	for i_buf < buflen {
 		mut c := 0
-		x = default_rng.u64()
+		x = rng.u64()
 		// do most of the bit manipulation at once:
 		x &= 0x0F0F0F0F0F0F0F0F
 		x += 0x3030303030303030
@@ -54,18 +58,7 @@ pub fn uuid_v4() string {
 
 const ulid_encoding = '0123456789ABCDEFGHJKMNPQRSTVWXYZ'
 
-// ulid generates an Unique Lexicographically sortable IDentifier.
-// See https://github.com/ulid/spec .
-// Note: ULIDs can leak timing information, if you make them public, because
-// you can infer the rate at which some resource is being created, like
-// users or business transactions.
-// (https://news.ycombinator.com/item?id=14526173)
-pub fn ulid() string {
-	return ulid_at_millisecond(u64(time.utc().unix_time_milli()))
-}
-
-// ulid_at_millisecond does the same as `ulid` but takes a custom Unix millisecond timestamp via `unix_time_milli`.
-pub fn ulid_at_millisecond(unix_time_milli u64) string {
+fn internal_ulid_at_millisecond(mut rng PRNG, unix_time_milli u64) string {
 	buflen := 26
 	mut buf := unsafe { malloc_noscan(27) }
 	mut t := unix_time_milli
@@ -78,7 +71,7 @@ pub fn ulid_at_millisecond(unix_time_milli u64) string {
 		i--
 	}
 	// first rand set
-	mut x := default_rng.u64()
+	mut x := rng.u64()
 	i = 10
 	for i < 19 {
 		unsafe {
@@ -88,7 +81,7 @@ pub fn ulid_at_millisecond(unix_time_milli u64) string {
 		i++
 	}
 	// second rand set
-	x = default_rng.u64()
+	x = rng.u64()
 	for i < 26 {
 		unsafe {
 			buf[i] = rand.ulid_encoding[x & 0x1F]
@@ -102,8 +95,7 @@ pub fn ulid_at_millisecond(unix_time_milli u64) string {
 	}
 }
 
-// string_from_set returns a string of length `len` containing random characters sampled from the given `charset`
-pub fn string_from_set(charset string, len int) string {
+fn internal_string_from_set(mut rng PRNG, charset string, len int) string {
 	if len == 0 {
 		return ''
 	}
@@ -117,21 +109,6 @@ pub fn string_from_set(charset string, len int) string {
 		buf[len] = 0
 	}
 	return unsafe { buf.vstring_with_len(len) }
-}
-
-// string returns a string of length `len` containing random characters in range `[a-zA-Z]`.
-pub fn string(len int) string {
-	return string_from_set(english_letters, len)
-}
-
-// hex returns a hexadecimal number of length `len` containing random characters in range `[a-f0-9]`.
-pub fn hex(len int) string {
-	return string_from_set(hex_chars, len)
-}
-
-// ascii returns a random string of the printable ASCII characters with length `len`.
-pub fn ascii(len int) string {
-	return string_from_set(ascii_chars, len)
 }
 
 fn deinit() {
