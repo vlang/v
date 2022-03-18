vlang/v

commit 1d83ab6be167ccfde412c854c703bccf0a494619

parser: check using invalid keyword with none ident (#13743)

vlib/v/parser/parser.v
@@ -1943,6 +1943,7 @@ pub fn (mut p Parser) parse_ident(language ast.Language) ast.Ident {
 		p.register_auto_import('sync')
 	}
 	mut_pos := p.tok.pos()
+	kind_name := '$p.tok.kind'
 	is_mut := p.tok.kind == .key_mut || is_shared || is_atomic
 	if is_mut {
 		p.next()
@@ -1956,7 +1957,11 @@ pub fn (mut p Parser) parse_ident(language ast.Language) ast.Ident {
 		p.next()
 	}
 	if p.tok.kind != .name {
-		p.error('unexpected token `$p.tok.lit`')
+		if is_mut || is_static || is_volatile {
+			p.error_with_pos('the `$kind_name` keyword is invalid here', mut_pos)
+		} else {
+			p.error('unexpected token `$p.tok.lit`')
+		}
 		return ast.Ident{
 			scope: p.scope
 		}

vlib/v/parser/tests/invalid_using_atomic.out
@@ -1,0 +1,6 @@
+vlib/v/parser/tests/invalid_using_atomic.vv:2:5: error: the `atomic` keyword is invalid here
+    1 | fn main() {
+    2 |     if atomic true {
+      |        ~~~~~~
+    3 |         println(true)
+    4 |     }

vlib/v/parser/tests/invalid_using_atomic.vv
@@ -1,0 +1,5 @@
+fn main() {
+	if atomic true {
+		println(true)
+	}
+}

vlib/v/parser/tests/invalid_using_mut.out
@@ -1,0 +1,6 @@
+vlib/v/parser/tests/invalid_using_mut.vv:2:5: error: the `mut` keyword is invalid here
+    1 | fn main() {
+    2 |     if mut true {
+      |        ~~~
+    3 |         println(true)
+    4 |     }

vlib/v/parser/tests/invalid_using_shared.out
@@ -1,0 +1,6 @@
+vlib/v/parser/tests/invalid_using_shared.vv:2:5: error: the `shared` keyword is invalid here
+    1 | fn main() {
+    2 |     if shared true {
+      |        ~~~~~~
+    3 |         println(true)
+    4 |     }

vlib/v/parser/tests/invalid_using_shared.vv
@@ -1,0 +1,5 @@
+fn main() {
+	if shared true {
+		println(true)
+	}
+}

vlib/v/parser/tests/invalid_using_static.out
@@ -1,0 +1,6 @@
+vlib/v/parser/tests/invalid_using_static.vv:2:5: error: the `static` keyword is invalid here
+    1 | fn main() {
+    2 |     if static true {
+      |        ~~~~~~
+    3 |         println(true)
+    4 |     }

commit 0ca87ad09f5efd5a4841530ad861f0548f0b337d

parser: fix small performance regression after 1d83ab6b (by avoiding needless interpolation in hot paths)

vlib/v/parser/parser.v
@@ -1943,7 +1943,7 @@ pub fn (mut p Parser) parse_ident(language ast.Language) ast.Ident {
 		p.register_auto_import('sync')
 	}
 	mut_pos := p.tok.pos()
-	kind_name := '$p.tok.kind'
+	modifier_kind := p.tok.kind
 	is_mut := p.tok.kind == .key_mut || is_shared || is_atomic
 	if is_mut {
 		p.next()
@@ -1958,7 +1958,7 @@ pub fn (mut p Parser) parse_ident(language ast.Language) ast.Ident {
 	}
 	if p.tok.kind != .name {
 		if is_mut || is_static || is_volatile {
-			p.error_with_pos('the `$kind_name` keyword is invalid here', mut_pos)
+			p.error_with_pos('the `$modifier_kind` keyword is invalid here', mut_pos)
 		} else {
 			p.error('unexpected token `$p.tok.lit`')
 		}