5275c3a251a3f17503832f3659124d579409fd5c
Subhomoy Haldar <hungrybluedev@gmail.com>
2022-03-27 18:18:58 +0530

    fix shuffle usage

--- vlib/v/tests/generics_with_nested_external_generics_fn_test.v
+++ vlib/v/tests/generics_with_nested_external_generics_fn_test.v
@@ -1,20 +1,19 @@
-import rand.util
 import rand
 
-pub fn sample<T>(arr []T, k int) []T {
+pub fn sample<T>(arr []T, k int) ?[]T {
 	mut result := arr.clone()
 
 	rand.seed([u32(1), 2]) // set seed to produce same results in order
-	util.shuffle<T>(mut result, k)
+	rand.shuffle<T>(mut result) ?
 
 	return result[0..k]
 }
 
-fn test_generics_with_nested_external_generics_fn() {
+fn test_generics_with_nested_external_generics_fn() ? {
 	mut arr := [11, 32, 24, 45, 57, 32, 37, 52, 37, 24]
 	println(arr)
 
-	ret := sample<int>(arr, 5)
+	ret := sample<int>(arr, 5) ?
 	println(ret)
 
 	assert ret == [32, 45, 57, 11, 37]
