15ca57c3de8ddfe766559b0e7a8ab20ff7929c48
Delyan Angelov <delian66@gmail.com>
2022-04-01 17:24:44 +0300

    use `rng.shuffle(mut indices) ?` again

--- vlib/rand/rand.v
+++ vlib/rand/rand.v
@@ -328,7 +328,7 @@ pub fn (mut rng PRNG) choose<T>(array []T, k int) ?[]T {
 	mut results := []T{len: k}
 	mut indices := []int{len: n, init: it}
 	// enfoce the type to int to avoid ambiguity
-	rng.shuffle<int>(mut indices) ?
+	rng.shuffle(mut indices) ?
 	for i in 0 .. k {
 		results[i] = array[indices[i]]
 	}
