0b046c14a8784f47b7bc8aff9becf71bc3c71c54
Larpon <Larpon@users.noreply.github.com>
2022-04-05 12:06:32 +0200

    clipboard: add doc strings to all implementations (#13932)

--- vlib/clipboard/dummy/dummy_clipboard.v
+++ vlib/clipboard/dummy/dummy_clipboard.v
@@ -1,5 +1,8 @@
 module dummy
 
+// Clipboard represents a system clipboard.
+//
+// System "copy" and "paste" actions utilize the clipboard for temporary storage.
 pub struct Clipboard {
 mut:
 	text     string // text data sent or received
@@ -19,6 +22,8 @@ pub fn new_primary() &Clipboard {
 	return &Clipboard{}
 }
 
+// set_text transfers `text` to the system clipboard.
+// This is often associated with a *copy* action (`Ctrl` + `C`).
 pub fn (mut cb Clipboard) set_text(text string) bool {
 	cb.text = text
 	cb.is_owner = true
@@ -26,22 +31,31 @@ pub fn (mut cb Clipboard) set_text(text string) bool {
 	return true
 }
 
+// get_text retrieves the contents of the system clipboard
+// as a `string`.
+// This is often associated with a *paste* action (`Ctrl` + `V`).
 pub fn (mut cb Clipboard) get_text() string {
 	return cb.text
 }
 
+// clear empties the clipboard contents.
 pub fn (mut cb Clipboard) clear() {
 	cb.text = ''
 	cb.is_owner = false
 }
 
+// free releases all memory associated with the clipboard
+// instance.
 pub fn (mut cb Clipboard) free() {
 }
 
+// has_ownership returns true if the contents of
+// the clipboard were created by this clipboard instance.
 pub fn (cb &Clipboard) has_ownership() bool {
 	return cb.is_owner
 }
 
+// check_availability returns true if the clipboard is ready to be used.
 pub fn (cb &Clipboard) check_availability() bool {
 	// This is a dummy clipboard implementation,
 	// which can be always used, although it does not do much...
