2a202754125b5b64f25d23ef416a87cbc051f52d
yuyi98 <yuyi98@163.com>
2022-04-06 14:35:00 +0800

    cgen: fix json decode with optional argument

---
+++ vlib/json/json_decode_with_optional_arg_test.v
@@ -0,0 +1,19 @@
+import json
+import os
+
+struct DbConfig {}
+
+fn test_json_decode_with_optional_arg() {
+	if ret := print_info() {
+		println(ret)
+	} else {
+		println(err)
+	}
+	assert true
+}
+
+fn print_info() ?string {
+	dbconf := json.decode(DbConfig, os.read_file('dbconf.json') ?) ?
+	println(dbconf)
+	return '$dbconf'
+}
