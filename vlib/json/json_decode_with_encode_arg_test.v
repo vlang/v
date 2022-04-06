d1de9f2c7b5ebbb1ed4698f3415f4993c098551f
yuyi98 <yuyi98@163.com>
2022-04-06 14:56:44 +0800

    add json_decode_with_encode_arg_test.v

---
+++ vlib/json/json_decode_with_encode_arg_test.v
@@ -0,0 +1,12 @@
+import json
+
+struct TodoDto {
+	foo int
+}
+
+fn test_decode_with_encode_arg() ? {
+	body := TodoDto{}
+	ret := json.decode(TodoDto, json.encode(body)) ?
+	println(ret)
+	assert ret.foo == 0
+}
