f5dc192e5bea64dd655f019c6f1fd2cf2c310188
yuyi98 <yuyi98@163.com>
2022-04-28 05:27:05 +0800

    change go_anon_fn_call_with_ref_arg_test.v

--- vlib/v/tests/go_anon_fn_call_with_ref_arg_test.v
+++ vlib/v/tests/go_anon_fn_call_with_ref_arg_test.v
@@ -1,57 +1,15 @@
-import net.html
-import net.http
-import vweb
-
-struct App {
-	vweb.Context
-}
-
-struct HtmlData {
-mut:
-	inner_html string
-	data       []&HtmlData
-}
-
-fn convert_to_html_data(tag html.Tag) &HtmlData {
-	println('convert_to_html_data')
-	if tag.children.len == 0 {
-		return &HtmlData{
-			inner_html: tag.content
-			data: []
-		}
-	}
-
-	mut node := &HtmlData{
-		inner_html: tag.content
-		data: []&HtmlData{}
-	}
-
-	for child in tag.children {
-		child_data := convert_to_html_data(child)
-		node.data << child_data
-	}
-	return node
+struct Foo {
+	bar string
 }
 
 fn test_go_anon_fn_call_with_ref_arg() {
-	app := App{}
-	// vweb.run(app, 8081)
-	print('Hello, world!')
-	assert true
-}
-
-['/index']
-pub fn (mut app App) index() vweb.Result {
-	res := http.get('https://www.nzx.com/markets/NZSX') or {
-		http.new_response(status: .internal_server_error)
+	foo := &Foo{
+		bar: 'hello'
 	}
-	mut data := html.parse(res.text)
-	table := data.get_tag_by_attribute_value('class', 'small-12 medium-8 columns content')[0]
-
-	go fn (table html.Tag) {
-		final_table := convert_to_html_data(table)
-		println(final_table)
-	}(table)
-
-	return app.html(table.children.str())
+	g := go fn (foo Foo) string {
+		return foo.bar
+	}(foo)
+	ret := g.wait()
+	println(ret)
+	assert ret == 'hello'
 }
