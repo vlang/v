c802688690c0cf98e2182b16dbc7a7dd683281a7
yuyi <yuyi98@163.com>
2022-04-28 18:20:56 +0800

    ast: fix error for complex map operating (#14204)

---
+++ vlib/v/tests/complex_map_op_test.v
@@ -0,0 +1,24 @@
+fn test_complex_map_op() {
+	mut test_map := map[string]Point_map{}
+
+	test_map['test1'].points['point3'] << Point{10, 20}
+	test_map['test2'].points['point4'] << Point{50, 60}
+
+	println(test_map)
+	assert test_map.len == 2
+	assert Point{10, 20} in test_map['test1'].points['point3']
+	assert Point{50, 60} in test_map['test2'].points['point4']
+	assert Point{10, 20} !in test_map['test2'].points['point4']
+	assert Point{1, 2} !in test_map['test1'].points['point3']
+}
+
+struct Point {
+mut:
+	x int
+	y int
+}
+
+struct Point_map {
+mut:
+	points map[string][]Point
+}
