db185e6580a15670acd95bb4418cd04a8a637a32
StunxFS <56417208+StunxFS@users.noreply.github.com>
2022-04-29 18:25:29 -0400

    checker: check for reserved type names in `for in` (fix #14072) (#14212)

--- vlib/v/checker/checker.v
+++ vlib/v/checker/checker.v
@@ -38,8 +38,9 @@ pub const (
 	valid_comptime_not_user_defined  = all_valid_comptime_idents()
 	array_builtin_methods            = ['filter', 'clone', 'repeat', 'reverse', 'map', 'slice',
 		'sort', 'contains', 'index', 'wait', 'any', 'all', 'first', 'last', 'pop']
-	reserved_type_names              = ['bool', 'char', 'i8', 'i16', 'int', 'i64', 'byte', 'u16',
-		'u32', 'u64', 'f32', 'f64', 'map', 'string', 'rune']
+	// TODO: remove `byte` from this list when it is no longer supported
+	reserved_type_names              = ['byte', 'bool', 'char', 'i8', 'i16', 'int', 'i64', 'u8',
+		'u16', 'u32', 'u64', 'f32', 'f64', 'map', 'string', 'rune']
 	vroot_is_deprecated_message      = '@VROOT is deprecated, use @VMODROOT or @VEXEROOT instead'
 )
 
