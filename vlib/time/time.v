c071ce3520398ee1f7108b70e0b09438f142be8b
David 'Epper' Marshall <epper.marshall@gmail.com>
2022-04-30 19:07:53 -0400

    update now

--- vlib/time/time.v
+++ vlib/time/time.v
@@ -142,7 +142,7 @@ pub fn (t Time) relative() string {
 	} else {
 		suffix = ' ago'
 	}
-	if secs <= time.seconds_per_minute {
+	if secs < time.seconds_per_minute / 2 {
 		return 'now'
 	}
 	if secs < time.seconds_per_hour {
@@ -203,7 +203,7 @@ pub fn (t Time) relative_short() string {
 	} else {
 		suffix = ' ago'
 	}
-	if secs <= time.seconds_per_minute {
+	if secs < time.seconds_per_minute / 2 {
 		return 'now'
 	}
 	if secs < time.seconds_per_hour {
