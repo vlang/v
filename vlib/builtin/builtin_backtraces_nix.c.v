a558566adbc734aafd824fb83c5f8bd912a860f1
Delyan Angelov <delian66@gmail.com>
2022-05-02 12:34:39 +0300

    split builtin_backtraces_d_musl.c.v from builtin_backtraces_nix.c.v

--- vlib/builtin/builtin_backtraces_nix.c.v
+++ vlib/builtin/builtin_backtraces_nix.c.v
@@ -4,22 +4,3 @@ module builtin
 fn C.backtrace(a &voidptr, size int) int
 fn C.backtrace_symbols(a &voidptr, size int) &&char
 fn C.backtrace_symbols_fd(a &voidptr, size int, fd int)
-
-// These are just dummy implementations to appease gcc on musl/alpine							
-
-[export: 'backtrace_symbols']
-[weak]
-fn vbacktrace_symbols(const_buffer &voidptr, size int) &&char {
-	return 0
-}
-
-[export: 'backtrace']
-[weak]
-fn vbacktrace(buffer &voidptr, size int) int {
-	return 0
-}
-
-[export: 'backtrace_symbols_fd']
-[weak]
-fn vbacktrace_symbols_fd(const_buffer &voidptr, size int, fd int) {
-}
