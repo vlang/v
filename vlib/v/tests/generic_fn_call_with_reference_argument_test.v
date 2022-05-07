b04d46770b23b8499ac3cd1d30e1c4db5986b4ef
yuyi <yuyi98@163.com>
2022-05-08 01:22:20 +0800

    ast, checker, cgen: fix error for generic method with generic fn type argument (fix #14239) (#14333)

---
+++ vlib/v/tests/generic_fn_call_with_reference_argument_test.v
@@ -0,0 +1,25 @@
+struct Type<T> {
+	value T
+}
+
+struct ContainerType<T> {
+	typ &Type<T>
+}
+
+fn (instance &ContainerType<T>) contains(typ Type<T>) bool {
+	println(typ)
+	if instance.typ == typ {
+		return true
+	} else {
+		return false
+	}
+}
+
+fn test_generic_fn_call_with_reference_argument() {
+	con := ContainerType<int>{
+		typ: &Type<int>{0}
+	}
+	ret := con.contains(con.typ)
+	println(con)
+	assert ret
+}
