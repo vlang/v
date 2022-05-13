a4eef1419b23c7b03b8275e4733bb2a508cfe0ed
Ben Larisch <benlarisch2019@gmail.com>
2022-05-13 23:13:38 +0200

    os: add is_abs_path test (to filepath_test.v)

---
+++ vlib/os/filepath_test.v
@@ -0,0 +1,29 @@
+module os
+
+fn test_is_abs_path() {
+	if is_win {
+		assert is_abs_path('/')
+		assert is_abs_path('\\')
+		assert !is_abs_path('\\\\')
+		assert is_abs_path(r'C:\path\to\files\file.v')
+		assert is_abs_path(r'\\Host\share')
+		assert is_abs_path(r'//Host\share\files\file.v')
+		assert is_abs_path(r'\\.\BootPartition\Windows')
+		assert !is_abs_path(r'\\.\')
+		assert !is_abs_path(r'\\?\\')
+		assert !is_abs_path(r'C:path\to\dir')
+		assert !is_abs_path(r'dir')
+		assert !is_abs_path(r'.\')
+		assert !is_abs_path(r'.')
+		assert !is_abs_path(r'\\Host')
+		assert !is_abs_path(r'\\Host\')
+		return
+	}
+	assert is_abs_path('/')
+	assert is_abs_path('/path/to/files/file.v')
+	assert !is_abs_path('\\')
+	assert !is_abs_path('path/to/files/file.v')
+	assert !is_abs_path('dir')
+	assert !is_abs_path('./')
+	assert !is_abs_path('.')
+}
