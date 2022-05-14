9ec109d838e54acb51b329753f26b7e1dc56dee2
depperm <epper.marshall@gmail.com>
2022-05-14 08:20:28 -0400

    comment added

--- vlib/math/cbrt.v
+++ vlib/math/cbrt.v
@@ -1,5 +1,17 @@
 module math
 
+// The vlang code is a modified version of the original C code from
+// http://www.netlib.org/fdlibm/s_cbrt.c and came with this notice.
+//
+// ====================================================
+// Copyright (C) 1993 by Sun Microsystems, Inc. All rights reserved.
+//
+// Developed at SunSoft, a Sun Microsystems, Inc. business.
+// Permission to use, copy, modify, and distribute this
+// software is freely granted, provided that this notice
+// is preserved.
+// ====================================================
+
 // cbrt returns the cube root of a.
 //
 // special cases are:
