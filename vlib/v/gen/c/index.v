d75f24a036912a7047b4aa64faa6d9013d5b4668
yuyi98 <yuyi98@163.com>
2022-05-16 17:35:25 +0800

    cgen: fix error for struct field array index

--- vlib/v/gen/c/index.v
+++ vlib/v/gen/c/index.v
@@ -172,8 +172,7 @@ fn (mut g Gen) index_of_array(node ast.IndexExpr, sym ast.TypeSymbol) {
 	}
 	// `vals[i].field = x` is an exception and requires `array_get`:
 	// `(*(Val*)array_get(vals, i)).field = x;`
-	is_selector := node.left is ast.SelectorExpr
-	if g.is_assign_lhs && !is_selector && node.is_setter {
+	if g.is_assign_lhs && node.is_setter {
 		is_direct_array_access := (g.fn_decl != 0 && g.fn_decl.is_direct_arr) || node.is_direct
 		is_op_assign := g.assign_op != .assign && info.elem_type != ast.string_type
 		if is_direct_array_access {
