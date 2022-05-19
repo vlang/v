23568f19da447ebfa1eaf9f4f1a16125cd108270
David 'Epper' Marshall <epper.marshall@gmail.com>
2022-05-19 19:58:58 -0400

    url: fix parse (#14456)

--- vlib/net/urllib/urllib_test.v
+++ vlib/net/urllib/urllib_test.v
@@ -98,3 +98,24 @@ fn test_parse_empty_query_two() ? {
 	assert qvalues_map == {}
 	assert query_str == query_encode
 }
+
+fn test_parse() ? {
+	urls := [
+		'jdbc:mysql://test_user:ouupppssss@localhost:3306/sakila?profileSQL=true',
+		'ftp://ftp.is.co.za/rfc/rfc1808.txt',
+		'http://www.ietf.org/rfc/rfc2396.txt#header1',
+		'ldap://[2001:db8::7]/c=GB?objectClass=one&objectClass=two',
+		'mailto:John.Doe@example.com',
+		'news:comp.infosystems.www.servers.unix',
+		'tel:+1-816-555-1212',
+		'telnet://192.0.2.16:80/',
+		'urn:oasis:names:specification:docbook:dtd:xml:4.1.2',
+		'foo://example.com:8042/over/there?name=ferret#nose',
+	]
+	for url in urls {
+		_ := urllib.parse(url) or {
+			assert false
+			panic('FAILED parsing url')
+		}
+	}
+}
