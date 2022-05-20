43dd67df36f102deea9567740d5d44c8afb2441a
Subhomoy Haldar <hungrybluedev@gmail.com>
2022-05-21 01:14:38 +0530

    add LICENSE header

--- vlib/rand/config/config.v
+++ vlib/rand/config/config.v
@@ -1,3 +1,6 @@
+// Copyright (c) 2019-2022 Alexander Medvednikov. All rights reserved.
+// Use of this source code is governed by an MIT license
+// that can be found in the LICENSE file.
 module config
 
 import rand.seed
