14673aa32f1ffa0efbee79ed2ccd385d5502042b
Jah-On <58399643+Jah-On@users.noreply.github.com>
2022-05-21 13:33:27 +0000

    Forgot to fmt

--- vlib/clipboard/x11/clipboard.c.v
+++ vlib/clipboard/x11/clipboard.c.v
@@ -4,13 +4,12 @@ module x11
 
 import os
 
-
 [heap]
 pub struct Clipboard {
 mut:
-	text      string // text data sent or received
-	got_text  bool   // used to confirm that we have got the text
-	is_owner  bool   // to save selection owner state
+	text     string // text data sent or received
+	got_text bool   // used to confirm that we have got the text
+	is_owner bool   // to save selection owner state
 }
 
 // new_clipboard returns a new `Clipboard` instance allocated on the heap.
@@ -20,7 +19,7 @@ pub fn new_clipboard() &Clipboard {
 }
 
 pub fn (cb &Clipboard) check_availability() bool {
-	return os.execute("type xsel").exit_code == 0
+	return os.execute('type xsel').exit_code == 0
 }
 
 pub fn (mut cb Clipboard) free() {
@@ -28,7 +27,7 @@ pub fn (mut cb Clipboard) free() {
 
 // clear empties the current selection in the system clipboard.
 pub fn (mut cb Clipboard) clear() {
-	os.system("xsel -cb")
+	os.system('xsel -cb')
 	cb.is_owner = false
 	cb.text = ''
 }
@@ -42,14 +41,14 @@ fn (cb &Clipboard) take_ownership() {
 
 // set_text stores `text` in the system clipboard.
 pub fn (mut cb Clipboard) set_text(text string) bool {
-	output := os.execute("printf " + text + " | xsel -b")
+	output := os.execute('printf ' + text + ' | xsel -b')
 	cb.is_owner = output.exit_code == 0
 	return cb.is_owner
 }
 
 // get_text retrieves the current selection in the system clipboard.
 pub fn (mut cb Clipboard) get_text() string {
-	output := os.execute("xsel -b")
+	output := os.execute('xsel -b')
 	cb.text = output.output
 	return cb.text
 }
