c96075eacb471d69fa3114fb4bd40dce1935a242
lmp <dev.larpon@gmail.com>
2022-05-25 15:33:14 +0200

    embed_file: remove extra note about if it was forced

--- vlib/v/embed_file/embed_file.v
+++ vlib/v/embed_file/embed_file.v
@@ -112,7 +112,7 @@ pub fn find_index_entry_by_path(start voidptr, path string, algo string) &EmbedF
 		}
 	}
 	$if trace_embed_file ? {
-		eprintln('>> v.embed_file (forced embedding) find_index_entry_by_path ${ptr_str(start)}, id: $x.id, path: "$path", algo: "$algo" => ${ptr_str(x)}')
+		eprintln('>> v.embed_file find_index_entry_by_path ${ptr_str(start)}, id: $x.id, path: "$path", algo: "$algo" => ${ptr_str(x)}')
 	}
 	return x
 }
