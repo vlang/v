9323f08f4938cde27cb2b8d1ec6061317e4c41ad
Hunam <hunam@disroot.org>
2022-05-26 22:04:41 +0200

    mime: rm opt types
    Signed-off-by: Hunam <hunam@disroot.org>

--- vlib/net/http/mime/mime.v
+++ vlib/net/http/mime/mime.v
@@ -8,25 +8,29 @@ pub struct MimeType {
 }
 
 // returns a `MimeType` for the given MIME type
-pub fn get_complete_mime_type(mt string) ?MimeType {
+pub fn get_complete_mime_type(mt string) MimeType {
 	return db[mt]
 }
 
 // returns the MIME type for the given file extension
-pub fn get_mime_type(ext string) ?string {
+pub fn get_mime_type(ext string) string {
 	return ext_to_mt_str[ext]
 }
 
 // returns a `content-type` header ready to use for the given MIME type
-pub fn get_content_type(mt string) ?string {
+pub fn get_content_type(mt string) string {
 	mt_struct := db[mt]
 	charset := if mt_struct.charset.len > 0 { mt_struct.charset.to_lower() } else { 'utf-8' }
 	return '$mt; charset=$charset'
 }
 
 // returns the default extension for the given MIME type
-pub fn get_default_ext(mt string) ?string {
-	return db[mt].extensions[0]
+pub fn get_default_ext(mt string) string {
+	return if db[mt].extensions.len > 0 {
+		db[mt].extensions[0]
+	} else {
+		''
+	}
 }
 
 // returns true if the given MIME type exists
