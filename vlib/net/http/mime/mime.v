f49d424a76875e1aa06ac66b251f85f8c2ee8044
Hunam <hunam@disroot.org>
2022-05-26 15:41:03 +0200

    faster
    Signed-off-by: Hunam <hunam@disroot.org>

--- vlib/net/http/mime/mime.v
+++ vlib/net/http/mime/mime.v
@@ -7,51 +7,29 @@ pub struct MimeType {
 	charset      string
 }
 
-// returns a `MimeType` and the the MIME type for the given file extension or MIME type
-fn mime_type(str string) ?(MimeType, string) {
-	// if `str` is a mime type
-	if str in db {
-		return db[str], str
-	}
-	// if `str` is an extension
-	for mt_str, mt in db {
-		if str in mt.extensions {
-			return mt, mt_str
-		}
-	}
-	return none, none
-}
-
-// returns a `MimeType` for the given file extension or MIME type
-pub fn get_complete_mime_type(str string) ?MimeType {
-	mt, _ := mime_type(str)?
-	return mt
+// returns a `MimeType` for the given MIME type
+pub fn get_complete_mime_type(mt string) ?MimeType {
+	return db[mt]
 }
 
 // returns the MIME type for the given file extension
 pub fn get_mime_type(ext string) ?string {
-	_, mt := mime_type(ext)?
-	return mt
+	return ext_to_mt_str[ext]
 }
 
-// returns a `content-type` header ready to use for the given file extension or MIME type
-pub fn get_content_type(str string) ?string {
-	mt, mt_str := mime_type(str)?
-	charset := if mt.charset.len > 0 { mt.charset.to_lower() } else { 'utf-8' }
-	return '$mt_str; charset=$charset'
+// returns a `content-type` header ready to use for the given MIME type
+pub fn get_content_type(mt string) ?string {
+	mt_struct := db[mt]
+	charset := if mt_struct.charset.len > 0 { mt_struct.charset.to_lower() } else { 'utf-8' }
+	return '$mt; charset=$charset'
 }
 
-// returns the default extension for the given file extension or MIME type
-pub fn get_default_ext(str string) ?string {
-	mt, _ := mime_type(str)?
-	return mt.extensions[0]
+// returns the default extension for the given MIME type
+pub fn get_default_ext(mt string) ?string {
+	return db[mt].extensions[0]
 }
 
-// returns true if the given file extension or MIME type exists
-pub fn exists(str string) bool {
-	return if _ := mime_type(str) {
-		true
-	} else {
-		false
-	}
+// returns true if the given MIME type exists
+pub fn exists(mt string) bool {
+	return mt in db
 }
--- vlib/net/http/mime/mime_test.v
+++ vlib/net/http/mime/mime_test.v
@@ -1,17 +1,14 @@
 module mime
 
 fn test_mime() ? {
-	assert get_mime_type('png')? == 'image/png'
-
-	assert get_complete_mime_type('json')? == MimeType{
+	assert get_complete_mime_type('application/json')? == MimeType{
 		source: 'iana'
 		extensions: ['json', 'map']
 		compressible: true
 		charset: 'UTF-8'
 	}
-	assert get_mime_type('application/json')? == 'application/json'
 	assert get_mime_type('json')? == 'application/json'
-	assert get_content_type('json')? == 'application/json; charset=utf-8'
+	assert get_content_type('application/json')? == 'application/json; charset=utf-8'
 	assert get_default_ext('application/json')? == 'json'
 
 	assert get_complete_mime_type('text/markdown')? == MimeType{
@@ -20,12 +17,10 @@ fn test_mime() ? {
 		compressible: true
 		charset: ''
 	}
-	assert get_mime_type('text/markdown')? == 'text/markdown'
 	assert get_mime_type('md')? == 'text/markdown'
-	assert get_content_type('md')? == 'text/markdown; charset=utf-8'
+	assert get_content_type('text/markdown')? == 'text/markdown; charset=utf-8'
 	assert get_default_ext('text/markdown')? == 'md'
 
 	assert exists('application/json') == true
-	assert exists('md') == true
 	assert exists('udfsbsfib') == false
 }
