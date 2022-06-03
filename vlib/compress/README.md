65066098d80586eff4718cfbd3bb4db1063bbf56
ChAoS_UnItY <minecraft.kyle.train@gmail.com>
2022-06-03 14:00:11 +0800

    compress: Add gzip module & refactor compress & compress.zlib module (#14599)

--- vlib/compress/README.md
+++ vlib/compress/README.md
@@ -1,4 +1,4 @@
 ## Description:
 
 `compress` is a namespace for (multiple) compression algorithms supported by V.
-At the moment, only `compress.zlib` is implemented.
+At the moment, only `compress.zlib` and `compress.gzip` are implemented.
---
+++ vlib/compress/gzip/gzip.v
@@ -0,0 +1,16 @@
+module gzip
+
+import compress
+
+// compresses an array of bytes using gzip and returns the compressed bytes in a new array
+// Example: compressed := gzip.compress(b)?
+pub fn compress(data []u8) ?[]u8 {
+	return compress.compress(data, 0)
+}
+
+// decompresses an array of bytes using zlib and returns the decompressed bytes in a new array
+// Example: decompressed := zlib.decompress(b)?
+[manualfree]
+pub fn decompress(data []u8) ?[]u8 {
+	return compress.decompress(data, 0)
+}
