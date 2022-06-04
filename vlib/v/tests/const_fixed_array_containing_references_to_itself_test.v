f2171b4148d45054d6752be491b86829307cb47a
Delyan Angelov <delian66@gmail.com>
2022-06-04 09:15:37 +0300

    ci: fix macos clang failures with const_fixed_array_containing_references_to_itself_test.v

--- vlib/v/tests/const_fixed_array_containing_references_to_itself_test.v
+++ vlib/v/tests/const_fixed_array_containing_references_to_itself_test.v
@@ -5,13 +5,13 @@ struct Abc {
 const a = [Abc{voidptr(0)}, Abc{unsafe { &a[0] }}, Abc{unsafe { &a[1] }}]!
 
 fn test_fixed_array() {
-	dump(a)
-	dump(voidptr(&a[0]))
-	dump(voidptr(&a[1]))
-	dump(voidptr(&a[2]))
-	dump(voidptr(a[0].prev))
-	dump(voidptr(a[1].prev))
-	dump(voidptr(a[2].prev))
+	eprintln(a)
+	eprintln(voidptr(&a[0]))
+	eprintln(voidptr(&a[1]))
+	eprintln(voidptr(&a[2]))
+	eprintln(voidptr(a[0].prev))
+	eprintln(voidptr(a[1].prev))
+	eprintln(voidptr(a[2].prev))
 	assert voidptr(&a[0]) == voidptr(a[1].prev)
 	assert voidptr(&a[1]) == voidptr(a[2].prev)
 }
