b000728845535336cc1fc52d6d3d13c8add3ed20
ChAoS_UnItY <minecraft.kyle.train@gmail.com>
2022-06-05 13:48:38 +0800

    compress.gzip / deflate: rename gzip module into deflate module (#14682)

--- vlib/compress/deflate/deflate.v
+++ vlib/compress/deflate/deflate.v
@@ -1,4 +1,4 @@
-module gzip
+module deflate
 
 import compress
 
--- vlib/compress/deflate/deflate_test.v
+++ vlib/compress/deflate/deflate_test.v
@@ -1,8 +1,12 @@
-module gzip
+module deflate
+
+const gzip_magic_numbers = [u8(0x1f), 0x8b]
 
 fn test_gzip() ? {
 	uncompressed := 'Hello world!'
 	compressed := compress(uncompressed.bytes())?
+	first2 := compressed[0..2]
+	assert first2 != deflate.gzip_magic_numbers
 	decompressed := decompress(compressed)?
 	assert decompressed == uncompressed.bytes()
 }
