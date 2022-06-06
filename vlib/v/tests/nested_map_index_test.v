ce771876a38a94064e834f5b2a2499504ec63257
yuyi <yuyi98@163.com>
2022-06-06 11:29:22 +0800

    cgen: fix nested map index check (fix #14683) (#14687)

---
+++ vlib/v/tests/nested_map_index_test.v
@@ -0,0 +1,24 @@
+struct Foo {
+mut:
+	foo map[int]Bar
+}
+
+struct Bar {
+mut:
+	bar map[int]string
+}
+
+fn test_nested_map_index() ? {
+	f := Foo{
+		foo: {
+			11: Bar{
+				bar: {
+					22: 'hello'
+				}
+			}
+		}
+	}
+	ret := f.foo[11]?.bar[22]?
+	println(ret)
+	assert ret == 'hello'
+}
