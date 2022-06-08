40531c63c9fff3f042449f3a1c3f1553d5b3c9d9
Ben Larisch <benlarisch2019@gmail.com>
2022-06-08 09:37:05 +0200

    os: mv windows_volume function into filepath_win.v

--- vlib/os/filepath.v
+++ vlib/os/filepath.v
@@ -175,26 +175,6 @@ pub fn existing_path(path string) ?string {
 	return err
 }
 
-// windows_volume returns the volume name from the given `path` on a Windows system.
-// An empty string is returned if no Windows volume is present.
-// NOTE: An error is returned if the current operating system is not Windows.
-// Examples (on a Windows system):
-// ```v
-// assert os.windows_volume('C:\path\to\file.v') == 'C:'
-// assert os.windows_volume('D:') == 'D:'
-// assert os.windows_volume('\\Host\share\files\file.v') == '\\Host\share'
-// ```
-pub fn windows_volume(path string) ?string {
-	$if !windows {
-		return error(@FN + '() is only supported on a Windows system')
-	}
-	volume_len := win_volume_len(path)
-	if volume_len == 0 {
-		return os.empty_str
-	}
-	return path[..volume_len]
-}
-
 // clean_path returns the "cleaned" version of the given `path`
 // by turning forward slashes into back slashes
 // on a Windows system and eliminating:
