4becd316c1d6d31817ee13a9a37d080c62c042f1
lemoncmd <lemoncmd@gmail.com>
2022-06-14 16:36:01 +0900

    transformer: fix raw

--- vlib/v/transformer/transformer.v
+++ vlib/v/transformer/transformer.v
@@ -637,7 +637,7 @@ pub fn (mut t Transformer) expr(mut node ast.Expr) ast.Expr {
 		ast.SelectorExpr {
 			node.expr = t.expr(mut node.expr)
 			if mut node.expr is ast.StringLiteral && node.field_name == 'len' {
-				if !node.expr.val.contains('\\') {
+				if !node.expr.val.contains('\\') || node.expr.is_raw {
 					return ast.IntegerLiteral{
 						val: node.expr.val.len.str()
 						pos: node.pos
