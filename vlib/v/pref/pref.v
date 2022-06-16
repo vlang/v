b89617726c59c24e09d0a4665b6eee066f5d184a
Alexander Medvednikov <alexander@vlang.io>
2022-06-16 20:51:17 +0300

    pref: is_o

--- vlib/v/pref/pref.v
+++ vlib/v/pref/pref.v
@@ -654,6 +654,9 @@ pub fn parse_args_and_show_errors(known_external_commands []string, args []strin
 				}
 				i++
 			}
+			'-is_o' {
+				res.is_o = true
+			}
 			'-b', '-backend' {
 				sbackend := cmdline.option(current_args, arg, 'c')
 				res.build_options << '$arg $sbackend'
