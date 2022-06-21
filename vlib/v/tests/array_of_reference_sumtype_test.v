cab6355a3864beccf4c39191cdd4f1b183eb8369
yuyi <yuyi98@163.com>
2022-06-21 17:37:54 +0800

    cgen: fix array of reference sumtype index() (#14812)

--- vlib/v/tests/array_of_reference_sumtype_test.v
+++ vlib/v/tests/array_of_reference_sumtype_test.v
@@ -1,11 +1,14 @@
 struct Element {
 	AbstractNode
+mut:
 	name       string
+	value      string
 	attributes []&Attribute
 }
 
 struct Attribute {
 	AbstractNode
+mut:
 	name  string
 	value string
 }
@@ -21,7 +24,11 @@ pub fn (mut n AbstractNode) append_child(child &Node) {
 	n.child_nodes << child
 }
 
-fn test_array_of_reference_sumtype_append() {
+pub fn (n &AbstractNode) has_child(child &Node) int {
+	return n.child_nodes.index(child)
+}
+
+fn test_array_of_reference_sumtype() {
 	mut parent := &Element{
 		name: 'parent'
 	}
@@ -30,6 +37,9 @@ fn test_array_of_reference_sumtype_append() {
 	}
 	parent.append_child(child)
 
+	ret := parent.has_child(child)
+	println(ret)
 	dump(parent)
 	assert parent.child_nodes[0].name == 'child'
+	assert ret == 0
 }
