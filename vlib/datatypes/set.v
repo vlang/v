e1e07476b8b2bf74309a3cc4110733b78370a569
Vladimir Myshkovski <vladimirmyshkovski@gmail.com>
2022-06-26 20:03:19 +0400

    Update errors

--- vlib/datatypes/set.v
+++ vlib/datatypes/set.v
@@ -22,7 +22,7 @@ fn (mut set Set<T>) add(element T) {
 // removes the element from set, if it is present.
 fn (mut set Set<T>) remove(element T) {
 	if element !in set.elements {
-		error("remove error: Item doesn't exist in set")
+		error("Item doesn't exist in set")
 	}
 	elements := set.elements.filter(it != element)
 	set.elements = elements
@@ -30,27 +30,27 @@ fn (mut set Set<T>) remove(element T) {
 
 // returns an arbitrary element of set
 fn (mut set Set<T>) pick() ?T {
-	if set.elements.len == 0 {
-		error("pick error: It's not possible to get element from an empty set")
+	if set.is_empty() {
+		error('Set is empty')
 	}
 	return set.elements[rand.intn(set.size())?]
 }
 
 // returns the set consisting of all elements except for the arbitrary element.
 fn (mut set Set<T>) rest() ?[]T {
-	if set.elements.len == 0 {
-		error("rest error: It's not possible to remove element from an empty set")
+	if set.is_empty() {
+		error('Set is empty')
 	}
-	element := set.pick() or { panic('vlib/datatypes/set/rest') }
+	element := set.pick() or { return error('Failed to retrieve an arbitrary element') }
 	return set.elements.filter(it != element)
 }
 
 // returns an arbitrary element and deleting it from set.
 fn (mut set Set<T>) pop() ?T {
-	if set.elements.len == 0 {
-		error("pop error: It's not possible to remove element from an empty set")
+	if set.is_empty() {
+		error('Stack is empty')
 	}
-	element := set.pick() or { panic('vlib/datatypes/set/pop') }
+	element := set.pick() or { return error('Failed to retrieve an arbitrary element') }
 	elements := set.elements.filter(it != element)
 	set.elements = elements
 	return element
