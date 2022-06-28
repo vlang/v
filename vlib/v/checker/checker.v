5c0f4ace0d4ded782f6eaa58a64a6d891654ba94
yuyi98 <yuyi98@163.com>
2022-06-28 11:56:30 +0800

    checker: optimize casting sumtype error message

--- vlib/v/checker/checker.v
+++ vlib/v/checker/checker.v
@@ -2508,7 +2508,7 @@ pub fn (mut c Checker) cast_expr(mut node ast.CastExpr) ast.Type {
 		&& final_to_sym.kind !in [.sum_type, .interface_] {
 		ft := c.table.type_to_str(from_type)
 		tt := c.table.type_to_str(to_type)
-		c.error('cannot cast `$ft` to `$tt`, please use `as` instead, e.g. `expr as Ident`',
+		c.error('cannot cast `$ft` $from_sym.kind value to `$tt`, use `$node.expr as $tt` instead',
 			node.pos)
 	}
 
