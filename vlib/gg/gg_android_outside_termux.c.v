f1a855e6d224f0c13ba633ebb75e3b4e1f7d83e5
Delyan Angelov <delian66@gmail.com>
2022-07-06 16:02:50 +0300

    gg: fix android compilation for ~/.vmodules/ui/examples/rectangles.v

--- vlib/gg/gg_android_outside_termux.c.v
+++ vlib/gg/gg_android_outside_termux.c.v
@@ -1,6 +1,5 @@
 module gg
 
-import os
 import sokol.sapp
 
 #include <android/configuration.h>
@@ -13,7 +12,7 @@ fn C.AConfiguration_delete(voidptr)
 // android_dpi_scale returns the scale factor of the device.
 pub fn android_dpi_scale() f32 {
 	config := C.AConfiguration_new()
-	activity := &os.NativeActivity(sapp.android_get_native_activity())
+	activity := &C.ANativeActivity(sapp.android_get_native_activity())
 	C.AConfiguration_fromAssetManager(config, activity.assetManager)
 	density := C.AConfiguration_getDensity(config)
 	C.AConfiguration_delete(config)
