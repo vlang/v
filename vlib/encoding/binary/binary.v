dabaf0cb937d6a73f6f5818dcf536578c323a040
Subhomoy Haldar <hungrybluedev@gmail.com>
2022-07-09 16:21:24 +0530

    add documentation and unit tests

--- vlib/encoding/binary/binary.v
+++ vlib/encoding/binary/binary.v
@@ -3,13 +3,14 @@
 // that can be found in the LICENSE file.
 module binary
 
-// Little Endian
+// little_endian_u16 creates a u16 from two bytes in the array b in little endian order.
 [direct_array_access; inline]
 pub fn little_endian_u16(b []u8) u16 {
 	_ = b[1] // bounds check
 	return u16(b[0]) | (u16(b[1]) << u16(8))
 }
 
+// little_endian_put_u16 writes a u16 to the first two bytes in the array b in little endian order.
 [direct_array_access; inline]
 pub fn little_endian_put_u16(mut b []u8, v u16) {
 	_ = b[1] // bounds check
@@ -17,12 +18,14 @@ pub fn little_endian_put_u16(mut b []u8, v u16) {
 	b[1] = u8(v >> u16(8))
 }
 
+// little_endian_u32 creates a u32 from four bytes in the array b in little endian order.
 [direct_array_access; inline]
 pub fn little_endian_u32(b []u8) u32 {
 	_ = b[3] // bounds check
 	return u32(b[0]) | (u32(b[1]) << u32(8)) | (u32(b[2]) << u32(16)) | (u32(b[3]) << u32(24))
 }
 
+// little_endian_put_u32 writes a u32 to the first four bytes in the array b in little endian order.
 [direct_array_access; inline]
 pub fn little_endian_put_u32(mut b []u8, v u32) {
 	_ = b[3] // bounds check
@@ -32,12 +35,14 @@ pub fn little_endian_put_u32(mut b []u8, v u32) {
 	b[3] = u8(v >> u32(24))
 }
 
+// little_endian_u64 creates a u64 from eight bytes in the array b in little endian order.
 [direct_array_access; inline]
 pub fn little_endian_u64(b []u8) u64 {
 	_ = b[7] // bounds check
 	return u64(b[0]) | (u64(b[1]) << u64(8)) | (u64(b[2]) << u64(16)) | (u64(b[3]) << u64(24)) | (u64(b[4]) << u64(32)) | (u64(b[5]) << u64(40)) | (u64(b[6]) << u64(48)) | (u64(b[7]) << u64(56))
 }
 
+// little_endian_put_u64 writes a u64 to the first eight bytes in the array b in little endian order.
 [direct_array_access; inline]
 pub fn little_endian_put_u64(mut b []u8, v u64) {
 	_ = b[7] // bounds check
@@ -51,13 +56,14 @@ pub fn little_endian_put_u64(mut b []u8, v u64) {
 	b[7] = u8(v >> u64(56))
 }
 
-// Big Endian
+// big_endian_u16 creates a u16 from two bytes in the array b in big endian order.
 [direct_array_access; inline]
 pub fn big_endian_u16(b []u8) u16 {
 	_ = b[1] // bounds check
 	return u16(b[1]) | (u16(b[0]) << u16(8))
 }
 
+// big_endian_put_u16 writes a u16 to the first two bytes in the array b in big endian order.
 [direct_array_access; inline]
 pub fn big_endian_put_u16(mut b []u8, v u16) {
 	_ = b[1] // bounds check
@@ -65,12 +71,14 @@ pub fn big_endian_put_u16(mut b []u8, v u16) {
 	b[1] = u8(v)
 }
 
+// big_endian_u32 creates a u32 from four bytes in the array b in big endian order.
 [direct_array_access; inline]
 pub fn big_endian_u32(b []u8) u32 {
 	_ = b[3] // bounds check
 	return u32(b[3]) | (u32(b[2]) << u32(8)) | (u32(b[1]) << u32(16)) | (u32(b[0]) << u32(24))
 }
 
+// big_endian_put_u32 writes a u32 to the first four bytes in the array b in big endian order.
 [direct_array_access; inline]
 pub fn big_endian_put_u32(mut b []u8, v u32) {
 	_ = b[3] // bounds check
@@ -80,12 +88,14 @@ pub fn big_endian_put_u32(mut b []u8, v u32) {
 	b[3] = u8(v)
 }
 
+// big_endian_u64 creates a u64 from eight bytes in the array b in big endian order.
 [direct_array_access; inline]
 pub fn big_endian_u64(b []u8) u64 {
 	_ = b[7] // bounds check
 	return u64(b[7]) | (u64(b[6]) << u64(8)) | (u64(b[5]) << u64(16)) | (u64(b[4]) << u64(24)) | (u64(b[3]) << u64(32)) | (u64(b[2]) << u64(40)) | (u64(b[1]) << u64(48)) | (u64(b[0]) << u64(56))
 }
 
+// big_endian_put_u64 writes a u64 to the first eight bytes in the array b in big endian order.
 [direct_array_access; inline]
 pub fn big_endian_put_u64(mut b []u8, v u64) {
 	_ = b[7] // bounds check
