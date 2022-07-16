4cf8aa1438b9ad33649486ab4a167e1c21108540
boris-741 <boris_741@mail.ru>
2022-07-16 17:10:30 +0300

    test fix

--- vlib/encoding/csv/reader_test.v
+++ vlib/encoding/csv/reader_test.v
@@ -286,7 +286,7 @@ struct Test{
     amount int
 }
 
-fn test_decode_to_struct
+fn test_decode_to_struct()
 {
 	text := 'id,bonus,amount\n\r1,bomb,1\n\r2,rocket,1\n\r3,lightning,2'
 	arr := csv.decode<Test>( text )
