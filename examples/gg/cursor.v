092dbd9cafdfbf186b6ef8d0a1b677b121219933
Delyan Angelov <delian66@gmail.com>
2022-07-18 02:19:44 +0300

    simplify the cursor example even further

--- examples/gg/cursor.v
+++ examples/gg/cursor.v
@@ -5,27 +5,20 @@ import gx
 import sokol.sapp
 
 fn main() {
-	mut context := gg.new_context(
-		bg_color: gx.rgb(255, 255, 255)
-		width: 600
-		height: 400
+	gg.new_context(
+		bg_color: gx.white
 		window_title: 'Cursor'
 		frame_fn: frame
 		init_fn: init
-	)
-	context.run()
+	).run()
 }
 
 fn init(mut ctx gg.Context) {
-	sapp.set_mouse_cursor(sapp.MouseCursor.text)
+	sapp.set_mouse_cursor(.ibeam)
 }
 
 fn frame(mut ctx gg.Context) {
 	ctx.begin()
-	sapp.set_mouse_cursor(sapp.MouseCursor.text)
-	ctx.draw_convex_poly([f32(100.0), 100.0, 200.0, 100.0, 300.0, 200.0, 200.0, 300.0, 100.0, 300.0],
-		gx.blue)
-	ctx.draw_poly_empty([f32(50.0), 50.0, 70.0, 60.0, 90.0, 80.0, 70.0, 110.0], gx.black)
-	ctx.draw_triangle_filled(450, 142, 530, 280, 370, 280, gx.red)
+	ctx.draw_text_def(10, 25, 'Your cursor should be an I shaped beam.')
 	ctx.end()
 }
