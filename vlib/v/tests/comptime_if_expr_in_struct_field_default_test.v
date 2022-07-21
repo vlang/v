67e6fc45aba4545065a29731f092ba90c13f463d
yuyi98 <yuyi98@163.com>
2022-07-21 14:31:12 +0800

    fmt comptime_if_expr_in_struct_field_default_test.v

--- vlib/v/tests/comptime_if_expr_in_struct_field_default_test.v
+++ vlib/v/tests/comptime_if_expr_in_struct_field_default_test.v
@@ -1,11 +1,13 @@
-struct Foo{
-	text string = $if linux { 'linux' } $else {
+struct Foo {
+	text string = $if linux {
+		'linux'
+	} $else {
 		println('else')
 		'else'
 	}
 }
 
-fn test_comptime_if_expr_in_struct_field_default(){
+fn test_comptime_if_expr_in_struct_field_default() {
 	f := Foo{}
 	println(f)
 	assert f.text.len > 0
