bf389ed66cfba27b8decd408f01a9dd8d038b15f
yuyi98 <yuyi98@163.com>
2022-07-21 19:43:30 +0800

    optimize stmt terminator

--- vlib/v/gen/c/comptime.v
+++ vlib/v/gen/c/comptime.v
@@ -274,14 +274,18 @@ fn (mut g Gen) comptime_if(node ast.IfExpr) {
 					g.stmts(branch.stmts[..len - 1])
 					g.write('\t$tmp_var = ')
 					g.stmt(last)
-					g.writeln(';')
+					if !g.out.last_n(2).trim_space().ends_with(';') {
+						g.writeln(';')
+					}
 					g.writeln('}')
 					g.indent--
 				} else {
 					g.indent++
 					g.write('$styp $tmp_var = ')
 					g.stmt(last)
-					g.writeln(';')
+					if !g.out.last_n(2).trim_space().ends_with(';') {
+						g.writeln(';')
+					}
 					g.indent--
 				}
 			}
