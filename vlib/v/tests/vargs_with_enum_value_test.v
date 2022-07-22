b0c32e0dbf81dabbf65efccbe181bd28a967fe3e
yuyi <yuyi98@163.com>
2022-07-22 21:08:22 +0800

    checker: fix fn variadic with enum value (#15177)

---
+++ vlib/v/tests/vargs_with_enum_value_test.v
@@ -0,0 +1,19 @@
+enum Foo {
+	a
+	b
+	c
+}
+
+fn foo(n int, m ...map[Foo]int) {
+	println(m)
+	assert m.len == 2
+	assert '$m' == '[{a: 1}, {b: 2}]'
+}
+
+fn test_vargs_with_enum_value() {
+	foo(1, {
+		.a: 1,
+	}, {
+		.b: 2,
+	})
+}
