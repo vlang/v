32e94642f7b0e44fa0cdc56e78a37bee2fc87e9e
Delyan Angelov <delian66@gmail.com>
2022-07-24 04:36:34 +0300

    include both old KeywordMatcher and the new KeywordMatcherTrie in bench_compare_tokens.v

--- vlib/v/tests/bench/bench_compare_tokens.v
+++ vlib/v/tests/bench/bench_compare_tokens.v
@@ -4,11 +4,11 @@ import benchmark
 const max_repetitions = 4_000_000
 
 fn main() {
+	mut res := token.Kind{}
 	km := token.new_keywords_matcher(token.keywords)
+	km_trie := token.new_keywords_matcher_trie(token.keywords)
 	for kw in ['for', 'val', 'int', 'f32', 'struct', 'return', 'if', 'in', 'as', 'or', 'else',
-		'unsafe', 'return', 'assert', 'Abc', 'my_identifier', 'a'] {
-		mut res := token.Kind{}
-
+		'unsafe', 'return', 'assert', 'Abc', 'my_identifier', 'a', 'assez', 'returned'] {
 		mut bmark := benchmark.start()
 		for _ in 0 .. max_repetitions {
 			res = token.keywords[kw]
@@ -19,6 +19,11 @@ fn main() {
 			res = token.Kind(km.find(kw))
 		}
 		bmark.measure('$max_repetitions repetitions of km.find("$kw") = $res')
+
+		for _ in 0 .. max_repetitions {
+			res = token.Kind(km_trie.find(kw))
+		}
+		bmark.measure('$max_repetitions repetitions of km_trie.find("$kw") = $res')
 
 		println('--------------------------------')
 	}
