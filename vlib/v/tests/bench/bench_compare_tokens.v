77f2efe445d66e2820fa60fa83650d41748ab8e3
Delyan Angelov <delian66@gmail.com>
2022-07-24 04:41:18 +0300

    remove old code again, to reduce the binary size of the executables

--- vlib/v/tests/bench/bench_compare_tokens.v
+++ vlib/v/tests/bench/bench_compare_tokens.v
@@ -5,7 +5,6 @@ const max_repetitions = 4_000_000
 
 fn main() {
 	mut res := token.Kind{}
-	km := token.new_keywords_matcher(token.keywords)
 	km_trie := token.new_keywords_matcher_trie(token.keywords)
 	for kw in ['for', 'val', 'int', 'f32', 'struct', 'return', 'if', 'in', 'as', 'or', 'else',
 		'unsafe', 'return', 'assert', 'Abc', 'my_identifier', 'a', 'assez', 'returned'] {
@@ -14,11 +13,6 @@ fn main() {
 			res = token.keywords[kw]
 		}
 		bmark.measure('$max_repetitions repetitions of token.keywords["$kw"] = $res')
-
-		for _ in 0 .. max_repetitions {
-			res = token.Kind(km.find(kw))
-		}
-		bmark.measure('$max_repetitions repetitions of km.find("$kw") = $res')
 
 		for _ in 0 .. max_repetitions {
 			res = token.Kind(km_trie.find(kw))
