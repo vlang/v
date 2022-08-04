f1a7285f498a74d793cab86b2dc3a3c457f8a325
yuyi98 <yuyi98@163.com>
2022-08-04 11:11:26 +0800

    checker: check generic interface missing type parameter

--- vlib/v/checker/containers.v
+++ vlib/v/checker/containers.v
@@ -24,6 +24,30 @@ pub fn (mut c Checker) array_init(mut node ast.ArrayInit) ast.Type {
 							node.elem_type_pos)
 					}
 				}
+			} else if elem_sym.kind == .interface_ {
+				elem_info := elem_sym.info as ast.Interface
+				if elem_info.generic_types.len > 0 && elem_info.concrete_types.len == 0
+					&& !node.elem_type.has_flag(.generic) {
+					if c.table.cur_concrete_types.len == 0 {
+						c.error('generic interface must specify type parameter, e.g. Foo<int>',
+							node.elem_type_pos)
+					} else {
+						c.error('generic interface must specify type parameter, e.g. Foo<T>',
+							node.elem_type_pos)
+					}
+				}
+			} else if elem_sym.kind == .sum_type {
+				elem_info := elem_sym.info as ast.SumType
+				if elem_info.generic_types.len > 0 && elem_info.concrete_types.len == 0
+					&& !node.elem_type.has_flag(.generic) {
+					if c.table.cur_concrete_types.len == 0 {
+						c.error('generic sumtype must specify type parameter, e.g. Foo<int>',
+							node.elem_type_pos)
+					} else {
+						c.error('generic sumtype must specify type parameter, e.g. Foo<T>',
+							node.elem_type_pos)
+					}
+				}
 			}
 		}
 		if node.exprs.len == 0 {
