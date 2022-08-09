7be9963a41e6ee4bf7b0cad6aed3fcd8fbc01faf
yuyi <yuyi98@163.com>
2022-08-09 12:46:55 +0800

    checker: fix dumping generic fn mut argument (#15384)

--- vlib/v/checker/checker.v
+++ vlib/v/checker/checker.v
@@ -2168,8 +2168,9 @@ pub fn (mut c Checker) expr(node_ ast.Expr) ast.Type {
 				return ast.void_type
 			}
 
-			tsym := c.table.sym(node.expr_type)
-			c.table.dumps[int(node.expr_type)] = tsym.cname
+			unwrapped_expr_type := c.unwrap_generic(node.expr_type)
+			tsym := c.table.sym(unwrapped_expr_type)
+			c.table.dumps[int(unwrapped_expr_type)] = tsym.cname
 			node.cname = tsym.cname
 			return node.expr_type
 		}
