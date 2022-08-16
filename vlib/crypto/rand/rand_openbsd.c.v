503c88bdf685279026d29b99efae79ee6dc1dd2b
Delyan Angelov <delian66@gmail.com>
2022-08-16 21:02:55 +0300

    add a doc comment to the pub fn read in rand_openbsd.c.v
    make the CI job report-missing-fn-doc pass

--- vlib/crypto/rand/rand_openbsd.c.v
+++ vlib/crypto/rand/rand_openbsd.c.v
@@ -8,6 +8,7 @@ module rand
 
 fn C.arc4random_buf(p &byte, n usize)
 
+// read returns an array of `bytes_needed` random bytes read from the OS.
 pub fn read(bytes_needed int) ?[]u8 {
 	mut buffer := unsafe { malloc_noscan(bytes_needed) }
 	C.arc4random_buf(buffer, bytes_needed)
