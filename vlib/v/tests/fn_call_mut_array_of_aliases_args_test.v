374186f1f77f9d7299a3236d411e611411e4db6c
yuyi <yuyi98@163.com>
2022-08-17 13:49:53 +0800

    checker: fix fn call with mut array of aliases arguments (#15443)

---
+++ vlib/v/tests/fn_call_mut_array_of_aliases_args_test.v
@@ -0,0 +1,27 @@
+fn f1(mut b []byte) string {
+	return '$b'
+}
+
+fn f2(b []byte) string {
+	return '$b'
+}
+
+fn f3(mut b []u8) string {
+	return '$b'
+}
+
+fn test_fn_call_mut_array_of_aliases() {
+	mut s1 := 'test'.bytes()
+
+	ret1 := f1(mut s1)
+	println(ret1)
+	assert ret1 == '[116, 101, 115, 116]'
+
+	ret2 := f2(s1)
+	println(ret2)
+	assert ret2 == '[116, 101, 115, 116]'
+
+	ret3 := f3(mut s1)
+	println(ret3)
+	assert ret3 == '[116, 101, 115, 116]'
+}
