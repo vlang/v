adc488d2c149e1ac8d2503eb89dfa0ff51585353
yuyi98 <yuyi98@163.com>
2022-08-18 13:39:57 +0800

    fmt struct_init_with_update_test.v

--- vlib/v/tests/struct_init_with_update_test.v
+++ vlib/v/tests/struct_init_with_update_test.v
@@ -1,23 +1,23 @@
 module main
 
 struct Author {
-  username string
-  age int
+	username string
+	age      int
 }
 
 fn cool_author() Author {
-  return Author{
-    username: 'Terisback'
-    age: 18
-  }
+	return Author{
+		username: 'Terisback'
+		age: 18
+	}
 }
 
 fn test_struct_init_with_update_expr() {
-  mut o := Author{
-    ...cool_author()
-    age: 21
-  }
-  println(o)
-  assert o.username == 'Terisback'
-  assert o.age == 21
+	mut o := Author{
+		...cool_author()
+		age: 21
+	}
+	println(o)
+	assert o.username == 'Terisback'
+	assert o.age == 21
 }
