f2785578191dd4a1ea060fad899c1b2e549c5d11
yuyi98 <yuyi98@163.com>
2022-08-18 13:33:26 +0800

    checker: fix struct init with update expr

---
+++ vlib/v/tests/struct_init_with_update_test.v
@@ -0,0 +1,23 @@
+module main
+
+struct Author {
+  username string
+  age int
+}
+
+fn cool_author() Author {
+  return Author{
+    username: 'Terisback'
+    age: 18
+  }
+}
+
+fn test_struct_init_with_update_expr() {
+  mut o := Author{
+    ...cool_author()
+    age: 21
+  }
+  println(o)
+  assert o.username == 'Terisback'
+  assert o.age == 21
+}
