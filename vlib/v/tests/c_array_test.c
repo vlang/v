6376c509c8cece6bb069886d4cbd48195c051f70
lmp <dev.larpon@gmail.com>
2022-08-23 09:00:21 +0200

    arrays: move carray_to_varray, make it generic

--- vlib/v/tests/c_array_test.c
+++ vlib/v/tests/c_array_test.c
@@ -6,3 +6,10 @@ void* gen_c_array(int size) {
     return c_array;
 }
 
+void* gen_c_int_array(int size) {
+    int *c_array = malloc(size);
+    for(int i = 0; i < size; i++) {
+        c_array[i] = i & 0xFF;
+    }
+    return c_array;
+}
