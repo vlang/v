1958450aab00b7b10424d7c9e88898533adfaf5b
Zakarias <46316857+zach721@users.noreply.github.com>
2022-08-25 13:19:22 +0200

    script formated with v fmt -w fibonacci.v

--- examples/fibonacci.v
+++ examples/fibonacci.v
@@ -1,30 +1,30 @@
 //
-//Get Fibonacci numbers with V
-//Author:Zach721
-//You can this code for free to your
-//own projects and purposes
+// Get Fibonacci numbers with V
+// Author:Zach721
+// You can this code for free to your
+// own projects and purposes
 //
 
-//Get is a function which returns the Fibonacci
-//secuence
- fn get(number f64){
-
-	//All variables must be f64 to not get any calculation error
-    mut first := f64(0)
-    mut second := f64(1)
-    println('Fibbonacci secuence:')
-    for  i:= 0; i<number-1;i++{
-        if first==0{
-            println(first)
-        }
-    mut third := f64(first+second)
-    println(first+second)
-    first = f64(second)
-     second = f64(third)
-    }}
-
-	fn main(){
-    //Let's print the first 100 numbers of the Fibonacci secuence
-		get(100)
-	
+// Get is a function which returns the Fibonacci
+// secuence
+fn get(number f64) {
+	// All variables must be f64 to not get any calculation error
+	mut first := f64(0)
+	mut second := f64(1)
+	println('Fibbonacci secuence:')
+	for i := 0; i < number - 1; i++ {
+		if first == 0 {
+			println(first)
+		}
+		mut third := f64(first + second)
+		println(first + second)
+		first = f64(second)
+		second = f64(third)
 	}
+}
+
+fn main() {
+	// Let's print the first 100 numbers of the Fibonacci secuence
+	get(100)
+}
+
