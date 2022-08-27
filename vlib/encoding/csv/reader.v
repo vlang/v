d7c24528d11cb3b7431dbd81f9773d89aade2c7f
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-08-28 01:49:03 +0800

    Cleanup reader.v

--- vlib/encoding/csv/reader.v
+++ vlib/encoding/csv/reader.v
@@ -38,22 +38,23 @@ fn (err InvalidLineEndingError) msg() string {
 	return 'encoding.csv: could not find any valid line endings'
 }
 
-pub struct Reader {
+struct Reader {
 	// not used yet
 	// has_header        bool
 	// headings          []string
-	data string
-pub mut:
+	data              string
 	delimiter         u8
 	comment           u8
 	is_mac_pre_osx_le bool
-	row_pos           int
+mut:
+	row_pos int
 }
 
 [params]
 pub struct ReaderConfig {
-	delimiter u8 = `,`
-	comment   u8 = `#`
+	delimiter         u8   = `,`
+	comment           u8   = `#`
+	is_mac_pre_osx_le bool = false
 }
 
 // new_reader initializes a Reader with string data to parse and,
@@ -63,6 +64,7 @@ pub fn new_reader(data string, config ReaderConfig) &Reader {
 		data: data
 		delimiter: config.delimiter
 		comment: config.comment
+		is_mac_pre_osx_le: config.is_mac_pre_osx_le
 	}
 }
 
