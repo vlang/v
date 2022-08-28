670370dedfefba6841cfe51c5c43492333fccc3f
floscodes <petautschnig.florian@gmail.com>
2022-08-28 23:45:02 +0200

    adding tests to vweb csrf-module

---
+++ vlib/vweb/tests/vweb_csrf_test.v
@@ -0,0 +1,24 @@
+module main
+
+import vweb
+import csrf
+
+struct App {
+	csrf.App
+}
+
+fn main() {
+	    vweb.run_at(&App{}, vweb.RunParams{
+        port: 8080
+    }) or { panic(err) }
+}
+
+fn (mut app App) index() vweb.Result {
+	app.set_csrf_cookie()
+	return app.text("Csrf-Token set!")
+}
+
+fn (mut app App) foo() vweb.Result {
+	app.csrf_protect()
+	return app.text("Checked and passed csrf-guard")
+}
