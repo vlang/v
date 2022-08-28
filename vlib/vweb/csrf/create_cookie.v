2d8c8869b46be55ee52f34c8f871b153a85c29ef
floscodes <petautschnig.florian@gmail.com>
2022-08-28 23:28:18 +0200

    adding csrf protection module to vweb

--- vlib/vweb/csrf/create_cookie.v
+++ vlib/vweb/csrf/create_cookie.v
@@ -2,44 +2,47 @@ module csrf
 
 import rand
 
-//creating the value of the csrf-token
-const chars = "QWERTZUIOPASDFGHJKLYXCVBNMqwertzuiopasdfghjklyxcvbnm1234567890_-"
-const cookie_key = "__Host-Csrf-Token"
+// creating the value of the csrf-token
+const chars = 'QWERTZUIOPASDFGHJKLYXCVBNMqwertzuiopasdfghjklyxcvbnm1234567890_-'
 
-//Public function that generates the token and sets the cookie
+const cookie_key = '__Host-Csrf-Token'
+
+// Public function that generates the token and sets the cookie
 pub fn (mut app App) set_csrf_cookie() App {
 	app.set_cookie(create_cookie().Cookie)
 	return app
 }
 
 fn generate() string {
-	mut out := ""
-	for _ in 0..42 {
-		i := rand.intn(chars.len_utf8()) or {panic("Error while trying to generate Csrf-Token: $err")}
-		out=out+chars[i..i+1]
+	mut out := ''
+	for _ in 0 .. 42 {
+		i := rand.intn(csrf.chars.len_utf8()) or {
+			panic('Error while trying to generate Csrf-Token: $err')
+		}
+		out = out + csrf.chars[i..i + 1]
 	}
 	return out
 }
 
-//create the cookie
+// create the cookie
 fn create_cookie() CsrfCookie {
-	return CsrfCookie {
-		name: cookie_key
+	return CsrfCookie{
+		name: csrf.cookie_key
 		value: generate()
-		path: "/"
+		path: '/'
 		max_age: 0
 		secure: true
 		http_only: true
 	}
 }
 
-//optional set of http_only
+// optional set of http_only
 fn (mut cookie CsrfCookie) http_only(b bool) CsrfCookie {
-	cookie.http_only=b
+	cookie.http_only = b
 	return cookie
 }
 
-//Public function that returns the token as a string
+// Public function that returns the token as a string
 pub fn (mut app App) get_csrf_token() string {
-	return app.get_cookie(cookie_key) or {""}
+	return app.get_cookie(csrf.cookie_key) or { '' }
 }
--- vlib/vweb/csrf/protect.v
+++ vlib/vweb/csrf/protect.v
@@ -3,28 +3,29 @@ module csrf
 import net.http
 
 pub fn (mut app App) csrf_protect() CheckedApp {
-	req_cookies:=app.req.cookies.clone()
-	app_csrf_cookie_str:=app.get_cookie(cookie_key) or {
-		app.set_status(403, "")
-		return app.text("Error 403 - Forbidden")
+	req_cookies := app.req.cookies.clone()
+	app_csrf_cookie_str := app.get_cookie(cookie_key) or {
+		app.set_status(403, '')
+		return app.text('Error 403 - Forbidden')
 	}
 
 	if cookie_key in req_cookies && req_cookies[cookie_key] == app_csrf_cookie_str {
-		//Csrf-Check OK - return app as normal in order to handel request normally
+		// Csrf-Check OK - return app as normal in order to handel request normally
 		return app
 	} else if app.check_headers(app_csrf_cookie_str) {
-		//Csrf-Check OK - return app as normal in order to handel request normally
+		// Csrf-Check OK - return app as normal in order to handel request normally
 		return app
 	} else {
-		//TO-DO: Do not return normally!! The client has not passed the Csrf-Check!!
-		app.set_status(403, "")
-		return app.text("Error 403 - Forbidden")
+		// TO-DO: Do not return normally!! The client has not passed the Csrf-Check!!
+		app.set_status(403, '')
+		return app.text('Error 403 - Forbidden')
 	}
-
 }
 
 fn (app App) check_headers(app_csrf_cookie_str string) bool {
-	token := app.req.header.get_custom("Csrf-Token", http.HeaderQueryConfig{true}) or {return false}
+	token := app.req.header.get_custom('Csrf-Token', http.HeaderQueryConfig{true}) or {
+		return false
+	}
 	if token == app_csrf_cookie_str {
 		return true
 	} else {
