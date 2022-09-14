fd1b3fc861b8351bfdcf5b4bfe20ab922051f8a8
yuyi <yuyi98@163.com>
2022-09-14 22:43:16 +0800

    cgen: fix error of 'in array of sumtype' (#15756)

--- vlib/v/gen/c/infix.v
+++ vlib/v/gen/c/infix.v
@@ -437,12 +437,12 @@ fn (mut g Gen) infix_expr_in_op(node ast.InfixExpr) {
 			elem_type := right.sym.info.elem_type
 			elem_type_ := g.unwrap(elem_type)
 			if elem_type_.sym.kind == .sum_type {
-				if node.left_type in elem_type_.sym.sumtype_info().variants {
+				if ast.mktyp(node.left_type) in elem_type_.sym.sumtype_info().variants {
 					new_node_left := ast.CastExpr{
 						arg: ast.empty_expr
 						typ: elem_type
 						expr: node.left
-						expr_type: node.left_type
+						expr_type: ast.mktyp(node.left_type)
 					}
 					g.gen_array_contains(node.right_type, node.right, new_node_left)
 					return
@@ -507,12 +507,12 @@ fn (mut g Gen) infix_expr_in_op(node ast.InfixExpr) {
 			elem_type := right.sym.info.elem_type
 			elem_type_ := g.unwrap(elem_type)
 			if elem_type_.sym.kind == .sum_type {
-				if node.left_type in elem_type_.sym.sumtype_info().variants {
+				if ast.mktyp(node.left_type) in elem_type_.sym.sumtype_info().variants {
 					new_node_left := ast.CastExpr{
 						arg: ast.empty_expr
 						typ: elem_type
 						expr: node.left
-						expr_type: node.left_type
+						expr_type: ast.mktyp(node.left_type)
 					}
 					g.gen_array_contains(node.right_type, node.right, new_node_left)
 					return
