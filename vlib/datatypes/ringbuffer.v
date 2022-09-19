7d709881d939994f67ab52fadc2fc278ca38d677
floscodes <petautschnig.florian@gmail.com>
2022-09-19 12:54:25 +0200

    Implementing head and tail pointers, as well as fixed sized array

--- vlib/datatypes/ringbuffer.v
+++ vlib/datatypes/ringbuffer.v
@@ -4,47 +4,51 @@ module datatypes
 
 pub struct RingBuffer<T> {
 mut:
+	reader  int // index of the tail where data is going to be read
+	writer  int // index of the head where data is going to be written
 	content []T
 }
 
-// new - creates an empty ringbuffer
+// new_ringbuffer - creates an empty ringbuffer
 pub fn new_ringbuffer<T>(s int) RingBuffer<T> {
 	return RingBuffer<T>{
-		content: []T{cap: s}
-	}
+		reader: 0
+		writer: 0
+		content: []T{len: s + 1, cap: s + 1}
+	} // increasing custom set size by one element in order to make ring flow possible, so that writer cannot equal reader before reader-index has been read.
 }
 
-// push - adds an element to the ringbuffer ensuring that the buffer will never grow
-pub fn (mut rb RingBuffer<T>) push(element T) ? {
-	if rb.content.len == rb.content.cap {
-		eprintln('Error: buffer overflow\n\nCannot push value $element to buffer because buffer has only a capacity of $rb.content.cap')
+// push - adds an element to the ringbuffer
+pub fn (mut rb RingBuffer<T>) push(element T) {
+	if rb.is_full() {
+		eprintln('Buffer overflow')
 	} else {
-		rb.content.insert(0, element)
+		rb.content[rb.writer] = element
+		rb.writer = rb.writer + 1
+		if rb.writer > rb.content.len - 1 {
+			rb.writer = 0
+		}
 	}
 }
 
-// pop - returns the oldest element and deletes it from the ringbuffer
-pub fn (mut rb RingBuffer<T>) pop() ?T {
-	if rb.content.len == 0 {
-		eprintln('Error: buffer is empty')
+// pop - returns the oldest element of the buffer
+pub fn (mut rb RingBuffer<T>) pop() T {
+	mut v := rb.content[rb.reader]
+	if rb.is_empty() {
+		eprintln('Buffer is empty')
+	} else {
+		rb.reader = rb.reader + 1
+		if rb.reader > rb.content.len - 1 {
+			rb.reader = 0
+		}
 	}
-	return rb.content.pop()
-}
-
-// clear - emptys the ringbuffer
-pub fn (mut rb RingBuffer<T>) clear() {
-	rb.content = []T{len: 0, cap: rb.content.cap}
-}
-
-// capacity - returns the capacity of the ringbuffer
-pub fn (rb RingBuffer<T>) capacity() int {
-	return rb.content.cap
+	return v
 }
 
 // is_empty - checks if the ringbuffer is empty
 pub fn (rb RingBuffer<T>) is_empty() bool {
-	if rb.content.len == 0 {
-		return true
+	if rb.reader == rb.writer {
+		return true // if reader equals writer it means that no value to read has been written before. It follows that the buffer is empty.
 	} else {
 		return false
 	}
@@ -52,14 +56,25 @@ pub fn (rb RingBuffer<T>) is_empty() bool {
 
 // is_full - checks if the ringbuffer is full
 pub fn (rb RingBuffer<T>) is_full() bool {
-	if rb.content.len < rb.content.cap {
-		return false
-	} else {
+	if rb.writer + 1 == rb.reader {
 		return true
+	} else if rb.writer == rb.content.len - 1 && rb.reader == 0 {
+		return true
+	} else {
+		return false
 	}
 }
 
-// remaining -  returns the remaining capacity of the ringbuffer
-pub fn (rb RingBuffer<T>) remaining() int {
-	return rb.content.cap - rb.content.len
+// capacity - returns the capacity of the ringbuffer
+pub fn (rb RingBuffer<T>) capacity() int {
+	return rb.content.cap - 1 // reduce by one because of the extra element explained in function `new_ringbuffer()`
+}
+
+// clear - emptys the ringbuffer
+pub fn (mut rb RingBuffer<T>) clear() {
+	rb = RingBuffer<T>{
+		reader: 0
+		writer: 0
+		content: []T{len: rb.content.len, cap: rb.content.cap}
+	}
 }
--- vlib/datatypes/ringbuffer_test.v
+++ vlib/datatypes/ringbuffer_test.v
@@ -3,26 +3,26 @@ import datatypes
 fn test_push_and_pop() {
 	mut r := datatypes.new_ringbuffer<int>(2)
 
-	r.push(3) or { panic(err) }
-	r.push(4) or { panic(err) }
+	r.push(3)
+	r.push(4)
 
-	mut oldest_value := r.pop() or { panic(err) }
+	mut oldest_value := r.pop()
 
 	assert oldest_value == 3
 
-	r.push(5) or { panic(err) }
+	r.push(5)
 
-	oldest_value = r.pop() or { panic(err) }
+	oldest_value = r.pop()
 
 	assert oldest_value == 4
 }
 
 fn test_clear_and_empty() {
 	mut r := datatypes.new_ringbuffer<int>(4)
-	r.push(3) or { panic(err) }
-	r.push(4) or { panic(err) }
+	r.push(3)
+	r.push(4)
 
-	oldest_value := r.pop() or { panic(err) }
+	oldest_value := r.pop()
 	assert oldest_value == 3
 
 	r.clear()
@@ -30,13 +30,15 @@ fn test_clear_and_empty() {
 	assert r.is_empty() == true
 }
 
-fn test_capacity_remaining_is_full() {
+fn test_capacity_and_is_full() {
 	mut r := datatypes.new_ringbuffer<int>(4)
 
-	r.push(3) or { panic(err) }
-	r.push(4) or { panic(err) }
-
-	assert r.is_full() == false
 	assert r.capacity() == 4
-	assert r.remaining() == 2
+
+	r.push(3)
+	r.push(4)
+	r.push(5)
+	r.push(6)
+
+	assert r.is_full() == true
 }
