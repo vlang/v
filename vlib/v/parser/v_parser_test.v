f6a68003d3d24e844ccc2c4ab7e37e9a65818466
yuyi <yuyi98@163.com>
2022-10-01 18:18:46 +0800

    ci,parser: fix error in v_parser_test.v (#15936)

--- vlib/v/parser/v_parser_test.v
+++ vlib/v/parser/v_parser_test.v
@@ -107,7 +107,8 @@ fn test_one() {
 	}
 	mut checker := checker.new_checker(table, vpref)
 	checker.check(program)
-	res, _, _, _ := c.gen([program], table, vpref).replace('\n', '').trim_space().after('#endif')
+	mut res, _, _, _ := c.gen([program], table, vpref)
+	res = res.replace('\n', '').trim_space().after('#endif')
 	println(res)
 	ok := expected == res
 	println(res)
@@ -148,7 +149,8 @@ fn test_parse_expr() {
 		global_scope: scope
 	}
 	chk.check(program)
-	res, _, _, _ := c.gen([program], table, vpref).after('#endif')
+	mut res, _, _, _ := c.gen([program], table, vpref)
+	res = res.after('#endif')
 	println('========')
 	println(res)
 	println('========')
