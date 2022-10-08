5706879ee11f2a4e8901724f053606197d80509d
Delyan Angelov <delian66@gmail.com>
2022-10-08 12:52:52 +0300

    support nested map inits too

--- vlib/v/tests/map_init_with_multi_enum_keys_test.v
+++ vlib/v/tests/map_init_with_multi_enum_keys_test.v
@@ -3,6 +3,14 @@ enum Foo {
 	b
 }
 
+type NestedAbc = map[string]string | string
+
+enum NestedFoo {
+	a
+	b
+	c
+}
+
 fn test_map_init_with_multi_enum_keys() {
 	mp := {
 		Foo.a: 'A'
@@ -11,4 +19,24 @@ fn test_map_init_with_multi_enum_keys() {
 	println(mp)
 	assert mp[.a] == 'A'
 	assert mp[.b] == 'B'
+}
+
+fn test_nested_map_init_with_multi_enum_keys() {
+	mp := {
+		NestedFoo.a: NestedAbc({
+			'A': 'AA'
+		})
+		.b:          'B',
+		.c:          {
+			'c': 'C'
+		},
+	}
+	println(mp)
+	assert mp[.a]? == NestedAbc({
+		'A': 'AA'
+	})
+	assert mp[.b]? == NestedAbc('B')
+	assert mp[.c]? == NestedAbc({
+		'c': 'C'
+	})
 }
