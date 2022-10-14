65ef18a4bdfe4990b6658a48f02dc80d0902ecff
yuyi <yuyi98@163.com>
2022-10-14 22:00:52 +0800

    change autobahn_tests and json2

--- vlib/net/websocket/tests/autobahn/autobahn_client_wss.v
+++ vlib/net/websocket/tests/autobahn/autobahn_client_wss.v
@@ -11,21 +11,21 @@ fn main() {
 	// update the reports
 	// uri := 'wss://localhost:9002/updateReports?agent=v-client'
 	uri := 'wss://autobahn_server_wss:9002/updateReports?agent=v-client'
-	mut ws := websocket.new_client(uri)?
-	ws.connect()?
-	ws.listen()?
+	mut ws := websocket.new_client(uri)!
+	ws.connect()!
+	ws.listen()!
 }
 
-fn handle_case(case_nr int) ? {
+fn handle_case(case_nr int) ! {
 	uri := 'wss://autobahn_server_wss:9002/runCase?case=$case_nr&agent=v-client'
 	// uri := 'wss://localhost:9002/runCase?case=$case_nr&agent=v-client'
-	mut ws := websocket.new_client(uri)?
+	mut ws := websocket.new_client(uri)!
 	ws.on_message(on_message)
-	ws.connect()?
-	ws.listen()?
+	ws.connect()!
+	ws.listen()!
 }
 
-fn on_message(mut ws websocket.Client, msg &websocket.Message) ? {
+fn on_message(mut ws websocket.Client, msg &websocket.Message) ! {
 	// autobahn tests expects to send same message back
 	if msg.opcode == .pong {
 		// We just wanna pass text and binary message back to autobahn
