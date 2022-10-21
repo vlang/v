c3d8408d6ca1cf8adef01885d7c123d43ea71c0f
Grantley Cullar <83633399+gospacedev@users.noreply.github.com>
2022-10-21 08:47:29 +0800

    Create mad_libs.v

---
+++ examples/mad_libs.v
@@ -0,0 +1,13 @@
+module main
+
+import os
+
+fn main() {
+	actor := os.input('Enter an actor: ')
+	food := os.input('Enter an food: ')
+	adjective := os.input('Enter a adjective: ')
+
+
+	println('$actor cooked a $food in a $adjective manner.')
+
+}
