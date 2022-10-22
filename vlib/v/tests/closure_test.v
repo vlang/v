32c90741d3ff6b48e6d0b69f2e07b28daff797f2
Delyan Angelov <delian66@gmail.com>
2022-10-22 13:21:17 +0300

    checker: fix taking a closure of x, inside `for x in y {` (fix #16141)

--- vlib/v/tests/closure_test.v
+++ vlib/v/tests/closure_test.v
@@ -179,7 +179,7 @@ struct Command {
 
 struct App {}
 
-fn main() {
+fn test_larger_closure_parameters() {
 	mut app := &App{}
 	eprintln('app ptr: ${u64(app)}')
 	f := fn [mut app] (cmd Command) u64 {
@@ -197,4 +197,16 @@ fn main() {
 	res := f(cmd)
 	println('> res: $res | sizeof Command: ${sizeof(Command)}')
 	assert res == u64(app)
+}
+
+fn test_closure_in_for_in_loop() {
+	a := [2, 4, 6, 9]
+	for v in a {
+		func := fn [v] (msg string) string {
+			return '$msg: $v'
+		}
+		res := func('hello')
+		assert res == 'hello: $v'
+		// dump(res)
+	}
 }
