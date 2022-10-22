01f9a9d7e61b6a95bdb7a94b40a85eb353378745
l1mey112 <l-m@l-m.dev>
2022-10-22 19:23:17 +1100

    builtin: add tests for trim_indexes

--- vlib/builtin/string_test.v
+++ vlib/builtin/string_test.v
@@ -538,6 +538,16 @@ fn test_trim() {
 	assert 'aaabccc'.trim('ac') == 'b'
 }
 
+fn test_trim_indexes() {
+	mut left, mut right := 0, 0
+	left, right = '- -- - '.trim_indexes(' -')
+	assert left == 0 && right == 0
+	left, right = '- hello-world!\t'.trim_indexes(' -\t')
+	assert left == 2 && right == 14
+	left, right = 'abc'.trim_indexes('ac')
+	assert left == 1 && right == 2
+}
+
 fn test_trim_left() {
 	mut s := 'module main'
 	assert s.trim_left(' ') == 'module main'
