338965a48202785df4f06c4a3b6c15e05272a2e6
Delta456 <swstkbaranwal@gmail.com>
2022-10-24 00:49:11 +0530

    checker: add error for unwrapped result in infix expr

--- vlib/v/checker/infix.v
+++ vlib/v/checker/infix.v
@@ -619,6 +619,16 @@ pub fn (mut c Checker) infix_expr(mut node ast.InfixExpr) ast.Type {
 		opt_infix_pos := if left_is_optional { left_pos } else { right_pos }
 		c.error('unwrapped optional cannot be used in an infix expression', opt_infix_pos)
 	}
+
+	left_is_result := left_type.has_flag(.result)
+	right_is_result := right_type.has_flag(.result)
+	if left_is_result && right_is_result {
+		c.error('unwrapped result cannot be used in an infix expression', left_right_pos)
+	} else if left_is_result || right_is_result {
+		opt_infix_pos := if left_is_result { left_pos } else { right_pos }
+		c.error('unwrapped result cannot be used in an infix expression', opt_infix_pos)
+	}
+
 	// Dual sides check (compatibility check)
 	if node.left !is ast.ComptimeCall && node.right !is ast.ComptimeCall {
 		if !(c.symmetric_check(left_type, right_type) && c.symmetric_check(right_type, left_type))
