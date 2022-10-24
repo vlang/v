701586fa84638eb1a38838db93fcb2e9a34aee09
shove <shove@163.com>
2022-10-24 17:02:24 +0800

    checker, cgen: enable calls to methods of the parent element array, when an array element is an alias (fix #16169) (#16187)

--- vlib/v/gen/c/fn.v
+++ vlib/v/gen/c/fn.v
@@ -796,6 +796,13 @@ fn (mut g Gen) method_call(node ast.CallExpr) {
 	if typ_sym.kind == .alias && node.name != 'str' && !typ_sym.has_method(node.name) {
 		unwrapped_rec_type = (typ_sym.info as ast.Alias).parent_type
 		typ_sym = g.table.sym(unwrapped_rec_type)
+	} else if typ_sym.kind == .array && !typ_sym.has_method(node.name) {
+		typ := g.table.unaliased_type((typ_sym.info as ast.Array).elem_type)
+		typ_idx := g.table.find_type_idx(g.table.array_name(typ))
+		if typ_idx > 0 {
+			unwrapped_rec_type = ast.Type(typ_idx)
+			typ_sym = g.table.sym(unwrapped_rec_type)
+		}
 	}
 	rec_cc_type := g.cc_type(unwrapped_rec_type, false)
 	mut receiver_type_name := util.no_dots(rec_cc_type)
