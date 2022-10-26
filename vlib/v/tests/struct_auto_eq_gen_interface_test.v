01977cde4d06e2ed6e357eb28488574a683cc5f7
shove70 <shove@163.com>
2022-10-26 22:18:58 +0800

    cgen: fix comparing struct with pointers to interface values autogenerates working eq method (fix #16074)

--- vlib/v/tests/struct_auto_eq_gen_interface_test.v
+++ vlib/v/tests/struct_auto_eq_gen_interface_test.v
@@ -21,3 +21,23 @@ fn test_struct_auto_eq_gen_interface_case() {
 	w2 := new()
 	assert w1 == w2
 }
+
+// For https://github.com/vlang/v/issues/16074
+pub interface Logger {
+mut:
+	info(s string)
+}
+
+struct LogContainer {
+	name string
+mut:
+	logger &Logger = unsafe { nil }
+}
+
+fn test_comparing_struct_with_pointers_to_interface_values_autogenerates_working_eq_method() {
+	assert LogContainer{
+		name: 'abc'
+	} == LogContainer{
+		name: 'abc'
+	}
+}
