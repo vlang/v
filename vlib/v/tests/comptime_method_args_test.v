ffc0cd4171630a583aec9323695bdf2432a53eb7
shove70 <shove@163.com>
2022-10-27 15:54:50 +0800

    invalid test removed

--- vlib/v/tests/comptime_method_args_test.v
+++ vlib/v/tests/comptime_method_args_test.v
@@ -52,9 +52,3 @@ fn test_comptime_call_method() {
 	assert t.two_args_called
 	assert t.three_args_called
 }
-
-fn test_compile_error_and_args() {
-	$if !linux {
-		$compile_error('Only Lin\"ux is currently supported')
-	}
-}
