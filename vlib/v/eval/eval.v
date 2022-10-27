9e15d8e08ade23717355f49178f0b9b56d3a83ef
Delyan Angelov <delian66@gmail.com>
2022-10-27 10:41:59 +0300

    run `v fmt -w vlib/v/eval/eval.v`

--- vlib/v/eval/eval.v
+++ vlib/v/eval/eval.v
@@ -233,7 +233,7 @@ fn (e Eval) error(msg string) {
 fn (e Eval) panic(s string) {
 	commithash := unsafe { tos5(&char(C.V_CURRENT_COMMIT_HASH)) }
 	eprintln('V panic: $s')
-	eprintln('V hash: ${commithash}')
+	eprintln('V hash: $commithash')
 	e.print_backtrace()
 	exit(1)
 }
