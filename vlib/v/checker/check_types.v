9dd56f5e15db3e30096750ef1866ffb077cfb727
yuyi <yuyi98@163.com>
2022-10-31 19:03:09 +0800

    checker: check fn type mismatch of return result type fn

--- vlib/v/checker/check_types.v
+++ vlib/v/checker/check_types.v
@@ -381,6 +381,9 @@ pub fn (mut c Checker) check_matching_function_symbols(got_type_sym &ast.TypeSym
 	if got_fn.return_type.has_flag(.optional) != exp_fn.return_type.has_flag(.optional) {
 		return false
 	}
+	if got_fn.return_type.has_flag(.result) != exp_fn.return_type.has_flag(.result) {
+		return false
+	}
 	if !c.check_basic(got_fn.return_type, exp_fn.return_type) {
 		return false
 	}
