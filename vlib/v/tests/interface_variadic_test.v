1bf441e6dcad9fac518c15c490532b669af32932
shove70 <shove@163.com>
2022-11-02 18:59:18 +0800

    cgen: fix call method with an empty args on an interface with variadic (fix #16286)

--- vlib/v/tests/interface_variadic_test.v
+++ vlib/v/tests/interface_variadic_test.v
@@ -40,3 +40,29 @@ fn check_animals(animals ...Animal) {
 	assert animals[0] is Cat
 	assert animals[1] is Dog
 }
+
+// For issue: 16286 passing nothing to a variatic parameter on an interface method gives builder error
+interface Bar {
+	get(...int) []int
+}
+
+struct Baz {}
+
+fn (b Baz) get(n ...int) []int {
+	if n.len == 0 {
+		return [-1]
+	} else {
+		return n
+	}
+}
+
+fn test_empty_args_call_interface_methods() {
+	b := Baz{}
+	assert b.get() == [-1]
+
+	b_values := Bar(Baz{})
+	assert b_values.get(1, 2, 3) == [1, 2, 3]
+
+	b_empty := Bar(Baz{})
+	assert b_empty.get() == [-1]
+}
