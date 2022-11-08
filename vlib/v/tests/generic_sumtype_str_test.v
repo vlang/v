1d04b71c91dc46c5af536063b8fcefa6c39f9aee
yuyi <yuyi98@163.com>
2022-11-08 16:49:09 +0800

    cgen: fix generic sumtype auto str (#16355)

---
+++ vlib/v/tests/generic_sumtype_str_test.v
@@ -0,0 +1,33 @@
+module main
+
+struct None {}
+
+pub type Maybe<T> = None | T
+
+pub fn (m Maybe<T>) str<T>() string {
+	return if m is T {
+		x := m as T
+		'Some($x)'
+	} else {
+		'Noth'
+	}
+}
+
+pub fn some<T>(v T) Maybe<T> {
+	return Maybe<T>(v)
+}
+
+fn test_generic_sumtype_str() {
+	a := some(123)
+	b := some('abc')
+
+	println(a.str())
+	println(a)
+	println('$a')
+	assert '$a' == 'Some(123)'
+
+	println(b.str())
+	println(b)
+	println('$b')
+	assert '$b' == 'Some(abc)'
+}
