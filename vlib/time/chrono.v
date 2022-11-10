14329ae21cb5bfe2d9a59c7dc6607d4e5f428686
enghitalo <enghitalo@gmail.com>
2022-11-10 12:15:59 -0300

    feat: fn (time Time) days_from_unix_epoch() int, feat: fn date_from_days_after_unix_epoch(days int) Time, tests

--- vlib/time/chrono.v
+++ vlib/time/chrono.v
@@ -22,3 +22,50 @@ pub fn days_from_unix_epoch(year int, month int, day int) int {
 pub fn days_from_civil(year int, month int, day int) int {
 	return days_from_unix_epoch(year, month, day)
 }
+
+// (time Time) days_from_unix_epoch - return the number of days since the
+// Unix epoch 1970-01-01. A detailed description of the algorithm here
+// is in: http://howardhinnant.github.io/date_algorithms.html
+// Note that it will return negative values for days before 1970-01-01.
+pub fn (time Time) days_from_unix_epoch() int {
+	y := if time.month <= 2 { time.year - 1 } else { time.year }
+	era := y / f64(400)
+	year_of_the_era := y - era * 400 // [0, 399]
+	day_of_year := (153 * (time.month + (if time.month > 2 { -3 } else { 9 })) + 2) / f64(5) +
+		time.day - 1 // [0, 365]
+	day_of_the_era := year_of_the_era * 365 + year_of_the_era / 4 - year_of_the_era / f64(100) +
+		day_of_year // [0, 146096]
+	return int(era) * 146097 + int(day_of_the_era) - 719468
+}
+
+// date_from_days_after_unix_epoch - convert days after unix epoch
+// in Time format
+pub fn date_from_days_after_unix_epoch(days int) Time {
+	// This is a shift which aligns this algorithm with all known implementations of std::chrono::system_clock.
+	// It makes the serial date 0 be equivalent to 1970-01-01 instead of 0000-03-01.
+	z := days + 719468
+
+	era := (if z >= 0 { z } else { z - 146096 }) / 146097
+
+	day_of_the_era := z - era * 146097 // [0, 146096]
+
+	year_of_the_era := (day_of_the_era - day_of_the_era / 1460 + day_of_the_era / 36524 - day_of_the_era / 146096) / 365
+
+	year := year_of_the_era + era * 400
+
+	day_of_year := day_of_the_era - (365 * year_of_the_era + year_of_the_era / 4 - year_of_the_era / 100)
+
+	mp := (5 * day_of_year + 2) / 153 // [0, 11]
+	println('mp: $mp')
+
+	day := day_of_year - (153 * mp + 2) / 5 + 1
+	month := int(if mp < 10 { mp + 3 } else { mp - 9 })
+
+	time_result := Time{
+		year: year + if month <= 2 { 1 } else { 0 }
+		month: month
+		day: day
+	}
+
+	return time_result
+}
--- vlib/time/chrono_test.v
+++ vlib/time/chrono_test.v
@@ -1,6 +1,37 @@
 module time
 
 fn test_days_from_unix_epoch() {
+	time_test := Time{
+		year: 2000
+		month: 5
+		day: 10
+		hour: 22
+		minute: 11
+		second: 3
+		microsecond: 100
+	}
+	assert time_test.days_from_unix_epoch() == 11087
+
+	assert date_from_days_after_unix_epoch(11087).year == 2000
+	assert date_from_days_after_unix_epoch(11087).month == 5
+	assert date_from_days_after_unix_epoch(11087).day == 10
+
 	assert days_from_unix_epoch(1970, 1, 1) == 0
+
+	assert days_from_unix_epoch(1970, 2, 1) == 31
+	assert days_from_unix_epoch(1970, 3, 1) == 59
+
 	assert days_from_unix_epoch(2022, 11, 10) == 19306
+
+	assert date_from_days_after_unix_epoch(1).year == 1970
+	assert date_from_days_after_unix_epoch(1).month == 1
+	assert date_from_days_after_unix_epoch(1).day == 2
+
+	assert date_from_days_after_unix_epoch(31).year == 1970
+	assert date_from_days_after_unix_epoch(31).month == 2
+	assert date_from_days_after_unix_epoch(31).day == 1
+
+	assert date_from_days_after_unix_epoch(59).year == 1970
+	assert date_from_days_after_unix_epoch(59).month == 3
+	assert date_from_days_after_unix_epoch(59).day == 1
 }
