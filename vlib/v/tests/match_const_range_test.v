0754551e9cd15fb46d00bcf10f9e09fdd90f6b19
Delyan Angelov <delian66@gmail.com>
2022-11-25 15:30:14 +0200

    tests: just enumerate the taken branches in the test, and test all decisions at once after the loop

--- vlib/v/tests/match_const_range_test.v
+++ vlib/v/tests/match_const_range_test.v
@@ -1,11 +1,9 @@
 const (
-	start   = 1
-	start_2 = 4
-	end     = 3
-	end_2   = 8
-)
-
-const (
+	start        = 1
+	start_2      = 4
+	end          = 3
+	end_2        = 8
+	//
 	start_rune   = `a`
 	start_2_rune = `d`
 	end_rune     = `c`
@@ -18,46 +16,50 @@ fn test_match_int_const_ranges() {
 		match x {
 			start...end { results << 1 }
 			start_2...5 { results << 2 }
-			6...end_2 { results << -1 }
-			else { results << 0 }
+			6...end_2 { results << 3 }
+			else { results << 4 }
 		}
 	}
-	assert results == [0, 1, 1, 1, 2, 2, -1, -1, -1, 0]
+	assert results == [4, 1, 1, 1, 2, 2, 3, 3, 3, 4]
 }
 
 fn test_match_rune_const_ranges() {
-	mut results := []rune{}
+	mut results := []int{}
 	for x in `a` .. `l` {
 		match x {
-			start_rune...end_rune { results << `1` }
-			start_2_rune...`e` { results << `2` }
-			`f`...end_2_rune { results << `3` }
-			else { results << `0` }
+			start_rune...end_rune { results << 1 }
+			start_2_rune...`e` { results << 2 }
+			`f`...end_2_rune { results << 3 }
+			else { results << 4 }
 		}
 	}
-	assert results == [`1`, `1`, `1`, `2`, `2`, `3`, `3`, `3`, `3`, `0`, `0`]
+	assert results == [1, 1, 1, 2, 2, 3, 3, 3, 3, 4, 4]
 }
 
 fn test_match_expr_int_const_ranges() {
+	mut results := []int{}
 	for x in 0 .. 10 {
 		result := match x {
 			start...end { 1 }
 			start_2...5 { 2 }
-			6...end_2 { -1 }
-			else { 0 }
+			6...end_2 { 3 }
+			else { 4 }
 		}
-		assert result in [1, 2, -1, 0]
+		results << result
 	}
+	assert results == [4, 1, 1, 1, 2, 2, 3, 3, 3, 4]
 }
 
 fn test_match_expr_rune_const_ranges() {
+	mut results := []int{}
 	for x in `a` .. `l` {
 		result := match x {
-			start_rune...end_rune { `1` }
-			start_2_rune...`e` { `2` }
-			`f`...end_2_rune { `3` }
-			else { `0` }
+			start_rune...end_rune { 1 }
+			start_2_rune...`e` { 2 }
+			`f`...end_2_rune { 3 }
+			else { 4 }
 		}
-		assert result in [`1`, `2`, `3`, `0`]
+		results << result
 	}
+	assert results == [1, 1, 1, 2, 2, 3, 3, 3, 3, 4, 4]
 }
