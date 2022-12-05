83110204a7d088bc2245d5387423f82b1565622d
Delyan Angelov <delian66@gmail.com>
2022-12-05 09:59:29 +0200

    fix if expressions with several arr.any() calls as well

--- vlib/v/tests/if_expr_with_multi_array_call_test.v
+++ vlib/v/tests/if_expr_with_multi_array_call_test.v
@@ -14,3 +14,16 @@ fn foo() string {
 	}
 	return ''
 }
+
+fn test_if_expr_with_several_any_calls() {
+	x := [3, 4]
+	y := [2, 3, 4, 5]
+	if x.any(it in y) || y.any(it in x) {
+		println('then')
+		assert true
+	} else if x.any(it in y) {
+		println('else')
+		assert false
+	}
+	assert true
+}
