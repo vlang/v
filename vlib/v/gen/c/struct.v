9ef59a132776b9dde5a4c2c889bbe914ff023d20
Felipe Pena <felipensp@gmail.com>
2022-12-08 02:56:10 -0300

    fix checking

--- vlib/v/gen/c/struct.v
+++ vlib/v/gen/c/struct.v
@@ -44,7 +44,7 @@ fn (mut g Gen) struct_init(node ast.StructInit) {
 		is_anon = info.is_anon
 	}
 
-	if !(is_gcc && g.inside_struct_fields && !g.inside_call) && !is_anon {
+	if !is_gcc && !is_anon {
 		g.write('(')
 		defer {
 			g.write(')')
@@ -65,7 +65,7 @@ fn (mut g Gen) struct_init(node ast.StructInit) {
 		} else {
 			g.write('&(${basetyp}){')
 		}
-	} else if is_gcc && g.inside_struct_fields && !g.inside_call {
+	} else if is_gcc && g.inside_struct_fields {
 		if is_multiline {
 			g.writeln('{')
 		} else {
