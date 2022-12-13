48ec8699a3fe859930322094bea04e955dc7f19a
Subhomoy Haldar <hungrybluedev@gmail.com>
2022-12-13 18:23:18 +0000

    simplify definition

--- vlib/encoding/html/escape.v
+++ vlib/encoding/html/escape.v
@@ -2,15 +2,14 @@ module html
 
 [params]
 pub struct EscapeConfig {
-	input string
 	quote bool = true
 }
 
 // escape converts special characters in the input, specifically "<", ">", and "&"
 // to HTML-safe sequences. If `quote` is set to true (which is default), quotes in
 // HTML will also be translated. Both double and single quotes will be affected.
-pub fn escape(config EscapeConfig) string {
-	tag_free_input := config.input.replace_each(['&', '&amp;', '<', '&lt;', '>', '&gt;'])
+pub fn escape(input string, config EscapeConfig) string {
+	tag_free_input := input.replace_each(['&', '&amp;', '<', '&lt;', '>', '&gt;'])
 	return if config.quote {
 		tag_free_input.replace_each(['"', '&quot;', "'", '&#x27;'])
 	} else {
