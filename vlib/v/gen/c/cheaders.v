e89efef3756588c145329248bb732122a388d6a8
Delyan Angelov <delian66@gmail.com>
2022-12-22 10:41:29 +0000

    cgen: improve generated source compatibility with latest Alpine (lacking libexecinfo-dev and execinfo.h) and the prebuilt tcc

--- vlib/v/gen/c/cheaders.v
+++ vlib/v/gen/c/cheaders.v
@@ -318,6 +318,14 @@ const c_common_macros = '
 	#define EMPTY_STRUCT_INITIALIZATION 0
 #endif
 
+#ifndef _WIN32
+	#if defined __has_include
+		#if __has_include (<execinfo.h>)
+			#include <execinfo.h>
+		#endif
+	#endif
+#endif
+		      
 #ifdef __TINYC__
 	#define _Atomic volatile
 	#undef EMPTY_STRUCT_DECLARATION
@@ -333,7 +341,6 @@ const c_common_macros = '
 	#define TCCSKIP(x)
 	// #include <byteswap.h>
 	#ifndef _WIN32
-		#include <execinfo.h>
 		int tcc_backtrace(const char *fmt, ...);
 	#endif
 #endif
@@ -500,14 +507,6 @@ typedef int (*qsort_callback_func)(const void*, const void*);
 #include <stdio.h>  // TODO remove all these includes, define all function signatures and types manually
 #include <stdlib.h>
 #include <string.h>
-
-#ifndef _WIN32
-	#if defined __has_include
-		#if __has_include (<execinfo.h>)
-			#include <execinfo.h>
-		#endif
-	#endif
-#endif
 
 #include <stdarg.h> // for va_list
 
