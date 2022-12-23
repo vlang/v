07447494f1c516fb54b55ef43c4dda387d49c6cf
Felipe Pena <felipensp@gmail.com>
2022-12-23 12:53:37 -0300

    add int signess mismatch checking

--- vlib/v/checker/check_types.v
+++ vlib/v/checker/check_types.v
@@ -225,6 +225,13 @@ fn (mut c Checker) check_expected_call_arg(got ast.Type, expected_ ast.Type, lan
 			return
 		}
 	}
+	// check int signed/unsigned mismatch
+	if got == ast.int_literal_type_idx && expected in ast.unsigned_integer_type_idxs
+		&& arg.expr is ast.IntegerLiteral && (arg.expr as ast.IntegerLiteral).val.i64() < 0 {
+		_, expected_typ_str := c.get_string_names_of(got, expected)
+		return error('cannot use literal signed integer as `${expected_typ_str}`')
+	}
+
 	idx_got := got.idx()
 	idx_expected := expected.idx()
 	if idx_got in [ast.byteptr_type_idx, ast.charptr_type_idx]
