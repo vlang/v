236d4d699713cffff79bfc90616b6adbe6f36d8e
Delyan Angelov <delian66@gmail.com>
2022-12-31 11:06:33 +0200

    more cleanup of comptime_for_in_field_typeof_test.v

--- vlib/v/tests/comptime_for_in_field_typeof_test.v
+++ vlib/v/tests/comptime_for_in_field_typeof_test.v
@@ -9,6 +9,26 @@ struct ComplexStruct {
 	d map[string]int
 }
 
+fn test_typeof_in_comptime_for_in_fields() {
+	out := encode_struct(ComplexStruct{})
+	dump(out)
+	assert out[0] == 'a'
+	assert out[1] == typeof[int]().idx.str()
+	assert out[2] == 'int'
+
+	assert out[3] == 'b'
+	assert out[4] == typeof[[]Ab]().idx.str()
+	assert out[5] == '[]Ab'
+
+	assert out[6] == 'c'
+	assert out[7] == typeof[string]().idx.str()
+	assert out[8] == 'string'
+
+	assert out[9] == 'd'
+	assert out[10] == typeof[map[string]int]().idx.str()
+	assert out[11] == 'map[string]int'
+}
+
 fn encode_struct[T](val T) []string {
 	mut out := []string{}
 	$for field in T.fields {
@@ -18,22 +38,4 @@ fn encode_struct[T](val T) []string {
 		out << typeof(value).name
 	}
 	return out
-}
-
-fn test_typeof_in_comptime_for_in_fields() {
-	s := ComplexStruct{}
-	mut out := encode_struct(s)
-	dump(out)
-	assert out[0] == 'a'
-	assert out[1] == typeof[int]().idx.str()
-	assert out[2] == typeof[int]().name
-	assert out[3] == 'b'
-	assert out[4] == typeof[[]Ab]().idx.str()
-	assert out[5] == typeof[[]Ab]().name
-	assert out[6] == 'c'
-	assert out[7] == typeof[string]().idx.str()
-	assert out[8] == typeof[string]().name
-	assert out[9] == 'd'
-	assert out[10] == typeof[map[string]int]().idx.str()
-	assert out[11] == typeof[map[string]int]().name
 }
