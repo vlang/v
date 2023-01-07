20d2f30cf67f041c39d9f3b8f464e5618ebeac54
Felipe Pena <felipensp@gmail.com>
2023-01-06 21:21:51 -0300

    fix test

--- vlib/v/tests/comptime_in_type_checking_test.v
+++ vlib/v/tests/comptime_in_type_checking_test.v
@@ -1,13 +1,13 @@
 fn check[T](val T) string {
 	$if T in [$Array, $Struct] {
 		return 'array or struct'
-	} $else $if T == $Int {
+	} $else $if T in [$Int, $Struct] {
 		return 'int'
-	} $else $if T == $Float {
+	} $else $if T in [$Float, $Struct] {
 		return 'f64'
-	} $else $if T == $Map {
+	} $else $if T in [$Map, $Struct] {
 		return 'map'
-	} $else $if T == $Sumtype {
+	} $else $if T in [$Sumtype, $Struct] {
 		return 'sumtype'
 	}
 	return ''
