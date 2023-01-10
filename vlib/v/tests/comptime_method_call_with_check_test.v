0ca4d8329e60fa4c17ec67f315c0db4e5202c8bc
Felipe Pena <felipensp@gmail.com>
2023-01-10 08:28:31 -0300

    fix test fmt

--- vlib/v/tests/comptime_method_call_with_check_test.v
+++ vlib/v/tests/comptime_method_call_with_check_test.v
@@ -6,22 +6,21 @@ fn (t TestStruct) one_arg(a string) string {
 }
 
 fn (t TestStruct) two_args(a string, b int) string {
-	println('$a:$b')
+	println('${a}:${b}')
 	return a
 }
 
 fn test_main() {
 	t := TestStruct{}
 	mut out := ''
-	$for method in TestStruct.methods { 
-    	$if method.name == 'one_arg' {
+	$for method in TestStruct.methods {
+		$if method.name == 'one_arg' {
 			res := t.$method(method.name)
 			out += res
-    	} $else $if method.name == 'two_args' {
-      	  	res := t.$method(method.name, 42) 
+		} $else $if method.name == 'two_args' {
+			res := t.$method(method.name, 42)
 			out += res
-    	}
+		}
 	}
-
 	assert out == 'one_argtwo_args'
 }
