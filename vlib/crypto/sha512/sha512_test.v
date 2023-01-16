84140d7747efc5b7b0b42ac665b200c3b344196b
MatejMagat305 <61238240+MatejMagat305@users.noreply.github.com>
2023-01-16 12:53:10 +0100

    reduce memory  leak s sha512

--- vlib/crypto/sha512/sha512_test.v
+++ vlib/crypto/sha512/sha512_test.v
@@ -6,3 +6,23 @@ import crypto.sha512
 fn test_crypto_sha512() {
 	assert sha512.sum512('This is a sha512 checksum.'.bytes()).hex() == '4143e55fcba7e39b20f62a1368e5eb28f64a8859458886117ac66027832e0f9f5263daec688c439d2d0fa07059334668d39e59543039703dbb7e03ec9da7f8d7'
 }
+
+fn test_crypto_sha256_writer() {
+	mut digest := sha512.new_digest(.sha512)
+	digest.write('This is a'.bytes()) or { assert false }
+	digest.write(' sha512 checksum.'.bytes()) or { assert false }
+	sum := digest.checksum()
+	assert sum.hex() == '4143e55fcba7e39b20f62a1368e5eb28f64a8859458886117ac66027832e0f9f5263daec688c439d2d0fa07059334668d39e59543039703dbb7e03ec9da7f8d7'
+}
+
+fn test_crypto_sha256_writer_reset() {
+	mut digest := sha512.new_digest(.sha512)
+	digest.write('This is a'.bytes()) or { assert false }
+	digest.write(' sha512 checksum.'.bytes()) or { assert false }
+	_ = digest.checksum()
+	digest.reset()
+	digest.write('This is a'.bytes()) or { assert false }
+	digest.write(' sha512 checksum.'.bytes()) or { assert false }
+	sum := digest.checksum()
+	assert sum.hex() == '4143e55fcba7e39b20f62a1368e5eb28f64a8859458886117ac66027832e0f9f5263daec688c439d2d0fa07059334668d39e59543039703dbb7e03ec9da7f8d7'
+}
