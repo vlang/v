8e022b57e9b48b168ff535ee034e2877d617ce1a
phoreverpheebs <phoreverphoebs@proton.me>
2023-01-19 06:53:43 +0900

    Add PEM to the standard library

---
+++ vlib/crypto/pem/pem.v
@@ -0,0 +1,101 @@
+// Implements PEM (Privacy Enhanced Mail) data encoding
+// according to RFC 1421
+module pem
+
+const (
+	pem_begin = '-----BEGIN '
+	pem_end   = '\n-----END '
+	pem_eol   = '-----'
+	colon     = ':'
+)
+
+pub struct EncodeConfig {
+pub mut:
+	// inner text wrap around
+	line_length int = 64
+	// line ending (alternatively '\r\n')
+	line_ending string = '\n'
+}
+
+// Headers as described in RFC 1421 Section 9
+pub enum Header {
+	proctype
+	contentdomain
+	dekinfo
+	origid_asymm
+	origid_symm
+	recipid_asymm
+	recipid_symm
+	cert
+	issuercert
+	micinfo
+	keyinfo
+	crl
+}
+
+pub fn (header Header) str() string {
+	return match header {
+		.proctype { 'Proc-Type' }
+		.contentdomain { 'Content-Domain' }
+		.dekinfo { 'DEK-Info' }
+		.origid_asymm { 'Originator-ID-Asymmetric' }
+		.origid_symm { 'Originator-ID-Symmetric' }
+		.recipid_asymm { 'Recipient-ID-Asymmetric' }
+		.recipid_symm { 'Recipient-ID-Symmetric' }
+		.cert { 'Originator-Certificate' }
+		.issuercert { 'Issuer-Certificate' }
+		.micinfo { 'MIC-Info' }
+		.keyinfo { 'Key-Info' }
+		.crl { 'CRL' }
+	}
+}
+
+pub struct Block {
+pub mut:
+	// from preamble
+	block_type string
+	// optional headers
+	headers map[string][]string
+	// decoded contents
+	data []u8
+}
+
+// returns a new `Block` with the specified block_type
+[inline]
+pub fn new(block_type string) Block {
+	return Block{
+		block_type: block_type
+	}
+}
+
+// returns a map of headers
+//
+// same as `block.headers`
+[inline]
+pub fn (block Block) headers() map[string][]string {
+	return block.headers
+}
+
+// returns the selected key from headers
+//
+// same as `block.headers[key]`
+[inline]
+pub fn (block Block) header_by_str(key string) []string {
+	return block.headers[key]
+}
+
+// returns the selected key using the Header enum
+//
+// same as `block.headers[key.str()]`
+[inline]
+pub fn (block Block) header_by_key(key Header) []string {
+	return block.headers[key.str()]
+}
+
+// returns the block data
+//
+// same as `block.data`
+[inline]
+pub fn (block Block) data() []u8 {
+	return block.data
+}
