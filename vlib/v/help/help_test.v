8e221426854a02223c9ed3956d1063cebd819721
Christopher Fore <contact@chrisfore.us>
2023-01-29 00:55:50 -0500

    Add unit test for checking if the commands are there

--- vlib/v/help/help_test.v
+++ vlib/v/help/help_test.v
@@ -19,3 +19,22 @@ fn test_help_as_long_option() {
 	assert res.exit_code == 0
 	assert res.output.starts_with('V is a tool for managing V source code.')
 }
+
+fn test_all_help() {
+	vroot := os.dir(vexe)
+	mut topics := []string{}
+	mut ptopics := &topics
+	topicdir := os.join_path(vroot, 'vlib', 'v', 'help')
+
+	os.walk(topicdir, fn [mut ptopics] (topic string) {
+		if os.file_ext(topic) == '.txt' {
+			ptopics << os.file_name(topic).replace('.txt', '').replace('default,', '')
+		}
+	})
+
+	for topic in topics {
+		res := os.execute('${os.quoted_path(vexe)} help $topic')
+		assert res.exit_code == 0
+		assert res.output != ''
+	}
+}
