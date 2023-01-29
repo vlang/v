2e3de962960272a74d006212736ec2283c61fbb1
Christopher Fore <contact@chrisfore.us>
2023-01-28 20:33:34 -0500

    Fix the VPM help issue

---
+++ cmd/tools/modules/vhelp/categories.v
@@ -0,0 +1,57 @@
+module vhelp
+
+const (
+	categories = {
+		'build':       [
+			'build-c',
+			'build-js',
+			'build-native',
+		]
+		'common':      [
+			'doc',
+			'fmt',
+			'missdoc',
+			'repl',
+			'run',
+			'test',
+			'vet',
+			'watch',
+			'where',
+		]
+		'installation':     [
+			'self',
+			'symlink',
+			'up',
+			'version',
+		]
+		'other':       [
+			'ast',
+			'bin2v',
+			'bug',
+			'bump',
+			'check-md',
+			'complete',
+			'doctor',
+			'gret',
+			'ls',
+			'other',
+			'shader',
+			'tracev',
+		]
+		'scaffolding': [
+			'init',
+			'new',
+		]
+		'vpm':         [
+			'install',
+			'list',
+			'outdated',
+			'remove',
+			'search',
+			'show',
+			'update',
+			'upgrade',
+			'vpm',
+		]
+	}
+)
