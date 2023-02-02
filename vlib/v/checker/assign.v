462fdaee524645036d06568eb398f4becfa94f1f
Delta456 <swstkbaranwal@gmail.com>
2023-02-02 19:22:27 +0530

    refactor

--- vlib/v/checker/assign.v
+++ vlib/v/checker/assign.v
@@ -18,7 +18,7 @@ fn (mut c Checker) assign_stmt(mut node ast.AssignStmt) {
 	mut right_type0 := ast.void_type
 	for i, mut right in node.right {
 		if right in [ast.CallExpr, ast.IfExpr, ast.LockExpr, ast.MatchExpr, ast.DumpExpr,
-			ast.SelectorExpr] {
+			ast.SelectorExpr, ast.ParExpr] {
 			if right in [ast.IfExpr, ast.MatchExpr] && node.left.len == node.right.len && !is_decl
 				&& node.left[i] in [ast.Ident, ast.SelectorExpr] && !node.left[i].is_blank_ident() {
 				c.expected_type = c.expr(node.left[i])
@@ -79,22 +79,18 @@ fn (mut c Checker) assign_stmt(mut node ast.AssignStmt) {
 			}
 			c.error('assignment mismatch: ${node.left.len} variable(s) but `${right_first.name}()` returns ${right_len} value(s)',
 				node.pos)
+		} else if right_first is ast.ParExpr {
+			if right_first.expr is ast.CallExpr {
+				if right_first.expr.return_type == ast.void_type {
+					c.error('assignment mismatch: ${node.left.len} variable(s) but `${right_first.expr.name}()` returns ${right_len} value(s)',
+						node.pos)
+				}
+			}
 		} else {
 			c.error('assignment mismatch: ${node.left.len} variable(s) ${right_len} value(s)',
 				node.pos)
 		}
 		return
-	}
-
-	if right_first is ast.ParExpr {
-		if right_first.expr is ast.CallExpr {
-			if node.left_types.len > 0 && node.left_types[0] == ast.void_type {
-				// If it's a void type, it's an unknown variable, already had an error earlier.
-				return
-			}
-			c.error('assignment mismatch: ${node.left.len} variable(s) but `${right_first.expr.name}()` returns 0 value(s)',
-				node.pos)
-		}
 	}
 
 	for i, mut left in node.left {
