339472ec6caf1764a37ab06c904a8875c002afc1
Kyle Lin <minecraft.kyle.train@gmail.com>
2023-02-02 17:22:26 +0800

    forbid variable name declare same as import symbol

--- vlib/v/checker/assign.v
+++ vlib/v/checker/assign.v
@@ -304,6 +304,10 @@ fn (mut c Checker) assign_stmt(mut node ast.AssignStmt) {
 								c.warn('duplicate of a const name `${full_name}`', left.pos)
 							}
 						}
+						// Check if variable name is already registered as imported module symbol
+						if c.file.imports.any(it.mod == left.name) {
+							c.error('duplicate of a import symbol `${left.name}`', left.pos)
+						}
 					}
 				}
 			}
