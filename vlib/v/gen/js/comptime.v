fde167898e96fe298b5f3dd2bdd6d037e183cc79
MatejMagat305 <61238240+MatejMagat305@users.noreply.github.com>
2023-02-03 18:33:58 +0100

    fix ignore stack size from js

--- vlib/v/gen/js/comptime.v
+++ vlib/v/gen/js/comptime.v
@@ -4,9 +4,6 @@ import v.ast
 import v.pref
 
 fn (mut g JsGen) comptime_if(node ast.IfExpr) {
-	if node.method_name == 'stack_size' {
-		return
-	}
 	if !node.is_expr && !node.has_else && node.branches.len == 1 {
 		if node.branches[0].stmts.len == 0 {
 			// empty ifdef; result of target OS != conditional => skip
