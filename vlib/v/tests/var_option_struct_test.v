2a63e973edadb8753820c292dc3f7021f6c840e5
Felipe Pena <felipensp@gmail.com>
2023-02-06 17:21:51 -0300

    allow access field of Option

--- vlib/v/tests/var_option_struct_test.v
+++ vlib/v/tests/var_option_struct_test.v
@@ -3,12 +3,28 @@ mut:
 	name string
 }
 
+struct Test {
+pub mut:
+	a string
+	b ?string
+}
+
 fn (mut f Foo) test(arr ?[]int) ?string {
 	return arr?.len.str()
 }
 
 fn (mut f Foo) test2() ?string {
 	return none
+}
+
+fn fn_a(s ?Test) ? {
+	println(s?.a)
+	assert false
+}
+
+fn fn_b(s ?Test) ? {
+	println(s?.b?)
+	assert false
 }
 
 fn test_main() {
@@ -19,4 +35,10 @@ fn test_main() {
 	assert v == '3'
 	var := m?.test2() or { '' }
 	assert var == ''
+}
+
+fn test_opt_call() {
+	mut t := ?Test{}
+	fn_a(none) // returns none
+	fn_b(t) // returs none
 }
