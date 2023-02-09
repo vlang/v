9dc4308d4c8f5a4c3f9849184e8445fad6c52358
Riccardo <68387948+RIC217@users.noreply.github.com>
2023-02-09 21:07:53 +0100

    Optimised patch

--- vlib/v/builder/cc.v
+++ vlib/v/builder/cc.v
@@ -196,12 +196,12 @@ fn (mut v Builder) setup_ccompiler_options(ccompiler string) {
 			}
 		}
 	}
-	//
-	ccoptions.is_cc_tcc = os.file_name(ccompiler).contains('tcc') || ccoptions.guessed_compiler == 'tcc'
-	ccoptions.is_cc_gcc = os.file_name(ccompiler).contains('gcc') || ccoptions.guessed_compiler == 'gcc'
-	ccoptions.is_cc_icc = os.file_name(ccompiler).contains('icc') || ccoptions.guessed_compiler == 'icc'
-	ccoptions.is_cc_msvc = os.file_name(ccompiler).contains('msvc') || ccoptions.guessed_compiler == 'msvc'
-	ccoptions.is_cc_clang = os.file_name(ccompiler).contains('clang') || ccoptions.guessed_compiler == 'clang'
+	ccompiler_file_name := os.file_name(ccompiler)
+	ccoptions.is_cc_tcc = ccompiler_file_name.contains('tcc') || ccoptions.guessed_compiler == 'tcc'
+	ccoptions.is_cc_gcc = ccompiler_file_name.contains('gcc') || ccoptions.guessed_compiler == 'gcc'
+	ccoptions.is_cc_icc = ccompiler_file_name.contains('icc') || ccoptions.guessed_compiler == 'icc'
+	ccoptions.is_cc_msvc = ccompiler_file_name.contains('msvc') || ccoptions.guessed_compiler == 'msvc'
+	ccoptions.is_cc_clang = ccompiler_file_name.contains('clang') || ccoptions.guessed_compiler == 'clang'
 	// For C++ we must be very tolerant
 	if ccoptions.guessed_compiler.contains('++') {
 		ccoptions.args << '-fpermissive'
