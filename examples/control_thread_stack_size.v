7344740b62a38d328f64625b947c506f0d9acce6
MatejMagat305 <61238240+MatejMagat305@users.noreply.github.com>
2023-02-12 13:36:16 +0100

    Update control_thread_stack_size.v

--- examples/control_thread_stack_size.v
+++ examples/control_thread_stack_size.v
@@ -1,19 +1,36 @@
+
 module main
 
 fn main() {
-	th := spawn my_print1()
-	th.wait()
+	th := spawn my_print()
+	th.wait()	
+	th1 := spawn my_print1()
+	th1.wait()
 	th2 := spawn my_print2()
 	th2.wait()
+	th3 := spawn my_print3()
+	th3.wait()
 }
 
 // default stack size
+fn my_print() {
+	println('hi')
+}
+
+// 32MB  stack size
+[spawn_stack: 32M]
 fn my_print1() {
 	println('hello word')
 }
 
-// 2MB  stack size
-[spawn_stack: 2097152]
+// 64KB  stack size
+[spawn_stack: 65536]
 fn my_print2() {
 	println('ahoj svet')
+}
+
+// 128KB  stack size
+[spawn_stack: 128K]
+fn my_print3() {
+	println('здравей свят')
 }
