48dc4de2bf4a505d1292b53e44c630c6fed8443a
yuyi <yuyi98@163.com>
2023-02-17 21:35:24 +0800

    fix markused for array methods

--- vlib/v/markused/markused.v
+++ vlib/v/markused/markused.v
@@ -22,7 +22,6 @@ pub fn mark_used(mut table ast.Table, pref_ &pref.Preferences, ast_files []&ast.
 		'__new_array_with_default',
 		'__new_array_with_defaults',
 		'__new_array_with_array_default',
-		'__new_array_with_map_default',
 		'init_global_allocator' /* needed for linux_bare and wasm_bare */,
 		'v_realloc' /* needed for _STR */,
 		'malloc',
