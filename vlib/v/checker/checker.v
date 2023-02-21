1cd9d23e4a69a0d6f45c473ad614b6297dd016f2
Felipe Pena <felipensp@gmail.com>
2023-02-21 19:52:05 -0300

    fix cycle const error message

--- vlib/v/checker/checker.v
+++ vlib/v/checker/checker.v
@@ -3118,7 +3118,7 @@ fn (mut c Checker) ident(mut node ast.Ident) ast.Type {
 					return c.expected_type
 				}
 			}
-			c.error('cycle in constant `${c.const_var}`', node.pos)
+			c.error('cycle in constant `${c.const_var.name}`', node.pos)
 			return ast.void_type
 		}
 		c.const_deps << name
