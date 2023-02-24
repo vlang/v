1a1a8745a8760b4f12c2292e247ff5368f9fc396
Felipe Pena <felipensp@gmail.com>
2023-02-24 12:43:31 -0300

    improve test

--- vlib/v/tests/option_ptr_test.v
+++ vlib/v/tests/option_ptr_test.v
@@ -13,13 +13,33 @@ fn test_simple_writing() {
 	assert val == 321
 }
 
+fn test_simple_deref() ? {
+	val := 123
+	mut var := unsafe { ?&int(&val) }
+	assert *var? == val
+	mut r := var?
+	unsafe {
+		*r = 321
+	}
+	assert val == 321
+}
+
+fn f_ref(val ?&int) ? {
+	t := 123
+	assert *val? == t
+}
+
 fn test_simple_writing2() {
 	val := 123
 	mut var := unsafe { ?&int(&val) }
-	// unsafe { var = 0 } // error
 	unsafe {
 		*var? = 321
 	}
 	x := *var?
 	assert val == x
+}
+
+fn test_simple_fn() {
+	var := 123
+	f_ref(&var)
 }
