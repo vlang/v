6f7192359a9227bdd13ad167838fc43a321c5105
l-m <l-m@l-m.dev>
2023-03-04 20:24:33 +1100

    wasm: bug fixes and memory based changes (#17497)

--- vlib/builtin/wasm/alloc.v
+++ vlib/builtin/wasm/alloc.v
@@ -4,17 +4,7 @@ module builtin
 // Shitty `sbrk` basic `malloc` and `free` impl
 // TODO: implement pure V `walloc` later
 
-const wasm_page_size = 64 * 1024
-
-__global g_heap_base = isize(0)
-
-fn init() {
-	g_heap_base = __memory_grow(3)
-	if g_heap_base == -1 {
-		panic('g_heap_base: malloc() == nil')
-	}
-	g_heap_base *= wasm_page_size
-}
+__global g_heap_base = usize(__heap_base())
 
 // malloc dynamically allocates a `n` bytes block of memory on the heap.
 // malloc returns a `byteptr` pointing to the memory address of the allocated space.
@@ -26,7 +16,7 @@ pub fn malloc(n isize) &u8 {
 	}
 
 	res := g_heap_base
-	g_heap_base += n
+	g_heap_base += usize(n)
 
 	return &u8(res)
 }
@@ -36,46 +26,4 @@ pub fn malloc(n isize) &u8 {
 [unsafe]
 pub fn free(ptr voidptr) {
 	_ := ptr
-}
-
-// vcalloc dynamically allocates a zeroed `n` bytes block of memory on the heap.
-// vcalloc returns a `byteptr` pointing to the memory address of the allocated space.
-// Unlike `v_calloc` vcalloc checks for negative values given in `n`.
-[unsafe]
-pub fn vcalloc(n isize) &u8 {
-	if n <= 0 {
-		panic('vcalloc(n <= 0)')
-	} else if n == 0 {
-		return &u8(0)
-	}
-
-	res := unsafe { malloc(n) }
-
-	__memory_fill(res, 0, n)
-
-	return res
-}
-
-// vmemcpy copies n bytes from memory area src to memory area dest.
-// The memory areas **CAN** overlap. vmemcpy returns a pointer to `dest`.
-[unsafe]
-pub fn vmemcpy(dest voidptr, const_src voidptr, n isize) voidptr {
-	__memory_copy(dest, const_src, n)
-	return dest
-}
-
-// vmemmove copies n bytes from memory area src to memory area dest.
-// The memory areas **CAN** overlap. vmemmove returns a pointer to `dest`.
-[unsafe]
-pub fn vmemmove(dest voidptr, const_src voidptr, n isize) voidptr {
-	__memory_copy(dest, const_src, n)
-	return dest
-}
-
-// vmemset fills the first `n` bytes of the memory area pointed to by `s`,
-// with the constant byte `c`. It returns a pointer to the memory area `s`.
-[unsafe]
-pub fn vmemset(s voidptr, c int, n isize) voidptr {
-	__memory_fill(s, c, n)
-	return s
 }
