e94395ee57a7eb716341fc7c027e6f98f9d1ceb3
Felipe Pena <felipensp@gmail.com>
2023-03-06 11:17:28 -0300

    fix

--- vlib/json/json_option_test.v
+++ vlib/json/json_option_test.v
@@ -1,5 +1,16 @@
 import json
 
+pub struct MyStruct {
+pub mut:
+	valuea int
+}
+
+pub struct MyStruct2 {
+pub mut:
+	valuea int
+	valueb ?MyStruct
+}
+
 struct Node {
 	location NodeLocation [json: 'loc']
 }
@@ -12,11 +23,43 @@ struct SourceFile {
 	path string [json: 'file']
 }
 
+fn test_encode_decode() {
+	assert json.encode(MyStruct2{ valuea: 1 }) == '{"valuea":1}'
+
+	assert json.decode(MyStruct2, '{"valuea": 1}')! == MyStruct2{
+		valuea: 1
+		valueb: none
+	}
+}
+
+fn test_encode_decode2() {
+	assert json.encode(MyStruct2{ valuea: 1, valueb: none }) == '{"valuea":1}'
+
+	assert json.decode(MyStruct2, '{"valuea": 1}')! == MyStruct2{
+		valuea: 1
+		valueb: none
+	}
+}
+
+fn test_encode_decode3() {
+	assert json.encode(MyStruct2{
+		valuea: 1
+		valueb: MyStruct{
+			valuea: 123
+		}
+	}) == '{"valuea":1,"valueb":{"valuea":123}}'
+
+	assert json.decode(MyStruct2, '{"valuea": 1}')! == MyStruct2{
+		valuea: 1
+		valueb: none
+	}
+}
+
 fn test_main() {
 	node := json.decode(Node, '{"loc": { "includedFrom": { "file": "/bin/foo" } } }')!
 
 	mut source_file := node.location.source_file or { none }
-	assert source_file.path == ''
+	assert source_file.path == '/bin/foo'
 
 	source_file = node.location.source_file or {
 		SourceFile{
@@ -24,5 +67,5 @@ fn test_main() {
 		}
 	}
 
-	assert source_file.path == '-'
+	assert source_file.path == '/bin/foo'
 }
