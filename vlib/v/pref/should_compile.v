c9a06d7a2517c95de49d938ab215b0c9e4ea9b2e
Thomas Mangin <thomas.mangin@exa.net.uk>
2023-03-12 13:07:40 +0000

    build: handle _qnx.c.v

--- vlib/v/pref/should_compile.v
+++ vlib/v/pref/should_compile.v
@@ -204,6 +204,9 @@ pub fn (prefs &Preferences) should_compile_c(file string) bool {
 	if prefs.os != .solaris && file.ends_with('_solaris.c.v') {
 		return false
 	}
+	if prefs.os != .qnx && file.ends_with('_qnx.c.v') {
+		return false
+	}
 	if prefs.os != .serenity && file.ends_with('_serenity.c.v') {
 		return false
 	}
