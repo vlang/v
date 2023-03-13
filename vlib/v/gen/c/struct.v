eb50e4a087f26ff732e36509183779ee8a55bf4f
yuyi <yuyi98@163.com>
2023-03-13 11:35:03 +0800

    cgen: fix option struct init with reference option

--- vlib/v/gen/c/struct.v
+++ vlib/v/gen/c/struct.v
@@ -547,10 +547,11 @@ fn (mut g Gen) struct_init_field(sfield ast.StructInitField, language ast.Langua
 				g.write('/* autoref */&')
 			}
 
-			if sfield.expected_type.has_flag(.option)
+			if (sfield.expected_type.has_flag(.option) && !sfield.typ.has_flag(.option))
 				|| (sfield.expected_type.has_flag(.result) && !sfield.typ.has_flag(.result)) {
 				g.expr_with_opt(sfield.expr, sfield.typ, sfield.expected_type)
 			} else {
+				g.left_is_opt = true
 				g.expr_with_cast(sfield.expr, sfield.typ, sfield.expected_type)
 			}
 		}
