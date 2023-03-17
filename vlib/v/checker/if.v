8535a4f4d3b72a281872c7b568684e752e43ca15
Felipe Pena <felipensp@gmail.com>
2023-03-17 15:20:48 -0300

    fix

--- vlib/v/checker/if.v
+++ vlib/v/checker/if.v
@@ -320,7 +320,8 @@ fn (mut c Checker) if_expr(mut node ast.IfExpr) ast.Type {
 						c.error('mismatched types `${c.table.type_to_str(node.typ)}` and `${c.table.type_to_str(stmt.typ)}`',
 							node.pos)
 					} else {
-						if c.inside_assign && stmt.typ.is_ptr() != node.typ.is_ptr() {
+						if node.is_expr && !node.typ.has_flag(.shared_f)
+							&& stmt.typ.is_ptr() != node.typ.is_ptr() {
 							c.error('mismatched types `${c.table.type_to_str(node.typ)}` and `${c.table.type_to_str(stmt.typ)}`',
 								node.pos)
 						}
