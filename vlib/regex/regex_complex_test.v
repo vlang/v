7c1f7754048eed9c78f8ea7d6122fb6fd915c0f4
penguindark <dariodeledda@hotmail.com>
2023-03-25 17:58:37 +0000

    fix and test for issue from PR #17701

--- vlib/regex/regex_complex_test.v
+++ vlib/regex/regex_complex_test.v
@@ -17,3 +17,24 @@ empty_const   = ()
 	mut a := re.find_all(tmp_text)
 	assert a == [86, 138]
 }
+
+fn test_complex_matching2() {
+	content := '<?xml version="1.0" encoding="utf-8"?>
+<resources>
+    <string name="v_app_name">V0</string>
+    <string name="v_lib_name">v1</string>
+    <string name="v_lib_name2">v2</string>
+</resources>
+'
+
+mut re := regex.regex_opt(r'.*<resources>.+<string name="v_lib_name">([^<]+)') or {
+        panic(err)
+    }
+    
+    start, end := re.match_string(content)
+    if start >= 0  && re.groups.len > 0 {
+    	assert content#[re.groups[0]..re.groups[1]] == 'v1'
+    	return
+    }
+    assert false
+}
