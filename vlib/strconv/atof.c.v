6756d28595e169d9a1bd802b5b33f645d714cd26
Alexander Medvednikov <alexander@vlang.io>
2023-03-28 22:55:57 +0200

    all: 2023 copyright

--- vlib/strconv/atof.c.v
+++ vlib/strconv/atof.c.v
@@ -1,6 +1,6 @@
 module strconv
 
-// Copyright (c) 2019-2022 Dario Deledda. All rights reserved.
+// Copyright (c) 2019-2023 Dario Deledda. All rights reserved.
 // Use of this source code is governed by an MIT license
 // that can be found in the LICENSE file.
 //
