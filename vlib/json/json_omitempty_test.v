e4c8f5595045eb5462fbe528bc5bddf29e146c61
Felipe Pena <felipensp@gmail.com>
2023-03-29 15:03:36 -0300

    improve test

--- vlib/json/json_omitempty_test.v
+++ vlib/json/json_omitempty_test.v
@@ -4,15 +4,22 @@ pub struct MyStruct {
 pub mut:
 	code    int
 	message string
-	data    string [omitempty]
-	// data2 ?string [omitempty]
+	data    string  [omitempty]
+	data2   ?string [omitempty]
 }
 
 fn test_simple() {
 	obj := MyStruct{
 		code: 1
 		message: 'yes'
-		// data2: "a"
+		data2: 'a'
 	}
-	dump(json.encode(obj))
+	assert dump(json.encode(obj)) == '{"code":1,"message":"yes","data2":"a"}'
+}
+
+fn test_none() {
+	obj := MyStruct{
+		code: 1
+	}
+	assert dump(json.encode(obj)) == '{"code":1,"message":""}'
 }
