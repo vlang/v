88de0decf6f377b74581ddfc384376b3fe19ea8b
Delyan Angelov <delian66@gmail.com>
2023-04-03 05:07:12 +0300

    v.pref: allow passing file names to v scripts, that use -raw-vsh-tmp-prefix, without showing a `Too many targets` error message.

--- vlib/v/pref/pref.v
+++ vlib/v/pref/pref.v
@@ -811,7 +811,7 @@ pub fn parse_args_and_show_errors(known_external_commands []string, args []strin
 							break
 						}
 					} else if is_source_file(command) && is_source_file(arg)
-						&& command !in known_external_commands {
+						&& command !in known_external_commands && res.raw_vsh_tmp_prefix == '' {
 						eprintln('Too many targets. Specify just one target: <target.v|target_directory>.')
 						exit(1)
 					}
