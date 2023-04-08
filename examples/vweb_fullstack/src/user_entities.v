756e5d931eb47ad278edf8c02300f8caab49d1e1
l-33ter <77815858+l-33ter@users.noreply.github.com>
2023-04-08 16:18:48 +0200

    examples/vweb_fullstack: fix wrong db ORM-fields (#17907)

--- examples/vweb_fullstack/src/user_entities.v
+++ examples/vweb_fullstack/src/user_entities.v
@@ -4,8 +4,8 @@ module main
 pub struct User {
 mut:
 	id       int       [primary; sql: serial]
-	username string    [required; sql_type: 'TEXT'; unique]
-	password string    [required; sql_type: 'TEXT']
+	username string    [nonull; sql_type: 'TEXT'; unique]
+	password string    [nonull; sql_type: 'TEXT']
 	active   bool
 	products []Product [fkey: 'user_id']
 }
