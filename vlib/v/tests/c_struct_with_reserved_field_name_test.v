cd64cd4a407bd810e9c480643326d5a806d460d2
yuyi <yuyi98@163.com>
2023-04-20 13:50:37 +0800

    cgen: fix c struct with reserved field name

---
+++ vlib/v/tests/c_struct_with_reserved_field_name_test.v
@@ -0,0 +1,22 @@
+import gg
+import gx
+
+struct Game {
+mut:
+	gg ?gg.Context
+}
+
+fn test_c_struct_with_reserved_field_name() {
+	mut game := Game{
+		gg: none
+	}
+	mut cont := gg.new_context(
+		bg_color: gx.rgb(174, 198, 255)
+		width: 600
+		height: 400
+		window_title: 'Polygons'
+	)
+	game.gg = cont
+	println(game.gg)
+	assert true
+}
