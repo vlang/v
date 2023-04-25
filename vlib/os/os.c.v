b319463f29cd1c0f82746a97a48438ba7c19339c
Felipe Pena <felipensp@gmail.com>
2023-04-25 10:10:33 -0300

    fix

--- vlib/os/os.c.v
+++ vlib/os/os.c.v
@@ -571,7 +571,8 @@ pub fn get_raw_line() string {
 		max := usize(0)
 		buf := &char(0)
 		nr_chars := unsafe { C.getline(&buf, &max, C.stdin) }
-		ret := unsafe { tos_clone(&u8(buf)) }
+		str := unsafe { tos(&u8(buf), if nr_chars < 0 { 0 } else { nr_chars }) }
+		ret := str.clone()
 		unsafe {
 			if nr_chars > 0 && buf != 0 {
 				C.free(buf)
