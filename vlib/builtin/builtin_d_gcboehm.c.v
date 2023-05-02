b6631b6b6ce1ca53f625f4594cf9c28be5343a59
Felipe Pena <felipensp@gmail.com>
2023-05-02 19:34:45 -0300

    remove unused

--- vlib/builtin/builtin_d_gcboehm.c.v
+++ vlib/builtin/builtin_d_gcboehm.c.v
@@ -135,6 +135,5 @@ pub fn gc_check_leaks() {
 
 fn C.GC_get_heap_size() usize
 fn C.GC_get_free_bytes() usize
-fn C.GC_get_bytes_since_gc() usize
 fn C.GC_get_memory_use() usize
 fn C.GC_get_total_bytes() usize
