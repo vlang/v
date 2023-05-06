42a051beb09b43cd767bc58f42544aa99aca6e23
squidink7 <squidinkrf@gmail.com>
2023-05-06 10:13:34 +0930

    Warn instead of error for unnessecary brackets

--- vlib/v/checker/match.v
+++ vlib/v/checker/match.v
@@ -9,7 +9,7 @@ fn (mut c Checker) match_expr(mut node ast.MatchExpr) ast.Type {
 	node.is_expr = c.expected_type != ast.void_type
 	node.expected_type = c.expected_type
 	if mut node.cond is ast.ParExpr && !c.pref.translated && !c.file.is_translated {
-		c.error('unnecessary `()` in `match` condition, use `match expr {` instead of `match (expr) {`.',
+		c.warn('unnecessary `()` in `match` condition, use `match expr {` instead of `match (expr) {`.',
 			node.cond.pos)
 	}
 	if node.is_expr {
