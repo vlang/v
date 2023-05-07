9e578ccab3a68986fe88a6d0afebaef3106b1b85
Turiiya <34311583+tobealive@users.noreply.github.com>
2023-05-08 01:34:18 +0200

    net.http: fix chunked decoding of response body

--- vlib/net/http/chunked/dechunk.v
+++ vlib/net/http/chunked/dechunk.v
@@ -1,6 +1,6 @@
 module chunked
 
-import strings
+import io
 // See: https://en.wikipedia.org/wiki/Chunked_transfer_encoding
 // /////////////////////////////////////////////////////////////
 // The chunk size is transferred as a hexadecimal number
@@ -9,64 +9,32 @@ import strings
 // The end is marked with a chunk with size 0.
 
 struct ChunkScanner {
-mut:
-	pos  int
 	text string
+mut:
+	pos int
 }
 
-fn (mut s ChunkScanner) read_chunk_size() u32 {
-	mut n := u32(0)
-	for {
-		if s.pos >= s.text.len {
-			break
-		}
-		c := s.text[s.pos]
-		if !c.is_hex_digit() {
-			break
-		}
-		n = n << 4
-		n += u32(unhex(c))
-		s.pos++
+fn (mut s ChunkScanner) read(mut buf []u8) !int {
+	if s.pos >= s.text.len {
+		return io.Eof{}
 	}
+	max_bytes := 100
+	end := if s.pos + max_bytes >= s.text.len { s.text.len } else { s.pos + max_bytes }
+	n := copy(mut buf, s.text[s.pos..end].bytes())
+	s.pos += n
 	return n
 }
 
-fn unhex(c u8) u8 {
-	if `0` <= c && c <= `9` {
-		return c - `0`
-	} else if `a` <= c && c <= `f` {
-		return c - `a` + 10
-	} else if `A` <= c && c <= `F` {
-		return c - `A` + 10
-	}
-	return 0
-}
-
-fn (mut s ChunkScanner) skip_crlf() {
-	s.pos += 2
-}
-
-fn (mut s ChunkScanner) read_chunk(chunksize u32) string {
-	startpos := s.pos
-	s.pos += int(chunksize)
-	return s.text[startpos..s.pos]
+fn reader(s string) &io.BufferedReader {
+	return io.new_buffered_reader(
+		reader: &ChunkScanner{
+			text: s
+		}
+	)
 }
 
 pub fn decode(text string) string {
-	mut sb := strings.new_builder(100)
-	mut cscanner := ChunkScanner{
-		pos: 0
-		text: text
-	}
-	for {
-		csize := cscanner.read_chunk_size()
-		if 0 == csize {
-			break
-		}
-		cscanner.skip_crlf()
-		sb.write_string(cscanner.read_chunk(csize))
-		cscanner.skip_crlf()
-	}
-	cscanner.skip_crlf()
-	return sb.str()
+	mut reader_ := reader(text)
+	sb := io.read_all(reader: reader_) or { ''.bytes() }
+	return sb.bytestr()
 }
