9fda30a6b971efad65143c08b52a40b9b089242b
l1mey112 <l-m@l-m.dev>
2023-05-07 22:07:16 +1000

    add copyrights

--- vlib/wasm/constant.v
+++ vlib/wasm/constant.v
@@ -1,3 +1,6 @@
+// Copyright (c) 2023 l-m.dev. All rights reserved.
+// Use of this source code is governed by an MIT license
+// that can be found in the LICENSE file.
 module wasm
 
 import encoding.leb128
