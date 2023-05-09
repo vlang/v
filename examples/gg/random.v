c7472e013de10b3db6e4099bfcba9e4c1856c891
Swastik Baranwal <swstkbaranwal@gmail.com>
2023-05-09 23:58:38 +0530

    oops

--- examples/gg/random.v
+++ examples/gg/random.v
@@ -30,7 +30,7 @@ fn (mut state AppState) update() {
 
 fn (mut state AppState) draw() {
 	mut istream_image := state.gg.get_cached_image_by_idx(state.istream_idx)
-	istream_image.update_pixel_data(&u8((&state.pixels))
+	istream_image.update_pixel_data(&u8((&state.pixels)))
 	size := gg.window_size()
 	state.gg.draw_image(0, 0, size.width, size.height, istream_image)
 }
