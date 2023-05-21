4b22ea780349f02a3f25452a63662825a5663ca4
Swastik Baranwal <swstkbaranwal@gmail.com>
2023-05-21 18:51:57 +0530

    parser: disallow all `array` usage outside of builtin (#18222)

--- vlib/v/parser/parse_type.v
+++ vlib/v/parser/parse_type.v
@@ -489,6 +489,11 @@ pub fn (mut p Parser) parse_type() ast.Type {
 	if is_atomic {
 		typ = typ.set_flag(.atomic_f)
 	}
+	if typ.idx() == ast.array_type && !p.builtin_mod && p.mod !in ['os', 'strconv', 'sync']
+		&& !p.inside_unsafe {
+		p.error_with_pos('`array` is an internal type, it cannot be used directly. Use `[]int`, `[]Foo` etc',
+			pos)
+	}
 	if nr_muls > 0 {
 		typ = typ.set_nr_muls(nr_muls)
 		if is_array && nr_amps > 0 {
