65821b543e3fb949ed929e8bf3413b9a79531007
Delyan Angelov <delian66@gmail.com>
2023-05-21 20:44:41 +0300

    Revert "rename htn to hton, nth to ntoh"
    This reverts commit 34e263502fb26ef2f5d0e4ccae6f9cdf9773d867.

--- vlib/net/conv/conv.v
+++ vlib/net/conv/conv.v
@@ -3,7 +3,7 @@ module conv
 import encoding.binary
 
 // host to net 64 (htonll)
-pub fn hton64(host u64) u64 {
+pub fn htn64(host u64) u64 {
 	$if little_endian {
 		mut b := []u8{len: 8}
 		binary.little_endian_put_u64(mut b, host)
@@ -14,7 +14,7 @@ pub fn hton64(host u64) u64 {
 }
 
 // host to net 32 (htonl)
-pub fn hton32(host u32) u32 {
+pub fn htn32(host u32) u32 {
 	$if little_endian {
 		mut b := []u8{len: 4}
 		binary.big_endian_put_u32(mut b, host)
@@ -25,7 +25,7 @@ pub fn hton32(host u32) u32 {
 }
 
 // host to net 16 (htons)
-pub fn hton16(host u16) u16 {
+pub fn htn16(host u16) u16 {
 	$if little_endian {
 		mut b := []u8{len: 2}
 		binary.big_endian_put_u16(mut b, host)
@@ -36,16 +36,16 @@ pub fn hton16(host u16) u16 {
 }
 
 // net to host 64 (ntohll)
-pub fn ntoh64(net u64) u64 {
-	return hton64(net)
+pub fn nth64(net u64) u64 {
+	return htn64(net)
 }
 
 // net to host 32 (ntohl)
-pub fn ntoh32(net u32) u32 {
-	return hton32(net)
+pub fn nth32(net u32) u32 {
+	return htn32(net)
 }
 
 // net to host 16 (ntohs)
-pub fn ntoh16(net u16) u16 {
-	return hton16(net)
+pub fn nth16(net u16) u16 {
+	return htn16(net)
 }
--- vlib/net/conv/conv_test.v
+++ vlib/net/conv/conv_test.v
@@ -13,32 +13,32 @@ fn check[T](f fn (a T) T, finv fn (b T) T, x T) {
 	}
 }
 
-fn test_hton64_ntoh64() {
-	assert 0 == conv.hton64(0)
-	assert 0 == conv.ntoh64(0)
-	assert 0xFFFF_FFFF_FFFF_FFFF == conv.ntoh64(0xFFFF_FFFF_FFFF_FFFF)
-	assert 0xFFFF_FFFF_FFFF_FFFF == conv.hton64(0xFFFF_FFFF_FFFF_FFFF)
+fn test_htn64_nth64() {
+	assert 0 == conv.htn64(0)
+	assert 0 == conv.nth64(0)
+	assert 0xFFFF_FFFF_FFFF_FFFF == conv.nth64(0xFFFF_FFFF_FFFF_FFFF)
+	assert 0xFFFF_FFFF_FFFF_FFFF == conv.htn64(0xFFFF_FFFF_FFFF_FFFF)
 	for x in [u64(1), 2, 128, 65536, 2147483648] {
-		check(conv.hton64, conv.ntoh64, x)
+		check(conv.htn64, conv.nth64, x)
 	}
 }
 
-fn test_hton32_ntoh32() {
-	assert 0 == conv.hton32(0)
-	assert 0 == conv.ntoh32(0)
-	assert 0xFFFF_FFFF == conv.ntoh32(0xFFFF_FFFF)
-	assert 0x0101_0101 == conv.hton32(0x0101_0101)
+fn test_htn32_nth32() {
+	assert 0 == conv.htn32(0)
+	assert 0 == conv.nth32(0)
+	assert 0xFFFF_FFFF == conv.nth32(0xFFFF_FFFF)
+	assert 0x0101_0101 == conv.htn32(0x0101_0101)
 	for x in [u32(1), 2, 128, 65536, 2147483648] {
-		check(conv.hton32, conv.ntoh32, x)
+		check(conv.htn32, conv.nth32, x)
 	}
 }
 
-fn test_hton16_ntoh16() {
-	assert 0 == conv.hton16(0)
-	assert 0 == conv.ntoh16(0)
-	assert 0xFFFF == conv.ntoh16(0xFFFF)
-	assert 0x0101 == conv.hton16(0x0101)
+fn test_htn16_nth16() {
+	assert 0 == conv.htn16(0)
+	assert 0 == conv.nth16(0)
+	assert 0xFFFF == conv.nth16(0xFFFF)
+	assert 0x0101 == conv.htn16(0x0101)
 	for x in [u16(1), 2, 128, 65534] {
-		check(conv.hton16, conv.ntoh16, x)
+		check(conv.htn16, conv.nth16, x)
 	}
 }
