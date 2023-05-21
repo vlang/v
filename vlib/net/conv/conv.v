4419b393872a502f63421f7433f6cb6203d5d154
Delyan Angelov <delian66@gmail.com>
2023-05-21 21:01:45 +0300

    use bitops directly, instead of using the generic array oriented APIs in encoding.binary

--- vlib/net/conv/conv.v
+++ vlib/net/conv/conv.v
@@ -1,13 +1,20 @@
 module conv
 
-import encoding.binary
-
 // host to net 64 (htonll)
 pub fn htn64(host u64) u64 {
 	$if little_endian {
-		mut b := []u8{len: 8}
-		binary.little_endian_put_u64(mut b, host)
-		return binary.big_endian_u64(b)
+		// vfmt off
+		return (
+			((host >> 56) & 0x00000000_000000FF) | 
+			((host >> 40) & 0x00000000_0000FF00) | 
+			((host >> 24) & 0x00000000_00FF0000) | 
+			((host >> 8)  & 0x00000000_FF000000) |
+			((host << 8)  & 0x000000FF_00000000) | 
+			((host << 24) & 0x0000FF00_00000000) | 
+			((host << 40) & 0x00FF0000_00000000) | 
+			((host << 56) & 0xFF000000_00000000)
+		)
+		// vfmt on
 	} $else {
 		return host
 	}
@@ -16,9 +23,14 @@ pub fn htn64(host u64) u64 {
 // host to net 32 (htonl)
 pub fn htn32(host u32) u32 {
 	$if little_endian {
-		mut b := []u8{len: 4}
-		binary.big_endian_put_u32(mut b, host)
-		return binary.little_endian_u32(b)
+		// vfmt off
+		return (
+			((host >> 24) & 0x0000_00FF) | 
+			((host >> 8)  & 0x0000_FF00) | 
+			((host << 8)  & 0x00FF_0000) | 
+			((host << 24) & 0xFF00_0000)
+		)
+		// vfmt on
 	} $else {
 		return host
 	}
@@ -27,9 +39,12 @@ pub fn htn32(host u32) u32 {
 // host to net 16 (htons)
 pub fn htn16(host u16) u16 {
 	$if little_endian {
-		mut b := []u8{len: 2}
-		binary.big_endian_put_u16(mut b, host)
-		return binary.little_endian_u16(b)
+		// vfmt off
+		return (
+			((host >> 8) & 0x00FF) | 
+			((host << 8) & 0xFF00)
+		)
+		// vfmt on
 	} $else {
 		return host
 	}
