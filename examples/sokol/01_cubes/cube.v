08e8cfe23c3f5616c492220ab0cf2993c9ec43c2
Delyan Angelov <delian66@gmail.com>
2023-05-25 16:53:54 +0300

    Update examples/sokol/01_cubes/cube.v

--- examples/sokol/01_cubes/cube.v
+++ examples/sokol/01_cubes/cube.v
@@ -53,7 +53,7 @@ fn create_texture(w int, h int, buf &u8) gfx.Image {
 		label: &u8(0)
 		d3d11_texture: 0
 	}
-	// common if .dynamic is enabled
+	// comment, if .dynamic is enabled
 	img_desc.data.subimage[0][0] = gfx.Range{
 		ptr: buf
 		size: usize(sz)
