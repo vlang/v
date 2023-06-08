918eabf007377dc9a839506fa66449a3570be8ad
yuyi <yuyi98@163.com>
2023-06-08 11:38:28 +0800

    parser: fix generic function variable

--- vlib/v/parser/parser.v
+++ vlib/v/parser/parser.v
@@ -2140,7 +2140,13 @@ fn (mut p Parser) is_following_concrete_types() bool {
 		} else if cur_tok.kind == .rsbr {
 			break
 		} else if cur_tok.kind == .name {
-			if !(p.is_typename(cur_tok) && !(cur_tok.lit.len == 1 && !cur_tok.lit[0].is_capital())) {
+			if p.peek_token(i + 1).kind == .dot {
+				if p.is_typename(cur_tok) {
+					return false
+				}
+				i++
+			} else if !(p.is_typename(cur_tok) && !(cur_tok.lit.len == 1
+				&& !cur_tok.lit[0].is_capital())) {
 				return false
 			}
 		} else if cur_tok.kind != .comma {
