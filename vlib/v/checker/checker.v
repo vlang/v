ca663eeaa5ef154b96410556d0eeec0b1a2733c4
Turiiya <34311583+tobealive@users.noreply.github.com>
2023-06-13 07:51:01 +0200

    prefix line number with `on line`
    ...error should be orientated on other error message now

--- vlib/v/checker/checker.v
+++ vlib/v/checker/checker.v
@@ -169,12 +169,12 @@ pub fn (mut c Checker) check(ast_file_ &ast.File) {
 	c.change_current_file(ast_file)
 	for i, ast_import in ast_file.imports {
 		if c.mod == ast_import.mod {
-			c.error('duplicate of module name `${ast_import.mod}` ${ast_file.stmts[0].pos.line_nr +
-				1}', ast_import.mod_pos)
+			c.error('duplicate of module name `${ast_import.mod}` on line ${
+				ast_file.stmts[0].pos.line_nr + 1}', ast_import.mod_pos)
 		}
 		if c.mod == ast_import.alias {
-			c.error('duplicate of module name `${ast_import.mod}` ${ast_file.stmts[0].pos.line_nr +
-				1}', ast_import.alias_pos)
+			c.error('duplicate of module name `${ast_import.mod}` on line ${
+				ast_file.stmts[0].pos.line_nr + 1}', ast_import.alias_pos)
 		}
 		for sym in ast_import.syms {
 			full_name := ast_import.mod + '.' + sym.name
