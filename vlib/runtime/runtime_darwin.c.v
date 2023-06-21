ed37cde0732c8cc57184cd9a47722d7d0d5c6383
kbkpbot <kbkpbot@gmail.com>
2023-06-21 21:15:33 +0800

    fix darwin compile warnings

--- vlib/runtime/runtime_darwin.c.v
+++ vlib/runtime/runtime_darwin.c.v
@@ -2,6 +2,10 @@ module runtime
 
 #include <mach/mach.h>
 
+[typedef]
+struct C.vm_size_t {
+}
+
 struct C.host_basic_info {
 	max_mem u64
 }
@@ -15,16 +19,18 @@ struct C.vm_statistics64_data_t {
 }
 
 fn C.mach_host_self() C.host_t
-fn C.host_info(host C.host_t, flavor int, host_info_out voidptr, host_info_outCnt voidptr) int
-fn C.host_page_size(host C.host_t, out_page_size &u32) int
-fn C.host_statistics64(host C.host_t, flavor int, host_info_out voidptr, host_info_outCnt voidptr) int
+fn C.host_info(host C.host_t, flavor int, host_info_out &int, host_info_outCnt &u32) int
+fn C.host_statistics64(host C.host_t, flavor int, host_info_out &int, host_info_outCnt &u32) int
+fn C.host_page_size(host C.host_t, out_page_size &C.vm_size_t) int
 
 // total_memory returns total physical memory found on the system.
 pub fn total_memory() usize {
 	mut hbi := C.host_basic_info{}
-	mut memsz := C.HOST_BASIC_INFO_COUNT
+	mut memsz := u32(C.HOST_BASIC_INFO_COUNT)
 	mut host := C.mach_host_self()
-	C.host_info(host, C.HOST_BASIC_INFO, &hbi, &memsz)
+	unsafe {
+		C.host_info(host, C.HOST_BASIC_INFO, &int(&hbi), &memsz)
+	}
 
 	return usize(hbi.max_mem)
 }
@@ -32,11 +38,12 @@ pub fn total_memory() usize {
 // free_memory returns free physical memory found on the system.
 pub fn free_memory() usize {
 	mut hs := C.vm_statistics64_data_t{}
-	mut vmsz := C.HOST_VM_INFO64_COUNT
+	mut vmsz := u32(C.HOST_VM_INFO64_COUNT)
 	mut hps := u32(0)
 	mut host := C.mach_host_self()
-	C.host_statistics64(host, C.HOST_VM_INFO64, &hs, &vmsz)
-	C.host_page_size(host, &hps)
-
+	unsafe {
+		C.host_statistics64(host, C.HOST_VM_INFO64, &int(&hs), &vmsz)
+		C.host_page_size(host, &C.vm_size_t(&hps))
+	}
 	return usize(u64(hs.free_count) * u64(hps))
 }
