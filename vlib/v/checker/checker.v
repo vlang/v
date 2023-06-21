b61fdfa094c1baca12e2987b54ce4fc5a06fddba
yuyi <yuyi98@163.com>
2023-06-21 17:09:53 +0800

    checker, cgen: fix generic method variable (#18509)

--- vlib/v/checker/checker.v
+++ vlib/v/checker/checker.v
@@ -1484,7 +1484,7 @@ fn (mut c Checker) selector_expr(mut node ast.SelectorExpr) ast.Type {
 		}
 		return field.typ
 	}
-	if mut method := c.table.find_method(sym, field_name) {
+	if mut method := sym.find_method_with_generic_parent(field_name) {
 		if c.expected_type != 0 && c.expected_type != ast.none_type {
 			fn_type := ast.new_type(c.table.find_or_register_fn_type(method, false, true))
 			// if the expected type includes the receiver, don't hide it behind a closure
