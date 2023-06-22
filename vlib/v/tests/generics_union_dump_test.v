3123abc56831ad85251608b0a048989944a23d2c
Delyan Angelov <delian66@gmail.com>
2023-06-22 11:56:27 +0300

    fix tests-sanitize-memory-clang failure

--- vlib/v/tests/generics_union_dump_test.v
+++ vlib/v/tests/generics_union_dump_test.v
@@ -14,15 +14,15 @@ fn test_conversion_works() {
 
 fn test_dumping_of_a_generic_union_value() {
 	dump(Convertor[u8]{
-		value: 123
+		bytes: [u8(210), 4, 0, 0, 5, 0, 0, 0]!
 	})
 	dump(Convertor[i16]{
-		value: 1234
+		bytes: [u8(210), 4, 0, 0, 5, 0, 0, 0]!
 	})
 	dump(Convertor[int]{
-		value: 1234
+		bytes: [u8(210), 4, 0, 0, 5, 0, 0, 0]!
 	})
 	dump(Convertor[i64]{
-		value: 1234
+		bytes: [u8(210), 4, 0, 0, 5, 0, 0, 0]!
 	})
 }
