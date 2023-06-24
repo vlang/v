d90c5ae54d116432b05f0c253b79819087481cd3
Artem Yurchenko <zumikq78@gmail.com>
2023-06-24 17:10:23 +0200

    vpm: fix installation of mixed modules

--- cmd/tools/vpm.v
+++ cmd/tools/vpm.v
@@ -88,24 +88,27 @@ fn main() {
 				manifest := vmod.from_file('./v.mod') or { panic(err) }
 				module_names = manifest.dependencies.clone()
 			}
-			mut source := Source.vpm
-			if module_names.all(it.starts_with('https://')) {
-				source = Source.git
-			}
+
 			if '--once' in options {
 				module_names = vpm_once_filter(module_names)
+
 				if module_names.len == 0 {
 					return
 				}
 			}
-			if '--git' in options {
-				source = Source.git
-			}
+
+			external_module_names := module_names.filter(it.starts_with('https://'))
+			vpm_module_names := module_names.filter(it !in external_module_names)
+
+			vpm_install(vpm_module_names, Source.vpm)
+
+			mut external_source := Source.git
+
 			if '--hg' in options {
-				source = Source.hg
+				external_source = Source.hg
 			}
 
-			vpm_install(module_names, source)
+			vpm_install(external_module_names, external_source)
 		}
 		'update' {
 			vpm_update(module_names)
