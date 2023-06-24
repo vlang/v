2bf05dd3fb444e4a7c9b7c9cd9ca3d7ee43b1778
Delyan Angelov <delian66@gmail.com>
2023-06-24 10:04:01 +0300

    cleanup, fix cross/bootstrapping compilation of v.c on macos

--- vlib/runtime/free_memory_impl_darwin.c.v
+++ vlib/runtime/free_memory_impl_darwin.c.v
@@ -1,13 +1,34 @@
 module runtime
 
+#include <mach/mach.h>
+
+[typedef]
+struct C.vm_size_t {
+}
+
+[typedef]
+struct C.vm_statistics64_data_t {
+	free_count          u32
+	purgeable_count     u32
+	speculative_count   u32
+	external_page_count u32
+}
+
+fn C.mach_host_self() C.host_t
+fn C.host_page_size(host C.host_t, out_page_size &C.vm_size_t) int
+fn C.host_statistics64(host C.host_t, flavor int, host_info_out &int, host_info_outCnt &u32) int
+
 fn free_memory_impl() usize {
-	mut hs := C.vm_statistics64_data_t{}
-	mut vmsz := u32(C.HOST_VM_INFO64_COUNT)
-	mut hps := u32(0)
-	mut host := C.mach_host_self()
-	unsafe {
-		C.host_statistics64(host, C.HOST_VM_INFO64, &int(&hs), &vmsz)
-		C.host_page_size(host, &C.vm_size_t(&hps))
+	$if macos {
+		mut hs := C.vm_statistics64_data_t{}
+		mut vmsz := u32(C.HOST_VM_INFO64_COUNT)
+		mut hps := u32(0)
+		mut host := C.mach_host_self()
+		unsafe {
+			C.host_statistics64(host, C.HOST_VM_INFO64, &int(&hs), &vmsz)
+			C.host_page_size(host, &C.vm_size_t(&hps))
+		}
+		return usize(u64(hs.free_count) * u64(hps))
 	}
-	return usize(u64(hs.free_count) * u64(hps))
+	return 1
 }
--- vlib/runtime/free_memory_impl_linux.c.v
+++ vlib/runtime/free_memory_impl_linux.c.v
@@ -1,7 +1,10 @@
 module runtime
 
 fn free_memory_impl() usize {
-	page_size := usize(C.sysconf(C._SC_PAGESIZE))
-	av_phys_pages := usize(C.sysconf(C._SC_AVPHYS_PAGES))
-	return page_size * av_phys_pages
+	$if linux {
+		page_size := usize(C.sysconf(C._SC_PAGESIZE))
+		av_phys_pages := usize(C.sysconf(C._SC_AVPHYS_PAGES))
+		return page_size * av_phys_pages
+	}
+	return 1
 }
