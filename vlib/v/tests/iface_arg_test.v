329e063752820e0e4d42a804cb4e5beecd1b0f31
Felipe Pena <felipensp@gmail.com>
2023-07-02 08:18:53 -0300

    checker: fix wrong interface parameter resolution for anonymous fn (#18724)

---
+++ vlib/v/tests/iface_arg_test.v
@@ -0,0 +1,28 @@
+interface ITest {
+}
+
+struct Test {
+}
+
+struct Cmdable {
+mut:
+	call fn (cmd ITest)
+}
+
+fn (t Test) test(a ITest) {}
+
+fn test(a ITest) {}
+
+fn get() Test {
+	return Test{}
+}
+
+fn test_main() {
+	mut a := Cmdable{}
+	a.call = test
+	a.call(Test{})
+	test(Test{})
+	Test{}.test(a)
+
+	assert true
+}
