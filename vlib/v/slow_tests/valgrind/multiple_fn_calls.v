c481b2251f3f2e22142f82e4d65488224427d8ec
walkingdevel <mosquito.is.dead@gmail.com>
2023-07-02 19:02:15 +0000

    fix: skip_valgrind_files

--- vlib/v/slow_tests/valgrind/multiple_fn_calls.v
+++ vlib/v/slow_tests/valgrind/multiple_fn_calls.v
@@ -1,17 +1,13 @@
-fn return_string(s string) !string {
-	return s
-}
+import strconv
 
-// get_hex_rgb_colors returns hex codes of RGB colors
-// with redundant cloning of strings to check autofree is working.
-fn get_hex_rgb_colors(color string) []string {
-	colors := color.replace('#', '')
-
-	return [return_string(colors[0..2]) or { return ['', '', ''] },
-		return_string(colors[2..4]) or { return ['', '', ''] },
-		return_string(colors[4..6]) or { return ['', '', ''] }]
+fn color_code_to_rgb(color string) []int {
+	clr := color.replace('#', '')
+	return [int(strconv.parse_int(clr[0..2], 16, 0) or { return [0, 0, 0] }),
+		int(strconv.parse_int(clr[2..4], 16, 0) or { return [0, 0, 0] }),
+		int(strconv.parse_int(clr[4..6],
+			16, 0) or { return [0, 0, 0] })]
 }
 
 fn main() {
-	dump(get_hex_rgb_colors('#abcdef'))
+	dump(color_code_to_rgb('#abcdef'))
 }
