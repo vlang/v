6f8e9dffb82e604e7df4fe0f9462896db2bf0f71
Swastik <swstkbaranwal@gmail.com>
2023-07-03 12:47:00 +0530

    fix

--- vlib/v/checker/assign.v
+++ vlib/v/checker/assign.v
@@ -393,7 +393,8 @@ fn (mut c Checker) assign_stmt(mut node ast.AssignStmt) {
 						}
 					}
 				} else if left.op == .amp {
-					c.error('cannot use a reference on the left side of `${node.op}`', left.pos)
+					c.error('cannot use a reference on the left side of `${node.op}`',
+						left.pos)
 				} else {
 					c.error('cannot use `${left.op}` on the left of `${node.op}`', left.pos)
 				}
