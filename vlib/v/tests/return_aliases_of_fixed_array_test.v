26f37c76e8be6cba6c8071a9ac2e9e71fe625154
Felipe Pena <felipensp@gmail.com>
2023-07-08 10:49:06 -0300

    fix test

--- vlib/v/tests/return_aliases_of_fixed_array_test.v
+++ vlib/v/tests/return_aliases_of_fixed_array_test.v
@@ -1,14 +1,16 @@
 type Mat = [2][2]int
 
-fn main() {
+fn test_main() {
 	a := Mat([[1, 2]!, [3, 4]!]!)
 	println(a.foo())
+	z := a.foo()
+	assert z == [[1, 2]!, [3, 4]!]!
 }
 
-fn (v Mat) foo() Mat {
+fn (v Mat) foo() [2][2]int {
 	return v
 }
 
-fn bar() Mat {
+fn bar() [2][2]int {
 	return Mat([[1, 2]!, [3, 4]!]!)
 }
