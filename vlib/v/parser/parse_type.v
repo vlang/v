7498fc274b133ba43fb3a930d1dc2cf578648b56
Felipe Pena <felipensp@gmail.com>
2023-07-09 14:11:39 -0300

    fix

--- vlib/v/parser/parse_type.v
+++ vlib/v/parser/parse_type.v
@@ -648,9 +648,10 @@ fn (mut p Parser) parse_any_type(language ast.Language, is_ptr bool, check_dot b
 						ret = ast.int_literal_type
 					}
 					'any' {
-						if !(p.file_backend_mode != .js && p.mod != 'builtin') {
-							ret = ast.any_type
+						if p.file_backend_mode != .js && p.mod != 'builtin' {
+							p.error('cannot use `any` type here')
 						}
+						ret = ast.any_type
 					}
 					else {
 						p.next()
