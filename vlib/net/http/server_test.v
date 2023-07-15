00c00ee2cabe44b25a869ee2ecb2401822003aaf
alexferl <me@alexferl.com>
2023-07-15 00:21:44 -0400

    net/http: make listener public and add addr

--- vlib/net/http/server_test.v
+++ vlib/net/http/server_test.v
@@ -1,8 +1,10 @@
-import net.http
+module http
+
+import net
 import time
 
 fn test_server_stop() {
-	mut server := &http.Server{
+	mut server := &Server{
 		accept_timeout: 1 * time.second
 	}
 	t := spawn server.listen_and_serve()
@@ -16,9 +18,25 @@ fn test_server_stop() {
 }
 
 fn test_server_close() {
-	mut server := &http.Server{
+	mut server := &Server{
 		accept_timeout: 1 * time.second
 		handler: MyHttpHandler{}
+	}
+	t := spawn server.listen_and_serve()
+	time.sleep(250 * time.millisecond)
+	mut watch := time.new_stopwatch()
+	server.close()
+	assert server.status() == .closed
+	assert watch.elapsed() < 100 * time.millisecond
+	t.wait()
+	assert watch.elapsed() < 999 * time.millisecond
+}
+
+fn test_server_custom_listener() {
+	listener := net.listen_tcp(.ip6, ':8081')!
+	mut server := &Server{
+		accept_timeout: 1 * time.second
+		listener: listener
 	}
 	t := spawn server.listen_and_serve()
 	time.sleep(250 * time.millisecond)
@@ -37,10 +55,10 @@ mut:
 	not_founds int
 }
 
-fn (mut handler MyHttpHandler) handle(req http.Request) http.Response {
+fn (mut handler MyHttpHandler) handle(req Request) Response {
 	handler.counter++
 	// eprintln('$time.now() | counter: $handler.counter | $req.method $req.url\n$req.header\n$req.data - 200 OK\n')
-	mut r := http.Response{
+	mut r := Response{
 		body: req.data + ', ${req.url}'
 		header: req.header
 	}
@@ -62,28 +80,28 @@ const cport = 8198
 
 fn test_server_custom_handler() {
 	mut handler := MyHttpHandler{}
-	mut server := &http.Server{
+	mut server := &Server{
 		accept_timeout: 1 * time.second
 		handler: handler
-		port: cport
+		port: http.cport
 	}
 	t := spawn server.listen_and_serve()
 	for server.status() != .running {
 		time.sleep(10 * time.millisecond)
 	}
-	x := http.fetch(url: 'http://localhost:${cport}/endpoint?abc=xyz', data: 'my data')!
+	x := fetch(url: 'http://localhost:${http.cport}/endpoint?abc=xyz', data: 'my data')!
 	assert x.body == 'my data, /endpoint?abc=xyz'
 	assert x.status_code == 200
 	assert x.status_msg == 'OK'
 	assert x.http_version == '1.1'
-	y := http.fetch(url: 'http://localhost:${cport}/another/endpoint', data: 'abcde')!
+	y := fetch(url: 'http://localhost:${http.cport}/another/endpoint', data: 'abcde')!
 	assert y.body == 'abcde, /another/endpoint'
 	assert y.status_code == 200
 	assert x.status_msg == 'OK'
 	assert y.status() == .ok
 	assert y.http_version == '1.1'
 	//
-	http.fetch(url: 'http://localhost:${cport}/something/else')!
+	fetch(url: 'http://localhost:${http.cport}/something/else')!
 	server.stop()
 	t.wait()
 	assert handler.counter == 3
