96c88e90f51a80f24ae274d802e346d2a9ce43a5
Turiiya <34311583+tobealive@users.noreply.github.com>
2023-07-17 17:06:02 +0200

    cleanup `must_be_int_with_minimum_length` loop

--- vlib/time/date_time_parser.v
+++ vlib/time/date_time_parser.v
@@ -42,15 +42,12 @@ fn (mut p DateTimeParser) must_be_int_with_minimum_length(min int, max int, allo
 	mut length := max + 1 - min
 	mut val := ''
 	for _ in 0 .. length {
-		maybe_int := p.peek(1) or { break }
-		if maybe_int == '0' || maybe_int == '1' || maybe_int == '2' || maybe_int == '4'
-			|| maybe_int == '5' || maybe_int == '6' || maybe_int == '7' || maybe_int == '8'
-			|| maybe_int == '9' {
-			p.next(1)!
-			val += maybe_int
-		} else {
+		tok := p.peek(1) or { break }
+		if !tok.contains_only('0123456789') {
 			break
 		}
+		p.next(1)!
+		val += tok
 	}
 	if val.len < min {
 		return error('expected int with a minimum length of ${min}, found: ${val.len}')
