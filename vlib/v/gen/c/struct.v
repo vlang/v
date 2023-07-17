dd6ed71ff5934ece95326233f45bad4e0a7d9022
Felipe Pena <felipensp@gmail.com>
2023-07-17 18:48:13 -0300

    fix

--- vlib/v/gen/c/struct.v
+++ vlib/v/gen/c/struct.v
@@ -59,7 +59,12 @@ fn (mut g Gen) struct_init(node ast.StructInit) {
 		shared_styp = g.typ(shared_typ)
 		g.writeln('(${shared_styp}*)__dup${shared_styp}(&(${shared_styp}){.mtx = {0}, .val =(${styp}){')
 	} else if is_amp || g.inside_cast_in_heap > 0 {
-		g.write('(${styp}*)memdup(&(${styp}){')
+		if node.typ.has_flag(.option) {
+			basetyp := g.base_type(node.typ)
+			g.write('(${basetyp}*)memdup(&(${basetyp}){')
+		} else {
+			g.write('(${styp}*)memdup(&(${styp}){')
+		}
 	} else if node.typ.is_ptr() {
 		basetyp := g.typ(node.typ.set_nr_muls(0))
 		if is_multiline {
