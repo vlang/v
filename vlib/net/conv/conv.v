ebc19c28791236df00c8b5c0692996723f2c0b41
phoreverpheebs <phoreverphoebs@proton.me>
2023-07-21 20:51:13 +0900

    net.conv: improve byte swapping on x86

--- vlib/net/conv/conv.v
+++ vlib/net/conv/conv.v
@@ -3,18 +3,27 @@ module conv
 // htn64 converts a the 64 bit value `host` to the net format (htonll)
 pub fn htn64(host u64) u64 {
 	$if little_endian {
-		// vfmt off
-		return (
-			((host >> 56) & 0x00000000_000000FF) | 
-			((host >> 40) & 0x00000000_0000FF00) | 
-			((host >> 24) & 0x00000000_00FF0000) | 
-			((host >> 8)  & 0x00000000_FF000000) |
-			((host << 8)  & 0x000000FF_00000000) | 
-			((host << 24) & 0x0000FF00_00000000) | 
-			((host << 40) & 0x00FF0000_00000000) | 
-			((host << 56) & 0xFF000000_00000000)
-		)
-		// vfmt on
+		$if amd64 {
+			mut r := host
+			asm amd64 {
+				bswap a
+				; =r (r) as a
+			}
+			return r
+		} $else {
+			// vfmt off
+			return (
+				((host >> 56) & 0x00000000_000000FF) |
+				((host >> 40) & 0x00000000_0000FF00) |
+				((host >> 24) & 0x00000000_00FF0000) |
+				((host >> 8)  & 0x00000000_FF000000) |
+				((host << 8)  & 0x000000FF_00000000) |
+				((host << 24) & 0x0000FF00_00000000) |
+				((host << 40) & 0x00FF0000_00000000) |
+				((host << 56) & 0xFF000000_00000000)
+			)
+			// vfmt on
+		}
 	} $else {
 		return host
 	}
@@ -23,14 +32,23 @@ pub fn htn64(host u64) u64 {
 // htn32 converts the 32 bit value `host` to the net format (htonl)
 pub fn htn32(host u32) u32 {
 	$if little_endian {
-		// vfmt off
-		return (
-			((host >> 24) & 0x0000_00FF) | 
-			((host >> 8)  & 0x0000_FF00) | 
-			((host << 8)  & 0x00FF_0000) | 
-			((host << 24) & 0xFF00_0000)
-		)
-		// vfmt on
+		$if amd64 || i386 {
+			mut r := host
+			asm amd64 {
+				bswap a
+				; =r (r) as a
+			}
+			return r
+		} $else {
+			// vfmt off
+			return (
+				((host >> 24) & 0x0000_00FF) |
+				((host >> 8)  & 0x0000_FF00) |
+				((host << 8)  & 0x00FF_0000) |
+				((host << 24) & 0xFF00_0000)
+			)
+			// vfmt on
+		}
 	} $else {
 		return host
 	}
@@ -39,12 +57,21 @@ pub fn htn32(host u32) u32 {
 // htn16 converts the 16 bit value `host` to the net format (htons)
 pub fn htn16(host u16) u16 {
 	$if little_endian {
-		// vfmt off
-		return (
-			((host >> 8) & 0x00FF) | 
-			((host << 8) & 0xFF00)
-		)
-		// vfmt on
+		$if amd64 || i386 {
+			mut r := host
+			asm amd64 {
+				rol a, 8
+				; =r (r) as a
+			}
+			return r
+		} $else {
+			// vfmt off
+			return (
+				((host >> 8) & 0x00FF) |
+				((host << 8) & 0xFF00)
+			)
+			// vfmt on
+		}
 	} $else {
 		return host
 	}
