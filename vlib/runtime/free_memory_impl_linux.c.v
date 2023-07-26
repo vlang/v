54df136725b2dc3a4f602d50cba9e20f0cb5d959
phoreverpheebs <phoreverphoebs@proton.me>
2023-07-26 09:31:05 +0900

    runtime: cleanup free_memory comptime conditional

--- vlib/runtime/free_memory_impl_linux.c.v
+++ vlib/runtime/free_memory_impl_linux.c.v
@@ -1,10 +1,7 @@
 module runtime
 
 fn free_memory_impl() usize {
-	$if linux {
-		page_size := usize(C.sysconf(C._SC_PAGESIZE))
-		av_phys_pages := usize(C.sysconf(C._SC_AVPHYS_PAGES))
-		return page_size * av_phys_pages
-	}
-	return 1
+	page_size := usize(C.sysconf(C._SC_PAGESIZE))
+	av_phys_pages := usize(C.sysconf(C._SC_AVPHYS_PAGES))
+	return page_size * av_phys_pages
 }
