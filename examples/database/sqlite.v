04f697809d79ba5a8d164a07ccb06ab3d58ea137
jacksonmowry <96317858+jacksonmowry@users.noreply.github.com>
2023-08-09 10:50:59 +0000

    Apply suggestions from code review
    Change error handling
    
    Co-authored-by: Delyan Angelov <delian66@gmail.com>

--- examples/database/sqlite.v
+++ examples/database/sqlite.v
@@ -4,17 +4,17 @@ fn main() {
 	db := sqlite.connect(':memory:')!
 	db.exec("create table users (id integer primary key, name text default '');") or { panic(err) }
 
-	db.exec("insert into users (name) values ('Sam')") or { panic(err) }
-	db.exec("insert into users (name) values ('Peter')") or { panic(err) }
-	db.exec("insert into users (name) values ('Kate')") or { panic(err) }
+	db.exec("insert into users (name) values ('Sam')")!
+	db.exec("insert into users (name) values ('Peter')")!
+	db.exec("insert into users (name) values ('Kate')")!
 
-	nr_users := db.q_int('select count(*) from users') or { panic(err) }
+	nr_users := db.q_int('select count(*) from users')!
 	println('nr users = ${nr_users}')
 
-	name := db.q_string('select name from users where id = 1') or { panic(err) }
+	name := db.q_string('select name from users where id = 1')!
 	assert name == 'Sam'
 
-	users := db.exec('select * from users') or { panic(err) }
+	users := db.exec('select * from users')!
 	for row in users {
 		println(row.vals)
 	}
