16ff388efe10eca75d613918e6a47337942a3ef3
Delyan Angelov <delian66@gmail.com>
2023-08-13 12:17:35 +0300

    improve the test (avoid hard coding sizes)

--- vlib/v/tests/project_with_cpp_code/compiling_cpp_files_with_a_cplusplus_compiler_test.v
+++ vlib/v/tests/project_with_cpp_code/compiling_cpp_files_with_a_cplusplus_compiler_test.v
@@ -7,10 +7,12 @@ fn C.sizeof_char() int
 fn test_the_implementation_object_file_was_compiled_with_a_c_plus_plus_compiler() {
 	res := C.sizeof_char()
 	dump(res)
-	if res == 4 {
+	if res == sizeof(int) {
 		eprintln('implementation.o was compiled with a C compiler. Fail.')
-	} else if res == 1 {
+	} else if res == sizeof(char) {
 		println('implementation.o was compiled with a C++ compiler. Good.')
+	} else {
+		eprintln('¯\_(ツ)_/¯ ... unknown C/C++ compiler')
 	}
-	assert res == 1 // valid for C++ compilers
+	assert res == sizeof(char)
 }
