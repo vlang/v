9952211f566245bdbf684b22018348376524db63
Delyan Angelov <delian66@gmail.com>
2023-08-17 11:16:04 +0300

    re-apply a packing attribute to PeSectionHeader

--- vlib/v/gen/native/pe.v
+++ vlib/v/gen/native/pe.v
@@ -415,6 +415,7 @@ fn (mut g Gen) gen_pe_data_dirs() {
 
 // reference: https://learn.microsoft.com/en-us/windows/win32/debug/pe-format?redirectedfrom=MSDN#section-table-section-headers
 // should also be [packed], but -cstrict CI's didn't like it -> potential bug source
+[_pack: '1']
 [params]
 struct PeSectionHeader {
 	name [8]u8
