5f595a60f7daeee7ad39171448890a18526db412
Delyan Angelov <delian66@gmail.com>
2023-08-19 07:38:28 +0300

    tests: improve formatting of enum_bitfield_64bit_test.v

--- vlib/v/tests/enum_bitfield_64bit_test.v
+++ vlib/v/tests/enum_bitfield_64bit_test.v
@@ -1,75 +1,20 @@
-// TODO: fix the formatting here, when VFMT ON/OFF is working
-
+// vfmt off
 [flag]
 enum PawnsBoard as u64 {
-	a8
-	b8
-	c8
-	d8
-	e8
-	f8
-	g8
-	h8
-	a7
-	b7
-	c7
-	d7
-	e7
-	f7
-	g7
-	h7
-	a6
-	b6
-	c6
-	d6
-	e6
-	f6
-	g6
-	h6
-	a5
-	b5
-	c5
-	d5
-	e5
-	f5
-	g5
-	h5
-	a4
-	b4
-	c4
-	d4
-	e4
-	f4
-	g4
-	h4
-	a3
-	b3
-	c3
-	d3
-	e3
-	f3
-	g3
-	h3
-	a2
-	b2
-	c2
-	d2
-	e2
-	f2
-	g2
-	h2
-	a1
-	b1
-	c1
-	d1
-	e1
-	f1
-	g1
-	h1
+	a8 b8 c8 d8 e8 f8 g8 h8
+	a7 b7 c7 d7 e7 f7 g7 h7
+	a6 b6 c6 d6 e6 f6 g6 h6
+	a5 b5 c5 d5 e5 f5 g5 h5
+	a4 b4 c4 d4 e4 f4 g4 h4
+	a3 b3 c3 d3 e3 f3 g3 h3
+	a2 b2 c2 d2 e2 f2 g2 h2
+	a1 b1 c1 d1 e1 f1 g1 h1
 }
+// vfmt on
 
 fn test_flag_enum_with_64_value_bits() {
 	last_value := PawnsBoard.h1
+	dump(last_value)
 	dump(u64(last_value))
 	assert u64(last_value) == 0x8000_0000_0000_0000
 
