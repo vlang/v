644c68ce944cd857ab146ea55cc675db129230eb
Delyan Angelov <delian66@gmail.com>
2023-08-22 14:23:47 +0300

    tools: improve the output of parser_speed.v and scanner_speed.v

--- cmd/tools/measure/scanner_speed.v
+++ cmd/tools/measure/scanner_speed.v
@@ -1,5 +1,6 @@
 import os
 import time
+import term
 import v.scanner
 import v.pref
 
@@ -16,7 +17,16 @@ fn main() {
 	process_files(files)!
 }
 
+fn hline() {
+	println('-------------------------------------------------------------------------------------------------------------------------------------')
+}
+
+fn theader() {
+	println('        Time     Tokens      Bytes      Lines   Bytes/Token     Errors')
+}
+
 fn process_files(files []string) ! {
+	nthreads := 1 // TODO
 	mut pref_ := pref.new_preferences()
 	pref_.is_fmt = true
 	pref_.skip_warnings = true
@@ -25,6 +35,8 @@ fn process_files(files []string) ! {
 	mut total_us := i64(0)
 	mut total_bytes := i64(0)
 	mut total_tokens := i64(0)
+	mut total_lines := i64(0)
+	mut total_errors := i64(0)
 	for f in files {
 		if f == '' {
 			continue
@@ -38,7 +50,14 @@ fn process_files(files []string) ! {
 		total_us += f_us
 		total_bytes += s.text.len
 		total_tokens += s.all_tokens.len
-		println('${f_us:10}us ${s.all_tokens.len:10} ${s.text.len:10} ${(f64(s.text.len) / s.all_tokens.len):7.3f} ${f}')
+		total_lines += s.nr_lines
+		total_errors += s.errors.len
+		println('${f_us:10}us ${s.all_tokens.len:10} ${s.text.len:10} ${s.nr_lines:10} ${(f64(s.text.len) / s.all_tokens.len):13.3f} ${s.errors.len:10}   ${f}')
 	}
-	println('${total_us:10}us ${total_tokens:10} ${total_bytes:10} ${(f64(total_tokens) / total_bytes):7.3f} | speed: ${(f64(total_bytes) / total_us):2.5f} MB/s')
+	hline()
+	theader()
+	hline()
+	speed_mb_s := term.colorize(term.bright_yellow, '${(f64(total_bytes) / total_us):6.3f} MB/s')
+	speed_lines_s := term.colorize(term.bright_yellow, '${(1_000_000 * f64(total_lines) / total_us):10.1f} lines/s')
+	println('${total_us:10}us ${total_tokens:10} ${total_bytes:10} ${total_lines:10} ${(f64(total_bytes) / total_tokens):13.3} ${total_errors:10}   Scanner speed: ${speed_mb_s}, ${speed_lines_s}, ${nthreads} thread(s)')
 }
