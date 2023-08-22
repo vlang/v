3796b3c27833926012a25e074803ac8ca23ef6c7
Felipe Pena <felipensp@gmail.com>
2023-08-22 09:17:27 -0300

    test

---
+++ vlib/v/tests/const_as_enum_value_test.v
@@ -0,0 +1,15 @@
+const (
+	first_const  = 1000
+	second_const = 2000
+)
+
+enum Test {
+	example = first_const
+}
+
+fn test_main() {
+	a := Test.example
+	dump(Test.example)
+	assert a == unsafe { Test(1000) }
+	assert Test.example == unsafe { Test(1000) }
+}
