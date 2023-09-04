8c480f34655b798c9110229258c31ebc5a1b229a
Turiiya <34311583+ttytm@users.noreply.github.com>
2023-09-04 21:58:17 +0200

    update

--- cmd/tools/vdoc/html_tag_escape_test.v
+++ cmd/tools/vdoc/html_tag_escape_test.v
@@ -1,6 +1,6 @@
 module main
 
 fn test_html_tag_escape() {
-	assert html_tag_escape('assert <abc> 123') == 'assert &lt;abc&gt; 123'
-	assert html_tag_escape('`assert <abc> 123`') == '`assert <abc> 123`'
+	assert html_tag_escape('abc <b>bold</b> 123') == 'abc &lt;b&gt;bold&lt;/b&gt; 123'
+	assert html_tag_escape('`abc <b>bold</b> 123`') == '`abc <b>bold</b> 123`'
 }
