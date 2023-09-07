8c7cbfb7c5c830ef3f9bd9cb113d38faa4d9fe4e
Swastik <swstkbaranwal@gmail.com>
2023-09-07 16:10:44 +0530

    fix test

--- vlib/v/tests/struct_init_and_assign_test.v
+++ vlib/v/tests/struct_init_and_assign_test.v
@@ -12,15 +12,15 @@ struct Foo {
 }
 
 fn test_struct_init_and_assign() {
-	mut sql := FooFoo{}
-	sql.conn = Foo{
+	mut sql_ := FooFoo{}
+	sql_.conn = Foo{
 		username: 'username'
 		password: 'abc'
 		dbname: 'test'
 	}
-	assert sql.conn.host == '127.0.0.1'
-	assert sql.conn.port == 3306
-	assert sql.conn.username == 'username'
-	assert sql.conn.password == 'abc'
-	assert sql.conn.dbname == 'test'
+	assert sql_.conn.host == '127.0.0.1'
+	assert sql_.conn.port == 3306
+	assert sql_.conn.username == 'username'
+	assert sql_.conn.password == 'abc'
+	assert sql_.conn.dbname == 'test'
 }
