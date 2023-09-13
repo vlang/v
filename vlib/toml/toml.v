b4c0ed50856bc4b9b847f8060b034b8ff9549ab4
Turiiya <34311583+ttytm@users.noreply.github.com>
2023-09-13 04:00:43 +0200

    reduce nesting

--- vlib/toml/toml.v
+++ vlib/toml/toml.v
@@ -101,16 +101,14 @@ fn encode_struct[T](typ T) map[string]Any {
 		} $else $if field.is_array {
 			mut arr := []Any{}
 			for v in value {
-				$if v is $struct {
-					$if v is Date {
-						arr << Any(v)
-					} $else $if v is Time {
-						arr << Any(v)
-					} $else $if v is DateTime {
-						arr << Any(v)
-					} $else {
-						arr << Any(encode_struct(v))
-					}
+				$if v is Date {
+					arr << Any(v)
+				} $else $if v is Time {
+					arr << Any(v)
+				} $else $if v is DateTime {
+					arr << Any(v)
+				} $else $if v is $struct {
+					arr << Any(encode_struct(v))
 				} $else {
 					arr << Any(v)
 				}
