95d9e9feba188239253f7bd473981e64a63bddcc
yuyi <yuyi98@163.com>
2023-09-21 10:45:31 +0800

    cgen: fix fixed array of option type default

--- vlib/v/tests/fixed_array_of_option_test.v
+++ vlib/v/tests/fixed_array_of_option_test.v
@@ -1,7 +1,12 @@
 fn test_fixed_array_of_option() {
-	mut a := [3]?int{init: ?int(1)}
-	a[0] = none
-	a[1] = 2
-	println(a)
-	assert '${a}' == '[Option(none), Option(2), Option(1)]'
+	mut a1 := [3]?int{init: ?int(1)}
+	a1[0] = none
+	a1[1] = 2
+	println(a1)
+	assert '${a1}' == '[Option(none), Option(2), Option(1)]'
+
+	mut a2 := [3]?int{}
+	a2[0] = 1
+	println(a2)
+	assert '${a2}' == '[Option(1), Option(none), Option(none)]'
 }
