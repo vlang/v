dc54a7390a34432c2818fc16be44026e56fb298c
Delyan Angelov <delian66@gmail.com>
2023-09-30 10:48:28 +0300

    builtin: extract backtraces.c.v, backtraces_nix.c.v, backtraces_windows.c.v

--- vlib/builtin/builtin.c.v
+++ vlib/builtin/builtin.c.v
@@ -690,31 +690,6 @@ fn v_fixed_index(i int, len int) int {
 	return i
 }
 
-// print_backtrace shows a backtrace of the current call stack on stdout
-pub fn print_backtrace() {
-	// At the time of backtrace_symbols_fd call, the C stack would look something like this:
-	// * print_backtrace_skipping_top_frames
-	// * print_backtrace itself
-	// * the rest of the backtrace frames
-	// => top 2 frames should be skipped, since they will not be informative to the developer
-	$if !no_backtrace ? {
-		$if freestanding {
-			println(bare_backtrace())
-		} $else {
-			$if tinyc {
-				C.tcc_backtrace(c'Backtrace')
-			} $else {
-				// NOTE: TCC doesn't have the unwind library
-				$if use_libbacktrace ? {
-					print_libbacktrace(1)
-				} $else {
-					print_backtrace_skipping_top_frames(2)
-				}
-			}
-		}
-	}
-}
-
 // NOTE: g_main_argc and g_main_argv are filled in right after C's main start.
 // They are used internally by V's builtin; for user code, it is much
 // more convenient to just use `os.args` instead.
