8083744d1e8991ade1b2b5cc8650a2ce509c0137
Pratush Rai <pratushrai0309@gmail.com>
2023-10-04 05:12:05 +0530

    improved docstring for radian and degrees function

--- vlib/math/math.v
+++ vlib/math/math.v
@@ -36,7 +36,7 @@ pub fn copysign(x f64, y f64) f64 {
 	return f64_from_bits((f64_bits(x) & ~sign_mask) | (f64_bits(y) & sign_mask))
 }
 
-// degrees converts from radians to degrees.
+// degrees converts angle from radians to degrees.
 [inline]
 pub fn degrees(radians f64) f64 {
 	return radians * (180.0 / pi)
@@ -153,7 +153,7 @@ pub fn signi(n f64) int {
 	return int(copysign(1.0, n))
 }
 
-// radians converts from degrees to radians.
+// radians converts angle from degrees to radians.
 [inline]
 pub fn radians(degrees f64) f64 {
 	return degrees * (pi / 180.0)
