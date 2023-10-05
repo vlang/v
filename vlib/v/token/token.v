d573a2e41cd11700603dfab8036054f6dae2f660
Spydr06 <spydr06@web.de>
2023-10-05 14:16:45 +0200

    `v fmt`

--- vlib/v/token/token.v
+++ vlib/v/token/token.v
@@ -188,7 +188,8 @@ pub const (
 		.unsigned_right_shift_assign]
 
 	valid_at_tokens = ['@VROOT', '@VMODROOT', '@VEXEROOT', '@FN', '@METHOD', '@MOD', '@STRUCT',
-		'@VEXE', '@FILE', '@LINE', '@COLUMN', '@VHASH', '@VCURRENTHASH', '@VMOD_FILE', '@FILE_LINE', '@LOCATION']
+		'@VEXE', '@FILE', '@LINE', '@COLUMN', '@VHASH', '@VCURRENTHASH', '@VMOD_FILE', '@FILE_LINE',
+		'@LOCATION']
 
 	token_str       = build_token_str()
 
