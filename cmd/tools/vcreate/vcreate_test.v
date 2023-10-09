0f4f9515f566438056c119cdef8bee9654a31a7f
Turiiya <34311583+ttytm@users.noreply.github.com>
2023-10-09 12:58:35 +0200

    add `new_with_name_arg` and `new_with_model_arg` tests

--- cmd/tools/vcreate/vcreate_test.v
+++ cmd/tools/vcreate/vcreate_test.v
@@ -140,7 +140,7 @@ indent_style = tab
 	assert os.read_file('.editorconfig')! == editor_config_content
 }
 
-fn test_input() {
+fn test_with_new_no_arg_input() {
 	$if windows {
 		eprintln('Input test for windows are not yet implemented.')
 		return
@@ -149,9 +149,9 @@ fn test_input() {
 
 	// The expect script will create a new project in the temporary `test_path` directory.
 	project_name := 'my_project'
-	new_no_arg := os.execute('${os.join_path(expect_tests_path, 'new_no_arg.expect')} ${@VMODROOT} ${project_name}')
-	if new_no_arg.exit_code != 0 {
-		assert false, new_no_arg.output
+	res := os.execute('${os.join_path(expect_tests_path, 'new_with_no_arg.expect')} ${@VMODROOT} ${project_name}')
+	if res.exit_code != 0 {
+		assert false, res.output
 	}
 	// Assert mod data set in `new_no_arg.expect`.
 	mod := vmod.decode(os.read_file(os.join_path(test_path, project_name, 'v.mod')) or {
@@ -161,10 +161,65 @@ fn test_input() {
 		assert false, err.str()
 		return
 	}
-	assert mod.name == 'my_project'
-	assert mod.description == 'My awesome V project.'
+	assert mod.name == project_name
+	assert mod.description == 'My Awesome V Project.'
 	assert mod.version == '0.1.0'
 	assert mod.license == 'GPL'
+}
+
+fn test_new_with_name_arg_input() {
+	$if windows {
+		eprintln('Input test for windows are not yet implemented.')
+		return
+	}
+	expect_tests_path := os.join_path(@VMODROOT, 'cmd', 'tools', 'vcreate', 'tests')
+
+	// The expect script will create a new project in the temporary `test_path` directory.
+	project_name := 'my_other_project'
+	res := os.execute('${os.join_path(expect_tests_path, 'new_with_name_arg.expect')} ${@VMODROOT} ${project_name}')
+	if res.exit_code != 0 {
+		assert false, res.output
+	}
+	// Assert mod data set in `new_with_name_arg.expect`.
+	mod := vmod.decode(os.read_file(os.join_path(test_path, project_name, 'v.mod')) or {
+		assert false, 'Failed reading v.mod of ${project_name}'
+		return
+	}) or {
+		assert false, err.str()
+		return
+	}
+	assert mod.name == project_name
+	assert mod.description == ''
+	assert mod.version == '0.0.0'
+	assert mod.license == 'MIT'
+}
+
+fn test_new_with_model_arg_input() {
+	$if windows {
+		eprintln('Input test for windows are not yet implemented.')
+		return
+	}
+	expect_tests_path := os.join_path(@VMODROOT, 'cmd', 'tools', 'vcreate', 'tests')
+
+	// The expect script will create a new project in the temporary `test_path` directory.
+	project_name := 'my_lib'
+	model := 'lib'
+	res := os.execute('${os.join_path(expect_tests_path, 'new_with_model_arg.expect')} ${@VMODROOT} ${project_name} ${model}')
+	if res.exit_code != 0 {
+		assert false, res.output
+	}
+	// Assert mod data set in `new_with_model_arg.expect`.
+	mod := vmod.decode(os.read_file(os.join_path(test_path, project_name, 'v.mod')) or {
+		assert false, 'Failed reading v.mod of ${project_name}'
+		return
+	}) or {
+		assert false, err.str()
+		return
+	}
+	assert mod.name == project_name
+	assert mod.description == model
+	assert mod.version == '0.0.1'
+	assert mod.license == 'MIT'
 }
 
 fn testsuite_end() {
