vlang/v

commit e9a3817aed9b5d28db7fb13daef44c4648cf0c5c

checker: disallow enum initalization (#17361)

vlib/os/notify/backend_linux.c.v
@@ -142,7 +142,7 @@ fn (mut en EpollNotifier) close() ! {
 // event_mask_to_flag is a helper function that converts a bitmask
 // returned by epoll_wait to FdEventType
 fn event_mask_to_flag(mask u32) FdEventType {
-	mut flags := FdEventType{}
+	mut flags := FdEventType.read
 
 	if mask & notify.epoll_read != 0 {
 		flags.set(.read)

vlib/term/ui/input_windows.c.v
@@ -174,7 +174,7 @@ fn (mut ctx Context) parse_events() {
 					else { unsafe { KeyCode(ascii) } }
 				}
 
-				mut modifiers := Modifiers{}
+				mut modifiers := Modifiers.ctrl
 				if e.dwControlKeyState & (0x1 | 0x2) != 0 {
 					modifiers.set(.alt)
 				}
@@ -204,7 +204,7 @@ fn (mut ctx Context) parse_events() {
 				}
 				x := e.dwMousePosition.X + 1
 				y := int(e.dwMousePosition.Y) - sb_info.srWindow.Top + 1
-				mut modifiers := Modifiers{}
+				mut modifiers := Modifiers.ctrl
 				if e.dwControlKeyState & (0x1 | 0x2) != 0 {
 					modifiers.set(.alt)
 				}

vlib/term/ui/termios_nix.c.v
@@ -433,7 +433,7 @@ fn escape_sequence(buf_ string) (&Event, int) {
 		lo := typ & 0b00011
 		hi := typ & 0b11100
 
-		mut modifiers := Modifiers{}
+		mut modifiers := Modifiers.ctrl
 		if hi & 4 != 0 {
 			modifiers.set(.shift)
 		}
@@ -503,7 +503,7 @@ fn escape_sequence(buf_ string) (&Event, int) {
 	// ----------------------------
 
 	mut code := KeyCode.null
-	mut modifiers := Modifiers{}
+	mut modifiers := Modifiers.ctrl
 	match buf {
 		'[A', 'OA' { code = .up }
 		'[B', 'OB' { code = .down }

vlib/v/checker/struct.v
@@ -360,6 +360,9 @@ fn (mut c Checker) struct_init(mut node ast.StructInit, is_field_zero_struct_ini
 			&& type_sym.kind != .placeholder {
 			c.error('cannot initialize builtin type `${type_sym.name}`', node.pos)
 		}
+		if type_sym.kind == .enum_ && !c.pref.translated && !c.file.is_translated {
+			c.error('cannot initialize enums', node.pos)
+		}
 	}
 	if type_sym.kind == .sum_type && node.fields.len == 1 {
 		sexpr := node.fields[0].expr.str()

vlib/v/checker/tests/enum_init_err.out
@@ -1,0 +1,7 @@
+vlib/v/checker/tests/enum_init_err.vv:8:7: error: cannot initialize enums
+    6 |
+    7 | fn main() {
+    8 |     a := Hello{}
+      |          ~~~~~~~
+    9 |     println(a)
+   10 | }

vlib/v/checker/tests/enum_init_err.vv
@@ -1,0 +1,10 @@
+enum Hello {
+	aaa
+	bbb
+	ccc
+}
+
+fn main() {
+	a := Hello{}
+	println(a)
+}

vlib/v/gen/c/str_intp.v
@@ -26,7 +26,7 @@ fn (mut g Gen) str_format(node ast.StringInterLiteral, i int) (u64, string) {
 	}
 	mut remove_tail_zeros := false
 	fspec := node.fmts[i]
-	mut fmt_type := StrIntpType{}
+	mut fmt_type := StrIntpType.si_no_str
 	g.write('/*${fspec} ${sym}*/')
 	// upper cases
 	if (fspec - `A`) <= (`Z` - `A`) {

vlib/v/tests/enum_test.v
@@ -142,7 +142,7 @@ enum FileType {
 }
 
 fn test_enum_instance() {
-	mut filetype := FileType{}
+	mut filetype := FileType.unknown
 	eprintln(filetype)
 	s := 'x ${filetype} z'
 	assert s == 'x unknown z'

commit 121fa0e92d0995dcc95854f60bfc51a08a2c8043

term.ui,os.notify: fix .ctrl being stuck in examples/term.ui/event_viewer.v (regression happened in e9a3817a from 2023-02-20)

vlib/os/notify/backend_darwin.c.v
@@ -159,7 +159,7 @@ fn (mut kn KqueueNotifier) close() ! {
 // event_mask_to_flag is a helper function that converts a bitmask
 // returned by kevent() wait to FdEventType
 fn event_mask_to_flag(filter i16, flags u16) FdEventType {
-	mut res := FdEventType.read
+	mut res := unsafe { FdEventType(0) }
 
 	if filter & notify.kqueue_read != 0 {
 		res.set(.read)

vlib/os/notify/backend_linux.c.v
@@ -140,7 +140,7 @@ fn (mut en EpollNotifier) close() ! {
 // event_mask_to_flag is a helper function that converts a bitmask
 // returned by epoll_wait to FdEventType
 fn event_mask_to_flag(mask u32) FdEventType {
-	mut flags := FdEventType.read
+	mut flags := unsafe { FdEventType(0) }
 
 	if mask & notify.epoll_read != 0 {
 		flags.set(.read)

vlib/term/ui/input_windows.c.v
@@ -174,7 +174,7 @@ fn (mut ctx Context) parse_events() {
 					else { unsafe { KeyCode(ascii) } }
 				}
 
-				mut modifiers := Modifiers.ctrl
+				mut modifiers := unsafe { Modifiers(0) }
 				if e.dwControlKeyState & (0x1 | 0x2) != 0 {
 					modifiers.set(.alt)
 				}
@@ -204,7 +204,7 @@ fn (mut ctx Context) parse_events() {
 				}
 				x := e.dwMousePosition.X + 1
 				y := int(e.dwMousePosition.Y) - sb_info.srWindow.Top + 1
-				mut modifiers := Modifiers.ctrl
+				mut modifiers := unsafe { Modifiers(0) }
 				if e.dwControlKeyState & (0x1 | 0x2) != 0 {
 					modifiers.set(.alt)
 				}

vlib/term/ui/termios_nix.c.v
@@ -427,7 +427,7 @@ fn escape_sequence(buf_ string) (&Event, int) {
 		lo := typ & 0b00011
 		hi := typ & 0b11100
 
-		mut modifiers := Modifiers.ctrl
+		mut modifiers := unsafe { Modifiers(0) }
 		if hi & 4 != 0 {
 			modifiers.set(.shift)
 		}
@@ -497,7 +497,7 @@ fn escape_sequence(buf_ string) (&Event, int) {
 	// ----------------------------
 
 	mut code := KeyCode.null
-	mut modifiers := Modifiers.ctrl
+	mut modifiers := unsafe { Modifiers(0) }
 	match buf {
 		'[A', 'OA' { code = .up }
 		'[B', 'OB' { code = .down }

vlib/v/checker/tests/enum_cast.out
@@ -18,10 +18,11 @@ vlib/v/checker/tests/enum_cast.vv:21:19: warning: 10 does not represent a value 
    21 |     println(unsafe { Permissions(0b1010) })
       |                      ~~~~~~~~~~~~~~~~~~~
    22 |     println(unsafe { Permissions(-1) })
-   23 | }
+   23 |     println(unsafe { Permissions(0) })
 vlib/v/checker/tests/enum_cast.vv:22:19: warning: -1 does not represent a value of enum Permissions
    20 |     println(unsafe { Permissions(0b101) })
    21 |     println(unsafe { Permissions(0b1010) })
    22 |     println(unsafe { Permissions(-1) })
       |                      ~~~~~~~~~~~~~~~
-   23 | }
+   23 |     println(unsafe { Permissions(0) })
+   24 | }

vlib/v/checker/tests/enum_cast.vv
@@ -20,4 +20,5 @@ fn main() {
 	println(unsafe { Permissions(0b101) })
 	println(unsafe { Permissions(0b1010) })
 	println(unsafe { Permissions(-1) })
+	println(unsafe { Permissions(0) })
 }