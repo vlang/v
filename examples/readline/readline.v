c269f1a7a44d0a44f8eaed5bfb583275642dfe70
Turiiya <34311583+ttytm@users.noreply.github.com>
2023-10-10 12:17:52 +0200

    update comment

--- examples/readline/readline.v
+++ examples/readline/readline.v
@@ -11,7 +11,7 @@ fn run() ! {
 		eprintln('skipping on Windows, since raw mode and read_char are not yet implemented.')
 		return
 	} $else {
-		// Explicit comptime $else is required to not break compilation on Windows
+		// Explicit comptime block for other OSes than Windows is required to not break compilation on Windows.
 		mut r := readline.Readline{}
 		r.enable_raw_mode_nosig()
 		defer {
