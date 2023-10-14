281a9729265590b5c07b5a8649398da92118328c
pomid <o.a.pom.h@gmail.com>
2023-10-14 17:01:12 +0330

    Update net_udp_server_and_client.v
    formatted

--- examples/net_udp_server_and_client.v
+++ examples/net_udp_server_and_client.v
@@ -12,7 +12,7 @@ fn main() {
 	mut buf := []u8{len: 100}
 	if is_server {
 		println('UDP echo server, listening for udp packets on port: ${port}')
-			mut c := net.listen_udp('0.0.0.0:${port}')!
+		mut c := net.listen_udp('0.0.0.0:${port}')!
 		for {
 			read, addr := c.read(mut buf) or { continue }
 			println('received ${read} bytes from ${addr}')
