dfd91c314d1a8a192969b61cac52bc4d6eab42c2
zakuro <z@kuro.red>
2023-10-15 09:41:44 +0900

    crypto: fix wrong implementation of blowfish

--- vlib/crypto/blowfish/block.v
+++ vlib/crypto/blowfish/block.v
@@ -18,25 +18,25 @@ pub fn expand_key(key []u8, mut bf Blowfish) {
 	mut l := u32(0)
 	mut r := u32(0)
 	for i := 0; i < 18; i += 2 {
-		arr := setup_tables(l, r, mut bf)
-		bf.p[i], bf.p[i + 1] = arr[0], arr[1]
+		l, r = setup_tables(l, r, mut bf)
+		bf.p[i], bf.p[i + 1] = l, r
 	}
 
 	for i := 0; i < 256; i += 2 {
-		arr := setup_tables(l, r, mut bf)
-		bf.s[0][i], bf.s[0][i + 1] = arr[0], arr[1]
+		l, r = setup_tables(l, r, mut bf)
+		bf.s[0][i], bf.s[0][i + 1] = l, r
 	}
 	for i := 0; i < 256; i += 2 {
-		arr := setup_tables(l, r, mut bf)
-		bf.s[1][i], bf.s[1][i + 1] = arr[0], arr[1]
+		l, r = setup_tables(l, r, mut bf)
+		bf.s[1][i], bf.s[1][i + 1] = l, r
 	}
 	for i := 0; i < 256; i += 2 {
-		arr := setup_tables(l, r, mut bf)
-		bf.s[2][i], bf.s[2][i + 1] = arr[0], arr[1]
+		l, r = setup_tables(l, r, mut bf)
+		bf.s[2][i], bf.s[2][i + 1] = l, r
 	}
 	for i := 0; i < 256; i += 2 {
-		arr := setup_tables(l, r, mut bf)
-		bf.s[3][i], bf.s[3][i + 1] = arr[0], arr[1]
+		l, r = setup_tables(l, r, mut bf)
+		bf.s[3][i], bf.s[3][i + 1] = l, r
 	}
 }
 
@@ -54,76 +54,59 @@ pub fn expand_key_with_salt(key []u8, salt []u8, mut bf Blowfish) {
 	for i := 0; i < 18; i += 2 {
 		l ^= get_next_word(key, &j)
 		r ^= get_next_word(key, &j)
-		arr := setup_tables(l, r, mut bf)
-		bf.p[i], bf.p[i + 1] = arr[0], arr[1]
+		l, r = setup_tables(l, r, mut bf)
+		bf.p[i], bf.p[i + 1] = l, r
 	}
 
 	for i := 0; i < 256; i += 2 {
 		l ^= get_next_word(key, &j)
 		r ^= get_next_word(key, &j)
-		arr := setup_tables(l, r, mut bf)
-		bf.s[0][i], bf.s[0][i + 1] = arr[0], arr[1]
+		l, r = setup_tables(l, r, mut bf)
+		bf.s[0][i], bf.s[0][i + 1] = l, r
 	}
 	for i := 0; i < 256; i += 2 {
 		l ^= get_next_word(key, &j)
 		r ^= get_next_word(key, &j)
-		arr := setup_tables(l, r, mut bf)
-		bf.s[1][i], bf.s[1][i + 1] = arr[0], arr[1]
+		l, r = setup_tables(l, r, mut bf)
+		bf.s[1][i], bf.s[1][i + 1] = l, r
 	}
 	for i := 0; i < 256; i += 2 {
 		l ^= get_next_word(key, &j)
 		r ^= get_next_word(key, &j)
-		arr := setup_tables(l, r, mut bf)
-		bf.s[2][i], bf.s[2][i + 1] = arr[0], arr[1]
+		l, r = setup_tables(l, r, mut bf)
+		bf.s[2][i], bf.s[2][i + 1] = l, r
 	}
 	for i := 0; i < 256; i += 2 {
 		l ^= get_next_word(key, &j)
 		r ^= get_next_word(key, &j)
-		arr := setup_tables(l, r, mut bf)
-		bf.s[3][i], bf.s[3][i + 1] = arr[0], arr[1]
+		l, r = setup_tables(l, r, mut bf)
+		bf.s[3][i], bf.s[3][i + 1] = l, r
 	}
 }
 
 // setup_tables sets up the Blowfish cipher's pi and substitution tables.
-fn setup_tables(l u32, r u32, mut bf Blowfish) []u32 {
+fn setup_tables(l u32, r u32, mut bf Blowfish) (u32, u32) {
 	mut xl := l
 	mut xr := r
 	xl ^= bf.p[0]
-	xr ^= ((bf.s[0][u8(xl >> 24)] + bf.s[1][u8(xl >> 16)]) ^ bf.s[2][u8(xl >> 8)]) +
-		(bf.s[3][u8(xl)] ^ bf.p[1])
-	xl ^= ((bf.s[0][u8(xr >> 24)] + bf.s[1][u8(xr >> 16)]) ^ bf.s[2][u8(xr >> 8)]) +
-		(bf.s[3][u8(xr)] ^ bf.p[2])
-	xr ^= ((bf.s[0][u8(xl >> 24)] + bf.s[1][u8(xl >> 16)]) ^ bf.s[2][u8(xl >> 8)]) +
-		(bf.s[3][u8(xl)] ^ bf.p[3])
-	xl ^= ((bf.s[0][u8(xr >> 24)] + bf.s[1][u8(xr >> 16)]) ^ bf.s[2][u8(xr >> 8)]) +
-		(bf.s[3][u8(xr)] ^ bf.p[4])
-	xr ^= ((bf.s[0][u8(xl >> 24)] + bf.s[1][u8(xl >> 16)]) ^ bf.s[2][u8(xl >> 8)]) +
-		(bf.s[3][u8(xl)] ^ bf.p[5])
-	xl ^= ((bf.s[0][u8(xr >> 24)] + bf.s[1][u8(xr >> 16)]) ^ bf.s[2][u8(xr >> 8)]) +
-		(bf.s[3][u8(xr)] ^ bf.p[6])
-	xr ^= ((bf.s[0][u8(xl >> 24)] + bf.s[1][u8(xl >> 16)]) ^ bf.s[2][u8(xl >> 8)]) +
-		(bf.s[3][u8(xl)] ^ bf.p[7])
-	xl ^= ((bf.s[0][u8(xr >> 24)] + bf.s[1][u8(xr >> 16)]) ^ bf.s[2][u8(xr >> 8)]) +
-		(bf.s[3][u8(xr)] ^ bf.p[8])
-	xr ^= ((bf.s[0][u8(xl >> 24)] + bf.s[1][u8(xl >> 16)]) ^ bf.s[2][u8(xl >> 8)]) +
-		(bf.s[3][u8(xl)] ^ bf.p[9])
-	xl ^= ((bf.s[0][u8(xr >> 24)] + bf.s[1][u8(xr >> 16)]) ^ bf.s[2][u8(xr >> 8)]) +
-		(bf.s[3][u8(xr)] ^ bf.p[10])
-	xr ^= ((bf.s[0][u8(xl >> 24)] + bf.s[1][u8(xl >> 16)]) ^ bf.s[2][u8(xl >> 8)]) +
-		(bf.s[3][u8(xl)] ^ bf.p[11])
-	xl ^= ((bf.s[0][u8(xr >> 24)] + bf.s[1][u8(xr >> 16)]) ^ bf.s[2][u8(xr >> 8)]) +
-		(bf.s[3][u8(xr)] ^ bf.p[12])
-	xr ^= ((bf.s[0][u8(xl >> 24)] + bf.s[1][u8(xl >> 16)]) ^ bf.s[2][u8(xl >> 8)]) +
-		(bf.s[3][u8(xl)] ^ bf.p[13])
-	xl ^= ((bf.s[0][u8(xr >> 24)] + bf.s[1][u8(xr >> 16)]) ^ bf.s[2][u8(xr >> 8)]) +
-		(bf.s[3][u8(xr)] ^ bf.p[14])
-	xr ^= ((bf.s[0][u8(xl >> 24)] + bf.s[1][u8(xl >> 16)]) ^ bf.s[2][u8(xl >> 8)]) +
-		(bf.s[3][u8(xl)] ^ bf.p[15])
-	xl ^= ((bf.s[0][u8(xr >> 24)] + bf.s[1][u8(xr >> 16)]) ^ bf.s[2][u8(xr >> 8)]) +
-		(bf.s[3][u8(xr)] ^ bf.p[16])
+	xr ^= ((bf.s[0][u8(xl >> 24)] + bf.s[1][u8(xl >> 16)]) ^ bf.s[2][u8(xl >> 8)]) + bf.s[3][u8(xl)] ^ bf.p[1]
+	xl ^= ((bf.s[0][u8(xr >> 24)] + bf.s[1][u8(xr >> 16)]) ^ bf.s[2][u8(xr >> 8)]) + bf.s[3][u8(xr)] ^ bf.p[2]
+	xr ^= ((bf.s[0][u8(xl >> 24)] + bf.s[1][u8(xl >> 16)]) ^ bf.s[2][u8(xl >> 8)]) + bf.s[3][u8(xl)] ^ bf.p[3]
+	xl ^= ((bf.s[0][u8(xr >> 24)] + bf.s[1][u8(xr >> 16)]) ^ bf.s[2][u8(xr >> 8)]) + bf.s[3][u8(xr)] ^ bf.p[4]
+	xr ^= ((bf.s[0][u8(xl >> 24)] + bf.s[1][u8(xl >> 16)]) ^ bf.s[2][u8(xl >> 8)]) + bf.s[3][u8(xl)] ^ bf.p[5]
+	xl ^= ((bf.s[0][u8(xr >> 24)] + bf.s[1][u8(xr >> 16)]) ^ bf.s[2][u8(xr >> 8)]) + bf.s[3][u8(xr)] ^ bf.p[6]
+	xr ^= ((bf.s[0][u8(xl >> 24)] + bf.s[1][u8(xl >> 16)]) ^ bf.s[2][u8(xl >> 8)]) + bf.s[3][u8(xl)] ^ bf.p[7]
+	xl ^= ((bf.s[0][u8(xr >> 24)] + bf.s[1][u8(xr >> 16)]) ^ bf.s[2][u8(xr >> 8)]) + bf.s[3][u8(xr)] ^ bf.p[8]
+	xr ^= ((bf.s[0][u8(xl >> 24)] + bf.s[1][u8(xl >> 16)]) ^ bf.s[2][u8(xl >> 8)]) + bf.s[3][u8(xl)] ^ bf.p[9]
+	xl ^= ((bf.s[0][u8(xr >> 24)] + bf.s[1][u8(xr >> 16)]) ^ bf.s[2][u8(xr >> 8)]) + bf.s[3][u8(xr)] ^ bf.p[10]
+	xr ^= ((bf.s[0][u8(xl >> 24)] + bf.s[1][u8(xl >> 16)]) ^ bf.s[2][u8(xl >> 8)]) + bf.s[3][u8(xl)] ^ bf.p[11]
+	xl ^= ((bf.s[0][u8(xr >> 24)] + bf.s[1][u8(xr >> 16)]) ^ bf.s[2][u8(xr >> 8)]) + bf.s[3][u8(xr)] ^ bf.p[12]
+	xr ^= ((bf.s[0][u8(xl >> 24)] + bf.s[1][u8(xl >> 16)]) ^ bf.s[2][u8(xl >> 8)]) + bf.s[3][u8(xl)] ^ bf.p[13]
+	xl ^= ((bf.s[0][u8(xr >> 24)] + bf.s[1][u8(xr >> 16)]) ^ bf.s[2][u8(xr >> 8)]) + bf.s[3][u8(xr)] ^ bf.p[14]
+	xr ^= ((bf.s[0][u8(xl >> 24)] + bf.s[1][u8(xl >> 16)]) ^ bf.s[2][u8(xl >> 8)]) + bf.s[3][u8(xl)] ^ bf.p[15]
+	xl ^= ((bf.s[0][u8(xr >> 24)] + bf.s[1][u8(xr >> 16)]) ^ bf.s[2][u8(xr >> 8)]) + bf.s[3][u8(xr)] ^ bf.p[16]
 	xr ^= bf.p[17]
-	res := [xl, xr]
-	return res
+	return xr, xl
 }
 
 // get_next_word returns the next big-endian u32 value from the byte
