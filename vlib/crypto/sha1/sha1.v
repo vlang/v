316be4d3ba8deb39be97255767b7fe803615a890
shove70 <shove@163.com>
2023-10-30 08:59:50 +0800

    oops: sha1

--- vlib/crypto/sha1/sha1.v
+++ vlib/crypto/sha1/sha1.v
@@ -64,6 +64,14 @@ pub fn (mut d Digest) reset() {
 	d.len = 0
 }
 
+fn (d &Digest) clone() &Digest {
+	return &Digest{
+		...d
+		h: d.h.clone()
+		x: d.x.clone()
+	}
+}
+
 // new returns a new Digest (implementing hash.Hash) computing the SHA1 checksum.
 pub fn new() &Digest {
 	mut d := &Digest{}
@@ -110,7 +118,7 @@ pub fn (mut d Digest) write(p_ []u8) !int {
 // sum returns a copy of the generated sum of the bytes in `b_in`.
 pub fn (d &Digest) sum(b_in []u8) []u8 {
 	// Make a copy of d so that caller can keep writing and summing.
-	mut d0 := *d
+	mut d0 := d.clone()
 	hash := d0.checksum()
 	mut b_out := b_in.clone()
 	for b in hash {
