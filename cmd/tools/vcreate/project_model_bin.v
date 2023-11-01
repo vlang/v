ec45e1633a1a085df94054b89dacd496cde7d6ce
JalonSolov <JalonSolov@gmail.com>
2023-11-01 14:04:56 -0400

    undo vcreate changes

--- cmd/tools/vcreate/project_model_bin.v
+++ cmd/tools/vcreate/project_model_bin.v
@@ -2,7 +2,7 @@ module main
 
 fn (mut c Create) set_bin_project_files(new bool) {
 	c.files << ProjectFiles{
-		path: if new { '${c.name}/src/main.v' } else { 'src/main.c.v' }
+		path: if new { '${c.name}/src/main.v' } else { 'src/main.v' }
 		content: "module main
 
 fn main() {
