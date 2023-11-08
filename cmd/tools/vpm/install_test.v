ee266142c20a009e5e7e27f32baa7dd4db31a6b3
Turiiya <34311583+ttytm@users.noreply.github.com>
2023-11-08 22:56:11 +0100

    add version install tests

--- cmd/tools/vpm/install_test.v
+++ cmd/tools/vpm/install_test.v
@@ -109,3 +109,71 @@ fn test_missing_repo_name_in_url() {
 	assert res.exit_code == 1
 	assert res.output.contains('failed to retrieve module name for `${incomplete_url}`'), res.output
 }
+
+fn test_install_from_vpm_with_git_version_tag() {
+	ident := 'ttytm.webview'
+	mut tag := 'v0.6.0'
+	mut res := os.execute_or_exit('${v} install ${ident}@${tag}')
+	assert res.output.contains('Installing `${ident}`'), res.output
+	assert res.output.contains('Installed `${ident}`'), res.output
+	mut name, mut version := get_mod_name_and_version(os.join_path('ttytm', 'webview'))
+	assert name == 'webview'
+	assert version == '0.6.0'
+	// Install same version without force flag.
+	res = os.execute_or_exit('${v} install ${ident}@${tag}')
+	assert res.output.contains('Module `${ident}@${tag}` is already installed, use --force to overwrite'), res.output
+	// Install another version, add force flag to surpass confirmation.
+	tag = 'v0.5.0'
+	res = os.execute_or_exit('${v} install -f ${ident}@${tag}')
+	assert res.output.contains('Installed `${ident}`'), res.output
+	name, version = get_mod_name_and_version(os.join_path('ttytm', 'webview'))
+	assert name == 'webview'
+	assert version == '0.5.0'
+	// Install invalid version.
+	tag = '6.0'
+	res = os.execute('${v} install -f ${ident}@${tag}')
+	assert res.exit_code == 1
+	assert res.output.contains('failed to install `${ident}`'), res.output
+	// Install invalid version verbose.
+	res = os.execute('${v} install -f -v ${ident}@${tag}')
+	assert res.exit_code == 1
+	assert res.output.contains('failed to install `${ident}`'), res.output
+	assert res.output.contains('Remote branch 6.0 not found in upstream origin'), res.output
+}
+
+fn test_install_from_url_with_git_version_tag() {
+	url := 'https://github.com/vlang/vsl'
+	mut tag := 'v0.1.50'
+	mut res := os.execute_or_exit('v install ${url}@${tag}')
+	assert res.output.contains('Installing `vsl`'), res.output
+	assert res.output.contains('Installed `vsl`'), res.output
+	mut name, mut version := get_mod_name_and_version('vsl')
+	assert name == 'vsl'
+	assert version == '0.1.50'
+	// Install same version without force flag.
+	res = os.execute_or_exit('${v} install ${url}@${tag}')
+	assert res.output.contains('Module `vsl@${tag}` is already installed, use --force to overwrite'), res.output
+	// Install another version, add force flag to surpass confirmation.
+	tag = 'v0.1.47'
+	res = os.execute_or_exit('${v} install -f ${url}@${tag}')
+	assert res.output.contains('Installed `vsl`'), res.output
+	name, version = get_mod_name_and_version('vsl')
+	assert name == 'vsl'
+	assert version == '0.1.47'
+	// Install invalid version.
+	tag = 'abc'
+	res = os.execute('${v} install -f ${url}@${tag}')
+	assert res.exit_code == 1
+	// Install invalid version verbose.
+	res = os.execute('${v} install -f -v ${url}@${tag}')
+	assert res.exit_code == 1
+	assert res.output.contains('Remote branch abc not found in upstream origin'), res.output
+}
+
+fn get_mod_name_and_version(path string) (string, string) {
+	mod := vmod.from_file(os.join_path(test_path, path, 'v.mod')) or {
+		eprintln(err)
+		return '', ''
+	}
+	return mod.name, mod.version
+}
