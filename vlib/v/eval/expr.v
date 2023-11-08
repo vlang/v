4ef8681f3fce76645565f26759bf615f88fad3e7
Artem Yurchenko <zumikq78@gmail.com>
2023-11-08 14:39:45 +0100

    Fix: add support of i32 type to eval

--- vlib/v/eval/expr.v
+++ vlib/v/eval/expr.v
@@ -590,7 +590,7 @@ fn (e Eval) type_to_size(typ ast.Type) u64 {
 				32
 			})
 		}
-		ast.i8_type_idx, ast.i16_type_idx, ast.int_type_idx, ast.i64_type_idx {
+		ast.i8_type_idx, ast.i16_type_idx, ast.int_type_idx, ast.i32_type_idx, ast.i64_type_idx {
 			return u64(math.exp2(f64(typ - 2))) // this formula converts the type number to the bitsize
 		}
 		ast.u8_type_idx, ast.u16_type_idx, ast.u32_type_idx, ast.u64_type_idx {
