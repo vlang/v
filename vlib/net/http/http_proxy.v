e4f55fb2994751f2de590993508b23e98f40742b
Roman <90373295+sibkod@users.noreply.github.com>
2023-11-10 04:56:16 +0700

    net.http: add socks5|http(s) proxy support [Linux] (#19676)

--- vlib/net/http/http_proxy.v
+++ vlib/net/http/http_proxy.v
@@ -1,8 +1,10 @@
 module http
 
-import net.urllib
 import encoding.base64
 import net
+import net.urllib
+import net.ssl
+import net.socks
 
 [heap]
 struct HttpProxy {
@@ -21,7 +23,7 @@ pub fn new_http_proxy(raw_url string) !&HttpProxy {
 	mut url := urllib.parse(raw_url) or { return error('malformed proxy url') }
 	scheme := url.scheme
 
-	if scheme != 'http' && scheme != 'https' {
+	if scheme !in ['http', 'https', 'socks5'] {
 		return error('invalid scheme')
 	}
 
@@ -38,11 +40,14 @@ pub fn new_http_proxy(raw_url string) !&HttpProxy {
 	if port == 0 {
 		if scheme == 'https' {
 			port = 443
+			host += ':' + port.str()
 		} else if scheme == 'http' {
 			port = 80
+			host += ':' + port.str()
 		}
-
-		host += ':' + port.str()
+	}
+	if port == 0 {
+		return error('Unknown port')
 	}
 
 	return &HttpProxy{
@@ -56,21 +61,17 @@ pub fn new_http_proxy(raw_url string) !&HttpProxy {
 	}
 }
 
-fn (proxy &HttpProxy) build_proxy_headers(request &Request, host string, path string) string {
-	ua := request.user_agent
+// host format - ip:port
+fn (pr &HttpProxy) build_proxy_headers(host string) string {
 	mut uheaders := []string{}
-
-	uheaders << 'Host: ${host}\r\n'
-	uheaders << 'User-Agent: ${ua}\r\n'
-	uheaders << 'Upgrade-Insecure-Requests: 1\r\n'
-	// uheaders << 'Proxy-Connection: Keep-Alive\r\n'
-
-	if proxy.username != '' {
+	address := host.all_before_last(':')
+	uheaders << 'Proxy-Connection: Keep-Alive\r\n'
+	if pr.username != '' {
 		mut authinfo := ''
 
-		authinfo += proxy.username
-		if proxy.password != '' {
-			authinfo += ':${proxy.password}'
+		authinfo += pr.username
+		if pr.password != '' {
+			authinfo += ':${pr.password}'
 		}
 
 		encoded_authinfo := base64.encode(authinfo.bytes())
@@ -80,31 +81,87 @@ fn (proxy &HttpProxy) build_proxy_headers(request &Request, host string, path st
 
 	version := Version.v1_1
 
-	// we always make requests on http scheme
-	url := 'http://${host}${path}'
-
-	return '${request.method} ${url} ${version}\r\n' + uheaders.join('') + '\r\n'
+	return 'CONNECT ${host} ${version}\r\nHost: ${address}\r\n' + uheaders.join('') + '\r\n'
 }
 
-fn (proxy &HttpProxy) http_do(host string, method Method, path string, req &Request) !Response {
-	host_name, _ := net.split_address(host)!
-	s := proxy.build_proxy_headers(req, host_name, path)
-	mut client := net.dial_tcp(proxy.host)!
-	client.set_read_timeout(req.read_timeout)
-	client.set_write_timeout(req.write_timeout)
-	// TODO this really needs to be exposed somehow
-	client.write_string(s)!
-	$if trace_http_request ? {
-		eprintln('> ${s}')
+fn (pr &HttpProxy) http_do(host urllib.URL, method Method, path string, req &Request) !Response {
+	host_name, _ := net.split_address(host.hostname())!
+
+	s := req.build_request_headers(req.method, host_name, path)
+	if host.scheme == 'https' {
+		mut client := pr.ssl_dial('${host.host}:443')!
+
+		$if windows {
+			return error('Windows Not SUPPORTED') // todo windows ssl
+			// response_text := req.do_request(req.build_request_headers(req.method, host_name,
+			// 	path))!
+			// client.shutdown()!
+			// return response_text
+		} $else {
+			response_text := req.do_request(req.build_request_headers(req.method, host_name,
+				path), mut client)!
+			client.shutdown()!
+			return response_text
+		}
+	} else if host.scheme == 'http' {
+		mut client := pr.dial('${host.host}:80')!
+		client.set_read_timeout(req.read_timeout)
+		client.set_write_timeout(req.write_timeout)
+		client.write_string(s)!
+		$if trace_http_request ? {
+			eprintln('> ${s}')
+		}
+		mut bytes := req.read_all_from_client_connection(client)!
+		client.close()!
+		response_text := bytes.bytestr()
+		$if trace_http_response ? {
+			eprintln('< ${response_text}')
+		}
+		if req.on_finish != unsafe { nil } {
+			req.on_finish(req, u64(response_text.len))!
+		}
+		return parse_response(response_text)
 	}
-	mut bytes := req.read_all_from_client_connection(client)!
-	client.close()!
-	response_text := bytes.bytestr()
-	$if trace_http_response ? {
-		eprintln('< ${response_text}')
+	return error('Invalid Scheme')
+}
+
+fn (pr &HttpProxy) dial(host string) !&net.TcpConn {
+	if pr.scheme in ['http', 'https'] {
+		mut tcp := net.dial_tcp(pr.host)!
+		tcp.write(pr.build_proxy_headers(host).bytes())!
+		mut bf := []u8{len: 4096}
+
+		tcp.read(mut bf)!
+		return tcp
+	} else if pr.scheme == 'socks5' {
+		return socks.socks5_dial(pr.host, host, pr.username, pr.password)!
+	} else {
+		return error('http_proxy dial: invalid proxy scheme')
 	}
-	if req.on_finish != unsafe { nil } {
-		req.on_finish(req, u64(response_text.len))!
+}
+
+fn (pr &HttpProxy) ssl_dial(host string) !&ssl.SSLConn {
+	if pr.scheme in ['http', 'https'] {
+		mut tcp := net.dial_tcp(pr.host)!
+		tcp.write(pr.build_proxy_headers(host).bytes())!
+		mut bf := []u8{len: 4096}
+		tcp.read(mut bf)!
+		if !bf.bytestr().contains('HTTP/1.1 200') {
+			return error('ssl dial error: ${bf.bytestr()}')
+		}
+
+		mut ssl_conn := ssl.new_ssl_conn(
+			verify: ''
+			cert: ''
+			cert_key: ''
+			validate: false
+			in_memory_verification: false
+		)!
+		ssl_conn.connect(mut tcp, host.all_before_last(':'))!
+		return ssl_conn
+	} else if pr.scheme == 'socks5' {
+		return socks.socks5_ssl_dial(pr.host, host, pr.username, pr.password)!
+	} else {
+		return error('http_proxy ssl_dial: invalid proxy scheme')
 	}
-	return parse_response(response_text)
 }
