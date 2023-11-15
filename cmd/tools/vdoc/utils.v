f2c4d304ffaad38e0ccfe01ee57eb04bfee8e302
Swastik <swstkbaranwal@gmail.com>
2023-11-16 00:49:07 +0530

    vdoc: fix syntax highlighting for symbols before `!` and between `(` and `,`

--- cmd/tools/vdoc/utils.v
+++ cmd/tools/vdoc/utils.v
@@ -213,9 +213,12 @@ fn color_highlight(code string, tb &ast.Table) string {
 					} else if
 						(next_tok.kind in [.lcbr, .rpar, .eof, .comma, .pipe, .name, .rcbr, .assign, .key_pub, .key_mut, .pipe, .comma, .comment, .lt, .lsbr]
 						&& next_tok.lit !in builtin)
-						&& (prev.kind in [.name, .amp, .lcbr, .rsbr, .key_type, .assign, .dot, .question, .rpar, .key_struct, .key_enum, .pipe, .key_interface, .comment, .ellipsis]
+						&& (prev.kind in [.name, .amp, .lcbr, .rsbr, .key_type, .assign, .dot, .not, .question, .rpar, .key_struct, .key_enum, .pipe, .key_interface, .comment, .ellipsis, .comma]
 						&& prev.lit !in builtin) && ((tok.lit != '' && tok.lit[0].is_capital())
 						|| prev_prev.lit in ['C', 'JS']) {
+						tok_typ = .symbol
+					} else if tok.lit[0].is_capital() && prev.kind == .lpar
+						&& next_tok.kind == .comma {
 						tok_typ = .symbol
 					} else if next_tok.kind == .lpar
 						|| (!(tok.lit != '' && tok.lit[0].is_capital())
