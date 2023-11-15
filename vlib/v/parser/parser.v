5669f921002a5a9c03f5e4c514fb906c44770d22
Joe C <joe.conigliaro@gmail.com>
2023-11-15 19:54:15 +1100

    Update vlib/v/parser/parser.v
    Co-authored-by: Delyan Angelov <delian66@gmail.com>

--- vlib/v/parser/parser.v
+++ vlib/v/parser/parser.v
@@ -1815,7 +1815,7 @@ fn (mut p Parser) attributes() {
 	start_pos := p.tok.pos()
 	mut is_at := false
 	if p.tok.kind == .lsbr {
-		p.warn('`[attr]` has been deprecated, use `@[attr]` instead')
+		p.note('`[attr]` has been deprecated, use `@[attr]` instead')
 		// [attr]
 		p.check(.lsbr)
 	} else if p.tok.kind == .at {
