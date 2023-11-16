4dff383e357fabb3fb8486ca9fb7caf06d3f065c
shove <shove@163.com>
2023-11-16 14:20:28 +0800

    parser, checker: fix const var below at struct fixed array fields(fix#19593) (#19893)

--- vlib/v/tests/constant_array_size_test.v
+++ vlib/v/tests/constant_array_size_test.v
@@ -10,3 +10,15 @@ fn test_consant_array_size() {
 	mut b := [c_b_s]int{}
 	b = [1, 2]!
 }
+
+// test const was declared below struct fixed array fields declaration
+struct Foo {
+	posts [max_posts_count]int
+}
+
+const max_posts_count = 5
+
+fn test_const_below_at_struct_fixed_array_fields() {
+	foo := Foo{}
+	assert foo.posts == [0, 0, 0, 0, 0]!
+}
