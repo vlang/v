94aaf17514c2ad2d04b0533bdc968f0826eb6090
Turiiya <34311583+ttytm@users.noreply.github.com>
2023-11-17 18:39:30 +0100

    add http install tests

--- cmd/tools/vpm/install_test.v
+++ cmd/tools/vpm/install_test.v
@@ -52,13 +52,16 @@ fn test_install_from_git_url() {
 	}
 	assert mod.name == 'markdown'
 	assert mod.dependencies == []string{}
-	res = os.execute_or_exit('${v} install https://github.com/vlang/vsl')
-	assert res.output.contains('Installing `vsl`'), res.output
-	mod = vmod.from_file(os.join_path(test_path, 'vsl', 'v.mod')) or {
+	res = os.execute_or_exit('${v} install http://github.com/Wertzui123/HashMap')
+	assert res.output.contains('Installing `HashMap`'), res.output
+	assert res.output.contains('`http` support is deprecated, switch to `https` to ensure future compatibility.'), res.output
+	mod = vmod.from_file(os.join_path(test_path, 'wertzui123', 'hashmap', 'v.mod')) or {
 		assert false, err.msg()
 		return
 	}
-	assert mod.name == 'vsl'
+	res = os.execute_or_exit('${v} install http://github.com/Wertzui123/HashMap')
+	assert res.output.contains('Updating module `wertzui123.hashmap`'), res.output
+	assert res.output.contains('`http` support is deprecated, switch to `https` to ensure future compatibility.'), res.output
 }
 
 fn test_install_already_existent() {
