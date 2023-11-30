300ec1b5d0fbe8741b70e3301a994c2530d90a40
shove70 <shove@163.com>
2023-11-30 15:56:54 +0800

    ast: fix generic structs with multiple levels of generic embedding

--- vlib/v/tests/generics_with_embed_generics_structs_test.v
+++ vlib/v/tests/generics_with_embed_generics_structs_test.v
@@ -2,11 +2,28 @@ struct Foo[T] {
 	field T
 }
 
-struct MainStruct[T] {
+struct Bar[T] {
+	Foo[T]
+}
+
+struct Baz[T] {
+	Bar[T]
+}
+
+struct OneLevelEmbed[T] {
 	Foo[T]
 }
 
 fn test_main() {
-	m := MainStruct[int]{}
+	m := OneLevelEmbed[int]{}
+	assert m.field == 0
+}
+
+struct MultiLevelEmbed[T] {
+	Baz[T]
+}
+
+fn test_multi_level() {
+	m := MultiLevelEmbed[int]{}
 	assert m.field == 0
 }
