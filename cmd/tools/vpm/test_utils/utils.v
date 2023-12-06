5ad3890697f7230704d4da1b8290bbd7b27d1aaa
Turiiya <tobmoj@web.de>
2023-12-07 00:15:34 +0100

    add timeout, just in case

--- cmd/tools/vpm/test_utils/utils.v
+++ cmd/tools/vpm/test_utils/utils.v
@@ -2,6 +2,7 @@ module test_utils
 
 import os
 import net
+import time
 
 fn hg_serve(hg_path string, path string) (&os.Process, int) {
 	mut port := 8000
@@ -17,10 +18,17 @@ fn hg_serve(hg_path string, path string) (&os.Process, int) {
 	p.set_args(['serve', '--print-url', '--port', port.str()])
 	p.set_redirect_stdio()
 	p.run()
+	mut i := 0
 	for p.is_alive() {
+		if i == 500 { // Wait max. 5 seconds.
+			eprintln('Failed to serve mercurial repository on localhost.')
+			exit(1)
+		}
 		if p.stdout_read().contains(':${port}') {
 			break
 		}
+		time.sleep(10 * time.millisecond)
+		i++
 	}
 	return p, port
 }
