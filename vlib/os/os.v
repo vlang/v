4d4a5ae700508534b062e3961740b5be03bcd848
Turiiya <tobmoj@web.de>
2023-12-12 02:30:19 +0100

    revert changes to mv, just keep overwrite option

--- vlib/os/os.v
+++ vlib/os/os.v
@@ -138,17 +138,8 @@ pub fn mv_by_cp(source string, target string, opts MvParams) ! {
 
 // mv moves files or folders from `src` to `dst`.
 pub fn mv(source string, target string, opts MvParams) ! {
-	if exists(target) {
-		if !opts.overwrite {
-			return error('Destination path already exist')
-		}
-		if source != target {
-			if is_dir(target) {
-				rmdir_all(target)!
-			} else {
-				rm(target)!
-			}
-		}
+	if !opts.overwrite && exists(target) {
+		return error('Destination path already exist')
 	}
 	rename(source, target) or { mv_by_cp(source, target, opts)! }
 }
