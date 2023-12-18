fce27aa5619d1753ca48ca5a8a82a0761b11f39a
Felipe Pena <felipensp@gmail.com>
2023-12-18 11:01:14 -0300

    credits

--- vlib/v/comptime/comptimeinfo.v
+++ vlib/v/comptime/comptimeinfo.v
@@ -1,3 +1,5 @@
+// Copyright (c) 2019-2023 V devs. All rights reserved.
+// Use of this source code is governed by an MIT license that can be found in the LICENSE file.
 module comptime
 
 import v.ast
