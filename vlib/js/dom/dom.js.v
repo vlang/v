48eeed3b8d33f0b3a6748cbd3bc0284357111b8b
Irving <moixllik@outlook.com>
2023-12-21 07:23:17 -0500

    js.dom: add querySelector[All] and NodeList

--- vlib/js/dom/dom.js.v
+++ vlib/js/dom/dom.js.v
@@ -206,7 +206,7 @@ pub interface JS.Node {
 	lookupPrefix(namespace JS.String) JS.String
 	normalize()
 	removeChild(child JS.Node) JS.Node
-	replaceChild(node JS.Node, child JS.Node) JS.Npde
+	replaceChild(node JS.Node, child JS.Node) JS.Node
 mut:
 	nodeValue   JS.String
 	textContent JS.String
@@ -378,6 +378,8 @@ pub interface JS.HTMLElement {
 	offsetTop      JS.Number
 	offsetWidth    JS.Number
 	click()
+	querySelector(selectors JS.String) ?JS.HTMLElement
+	querySelectorAll(selectors JS.String) JS.NodeList
 mut:
 	accessKey      JS.String
 	autocapitalize JS.String
@@ -390,6 +392,14 @@ mut:
 	spellcheck     JS.Boolean
 	title          JS.String
 	translate      JS.Boolean
+}
+
+pub type NodeListForEachCb = fn (JS.HTMLElement, JS.Number, JS.NodeList)
+
+pub interface JS.NodeList {
+	length JS.Number
+	forEach(cb NodeListForEachCb, thisArg JS.Any)
+	item(idx JS.Number) ?JS.Any
 }
 
 pub fn JS.HTMLElement.prototype.constructor() JS.HTMLElement
