97417a4931f3e1ffc7c4e2ae368500749002d68b
Felipe Pena <felipensp@gmail.com>
2023-12-24 13:29:09 -0300

    fix

--- vlib/v/parser/parser.v
+++ vlib/v/parser/parser.v
@@ -562,7 +562,7 @@ fn (p &Parser) is_array_type_from_offset(offset int) int {
 		if tok.kind in [.name, .amp] {
 			return i
 		}
-		if tok.kind == .eof {
+		if tok.kind !in [.name, .amp, .lsbr, .rsbr] {
 			break
 		}
 		i++
