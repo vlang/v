55061e40c721079a99c10858db3c46f55f07bfbe
Felipe Pena <felipensp@gmail.com>
2023-12-25 08:49:01 -0300

    cgen: fix fixed array return when returning fixed array initialization (#20262)

--- vlib/v/gen/c/cgen.v
+++ vlib/v/gen/c/cgen.v
@@ -5282,11 +5282,18 @@ fn (mut g Gen) return_stmt(node ast.Return) {
 					if node.exprs[0] is ast.Ident {
 						g.write('memcpy(${tmpvar}.ret_arr, ${g.expr_string(node.exprs[0])}, sizeof(${g.typ(node.types[0])})) /*ret*/')
 					} else if node.exprs[0] in [ast.ArrayInit, ast.StructInit] {
-						tmpvar2 := g.new_tmp_var()
-						g.write('${g.typ(node.types[0])} ${tmpvar2} = ')
-						g.expr_with_cast(node.exprs[0], node.types[0], g.fn_decl.return_type)
-						g.writeln(';')
-						g.write('memcpy(${tmpvar}.ret_arr, ${tmpvar2}, sizeof(${g.typ(node.types[0])})) /*ret*/')
+						if node.exprs[0] is ast.ArrayInit && node.exprs[0].is_fixed
+							&& node.exprs[0].has_init {
+							g.write('memcpy(${tmpvar}.ret_arr, ')
+							g.expr_with_cast(node.exprs[0], node.types[0], g.fn_decl.return_type)
+							g.write(', sizeof(${g.typ(node.types[0])})) /*ret*/')
+						} else {
+							tmpvar2 := g.new_tmp_var()
+							g.write('${g.typ(node.types[0])} ${tmpvar2} = ')
+							g.expr_with_cast(node.exprs[0], node.types[0], g.fn_decl.return_type)
+							g.writeln(';')
+							g.write('memcpy(${tmpvar}.ret_arr, ${tmpvar2}, sizeof(${g.typ(node.types[0])})) /*ret*/')
+						}
 					} else {
 						g.write('memcpy(${tmpvar}.ret_arr, ')
 						g.expr_with_cast(node.exprs[0], node.types[0], g.fn_decl.return_type)
