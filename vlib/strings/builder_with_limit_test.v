7dc1b35d76b34388608ca1f2c5989efd0f2ab7d4
Hitalo Souza <enghitalo@gmail.com>
2024-01-05 04:33:27 -0400

    v fmt vlib/strings -w

--- vlib/strings/builder_with_limit_test.v
+++ vlib/strings/builder_with_limit_test.v
@@ -8,10 +8,10 @@ const max_iterations = 10
 
 fn test_buf_memory() {
 	mut new_sb := strings.new_builder_limited_by_fixed_array([3]u8{})
- mut new_sb2 := strings.new_builder_limited_by_fixed_array([3]u8{})
+	mut new_sb2 := strings.new_builder_limited_by_fixed_array([3]u8{})
 
 	new_sb.write_string('87')
- new_sb2.write_string('87')
+	new_sb2.write_string('87')
 
 	assert ptr_str(new_sb.str().str) != ptr_str(new_sb2.str().str)
 }
