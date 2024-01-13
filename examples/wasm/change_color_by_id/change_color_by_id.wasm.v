dab57ede80c005c4842c78bb3620ff4614a0ac5e
Hitalo Souza <enghitalo@gmail.com>
2024-01-13 11:44:46 -0400

    example: change_color_by_id

---
+++ examples/wasm/change_color_by_id/change_color_by_id.wasm.v
@@ -0,0 +1,14 @@
+fn JS.change_color_by_id(ptr u8, len int, color_ptr u8, color_len int)
+
+// `main` must be public!
+pub fn main() {
+	println('starting main.main...')
+	change_color_by_id('description', 'red')
+	change_color_by_id('description1', 'green')
+	change_color_by_id('description2', 'blue')
+	change_color_by_id('description3', 'black')
+}
+
+fn change_color_by_id(id string, color string) {
+	JS.change_color_by_id(id.str, id.len, color.str, color.len)
+}
