ca3da8b630c3f1307707ac9573c6fdbee919f534
shove <shove@163.com>
2024-01-15 02:01:48 +0800

    ast, markused, checker: fix mark methods into used-list, when generics as receivers (fix #20509) (#20527)

--- vlib/v/ast/ast.v
+++ vlib/v/ast/ast.v
@@ -736,34 +736,35 @@ pub:
 	name_pos token.Pos
 	mod      string
 pub mut:
-	name               string // left.name()
-	is_method          bool
-	is_field           bool // temp hack, remove ASAP when re-impl CallExpr / Selector (joe)
-	is_fn_var          bool // fn variable, `a := fn() {}`, then: `a()`
-	is_fn_a_const      bool // fn const, `const c = abc`, where `abc` is a function, then: `c()`
-	is_keep_alive      bool // GC must not free arguments before fn returns
-	is_noreturn        bool // whether the function/method is marked as [noreturn]
-	is_ctor_new        bool // if JS ctor calls requires `new` before call, marked as `[use_new]` in V
-	is_file_translated bool // true, when the file it resides in is `[translated]`
-	args               []CallArg
-	expected_arg_types []Type
-	comptime_ret_val   bool
-	language           Language
-	or_block           OrExpr
-	left               Expr // `user` in `user.register()`
-	left_type          Type // type of `user`
-	receiver_type      Type // User
-	return_type        Type
-	fn_var_type        Type   // the fn type, when `is_fn_a_const` or `is_fn_var` is true
-	const_name         string // the fully qualified name of the const, i.e. `main.c`, given `const c = abc`, and callexpr: `c()`
-	should_be_skipped  bool   // true for calls to `[if someflag?]` functions, when there is no `-d someflag`
-	concrete_types     []Type // concrete types, e.g. <int, string>
-	concrete_list_pos  token.Pos
-	raw_concrete_types []Type
-	free_receiver      bool // true if the receiver expression needs to be freed
-	scope              &Scope = unsafe { nil }
-	from_embed_types   []Type // holds the type of the embed that the method is called from
-	comments           []Comment
+	name                   string // left.name()
+	is_method              bool
+	is_field               bool // temp hack, remove ASAP when re-impl CallExpr / Selector (joe)
+	is_fn_var              bool // fn variable, `a := fn() {}`, then: `a()`
+	is_fn_a_const          bool // fn const, `const c = abc`, where `abc` is a function, then: `c()`
+	is_keep_alive          bool // GC must not free arguments before fn returns
+	is_noreturn            bool // whether the function/method is marked as [noreturn]
+	is_ctor_new            bool // if JS ctor calls requires `new` before call, marked as `[use_new]` in V
+	is_file_translated     bool // true, when the file it resides in is `[translated]`
+	args                   []CallArg
+	expected_arg_types     []Type
+	comptime_ret_val       bool
+	language               Language
+	or_block               OrExpr
+	left                   Expr // `user` in `user.register()`
+	left_type              Type // type of `user`
+	receiver_type          Type // User
+	receiver_concrete_type Type // We need the receiver to be T in cgen, so save the concrete type to node.receiver_concrete_type
+	return_type            Type
+	fn_var_type            Type   // the fn type, when `is_fn_a_const` or `is_fn_var` is true
+	const_name             string // the fully qualified name of the const, i.e. `main.c`, given `const c = abc`, and callexpr: `c()`
+	should_be_skipped      bool   // true for calls to `[if someflag?]` functions, when there is no `-d someflag`
+	concrete_types         []Type // concrete types, e.g. <int, string>
+	concrete_list_pos      token.Pos
+	raw_concrete_types     []Type
+	free_receiver          bool // true if the receiver expression needs to be freed
+	scope                  &Scope = unsafe { nil }
+	from_embed_types       []Type // holds the type of the embed that the method is called from
+	comments               []Comment
 }
 
 /*
