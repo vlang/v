0725ce8c7910dc4e9389718ddefae539ca252767
Hitalo Souza <enghitalo@gmail.com>
2024-01-17 15:12:15 -0400

    Revert "fix: fmt"
    This reverts commit 96336a2a404d4a0c2602ce7f45c16950c239b58b.

--- vlib/picoev/loop_default.c.v
+++ vlib/picoev/loop_default.c.v
@@ -76,14 +76,13 @@ fn (mut pv Picoev) poll_once(max_wait_in_sec int) int {
 		// Iterates through file descriptors and calls their callbacks for triggered events
 		for target in pv.file_descriptors {
 			if target.loop_id == pv.loop.id {
-				mut read_events := 0
-				if C.FD_ISSET(target.fd, &readfds) != 0 {
-					read_events |= picoev_read
-				}
-				if C.FD_ISSET(target.fd, &writefds) != 0 {
-					read_events |= picoev_write
-				}
-
+				// vfmt off
+				read_events := (
+					(if C.FD_ISSET(target.fd, &readfds) != 0 { picoev_read } else { 0 })
+						|
+					(if C.FD_ISSET(target.fd, &writefds) != 0 { picoev_write } else { 0 })
+				)
+				// vfmt on
 				if read_events != 0 {
 					$if trace_fd ? {
 						eprintln('do callback ${target.fd}')
