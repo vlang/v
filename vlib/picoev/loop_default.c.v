96336a2a404d4a0c2602ce7f45c16950c239b58b
Hitalo Souza <enghitalo@gmail.com>
2024-01-17 12:05:11 -0400

    fix: fmt

--- vlib/picoev/loop_default.c.v
+++ vlib/picoev/loop_default.c.v
@@ -72,13 +72,14 @@ fn (mut pv Picoev) poll_once(max_wait int) int {
 	} else if r > 0 {
 		for target in pv.file_descriptors {
 			if target.loop_id == pv.loop.id {
-				// vfmt off
-				read_events := (
-					(if C.FD_ISSET(target.fd, &readfds) != 0 { picoev_read } else { 0 })
-						|
-					(if C.FD_ISSET(target.fd, &writefds) != 0 { picoev_write } else { 0 })
-				)
-				// vfmt on
+				mut read_events := 0
+				if C.FD_ISSET(target.fd, &readfds) != 0 {
+					read_events |= picoev_read
+				}
+				if C.FD_ISSET(target.fd, &writefds) != 0 {
+					read_events |= picoev_write
+				}
+
 				if read_events != 0 {
 					$if trace_fd ? {
 						eprintln('do callback ${target.fd}')
