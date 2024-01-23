30fd97f3f604a87313c945e6de65407f64d6f71b
Casper Kuethe <casperck64@gmail.com>
2024-01-23 17:58:41 +0100

    add sessions module

---
+++ vlib/x/vweb/sessions/tests/memory_store_test.v
@@ -0,0 +1,52 @@
+import time
+import x.vweb.sessions
+
+const max_age = time.second
+
+pub struct User {
+	name string
+	age  int
+}
+
+const default_user = User{
+	name: 'john'
+	age: 99
+}
+
+fn test_store_set() {
+	mut store := sessions.MemoryStore[User]{}
+	store.set('a', default_user)
+
+	// check if created at time is not empty
+	assert store.data['a'].created_at != time.Time{}
+	assert store.data['a'].data == default_user
+
+	first_created := store.data['a'].created_at
+	store.set('a', User{ age: 99 })
+
+	assert store.data['a'].created_at == first_created
+	assert store.data['a'].data.age == 99
+}
+
+fn test_store_get() {
+	mut store := sessions.MemoryStore[User]{}
+	store.set('a', default_user)
+
+	if data := store.get('a', max_age) {
+		assert data == default_user
+	} else {
+		assert true == false, 'session data should not be none'
+	}
+}
+
+fn test_store_session_expired() {
+	mut store := sessions.MemoryStore[User]{}
+	store.set('a', default_user)
+
+	time.sleep(2 * max_age)
+
+	if data := store.get('a', max_age) {
+		assert true == false, 'session should be expired!'
+	}
+	assert store.data.len == 0
+}
