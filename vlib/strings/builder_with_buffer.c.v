c538bc6a8e253187bb85f50ba5e0482677d15c1b
Hitalo Souza <enghitalo@gmail.com>
2024-01-24 12:27:57 -0400

    ops

--- vlib/strings/builder_with_buffer.c.v
+++ vlib/strings/builder_with_buffer.c.v
@@ -5,10 +5,10 @@ struct BuilderWithBuffer {
 	initial_buffer_cap int
 mut:
 	buf        []u8
-	char_count int
+	char_count u32
 }
 
-// new_builder_limited_by_fixed_array
+// new_builder_with_buffer
 pub fn new_builder_with_buffer(buffer []u8) BuilderWithBuffer {
 	return BuilderWithBuffer{
 		buf: buffer // [array already allocate]
@@ -18,19 +18,19 @@ pub fn new_builder_with_buffer(buffer []u8) BuilderWithBuffer {
 
 // write_string
 @[direct_array_access]
-pub fn (mut b BuilderWithBuffer) write_string(s string) ! {
+pub fn (mut b BuilderWithBuffer) write_string(s string) {
 	if s.len == 0 {
 		return
 	}
 	// try keep at leat one zero at end
 	if b.char_count + s.len + 1 > b.initial_buffer_cap {
-		error('string overflowing inital capacity. To performance improvement increase array capacity from []u8{cap: ${b.initial_buffer_cap}} to []u8{cap: ${
+		eprintln('string overflowing inital capacity. To performance improvement increase array capacity from []u8{cap: ${b.initial_buffer_cap}} to []u8{cap: ${
 			b.char_count + s.len + 1}} or more')
 	}
 
 	unsafe { b.buf.push_many(s.str, s.len) }
 
-	b.char_count += s.len
+	b.char_count += u32(s.len)
 }
 
 // str
--- vlib/strings/builder_with_buffer_test.v
+++ vlib/strings/builder_with_buffer_test.v
@@ -7,11 +7,12 @@ fn test_buf_memory() {
 	mut new_sb := strings.new_builder_with_buffer([]u8{cap: 3})
 	mut new_sb2 := strings.new_builder_with_buffer([]u8{cap: 3})
 
-	new_sb.write_string('87')!
-	new_sb2.write_string('87')!
+	new_sb.write_string('87')
+	new_sb2.write_string('87')
 
 	assert ptr_str(new_sb.str().str) != ptr_str(new_sb2.str().str)
-	new_sb.free()
+
+	unsafe { new_sb.free() }
 }
 
 fn test_compatibility() {
@@ -20,7 +21,7 @@ fn test_compatibility() {
 	mut b := benchmark.start()
 
 	for _ in 0 .. max_iterations {
-		new_sb.write_string('87')!
+		new_sb.write_string('87')
 	}
 	str_from_new_sb := new_sb.str()
 	b.measure('new_builder_with_buffer([]u8{cap: n})')
@@ -36,5 +37,5 @@ fn test_compatibility() {
 	b.measure('strings.new_builder(n)')
 	assert str_from_old_sb == str_from_new_sb
 
-	new_sb.free()
+	unsafe { new_sb.free() }
 }
