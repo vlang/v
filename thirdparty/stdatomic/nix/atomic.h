6d3244531f67945ec08045c144f44332b87ceb18
kbkpbot <kbkpbot@gmail.com>
2024-02-05 20:23:01 +0800

    If apply tcc commit 105d70f7b4ce7706652853ece2e35ea8c774005f, then our  can be simplified

--- thirdparty/stdatomic/nix/atomic.h
+++ thirdparty/stdatomic/nix/atomic.h
@@ -16,85 +16,12 @@
 // Since V might be confused with "generic" C functions either we provide special versions
 // for gcc/clang, too
 
-#ifdef __TINYC__
-	// Add atomic_thread_fence before load, to ensure memory barrier work
-	static inline unsigned long long atomic_load_u64(unsigned long long* x) {
-		atomic_thread_fence(memory_order_acquire);
-		return atomic_load_explicit((_Atomic (unsigned long long)*)x, memory_order_seq_cst);
-	}
-	static inline void* atomic_load_ptr(void** x) {
-		atomic_thread_fence(memory_order_acquire);
-		return (void*)atomic_load_explicit((_Atomic(uintptr_t)*)x, memory_order_seq_cst);
-	}
-	static inline unsigned atomic_load_u32(unsigned* x) {
-		atomic_thread_fence(memory_order_acquire);
-		return atomic_load_explicit((_Atomic(unsigned)*)x, memory_order_seq_cst);
-	}
-	static inline unsigned short atomic_load_u16(unsigned short* x) {
-		atomic_thread_fence(memory_order_acquire);
-		return atomic_load_explicit((_Atomic(unsigned short)*)x, memory_order_seq_cst);
-	}
-	static inline unsigned char atomic_load_byte(unsigned char* x) {
-		atomic_thread_fence(memory_order_acquire);
-		return atomic_load_explicit((_Atomic(unsigned char)*)x, memory_order_seq_cst);
-	}
-	
-	// Add atomic_thread_fence after store, to ensure memory barrier work
-	static inline void atomic_store_u64(unsigned long long* x, unsigned long long y) {
-		atomic_store_explicit((_Atomic(unsigned long long)*)x, y, memory_order_seq_cst);
-		atomic_thread_fence(memory_order_release);
-	}
-	static inline void atomic_store_ptr(void** x, void* y) {
-		atomic_store_explicit((_Atomic(uintptr_t)*)x, (uintptr_t)y, memory_order_seq_cst);
-		atomic_thread_fence(memory_order_release);
-	}
-	static inline void atomic_store_u32(unsigned* x, unsigned y) {
-		atomic_store_explicit((_Atomic(unsigned)*)x, y, memory_order_seq_cst);
-		atomic_thread_fence(memory_order_release);
-	}
-	static inline void atomic_store_u16(void* x, unsigned short y) {
-		atomic_store_explicit((_Atomic(unsigned short)*)x, y, memory_order_seq_cst);
-		atomic_thread_fence(memory_order_release);
-	}
-	static inline void atomic_store_byte(unsigned char* x, unsigned char y) {
-		atomic_store_explicit((_Atomic(unsigned char)*)x, y, memory_order_seq_cst);
-		atomic_thread_fence(memory_order_release);
-	}
-#else
-	static inline unsigned long long atomic_load_u64(unsigned long long* x) {
-		return atomic_load_explicit((_Atomic (unsigned long long)*)x, memory_order_seq_cst);
-	}
-	static inline void* atomic_load_ptr(void** x) {
-		return (void*)atomic_load_explicit((_Atomic(uintptr_t)*)x, memory_order_seq_cst);
-	}
-	static inline unsigned atomic_load_u32(unsigned* x) {
-		return atomic_load_explicit((_Atomic(unsigned)*)x, memory_order_seq_cst);
-	}
-	static inline unsigned short atomic_load_u16(unsigned short* x) {
-		return atomic_load_explicit((_Atomic(unsigned short)*)x, memory_order_seq_cst);
-	}
-	static inline unsigned char atomic_load_byte(unsigned char* x) {
-		atomic_thread_fence(memory_order_acquire);
-		return atomic_load_explicit((_Atomic(unsigned char)*)x, memory_order_seq_cst);
-	}
-	static inline void atomic_store_u64(unsigned long long* x, unsigned long long y) {
-		atomic_store_explicit((_Atomic(unsigned long long)*)x, y, memory_order_seq_cst);
-	}
-	static inline void atomic_store_ptr(void** x, void* y) {
-		atomic_store_explicit((_Atomic(uintptr_t)*)x, (uintptr_t)y, memory_order_seq_cst);
-	}
-	static inline void atomic_store_u32(unsigned* x, unsigned y) {
-		atomic_store_explicit((_Atomic(unsigned)*)x, y, memory_order_seq_cst);
-	}
-	static inline void atomic_store_u16(void* x, unsigned short y) {
-		atomic_store_explicit((_Atomic(unsigned short)*)x, y, memory_order_seq_cst);
-	}
-	static inline void atomic_store_byte(unsigned char* x, unsigned char y) {
-		atomic_store_explicit((_Atomic(unsigned char)*)x, y, memory_order_seq_cst);
-		atomic_thread_fence(memory_order_release);
-	}
-#endif
-
+static inline unsigned long long atomic_load_u64(unsigned long long* x) {
+	return atomic_load_explicit((_Atomic (unsigned long long)*)x, memory_order_seq_cst);
+}
+static inline void atomic_store_u64(unsigned long long* x, unsigned long long y) {
+	atomic_store_explicit((_Atomic(unsigned long long)*)x, y, memory_order_seq_cst);
+}
 static inline int atomic_compare_exchange_weak_u64(unsigned long long* x, unsigned long long* expected, unsigned long long y) {
 	return (int)atomic_compare_exchange_weak_explicit((_Atomic(unsigned long long)*)x, expected, y, memory_order_seq_cst, memory_order_seq_cst);
 }
@@ -121,6 +48,12 @@ static inline unsigned long long atomic_fetch_xor_u64(unsigned long long* x, uns
 }
 
 
+static inline void* atomic_load_ptr(void** x) {
+	return (void*)atomic_load_explicit((_Atomic(uintptr_t)*)x, memory_order_seq_cst);
+}
+static inline void atomic_store_ptr(void** x, void* y) {
+	atomic_store_explicit((_Atomic(uintptr_t)*)x, (uintptr_t)y, memory_order_seq_cst);
+}
 static inline int atomic_compare_exchange_weak_ptr(void** x, void** expected, void* y) {
 	return (int)atomic_compare_exchange_weak_explicit((_Atomic(uintptr_t)*)x, (unsigned long *)expected, (uintptr_t)y, memory_order_seq_cst, memory_order_seq_cst);
 }
@@ -147,6 +80,12 @@ static inline void* atomic_fetch_xor_ptr(void** x, void* y) {
 }
 
 
+static inline unsigned atomic_load_u32(unsigned* x) {
+	return atomic_load_explicit((_Atomic(unsigned)*)x, memory_order_seq_cst);
+}
+static inline void atomic_store_u32(unsigned* x, unsigned y) {
+	atomic_store_explicit((_Atomic(unsigned)*)x, y, memory_order_seq_cst);
+}
 static inline int atomic_compare_exchange_weak_u32(unsigned* x, unsigned* expected, unsigned y) {
 	return (int)atomic_compare_exchange_weak_explicit((_Atomic(unsigned)*)x, expected, y, memory_order_seq_cst, memory_order_seq_cst);
 }
@@ -172,6 +111,12 @@ static inline unsigned atomic_fetch_xor_u32(unsigned* x, unsigned y) {
 	return atomic_fetch_xor_explicit((_Atomic(unsigned)*)x, y, memory_order_seq_cst);
 }
 
+static inline unsigned short atomic_load_u16(unsigned short* x) {
+	return atomic_load_explicit((_Atomic(unsigned short)*)x, memory_order_seq_cst);
+}
+static inline void atomic_store_u16(void* x, unsigned short y) {
+	atomic_store_explicit((_Atomic(unsigned short)*)x, y, memory_order_seq_cst);
+}
 static inline int atomic_compare_exchange_weak_u16(void* x, unsigned short* expected, unsigned short y) {
 	return (int)atomic_compare_exchange_weak_explicit((_Atomic(unsigned short)*)x, expected, y, memory_order_seq_cst, memory_order_seq_cst);
 }
@@ -197,6 +142,14 @@ static inline unsigned short atomic_fetch_xor_u16(unsigned short* x, unsigned sh
 	return atomic_fetch_xor_explicit((_Atomic(unsigned short)*)x, y, memory_order_seq_cst);
 }
 
+static inline unsigned char atomic_load_byte(unsigned char* x) {
+	atomic_thread_fence(memory_order_acquire);
+	return atomic_load_explicit((_Atomic(unsigned char)*)x, memory_order_seq_cst);
+}
+static inline void atomic_store_byte(unsigned char* x, unsigned char y) {
+	atomic_store_explicit((_Atomic(unsigned char)*)x, y, memory_order_seq_cst);
+	atomic_thread_fence(memory_order_release);
+}
 static inline int atomic_compare_exchange_weak_byte(unsigned char* x, unsigned char* expected, unsigned char y) {
 	return (int)atomic_compare_exchange_weak_explicit((_Atomic(unsigned char)*)x, expected, y, memory_order_seq_cst, memory_order_seq_cst);
 }
