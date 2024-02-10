4eca32b33f18ea1278cfaf9985845dd956664a46
Delyan Angelov <delian66@gmail.com>
2024-02-10 16:21:51 +0200

    simplify test, skip it on more CI jobs

--- vlib/v/tests/orm_create_several_tables_test.v
+++ vlib/v/tests/orm_create_several_tables_test.v
@@ -1,6 +1,5 @@
 module main
 
-import os
 import db.sqlite
 
 @[table: 'visits']
@@ -23,24 +22,12 @@ struct User {
 	sites []Site @[fkey: 'owner']
 }
 
-fn init_db(path string) !sqlite.DB {
-	mut db := sqlite.connect(path)!
-
+fn test_creating_db() {
+	mut db := sqlite.connect(':memory:')!
 	sql db {
 		create table User
 		create table Site
 		create table Visit
 	}!
-
-	return db
-}
-
-fn test_creating_db() {
-	db_folder := os.join_path(os.vtmp_dir(), 'creating_db')
-	os.mkdir_all(db_folder)!
-	db_path := os.join_path(db_folder, 'abc.db')
-	dump(db_path)
-	mut db := init_db(db_path)!
-	dump(db)
 	db.close()!
 }
