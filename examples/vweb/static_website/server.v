26b0474c54ea56318ba100a85507470bf650110b
Delyan Angelov <delian66@gmail.com>
2024-02-13 12:29:43 +0200

    run vfmt, explain why os.chdir(os.dir(os.executable()))! is needed

--- examples/vweb/static_website/server.v
+++ examples/vweb/static_website/server.v
@@ -1,6 +1,7 @@
 module main
 
 import x.vweb
+import os
 
 pub struct Context {
 	vweb.Context
@@ -11,10 +12,10 @@ pub struct App {
 }
 
 fn main() {
+	// make sure that the working folder is the one, containing the executable,
+	// so that 'dist' is a valid relative path from it later:
 	os.chdir(os.dir(os.executable()))!
-	port := 8080
-
 	mut app := &App{}
 	app.handle_static('dist', true)!
-	vweb.run[App, Context](mut app, port)
+	vweb.run[App, Context](mut app, 8080)
 }
