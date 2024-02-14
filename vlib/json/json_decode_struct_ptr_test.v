1788a9ee80314761aeacfe7d24c0793c4203a36a
Felipe Pena <felipensp@gmail.com>
2024-02-14 18:30:31 -0300

    fix tests

--- vlib/json/json_decode_struct_ptr_test.v
+++ vlib/json/json_decode_struct_ptr_test.v
@@ -10,13 +10,13 @@ mut:
 fn test_main() {
 	mut json_data := '{"id": 1, "text": "Hello", "reply_to": {"id": 2, "text": "Hi"}}'
 	mut message := json.decode(Message, json_data)!
-	println(message.reply_to.id == 2)
+	assert message.reply_to.id == 2
 
 	json_data = '{"id": 1, "text": "Hello", "reply_to": {"id": 2, "text": "Hi", "reply_to": {}}}'
 	message = json.decode(Message, json_data)!
-	println(message.reply_to.reply_to == unsafe { nil })
+	assert message.reply_to.reply_to.reply_to == unsafe { nil }
 
 	json_data = '{"id": 1, "text": "Hello", "reply_to": {"id": 2, "text": "Hi", "reply_to": {"id": 5}}}'
 	message = json.decode(Message, json_data)!
-	println(message.reply_to.reply_to.id == 5)
+	assert message.reply_to.reply_to.id == 5
 }
