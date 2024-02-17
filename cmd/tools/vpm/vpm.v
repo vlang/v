8b52420035b0f01afc2c83720e7c536d378a607e
Turiiya <34311583+ttytm@users.noreply.github.com>
2024-02-17 15:54:45 +0700

    tools.vpm: fix remove command on Windows, add test (#20852)

--- cmd/tools/vpm/vpm.v
+++ cmd/tools/vpm/vpm.v
@@ -97,7 +97,7 @@ fn vpm_remove(query []string) {
 		final_module_path := get_path_of_existing_module(m) or { continue }
 		println('Removing module "${m}" ...')
 		vpm_log(@FILE_LINE, @FN, 'removing: ${final_module_path}')
-		os.rmdir_all(final_module_path) or { vpm_error(err.msg(), verbose: true) }
+		rmdir_all(final_module_path) or { vpm_error(err.msg(), verbose: true) }
 		// Delete author directory if it is empty.
 		author := m.split('.')[0]
 		author_dir := os.real_path(os.join_path(settings.vmodules_path, author))
@@ -106,7 +106,7 @@ fn vpm_remove(query []string) {
 		}
 		if os.is_dir_empty(author_dir) {
 			verbose_println('Removing author folder ${author_dir}')
-			os.rmdir(author_dir) or { vpm_error(err.msg(), verbose: true) }
+			rmdir_all(author_dir) or { vpm_error(err.msg(), verbose: true) }
 		}
 	}
 }
