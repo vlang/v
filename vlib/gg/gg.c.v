4967c12b5b4fcd494c3dbeb9686889d77d367143
Jan Kåre Vatne <jkvatne@online.no>
2024-02-22 12:58:54 +0100

    Handle dpi change when moving window to another monitor

--- vlib/gg/gg.c.v
+++ vlib/gg/gg.c.v
@@ -406,6 +406,11 @@ fn gg_event_fn(ce voidptr, user_data voidptr) {
 			}
 		}
 		.resized {
+			ctx.scale = dpi_scale()
+			mut ft_scale := &ctx.ft.scale
+			unsafe {
+				*ft_scale = ctx.scale
+			}
 			if ctx.config.resized_fn != unsafe { nil } {
 				ctx.config.resized_fn(e, ctx.config.user_data)
 			}
