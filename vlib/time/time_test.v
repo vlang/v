8c9003fbd3497a4ce64ad572f76a2aeef6b36871
Hitalo Souza <63821277+enghitalo@users.noreply.github.com>
2024-02-26 05:49:12 -0400

    time: add a tm_gmtoff field to `C.tm` (a BSD and GNU extension) (#20907)

--- vlib/time/time_test.v
+++ vlib/time/time_test.v
@@ -344,3 +344,16 @@ fn test_parse_three_letters_month() {
 	tm_tm := time.parse_format(tm_s, format)!
 	assert tm_tm.month == tm.month
 }
+
+fn test_tm_gmtoff() {
+	$if windows {
+		return
+	} $else {
+		rawtime := i64(0) // C.time_t{}
+
+		C.time(&rawtime) // C.tm{}
+
+		info := C.localtime(&rawtime)
+		assert info.tm_gmtoff == time.now().unix - time.utc().unix
+	}
+}
