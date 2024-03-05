02034497253fd009200ab28c47f18981ab63a70a
Hitalo Souza <enghitalo@gmail.com>
2024-03-05 16:21:51 -0400

    test nested struct

--- vlib/x/json/tests/decode_struct_test.v
+++ vlib/x/json/tests/decode_struct_test.v
@@ -5,6 +5,10 @@ mut:
 	val T
 }
 
+pub struct StructTypeSub {
+	test string
+}
+
 fn test_types() {
 	assert json.decode[StructType[string]]('{"val": ""}')!.val == ''
 	assert json.decode[StructType[string]]('{"val": "0"}')!.val == '0'
@@ -14,4 +18,6 @@ fn test_types() {
 	assert json.decode[StructType[int]]('{"val": 0}')!.val == 0
 	assert json.decode[StructType[int]]('{"val": 1}')!.val == 1
 	assert json.decode[StructType[int]]('{"val": 2}')!.val == 2
+
+	assert json.decode[StructType[StructTypeSub]]('{"val": {"test": "test"}}')!.val.test == 'test'
 }
