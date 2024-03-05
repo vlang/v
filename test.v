d7950ed358a094575edab5b58b019d3f89cb7c39
MCausc78 <mcausc78@gmail.com>
2024-03-05 18:14:31 +0200

    didn't mean to add test.v

--- test.v
+++
@@ -1,9 +0,0 @@
-struct Undefined {}
-
-@[on_generic_duplicate: 'note: Sentinels are internal']
-pub type UndefinedOr[T] = Undefined | T
-
-fn main() {
-	x := UndefinedOr[Undefined](Undefined{})
-	dump(x)
-}
