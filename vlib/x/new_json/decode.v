96421259bbb4fc54ab2158562637fb02867e3d92
Hitalo Souza <enghitalo@gmail.com>
2024-03-06 21:01:57 -0400

    new features

--- vlib/x/new_json/decode.v
+++ vlib/x/new_json/decode.v
@@ -14,6 +14,15 @@ mut:
 	idx int
 }
 
+pub enum ValueKind {
+	unknown
+	array
+	object
+	string_
+	number
+	boolean
+}
+
 // check_json
 fn check_json(val string) ! {
 	if val.len == 0 {
@@ -43,10 +52,6 @@ pub fn decode[T](val string) !T {
 // decode_value
 fn (mut decoder Decoder) decode_value[T](nodes []Node, val &T) {
 	$if val is $option {
-		workaround := val
-		if workaround != none {
-			decoder.decode_value(nodes, workaround)
-		}
 	} $else $if T is string {
 	} $else $if T is $sumtype {
 		$for v in val.variants {
@@ -69,6 +74,57 @@ fn (mut decoder Decoder) decode_value[T](nodes []Node, val &T) {
 	}
 }
 
+fn get_value_kind(value rune) ValueKind {
+	mut value_kind := ValueKind.unknown
+
+	if value == `"` {
+		value_kind = .string_
+	} else if value == `t` || value == `f` {
+		value_kind = .boolean
+	} else if value == `{` {
+		value_kind = .object
+	} else if value == `[` {
+		value_kind = .array
+	} else if value >= `0` && value <= `9` {
+		value_kind = .number
+	}
+	return value_kind
+}
+
+// decode_optional_value_in_actual_node
+fn (mut decoder Decoder) decode_optional_value_in_actual_node[T](node Node, val ?T) T {
+	start := (node.key_pos + node.key_len) + 3
+	mut end := start
+	for {
+		if decoder.json_data[end] == `,` || decoder.json_data[end] == `}` {
+			break
+		}
+
+		end++
+	}
+	mut value_kind := get_value_kind(decoder.json_data[start])
+
+	$if T is string {
+		if value_kind == .string_ {
+			return decoder.json_data[start + 1..end - 1]
+		} else if value_kind == .object {
+		} else if value_kind == .array {
+		} else {
+			return decoder.json_data[start..end]
+		}
+		return ''
+	} $else $if T is $int {
+		if value_kind == .string_ {
+			return decoder.json_data[start + 1..end - 1].int()
+		} else if value_kind == .object {
+		} else if value_kind == .array {
+		} else {
+			return decoder.json_data[start..end].int()
+		}
+	}
+	return T{}
+}
+
 // decode_struct
 fn (mut decoder Decoder) decode_struct[T](nodes []Node, value &T) {
 	$for field in T.fields {
@@ -79,36 +135,181 @@ fn (mut decoder Decoder) decode_struct[T](nodes []Node, value &T) {
 				if unsafe {
 					vmemcmp(decoder.json_data.str + node.key_pos, field.name.str, field.name.len) == 0
 				} {
-					$if field.typ is string {
-						start := (node.key_pos + node.key_len) + 4
-						mut end := start
-						for {
-							if decoder.json_data[end] == `"` {
-								if decoder.json_data[end + 1] == `,`
-									|| decoder.json_data[end + 1] == `}` {
-									break
-								}
-							}
-							end++
+					start := (node.key_pos + node.key_len) + 3
+					mut end := start
+					for {
+						if decoder.json_data[end] == `,` || decoder.json_data[end] == `}` {
+							break
 						}
-						value.$(field.name) = decoder.json_data[start..end]
-					} $else $if field.typ is $int {
-						start := (node.key_pos + node.key_len) + 3
-						mut end := start
-						for {
-							if decoder.json_data[end] == `,` || decoder.json_data[end] == `}` {
-								break
-							}
 
-							end++
+						end++
+					}
+
+					mut value_kind := get_value_kind(decoder.json_data[start])
+					$if field.indirections != 0 {
+						// REVIEW Needs clone?
+						$if field.indirections == 1 {
+							// TODO
+							// unsafe {
+							// 	value.$(field.name) = &(decoder.json_data[start + 1..end - 1])
+							// }
+						} $else $if field.indirections == 2 {
+							// TODO
+							// unsafe {
+							// 	value.$(field.name) = &&(decoder.json_data[start + 1..end - 1])
+							// }
+						} $else $if field.indirections == 3 {
+							// TODO
+							// unsafe {
+							// 	value.$(field.name) = &&&(decoder.json_data[start + 1..end - 1])
+							// }
 						}
-						value.$(field.name) = decoder.json_data[start..end].int()
-					} $else $if T is bool {
-					} $else $if T is time.Time {
-					} $else $if T is $struct {
+					} $else $if field.typ is $option {
+						value.$(field.name) = decoder.decode_optional_value_in_actual_node(node,
+							value.$(field.name))
+					} $else $if field.typ is $sumtype {
+						// dump(value.$(field.name))
+
+						workaround := value.$(field.name)
+						// z := value.$(field.name)
+
+						$for v in workaround.variants {
+							$if v.typ is string {
+								if value_kind == .string_ {
+									value.$(field.name) = decoder.json_data[start + 1..end - 1]
+								} else {
+									value.$(field.name) = decoder.json_data[start..end]
+								}
+							} $else $if v.typ in [$int, $float] {
+								$if v.typ is u32 {
+									value.$(field.name) = decoder.json_data[start..end].u32()
+								} $else $if v.typ is u32 {
+								}
+
+								$if v.typ is i8 {
+									value.$(field.name) = decoder.json_data[start..end].i8()
+								} $else $if v.typ is i16 {
+									value.$(field.name) = decoder.json_data[start..end].i16()
+								} $else $if v.typ is i32 {
+									value.$(field.name) = decoder.json_data[start..end].i32()
+								} $else $if v.typ is int {
+									value.$(field.name) = decoder.json_data[start..end].int()
+								} $else $if v.typ is i64 {
+									value.$(field.name) = decoder.json_data[start..end].i64()
+								} $else $if v.typ is u8 {
+									value.$(field.name) = decoder.json_data[start..end].u8()
+								} $else $if v.typ is u16 {
+									value.$(field.name) = decoder.json_data[start..end].u16()
+								} $else $if v.typ is u32 {
+									value.$(field.name) = decoder.json_data[start..end].u32()
+								} $else $if v.typ is u64 {
+									value.$(field.name) = decoder.json_data[start..end].u64()
+								} $else $if v.typ is f32 {
+									value.$(field.name) = decoder.json_data[start..end].f32()
+								} $else $if v.typ is f64 {
+									value.$(field.name) = decoder.json_data[start..end].f64()
+								}
+							} $else $if v.typ is bool {
+								if decoder.json_data[start] == `t` {
+									value.$(field.name) = true
+								} else if decoder.json_data[start] == `f` {
+									value.$(field.name) = false
+								}
+							} $else $if v.typ is time.Time {
+								if value_kind == .string_ {
+									value.$(field.name) = time.parse(decoder.json_data[start + 1..end - 1]) or {
+										time.Time{}
+									}
+								}
+							} $else $if v.typ is $struct {
+								if node.children != none {
+									// FIXME
+									// decoder.decode_value(node.children or {
+									// 	panic('It will never happens')
+									// }, value.$(field.name))
+								}
+							} $else $if v.typ is $array {
+								if value_kind == .array {
+									// TODO
+								}
+							} $else $if v.typ is $map {
+								if value_kind == .object {
+									// TODO
+								}
+							} $else {
+								eprintln('not supported')
+							}
+						}
+						if value_kind == .string_ {
+							value.$(field.name) = decoder.json_data[start + 1..end - 1]
+						} else if decoder.json_data[start] == `t` {
+							value.$(field.name) = true
+						} else if decoder.json_data[start] == `f` {
+							value.$(field.name) = false
+						} else if value_kind == .object {
+						} else if value_kind == .array {
+						} else if value_kind == .number {
+							// value.$(field.name) = decoder.json_data[start..end].int()
+						} else {
+						}
+					} $else $if field.typ is string {
+						if value_kind == .string_ {
+							value.$(field.name) = decoder.json_data[start + 1..end - 1]
+						} else {
+							value.$(field.name) = decoder.json_data[start..end]
+						}
+					} $else $if field.typ in [$int, $float] {
+						$if field.typ is i8 {
+							value.$(field.name) = decoder.json_data[start..end].i8()
+						} $else $if field.typ is i16 {
+							value.$(field.name) = decoder.json_data[start..end].i16()
+						} $else $if field.typ is i32 {
+							value.$(field.name) = decoder.json_data[start..end].i32()
+						} $else $if field.typ is int {
+							value.$(field.name) = decoder.json_data[start..end].int()
+						} $else $if field.typ is i64 {
+							value.$(field.name) = decoder.json_data[start..end].i64()
+						} $else $if field.typ is u8 {
+							value.$(field.name) = decoder.json_data[start..end].u8()
+						} $else $if field.typ is u16 {
+							value.$(field.name) = decoder.json_data[start..end].u16()
+						} $else $if field.typ is u32 {
+							value.$(field.name) = decoder.json_data[start..end].u32()
+						} $else $if field.typ is u64 {
+							value.$(field.name) = decoder.json_data[start..end].u64()
+						} $else $if field.typ is f32 {
+							value.$(field.name) = decoder.json_data[start..end].f32()
+						} $else $if field.typ is f64 {
+							value.$(field.name) = decoder.json_data[start..end].f64()
+						}
+					} $else $if field.typ is bool {
+						if decoder.json_data[start] == `t` {
+							value.$(field.name) = true
+						} else if decoder.json_data[start] == `f` {
+							value.$(field.name) = false
+						}
+					} $else $if field.typ is time.Time {
+						if value_kind == .string_ {
+							value.$(field.name) = time.parse(decoder.json_data[start + 1..end - 1]) or {
+								time.Time{}
+							}
+						}
+					} $else $if field.typ is $struct {
 						if node.children != none {
 							decoder.decode_value(node.children or { panic('It will never happens') },
 								value.$(field.name))
+						}
+					} $else $if field.typ is $array {
+						if value_kind == .array {
+							// TODO
+						}
+					} $else $if field.typ is $map {
+						if value_kind == .object {
+							if node.children != none {
+								decoder.decode_map(node.children or {
+									panic('It will never happens')
+								}, mut value.$(field.name))
+							}
 						}
 					} $else {
 						eprintln('not supported')
@@ -116,6 +317,34 @@ fn (mut decoder Decoder) decode_struct[T](nodes []Node, value &T) {
 					break
 				}
 			}
+		}
+		// }
+	}
+}
+
+// fn (mut decoder Decoder) decode_map[K, V](nodes []Node, mut val map[K]V) {
+fn (mut decoder Decoder) decode_map[T](nodes []Node, mut val T) {
+	for i := 0; i < nodes.len; i++ {
+		mut node := nodes[i]
+
+		start := (node.key_pos + node.key_len) + 3
+		mut end := start
+
+		for {
+			if decoder.json_data[end] == `,` || decoder.json_data[end] == `}` {
+				break
+			}
+
+			end++
+		}
+
+		mut value_kind := get_value_kind(decoder.json_data[start])
+
+		if value_kind == .string_ {
+			val[decoder.json_data[node.key_pos..node.key_pos + node.key_len]] = decoder.json_data[
+				start + 1..end - 1]
+		} else {
+			val[decoder.json_data[node.key_pos..node.key_pos + node.key_len]] = decoder.json_data[start..end]
 		}
 	}
 }
--- vlib/x/new_json/tests/decode_struct_test.v
+++ vlib/x/new_json/tests/decode_struct_test.v
@@ -1,23 +1,143 @@
 import x.new_json as json
+import time
+
+const fixed_time = time.Time{
+	year: 2022
+	month: 3
+	day: 11
+	hour: 13
+	minute: 54
+	second: 25
+	unix: 1647006865
+}
+
+type StringAlias = string
+type BoolAlias = bool
+type IntAlias = int
+type TimeAlias = time.Time
+type StructAlias = StructType[int]
+type EnumAlias = Enumerates
+
+type SumTypes = StructType[string] | []SumTypes | []string | bool | string | time.Time | u32
+
+enum Enumerates {
+	a
+	b
+	c
+	d
+	e = 99
+	f
+}
 
 struct StructType[T] {
 mut:
 	val T
 }
 
-pub struct StructTypeSub {
-	test string
+struct StructTypeOption[T] {
+mut:
+	val ?T
+}
+
+struct StructTypePointer[T] {
+mut:
+	val &T
 }
 
 fn test_types() {
 	assert json.decode[StructType[string]]('{"val": ""}')!.val == ''
-	assert json.decode[StructType[string]]('{"val": "0"}')!.val == '0'
-	assert json.decode[StructType[string]]('{"val": "1"}')!.val == '1'
+
 	assert json.decode[StructType[string]]('{"val": "2"}')!.val == '2'
 
-	assert json.decode[StructType[int]]('{"val": 0}')!.val == 0
-	assert json.decode[StructType[int]]('{"val": 1}')!.val == 1
 	assert json.decode[StructType[int]]('{"val": 2}')!.val == 2
 
-	assert json.decode[StructType[StructTypeSub]]('{"val": {"test": "test"}}')!.val.test == 'test'
+	assert json.decode[StructType[SumTypes]]('{"val": "2"}')!.val == SumTypes('2')
+	assert json.decode[StructType[SumTypes]]('{"val": 2}')!.val == SumTypes(u32(2))
+
+	assert json.decode[StructType[StructType[string]]]('{"val": {"val": "test"}}')!.val.val == 'test'
+
+	assert json.decode[StructType[map[string]string]]('{"val": {"val": "test"}}')!.val['val'] == 'test'
+
+	// assert json.decode[StructType[map[string]Any]]('{"val": {"val": "test"}}')!.val.val == 'test'
+
+	// assert json.decode[StructType[Enumerates]]('{"val": 2}')!.val == Enumerates(2)
+
+	// assert json.decode[StructType[IntAlias]]('{"val": 2}')!.val == IntAlias(2)
+
+	assert json.decode[StructType[time.Time]]('{"val": "2022-03-11T13:54:25.000Z"}')!.val.year == fixed_time.year
+	// assert json.decode[StructType[time.Time]]('{"val": "2022-03-11T13:54:25.000Z"}')!.val.month == fixed_time.month
+	// assert json.decode[StructType[time.Time]]('{"val": "2022-03-11T13:54:25.000Z"}')!.val.day == fixed_time.day
+	// assert json.decode[StructType[time.Time]]('{"val": "2022-03-11T13:54:25.000Z"}')!.val.hour == fixed_time.hour
+	// assert json.decode[StructType[time.Time]]('{"val": "2022-03-11T13:54:25.000Z"}')!.val.minute == fixed_time.minute
+	// assert json.decode[StructType[time.Time]]('{"val": "2022-03-11T13:54:25.000Z"}')!.val.second == fixed_time.second
+	// assert json.decode[StructType[time.Time]]('{"val": "2022-03-11T13:54:25.000Z"}')!.val.unix == fixed_time.unix
+}
+
+fn test_option_types() {
+	if x := json.decode[StructTypeOption[string]]('{}')!.val {
+		assert false, 'Should return none'
+	} else {
+		assert err.msg() == ''
+		assert true
+	}
+
+	if x := json.decode[StructTypeOption[string]]('{"val": "2"}')!.val {
+		assert x == '2'
+	} else {
+		assert false, 'Should not return none'
+	}
+
+	// if x := json.decode[StructTypeOption[int]]('{"val": 2}')!.val {
+	// 	assert x == 2
+	// } else {
+	// 	assert false, 'Should not return none'
+	// }
+
+	// assert json.decode[StructTypeOption[string]]('{"val": "2"}')!.val == '2'
+
+	// assert json.decode[StructTypeOption[int]]('{"val": 2}')!.val == 2
+
+	// assert json.decode[StructTypeOption[SumTypes]]('{"val": "2"}')!.val == SumTypes('2')
+	// assert json.decode[StructTypeOption[SumTypes]]('{"val": 2}')!.val == SumTypes(2)
+
+	// assert json.decode[StructTypeOption[Enumerates]]('{"val": 2}')!.val == Enumerates(2)
+
+	// assert json.decode[StructTypeOption[IntAlias]]('{"val": 2}')!.val == IntAlias(2)
+
+	// assert json.decode[StructTypeOption[StructType[string]]]('{"val": {"val": "test"}}')!.val.val == 'test'
+
+	// assert json.decode[StructTypeOption[time.Time]]('{"val": "2022-03-11T13:54:25.000Z"}')!.val.year == fixed_time.year
+	// assert json.decode[StructTypeOption[time.Time]]('{"val": "2022-03-11T13:54:25.000Z"}')!.val.month == fixed_time.month
+	// assert json.decode[StructTypeOption[time.Time]]('{"val": "2022-03-11T13:54:25.000Z"}')!.val.day == fixed_time.day
+	// assert json.decode[StructTypeOption[time.Time]]('{"val": "2022-03-11T13:54:25.000Z"}')!.val.hour == fixed_time.hour
+	// assert json.decode[StructTypeOption[time.Time]]('{"val": "2022-03-11T13:54:25.000Z"}')!.val.minute == fixed_time.minute
+	// assert json.decode[StructTypeOption[time.Time]]('{"val": "2022-03-11T13:54:25.000Z"}')!.val.second == fixed_time.second
+	// assert json.decode[StructTypeOption[time.Time]]('{"val": "2022-03-11T13:54:25.000Z"}')!.val.unix == fixed_time.unix
+}
+
+fn test_pointer_types() {
+	// assert json.decode[StructTypePointer[string]]('{}')!.val == unsafe { nil }
+
+	// assert json.decode[StructTypePointer[string]]('{"val": ""}')!.val == ''
+
+	// assert json.decode[StructTypePointer[string]]('{"val": "2"}')!.val == '2'
+
+	// assert json.decode[StructTypePointer[int]]('{"val": 2}')!.val == 2
+
+	// assert json.decode[StructTypePointer[SumTypes]]('{"val": "2"}')!.val == SumTypes('2')
+	// assert json.decode[StructTypePointer[SumTypes]]('{"val": 2}')!.val == SumTypes(2)
+
+	// assert json.decode[StructTypePointer[Enumerates]]('{"val": 2}')!.val == Enumerates(2)
+
+	// assert json.decode[StructTypePointer[IntAlias]]('{"val": 2}')!.val == IntAlias(2)
+
+	// assert json.decode[StructTypePointer[StructType[string]]]('{"val": {"val": "test"}}')!.val.val == 'test'
+
+	// assert json.decode[StructTypePointer[time.Time]]('{"val": "2022-03-11T13:54:25.000Z"}')!.val.year == fixed_time.year
+	// assert json.decode[StructTypePointer[time.Time]]('{"val": "2022-03-11T13:54:25.000Z"}')!.val.month == fixed_time.month
+	// assert json.decode[StructTypePointer[time.Time]]('{"val": "2022-03-11T13:54:25.000Z"}')!.val.day == fixed_time.day
+	// assert json.decode[StructTypePointer[time.Time]]('{"val": "2022-03-11T13:54:25.000Z"}')!.val.hour == fixed_time.hour
+	// assert json.decode[StructTypePointer[time.Time]]('{"val": "2022-03-11T13:54:25.000Z"}')!.val.minute == fixed_time.minute
+	// assert json.decode[StructTypePointer[time.Time]]('{"val": "2022-03-11T13:54:25.000Z"}')!.val.second == fixed_time.second
+	// assert json.decode[StructTypePointer[time.Time]]('{"val": "2022-03-11T13:54:25.000Z"}')!.val.unix == fixed_time.unix
 }
