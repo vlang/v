8247c7b5ca87ba5609884deec1a55da63ea58f0d
Hitalo Souza <enghitalo@gmail.com>
2024-03-10 16:21:17 -0400

    undo

--- vlib/x/json2/count.v
+++ vlib/x/json2/count.v
@@ -47,7 +47,7 @@ fn (mut count Count) chars_in_struct[T](val T) {
 	$for field in T.fields {
 		// TODO handle attributes
 		count.total += field.name.len + 3 // "":
-		workaround := &val.$(field.name)
+		workaround := val.$(field.name)
 		count.count_chars(workaround)
 	}
 }
