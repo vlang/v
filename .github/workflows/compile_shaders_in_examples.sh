e34655bfa83283cb29a82974eb87e93a9ed4dfdd
Delyan Angelov <delian66@gmail.com>
2024-03-21 02:03:24 +0200

    ci: cleanup compile_shaders_in_examples.sh

--- .github/workflows/compile_shaders_in_examples.sh
+++ .github/workflows/compile_shaders_in_examples.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-for f in examples/sokol/sdf \
-      examples/sokol/simple_shader_glsl \
-      examples/sokol/02_cubes_glsl/cube_glsl \
-      examples/sokol/03_march_tracing_glsl/rt_glsl \
-      examples/sokol/04_multi_shader_glsl/rt_glsl_puppy \
-      examples/sokol/04_multi_shader_glsl/rt_glsl_march \
-      examples/sokol/05_instancing_glsl/rt_glsl_instancing \
-      examples/sokol/06_obj_viewer/gouraud \
-      ; do 
-      echo "compiling shaders for $f ...";
-      ./v shader $f;
+for f in examples/sokol/*/ ; do 
+    echo "compiling shaders for $f ...";
+    time ./v shader $f;
+	echo "done";
 done;
