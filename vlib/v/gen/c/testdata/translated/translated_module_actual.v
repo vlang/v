692778331359cde420f63d3bd20569bd596877f8
Delyan Angelov <delian66@gmail.com>
2024-03-26 10:24:38 +0200

    tests: test `@[c_extern]` for a C global variable as well

--- vlib/v/gen/c/testdata/translated/translated_module_actual.v
+++ vlib/v/gen/c/testdata/translated/translated_module_actual.v
@@ -5,3 +5,14 @@ module translated
 
 @[c: 'ExternalSymbol']
 pub fn external_symbol(&char) int
+
+struct C.my_struct {
+	active bool
+}
+
+@[c_extern]
+__global my_instance C.my_struct
+
+pub fn is_my_instance_active() bool {
+	return my_instance.active
+}
