3a633e05b624e2b058a8f2e7ed7cd025f42507e6
Felipe Pena <felipensp@gmail.com>
2024-03-26 09:52:28 -0300

    fix

--- vlib/v/gen/c/infix.v
+++ vlib/v/gen/c/infix.v
@@ -1053,7 +1053,7 @@ fn (mut g Gen) gen_is_none_check(node ast.InfixExpr) {
 		g.inside_opt_or_res = true
 		g.write('(')
 		g.expr(node.left)
-		g.write(')')	
+		g.write(')')
 		g.inside_opt_or_res = old_inside_opt_or_res
 		g.write('.state')
 	} else {
