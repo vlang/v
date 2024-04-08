b81dc66febbbdeec7b737b3c983c31f0b3041592
Turiiya <tobmoj@web.de>
2024-04-09 01:15:10 +0200

    fix

--- cmd/tools/vdoc/utils.v
+++ cmd/tools/vdoc/utils.v
@@ -30,7 +30,7 @@ fn get_node_id(dn doc.DocNode) string {
 }
 
 fn is_module_readme(dn doc.DocNode) bool {
-	return dn.comments.len > 0 && dn.content == 'module ${dn.name}'
+	return dn.comments.len > 0 && (dn.content == 'module ${dn.name}' || dn.name == 'README')
 }
 
 fn trim_doc_node_description(desc string) string {
