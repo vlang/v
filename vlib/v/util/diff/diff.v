e9aaad006865203fb91a0ddb7faa30ce11dd3c67
Turiiya <tobmoj@web.de>
2024-04-09 16:44:10 +0200

    util: improve detection for opendiff when automatically searching difftools

--- vlib/v/util/diff/diff.v
+++ vlib/v/util/diff/diff.v
@@ -19,11 +19,9 @@ pub fn find_working_diff_command() !string {
 		'code', 'code.cmd']
 	// NOTE: code.cmd is the Windows variant of the `code` cli tool
 	for diffcmd in known_diff_tools {
-		if diffcmd == 'opendiff' { // opendiff has no `--version` option
-			if opendiff_exists() {
-				return diffcmd
-			}
-			continue
+		if diffcmd == 'opendiff' {
+			os.find_abs_path_of_executable('opendiff') or { continue }
+			return diffcmd
 		}
 		$if freebsd || openbsd {
 			if diffcmd == 'diff' { // FreeBSD/OpenBSD diff have no `--version` option
@@ -51,23 +49,20 @@ pub fn find_working_diff_command() !string {
 	return error('No working "diff" command found')
 }
 
-// determine if the FileMerge opendiff tool is available
-fn opendiff_exists() bool {
-	o := os.execute('opendiff')
-	if o.exit_code < 0 {
-		return false
-	}
-	if o.exit_code == 1 { // failed (expected), but found (i.e. not 127)
-		if o.output.contains('too few arguments') { // got some expected output
-			return true
+pub fn color_compare_files(diff_cmd string, file1 string, file2 string) string {
+	if diff_cmd != '' {
+		mut full_cmd := '${diff_cmd} --minimal --text --unified=2  --show-function-line="fn " ${os.quoted_path(file1)} ${os.quoted_path(file2)} '
+		$if freebsd {
+			full_cmd = '${diff_cmd} --minimal --text --unified=2  ${os.quoted_path(file1)} ${os.quoted_path(file2)} '
 		}
-	}
-	return false
-}
-
-pub fn color_compare_files(diff_cmd string, path1 string, path2 string) string {
-	os.find_abs_path_of_executable(diff_cmd) or {
-		return 'comparison command: `${diff_cmd}` not found'
+		$if openbsd {
+			full_cmd = '${diff_cmd} -d -a -U 2 ${os.quoted_path(file1)} ${os.quoted_path(file2)} '
+		}
+		x := os.execute(full_cmd)
+		if x.exit_code < 0 {
+			return 'comparison command: `${full_cmd}` not found'
+		}
+		return x.output.trim_right('\r\n')
 	}
 	flags := $if openbsd {
 		['-d', '-a', '-U', '2']
