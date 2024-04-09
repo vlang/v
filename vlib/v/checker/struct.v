06b6b9a8ad81364753a3e7b588fe343acc61cc06
Turiiya <tobmoj@web.de>
2024-04-09 23:36:22 +0200

    update warning message

--- vlib/v/checker/struct.v
+++ vlib/v/checker/struct.v
@@ -750,8 +750,10 @@ or use an explicit `unsafe{ a[..] }`, if you do not want a copy of the slice.',
 									} else {
 										parts.last()
 									}
-									c.warn('cannot access private field `${field.name}` on `${mod_type}`',
+									c.add_error_detail('this will become an error after 2024-05-31')
+									c.warn('initalizing private field `${field.name}` of `${mod_type}`',
 										init_field.pos)
+									// c.error('cannot access private field `${field.name}` on `${mod_type}`', init_field.pos)
 									break
 								}
 							}
