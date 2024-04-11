8fb2bf871218f73c2d05002cdb653bc5f2df9d4d
Turiiya <tobmoj@web.de>
2024-04-12 00:39:38 +0200

    fix handling git --version output on windows

--- cmd/tools/vpm/vcs.v
+++ cmd/tools/vpm/vcs.v
@@ -26,7 +26,7 @@ const vcs_info = init_vcs_info() or {
 }
 
 fn init_vcs_info() !map[VCS]VCSInfo {
-	git_installed_raw_ver := os.execute_opt('git --version')!.output.all_after_last(' ').trim_space()
+	git_installed_raw_ver := os.execute_opt('git --version')!.output.all_after_last(' ').all_before('.windows').trim_space()
 	git_installed_ver := semver.from(git_installed_raw_ver)!
 	git_submod_filter_ver := semver.from('2.36.0')!
 	mut git_install_cmd := 'clone --depth=1 --recursive --shallow-submodules --filter=blob:none'
