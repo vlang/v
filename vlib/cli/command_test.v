0533a79ee1a07a6959d86bd5186c04ed9f6a4cfa
Turiiya <tobmoj@web.de>
2024-04-18 19:39:12 +0200

    cli: cleanup and extend `command_test.v`

--- vlib/cli/command_test.v
+++ vlib/cli/command_test.v
@@ -6,7 +6,7 @@ fn test_if_command_parses_empty_args() {
 		execute: empty_func
 	}
 	cmd.parse(['command'])
-	assert cmd.name == 'command' && compare_arrays(cmd.args, [])
+	assert cmd.name == 'command' && cmd.args == []string{}
 }
 
 fn test_if_command_parses_args() {
@@ -15,7 +15,7 @@ fn test_if_command_parses_args() {
 		execute: empty_func
 	}
 	cmd.parse(['command', 'arg0', 'arg1'])
-	assert cmd.name == 'command' && compare_arrays(cmd.args, ['arg0', 'arg1'])
+	assert cmd.name == 'command' && cmd.args == ['arg0', 'arg1']
 }
 
 fn test_if_subcommands_parse_args() {
@@ -31,24 +31,23 @@ fn test_if_subcommands_parse_args() {
 }
 
 fn if_subcommands_parse_args_func(cmd cli.Command) ! {
-	assert cmd.name == 'subcommand' && compare_arrays(cmd.args, ['arg0', 'arg1'])
+	assert cmd.name == 'subcommand' && cmd.args == ['arg0', 'arg1']
 }
 
-fn test_if_command_has_default_help_subcommand() {
+fn test_default_subcommands() {
 	mut cmd := cli.Command{
 		name: 'command'
 	}
 	cmd.parse(['command'])
-	assert has_command(cmd, 'help')
-}
+	assert cmd.commands.any(it.name == 'help')
+	assert cmd.commands.any(it.name == 'man')
 
-fn test_if_command_has_default_version_subcommand_if_version_is_set() {
-	mut cmd := cli.Command{
+	cmd = cli.Command{
 		name: 'command'
 		version: '1.0.0'
 	}
 	cmd.parse(['command'])
-	assert has_command(cmd, 'version')
+	assert cmd.commands.any(it.name == 'version')
 }
 
 fn flag_should_be_set(cmd cli.Command) ! {
@@ -198,25 +197,4 @@ fn test_command_setup() {
 
 // helper functions
 fn empty_func(cmd cli.Command) ! {
-}
-
-fn has_command(cmd cli.Command, name string) bool {
-	for subcmd in cmd.commands {
-		if subcmd.name == name {
-			return true
-		}
-	}
-	return false
-}
-
-fn compare_arrays(array0 []string, array1 []string) bool {
-	if array0.len != array1.len {
-		return false
-	}
-	for i in 0 .. array0.len {
-		if array0[i] != array1[i] {
-			return false
-		}
-	}
-	return true
 }
