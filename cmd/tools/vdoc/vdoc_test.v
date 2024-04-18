373841e8935df0660991182e156b61d62426ffdc
Turiiya <tobmoj@web.de>
2024-04-18 16:19:58 +0200

    merge vdoc unit tests

--- cmd/tools/vdoc/vdoc_test.v
+++ cmd/tools/vdoc/vdoc_test.v
@@ -3,6 +3,23 @@ module main
 import os
 import arrays
 
+fn test_trim_doc_node_description() {
+	mod := 'foo'
+	mut readme := '## Description
+
+`foo` is a module that provides tools and utility functions to assist in working with bar.
+It also assists with composing and testing baz.'
+	expected := 'is a module that provides tools and utility functions to assist in working with'
+	res := trim_doc_node_description(mod, readme).trim_space()
+	assert res == expected
+
+	readme = '# Foo
+`foo` is a module that provides tools and utility functions to assist in working with bar.
+It also assists with composing and testing baz.'
+	res2 := trim_doc_node_description(mod, readme).trim_space()
+	assert res2 == res
+}
+
 fn test_get_module_list() {
 	tpath := os.join_path(os.vtmp_dir(), 'vod_test_module')
 	os.rmdir_all(tpath) or {}
