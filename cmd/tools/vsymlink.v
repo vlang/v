06e1d683afa0ee89270de496c2532f25fe78609d
Delyan Angelov <delian66@gmail.com>
2024-04-24 10:14:40 +0300

    ci: remove sudo usage, improve the diagnostic message for `sudo ./v symlink -githubci`.

--- cmd/tools/vsymlink.v
+++ cmd/tools/vsymlink.v
@@ -39,7 +39,12 @@ fn setup_symlink_github() {
 	// 1. https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#environment-files
 	// 2. https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-environment-variable
 	mut content := os.read_file(os.getenv('GITHUB_PATH')) or {
-		panic('Failed to read GITHUB_PATH.')
+		eprintln('The `GITHUB_PATH` env variable is not defined.')
+		eprintln('    This command: `v symlink -githubci` is intended to be used within GithubActions .yml files.')
+		eprintln('    It also needs to be run *as is*, *** without `sudo` ***, otherwise it will not work.')
+		eprintln('    For local usage, outside CIs, on !windows, prefer `sudo ./v symlink` .')
+		eprintln('    On windows, use `.\\v.exe symlink` instead.')
+		exit(1)
 	}
 	content += '\n${os.getwd()}\n'
 	os.write_file(os.getenv('GITHUB_PATH'), content) or { panic('Failed to write to GITHUB_PATH.') }
