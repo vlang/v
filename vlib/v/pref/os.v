47a0377ccef3e8567f4d1e933838e73c5603de20
Turiiya <tobmoj@web.de>
2024-04-30 17:06:01 +0200

    pref: update `os_from_string`, add missing `qnx`, remove deprecated wasm options that used `-` instead of `_`

--- vlib/v/pref/os.v
+++ vlib/v/pref/os.v
@@ -86,6 +86,9 @@ pub fn os_from_string(os_str string) !OS {
 		'serenity' {
 			return .serenity
 		}
+		'qnx' {
+			return .qnx
+		}
 		'plan9' {
 			return .plan9
 		}
@@ -122,18 +125,6 @@ pub fn os_from_string(os_str string) !OS {
 			return .wasi
 		}
 		else {
-			// handle deprecated names:
-			match os_str {
-				'wasm32-emscripten' {
-					eprintln('Please use `-os wasm32_emscripten` instead.')
-					return .wasm32_emscripten
-				}
-				'wasm32-wasi' {
-					eprintln('Please use `-os wasm32_wasi` instead.')
-					return .wasm32_wasi
-				}
-				else {}
-			}
 			return error('bad OS ${os_str}')
 		}
 	}
