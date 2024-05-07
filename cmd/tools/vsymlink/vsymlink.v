ec390e7acc2c03b5f1bb785008f43cbf3d13aa86
Turiiya <tobmoj@web.de>
2024-05-07 13:55:47 +0200

    add deprecation message

--- cmd/tools/vsymlink/vsymlink.v
+++ cmd/tools/vsymlink/vsymlink.v
@@ -5,8 +5,14 @@ const vexe = os.real_path(os.getenv_opt('VEXE') or { @VEXE })
 fn main() {
 	at_exit(|| os.rmdir_all(os.vtmp_dir()) or {}) or {}
 	if os.args.len > 2 {
-		print('usage: v symlink')
-		exit(1)
+		if '-githubci' in os.args {
+			// TODO: remove warning for deprecation of `-githubci` flag and
+			// only use the block below after 2024-09-31.
+			println('::warning::Use `v symlink` instead of `v symlink -githubci`')
+		} else {
+			println('usage: v symlink')
+			exit(1)
+		}
 	}
 	setup_symlink()
 }
