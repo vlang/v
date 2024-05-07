5e7d8cd99a60685eb8c06089dd3e85d3d147bd45
Delyan Angelov <delian66@gmail.com>
2024-05-07 19:34:40 +0300

    ci: replace .github/workflows/retry.sh usages in the CI with the shorter `v retry --`, move `v test-cleancode` upwards to save time for unformatted PRs (#21452)

--- .github/workflows/download_full_toml_test_suites.sh
+++ .github/workflows/download_full_toml_test_suites.sh
@@ -4,13 +4,13 @@ set -ex
 
 rm -rf vlib/toml/tests/testdata/burntsushi  vlib/toml/tests/testdata/iarna  vlib/toml/tests/testdata/alexcrichton  vlib/toml/tests/testdata/large_toml_file_test.toml
 
-.github/workflows/retry.sh wget https://gist.githubusercontent.com/Larpon/89b0e3d94c6903851ff15559e5df7a05/raw/62a1f87a4e37bf157f2e0bfb32d85d840c98e422/large_toml_file_test.toml -O vlib/toml/tests/testdata/large_toml_file_test.toml
+./v retry -- wget https://gist.githubusercontent.com/Larpon/89b0e3d94c6903851ff15559e5df7a05/raw/62a1f87a4e37bf157f2e0bfb32d85d840c98e422/large_toml_file_test.toml -O vlib/toml/tests/testdata/large_toml_file_test.toml
 
-.github/workflows/retry.sh git clone -n https://github.com/iarna/toml-spec-tests.git vlib/toml/tests/testdata/iarna
+./v retry -- git clone -n https://github.com/iarna/toml-spec-tests.git vlib/toml/tests/testdata/iarna
 git -C vlib/toml/tests/testdata/iarna checkout 1880b1a
 
-.github/workflows/retry.sh git clone -n https://github.com/toml-lang/toml-test.git vlib/toml/tests/testdata/burntsushi
+./v retry -- git clone -n https://github.com/toml-lang/toml-test.git vlib/toml/tests/testdata/burntsushi
 git -C vlib/toml/tests/testdata/burntsushi checkout f30c716
 
-.github/workflows/retry.sh git clone -n https://github.com/toml-rs/toml.git vlib/toml/tests/testdata/alexcrichton
+./v retry -- git clone -n https://github.com/toml-rs/toml.git vlib/toml/tests/testdata/alexcrichton
 git -C vlib/toml/tests/testdata/alexcrichton reset --hard 499e8c4
