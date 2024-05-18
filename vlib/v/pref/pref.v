5c46fb12021b4ab1c980b4dd7dad890243148c76
Delyan Angelov <delian66@gmail.com>
2024-05-18 14:25:25 +0300

    tests: move vsh_envbang_test.v to vlib/v/pref/vsh_envbang_test.v, fix behaviour for `v file.vsh -some -options`

--- vlib/v/pref/pref.v
+++ vlib/v/pref/pref.v
@@ -924,6 +924,10 @@ pub fn parse_args_and_show_errors(known_external_commands []string, args []strin
 				if command == 'build' && is_source_file(arg) {
 					eprintln_exit('Use `v ${arg}` instead.')
 				}
+				if is_source_file(arg) && arg.ends_with('.vsh') {
+					// store for future iterations
+					res.is_vsh = true
+				}
 				if !arg.starts_with('-') {
 					if command == '' {
 						command, command_idx = arg, i
@@ -938,6 +942,10 @@ pub fn parse_args_and_show_errors(known_external_commands []string, args []strin
 				}
 				if command !in ['', 'build-module'] && !is_source_file(command) {
 					// arguments for e.g. fmt should be checked elsewhere
+					continue
+				}
+				if res.is_vsh && command_idx < i {
+					// Allow for `script.vsh abc 123 -option`, because -option is for the .vsh program, not for v
 					continue
 				}
 				err_detail := if command == '' { '' } else { ' for command `${command}`' }
