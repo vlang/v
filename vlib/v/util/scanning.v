020143cf1c4f5f5f7357c99053edab9a5460f0a3
Delyan Angelov <delian66@gmail.com>
2024-05-22 14:42:10 +0300

    Revert "v.util: revert changes to vlib/v/util/scanning.v"
    This reverts commit 17da6c1a5f8424ea8b18b2776a66ca662a67402d.

--- vlib/v/util/scanning.v
+++ vlib/v/util/scanning.v
@@ -1,13 +1,34 @@
 module util
 
-@[inline]
-pub fn is_name_char(c u8) bool {
-	return (c >= `a` && c <= `z`) || (c >= `A` && c <= `Z`) || c == `_`
+pub const name_char_table = get_name_char_table()
+
+fn get_name_char_table() [255]bool {
+	mut res := [255]bool{}
+	for c in 0 .. 255 {
+		res[c] = (c >= `a` && c <= `z`) || (c >= `A` && c <= `Z`) || c == `_`
+	}
+	return res
 }
 
-@[inline]
+pub const func_char_table = get_func_char_table()
+
+fn get_func_char_table() [255]bool {
+	mut res := [255]bool{}
+	for c in 0 .. 255 {
+		res[c] = (c >= `a` && c <= `z`) || (c >= `A` && c <= `Z`) || c == `_`
+			|| (c >= `0` && c <= `9`)
+	}
+	return res
+}
+
+@[direct_array_access; inline]
+pub fn is_name_char(c u8) bool {
+	return util.name_char_table[c]
+}
+
+@[direct_array_access; inline]
 pub fn is_func_char(c u8) bool {
-	return (c >= `a` && c <= `z`) || (c >= `A` && c <= `Z`) || c == `_` || (c >= `0` && c <= `9`)
+	return util.func_char_table[c]
 }
 
 pub fn contains_capital(s string) bool {
