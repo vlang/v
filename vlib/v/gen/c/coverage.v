c223eb93bc63151d81a29e49e11bbb5a3077ff93
Felipe Pena <felipensp@gmail.com>
2024-05-24 10:24:25 -0300

    wip

--- vlib/v/gen/c/coverage.v
+++ vlib/v/gen/c/coverage.v
@@ -3,23 +3,8 @@
 // that can be found in the LICENSE file.
 module c
 
-import hash.fnv1a
 import v.token
-
-@[inline]
-fn current_time() u64 {
-	unsafe {
-		$if windows {
-			tm := u64(0)
-			C.QueryPerformanceCounter(&tm)
-			return tm
-		} $else {
-			ts := C.timespec{}
-			C.clock_gettime(C.CLOCK_MONOTONIC, &ts)
-			return u64(ts.tv_sec) * 1000000000 + u64(ts.tv_nsec)
-		}
-	}
-}
+import rand { ulid }
 
 @[inline]
 fn (mut g Gen) write_coverage_point(pos token.Pos) {
@@ -40,9 +25,6 @@ fn (mut g Gen) write_coverage_point(pos token.Pos) {
 }
 
 fn (mut g Gen) write_coverage_stats() {
-	is_stdout := false // g.pref.coverage_file == '-'
-	is_json := true
-
 	g.cov_declarations.writeln('char* vcoverage_fnv1a(const u8* data) {')
 	g.cov_declarations.writeln('\tu32 h = 2166136261UL;')
 	g.cov_declarations.writeln('\tsize_t size = strlen(data);')
@@ -54,63 +36,35 @@ fn (mut g Gen) write_coverage_stats() {
 	g.cov_declarations.writeln('}')
 	g.cov_declarations.writeln('')
 
-	build_hash := fnv1a.sum64_string(current_time().str())
+	build_hash := ulid()
 
 	g.cov_declarations.writeln('void vprint_coverage_stats() {')
-	if is_stdout {
-		g.cov_declarations.writeln('\tprintf("V coverage\\n");')
-		g.cov_declarations.writeln('\tprintf("${'-':50r}\\n");')
-	} else {
-		g.cov_declarations.writeln('time_t rawtime;')
-		g.cov_declarations.writeln('char cov_filename[200];')
-		g.cov_declarations.writeln('time(&rawtime);')
-		g.cov_declarations.writeln('snprintf(cov_filename, 200, "vcover.%s.%s", "${build_hash}", vcoverage_fnv1a(asctime(localtime(&rawtime))));')
 
-		g.cov_declarations.writeln('\tFILE *fp = fopen(cov_filename, "w+");')
-
-		if is_json {
-			g.cov_declarations.writeln('\tfprintf(fp, "[");')
-		} else {
-			g.cov_declarations.writeln('\tfprintf(fp, "V coverage\\n");')
-			g.cov_declarations.writeln('\tfprintf(fp, "${'-':50r}\\n");')
-		}
-	}
+	g.cov_declarations.writeln('time_t rawtime;')
+	g.cov_declarations.writeln('char cov_filename[120];')
+	g.cov_declarations.writeln('time(&rawtime);')
+	g.cov_declarations.writeln('snprintf(cov_filename, 120, "vcover.%s.%s", "${build_hash}", vcoverage_fnv1a(asctime(localtime(&rawtime))));')
+	g.cov_declarations.writeln('\tFILE *fp = fopen(cov_filename, "w+");')
+	g.cov_declarations.writeln('\tfprintf(fp, "[");')
 	g.cov_declarations.writeln('\tint t_counter = 0;')
 	mut last_offset := 0
-	mut t_points := 0
 	mut is_first := true
 	for k, cov in g.coverage_files {
 		nr_points := cov.points.len
-		t_points += nr_points
 		g.cov_declarations.writeln('\t{')
 		g.cov_declarations.writeln('\t\tint counter = 0;')
 		g.cov_declarations.writeln('\t\tfor (int i = 0, offset = ${last_offset}; i < ${nr_points}; ++i)')
 		g.cov_declarations.writeln('\t\t\tif (_v_cov[_v_cov_file_offset_${k}+i]) counter++;')
 		g.cov_declarations.writeln('\t\tt_counter += counter;')
 		g.cov_declarations.writeln('\t\tif (counter) {')
-		if is_stdout {
-			g.cov_declarations.writeln('\t\t\tprintf("[%7.2f%%] %4d / ${nr_points:4d} | ${cov.file.path}\\n", ${nr_points} > 0 ? ((double)counter/${nr_points})*100 : 0, counter);')
-		} else if is_json {
-			g.cov_declarations.writeln("\t\t\tfprintf(fp, \"%s{\\\"file\\\":\\\"%s\\\",\\\"cov\\\":%.2f,\\\"points\\\":%d}\", ${int(is_first)} ? \"\" : \",\", \"${cov.file.path}\", ${nr_points} > 0 ? ((double)counter/${nr_points})*100 : 0, ${nr_points});")
-			if is_first {
-				is_first = !is_first
-			}
-		} else {
-			g.cov_declarations.writeln('\t\t\tfprintf(fp, "[%7.2f%%] %4d / ${nr_points:4d} | ${cov.file.path}\\n", ${nr_points} > 0 ? ((double)counter/${nr_points})*100 : 0, counter);')
+		g.cov_declarations.writeln("\t\t\tfprintf(fp, \"%s{\\\"file\\\":\\\"%s\\\",\\\"cov\\\":%.2f,\\\"points\\\":%d}\", ${int(is_first)} ? \"\" : \",\", \"${cov.file.path}\", ${nr_points} > 0 ? ((double)counter/${nr_points})*100 : 0, ${nr_points});")
+		if is_first {
+			is_first = !is_first
 		}
 		g.cov_declarations.writeln('\t\t}')
 		g.cov_declarations.writeln('\t}')
 		last_offset += nr_points
 	}
-	if is_stdout {
-		g.cov_declarations.writeln('\tprintf("${'-':50r}\\n");')
-		g.cov_declarations.writeln('\tprintf("Total coverage: ${g.coverage_files.len} files, %.2f%% coverage\\n", ((double)t_counter/${t_points})*100);')
-	} else if is_json {
-		g.cov_declarations.writeln('\tfprintf(fp, "]");')
-	} else {
-		g.cov_declarations.writeln('\tfprintf(fp, "${'-':50r}\\n");')
-		g.cov_declarations.writeln('\tfprintf(fp, "Total coverage: ${g.coverage_files.len} files, %.2f%% coverage\\n", ((double)t_counter/${t_points})*100);')
-		g.cov_declarations.writeln('\tfclose(fp);')
-	}
+	g.cov_declarations.writeln('\tfprintf(fp, "]");')
 	g.cov_declarations.writeln('}')
 }
