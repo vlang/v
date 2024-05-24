92e4dfcb0ae87087c25b37f7d070c1625a8d000b
Delyan Angelov <delian66@gmail.com>
2024-05-24 19:34:20 +0300

    ci: fix -cstrict builds with clang-18, remove obsolete TODO comment

--- vlib/builtin/string.v
+++ vlib/builtin/string.v
@@ -355,14 +355,12 @@ pub fn (s string) replace(rep string, with string) string {
 	if !s.contains(rep) {
 		return s.clone()
 	}
-	// TODO: PERF Allocating ints is expensive. Should be a stack array
-	// Get locations of all reps within this string
 	mut pidxs_len := 0
 	pidxs_cap := s.len / rep.len
 	mut stack_idxs := [replace_stack_buffer_size]int{}
 	mut pidxs := unsafe { &stack_idxs[0] }
 	if pidxs_cap > replace_stack_buffer_size {
-		pidxs = unsafe { malloc(sizeof(int) * pidxs_cap) }
+		pidxs = unsafe { &int(malloc(sizeof(int) * pidxs_cap)) }
 	}
 	defer {
 		if pidxs_cap > replace_stack_buffer_size {
@@ -1262,7 +1260,7 @@ fn (s string) index_kmp(p string) int {
 	mut stack_prefixes := [kmp_stack_buffer_size]int{}
 	mut p_prefixes := unsafe { &stack_prefixes[0] }
 	if p.len > kmp_stack_buffer_size {
-		p_prefixes = unsafe { vcalloc(p.len * sizeof(int)) }
+		p_prefixes = unsafe { &int(vcalloc(p.len * sizeof(int))) }
 	}
 	defer {
 		if p.len > kmp_stack_buffer_size {
