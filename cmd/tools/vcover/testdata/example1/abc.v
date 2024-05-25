2f4dbb1103d9c0e6e3aa8606b66829228d81dbed
Delyan Angelov <delian66@gmail.com>
2024-05-25 22:01:58 +0300

    make test runs of cmd/tools/vcover/testdata/example1/ silent for now

--- cmd/tools/vcover/testdata/example1/abc.v
+++ cmd/tools/vcover/testdata/example1/abc.v
@@ -1,313 +1,313 @@
 module example1
 
-fn abc01() {
+fn abc01() int {
 	if true {
-		println('true')
+		return 1
 	} else {
-		println(false)
+		return 2
 	}
 }
 
-fn abc02() {
+fn abc02() int {
 	if true {
-		println('true')
+		return 1
 	} else {
-		println(false)
+		return 0
 	}
 }
 
-fn abc03() {
+fn abc03() int {
 	if true {
-		println('true')
+		return 1
 	} else {
-		println(false)
+		return 0
 	}
 }
 
-fn abc04() {
+fn abc04() int {
 	if true {
-		println('true')
+		return 1
 	} else {
-		println(false)
+		return 0
 	}
 }
 
-fn abc05() {
+fn abc05() int {
 	if true {
-		println('true')
+		return 1
 	} else {
-		println(false)
+		return 0
 	}
 }
 
-fn abc06() {
+fn abc06() int {
 	if true {
-		println('true')
+		return 1
 	} else {
-		println(false)
+		return 0
 	}
 }
 
-fn abc07() {
+fn abc07() int {
 	if true {
-		println('true')
+		return 1
 	} else {
-		println(false)
+		return 0
 	}
 }
 
-fn abc08() {
+fn abc08() int {
 	if true {
-		println('true')
+		return 1
 	} else {
-		println(false)
+		return 0
 	}
 }
 
-fn abc09() {
+fn abc09() int {
 	if true {
-		println('true')
+		return 1
 	} else {
-		println(false)
+		return 0
 	}
 }
 
-fn abc10() {
+fn abc10() int {
 	if true {
-		println('true')
+		return 1
 	} else {
-		println(false)
+		return 0
 	}
 }
 
-fn abc11() {
+fn abc11() int {
 	if true {
-		println('true')
+		return 1
 	} else {
-		println(false)
+		return 0
 	}
 }
 
-fn abc12() {
+fn abc12() int {
 	if true {
-		println('true')
+		return 1
 	} else {
-		println(false)
+		return 0
 	}
 }
 
-fn abc13() {
+fn abc13() int {
 	if true {
-		println('true')
+		return 1
 	} else {
-		println(false)
+		return 0
 	}
 }
 
-fn abc14() {
+fn abc14() int {
 	if true {
-		println('true')
+		return 1
 	} else {
-		println(false)
+		return 0
 	}
 }
 
-fn abc15() {
+fn abc15() int {
 	if true {
-		println('true')
+		return 1
 	} else {
-		println(false)
+		return 0
 	}
 }
 
-fn abc16() {
+fn abc16() int {
 	if true {
-		println('true')
+		return 1
 	} else {
-		println(false)
+		return 0
 	}
 }
 
-fn abc17() {
+fn abc17() int {
 	if true {
-		println('true')
+		return 1
 	} else {
-		println(false)
+		return 0
 	}
 }
 
-fn abc18() {
+fn abc18() int {
 	if true {
-		println('true')
+		return 1
 	} else {
-		println(false)
+		return 0
 	}
 }
 
-fn abc19() {
+fn abc19() int {
 	if true {
-		println('true')
+		return 1
 	} else {
-		println(false)
+		return 0
 	}
 }
 
-fn abc20() {
+fn abc20() int {
 	if true {
-		println('true')
+		return 1
 	} else {
-		println(false)
+		return 0
 	}
 }
 
-fn abc21() {
+fn abc21() int {
 	if true {
-		println('true')
+		return 1
 	} else {
-		println(false)
+		return 0
 	}
 }
 
-fn abc22() {
+fn abc22() int {
 	if true {
-		println('true')
+		return 1
 	} else {
-		println(false)
+		return 0
 	}
 }
 
-fn abc23() {
+fn abc23() int {
 	if true {
-		println('true')
+		return 1
 	} else {
-		println(false)
+		return 0
 	}
 }
 
-fn abc24() {
+fn abc24() int {
 	if true {
-		println('true')
+		return 1
 	} else {
-		println(false)
+		return 0
 	}
 }
 
-fn abc25() {
+fn abc25() int {
 	if true {
-		println('true')
+		return 1
 	} else {
-		println(false)
+		return 0
 	}
 }
 
-fn abc26() {
+fn abc26() int {
 	if true {
-		println('true')
+		return 1
 	} else {
-		println(false)
+		return 0
 	}
 }
 
-fn abc27() {
+fn abc27() int {
 	if true {
-		println('true')
+		return 1
 	} else {
-		println(false)
+		return 0
 	}
 }
 
-fn abc28() {
+fn abc28() int {
 	if true {
-		println('true')
+		return 1
 	} else {
-		println(false)
+		return 0
 	}
 }
 
-fn abc29() {
+fn abc29() int {
 	if true {
-		println('true')
+		return 1
 	} else {
-		println(false)
+		return 0
 	}
 }
 
-fn abc30() {
+fn abc30() int {
 	if true {
-		println('true')
+		return 1
 	} else {
-		println(false)
+		return 0
 	}
 }
 
-fn abc31() {
+fn abc31() int {
 	if true {
-		println('true')
+		return 1
 	} else {
-		println(false)
+		return 0
 	}
 }
 
-fn abc32() {
+fn abc32() int {
 	if true {
-		println('true')
+		return 1
 	} else {
-		println(false)
+		return 0
 	}
 }
 
-fn abc33() {
+fn abc33() int {
 	if true {
-		println('true')
+		return 1
 	} else {
-		println(false)
+		return 0
 	}
 }
 
-fn abc34() {
+fn abc34() int {
 	if true {
-		println('true')
+		return 1
 	} else {
-		println(false)
+		return 0
 	}
 }
 
-fn abc35() {
+fn abc35() int {
 	if true {
-		println('true')
+		return 1
 	} else {
-		println(false)
+		return 0
 	}
 }
 
-fn abc36() {
+fn abc36() int {
 	if true {
-		println('true')
+		return 1
 	} else {
-		println(false)
+		return 0
 	}
 }
 
-fn abc37() {
+fn abc37() int {
 	if true {
-		println('true')
+		return 1
 	} else {
-		println(false)
+		return 0
 	}
 }
 
-fn abc38() {
+fn abc38() int {
 	if true {
-		println('true')
+		return 1
 	} else {
-		println(false)
+		return 0
 	}
 }
 
-fn abc39() {
+fn abc39() int {
 	if true {
-		println('true')
+		return 1
 	} else {
-		println(false)
+		return 0
 	}
 }
