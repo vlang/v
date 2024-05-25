f08eafb205aadaa4a50dd6cddc79b8230791193f
Delyan Angelov <delian66@gmail.com>
2024-05-26 01:43:19 +0300

    update copyright

--- vlib/v/gen/c/coverage.v
+++ vlib/v/gen/c/coverage.v
@@ -1,4 +1,4 @@
-// Copyright (c) 2024 Felipe Pena. All rights reserved.
+// Copyright (c) 2024 Felipe Pena and Delyan Angelov. All rights reserved.
 // Use of this source code is governed by an MIT license
 // that can be found in the LICENSE file.
 module c
