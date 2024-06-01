d750528f06db1e0914cc27caa768f7320f1e6fd2
Felipe Pena <felipensp@gmail.com>
2024-06-01 18:44:28 -0300

    parser: allow multiple flag values in enum.from() for flag enums (fix #21569) (#21615)

--- vlib/v/parser/parser.v
+++ vlib/v/parser/parser.v
@@ -4161,15 +4161,18 @@ fn (mut p Parser) enum_decl() ast.EnumDecl {
 	isb.write_string('		val := unsafe{ ${enum_name}(input) }\n')
 	if is_flag {
 		isb.write_string('		if input == 0 { return val }\n')
+		all_bits_set_value := '0b' + '1'.repeat(fields.len)
+		isb.write_string('		if input & ~${all_bits_set_value} == 0 { return val }\n')
+	} else {
+		isb.write_string('		match val {\n')
+		for f in fields {
+			isb.write_string('			.${f.source_name} { return ${enum_name}.${f.source_name} }\n')
+		}
+		if is_flag {
+			isb.write_string('			else{}\n')
+		}
+		isb.write_string('		}\n')
 	}
-	isb.write_string('		match val {\n')
-	for f in fields {
-		isb.write_string('			.${f.source_name} { return ${enum_name}.${f.source_name} }\n')
-	}
-	if is_flag {
-		isb.write_string('			else{}\n')
-	}
-	isb.write_string('		}\n')
 	isb.write_string('	}\n')
 	isb.write_string('	\$if input is \$string {\n')
 	isb.write_string('		val := input.str()\n') // TODO: this should not be needed, the `$if input is $string` above should have already smartcasted `input`
