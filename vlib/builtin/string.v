9b3a59f0f72b86a86f7a5db77e77e6435720cc54
Turiiya <tobmoj@web.de>
2024-06-24 16:48:43 +0200

    fmt

--- vlib/builtin/string.v
+++ vlib/builtin/string.v
@@ -2662,9 +2662,9 @@ pub fn (s string) camel_to_snake() string {
 		}
 		lower_first_c, lower_second_c
 	} else {
-		first_c := s[0]
-		second_c:= u8(if s[1] >= `A` && s[1] <= `Z` { `_` } else { s[1] })
-		first_c, second_c
+		lower_first_c := s[0]
+		second_c := u8(if s[1] >= `A` && s[1] <= `Z` { `_` } else { s[1] })
+		lower_first_c, second_c
 	}
 	unsafe {
 		b[0] = first_char
