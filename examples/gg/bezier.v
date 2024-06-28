4c42fe3420200f66d7c83c3a1811759ea5bceac2
Delyan Angelov <delian66@gmail.com>
2024-06-28 08:30:45 +0300

    gg: add Context.draw_cubic_bezier_recursive/2 and Context.draw_cubic_bezier_recursive_scalar/9 (#21749)

--- examples/gg/bezier.v
+++ examples/gg/bezier.v
@@ -7,7 +7,8 @@ const points = [f32(200.0), 200.0, 200.0, 100.0, 400.0, 100.0, 400.0, 300.0]
 
 struct App {
 mut:
-	gg &gg.Context = unsafe { nil }
+	gg    &gg.Context = unsafe { nil }
+	steps int = 30
 }
 
 fn main() {
@@ -23,8 +24,20 @@ fn main() {
 	app.gg.run()
 }
 
+fn (mut app App) change(delta int) {
+	app.steps += delta
+	println('app.steps: ${app.steps}')
+}
+
 fn frame(mut app App) {
 	app.gg.begin()
-	app.gg.draw_cubic_bezier(points, gx.blue)
+	app.gg.draw_cubic_bezier_in_steps(points, u32(app.steps), gx.blue)
+	app.gg.draw_cubic_bezier_recursive(points, gx.rgba(255, 50, 50, 150))
 	app.gg.end()
+	if app.gg.pressed_keys[int(gg.KeyCode.down)] {
+		app.change(-1)
+	}
+	if app.gg.pressed_keys[int(gg.KeyCode.up)] {
+		app.change(1)
+	}
 }
