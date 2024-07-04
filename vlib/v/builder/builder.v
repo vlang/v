96eada18355883b1fbb899b6c76bc9588e0d5bfc
Delyan Angelov <delian66@gmail.com>
2024-07-04 13:50:53 +0300

    v.builder: print the suggestion for using `v xyz` instead of `v xyz.v` just once (#21801)

--- vlib/v/builder/builder.v
+++ vlib/v/builder/builder.v
@@ -465,15 +465,13 @@ pub fn (b &Builder) show_total_warns_and_errors_stats() {
 		}
 	}
 	if b.checker.nr_errors > 0 && b.pref.path.ends_with('.v') && os.is_file(b.pref.path) {
-		for err in b.checker.errors {
-			if err.message.starts_with('unknown ') {
-				// Sometimes users try to `v main.v`, when they have several .v files in their project.
-				// Then, they encounter puzzling errors about missing or unknown types. In this case,
-				// the intended command may have been `v .` instead, so just suggest that:
-				old_cmd := util.bold('v ${b.pref.path}')
-				new_cmd := util.bold('v ${os.dir(b.pref.path)}')
-				eprintln(util.color('notice', 'If the code of your project is in multiple files, try with `${new_cmd}` instead of `${old_cmd}`'))
-			}
+		if b.checker.errors.any(it.message.starts_with('unknown ')) {
+			// Sometimes users try to `v main.v`, when they have several .v files in their project.
+			// Then, they encounter puzzling errors about missing or unknown types. In this case,
+			// the intended command may have been `v .` instead, so just suggest that:
+			old_cmd := util.bold('v ${b.pref.path}')
+			new_cmd := util.bold('v ${os.dir(b.pref.path)}')
+			eprintln(util.color('notice', 'If the code of your project is in multiple files, try with `${new_cmd}` instead of `${old_cmd}`'))
 		}
 	}
 }
