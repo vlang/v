2d8e805703c469f7cb69deda20f7019dbee7e782
Felipe Pena <felipensp@gmail.com>
2024-07-18 00:14:26 -0300

    test

---
+++ vlib/v/tests/map_value_ref_interp_test.v
@@ -0,0 +1,30 @@
+module main
+
+struct Inner {
+	value int
+}
+
+struct Outer {
+	value map[string]&Inner
+}
+
+fn test_main() {
+	outer := Outer{
+		value: {
+			'test': &Inner{
+				value: 1
+			}
+		}
+	}
+	unexpected := "Outer{
+    value: {'test': Inner{
+        value: 1
+    }}
+}"
+	expected := "Outer{
+    value: {'test': &Inner{
+        value: 1
+    }}
+}"
+	assert '${outer}' == expected
+}
