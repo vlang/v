f524d3ea9e6dae96baab78fcbacaecb58a82e7ae
Felipe Pena <felipensp@gmail.com>
2024-07-27 20:48:46 -0300

    improve

--- vlib/v/gen/c/auto_str_methods.v
+++ vlib/v/gen/c/auto_str_methods.v
@@ -447,6 +447,7 @@ fn (mut g Gen) gen_str_for_union_sum_type(info ast.SumType, styp string, typ_str
 		if typ.idx() in idxs {
 			continue
 		}
+		idxs << typ.idx()
 		typ_name := g.typ(typ)
 		mut func_name := g.get_str_fn(typ)
 		sym := g.table.sym(typ)
@@ -486,7 +487,6 @@ fn (mut g Gen) gen_str_for_union_sum_type(info ast.SumType, styp string, typ_str
 			}))'
 			fn_builder.write_string('\t\tcase ${int(typ)}: return ${res};\n')
 		}
-		idxs << typ.idx()
 	}
 	fn_builder.writeln('\t\tdefault: return _SLIT("unknown sum type value");')
 	fn_builder.writeln('\t}')
