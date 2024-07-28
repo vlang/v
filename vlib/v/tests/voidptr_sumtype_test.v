f57272a2546b55456766dc8a8b8cbed379165a20
Felipe Pena <felipensp@gmail.com>
2024-07-28 18:11:20 -0300

    fix

---
+++ vlib/v/tests/voidptr_sumtype_test.v
@@ -0,0 +1,14 @@
+type MySum = string | voidptr
+type Alias = voidptr
+
+fn t(a MySum) MySum {
+	return dump(a)
+}
+
+fn test_main() {
+	a := 'foo'
+	assert t(a) == MySum('foo')
+
+	b := voidptr(0x100)
+	assert t(b) == MySum(voidptr(0x100))
+}
