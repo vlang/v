52e9babf611b0c817bc036bb19ea90fbeb01fee8
Felipe Pena <felipensp@gmail.com>
2024-07-28 17:56:16 -0300

    fix

--- vlib/v/gen/c/cgen.v
+++ vlib/v/gen/c/cgen.v
@@ -1002,12 +1002,12 @@ pub fn (mut g Gen) write_typeof_functions() {
 				g.writeln('\t\tcase ${tidx}: return "${util.strip_main_name(sym.name)}";')
 				mut idxs := []int{}
 				for v in sum_info.variants {
-					if v.idx() in idxs {
+					if v in idxs {
 						continue
 					}
 					subtype := g.table.sym(v)
 					g.writeln('\t\tcase ${int(v)}: return "${util.strip_main_name(subtype.name)}";')
-					idxs << v.idx()
+					idxs << v
 				}
 				g.writeln('\t\tdefault: return "unknown ${util.strip_main_name(sym.name)}";')
 				g.writeln('\t}')
@@ -1028,11 +1028,11 @@ pub fn (mut g Gen) write_typeof_functions() {
 				g.writeln('\t\tcase ${tidx}: return ${int(ityp)};')
 				mut idxs := []int{}
 				for v in sum_info.variants {
-					if v.idx() in idxs {
+					if v in idxs {
 						continue
 					}
 					g.writeln('\t\tcase ${int(v)}: return ${int(v)};')
-					idxs << v.idx()
+					idxs << v
 				}
 				g.writeln('\t\tdefault: return ${int(ityp)};')
 				g.writeln('\t}')
@@ -6690,19 +6690,20 @@ fn (mut g Gen) write_types(symbols []&ast.TypeSymbol) {
 				g.type_definitions.writeln('// Union sum type ${name} = ')
 				mut idxs := []int{}
 				for variant in sym.info.variants {
-					if variant.idx() in idxs {
+					if variant in idxs {
 						continue
 					}
 					g.type_definitions.writeln('//          | ${variant:4d} = ${g.typ(variant.idx()):-20s}')
-					idxs << variant.idx()
+					idxs << variant
 				}
 				idxs.clear()
 				g.type_definitions.writeln('struct ${name} {')
 				g.type_definitions.writeln('\tunion {')
 				for variant in sym.info.variants {
-					if variant.idx() in idxs {
+					if variant in idxs {
 						continue
 					}
+					idxs << variant
 					variant_sym := g.table.sym(variant)
 					mut var := if variant.has_flag(.option) { variant } else { variant.ref() }
 					variant_name := g.get_sumtype_variant_name(variant, variant_sym)
@@ -6713,7 +6714,6 @@ fn (mut g Gen) write_types(symbols []&ast.TypeSymbol) {
 					}
 					var_type := if variant.has_flag(.option) { '${g.typ(var)}*' } else { g.typ(var) }
 					g.type_definitions.writeln('\t\t${var_type} _${variant_name};')
-					idxs << variant.idx()
 				}
 				g.type_definitions.writeln('\t};')
 				g.type_definitions.writeln('\tint _typ;')
