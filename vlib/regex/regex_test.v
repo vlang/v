5e435a71119c177b22f0440e61f1436dd7e080ad
yuyi <yuyi98@163.com>
2024-07-29 19:03:50 +0800

    regex: fix regex.split() (fix #16876) (#21953)

--- vlib/regex/regex_test.v
+++ vlib/regex/regex_test.v
@@ -431,14 +431,14 @@ const (
 	split_test_suite = [
 		Test_split{'abcd 1234 efgh 1234 ghkl1234 ab34546df', r'\d+', ['abcd ', ' efgh ', ' ghkl',
 			' ab', 'df']},
-		Test_split{'abcd 1234 efgh 1234 ghkl1234 ab34546df', r'\a+', [' 1234 ', ' 1234 ', '1234 ',
-			'34546']},
+		Test_split{'abcd 1234 efgh 1234 ghkl1234 ab34546df', r'\a+', ['', ' 1234 ', ' 1234 ', '1234 ',
+			'34546', '']},
 		Test_split{'oggi pippo è andato a casa di pluto ed ha trovato pippo', r'p[iplut]+o', [
-			'oggi ', ' è andato a casa di ', ' ed ha trovato ']},
+			'oggi ', ' è andato a casa di ', ' ed ha trovato ', '']},
 		Test_split{'oggi pibao è andato a casa di pbababao ed ha trovato pibabababao', r'(pi?(ba)+o)', [
-			'oggi ', ' è andato a casa di ', ' ed ha trovato ']},
+			'oggi ', ' è andato a casa di ', ' ed ha trovato ', '']},
 		Test_split{'Today is a good day and tomorrow will be for sure.', r'[Tt]o\w+', [
-			' is a good day and ', ' will be for sure.']},
+			'', ' is a good day and ', ' will be for sure.']},
 		Test_split{'pera\nurl = https://github.com/dario/pig.html\npippo', r'url *= *https?://[\w./]+', [
 			'pera\n', '\npippo']},
 		Test_split{'pera\nurl = https://github.com/dario/pig.html\npippo', r'url *= *https?://.*' +
@@ -446,14 +446,16 @@ const (
 		Test_split{'#.#......##.#..#..##........##....###...##...######.......#.....#..#......#...#........###.#..#.', r'#[.#]{4}##[.#]{4}##[.#]{4}###', [
 			'#.#......##.#..#..##........#', '##.......#.....#..#......#...#........###.#..#.']},
 		Test_split{'#.#......##.#..#..##........##....###...##...######.......#.....#..#......#...#........###.#..#.', r'.*#[.#]{4}##[.#]{4}##[.#]{4}###', [
-			'##.......#.....#..#......#...#........###.#..#.']},
-		Test_split{'1234 Aa dddd Aaf 12334 Aa opopo Aaf', r'Aa.+Aaf', ['1234 ', ' 12334 ']},
+			'', '##.......#.....#..#......#...#........###.#..#.']},
+		Test_split{'1234 Aa dddd Aaf 12334 Aa opopo Aaf', r'Aa.+Aaf', ['1234 ', ' 12334 ', '']},
 		Test_split{'@for something @endfor @for something else @endfor altro testo @for body @endfor uno due @for senza dire più @endfor pippo', r'@for.+@endfor', [
-			' ', ' altro testo ', ' uno due ', ' pippo']},
+			'', ' ', ' altro testo ', ' uno due ', ' pippo']},
 		Test_split{'+++pippo+++\n elvo +++ pippo2 +++ +++ oggi+++', r'\+{3}.*\+{3}', [
-			'\n elvo ', ' ']},
+			'', '\n elvo ', ' ', '']},
 		Test_split{'foobar', r'\d', ['foobar']},
-		Test_split{'1234', r'\d+', []},
+		Test_split{'1234', r'\d+', ['', '']},
+		Test_split{'a-', r'-', ['a', '']},
+		Test_split{'-a', r'-', ['', 'a']},
 	]
 )
 // vfmt on
