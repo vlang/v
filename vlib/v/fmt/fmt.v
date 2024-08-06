d9aa5ae6c580f21c7a679353616855b2fc405f03
yuyi <yuyi98@163.com>
2024-08-06 15:49:20 +0800

    fix fmt.v

--- vlib/v/fmt/fmt.v
+++ vlib/v/fmt/fmt.v
@@ -1068,7 +1068,7 @@ pub fn (mut f Fmt) enum_decl(node ast.EnumDecl) {
 	mut value_align_i := 0
 	mut attr_align_i := 0
 	mut comment_align_i := 0
-	for i, field in node.fields {
+	for field in node.fields {
 		f.write('\t${field.name}')
 		if field.has_expr {
 			if value_aligns[value_align_i].line_nr < field.pos.line_nr {
