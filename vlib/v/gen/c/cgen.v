2d86c34eacf6e72a15c9179e37aed067b278cd1b
CoderMuffin <john@mooseandlemur.com>
2024-08-07 19:45:20 +0100

    cgen: fix generation of closures from methods on aliases (#22004)

--- vlib/v/gen/c/cgen.v
+++ vlib/v/gen/c/cgen.v
@@ -4044,7 +4044,15 @@ fn (mut g Gen) selector_expr(node ast.SelectorExpr) {
 			if !node.expr_type.is_ptr() {
 				g.write('&')
 			}
-			g.expr(node.expr)
+			if !node.expr.is_lvalue() {
+				current_stmt := g.go_before_last_stmt()
+				g.empty_line = true
+				var := g.new_ctemp_var_then_gen(node.expr, node.expr_type)
+				g.write(current_stmt.trim_left('\t '))
+				g.expr(ast.Expr(var))
+			} else {
+				g.expr(node.expr)
+			}
 			if !receiver.typ.is_ptr() {
 				g.write(', sizeof(${expr_styp}))')
 			}
