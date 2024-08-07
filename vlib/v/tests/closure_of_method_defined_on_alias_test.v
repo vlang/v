3467b25bc3f372638b7cd422d2da47caf532dcea
Delyan Angelov <delian66@gmail.com>
2024-08-07 21:11:52 +0300

    assert that the produced closure for the method was called, and returns a correct value

--- vlib/v/tests/closure_of_method_defined_on_alias_test.v
+++ vlib/v/tests/closure_of_method_defined_on_alias_test.v
@@ -1,13 +1,15 @@
 type UInt = u32
 
-fn (me UInt) member() {
+fn (me UInt) member() u32 {
 	println('member called')
+	return me * 10
 }
 
 fn test_1() {
 	println('start')
 	x := UInt(4).member
 	println('med')
-	x()
+	res := x()
 	println('done')
+	assert res == 40
 }
