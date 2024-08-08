e0a0d8cfb82c8fce463ca6e98283c3e93507871d
Martin Skou <msj@infoserv.dk>
2024-08-08 22:21:51 +0200

    Update mbedtls.c.v
    Imported functions needed to use SNI

--- vlib/net/mbedtls/mbedtls.c.v
+++ vlib/net/mbedtls/mbedtls.c.v
@@ -182,7 +182,11 @@ fn C.mbedtls_ssl_free(&C.mbedtls_ssl_context)
 fn C.mbedtls_ssl_config_init(&C.mbedtls_ssl_config)
 fn C.mbedtls_ssl_config_defaults(&C.mbedtls_ssl_config, int, int, int) int
 fn C.mbedtls_ssl_config_free(&C.mbedtls_ssl_config)
-
+fn C.mbedtls_ssl_conf_sni(&C.mbedtls_ssl_config, fn (voidptr, &C.mbedtls_ssl_context, &char, int) int, voidptr)
+fn C.mbedtls_ssl_set_hs_ca_chain(&C.mbedtls_ssl_config, &C.mbedtls_x509_crt, &C.mbedtls_x509_crl)
+fn C.mbedtls_ssl_set_hs_own_cert(&C.mbedtls_ssl_context, &C.mbedtls_x509_crt, &C.mbedtls_pk_context) int
+fn C.mbedtls_ssl_set_hs_authmode(&C.mbedtls_ssl_context, int)
+	
 fn C.mbedtls_pk_init(&C.mbedtls_pk_context)
 fn C.mbedtls_pk_free(&C.mbedtls_pk_context)
 fn C.mbedtls_pk_parse_key(&C.mbedtls_pk_context, &u8, usize, &u8, usize, fn (voidptr, &u8, usize) int,
@@ -204,3 +208,4 @@ fn C.mbedtls_x509_crt_parse(&C.mbedtls_x509_crt, &u8, usize) int
 fn C.mbedtls_x509_crt_parse_file(&C.mbedtls_x509_crt, &char) int
 
 fn C.mbedtls_high_level_strerr(int) &char
+
