be1bb60110fa6359a792986e46549ed15c15c2b0
kylepritchard <Kylepritchard@hotmail.co.uk>
2024-08-08 20:41:09 +0100

    parser: update `@include` in templates, to work with relative paths & prevent recursive calls (#21943)

--- vlib/v/tests/tmpl_test.v
+++ vlib/v/tests/tmpl_test.v
@@ -102,3 +102,45 @@ fn test_tmpl_interpolation() {
 fn my_fn(s string) string {
 	return s
 }
+
+// Add more examples of potentially buggy patterns in vlib/v/tests/tmpl/index.html
+fn test_tmpl_comptime() {
+	index := $tmpl('tmpl/index.html').trim_space()
+	// dump(index)
+	assert index.contains('<br>Line ending with percent %\n')
+	assert index.contains('<br>Line ending with at $\n')
+	assert index.contains('<br>Line ending with ampersand &\n')
+	assert index.contains('<br>Line ending with hash #\n')
+	assert index.contains('<br>Line ending with slash /\n')
+	assert index.contains('<br>Line ending with dollar $\n')
+	assert index.contains('<br>Line ending with caret ^\n')
+}
+
+// Add a tests for @include
+
+// File contents for building repsonse
+const base = '<p>This is the base file</p>'
+const child = '<p>This is the child file</p>'
+const grandchild = '<p>This is the grandchild file</p>'
+const parent = '<p>This is the parent file</p>'
+
+// Call the parent file which contains all child templates
+fn test_tmpl_include_parent() {
+	expected := [base, base, child, base, base, child, grandchild, parent].join('\n')
+	parent_tmpl := $tmpl('tmpl/parent.html')
+	assert parent_tmpl.contains(expected)
+}
+
+// Test the child template which calls parent template
+fn test_tmpl_include_child() {
+	expected := [base, child].join('\n')
+	child_tmpl := $tmpl('tmpl/nested/child.html')
+	assert child_tmpl.contains(expected)
+}
+
+// Test the grandchild templates calling both parent and grandparent templates
+fn test_tmpl_include_grandchild() {
+	expected := [base, base, child, grandchild].join('\n')
+	child_tmpl := $tmpl('tmpl/nested/nested_deeper/grandchild.html')
+	assert child_tmpl.contains(expected)
+}
