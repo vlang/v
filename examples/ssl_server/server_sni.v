10e45eaa4fd37ce709bbf30b18f2f94e53a76b23
Martin Skou <msj@infoserv.dk>
2024-08-08 22:29:12 +0200

    Create server_sni.v
    SSL Server example using the SNI extension

---
+++ examples/ssl_server/server_sni.v
@@ -0,0 +1,35 @@
+import io
+import os
+import net.mbedtls
+
+// Server Name Indication (SNI) is defined in RFC 6066.
+// See https://mbed-tls.readthedocs.io/en/latest/kb/how-to/use-sni/
+
+fn get_cert(host string) !mbedtls.SNICerts {
+  // callback to return the certificates to use for the connection
+  
+	println('read certs for ${host}')
+	sni_certs := mbedtls.SNICerts{
+		cert: os.read_file('cert/server.crt') or { return error('Failed to read certificate') }
+		key: os.read_file('cert/server.key') or { return error('Failed to read key') }
+	}
+	return sni_certs
+}
+
+fn main() {
+	mut server := mbedtls.new_ssl_listener('0.0.0.0:8443', mbedtls.SSLConnectConfig{
+    verify: os.resource_abs_path('cert/ca.crt')
+		cert: os.resource_abs_path('cert/server.crt')
+		cert_key: os.resource_abs_path('cert/server.key')
+		validate: false
+		get_certificate: get_cert // set the SNI callback to enable this feature
+	})!
+
+	mut client := server.accept()!
+	mut reader := io.new_buffered_reader(reader: client)
+	mut request := reader.read_line()!
+	println(request)
+	client.write_string('HTTP/1.1 200 OK\r\n')!
+	client.shutdown()!
+	server.shutdown()!
+}
