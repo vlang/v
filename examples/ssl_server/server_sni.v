f7600b2d08b5def8b27327fd7a458a7ad028d2eb
Delyan Angelov <delian66@gmail.com>
2024-08-09 19:52:52 +0300

    add more comments to server_sni.v, and a loop to handle multiple requests

--- examples/ssl_server/server_sni.v
+++ examples/ssl_server/server_sni.v
@@ -2,18 +2,29 @@ import io
 import os
 import net.mbedtls
 
+// This example shows a very minimal HTTP server, that supports SNI.
 // Server Name Indication (SNI) is defined in RFC 6066.
 // See https://mbed-tls.readthedocs.io/en/latest/kb/how-to/use-sni/
 
-fn get_cert(host string) !mbedtls.SNICerts {
-	// callback to return the certificates to use for the connection
+// Use the following commands, in separate shells, to test that it works,
+// after you start the server:
+//   curl -ik --resolve 1x.dk:8443:0.0.0.0        https://1x.dk:8443/abcd
+//   curl -ik --resolve example.com:8443:0.0.0.0  https://example.com:8443/xyz
+// Both of them should work, and the server should show that it tried to load
+// certificates, responding to the given domain name in each of the requests.
 
-	println('read certs for ${host}')
+// callback to return the certificates to use for the connection
+fn get_cert(host string) !mbedtls.SNICerts {
+	println('reading certificates for ${host} ...')
+	// For our example, just always use the same certificates. In a real server,
+	// that should be dependent on the host parameter, and perhaps there should
+	// be some caching, so that they are not read each time for each request from
+	// the filesystem.
 	sni_certs := mbedtls.SNICerts{
-		cert: os.read_file('cert/server.crt') or { return error('Failed to read certificate') }
-		key: os.read_file('cert/server.key') or { return error('Failed to read key') }
+		cert: os.read_file(os.resource_abs_path('cert/server.crt'))!
+		key: os.read_file(os.resource_abs_path('cert/server.key'))!
 	}
-	println('return certs')
+	println('returning certificates...')
 	return sni_certs
 }
 
@@ -25,15 +36,28 @@ fn main() {
 		validate: false
 		get_certificate: get_cert // set the SNI callback to enable this feature
 	})!
+	println('Listening on https://0.0.0.0:8443/ ...')
+	for {
+		mut client := server.accept() or {
+			println('accept error: ${err}')
+			continue
+		}
+		mut reader := io.new_buffered_reader(reader: client)
+		for {
+			line := reader.read_line() or { break }
+			if line.len == 0 {
+				break
+			}
+			println(line)
+		}
+		client.write_string('HTTP/1.1 200 OK\r\n') or { continue }
 
-	mut client := server.accept() or {
-		println('accept error: ${err}')
-		return
+		client.write_string('Content-Type: text/html\r\n') or { continue }
+		client.write_string('Connection: close\r\n') or { continue }
+		client.write_string('\r\n') or { continue }
+
+		client.write_string('Hello\n') or { continue }
+		client.shutdown()!
 	}
-	mut reader := io.new_buffered_reader(reader: client)
-	mut request := reader.read_line()!
-	println(request)
-	client.write_string('HTTP/1.1 200 OK\r\n')!
-	client.shutdown()!
 	server.shutdown()!
 }
