68ded6a1b4a3e2acd271e48ac103906885a93f11
Felipe Pena <felipensp@gmail.com>
2024-08-17 10:37:19 -0300

    fix

---
+++ vlib/v/tests/option_free_method_test.v
@@ -0,0 +1,35 @@
+pub struct Test2 {
+	a int
+}
+
+pub struct Test {
+	a Test2
+	b ?string
+	c ?Test2
+	d ?&Test2
+}
+
+fn test_main() {
+	t := Test{
+		b: 'b'
+	}
+
+	println('t: ${t}')
+
+	defer {
+		unsafe {
+			t.free()
+		}
+	}
+
+	mut t2 := ?Test(Test{
+		b: 'b'
+	})
+	println('t: ${t2}')
+
+	defer {
+		unsafe {
+			t2?.free()
+		}
+	}
+}
