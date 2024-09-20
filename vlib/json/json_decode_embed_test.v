d4c45928fd640fcc11aee9af48dbbdf900f6d076
Felipe Pena <felipensp@gmail.com>
2024-09-20 09:53:40 -0300

    fix

---
+++ vlib/json/json_decode_embed_test.v
@@ -0,0 +1,28 @@
+module main
+
+import json
+
+struct Json2 {
+	inner []f64
+}
+
+struct Json {
+	Json2
+	test f64
+}
+
+fn test_main() {
+	str := '{
+		"inner": [1, 2, 3, 4, 5],
+		"test": 1.0
+	}'
+	data := json.decode(Json, str) or {
+		eprintln('Failed to decode json, error: ${err}')
+		return
+	}
+	println(data)
+	assert data.inner.len == 5
+	assert data.inner[0] == 1.0
+	assert data.inner[4] == 5.0
+	assert data.test == 1.0
+}
