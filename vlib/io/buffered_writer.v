64b292943b4c2b477172bd77eae6429eb13a99c6
Einar Hjortdal <einar.hjortdal@outlook.com>
2024-09-20 14:48:14 +0200

    Add buffered writer
    Signed-off-by: Einar Hjortdal <einar.hjortdal@outlook.com>

---
+++ vlib/io/buffered_writer.v
@@ -0,0 +1,78 @@
+module io
+
+pub struct BufferedWriter {
+mut:
+	n  int
+	wr Writer
+pub mut:
+	buf []u8
+}
+
+@[params]
+pub struct BufferedWriterConfig {
+pub:
+	writer Writer
+	cap    int = 128 * 1024
+}
+
+pub fn new_buffered_writer(o BufferedWriterConfig) !&BufferedWriter {
+	if o.cap < 1 {
+		return error('`o.cap` must be a positive integer')
+	}
+
+	return &BufferedWriter{
+		buf: []u8{len: o.cap, cap: o.cap}
+		wr:  o.writer
+	}
+}
+
+pub fn (mut b BufferedWriter) reset() {
+	cap := b.buf.len
+	b.buf = []u8{len: cap, cap: cap}
+	b.n = 0
+}
+
+pub fn (b BufferedWriter) buffered() int {
+	return b.n
+}
+
+pub fn (mut b BufferedWriter) flush() ! {
+	if b.buffered() == 0 {
+		return
+	}
+
+	n := b.wr.write(b.buf[0..b.n])!
+	if n < b.n {
+		return error('Writer accepted less bytes than expected without returning any explicit error.')
+	}
+
+	b.n = 0
+	return
+}
+
+pub fn (b BufferedWriter) available() int {
+	return b.buf.len - b.n
+}
+
+pub fn (mut b BufferedWriter) write(src []u8) !int {
+	mut p := src.clone()
+	len := p.len
+	mut nn := 0
+	for len > b.available() {
+		mut n := 0
+		if b.buffered() == 0 {
+			n = b.wr.write(p)!
+		} else {
+			n = copy(mut b.buf[b.n..], p)
+			b.n += n
+			b.flush()!
+		}
+		nn += n
+		p = p[n..].clone()
+	}
+
+	n := copy(mut b.buf[b.n..], p)
+	b.n += n
+	nn += n
+	return nn
+}
---
+++ vlib/io/buffered_writer_test.v
@@ -0,0 +1,60 @@
+import arrays
+import io
+
+struct ArrayWriter {
+pub mut:
+	result []u8
+}
+
+// implement io.Writer
+fn (mut aw ArrayWriter) write(buf []u8) !int {
+	len := buf.len
+	mut res := 0
+	for i := 0; i < len; i++ {
+		aw.result = arrays.concat(aw.result, buf[i])
+		res++
+	}
+	return res
+}
+
+fn create_data(n int) []u8 {
+	mut res := []u8{}
+	for i := 0; i < n; i++ {
+		res = arrays.concat(res, 88) // x
+	}
+	res = arrays.concat(res, 10) // \n
+	return res
+}
+
+fn test_create_buffered_writer() {
+	bw := io.new_buffered_writer(writer: ArrayWriter{}, cap: 10)!
+}
+
+fn test_write() {
+	mut aw := ArrayWriter{}
+	mut bw := io.new_buffered_writer(writer: aw, cap: 10)!
+	data := create_data(6)
+	w1 := bw.write(data)!
+
+	assert w1 == 7 // 6*x + \n
+	assert bw.buffered() == 7
+
+	w2 := bw.write(data)!
+	assert w2 == 7
+	assert bw.buffered() == 4
+	assert aw.result.len == 10
+}
+
+fn test_flush() {
+	mut aw := ArrayWriter{}
+	mut bw := io.new_buffered_writer(writer: aw, cap: 10)!
+	data := create_data(6)
+	w := bw.write(data)!
+
+	assert w == 7 // 6*x + \n
+	assert bw.buffered() == 7
+
+	bw.flush()!
+	assert bw.buffered() == 0
+	assert aw.result.len == 7
+}
