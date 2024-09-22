4b176894fbb435e349945a6f4c66a74bdf0c48d1
Delyan Angelov <delian66@gmail.com>
2024-09-22 10:32:47 +0300

    os.asset: move comments to a module level README.md file

--- vlib/os/asset/asset.v
+++ vlib/os/asset/asset.v
@@ -1,13 +1,5 @@
 module asset
 
-// The `asset` module provides a cross platform way to read assets, without cluttering
-// the user code with comptime conditionals, like `$if ios {` or `$if android {` etc.
-// Currently it supports Android and desktop applications, that do not use archived/zipped
-// files, but could be extended to support them too in the future.
-// It relies on the assumption that each platform has a way to either read files,
-// relative to the location of the executable, or a platform specific way, to package all
-// asset files into the application archive/package/executable, that is then distributed
-// to the end users.
 import os
 
 // get_path returns a platform specific path, based on the given asset base folder and relative path
