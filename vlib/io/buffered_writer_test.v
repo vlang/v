bd28d73107e1417c69d3bf8ee29117327776350f
Einar Hjortdal <einar.hjortdal@outlook.com>
2024-09-22 11:12:55 +0200

    Add basic test for BufferedWriter.write method

--- vlib/io/buffered_writer_test.v
+++ vlib/io/buffered_writer_test.v
@@ -1,5 +1,5 @@
-import arrays
 import io
+import crypto.rand
 
 struct ArrayWriter {
 pub mut:
@@ -29,6 +29,34 @@ fn create_data(n int) []u8 {
 
 fn test_create_buffered_writer() {
 	bw := io.new_buffered_writer(writer: ArrayWriter{}, cap: 10)!
+}
+
+fn test_basic_write() {
+	mut aw := ArrayWriter{}
+	max := 65536
+	mut bw := io.new_buffered_writer(writer: aw, cap: max)!
+
+	// write less data than buffer capacity, the underlying writer should receive no data.
+	safe_max := 65536 - 255
+	mut total := 0
+	for total < safe_max {
+		a := rand.read(1)!
+		r := a[0]
+		d := rand.read(r)!
+		w := bw.write(d)!
+		total += w
+	}
+	assert total == bw.buffered()
+	assert aw.result.len == 0
+
+	// now exceed buffer capacity by a little
+	random_array := rand.read(1)!
+	little := random_array[0]
+	excess := bw.available() + little
+	excess_data := rand.read(excess)!
+	w := bw.write(excess_data)!
+	assert bw.available() == max - little
+	assert aw.result.len == max
 }
 
 fn test_write() {
