b9094b01f1689428b39e5c21567c1645ce38ad68
Meng Zhuo <mengzhuo1203@gmail.com>
2024-09-26 14:16:49 +0800

    vlib: enable GC lib on rv64 build

--- vlib/builtin/builtin_d_gcboehm.c.v
+++ vlib/builtin/builtin_d_gcboehm.c.v
@@ -44,7 +44,7 @@ $if dynamic_boehm ? {
 	$if macos || linux {
 		#flag -DGC_BUILTIN_ATOMIC=1
 		#flag -I @VEXEROOT/thirdparty/libgc/include
-		$if (prod && !tinyc && !debug) || !(amd64 || arm64 || i386 || arm32) {
+		$if (prod && !tinyc && !debug) || !(amd64 || arm64 || i386 || arm32 || rv64) {
 			// TODO: replace the architecture check with a `!$exists("@VEXEROOT/thirdparty/tcc/lib/libgc.a")` comptime call
 			#flag @VEXEROOT/thirdparty/libgc/gc.o
 		} $else {
