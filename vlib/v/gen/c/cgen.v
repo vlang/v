d8f18fb1107aa65bb0d3392c45796874c1f10228
Felipe Pena <felipensp@gmail.com>
2024-09-29 14:24:25 -0300

    fix

--- vlib/v/gen/c/cgen.v
+++ vlib/v/gen/c/cgen.v
@@ -7347,11 +7347,16 @@ fn (mut g Gen) type_default(typ_ ast.Type) string {
 		.struct_ {
 			mut has_none_zero := false
 			info := sym.info as ast.Struct
-			mut init_str := if (info.is_anon && !g.inside_global_decl)
-				|| (g.inside_global_decl && g.pref.ccompiler_type == .tinyc) {
+			mut init_str := if (info.is_anon && !g.inside_global_decl) {
 				'(${g.typ(typ)}){'
 			} else {
 				'{'
+			}
+			$if windows {
+				if g.pref.ccompiler_type == .tinyc && !typ.has_flag(.shared_f)
+					&& g.inside_global_decl {
+					init_str = '(${g.typ(typ)}){'
+				}
 			}
 			if sym.language in [.c, .v] {
 				for field in info.fields {
