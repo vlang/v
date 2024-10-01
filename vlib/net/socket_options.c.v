7f1558458f464587c96138848044c5d3d48ede10
Carlos Esquerdo Bernat <carlos@biteindustry.com>
2024-10-01 08:43:37 +0200

    comment tcp_defer_accept flag

--- vlib/net/socket_options.c.v
+++ vlib/net/socket_options.c.v
@@ -21,7 +21,7 @@ pub enum SocketOption {
 	ipv6_only        = C.IPV6_V6ONLY
 	ip_proto_ipv6    = C.IPPROTO_IPV6
 	//tcp_quickack     = C.TCP_QUICKACK // TODO make it work in os != linux
-	tcp_defer_accept = C.TCP_DEFER_ACCEPT
+	//tcp_defer_accept = C.TCP_DEFER_ACCEPT // TODO make it work in windows
 	tcp_fastopen     = C.TCP_FASTOPEN
 	reuse_port       = C.SO_REUSEPORT
 }
