af006f0b99e8bf768f2a88e529ce6921a890286c
Carlos Esquerdo Bernat <carlos@biteindustry.com>
2024-10-01 08:59:51 +0200

    inline sizeof(int)

--- vlib/net/tcp.c.v
+++ vlib/net/tcp.c.v
@@ -5,7 +5,6 @@ import strings
 
 pub const tcp_default_read_timeout = 30 * time.second
 pub const tcp_default_write_timeout = 30 * time.second
-pub const int_size = sizeof(int)
 
 // TCPDialer is a concrete instance of the Dialer interface,
 // for creating tcp connections.
@@ -572,7 +571,7 @@ pub fn tcp_socket_from_handle_raw(sockfd int) TcpSocket {
 }
 
 fn (mut s TcpSocket) set_option(level int, opt int, value int) ! {
-	socket_error(C.setsockopt(s.handle, level, opt, &value, int_size))!
+	socket_error(C.setsockopt(s.handle, level, opt, &value, sizeof(int)))!
 }
 
 pub fn (mut s TcpSocket) set_option_bool(opt SocketOption, value bool) ! {
