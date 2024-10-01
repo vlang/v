30952cf2bbf4d94103d30f0e7ac07b5bd4067bd2
Felipe Pena <felipensp@gmail.com>
2024-10-01 13:04:00 -0300

    cgen: fix codegen for const of fixed array (fix #22357) (#22364)

--- vlib/v/gen/c/cgen.v
+++ vlib/v/gen/c/cgen.v
@@ -6248,7 +6248,13 @@ fn (mut g Gen) const_decl_init_later_msvc_string_fixed_array(mod string, name st
 	cname := g.c_const_name(name)
 	mut init := strings.new_builder(100)
 	for i, elem_expr in expr.exprs {
-		init.writeln(g.expr_string_surround('\t${cname}[${i}] = ', elem_expr, ';'))
+		if elem_expr is ast.ArrayInit && elem_expr.is_fixed {
+			elem_typ := g.typ(elem_expr.typ)
+			init.writeln(g.expr_string_surround('\tmemcpy(${cname}[${i}], (${elem_typ})',
+				elem_expr, ', sizeof(${elem_typ}));'))
+		} else {
+			init.writeln(g.expr_string_surround('\t${cname}[${i}] = ', elem_expr, ';'))
+		}
 	}
 	mut def := '${styp} ${cname}'
 	g.global_const_defs[util.no_dots(name)] = GlobalConstDef{
