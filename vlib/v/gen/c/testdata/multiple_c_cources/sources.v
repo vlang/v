e0bbf9a3dc9083fd83ab0b729f31c404193748c5
Delyan Angelov <delian66@gmail.com>
2024-10-01 13:40:52 +0300

    add a test

---
+++ vlib/v/gen/c/testdata/multiple_c_cources/sources.v
@@ -0,0 +1,14 @@
+module multiple_c_cources
+
+#flag @VMODROOT/file1.c
+#flag @VMODROOT/file2.c
+
+fn C.f1() int
+fn C.f2() int
+
+pub fn call_c_functions() {
+	println('start')
+	println(C.f1())
+	println(C.f2())
+	println('done')
+}
