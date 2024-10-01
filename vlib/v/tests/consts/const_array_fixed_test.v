0ef5e2a223498a4fdf21c62da32760dfc571cb6c
Felipe Pena <felipensp@gmail.com>
2024-10-01 07:54:32 -0300

    improve test

--- vlib/v/tests/consts/const_array_fixed_test.v
+++ vlib/v/tests/consts/const_array_fixed_test.v
@@ -10,7 +10,23 @@ __global (
 )
 
 const c = [[a.bar]!]!
+const d = [[a.bar]]!
+const e = [[a.bar]!]
+const f = [[[a.bar]!]!]!
+const g = [a.bar]
 
-fn test_main() {
-	dump(c == [[120]!]!)
+fn test_selector() {
+	assert dump(c == [[a.bar]!]!)
+	assert dump(d == [[a.bar]]!)
+	assert dump(e == [[a.bar]!])
+	assert dump(f == [[[a.bar]!]!]!)
+	assert dump(g == [a.bar])
+}
+
+fn test_literal() {
+	assert dump(c == [[120]!]!)
+	assert dump(d == [[120]]!)
+	assert dump(e == [[120]!])
+	assert dump(f == [[[120]!]!]!)
+	assert dump(g == [120])
 }
