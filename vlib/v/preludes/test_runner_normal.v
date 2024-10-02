dafca9353cc5ae3cc19f6dafb8958114315aa4b1
Delyan Angelov <delian66@gmail.com>
2024-10-03 00:36:05 +0300

    tests: use `value` and the .len field, for the left/right values, shown in failing assertions (#22393)

--- vlib/v/preludes/test_runner_normal.v
+++ vlib/v/preludes/test_runner_normal.v
@@ -131,10 +131,10 @@ fn (mut runner NormalTestRunner) assert_fail(i &VAssertMetaInfo) {
 	}
 	eprintln('${final_filepath} ${final_funcname}')
 	if i.op.len > 0 && i.op != 'call' {
-		mut lvtitle := '    Left value:'
-		mut rvtitle := '    Right value:'
-		mut slvalue := '${i.lvalue}'
-		mut srvalue := '${i.rvalue}'
+		mut lvtitle := '    Left value (len: ${i.lvalue.len}):'
+		mut rvtitle := '    Right value (len: ${i.rvalue.len}):'
+		mut slvalue := '`${i.lvalue}`'
+		mut srvalue := '`${i.rvalue}`'
 		// Do not print duplicate values to avoid confusion. In mosts tests the developer does
 		// `assert foo() == [1, 2, 3]`
 		// There's no need to print "[1, 2, 3]" again (left: [1,2,3,4]  right:[1,2,3])
