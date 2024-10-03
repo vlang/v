9d3ffbee5066da59478e7eed8431931f00b314ee
kbkpbot <kbkpbot@sina.com>
2024-10-03 09:56:33 +0800

    fix iconv type -cstrict

--- vlib/encoding/iconv/iconv_nix.c.v
+++ vlib/encoding/iconv/iconv_nix.c.v
@@ -5,9 +5,9 @@ module iconv
 #include <iconv.h>
 #flag darwin -liconv
 
-fn C.iconv_open(tocode &u8, fromcode &u8) voidptr
+fn C.iconv_open(tocode charptr, fromcode charptr) voidptr
 fn C.iconv_close(cd voidptr) int
-fn C.iconv(cd voidptr, inbuf &&u8, inbytesleft &usize, outbuf &&u8, outbytesleft &usize) usize
+fn C.iconv(cd voidptr, inbuf &charptr, inbytesleft &usize, outbuf &charptr, outbytesleft &usize) usize
 
 // conv convert `fromcode` encoding string to `tocode` encoding string
 @[direct_array_access]
@@ -35,7 +35,7 @@ fn conv(tocode string, fromcode string, src &u8, src_len int) ![]u8 {
 		else {}
 	}
 
-	mut cd := C.iconv_open(dst_encoding.str, src_encoding.str)
+	mut cd := C.iconv_open(charptr(dst_encoding.str), charptr(src_encoding.str))
 	if isize(cd) == -1 {
 		return error('platform can\'t convert from ${src_encoding} to ${dst_encoding}')
 	}
@@ -43,8 +43,8 @@ fn conv(tocode string, fromcode string, src &u8, src_len int) ![]u8 {
 
 	mut dst := []u8{len: (src_len + 1) * 4} // this should be enough to hold the dst encoding string
 
-	mut src_ptr := &u8(src)
-	mut dst_ptr := &u8(dst.data)
+	mut src_ptr := charptr(src)
+	mut dst_ptr := charptr(dst.data)
 	mut src_left := usize(src_len)
 	mut dst_left := usize(dst.len)
 	res := C.iconv(cd, &src_ptr, &src_left, &dst_ptr, &dst_left)
