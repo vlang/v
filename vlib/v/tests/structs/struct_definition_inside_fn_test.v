e2962c9fb3dd9f96b8d045478a57744733da6f72
Jose Mendoza <56417208+StunxFS@users.noreply.github.com>
2024-10-07 15:31:06 +0000

    vfmt

--- vlib/v/tests/structs/struct_definition_inside_fn_test.v
+++ vlib/v/tests/structs/struct_definition_inside_fn_test.v
@@ -1,8 +1,10 @@
 fn test_struct_definition_inside_fn() {
+
 	struct App {
 		name string
 	}
-	assert App{"make"}.name == "make"
-	app := App{"v"}
-	assert app.name == "v"
+
+	assert App{'make'}.name == 'make'
+	app := App{'v'}
+	assert app.name == 'v'
 }
