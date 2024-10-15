0fb7fbde94daa6eb1fdc6594b3a138c10cc58c70
Delyan Angelov <delian66@gmail.com>
2024-10-15 11:32:19 +0300

    add assertions about .original_nr_channels too

--- vlib/stbi/stbi_test.v
+++ vlib/stbi/stbi_test.v
@@ -31,6 +31,7 @@ fn test_stbi_read_write() {
 	assert d_s.width == d_d.width
 	assert d_s.height == d_d.height
 	assert d_s.nr_channels == d_d.nr_channels
+	assert d_s.original_nr_channels == 4
 
 	mut v_s := unsafe { &u32(d_s.data) }
 	mut v_d := unsafe { &u32(d_d.data) }
@@ -52,6 +53,7 @@ fn test_stbi_resize() {
 	new_width, new_height := 100, 100
 
 	d_r := stbi.resize_uint8(d_s, new_width, new_height) or { panic(err) }
+	assert d_r.original_nr_channels == 4
 	println('Resized Image source data:\n ${d_s}')
 
 	out_path := os.join_path(tfolder, 'test.png')
@@ -66,13 +68,24 @@ fn test_stbi_resize() {
 	assert d_d.width == new_width
 	assert d_d.height == new_height
 	assert d_d.nr_channels == d_r.nr_channels
+	assert d_d.original_nr_channels == 4
 	os.rm(out_path) or {}
 }
 
 fn test_load_image_with_channels_different_than_4() {
 	img := stbi.load(background_path)!
 	assert img.nr_channels == 4, 'by default, stbi.load should convert images to 4 channels'
+	assert img.original_nr_channels == 3, 'the default, should not affect the img.original_nr_channels field; it should be based solely on the original image data on disk'
+
+	img_resized := stbi.resize_uint8(img, 128, 128)!
+	assert img_resized.nr_channels == 4
+	assert img_resized.original_nr_channels == 3
 
 	img3 := stbi.load(background_path, desired_channels: 0)!
 	assert img3.nr_channels == 3, 'stbi.load, with desired_channels: 0, should return an image, without any conversion. the nr_channels should be determined by the image data'
+	assert img.original_nr_channels == 3, 'the default, should not affect the img.original_nr_channels field; it should be based solely on the original image data on disk'
+
+	img3_resized := stbi.resize_uint8(img3, 128, 128)!
+	assert img3_resized.nr_channels == 3
+	assert img3_resized.original_nr_channels == 3
 }
