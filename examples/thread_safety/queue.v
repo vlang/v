183aafdf8af0b552b513358c4e7faf2b163d48c9
Hitalo Souza <enghitalo@gmail.com>
2024-10-21 15:51:17 -0400

    thread safe queue

--- examples/thread_safety/queue.v
+++ examples/thread_safety/queue.v
@@ -1,56 +1,77 @@
-/*
-This example demonstrates thread safety using channels in V.
-
-### Functions:
-- `producer(ch chan int)`: This function simulates a producer that sends integers from 1 to 99 to
-the channel `ch`. It prints each produced item.
-- `consumer(ch chan int)`: This function simulates a consumer that receives integers from the
-channel `ch`.
-
-### Thread Safety:
-- The use of channels ensures thread safety by providing a synchronized way to communicate between
-the producer and consumer threads.
-- Channels in V are designed to handle concurrent access, preventing race conditions and ensuring
-that data is safely passed between threads.
-- The `select` statement in the consumer function allows it to handle timeouts gracefully,
-ensuring that the program does not hang if the producer is not ready.
-*/
 import time
 import sync
 
-fn producer(ch chan int, mut mtx sync.Mutex) {
-	for i in 1 .. 100 {
+type Callback = fn (id string)
+
+fn producer(producer_name string, mut arr []Callback, mut mtx sync.Mutex) {
+	for i in 1 .. 5 {
 		mtx.lock()
-		ch <- i
+		arr << fn [producer_name, i] (consumer_name string) {
+			println('task ${i} created by producer ${producer_name}: consumed by ${consumer_name}')
+			time.sleep(500 * time.millisecond)
+		}
 		println('Produced: ${i}')
+		time.sleep(50 * time.millisecond)
 		mtx.unlock()
 	}
 }
 
-fn consumer(ch chan int, mut mtx sync.Mutex) {
+fn consumer(consumer_name string, mut arr []Callback, mut mtx sync.Mutex) {
 	for {
 		mtx.lock()
-		select {
-			item := <-ch {
-				println('Consumed: ${item}')
-			}
-			500 * time.millisecond {
-				println('Timeout: No producers were ready within 0.5s')
-				mtx.unlock()
-				break
-			}
+		if arr.len > 0 {
+			item := arr[0]
+			arr.delete(0)
+
+			mtx.unlock()
+			item(consumer_name) // run after unlocking to allow other threads to consume
+			continue
+		} else {
+			println('- No items to consume')
+			mtx.unlock()
+
+			// time.sleep(500 * time.millisecond)
+			// continue // uncomment to run forever
+
+			break // uncomment to stop after consuming all items
 		}
-		mtx.unlock()
 	}
+}
+
+fn heavy_processing(queue_id string) {
+	println('One more: ${queue_id}')
+	time.sleep(500 * time.millisecond)
 }
 
 fn main() {
 	mut mtx := sync.new_mutex()
-	ch := chan int{cap: 100}
+	mut arr := []Callback{}
 
-	producer_thread := spawn producer(ch, mut mtx)
-	consumer_thread := spawn consumer(ch, mut mtx)
+	producer_threads := [
+		spawn producer('Paula', mut &arr, mut mtx),
+		spawn producer('Adriano', mut &arr, mut mtx),
+		spawn producer('Kaka', mut &arr, mut mtx),
+		spawn producer('Hitalo', mut &arr, mut mtx),
+		spawn producer('Jonh', mut &arr, mut mtx),
+	]
 
-	producer_thread.wait()
-	consumer_thread.wait()
+	mut consumer_threads := [
+		spawn consumer('consumer number 0', mut &arr, mut mtx),
+	]
+
+	// spawn 16 consumers
+	for i in 1 .. 16 {
+		consumer_threads << spawn consumer('consumer number ${i}', mut &arr, mut mtx)
+	}
+
+	mtx.lock()
+	arr << heavy_processing
+	mtx.unlock()
+
+	for t in producer_threads {
+		t.wait()
+	}
+	for t in consumer_threads {
+		t.wait()
+	}
 }
