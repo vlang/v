71e19e5a054fd567259da94ae8b9687a82257e74
Felipe Pena <felipensp@gmail.com>
2024-10-22 11:01:26 -0300

    improve

--- vlib/v/gen/c/cgen.v
+++ vlib/v/gen/c/cgen.v
@@ -5524,32 +5524,29 @@ fn (mut g Gen) return_stmt(node ast.Return) {
 	g.defer_return_tmp_var = tmpvar
 	ret_typ := g.ret_styp(g.unwrap_generic(fn_ret_type))
 
+	// `return fn_call_opt()`
+	if node.exprs.len == 1 && (fn_return_is_option || fn_return_is_result)
+		&& node.exprs[0] is ast.CallExpr && node.exprs[0].return_type == g.fn_decl.return_type
+		&& node.exprs[0].or_block.kind == .absent {
+		if g.defer_stmts.len > 0 {
+			g.write('${ret_typ} ${tmpvar} = ')
+			g.expr(node.exprs[0])
+			g.writeln(';')
+			g.write_defer_stmts_when_needed()
+			g.writeln('return ${tmpvar};')
+		} else {
+			g.write_defer_stmts_when_needed()
+			g.write('return ')
+			g.expr(node.exprs[0])
+			g.writeln(';')
+		}
+		return
+	}
 	mut use_tmp_var := g.defer_stmts.len > 0 || g.defer_profile_code.len > 0
 		|| g.cur_lock.lockeds.len > 0
 		|| (fn_return_is_multi && node.exprs.len >= 1 && fn_return_is_option)
 		|| fn_return_is_fixed_array
 		|| (fn_return_is_multi && node.types.any(g.table.final_sym(it).kind == .array_fixed))
-
-	if node.exprs.len == 1 {
-		// `return fn_call_opt()`
-		if (fn_return_is_option || fn_return_is_result) && node.exprs[0] is ast.CallExpr
-			&& node.exprs[0].return_type == g.fn_decl.return_type
-			&& node.exprs[0].or_block.kind == .absent {
-			if use_tmp_var {
-				g.write('${ret_typ} ${tmpvar} = ')
-				g.expr(node.exprs[0])
-				g.writeln(';')
-				g.write_defer_stmts_when_needed()
-				g.writeln('return ${tmpvar};')
-			} else {
-				g.write_defer_stmts_when_needed()
-				g.write('return ')
-				g.expr(node.exprs[0])
-				g.writeln(';')
-			}
-			return
-		}
-	}
 	// handle promoting none/error/function returning _option'
 	if fn_return_is_option {
 		option_none := node.exprs[0] is ast.None
