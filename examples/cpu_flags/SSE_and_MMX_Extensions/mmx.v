3b997aade8fa37f41281183a8ada96d82e14b1d2
Hitalo Souza <enghitalo@gmail.com>
2024-10-24 18:31:46 -0400

    more

--- examples/cpu_flags/SSE_and_MMX_Extensions/mmx.v
+++ examples/cpu_flags/SSE_and_MMX_Extensions/mmx.v
@@ -1,5 +1,22 @@
 module main
 
+// MMX Instruction Set
+/*
+Several suffixes are used to indicate what data size the instruction operates on:
+
+Byte (8 bits)
+Word (16 bits)
+Double word (32 bits)
+Quad word (64 bits)
+The signedness of the operation is also signified by the suffix: US for unsigned and S for signed.
+
+For example, PSUBUSB subtracts unsigned bytes, while PSUBSD subtracts signed double words.
+
+MMX defined over 40 new instructions, listed below.
+
+EMMS, MOVD, MOVQ, PACKSSDW, PACKSSWB, PACKUSWB, PADDB, PADDD, PADDSB, PADDSW, PADDUSB, PADDUSW, PADDW, PAND, PANDN, PCMPEQB, PCMPEQD, PCMPEQW, PCMPGTB, PCMPGTD, PCMPGTW, PMADDWD, PMULHW, PMULLW, POR, PSLLD, PSLLQ, PSLLW, PSRAD, PSRAW, PSRLD, PSRLQ, PSRLW, PSUBB, PSUBD, PSUBSB, PSUBSW, PSUBUSB, PSUBUSW, PSUBW, PUNPCKHBW, PUNPCKHDQ, PUNPCKHWD, PUNPCKLBW, PUNPCKLDQ, PUNPCKLWD, PXOR
+*/
+
 fn add_vectors_mmx(a &u8, b &u8, result &u8) {
 	unsafe {
 		asm volatile amd64 {
@@ -27,9 +44,9 @@ fn main() {
 
 	assert result == [u8(9), 9, 9, 9, 9, 9, 9, 9]
 
-	unsafe {
-		asm volatile amd64 {
-			emms // Clear MMX state
-		}
-	}
+	// unsafe {
+	// 	asm volatile amd64 {
+	// 		emms // Clear MMX state
+	// 	}
+	// }
 }
