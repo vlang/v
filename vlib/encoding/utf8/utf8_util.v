74b600be22140a58aabb863b2ed2e7d373e9e150
Felipe Pena <felipensp@gmail.com>
2024-10-25 08:24:55 -0300

    Update vlib/encoding/utf8/utf8_util.v
    Co-authored-by: Delyan Angelov <delian66@gmail.com>

--- vlib/encoding/utf8/utf8_util.v
+++ vlib/encoding/utf8/utf8_util.v
@@ -190,7 +190,7 @@ pub fn is_uchar_punct(uchar int) bool {
 
 // is_rune_punct return true if the input unicode is a western unicode punctuation
 pub fn is_rune_punct(r rune) bool {
-	return find_punct_in_table(r, unicode_punct_western) != -1
+	return find_punct_in_table(r, unicode_punct_western) != rune(-1)
 }
 
 // Global
