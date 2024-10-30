4b9bd735f398d6d537dd7f305bbb3e29fb069dc6
kbkpbot <kbkpbot@sina.com>
2024-10-30 22:19:39 +0800

    Revert "add hamming distance/jaro_similarity/jaro_winkler_similarity"
    This reverts commit e66d5355d760e84cb180129fe6fb88fe15fd8488.

--- vlib/strings/similarity_test.v
+++ vlib/strings/similarity_test.v
@@ -12,29 +12,3 @@ fn test_levenshtein_distance() {
 	assert strings.levenshtein_distance('flomax', 'volmax') == 3
 	assert strings.levenshtein_distance('ab', 'cd') == 2
 }
-
-fn test_hamming_distance() {
-	assert strings.hamming_distance('', '') == 0
-	assert strings.hamming_distance('one', 'one') == 0
-	assert strings.hamming_distance('', 'two') == 3
-	assert strings.hamming_distance('three', '') == 5
-	assert strings.hamming_distance('bananna', '') == 7
-	assert strings.hamming_distance('cats', 'hats') == 1
-	assert strings.hamming_distance('hugs', 'shrugs') == 6
-	assert strings.hamming_distance('broom', 'shroom') == 5
-	assert strings.hamming_distance('flomax', 'volmax') == 3
-	assert strings.hamming_distance('ab', 'cd') == 2
-}
-
-fn test_jaro_similarity() {
-	assert strings.jaro_similarity('MARTHA', 'MARHTA') == 0.9444444444444445
-	assert strings.jaro_similarity('DIXON', 'DICKSONX') == 0.7666666666666666
-	assert strings.jaro_similarity('JELLYFISH', 'SMELLYFISH') == 0.8962962962962964
-}
-
-fn test_jaro_winkler_similarity() {
-	assert strings.jaro_winkler_similarity('accomodate', 'accommodate') == 0.018181818181818188
-	assert strings.jaro_winkler_similarity('accomodate', 'accompanist') == 0.1327272727272727
-	assert strings.jaro_winkler_similarity('untill', 'huntsville') == 0.1333333333333333
-	assert strings.jaro_winkler_similarity('wich', 'wichita') == 0.08571428571428574
-}
