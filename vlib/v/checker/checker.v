3af720921c6dbc9732d7096169b6e19cf19bf4ed
Felipe Pena <felipensp@gmail.com>
2024-10-30 11:56:21 -0300

    checker: fix selector with prefixed `&` structinit (#22689)

--- vlib/v/checker/checker.v
+++ vlib/v/checker/checker.v
@@ -4498,6 +4498,9 @@ fn (mut c Checker) prefix_expr(mut node ast.PrefixExpr) ast.Type {
 		} else if mut node.right is ast.SelectorExpr {
 			if node.right.expr.is_literal() {
 				c.error('cannot take the address of a literal value', node.pos.extend(node.right.pos))
+			} else if node.right.expr is ast.StructInit {
+				c.error('should not create object instance on the heap to simply access a member',
+					node.pos.extend(node.right.pos))
 			}
 			right_sym := c.table.sym(right_type)
 			expr_sym := c.table.sym(node.right.expr_type)
