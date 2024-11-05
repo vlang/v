2f2393419c702d37492e9a07c6f1b3b5390337d6
Felipe Pena <felipensp@gmail.com>
2024-11-05 06:41:53 -0300

    Update vlib/v/tests/range_expr_with_int_test.v
    Co-authored-by: Delyan Angelov <delian66@gmail.com>

--- vlib/v/tests/range_expr_with_int_test.v
+++ vlib/v/tests/range_expr_with_int_test.v
@@ -26,7 +26,7 @@ fn test_main() {
 	assert (t() !in 1..3) == false
 	assert r() !in 5..6
 	assert r() in 4..6
-	assert 1.22 !in z()?..t()
+	assert 1.22 in z()?..t()
 	assert 1.23 !in z()?..t()
 	assert 1.221 in z()?..t()
 
