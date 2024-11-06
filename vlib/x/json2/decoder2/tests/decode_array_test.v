aaeae8c9641258893a4a8894df080d58986b55b6
Hitalo Souza <enghitalo@gmail.com>
2024-11-06 14:30:56 -0400

    x.json2.decoder2: cleanup after #22751 (#22771)

--- vlib/x/json2/decoder2/tests/decode_array_test.v
+++ vlib/x/json2/decoder2/tests/decode_array_test.v
@@ -2,4 +2,26 @@ import x.json2.decoder2 as json
 
 fn test_array_of_strings() {
 	assert json.decode[[]int]('[1, 2, 3]')! == [1, 2, 3]
+
+	assert json.decode[[]string]('["a", "b", "c"]')! == ['a', 'b', 'c']
+
+	assert json.decode[[]bool]('[true, false, true]')! == [true, false, true]
+
+	assert json.decode[[]f64]('[1.1, 2.2, 3.3]')! == [1.1, 2.2, 3.3]
+
+	// nested arrays
+	assert json.decode[[][]int]('[[1, 22], [333, 4444]]')! == [
+		[1, 22],
+		[333, 4444],
+	]
+
+	assert json.decode[[][]string]('[["a", "b"], ["c", "d"]]')! == [
+		['a', 'b'],
+		['c', 'd'],
+	]
+
+	assert json.decode[[][]bool]('[[true, false], [false, true]]')! == [
+		[true, false],
+		[false, true],
+	]
 }
