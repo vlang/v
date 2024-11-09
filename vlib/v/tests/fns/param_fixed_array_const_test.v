0b478fabf6ba22fc1900ea62ff2e9c575dd8c756
Felipe Pena <felipensp@gmail.com>
2024-11-09 07:30:25 -0300

    fix test

--- vlib/v/tests/fns/param_fixed_array_const_test.v
+++ vlib/v/tests/fns/param_fixed_array_const_test.v
@@ -2,20 +2,6 @@ module main
 
 import crypto.ed25519
 
-struct SSHClientHello {
-	version    u8
-	public_key [crypto.ed25519.public_key_size]u8
-}
-
-struct SSHServerHello {
-	version u8
-	send_me [crypto.ed25519.public_key_size]u8
-}
-
-struct SSHClientProof {
-	result [crypto.ed25519.public_key_size]u8
-}
-
 fn new_client_hello(public_key [ed25519.public_key_size]u8) {
 	assert public_key.len == ed25519.public_key_size
 	assert public_key.len != 0
