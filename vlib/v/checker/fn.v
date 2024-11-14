0098405661968b068c3774bb0752e8bb9eee7ba2
Felipe Pena <felipensp@gmail.com>
2024-11-14 16:59:38 -0300

    cleanup

--- vlib/v/checker/fn.v
+++ vlib/v/checker/fn.v
@@ -2137,16 +2137,12 @@ fn (mut c Checker) method_call(mut node ast.CallExpr) ast.Type {
 			}
 		}
 	}
-	if !c.is_builtin_mod && c.mod != 'strings' {
-		c.table.used_features.builtin_types = true
-	}
 	left_type := c.expr(mut node.left)
 	if left_type == ast.void_type {
 		// c.error('cannot call a method using an invalid expression', node.pos)
 		return ast.void_type
 	}
-	if c.pref.skip_unused && !c.is_builtin_mod && left_type != 0 && left_type == ast.string_type
-		&& c.mod != 'strings' {
+	if c.pref.skip_unused && !c.is_builtin_mod && c.mod != 'strings' {
 		c.table.used_features.builtin_types = true
 	}
 	c.expected_type = left_type
