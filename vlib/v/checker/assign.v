abd25142fa442e2e3af288e2fbf1123a162059a1
Felipe Pena <felipensp@gmail.com>
2024-11-15 07:24:43 -0300

    fix

--- vlib/v/checker/assign.v
+++ vlib/v/checker/assign.v
@@ -413,8 +413,7 @@ fn (mut c Checker) assign_stmt(mut node ast.AssignStmt) {
 									if left.obj.ct_type_var in [.generic_var, .no_comptime]
 										&& c.table.cur_fn != unsafe { nil }
 										&& c.table.cur_fn.generic_names.len != 0
-										&& !right.comptime_ret_val
-										&& c.is_generic_expr(right) //&& right.return_type_generic.has_flag(.generic) {
+										&& !right.comptime_ret_val && c.is_generic_expr(right) {
 										// mark variable as generic var because its type changes according to fn return generic resolution type
 										left.obj.ct_type_var = .generic_var
 										if right.return_type_generic.has_flag(.generic) {
