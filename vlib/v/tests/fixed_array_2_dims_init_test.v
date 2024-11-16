5e1530f21920d197a2524bd10f150108ed3d4c4f
Felipe Pena <felipensp@gmail.com>
2024-11-16 19:02:15 -0300

    improve test

--- vlib/v/tests/fixed_array_2_dims_init_test.v
+++ vlib/v/tests/fixed_array_2_dims_init_test.v
@@ -5,14 +5,18 @@ struct Uniforms {
 }
 
 fn r() [4]f32 {
-	return [4]f32{}
+	return [f32(1.1), 1.2, 1.3, 1.4]!
 }
 
 fn test_main() {
-	_ := Uniforms{
+	v := Uniforms{
 		lights: [
 			r(),
 			r(),
 		]!
 	}
+	assert v.lights[0][0] == f32(1.1)
+	assert v.lights[0][1] == f32(1.2)
+	assert v.lights[0][2] == f32(1.3)
+	assert v.lights[0][3] == f32(1.4)
 }
