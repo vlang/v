67d53aaac6d32ace1778fd085f2592f50bb3f7d4
yuyi <yuyi98@163.com>
2024-11-19 22:52:16 +0800

    parser: fix block position's last line (#22913)

--- vlib/v/parser/parser.v
+++ vlib/v/parser/parser.v
@@ -1026,7 +1026,7 @@ fn (mut p Parser) stmt(is_top_level bool) ast.Stmt {
 				}
 			} else {
 				stmts := p.parse_block()
-				pos.last_line = p.prev_tok.line_nr
+				pos.update_last_line(p.prev_tok.line_nr)
 				return ast.Block{
 					stmts: stmts
 					pos:   pos
