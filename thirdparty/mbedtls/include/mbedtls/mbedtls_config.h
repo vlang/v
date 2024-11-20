4b015c8bc0caed28ad79e8f42149d27f74da5529
Delyan Angelov <delian66@gmail.com>
2024-11-20 23:42:01 +0200

    net.mbedtls: define MBEDTLS_THREADING_PTHREAD, in mbedtls_config.h; call C.mbedtls_ssl_conf_read_timeout explicitly in the wrapper, with a shorter timeout value of 317ms (determined experimentally)

--- thirdparty/mbedtls/include/mbedtls/mbedtls_config.h
+++ thirdparty/mbedtls/include/mbedtls/mbedtls_config.h
@@ -1903,7 +1903,7 @@
  *
  * Uncomment this to enable pthread mutexes.
  */
-//#define MBEDTLS_THREADING_PTHREAD
+#define MBEDTLS_THREADING_PTHREAD
 
 /**
  * \def MBEDTLS_USE_PSA_CRYPTO
@@ -3283,7 +3283,7 @@
  *
  * Enable this layer to allow use of mutexes within mbed TLS
  */
-//#define MBEDTLS_THREADING_C
+#define MBEDTLS_THREADING_C
 
 /**
  * \def MBEDTLS_TIMING_C
