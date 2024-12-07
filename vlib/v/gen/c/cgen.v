6fb31b53016f02f075a152343c4309016e0cd9c3
Felipe Pena <felipensp@gmail.com>
2024-12-07 15:32:35 -0300

    fix

--- vlib/v/gen/c/cgen.v
+++ vlib/v/gen/c/cgen.v
@@ -2078,7 +2078,7 @@ fn (mut g Gen) expr_with_tmp_var(expr ast.Expr, expr_typ ast.Type, ret_typ ast.T
 		g.write('${g.styp(ret_typ)} ${tmp_var} = ')
 		g.gen_option_error(ret_typ, expr)
 		g.writeln(';')
-	} else if expr is ast.Ident && expr.name == 'err' {
+	} else if expr is ast.Ident && expr_typ == ast.error_type {
 		g.writeln('${g.styp(ret_typ)} ${tmp_var} = {.state=2, .err = ${expr.name}};')
 	} else {
 		mut simple_assign := false
