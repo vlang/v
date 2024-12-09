2abb3da0a852afaa616d585d010f178ac56c6d40
Hitalo Souza <enghitalo@gmail.com>
2024-12-08 23:19:04 -0400

    fix: http_ok_response

--- examples/vanilla_http_server/src/controllers.v
+++ examples/vanilla_http_server/src/controllers.v
@@ -2,7 +2,7 @@ module main
 
 import strings
 
-const http_ok_response = 'HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nContent-Length: 1\r\nConnection: keep-alive\r\n\r\n1'.bytes()
+const http_ok_response = 'HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nContent-Length: 0\r\nConnection: keep-alive\r\n\r\n'.bytes()
 
 const http_created_response = 'HTTP/1.1 201 Created\r\nContent-Type: application/json\r\nContent-Length: 0\r\nConnection: keep-alive\r\n\r\n'.bytes()
 
