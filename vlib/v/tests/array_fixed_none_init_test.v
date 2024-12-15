aee0683eeecc88ae24572f485cc872a93945310e
Felipe Pena <felipensp@gmail.com>
2024-12-15 18:30:35 -0300

    fix

--- vlib/v/tests/array_fixed_none_init_test.v
+++ vlib/v/tests/array_fixed_none_init_test.v
@@ -1,13 +1,16 @@
 fn test_none_init() {
 	a := ?[3]u8(none)
-	println(a)
 	assert a == none
 
-	b := [3]u8{}
-	println(b)
+	b := ?[3]u8{}
 	assert b == none
 
-	c := [3]u8{}
-	println(c)
+	c := ?[3]u8{}
 	assert c == none
+}
+
+fn test_non_none_init() {
+	c := ?[3]u8{init: 2}
+	assert c? == [u8(2), 2, 2]!
+	assert c != none
 }
