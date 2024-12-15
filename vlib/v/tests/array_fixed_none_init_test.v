f5fd6f4df3531109c055aa5e526f0ae82b042102
Felipe Pena <felipensp@gmail.com>
2024-12-15 18:33:19 -0300

    improve test

--- vlib/v/tests/array_fixed_none_init_test.v
+++ vlib/v/tests/array_fixed_none_init_test.v
@@ -9,8 +9,15 @@ fn test_none_init() {
 	assert c == none
 }
 
+fn foo() u8 {
+	return 123
+}
+
 fn test_non_none_init() {
 	c := ?[3]u8{init: 2}
 	assert c? == [u8(2), 2, 2]!
 	assert c != none
+
+	d := ?[3]u8{init: foo()}
+	assert d? == [u8(123), 123, 123]!
 }
