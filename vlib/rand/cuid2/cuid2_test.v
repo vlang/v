ebb4b58c89fe77f73b7107c4cf348967b895ad4a
kbkpbot <kbkpbot@sina.com>
2024-12-16 22:04:42 +0800

    add successive calls tests

--- vlib/rand/cuid2/cuid2_test.v
+++ vlib/rand/cuid2/cuid2_test.v
@@ -41,4 +41,35 @@ fn test_cuid2() {
 	mt19937_prng := &mt19937.MT19937RNG{}
 	uuid_mt19937 := cuid2(prng: mt19937_prng, length: 32)
 	assert check_valid_cuid2(uuid_mt19937, 32)
+
+	// successive calls
+	uuid_1 := cuid2()
+	uuid_2 := cuid2()
+	uuid_3 := cuid2()
+	uuid_4 := cuid2()
+	uuid_5 := cuid2()
+
+	eprintln(uuid_1)
+	eprintln(uuid_2)
+	eprintln(uuid_3)
+	eprintln(uuid_4)
+	eprintln(uuid_5)
+
+	assert check_valid_cuid2(uuid_1, default_id_length)
+	assert check_valid_cuid2(uuid_2, default_id_length)
+	assert check_valid_cuid2(uuid_3, default_id_length)
+	assert check_valid_cuid2(uuid_4, default_id_length)
+	assert check_valid_cuid2(uuid_5, default_id_length)
+
+	// successive calls to cuid2 in a row should be unique
+	assert uuid_1 != uuid_2
+	assert uuid_1 != uuid_3
+	assert uuid_1 != uuid_4
+	assert uuid_1 != uuid_5
+	assert uuid_2 != uuid_3
+	assert uuid_2 != uuid_4
+	assert uuid_2 != uuid_5
+	assert uuid_3 != uuid_4
+	assert uuid_3 != uuid_5
+	assert uuid_4 != uuid_5
 }
