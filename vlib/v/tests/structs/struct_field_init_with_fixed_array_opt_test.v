9abb53aa1ed067766c1df4e7db6eda3947105990
yuyi <yuyi98@163.com>
2024-12-18 20:52:24 +0800

    fix other cases and add tests

--- vlib/v/tests/structs/struct_field_init_with_fixed_array_opt_test.v
+++ vlib/v/tests/structs/struct_field_init_with_fixed_array_opt_test.v
@@ -6,10 +6,27 @@ struct Foo {
 }
 
 fn test_struct_field_init_with_fixed_array_opt() {
-	f := Foo{
+	f1 := Foo{
 		bar: 1
 		baz: Arr([u8(5), 4, 3, 2]!)
 	}
-	println(f)
-	assert f.baz as Arr == [u8(5), 4, 3, 2]!
+	println(f1)
+	assert f1.baz as Arr == [u8(5), 4, 3, 2]!
+
+	f2 := Foo{
+		bar: 1
+		baz: ?Arr(none)
+	}
+	println(f2)
+	assert f2.bar == 1
+	assert f2.baz == none
+
+	arr := Arr([u8(5), 4, 3, 2]!)
+	f3 := Foo{
+		bar: 1
+		baz: arr
+	}
+	println(f3)
+	assert f3.bar == 1
+	assert f3.baz as Arr == [u8(5), 4, 3, 2]!
 }
