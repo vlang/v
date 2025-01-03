4531896c0d9090b817db5aa2266976a9b48dd720
Delyan Angelov <delian66@gmail.com>
2025-01-03 12:10:06 +0200

    add some state to the message.v example, to make it easier to test

--- examples/hot_reload/message.v
+++ examples/hot_reload/message.v
@@ -4,15 +4,27 @@ module main
 import time
 import v.live
 
+struct App {
+mut:
+	x       int
+	counter int
+}
+
 @[live]
-fn print_message() {
+fn print_message(mut app App) {
 	info := live.info()
 	println('OK reloads: ${info.reloads_ok:4d} | Total reloads: ${info.reloads:4d} | Hello! Modify this message while the program is running.')
+	eprintln('>> app: ${voidptr(app)} | g_live_reload_info: ${voidptr(g_live_reload_info)}')
+	app.x = 9991 // try changing this to another value, while the program is running ...
+	app.counter++
+	dump(app)
 }
 
 fn main() {
+	unbuffer_stdout()
+	mut app := &App{}
 	for {
-		print_message()
+		print_message(mut app)
 		time.sleep(500 * time.millisecond)
 	}
 }
