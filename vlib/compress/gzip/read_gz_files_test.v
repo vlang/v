7040514a61b2021d651ae2af896cef650c37c66f
kbkpbot <kbkpbot@sina.com>
2025-01-05 20:18:53 +0800

    compress.gzip: add compression flags (fix #14994) (#23370)

--- vlib/compress/gzip/read_gz_files_test.v
+++ vlib/compress/gzip/read_gz_files_test.v
@@ -39,3 +39,24 @@ fn test_reading_gzip_files_compressed_with_different_options() {
 	assert content9 == content5
 	assert content5 == content1
 }
+
+fn test_compress_with_deferent_level() {
+	compressed := os.read_bytes(s('readme_level_9.gz'))!
+	content9 := gzip.decompress(compressed)!
+
+	// compression: Huffman only=0
+	compress_0 := gzip.compress(content9, compression_level: 0)!
+	decompress_0 := gzip.decompress(compress_0)!
+
+	// compression: default_max_probes=128
+	compress_128 := gzip.compress(content9)!
+	decompress_128 := gzip.decompress(compress_128)!
+
+	// compression: Huffman+LZ=4095(slowest/best compression)
+	compress_4095 := gzip.compress(content9, compression_level: 4095)!
+	decompress_4095 := gzip.decompress(compress_4095)!
+
+	assert content9 == decompress_0
+	assert content9 == decompress_128
+	assert content9 == decompress_4095
+}
