903a10842b92841c548bd07ef64fed1cb7dc307b
Adam Oates <adamsoates@protonmail.com>
2025-01-05 08:10:58 -0600

    fix error message

--- vlib/v/parser/containers.v
+++ vlib/v/parser/containers.v
@@ -37,7 +37,7 @@ fn (mut p Parser) array_init(is_option bool, alias_array_type ast.Type) ast.Arra
 				// result of expr so we do those in checker
 				if elem_type != 0 {
 					if elem_type.has_flag(.result) {
-						p.error_with_pos('array does not support storing storing Result',
+						p.error_with_pos('arrays do not support storing Result values',
 							elem_type_pos)
 					}
 					idx := p.table.find_or_register_array(elem_type)
