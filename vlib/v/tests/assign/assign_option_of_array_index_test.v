f59e510a162bfa499e4eb01a57d317ed7e06b4ff
yuyi <yuyi98@163.com>
2025-01-13 23:46:08 +0800

    remove println in the test

--- vlib/v/tests/assign/assign_option_of_array_index_test.v
+++ vlib/v/tests/assign/assign_option_of_array_index_test.v
@@ -5,6 +5,5 @@ fn make_option() ?string {
 fn test_assign_option_of_array_index() {
 	arr := [make_option()]
 	unwrapped := arr[99] or { 'unknown' } // <- out of bounds access!
-	println('... ${unwrapped}')
 	assert '${unwrapped}' == "Option('unknown')"
 }
