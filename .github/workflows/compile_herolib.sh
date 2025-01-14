0b7cf912b0c7f9c2e53daae961027d56bf64aa76
Felipe Pena <felipensp@gmail.com>
2025-01-14 16:37:18 -0300

    fix

--- .github/workflows/compile_herolib.sh
+++ .github/workflows/compile_herolib.sh
@@ -18,18 +18,20 @@ show "Checkout last known good commit"
 git checkout fa0dac57ec218c0d65e0e8d5bba45ba2fd28d3d7
 
 v wipe-cache
+show "Build project no parallel (gcc)"
+v -cc gcc -cg -enable-globals -w -n hero.v 
+show "Checking build"
+ls -l ./hero
 
+v wipe-cache
 show "Build project with -parallel-cc (clang)"
 v -cc clang -cg -enable-globals -parallel-cc -w -n hero.v 
-
 show "Checking gcc build"
 ls -l ./hero
 
 v wipe-cache
-
 show "Build project with -parallel-cc (gcc)"
 v -cc gcc -cg -enable-globals -parallel-cc -w -n hero.v 
-
 show "Checking clang build"
 ls -l ./hero
 
