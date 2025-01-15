17fa599aac35c337608213f954062e4ec95c6534
Felipe Pena <felipensp@gmail.com>
2025-01-15 08:53:06 -0300

    fix

--- vlib/v/tests/generics/generic_alias_map_type_test.v
+++ vlib/v/tests/generics/generic_alias_map_type_test.v
@@ -15,57 +15,52 @@ pub mut:
 
 @[params]
 struct TreeCloneParams {
-	kids	[]&Tree
+	kids []&Tree
 }
+
 // Makes new derived node with different kids id defined.
-pub fn (tree Tree) clone (p TreeCloneParams) &Tree {
+pub fn (tree Tree) clone(p TreeCloneParams) &Tree {
 	return &Tree{
-		type: tree.type
+		type:  tree.type
 		value: tree.value
-		kids: p.kids
+		kids:  p.kids
 	}
 }
 
-/** Collection of hask tools for processing tree. */
+// Collection of hask tools for processing tree.
 pub type TreeBelt = map[string]fn (input &Tree, belt TreeBelt) []&Tree
 
-/** Hask tool for processing node. */
+// Hask tool for processing node.
 pub type TreeHack = fn (input &Tree, belt TreeBelt) []&Tree
 
-pub type TreeContext = map[string]string /* вывести тип! */
+pub type TreeContext = map[string]string
 
 @[params]
 struct TreeHackParams {
-	belt	TreeBelt
-	context	?TreeContext
+	belt    TreeBelt
+	context ?TreeContext
 }
 
 // Transform tree through context with transformers
-pub fn (tree Tree) hack(
-	p TreeHackParams
-) []&Tree {
-	return arrays.concat(
-		arrays.flatten( tree.kids.map( it.hack_self(p) ))
-	)
+pub fn (tree Tree) hack(p TreeHackParams) []&Tree {
+	return arrays.concat(arrays.flatten(tree.kids.map(it.hack_self(p))))
 }
-pub fn (tree Tree) hack_self(
-	p TreeHackParams
-) []&Tree {
 
-	mut handle :=  fn [tree](input &Tree, belt TreeBelt) []&Tree {
+pub fn (tree Tree) hack_self(p TreeHackParams) []&Tree {
+	mut handle := fn [tree] (input &Tree, belt TreeBelt) []&Tree {
 		return [
-			input.clone( kids: input.hack( belt: belt ) )
+			input.clone(kids: input.hack(belt: belt)),
 		]
 	}
 
-	if action := p.belt[ tree.type ] {
+	if action := p.belt[tree.type] {
 		handle = action
 	}
 
 	return handle(tree, p.belt)
 }
 
-fn test_main(){
+fn test_main() {
 	t := Tree{}
 	r := t.hack_self(belt: TreeBelt(map[string]fn (&Tree, TreeBelt) []&Tree{}))
 	assert r[0].type == ''
