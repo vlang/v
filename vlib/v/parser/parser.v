f9d3bd39a6287ce690d95b495403c8ca6cb9fdb8
Alexander Medvednikov <alexander@medvednikov.com>
2025-01-19 23:12:44 +0300

    parser: make old [attr] syntax an error

--- vlib/v/parser/parser.v
+++ vlib/v/parser/parser.v
@@ -1937,12 +1937,15 @@ fn (mut p Parser) is_attributes() bool {
 	return true
 }
 
-// when is_top_stmt is true attrs are added to p.attrs
+// when is_top_stmt is true, attrs are added to p.attrs
 fn (mut p Parser) attributes() {
 	start_pos := p.tok.pos()
 	mut is_at := false
 	if p.tok.kind == .lsbr {
-		p.warn('`[attr]` has been deprecated, use `@[attr]` instead')
+		if p.pref.is_fmt {
+		} else {
+			p.error('`[attr]` has been deprecated, use `@[attr]` instead')
+		}
 		// [attr]
 		p.check(.lsbr)
 	} else if p.tok.kind == .at {
