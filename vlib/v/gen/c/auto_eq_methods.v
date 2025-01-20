6d40bb8374f938d13455a7335cdf5c5941ad22b6
yuyi <yuyi98@163.com>
2025-01-20 16:33:35 +0800

    cgen: fix interface casting in anon fn (fix #23530) (#23533)

--- vlib/v/gen/c/auto_eq_methods.v
+++ vlib/v/gen/c/auto_eq_methods.v
@@ -605,7 +605,6 @@ fn (mut g Gen) gen_interface_equality_fn(left_type ast.Type) string {
 	left_arg := g.read_field(left_type, '_typ', 'a')
 	right_arg := g.read_field(left_type, '_typ', 'b')
 
-	fn_builder.writeln('${g.static_non_parallel}int v_typeof_interface_idx_${idx_fn}(int sidx); // for auto eq method')
 	fn_builder.writeln('${g.static_non_parallel}inline bool ${fn_name}_interface_eq(${ptr_styp} a, ${ptr_styp} b) {')
 	fn_builder.writeln('\tif (${left_arg} == ${right_arg}) {')
 	fn_builder.writeln('\t\tint idx = v_typeof_interface_idx_${idx_fn}(${left_arg});')
