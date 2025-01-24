772d21034c256be85444be876ad6961911774bd8
Felipe Pena <felipensp@gmail.com>
2025-01-24 03:49:01 -0300

    cgen: fix codegen for assigning from infixexpr with generic operand (fix #23560) (#23561)

--- vlib/v/gen/c/assign.v
+++ vlib/v/gen/c/assign.v
@@ -376,7 +376,9 @@ fn (mut g Gen) assign_stmt(node_ ast.AssignStmt) {
 						}
 					} else if val is ast.InfixExpr && val.op in [.plus, .minus, .mul, .div, .mod]
 						&& val.left_ct_expr {
-						ctyp := g.unwrap_generic(g.type_resolver.get_type(val.left))
+						ctyp := g.type_resolver.promote_type(g.unwrap_generic(g.type_resolver.get_type(val.left)),
+							g.unwrap_generic(g.type_resolver.get_type_or_default(val.right,
+							val.right_type)))
 						if ctyp != ast.void_type {
 							ct_type_var := g.comptime.get_ct_type_var(val.left)
 							if ct_type_var in [.key_var, .value_var] {
