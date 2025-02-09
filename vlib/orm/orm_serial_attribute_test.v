dd063faf6592df9479b6609c3d7b471f40357eb1
Alexander Mandrikov <mandrikov@pm.me>
2025-02-09 15:54:45 +0700

    orm,cgen: fix `@[sql: serial]` and `@[serial]` are not the same (fix #23346) (#23678)

--- vlib/orm/orm_serial_attribute_test.v
+++ vlib/orm/orm_serial_attribute_test.v
@@ -1,8 +1,14 @@
 import db.sqlite
 
+struct Child {
+	id        int @[primary; serial]
+	parent_id int
+}
+
 struct PlainNoArg {
-	id   int @[primary; serial; sql: 'custom_id']
-	name string
+	id       int @[primary; serial; sql: 'custom_id']
+	name     string
+	children []Child @[fkey: 'parent_id']
 }
 
 fn test_plain_no_arg() {
@@ -31,6 +37,26 @@ fn test_plain_no_arg() {
 
 	assert rows[0].id == 2
 	assert rows[0].name == 'second'
+}
+
+fn test_plain_no_arg_with_children() {
+	mut db := sqlite.connect(':memory:')!
+
+	parent := PlainNoArg{
+		children: [Child{}, Child{}]
+	}
+
+	sql db {
+		create table Child
+		create table PlainNoArg
+		insert parent into PlainNoArg
+	}!
+
+	parents := sql db {
+		select from PlainNoArg limit 1
+	}!
+
+	assert parent.children.len == parents[0].children.len
 }
 
 struct SqlSerial {
