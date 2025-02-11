fa5107eb02bd45c1bd8fea366ce330f28fc4af45
Delyan Angelov <delian66@gmail.com>
2025-02-11 14:02:44 +0200

    extract .github/workflows/freebsd_ci.sh, cleanup

---
+++ .github/workflows/freebsd_ci.sh
@@ -0,0 +1,41 @@
+#!/usr/local/bin/bash
+
+set -x ## print all commands as they are executed
+
+echo $PATH
+
+echo '----------------------------------------------------------'
+freebsd-version
+sysctl hw.model
+sysctl hw.ncpu
+sysctl hw.physmem
+sysctl hw.usermem
+echo '----------------------------------------------------------'
+whoami
+pwd
+ls -lah
+echo '----------------------------------------------------------'
+git config --global --add safe.directory /home/runner/work/v/v
+git log -n1
+echo '----------------------------------------------------------'
+cc --version
+echo '----------------------------------------------------------'
+echo 'Building local V'
+make
+./v symlink
+echo '----------------------------------------------------------'
+echo 'Build cmd/tools/fast'
+(cd cmd/tools/fast && v fast.v) ## && ./fast -clang
+echo '----------------------------------------------------------'
+echo 'Test the math module'
+v test vlib/math
+echo '----------------------------------------------------------'
+echo 'Test the math module, using only the pure V versions,'
+echo '                      without the .c.v overrides'
+v -exclude @vlib/math/*.c.v test vlib/math
+echo '----------------------------------------------------------'
+echo 'Test modules using thirdparty/zip'
+v test vlib/compress/
+echo '----------------------------------------------------------'
+echo 'Run test-self'
+VTEST_JUST_ESSENTIAL=1 ./v test-self
