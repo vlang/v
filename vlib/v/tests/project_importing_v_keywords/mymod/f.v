fa11ee9d6bb24b30b2b0bb2be8f0a55a8078f504
Delyan Angelov <delian66@gmail.com>
2025-02-12 10:36:27 +0200

    tests: add more tests for importing @keyword as function names, and for enums with c++ keyword field names

---
+++ vlib/v/tests/project_importing_v_keywords/mymod/f.v
@@ -0,0 +1,173 @@
+module mymod
+
+pub fn @as() {
+	println(@LOCATION)
+}
+
+pub fn @asm() {
+	println(@LOCATION)
+}
+
+pub fn @assert() {
+	println(@LOCATION)
+}
+
+pub fn @atomic() {
+	println(@LOCATION)
+}
+
+pub fn @break() {
+	println(@LOCATION)
+}
+
+pub fn @const() {
+	println(@LOCATION)
+}
+
+pub fn @continue() {
+	println(@LOCATION)
+}
+
+pub fn @defer() {
+	println(@LOCATION)
+}
+
+pub fn @else() {
+	println(@LOCATION)
+}
+
+pub fn @enum() {
+	println(@LOCATION)
+}
+
+pub fn @false() {
+	println(@LOCATION)
+}
+
+pub fn @fn() {
+	println(@LOCATION)
+}
+
+pub fn @for() {
+	println(@LOCATION)
+}
+
+pub fn @go() {
+	println(@LOCATION)
+}
+
+pub fn @goto() {
+	println(@LOCATION)
+}
+
+pub fn @if() {
+	println(@LOCATION)
+}
+
+pub fn @implements() {
+	println(@LOCATION)
+}
+
+pub fn @import() {
+	println(@LOCATION)
+}
+
+pub fn @in() {
+	println(@LOCATION)
+}
+
+pub fn @interface() {
+	println(@LOCATION)
+}
+
+pub fn @is() {
+	println(@LOCATION)
+}
+
+pub fn @isreftype() {
+	println(@LOCATION)
+}
+
+pub fn @lock() {
+	println(@LOCATION)
+}
+
+pub fn @match() {
+	println(@LOCATION)
+}
+
+pub fn @module() {
+	println(@LOCATION)
+}
+
+pub fn @mut() {
+	println(@LOCATION)
+}
+
+pub fn @none() {
+	println(@LOCATION)
+}
+
+pub fn @or() {
+	println(@LOCATION)
+}
+
+pub fn @pub() {
+	println(@LOCATION)
+}
+
+pub fn @return() {
+	println(@LOCATION)
+}
+
+pub fn @rlock() {
+	println(@LOCATION)
+}
+
+pub fn @select() {
+	println(@LOCATION)
+}
+
+pub fn @shared() {
+	println(@LOCATION)
+}
+
+pub fn @sizeof() {
+	println(@LOCATION)
+}
+
+pub fn @spawn() {
+	println(@LOCATION)
+}
+
+pub fn @static() {
+	println(@LOCATION)
+}
+
+pub fn @struct() {
+	println(@LOCATION)
+}
+
+pub fn @true() {
+	println(@LOCATION)
+}
+
+pub fn @type() {
+	println(@LOCATION)
+}
+
+pub fn @typeof() {
+	println(@LOCATION)
+}
+
+pub fn @union() {
+	println(@LOCATION)
+}
+
+pub fn @unsafe() {
+	println(@LOCATION)
+}
+
+pub fn @volatile() {
+	println(@LOCATION)
+}
