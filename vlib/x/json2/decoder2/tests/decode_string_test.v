f4789364880012d9e2c1e8771f1ac2a8d0f90be9
Hitalo Souza <enghitalo@gmail.com>
2025-02-25 15:22:33 -0400

    decoder2: minor test string

--- vlib/x/json2/decoder2/tests/decode_string_test.v
+++ vlib/x/json2/decoder2/tests/decode_string_test.v
@@ -9,7 +9,8 @@ fn test_json_escape_low_chars() {
 
 fn test_json_string() {
 	assert json.decode[string](r'"te\u2714st"')! == 'te✔st'
-	// assert json.decode[string]('te✔st')! == 'te✔st'
+	assert json.decode[string](r'"te✔st"')! == 'te✔st'
+	assert json.decode[string]('""')! == ''
 }
 
 fn test_json_string_emoji() {
@@ -27,4 +28,29 @@ fn test_json_string_non_ascii() {
 fn test_utf8_strings_are_not_modified() {
 	assert json.decode[string]('"ü"')! == 'ü'
 	assert json.decode[string]('"Schilddrüsenerkrankungen"')! == 'Schilddrüsenerkrankungen'
+}
+
+fn test_json_string_invalid_escapes() {
+	mut has_error := false
+
+	json.decode[string](r'"\x"') or {
+		assert err.msg() == '\n"\\\n ^ unknown escape sequence'
+		has_error = true
+	} // Invalid escape
+
+	assert has_error, 'Expected error'
+	has_error = false
+
+	json.decode[string](r'"\u123"') or {
+		assert err.msg() == '\n"\\\n ^ short unicode escape sequence \\u123"'
+		has_error = true
+	} // Incomplete Unicode
+
+	assert has_error, 'Expected error'
+}
+
+fn test_json_string_whitespace() {
+	// Test strings with whitespace
+	assert json.decode[string]('"   "')! == '   '
+	assert json.decode[string]('"\t\n\r"')! == '\t\n\r'
 }
