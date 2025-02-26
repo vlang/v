a1f7213de0bf77a1b5f7c376e80d647fdc1cea5f
Delyan Angelov <delian66@gmail.com>
2025-02-26 11:51:33 +0200

    sokol: allow for `v -os wasm32_emscripten -o examples/tetris/tetris.html examples/tetris/` (avoid using `-sMODULARIZE`) (#23814)

--- vlib/sokol/c/declaration.c.v
+++ vlib/sokol/c/declaration.c.v
@@ -41,9 +41,17 @@ $if emscripten ? {
 	#flag -DSOKOL_GLES3
 	#flag -DSOKOL_NO_ENTRY
 	#flag -s ERROR_ON_UNDEFINED_SYMBOLS=0
-	#flag -s ASSERTIONS=1
-	#flag -s MODULARIZE
 	#flag -s USE_WEBGL2
+	$if !prod {
+		#flag -s ASSERTIONS=1
+	}
+	$if prod {
+		#flag -s ASSERTIONS=0
+	}
+	// See https://emscripten.org/docs/tools_reference/settings_reference.html#modularize
+	// Note that it makes it impossible to use `v -os wasm32_emscripten -o file.html program.v` , due to:
+	// https://github.com/emscripten-core/emscripten/issues/7950
+	//	#flag -s MODULARIZE
 }
 
 // OPENGL
