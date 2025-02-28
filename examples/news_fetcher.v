8b89f94fe46798a0a48071ca5d3426e8cd6b8121
Hitalo Souza <enghitalo@gmail.com>
2025-02-27 21:05:02 -0400

    Update news_fetcher.v

--- examples/news_fetcher.v
+++ examples/news_fetcher.v
@@ -37,6 +37,10 @@ fn main() {
 		return
 	}#[0..10]
 
+ mut fetcher_pool := pool.new_pool_processor( 
+  callback: worker_fetch 
+ )
+
 	// Note: if you do not call set_max_jobs, the pool will try to use an optimal
 	// number of threads, one per each core in your system, which in most
 	// cases is what you want anyway... You can override the automatic choice
