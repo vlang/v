838ead69ade86eb840e488ab7d4ab1f6121d9868
lmp <dev.larpon@gmail.com>
2025-03-09 12:31:28 +0100

    docs, cmd: add `assert` to prevent regressions specific for `vlib`

--- cmd/tools/vdoc/vdoc.v
+++ cmd/tools/vdoc/vdoc.v
@@ -313,6 +313,8 @@ fn (mut vd VDoc) generate_docs_from_file() {
 		if manifest := vmod.from_file(manifest_path) {
 			vd.manifest = manifest
 		}
+	} else if cfg.is_vlib {
+		assert false, 'vdoc: manifest does not exists for vlib'
 	}
 	if cfg.include_readme || cfg.is_vlib {
 		mut readme_name := 'README'
