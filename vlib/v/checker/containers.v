5d5e2a93bf941d2da379c050fd8e9afe9cfc5882
Felipe Pena <felipensp@gmail.com>
2025-03-12 08:00:11 -0300

    fix

--- vlib/v/checker/containers.v
+++ vlib/v/checker/containers.v
@@ -110,7 +110,8 @@ fn (mut c Checker) array_init(mut node ast.ArrayInit) ast.Type {
 		if node.has_len {
 			c.check_elements_ref_fields_initialized(unwrap_elem_type, node.pos)
 		}
-		if node.expr_types.len == 0 {
+		// T{0} initialization when T is an array
+		if !node.is_fixed && node.expr_types.len == 0 {
 			for mut expr in node.exprs {
 				typ := c.expr(mut expr)
 				c.check_expected(typ, node.elem_type) or {
