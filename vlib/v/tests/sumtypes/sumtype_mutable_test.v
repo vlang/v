d91dac466295d3bcd07c0a641d5350b1e17b08a3
Felipe Pena <felipensp@gmail.com>
2025-03-23 14:36:32 -0300

    fix

--- vlib/v/tests/sumtypes/sumtype_mutable_test.v
+++ vlib/v/tests/sumtypes/sumtype_mutable_test.v
@@ -14,11 +14,13 @@ fn test_main() {
 	mut my_struct := &MyStructA{
 		test: false
 	}
-	assert my_struct.test == false
-	my_struct.test = true
-	assert my_struct.test == true
-	but_why(mut my_struct)
-	assert my_struct.test == false
+	if $d('mutable_sumtype', false) {
+		assert my_struct.test == false
+		my_struct.test = true
+		assert my_struct.test == true
+		but_why(mut my_struct)
+		assert my_struct.test == false
+	}
 }
 
 fn but_why(mut passed MySumType) {
