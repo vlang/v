aa3a06222c44556f6f926d4b94eb8152abf56ee2
Felipe Pena <felipensp@gmail.com>
2025-03-29 10:27:32 -0300

    fix test

--- vlib/v/tests/in_op_array_fixed_test.v
+++ vlib/v/tests/in_op_array_fixed_test.v
@@ -1,9 +1,9 @@
 fn test_main() {
 	arr := [u8(1), 2, 3, 4]!
-	if arr != [4]u8{} || arr != [4]u8{init: 255} { // OK
+	if arr != [4]u8{} || arr != [4]u8{init: 255} {
 		println('success')
 	}
-	if arr !in [[4]u8{}, [4]u8{init: 255}] { // Compilation failed with C error
+	if arr !in [[4]u8{}, [4]u8{init: 255}] {
 		println('success')
 	}
 }
