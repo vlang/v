1fe8d083e8980aea30a017ef23d11b4b5d7b11a7
Felipe Pena <felipensp@gmail.com>
2025-03-29 08:49:54 -0300

    fix + test

---
+++ vlib/v/tests/options/option_array_push_test.v
@@ -0,0 +1,22 @@
+pub struct Struct1 {
+pub mut:
+	strings ?[]string
+}
+
+fn test_main() {
+	mut s1 := Struct1{}
+	s1.strings = []
+	s1.strings? << ['foo']
+	s1.strings? << ['bar']
+
+	dump(s1)
+	assert s1.strings? == ['foo', 'bar']
+
+	assert s1.strings? == ['foo', 'bar']
+
+	s1.strings = []
+	assert s1.strings? == []
+
+	s1.strings? << ['foo']
+	assert s1.strings? == ['foo']
+}
