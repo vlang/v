5fe9e7994a7b85ef1dab11137dc05879538483f7
kbkpbot <kbkpbot@sina.com>
2025-03-29 12:59:48 +0800

    add more tests

--- vlib/v/depgraph/depgraph_test.v
+++ vlib/v/depgraph/depgraph_test.v
@@ -1,6 +1,6 @@
 module depgraph
 
-fn test_resolve_has_deps() {
+fn test_resolve_has_deps_no_cyclic() {
 	mut g := new_dep_graph()
 	g.add('A', [])
 	g.add('B', [])
@@ -11,18 +11,49 @@ fn test_resolve_has_deps() {
 	g.add('F', [])
 	g.add('G', ['A'])
 	g.add('H', [])
+	g.add('Y', [])
 	resolved_graph := g.resolve()
+	assert resolved_graph.acyclic
 	assert resolved_graph.display() == ' * A
  * B
  * C
  * E
  * F
  * H
+ * Y
  * G -> A
  * X -> Y
  * X -> G
  * D -> A
  * D -> X'
+}
+
+fn test_resolve_has_deps_cyclic() {
+	mut g := new_dep_graph()
+	g.add('A', [])
+	g.add('B', [])
+	g.add('X', ['Y', 'G'])
+	g.add('C', [])
+	g.add('D', ['A', 'X'])
+	g.add('E', [])
+	g.add('F', [])
+	g.add('G', ['D'])
+	g.add('H', [])
+	g.add('Y', [])
+	resolved_graph := g.resolve()
+	assert !resolved_graph.acyclic
+	assert resolved_graph.display() == ' * A
+ * B
+ * C
+ * E
+ * F
+ * H
+ * Y
+ * X -> Y
+ * X -> G
+ * D -> A
+ * D -> X
+ * G -> D'
 }
 
 fn test_resolve_no_deps() {
@@ -33,6 +64,7 @@ fn test_resolve_no_deps() {
 	g.add('C', [])
 	g.add('D', [])
 	resolved_graph := g.resolve()
+	assert resolved_graph.acyclic
 	assert resolved_graph.display() == ' * X
  * A
  * B
