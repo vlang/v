83f3960ea544c8b3bf775a547c419df373337fef
kbkpbot <kbkpbot@sina.com>
2025-04-01 16:03:45 +0800

    fix test name

--- vlib/v/tests/generics/generics_return_result_maps_test.v
+++ vlib/v/tests/generics/generics_return_result_maps_test.v
@@ -6,9 +6,9 @@ fn decode[T]() !T {
 	return decode_map(T{})!
 }
 
-fn test_main() ! {
-	x_str_str := decode[map[string]string]()!
-	x_str_int := decode[map[string]int]()!
+fn test_main() {
+	x_str_str := decode[map[string]string]() or { panic(err) }
+	x_str_int := decode[map[string]int]() or { panic(err) }
 
 	assert typeof(x_str_str).name == 'map[string]string'
 	assert typeof(x_str_int).name == 'map[string]int'
