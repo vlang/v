8f16413799073e7c5c28308755056e5967924c9c
Felipe Pena <felipensp@gmail.com>
2025-04-07 08:05:02 -0300

    fix

--- vlib/orm/orm_generic_test.v
+++ vlib/orm/orm_generic_test.v
@@ -1,106 +1,47 @@
-module main
-
-import time
 import db.sqlite
 
 pub enum MessageStatus as u8 {
 	ready
-	locked
-	done
-	failed
 }
 
-// Struct Message
 pub struct Message[T] {
-	data       string
 	message_id int @[primary; sql: serial]
 	status     MessageStatus
-	in_time    time.Time @[default: 'CURRENT_TIME']
-	lock_time  ?time.Time
-	done_time  ?time.Time
-	payload    T
 }
 
-// Struct Queue
 pub struct Queue[T] {
-pub mut:
-	conn    &sqlite.DB
-	handler ?fn (message Message[T]) !
+	conn &sqlite.DB
 }
 
-fn C.sqlite3_config(int)
-
-// Function NewQueue (Constructor)
 pub struct Queue_config {
-pub:
-	path             string
-	db               ?sqlite.DB @[omitempty]
-	pooling_interval i64 = time.second
+	path string
+	db   ?sqlite.DB @[omitempty]
 }
 
 pub fn new[T](config Queue_config) !Queue[T] {
-	C.sqlite3_config(3)
-
-	// use exist db connection or create file self
 	mut conn := if db := config.db {
 		db
 	} else {
 		sqlite.connect(config.path) or { return error('Failed to connect to database: ${err}') }
 	}
-	conn.busy_timeout(10 * 60_000)
-
 	mut queue := Queue[T]{
 		conn: &conn
 	}
-
-	sql queue.conn {
-		create table Message[T]
-	}!
-
 	return queue
 }
 
-// Method put
-pub fn (mut self Queue[T]) add(payload T) !Message[T] {
-	message := Message[T]{
-		payload: payload
-		status:  MessageStatus.ready
-		in_time: time.now()
-	}
-	dump(message)
-
-	sql self.conn {
-		insert message into Message[T]
-	} or { return err }
+pub fn (mut self Queue[T]) take() !Message[T] {
+	messages := sql self.conn {
+		select from Message[T] where status == MessageStatus.ready order by message_id limit 1
+	} or { return error('') }
+	message := messages.first()
 	return message
 }
 
-// Method pop_transaction
-pub fn (mut self Queue[T]) take() ?[]Message[T] {
-	messages := sql self.conn {
-		select from Message[T] where status == MessageStatus.ready order by message_id limit 1
-	} or { return none }
-
-	if messages.len == 0 {
-		return none
-	}
-
-	return messages
-}
-
-// usage
-struct Payload {
-	some_filed_1 string
-	some_filed_2 int
-}
-
-fn test_handler(message Message[Payload]) ! {
-	return error('AAAA')
-}
+struct Payload {}
 
 fn test_main() {
-	db_path := ':memory:'
 	_ := new[Payload](
-		path: db_path
+		path: ':memory:'
 	) or { panic('Failed to create queue: ${err}') }
 }
