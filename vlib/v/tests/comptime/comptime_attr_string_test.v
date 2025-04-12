5b111a249a352a44cdc14d952a2451d752aecba9
Felipe Pena <felipensp@gmail.com>
2025-04-12 09:08:49 -0300

    more cases

--- vlib/v/tests/comptime/comptime_attr_string_test.v
+++ vlib/v/tests/comptime/comptime_attr_string_test.v
@@ -1,9 +1,10 @@
 module main
 
 struct MyStruct {
-	id1 string @[sql: 'id']
+	id1 string @[sql: 'id\"']
 	id2 string @[sql: 'id']
 	id3 string @[sql: id]
+	id4 string @[sql: 'id"']
 }
 
 fn test_main() {
@@ -11,7 +12,8 @@ fn test_main() {
 	$for f in MyStruct.fields {
 		arr << f.attrs[0]
 	}
-	assert arr[0] == 'sql: "id"'
+	assert arr[0] == 'sql: "id""'
 	assert arr[1] == 'sql: "id"'
 	assert arr[2] == 'sql: id'
+	assert arr[3] == 'sql: "id""'
 }
