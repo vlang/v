4e046f364a2c883b73142c48aa30639143048bad
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2025-04-17 23:38:18 +0100

    js: fix array type checking in sum type match expressions (fix #24237 )
    This commit fixes a bug in the JavaScript code generator where array types in sum type match expressions were not being properly checked. The issue was that the generated code was checking if array elements were instances of the array type itself, rather than the element type of the array.

--- vlib/v/gen/js/js.v
+++ vlib/v/gen/js/js.v
@@ -561,7 +561,7 @@ fn (mut g JsGen) js_name(name_ string) string {
 		name = name[3..]
 		return name
 	}
-	name = name_.replace('.', '__')
+	name = name_.replace('[]', '').replace('.', '__')
 	if name in js_reserved {
 		return '_v_${name}'
 	}
@@ -2511,6 +2511,12 @@ fn (mut g JsGen) match_expr_sumtype(node ast.MatchExpr, is_expr bool, cond_var M
 						if tsym.language == .js && (tsym.name == 'JS.Number'
 							|| tsym.name == 'JS.Boolean' || tsym.name == 'JS.String') {
 							g.write(' === "${tsym.name[3..].to_lower_ascii()}"')
+						} else if tsym.kind == .array {
+							g.write(' && ')
+							g.match_cond(cond_var)
+							g.write('.arr.arr.every(x => x instanceof ')
+							g.expr(branch.exprs[sumtype_index])
+							g.write(')')
 						} else {
 							g.write(' instanceof ')
 							g.expr(branch.exprs[sumtype_index])
