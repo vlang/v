433a8ae2b940d12b926520593730447c803695b5
Felipe Pena <felipensp@gmail.com>
2025-04-20 17:25:30 -0300

    fix

--- vlib/v/checker/fn.v
+++ vlib/v/checker/fn.v
@@ -280,7 +280,8 @@ fn (mut c Checker) fn_decl(mut node ast.FnDecl) {
 				c.error('result type arguments are not supported', param.type_pos)
 			}
 			arg_typ_sym := c.table.sym(param.typ)
-			if node.language == .v && param.typ == ast.any_type {
+			if c.file.mod.name != 'builtin' && arg_typ_sym.language == .v
+				&& param.typ == ast.any_type {
 				c.note('the `any` type is deprecated and will be removed soon - either use an empty interface, or a sum type',
 					param.pos)
 			}
