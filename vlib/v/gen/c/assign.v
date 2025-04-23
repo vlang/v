33defb3bbc383ea0dc5bd5f5548acf84210f6021
Felipe Pena <felipensp@gmail.com>
2025-04-23 08:47:37 -0300

    fix

--- vlib/v/gen/c/assign.v
+++ vlib/v/gen/c/assign.v
@@ -938,15 +938,16 @@ fn (mut g Gen) assign_stmt(node_ ast.AssignStmt) {
 							|| gen_or {
 							g.expr_with_opt_or_block(val, val_type, left, var_type, is_option_auto_heap)
 						} else if val is ast.ArrayInit {
+							cvar_name := c_name(ident.name)
 							if val.is_fixed && ident.name in g.defer_vars {
 								g.go_before_last_stmt()
 								g.empty_line = true
-								g.write('memcpy(${c_name(ident.name)}, ')
+								g.write('memcpy(${cvar_name}, ')
 								g.write('(${styp})')
-								g.array_init(val, c_name(ident.name))
+								g.array_init(val, cvar_name)
 								g.write(', sizeof(${styp}))')
 							} else {
-								g.array_init(val, c_name(ident.name))
+								g.array_init(val, cvar_name)
 							}
 						} else if val_type.has_flag(.shared_f) {
 							g.expr_with_cast(val, val_type, var_type)
