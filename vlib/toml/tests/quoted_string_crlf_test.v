a50f655f5acf3bb476163c4e723a1bd7d25b95e7
kbkpbot <kbkpbot@sina.com>
2025-04-26 21:32:52 +0800

    fix test

--- vlib/toml/tests/quoted_string_crlf_test.v
+++ vlib/toml/tests/quoted_string_crlf_test.v
@@ -4,7 +4,6 @@ fn test_quoted_string_crlf() {
 	toml_txt := 'str1 = """tcc \\\r\nabc \\\r\n123"""'
 	toml_doc := toml.parse_text(toml_txt) or { panic(err) }
 
-	dump(toml_doc)
 	value := toml_doc.value('str1').string()
 	assert value == 'tcc abc 123'
 }
