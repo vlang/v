83f6b3e66668f84aa484d7d86ae67aa34ef05aad
Swastik <swstkbaranwal@gmail.com>
2025-05-06 00:30:39 +0530

    checker: check invalid comptime field name assignment

--- vlib/v/checker/assign.v
+++ vlib/v/checker/assign.v
@@ -876,10 +876,17 @@ or use an explicit `unsafe{ a[..] }`, if you do not want a copy of the slice.',
 							node.pos)
 					}
 				} else {
-					// allow `t.$(field.name) = 0` where `t.$(field.name)` is a enum
 					if c.comptime.comptime_for_field_var != '' && left is ast.ComptimeSelector {
-						field_sym := c.table.sym(c.unwrap_generic(c.comptime.comptime_for_field_type))
+						field_type := c.unwrap_generic(c.comptime.comptime_for_field_type)
+						field_sym := c.table.sym(field_type)
 
+						// disallow invalid `t.$(field.name)` type assignment
+						if !c.check_types(field_type, right_type) {
+							c.error('cannot assign to `${left}`: ${c.expected_msg(right_type,
+								field_type)}', right.pos())
+						}
+
+						// allow `t.$(field.name) = 0` where `t.$(field.name)` is a enum
 						if field_sym.kind == .enum && !right_type.is_int() {
 							c.error('enums can only be assigned `int` values', right.pos())
 						}
