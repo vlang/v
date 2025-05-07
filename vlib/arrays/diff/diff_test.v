646b3270658fbf78eb120045a9a3f749e984503c
kbkpbot <kbkpbot@sina.com>
2025-05-07 20:51:00 +0800

    fix example

--- vlib/arrays/diff/diff_test.v
+++ vlib/arrays/diff/diff_test.v
@@ -85,7 +85,7 @@ hi
 
 fn test_diff_runes() {
 	mut aa := 'brown fox jumps over the lazy dog'.runes()
-	mut bb := 'brwn faax junps ovver the lay dag'.runes()
+	mut bb := 'brwn faax junps ovver the lay daog'.runes()
 	mut ctx := diff.diff(aa, bb)
 
 	str1 := ctx.generate_patch()
@@ -120,11 +120,11 @@ a
 -y
 - 
 -d
--o
 +y
 + 
 +d
 +a
+o
 g
 '
 }
