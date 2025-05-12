e93c344b56cbeb56d4cba558a2cf44f6d1580d1f
Felipe Pena <felipensp@gmail.com>
2025-05-12 07:00:03 -0300

    checker: fix generic option array arg passing to `[]T` (fix #24423) (#24457)

--- vlib/v/checker/fn.v
+++ vlib/v/checker/fn.v
@@ -1852,6 +1852,12 @@ fn (mut c Checker) fn_call(mut node ast.CallExpr, mut continue_check &bool) ast.
 							c.handle_generic_lambda_arg(node, mut call_arg.expr)
 							continue
 						}
+						// passing []?T to []T
+						if !unwrap_typ.has_flag(.variadic) && unwrap_sym.kind == .array
+							&& c.table.final_sym(utyp).kind == .array
+							&& c.check_basic(c.table.value_type(utyp).clear_flag(.option), c.table.value_type(unwrap_typ)) {
+							continue
+						}
 						c.error('${err.msg()} in argument ${i + 1} to `${fn_name}`', call_arg.pos)
 					}
 				}
@@ -2606,6 +2612,12 @@ fn (mut c Checker) method_call(mut node ast.CallExpr, mut continue_check &bool) 
 					&& param_elem_type == arg_elem_type {
 					continue
 				}
+			}
+			// passing []?T to []T
+			if !exp_arg_typ.has_flag(.variadic) && param_typ_sym.kind == .array
+				&& c.table.final_sym(got_arg_typ).kind == .array
+				&& c.check_basic(c.table.value_type(got_arg_typ).clear_flag(.option), c.table.value_type(exp_arg_typ)) {
+				continue
 			}
 			c.error('${err.msg()} in argument ${i + 1} to `${left_sym.name}.${method_name}`',
 				arg.pos)
