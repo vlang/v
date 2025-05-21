777442cce4847905f5c1b037cf3f54a1d881b66a
Laurent Cheylus <foxy@free.fr>
2025-05-21 16:36:37 +0200

    encoding.utf8: add more tests for UTF-8 strings
    - add new tests for valid/invalid UTF-8 strings. Extract from Go UTF8
      tests https://cs.opensource.google/go/go/+/refs/tags/go1.24.3:src/unicode/utf8/utf8_test.go
    
    - add note about checks for UTF-8 Byte sequences according to Unicode Standard
    
    Signed-off-by: Laurent Cheylus <foxy@free.fr>

--- vlib/encoding/utf8/validate/validate_utf8.v
+++ vlib/encoding/utf8/validate/validate_utf8.v
@@ -48,6 +48,19 @@ fn (mut s Utf8State) seq(r0 bool, r1 bool, is_tail bool) bool {
 	return false
 }
 
+/* Check UTF-8 Byte sequences according to Unicode Standard
+ * https://www.unicode.org/versions/Unicode16.0.0/core-spec/chapter-3/
+ * Code Points        1st       2s       3s       4s
+ * U+0000..U+007F     00..7F
+ * U+0080..U+07FF     C2..DF   80..BF
+ * U+0800..U+0FFF     E0       A0..BF   80..BF
+ * U+1000..U+CFFF     E1..EC   80..BF   80..BF
+ * U+D000..U+D7FF     ED       80..9F   80..BF
+ * U+E000..U+FFFF     EE..EF   80..BF   80..BF
+ * U+10000..U+3FFFF   F0       90..BF   80..BF   80..BF
+ * U+40000..U+FFFFF   F1..F3   80..BF   80..BF   80..BF
+ * U+100000..U+10FFFF F4       80..8F   80..BF   80..BF
+ */
 fn (mut s Utf8State) next_state(c u8) {
 	// sequence 1
 	if s.index == 0 {
