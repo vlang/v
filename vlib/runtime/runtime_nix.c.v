7aec99457fe45172dcfba68c612fa5a5c1a1224c
kbkpbot <kbkpbot@sina.com>
2025-06-04 22:37:16 +0800

    fix error msg

--- vlib/runtime/runtime_nix.c.v
+++ vlib/runtime/runtime_nix.c.v
@@ -11,10 +11,13 @@ pub fn nr_cpus() int {
 pub fn total_memory() !usize {
 	page_size := usize(C.sysconf(C._SC_PAGESIZE))
 	c_errno_1 := C.errno
+	if page_size == usize(-1) {
+		return error('total_memory: `C.sysconf(C._SC_PAGESIZE)` return error code = ${c_errno_1}')
+	}
 	phys_pages := usize(C.sysconf(C._SC_PHYS_PAGES))
 	c_errno_2 := C.errno
-	if page_size == usize(-1) || phys_pages == usize(-1) {
-		return error('total_memory: error code = ${c_errno_1} ${c_errno_2}')
+	if phys_pages == usize(-1) {
+		return error('total_memory: `C.sysconf(C._SC_PHYS_PAGES)` return error code = ${c_errno_2}')
 	}
 	return page_size * phys_pages
 }
