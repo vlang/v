52e174a8cb72e92281ca4713d19921ab5c7f57dd
Felipe Pena <felipensp@gmail.com>
2025-06-04 09:08:11 -0300

    fix

--- vlib/v/parser/fn.v
+++ vlib/v/parser/fn.v
@@ -1003,9 +1003,7 @@ fn (mut p Parser) fn_params() ([]ast.Param, bool, bool, bool) {
 					return []ast.Param{}, false, false, false
 				}
 				if param_type.is_ptr() && p.table.sym(param_type).kind == .struct {
-					if !param_type.has_flag(.option) {
-						param_type = param_type.ref()
-					}
+					param_type = param_type.ref()
 				} else {
 					param_type = param_type.set_nr_muls(1)
 				}
@@ -1136,9 +1134,7 @@ fn (mut p Parser) fn_params() ([]ast.Param, bool, bool, bool) {
 					return []ast.Param{}, false, false, false
 				}
 				if typ.is_ptr() && p.table.sym(typ).kind == .struct {
-					if !typ.has_flag(.option) {
-						typ = typ.ref()
-					}
+					typ = typ.ref()
 				} else {
 					typ = typ.set_nr_muls(1)
 				}
