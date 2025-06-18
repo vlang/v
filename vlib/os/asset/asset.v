2cd1c9e26bcdc0a87ef800cbe98aee6c52804bfc
Delyan Angelov <delian66@gmail.com>
2025-06-18 16:37:57 +0300

    builtin,os: enable no warnings for gg programs like `v -gc boehm_leak -cg -keepc run examples/gg/minimal.v` (part 1 - before the `gg` loop) (#24749)

--- vlib/os/asset/asset.v
+++ vlib/os/asset/asset.v
@@ -6,11 +6,14 @@ import os
 // of a resource in it. On desktop systems, it returns a path relative to the location of the executable.
 // On Android, it will return just the relative_path segment, allowing you to later use os.read_apk_asset
 // to read from it.
+@[manualfree]
 pub fn get_path(base_folder string, relative_path string) string {
 	$if android {
-		return relative_path
+		return relative_path.clone()
 	} $else {
-		return os.resource_abs_path(os.join_path(base_folder, relative_path))
+		fpath := os.join_path_single(base_folder, relative_path)
+		defer { unsafe { fpath.free() } }
+		return os.resource_abs_path(fpath)
 	}
 }
 
@@ -18,17 +21,24 @@ pub fn get_path(base_folder string, relative_path string) string {
 // On Android, it will use os.read_apk_asset, relying that the asset base folder has been prepared, and
 // prepackaged inside your APK. On desktop systems, it will use the base_folder and relative_path, to
 // locate the file, in a way, that is relative to the executable.
+@[manualfree]
 pub fn read_bytes(base_folder string, relative_path string) ![]u8 {
+	fpath := get_path(base_folder, relative_path)
+	defer { unsafe { fpath.free() } }
+	mut f_read := os.read_bytes
 	$if android {
-		return os.read_apk_asset(get_path(base_folder, relative_path))
-	} $else {
-		return os.read_bytes(get_path(base_folder, relative_path))
+		f_read = os.read_apk_asset
 	}
+	res := f_read(fpath)!
+	return res
 }
 
 // read_text will return the full content of the given asset as a string.
 // See also read_bytes.
+@[manualfree]
 pub fn read_text(base_folder string, relative_path string) !string {
-	res := read_bytes(base_folder, relative_path)!
-	return res.bytestr()
+	bytes := read_bytes(base_folder, relative_path)!
+	defer { unsafe { bytes.free() } }
+	res := bytes.bytestr()
+	return res
 }
