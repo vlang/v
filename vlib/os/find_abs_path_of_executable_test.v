93d6a6feeeebdc030b4ca6412546bb27a964bf32
Davide Beatrici <git@davidebeatrici.dev>
2025-06-23 21:29:26 +0200

    tests: ensure find_abs_path_of_executable() treats symlinks correctly
    Also, the code is refactored to use testsuite_begin() and testsuite_end().

--- vlib/os/find_abs_path_of_executable_test.v
+++ vlib/os/find_abs_path_of_executable_test.v
@@ -1,44 +1,71 @@
 import os
 
+const tfolder = os.join_path(os.real_path(os.vtmp_dir()), 'filepath_tests')
 const original_path = os.getenv('PATH')
 
-fn test_find_abs_path_of_executable() {
-	tfolder := os.join_path(os.vtmp_dir(), 'filepath_tests')
+fn testsuite_begin() {
+	eprintln('testsuite_begin, tfolder = ${tfolder}')
 	os.rmdir_all(tfolder) or {}
-	assert !os.is_dir(tfolder)
-	os.mkdir_all(tfolder)!
-	defer {
-		os.rmdir_all(tfolder) or {}
-	}
+	os.mkdir_all(tfolder) or { panic(err) }
+	os.chdir(tfolder) or { panic(err) }
+}
 
-	original_wdir := os.getwd()
-	defer {
-		os.chdir(original_wdir) or {}
-	}
+fn testsuite_end() {
+	os.chdir(os.wd_at_startup) or {}
+	os.rmdir_all(tfolder) or {}
+}
 
-	mut myclang_file := os.join_path(tfolder, 'myclang')
+fn test_find_abs_path_of_executable() {
+	exefolder := os.join_path(tfolder, 'exe')
+	os.mkdir(exefolder) or { panic(err) }
+
+	mut myclang_file := os.join_path(exefolder, 'myclang')
 	$if windows {
 		myclang_file += '.bat'
 	}
 
-	os.write_file(myclang_file, 'echo hello')!
-	os.chmod(myclang_file, 0o0777)!
-	dump(os.real_path(myclang_file))
-	dump(os.is_executable(myclang_file))
-	defer {
-		os.rm(myclang_file) or {}
+	mut mylink_file := os.join_path(tfolder, 'mylink')
+	$if windows {
+		mylink_file += '.bat'
 	}
 
-	prepend_to_original_path(tfolder)
+	os.write_file(myclang_file, 'echo hello')!
+	os.chmod(myclang_file, 0o0777)!
+	dump(os.abs_path(myclang_file))
+	dump(os.real_path(myclang_file))
+	dump(os.is_executable(myclang_file))
+
+	$if windows {
+		eprintln('Windows requires admin privileges in order to create symlinks, related tests will be faked.')
+		os.cp(myclang_file, mylink_file) or { panic(err) }
+	} $else {
+		os.symlink(myclang_file, mylink_file) or { panic(err) }
+	}
+
+	dump(os.abs_path(mylink_file))
+	dump(os.real_path(mylink_file))
+	dump(os.is_executable(mylink_file))
+
+	prepend_to_original_path(exefolder)
 	assert find_and_check('myclang')? == myclang_file
+	assert find_and_check('mylink') == none
 
 	prepend_to_original_path('.')
-	os.chdir(tfolder)!
+	assert find_and_check('mylink')? == mylink_file
+	assert find_and_check('myclang') == none
+
+	os.chdir(exefolder) or { panic(err) }
 	assert find_and_check('myclang')? == myclang_file
+	assert find_and_check('mylink') == none
+
+	prepend_to_original_path(tfolder)
+	assert find_and_check('mylink')? == mylink_file
+	assert find_and_check('myclang') == none
 
 	restore_original_path()
 	os.chdir(os.home_dir())! // Change to a *completely* different folder, just in case the original PATH contains `.`
 	assert find_and_check('myclang') == none
+	assert find_and_check('mylink') == none
 }
 
 fn find_and_check(executable string) ?string {
