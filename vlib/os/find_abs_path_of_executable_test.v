1923b02bf5ab7b2510bb104f161bb380fbd9653f
Davide Beatrici <git@davidebeatrici.dev>
2025-06-23 21:27:19 +0200

    tests: ensure find_abs_path_of_executable() returns an absolute path

--- vlib/os/find_abs_path_of_executable_test.v
+++ vlib/os/find_abs_path_of_executable_test.v
@@ -1,5 +1,7 @@
 import os
 
+const original_path = os.getenv('PATH')
+
 fn test_find_abs_path_of_executable() {
 	tfolder := os.join_path(os.vtmp_dir(), 'filepath_tests')
 	os.rmdir_all(tfolder) or {}
@@ -9,21 +11,16 @@ fn test_find_abs_path_of_executable() {
 		os.rmdir_all(tfolder) or {}
 	}
 
-	original_path := os.getenv('PATH')
 	original_wdir := os.getwd()
 	defer {
 		os.chdir(original_wdir) or {}
 	}
 
-	new_path := tfolder + os.path_delimiter + original_path
-	os.setenv('PATH', new_path, true)
-
-	mut myclang_file := 'myclang'
+	mut myclang_file := os.join_path(tfolder, 'myclang')
 	$if windows {
 		myclang_file += '.bat'
 	}
 
-	os.chdir(tfolder)!
 	os.write_file(myclang_file, 'echo hello')!
 	os.chmod(myclang_file, 0o0777)!
 	dump(os.real_path(myclang_file))
@@ -32,16 +29,32 @@ fn test_find_abs_path_of_executable() {
 		os.rm(myclang_file) or {}
 	}
 
-	fpath := os.find_abs_path_of_executable('myclang') or {
-		assert false
-		return
-	}
-	dump(fpath)
+	prepend_to_original_path(tfolder)
+	assert find_and_check('myclang')? == myclang_file
 
+	prepend_to_original_path('.')
+	os.chdir(tfolder)!
+	assert find_and_check('myclang')? == myclang_file
+
+	restore_original_path()
+	os.chdir(os.home_dir())! // Change to a *completely* different folder, just in case the original PATH contains `.`
+	assert find_and_check('myclang') == none
+}
+
+fn find_and_check(executable string) ?string {
+	fpath := os.find_abs_path_of_executable(executable) or { return none }
+
+	dump(fpath)
+	assert os.is_abs_path(fpath)
+
+	return fpath
+}
+
+fn restore_original_path() {
 	os.setenv('PATH', original_path, true)
-	os.chdir(os.home_dir())! // change to a *completely* different folder, to avoid the original PATH containing `.`
-	if x := os.find_abs_path_of_executable('myclang') {
-		eprintln('> find_abs_path_of_executable should have failed, but instead it found: ${x}')
-		assert false
-	}
+}
+
+fn prepend_to_original_path(to_prepend string) {
+	new_path := to_prepend + os.path_delimiter + original_path
+	os.setenv('PATH', new_path, true)
 }
