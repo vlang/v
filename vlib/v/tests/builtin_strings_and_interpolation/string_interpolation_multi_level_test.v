dbe3b264f1b9affb478a5beb09a55ca9c71f4e8a
Delyan Angelov <delian66@gmail.com>
2025-06-28 14:58:45 +0300

    remove vfmt off/on tags

--- vlib/v/tests/builtin_strings_and_interpolation/string_interpolation_multi_level_test.v
+++ vlib/v/tests/builtin_strings_and_interpolation/string_interpolation_multi_level_test.v
@@ -5,21 +5,43 @@ fn foo() ?string {
 fn test_string_interpolation_multi_level() {
 	x := 0
 
-	// vfmt off
-	assert '${if x == 0 { '${x}' } else { '${'${x + 1}'}' }}' == '0'
+	assert '${if x == 0 {
+		'${x}'
+	} else {
+		'${'${x + 1}'}'
+	}}' == '0'
 
-	assert '${foo() or { '${if x == 0 { '${x}' } else { '${x + 1}' }}' }}' == '0'
+	assert '${foo() or { '${if x == 0 {
+		'${x}'
+	} else {
+		'${x + 1}'
+	}}' }}' == '0'
 
-	println('${match true { true { '${x}' } else { '${x + 1}' } }}' == '0')
+	println('${match true {
+		true { '${x}' }
+		else { '${x + 1}' }
+	}}' == '0')
 
 	// codegen error
 	// assert '${match true { true { '${x}' } else { '${x + 1}' } }}' == '0'
 
-	assert '${if x == 0 { '${match x { 1 { x == 1 } else { x != 0 } }}' } else { '' }}' == 'false'
+	assert '${if x == 0 {
+		'${match x {
+			1 { x == 1 }
+			else { x != 0 }
+		}}'
+	} else {
+		''
+	}}' == 'false'
 
 	assert '${if x == 0 {
-		'${if x == 1 { '${if x == 2 { '${x + 2}' } else { '${1}' }}' }
-			else {
+		'${if x == 1 {
+			'${if x == 2 {
+				'${x + 2}'
+			} else {
+				'${1}'
+			}}'
+		} else {
 			'${if x == 0 {
 				'${match x {
 					2 {
@@ -37,6 +59,9 @@ fn test_string_interpolation_multi_level() {
 		''
 	}}' == '3'
 
-	assert '${if m := foo() {'${m}'} else {'${x + 1}'}}' == '1'
-	// vfmt on
+	assert '${if m := foo() {
+		'${m}'
+	} else {
+		'${x + 1}'
+	}}' == '1'
 }
