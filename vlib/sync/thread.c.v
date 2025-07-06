311f3f1eacaea62c8cd77bdb1b932854499f68a0
kbkpbot <kbkpbot@sina.com>
2025-07-06 16:33:32 +0800

    move to tls files; conver to/from voidptr

--- vlib/sync/thread.c.v
+++
@@ -1,9 +0,0 @@
-module sync
-
-// ThreadLocalStorage provides thread-local storage for values of type T
-@[noinit]
-struct ThreadLocalStorage[T] {
-mut:
-	key    u64  // TLS key identifier. Note: While Linux uses unsigned int, Darwin requires unsigned long. u64 accommodates both.
-	in_use bool // Allocation status flag
-}
