f157596795e313f656a02c553ef6a3bb9d91ef14
kbkpbot <kbkpbot@sina.com>
2025-07-13 07:13:48 +0800

    fix test

--- vlib/v/tests/structs/struct_aligned_test.v
+++ vlib/v/tests/structs/struct_aligned_test.v
@@ -1,13 +1,13 @@
-@[aligned: 64]
+@[aligned: 512]
 struct MyStruct {
 	a int
 }
 
 fn test_struct_aligned() {
 	x := u64(voidptr(&MyStruct{}))
-	assert x % 64 == 0
+	assert x % 512 == 0
 
 	y := MyStruct{}
 	ptr := u64(voidptr(&y))
-	assert ptr % 64 == 0
+	assert ptr % 512 == 0
 }
