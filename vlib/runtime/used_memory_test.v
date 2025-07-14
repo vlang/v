b0089f647dea4c28da3b5ac27d4beedaf3b354f3
kbkpbot <kbkpbot@sina.com>
2025-07-14 16:22:40 +0800

    use vmemset to ensure memory used

--- vlib/runtime/used_memory_test.v
+++ vlib/runtime/used_memory_test.v
@@ -1,17 +1,17 @@
-// vtest build: !musl?
-// test fail for Docker CI / docker-ubuntu-musl
 import runtime
 
 fn test_used_memory() {
 	used1 := runtime.used_memory()!
 	println('used memory 1 : ${used1}')
 
-	_ := unsafe { malloc(4096 * 1024) }
+	mut mem1 := unsafe { malloc(4096 * 1024) }
+	unsafe { vmemset(mem1, 1, 4096 * 1024) }
 
 	used2 := runtime.used_memory()!
 	println('used memory 2 : ${used2}')
 
-	_ := unsafe { malloc(8192 * 1024) }
+	mut mem2 := unsafe { malloc(8192 * 1024) }
+	unsafe { vmemset(mem2, 1, 8192 * 1024) }
 
 	used3 := runtime.used_memory()!
 	println('used memory 3 : ${used3}')
