d4bc0133a7280db4214b25b95f19f9814fbe6a26
Felipe Pena <felipensp@gmail.com>
2025-07-19 11:39:01 -0300

    fix

--- vlib/v/markused/walker.v
+++ vlib/v/markused/walker.v
@@ -956,10 +956,16 @@ pub fn (mut w Walker) mark_by_sym(isym ast.TypeSymbol) {
 		ast.Map {
 			w.mark_by_type(isym.info.key_type)
 			w.mark_by_type(isym.info.value_type)
+			w.features.used_maps++
 		}
 		ast.FnType {
 			for param in isym.info.func.params {
 				w.mark_by_type(param.typ)
+			}
+		}
+		ast.MultiReturn {
+			for typ in isym.info.types {
+				w.mark_by_type(typ)
 			}
 		}
 		else {}
