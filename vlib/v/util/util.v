15a16186f33f90d2aabb3e1ffdf4f3e92b40282b
kbkpbot <kbkpbot@sina.com>
2025-07-22 21:05:36 +0800

    fix -usecache

--- vlib/v/util/util.v
+++ vlib/v/util/util.v
@@ -16,7 +16,8 @@ import runtime
 // math.bits is needed by strconv.ftoa
 pub const builtin_module_parts = ['math.bits', 'strconv', 'dlmalloc', 'strconv.ftoa', 'strings',
 	'builtin', 'builtin.closure']
-pub const bundle_modules = ['clipboard', 'fontstash', 'gg', 'gx', 'sokol', 'szip', 'ui']!
+pub const bundle_modules = ['clipboard', 'fontstash', 'gg', 'gx', 'sokol', 'szip', 'ui',
+	'builtin.closure']!
 
 pub const external_module_dependencies_for_tool = {
 	'vdoc': ['markdown']
