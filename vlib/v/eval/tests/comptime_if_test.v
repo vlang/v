8f1afb1d2e2384ac8551436a27a6d9bb3d231198
kbkpbot <kbkpbot@sina.com>
2025-07-23 20:02:46 +0800

    fix test

--- vlib/v/eval/tests/comptime_if_test.v
+++ vlib/v/eval/tests/comptime_if_test.v
@@ -25,3 +25,25 @@ fn test_comptime_if() {
 	assert ret[0].string() != 'unknown'
 	assert ret[1].float_val() == 1.5
 }
+
+fn test_comptime_if_without_func() {
+	mut e := eval.create()
+
+	ret := e.run('const a =
+	\$if amd64 { "amd" }
+	\$else \$if i386        { "i386" }
+	\$else \$if aarch64     { "aarch64" }
+	\$else \$if arm64       { "arm64" }
+	\$else \$if arm32       { "arm32" }
+	\$else \$if rv64        { "rv64" }
+	\$else \$if rv32        { "rv32" }
+	\$else \$if s390x       { "s390x" }
+	\$else \$if ppc64le     { "ppc64le" }
+	\$else \$if loongarch64 { "loongarch64" }
+	\$else { "unknown" }
+
+	const b = 1.5')!
+
+	dump(ret)
+	assert ret == []
+}
