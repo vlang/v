56c8af031613fcfb7d9b6bb90e9e9da817b5b701
kbkpbot <kbkpbot@sina.com>
2025-07-23 21:38:45 +0800

    fix if_test.v

--- vlib/v/eval/tests/if_test.v
+++ vlib/v/eval/tests/if_test.v
@@ -1,6 +1,6 @@
 import v.eval
 
-fn test_const() {
+fn test_if_return() {
 	mut e := eval.create()
 
 	ret := e.run('
@@ -15,4 +15,17 @@ fn test_const() {
 
 	dump(ret)
 	assert ret[0].int_val() == 999
+
+	ret1 := e.run('
+	fn display(a int) int {
+		if a == 100 {
+			return 666
+		} else {
+			return 999
+		}
+	}
+	display(100)')!
+
+	dump(ret1)
+	assert ret1[0].int_val() == 666
 }
