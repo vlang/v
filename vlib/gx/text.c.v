83afa246262719fe5eb8c4c83f58d0fa6017f85f
Delyan Angelov <delian66@gmail.com>
2025-07-24 19:10:52 +0300

    some fixes for ved and the macos CI

--- vlib/gx/text.c.v
+++ vlib/gx/text.c.v
@@ -1,20 +1,8 @@
 module gx
 
 import fontstash as _
+import gg
 
-@[deprecated: 'use gg.HorizontalAlign instead']
-@[deprecated_after: '2026-01-24']
-pub enum HorizontalAlign {
-	left   = C.FONS_ALIGN_LEFT
-	center = C.FONS_ALIGN_CENTER
-	right  = C.FONS_ALIGN_RIGHT
-}
+pub type HorizontalAlign = gg.HorizontalAlign
 
-@[deprecated: 'use gg.VerticalAlign instead']
-@[deprecated_after: '2026-01-24']
-pub enum VerticalAlign {
-	top      = C.FONS_ALIGN_TOP
-	middle   = C.FONS_ALIGN_MIDDLE
-	bottom   = C.FONS_ALIGN_BOTTOM
-	baseline = C.FONS_ALIGN_BASELINE
-}
+pub type VerticalAlign = gg.VerticalAlign
