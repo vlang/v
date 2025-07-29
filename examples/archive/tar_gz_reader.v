b973c9e5ac009bbaa62620b32945c4f82726168f
Jorge Mireles <jorgeluismireles@gmail.com>
2025-07-29 10:05:59 -0600

    tar_gz_reader: Rename Cfg by Context

--- examples/archive/tar_gz_reader.v
+++ examples/archive/tar_gz_reader.v
@@ -7,7 +7,7 @@ import term
 const default_url = 'https://github.com/vlang/v/archive/refs/tags/v0.1.3.tar.gz'
 
 @[heap]
-struct Cfg {
+struct Context {
 	url        string // Web starting with http:// or https://. Local starting with file:///
 	chunks     bool   // true: decompress with callback
 	debug      int    // print debug lines
@@ -15,21 +15,21 @@ struct Cfg {
 	filename   string // if filename is found as a path of a data block, stops early.
 }
 
-fn (c &Cfg) read_last_block(mut read tar.Read) bool {
-	if c.max_blocks > 0 && c.max_blocks < read.get_block_number() {
+fn (ctx &Context) read_last_block(mut read tar.Read) bool {
+	if ctx.max_blocks > 0 && ctx.max_blocks < read.get_block_number() {
 		read.stop_early = true
 		return true
 	}
 	return false
 }
 
-fn new_cfg() !&Cfg {
+fn new_context() !&Context {
 	mut fp := flag.new_flag_parser(os.args)
 	fp.application('tar_gz_reader')
 	fp.version('0.0.20250721')
 	fp.description('Reads into memory selected sections of *.tar.gz. archives from https or home_dir.')
 	fp.skip_executable()
-	cfg := &Cfg{
+	ctx := &Context{
 		url:        fp.string('url', `u`, default_url, 'archive *.tar.gz URL, default(${default_url}). Start name with file:/// for local')
 		chunks:     fp.bool('chunks', `c`, false, 'decompress with chunks to reduce RAM usage, default(false)')
 		debug:      fp.int('debug', `d`, 0, 'prints blocks: 1=other, 2:+dirs, 3=+files, 4=+data, default(0=silent)')
@@ -40,7 +40,7 @@ fn new_cfg() !&Cfg {
 	if additional.len > 0 {
 		println('unprocessed args ${additional.join_lines()}')
 	}
-	return cfg
+	return ctx
 }
 
 // Downloader downloads a *.tar.gz using HTTP chunks
@@ -78,61 +78,61 @@ fn (mut d Downloader) on_chunk(request &http.Request, chunk []u8, already_receiv
 fn (mut d Downloader) on_finish(request &http.Request, response &http.Response) ! {}
 
 struct FileReader implements tar.Reader {
-	cfg &Cfg
+	ctx &Context
 mut:
 	filepath string
 	content  []u8
 }
 
-fn new_file_reader(cfg &Cfg) FileReader {
+fn new_file_reader(ctx &Context) FileReader {
 	return FileReader{
-		cfg: cfg
+		ctx: ctx
 	}
 }
 
 fn (mut f FileReader) other_block(mut read tar.Read, details string) {
-	if f.cfg.read_last_block(mut read) {
+	if f.ctx.read_last_block(mut read) {
 		return
 	}
-	if f.cfg.debug > 0 {
+	if f.ctx.debug > 0 {
 		row := 'OTHER  block:${read.get_block_number():6} ${read.get_special()} ${details} ${read.get_path()} '
 		println(term.colorize(term.bright_yellow, row))
 	}
 }
 
 fn (mut f FileReader) dir_block(mut read tar.Read, size u64) {
-	if f.cfg.read_last_block(mut read) {
+	if f.ctx.read_last_block(mut read) {
 		return
 	}
-	if f.cfg.debug > 1 {
+	if f.ctx.debug > 1 {
 		row := 'DIR    block:${read.get_block_number():6} ${read.get_path()} size:${size}'
 		println(term.colorize(term.green, row))
 	}
 }
 
 fn (mut f FileReader) file_block(mut read tar.Read, size u64) {
-	if f.cfg.read_last_block(mut read) {
+	if f.ctx.read_last_block(mut read) {
 		return
 	}
 	path := read.get_path()
-	if f.cfg.debug > 2 {
+	if f.ctx.debug > 2 {
 		row := ' FILE  block:${read.get_block_number():6} ${path} size:${size}'
 		println(term.colorize(term.bright_blue, row))
 	}
-	if f.cfg.filename != '' && f.filepath == '' && path.ends_with(f.cfg.filename) {
+	if f.ctx.filename != '' && f.filepath == '' && path.ends_with(f.ctx.filename) {
 		f.filepath = path
 	}
 }
 
 fn (mut f FileReader) data_block(mut read tar.Read, data []u8, pending int) {
-	if f.cfg.read_last_block(mut read) {
+	if f.ctx.read_last_block(mut read) {
 		return
 	}
 	path := read.get_path()
-	if f.cfg.debug > 3 {
+	if f.ctx.debug > 3 {
 		println('  DATA block:${read.get_block_number():6} ${path} len:${data.len} pend:${pending}')
 	}
-	if f.cfg.filename != '' {
+	if f.ctx.filename != '' {
 		if f.filepath == path {
 			f.content << data
 			if pending == 0 {
@@ -144,20 +144,20 @@ fn (mut f FileReader) data_block(mut read tar.Read, data []u8, pending int) {
 }
 
 fn main() {
-	cfg := new_cfg()!
+	ctx := new_context()!
 	reader := FileReader{
-		cfg: cfg
+		ctx: ctx
 	}
 	mut untar := tar.new_untar(reader)
 	mut decompressor := tar.new_decompresor(untar)
-	downloader := new_downloader(cfg.url)!
-	if cfg.chunks {
+	downloader := new_downloader(ctx.url)!
+	if ctx.chunks {
 		decompressor.read_chunks(downloader.data)!
 	} else {
 		decompressor.read_all(downloader.data)!
 	}
 	println('-'.repeat(80))
-	println('Download: ${cfg.url} chunks:${downloader.chunks} bytes=${downloader.data.len}')
+	println('Download: ${ctx.url} chunks:${downloader.chunks} bytes=${downloader.data.len}')
 	println('Untar:    ${untar}')
 	println('Content:  Path:${reader.filepath} bytes:${reader.content.len}')
 	println('-'.repeat(80))
