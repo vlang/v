d249bfb04e9c040e24213b763829ac141a811c88
Larsimusrex <lars.luis@gmail.com>
2025-08-03 09:27:43 +0200

    decoder2: fix remaining json2 discrepancies (#25029)

--- vlib/time/json_decode.c.v
+++ vlib/time/json_decode.c.v
@@ -1,6 +1,36 @@
 module time
 
-// from_json_string implements a custom decoder for json2
+// from_json_string implements a custom decoder for json2 (unix)
+pub fn (mut t Time) from_json_number(raw_number string) ! {
+	t = unix(raw_number.i64())
+}
+
+// from_json_string implements a custom decoder for json2 (iso8601/rfc3339/unix)
 pub fn (mut t Time) from_json_string(raw_string string) ! {
-	t = parse_rfc3339(raw_string) or { Time{} }
+	is_iso8601 := raw_string[4] == `-` && raw_string[7] == `-`
+	if is_iso8601 {
+		t = parse_iso8601(raw_string)!
+		return
+	}
+
+	is_rfc3339 := raw_string.len == 24 && raw_string[23] == `Z` && raw_string[10] == `T`
+	if is_rfc3339 {
+		t = parse_rfc3339(raw_string)!
+		return
+	}
+
+	mut is_unix_timestamp := true
+	for c in raw_string {
+		if c == `-` || (c >= `0` && c <= `9`) {
+			continue
+		}
+		is_unix_timestamp = false
+		break
+	}
+	if is_unix_timestamp {
+		t = unix(raw_string.i64())
+		return
+	}
+
+	return error('Expected iso8601/rfc3339/unix time but got: ${raw_string}')
 }
