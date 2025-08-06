5abdb791bb7e13a6752be3b0d914764d21f5ca61
Delyan Angelov <delian66@gmail.com>
2025-08-06 12:50:55 +0300

    fix failures on windows, improve run_project_folders_test.v output on failure

--- vlib/v/slow_tests/run_project_folders/compile_obj_file_and_link_separately/main.v
+++ vlib/v/slow_tests/run_project_folders/compile_obj_file_and_link_separately/main.v
@@ -10,14 +10,20 @@ const gcc = os.quoted_path(os.find_abs_path_of_executable('gcc') or {
 	exit(0)
 })
 
+fn lexec(cmd string) os.Result {
+	println('>>> lexec cmd: ${cmd}')
+	res := os.execute_or_exit(cmd)
+	return res
+}
+
 fn main() {
 	os.chdir(@DIR)!
 	os.rmdir_all(tfolder) or {}
 	os.mkdir_all(tfolder)!
-	os.execute_or_exit('${vexe} -is_o -o ${tfolder}/abc.o        -gc none -cc ${gcc} abc/')
-	os.execute_or_exit('${gcc}        -o ${tfolder}/main_in_c.o  -c main_in_c.c')
-	os.execute_or_exit('${gcc}        -o ${tfolder}/program.exe  ${tfolder}/abc.o   ${tfolder}/main_in_c.o')
-	res := os.execute_or_exit('${tfolder}/program.exe')
+	lexec('${vexe} -is_o -o "${tfolder}/abc.o"        -gc none -cc ${gcc} abc/')
+	lexec('${gcc}        -o "${tfolder}/main_in_c.o"  -c main_in_c.c')
+	lexec('${gcc}        -o "${tfolder}/program.exe"  "${tfolder}/abc.o"   "${tfolder}/main_in_c.o"')
+	res := lexec('${tfolder}/program.exe')
 	print(res.output)
 	assert res.output.starts_with('Result of addition: 30')
 	os.rmdir_all(tfolder) or {}
