47268b1d3e408dfdd41c806e5fbd24023195eda3
Delyan Angelov <delian66@gmail.com>
2025-08-06 11:11:57 +0300

    v.help: add documentation and a test for the `-is_o` option (with gcc)

---
+++ vlib/v/slow_tests/run_project_folders/compile_obj_file_and_link_separately/main.v
@@ -0,0 +1,25 @@
+module main
+
+import os
+
+const tfolder = os.join_path(os.vtmp_dir(), 'vobjfile_${os.getuid()}')
+const vexe = os.quoted_path(@VEXE)
+const gcc = os.quoted_path(os.find_abs_path_of_executable('gcc') or {
+	println('This program needs `gcc` to be present.')
+	println('OK')
+	exit(0)
+})
+
+fn main() {
+	os.chdir(@DIR)!
+	os.rmdir_all(tfolder) or {}
+	os.mkdir_all(tfolder)!
+	os.execute_or_exit('${vexe} -is_o -o ${tfolder}/abc.o        -gc none -cc ${gcc} abc/')
+	os.execute_or_exit('${gcc}        -o ${tfolder}/main_in_c.o  -c main_in_c.c')
+	os.execute_or_exit('${gcc}        -o ${tfolder}/program.exe  ${tfolder}/abc.o   ${tfolder}/main_in_c.o')
+	res := os.execute_or_exit('${tfolder}/program.exe')
+	print(res.output)
+	assert res.output.starts_with('Result of addition: 30')
+	os.rmdir_all(tfolder) or {}
+	println('OK')
+}
