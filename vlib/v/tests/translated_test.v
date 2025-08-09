ece45f602452db7424648d0a9911dd384c085985
kbkpbot <kbkpbot@sina.com>
2025-08-09 21:25:29 +0800

    cgen: fix translated file pointer assignment

--- vlib/v/tests/translated_test.v
+++ vlib/v/tests/translated_test.v
@@ -12,3 +12,16 @@ const ssf = [1, 2, 3]!
 fn test_const_name_without_main_prefix() {
 	assert ssf[0] == 1
 }
+
+struct ASMOperand {
+	constraint [2]i8
+}
+
+fn test_pointer_assign_without_memcpy() {
+	op := ASMOperand{
+		constraint: [i8(5), 4]!
+	}
+	str := &i8(0)
+	str = op.constraint
+	assert !isnil(str)
+}
