ab80c767bdf8506677bf204b7522912fd184fb21
Delyan Angelov <delian66@gmail.com>
2025-08-13 13:03:01 +0300

    vdoc: extract vdoc_run_examples_test.v from vdoc_file_test.v

--- cmd/tools/vdoc/vdoc_file_test.v
+++ cmd/tools/vdoc/vdoc_file_test.v
@@ -44,31 +44,6 @@ fn test_output() {
 	assert total_fails == 0
 }
 
-fn test_run_examples_good() {
-	os.setenv('VCOLORS', 'never', true)
-	os.chdir(vroot)!
-	res := os.execute('${vexe} doc -comments -run-examples cmd/tools/vdoc/testdata/run_examples_good/main.v')
-	assert res.exit_code == 0
-	assert res.output.contains('module main'), res.output
-	assert res.output.contains('fn abc()'), res.output
-	assert res.output.contains("abc just prints 'xyz'"), res.output
-	assert res.output.contains('and should succeed'), res.output
-	assert res.output.contains('Example: assert 5 * 5 == 25'), res.output
-}
-
-fn test_run_examples_bad() {
-	os.setenv('VCOLORS', 'never', true)
-	os.chdir(vroot)!
-	res := os.execute('${vexe} doc -comments -run-examples cmd/tools/vdoc/testdata/run_examples_bad/main.v')
-	assert res.exit_code != 0
-	assert res.output.contains('error in documentation example'), res.output
-	assert res.output.contains(' left value: 5 * 5 = 25'), res.output
-	assert res.output.contains('right value: 77'), res.output
-	assert res.output.contains('V panic: Assertion failed...'), res.output
-	assert res.output.contains('module main'), res.output
-	assert res.output.contains('Example: assert 5 * 5 == 77'), res.output
-}
-
 const small_pure_v_vlib_module = 'bitfield'
 
 fn test_out_path() {
