f8737e5138d06e72a09d6e84f01ffd4a978c4732
Felipe Pena <felipensp@gmail.com>
2025-08-15 09:02:28 -0300

    fix

--- vlib/v/type_resolver/generic_resolver.v
+++ vlib/v/type_resolver/generic_resolver.v
@@ -342,7 +342,7 @@ pub fn (mut t TypeResolver) resolve_args(cur_fn &ast.FnDecl, func &ast.Fn, mut n
 			} else {
 				comptime_args[k] = ctyp
 			}
-		} else if mut call_arg.expr is ast.SelectorExpr
+		} else if mut call_arg.expr is ast.SelectorExpr && call_arg.expr.expr is ast.Ident
 			&& call_arg.expr.expr_type.has_flag(.generic) {
 			comptime_args[k] = t.typeof_type(call_arg.expr, call_arg.expr.typ)
 		}
