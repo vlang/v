eae35bc8c9173913709abf6c542e49a82582c8a1
kbkpbot <kbkpbot@sina.com>
2025-08-17 14:18:10 +0800

    fix test

--- vlib/v/tests/comptime/comptime_if_glibc_test.v
+++ vlib/v/tests/comptime/comptime_if_glibc_test.v
@@ -2,20 +2,20 @@ fn test_comptime_if_glibc() {
 	mut x := 1
 	mut result := ''
 	$if glibc {
-		result += ' glibc'
+		result += '+glibc'
 		x = 2
 	} $else {
-		result += ' else glibc'
+		result += '-glibc'
 		x = 3
 	}
 
 	$if !glibc {
-		result += ' !glibc'
+		result += '!glibc'
 	} $else {
-		result += ' else !glibc'
+		result += '!!glibc'
 	}
 
 	println(result)
-	assert result == ' glibc else !glibc' || result == ' else glibc  !glibc'
+	assert result == '+glibc!!glibc' || result == '-glibc!glibc'
 	println('done')
 }
