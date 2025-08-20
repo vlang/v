08692028406acf1445638040629984301c2d94ba
Delyan Angelov <delian66@gmail.com>
2025-08-20 22:19:41 +0300

    change of approach, replace in /etc/apt/apt-mirrors.txt

--- .github/workflows/disable_azure_mirror.sh
+++ .github/workflows/disable_azure_mirror.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-echo "APT MIRRORS:"
+echo "APT MIRRORS BEFORE:"
 cat /etc/apt/apt-mirrors.txt
+sudo sed -i 's@http://azure.archive.ubuntu.com@https://us.archive.ubuntu.com/g' /etc/apt/apt-mirrors.txt
+echo "APT MIRRORS AFTER:"
+cat /etc/apt/apt-mirrors.txt
+
 echo "ls -la /etc/apt/sources.list.d/"
 ls -la /etc/apt/sources.list.d/
-echo "replacing azure with the US mirror ..."
-sed -e 's/azure.archive.ubuntu.com/us.archive.ubuntu.com/g' -e t -e d /etc/apt/sources.list | sudo tee /etc/apt/sources.list.d/nonazure.list
-echo "/etc/apt/sources.list.d/nonazure.list:"
-cat /etc/apt/sources.list.d/nonazure.list
+for f in /etc/apt/sources.list.d/*; do echo "####### $f ######"; cat $f; done
