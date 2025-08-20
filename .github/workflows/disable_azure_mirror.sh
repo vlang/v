9a93e822390c79f324648b2c7555200025d2212f
Delyan Angelov <delian66@gmail.com>
2025-08-20 22:49:47 +0300

    ci: add a disable_azure_mirror.sh script, and use it before `apt update` to reduce the recent timeouts during installing dependencies (#25147)

---
+++ .github/workflows/disable_azure_mirror.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "APT MIRRORS BEFORE:"
+cat /etc/apt/apt-mirrors.txt
+sudo sed -i 's@http://azure.archive.ubuntu.com@https://us.archive.ubuntu.com@gm' /etc/apt/apt-mirrors.txt
+echo "APT MIRRORS AFTER:"
+cat /etc/apt/apt-mirrors.txt
+
+echo "ls -la /etc/apt/sources.list.d/"
+ls -la /etc/apt/sources.list.d/
+for f in /etc/apt/sources.list.d/*; do echo "####### $f ######"; cat $f; done
