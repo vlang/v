2e23f2a18b02ebfc805f2dc038f75acbdc72af3a
kbkpbot <kbkpbot@sina.com>
2025-08-21 15:27:30 +0800

    checker: ensure the defer behavior matches that of cgen (fix #25148) (#25146)

--- vlib/v/checker/return.v
+++ vlib/v/checker/return.v
@@ -33,6 +33,15 @@ fn (mut c Checker) return_stmt(mut node ast.Return) {
 	defer {
 		c.inside_return = prev_inside_return
 	}
+
+	// check `defer_stmts` in return, to ensure the same behavior with `cgen`
+	old_inside_defer := c.inside_defer
+	c.inside_defer = true
+	for i := c.table.cur_fn.defer_stmts.len - 1; i >= 0; i-- {
+		c.stmts(mut c.table.cur_fn.defer_stmts[i].stmts)
+	}
+	c.inside_defer = old_inside_defer
+
 	c.expected_type = c.table.cur_fn.return_type
 	mut expected_type := c.unwrap_generic(c.expected_type)
 	if expected_type != 0 && c.table.sym(expected_type).kind == .alias {
