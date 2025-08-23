a6538757da19cf4b30aeb1a95485f9df81afabe9
Delyan Angelov <delian66@gmail.com>
2025-08-23 08:24:02 +0300

    cgen: fix generated str method for enums that have C values (use ifs, instead of switch)

--- vlib/v/tests/project_with_c_code_2/modc/header.h
+++ vlib/v/tests/project_with_c_code_2/modc/header.h
@@ -15,5 +15,29 @@ void handle_array2(void *p, int n);
 void destroy_atype(void *p);
 
 
+// The following emulates the structure of the SDL3 header SDL_pixels.h, and the enum SDL_PixelFormat.
+// See also https://github.com/vlang/v/issues/25135 .
+#define ABC 1
+#define XYZ 1
+
+typedef enum EnumWithDuplicates {
+    UNKNOWN = 0,
+    NAME1 = 0x1u,
+    NAME2 = 0x2u,
+    NAME3 = 0x3u,
+    NAME4 = 0x4u,
+    NAME5 = 0x5u,
+    NAME6 = 0x6u,
+    #if ABC == XYZ
+	    COMMON_NAME1 = NAME1,
+	    COMMON_NAME2 = NAME2,
+	    COMMON_NAME3 = NAME3
+    #else
+	    COMMON_NAME1 = NAME4,
+	    COMMON_NAME2 = NAME5,
+	    COMMON_NAME3 = NAME6
+    #endif
+} EnumWithDuplicates;
+
 #endif
 
