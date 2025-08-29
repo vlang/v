ef58f27f7262dfbc441fb23c71926ea161d6e657
lmp <dev.larpon@gmail.com>
2025-08-29 16:14:49 +0200

    flag: better covering fix for #25166 tail/single bool flag error in `flag_to.v`

--- vlib/flag/flag_to_tail_bool_test.v
+++ vlib/flag/flag_to_tail_bool_test.v
@@ -1,14 +1,22 @@
 import flag
 
-const args_bool_short = ['some.exe', '-h']
-const args_bool_long = ['some.exe', '-help']
+const args_bool_short_tail = ['some.exe', '-h']
+const args_bool_long_tail = ['some.exe', '-help']
+
+const args_bool_short_mixed = ['some.exe', '-h', '-long', 'val']
+const args_bool_long_mixed = ['some.exe', '-help', '-long', 'val']
 
 struct CliOptions {
 	show_help bool @[long: 'help'; short: h]
 }
 
-fn test_short_tail_bool() {
-	cli_options, unmatched := flag.to_struct[CliOptions](args_bool_short,
+struct CliOptions2 {
+	show_help bool @[long: 'help'; short: h]
+	long      string
+}
+
+fn test_v_style_short_tail_bool() {
+	cli_options, unmatched := flag.to_struct[CliOptions](args_bool_short_tail,
 		skip:  1
 		style: .v
 		mode:  .relaxed
@@ -24,8 +32,8 @@ fn test_short_tail_bool() {
 	}
 }
 
-fn test_long_tail_bool() {
-	cli_options, unmatched := flag.to_struct[CliOptions](args_bool_long,
+fn test_v_style_long_tail_bool() {
+	cli_options, unmatched := flag.to_struct[CliOptions](args_bool_long_tail,
 		skip:  1
 		style: .v
 		mode:  .relaxed
@@ -39,4 +47,40 @@ fn test_long_tail_bool() {
 	} else {
 		assert false
 	}
+}
+
+fn test_v_style_short_bool() {
+	cli_options, unmatched := flag.to_struct[CliOptions2](args_bool_short_mixed,
+		skip:  1
+		style: .v
+		mode:  .relaxed
+	)!
+
+	if unmatched.len > 0 {
+		assert false
+	}
+	if cli_options.show_help {
+		assert true
+	} else {
+		assert false
+	}
+	assert cli_options.long == 'val'
+}
+
+fn test_v_style_long_bool() {
+	cli_options, unmatched := flag.to_struct[CliOptions2](args_bool_long_mixed,
+		skip:  1
+		style: .v
+		mode:  .relaxed
+	)!
+
+	if unmatched.len > 0 {
+		assert false
+	}
+	if cli_options.show_help {
+		assert true
+	} else {
+		assert false
+	}
+	assert cli_options.long == 'val'
 }
