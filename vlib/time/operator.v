d71e469dca0f9036d3b75ec9a7cbcdbe4443c60e
Leo Developer <leo@git.leodev.win>
2025-09-04 10:21:55 +0200

    make time.unix/0 always return a utc timestamp

--- vlib/time/operator.v
+++ vlib/time/operator.v
@@ -3,13 +3,15 @@ module time
 // operator `==` returns true if provided time is equal to time
 @[inline]
 pub fn (t1 Time) == (t2 Time) bool {
-	return t1.unix() == t2.unix() && t1.nanosecond == t2.nanosecond
+	return t1.is_local == t2.is_local && t1.local_unix() == t2.local_unix() && t1.nanosecond == t2.nanosecond
 }
 
 // operator `<` returns true if provided time is less than time
 @[inline]
 pub fn (t1 Time) < (t2 Time) bool {
-	return t1.unix() < t2.unix() || (t1.unix() == t2.unix() && t1.nanosecond < t2.nanosecond)
+	t1u := t1.unix()
+	t2u := t2.unix()
+	return t1u < t2u || (t1u == t2u && t1.nanosecond < t2.nanosecond)
 }
 
 // Time subtract using operator overloading.
