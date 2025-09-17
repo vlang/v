578b6bb56c66400e9192eb24b962a331abf1abc7
Dylan Donnell <dylan.donnell@student.griffith.ie>
2025-09-17 20:49:36 +0200

    autofree,flag: fix double free in FlagParser free

---
+++ vlib/flag/flag_autofree_test.v
@@ -0,0 +1,10 @@
+// vtest vflags: -autofree
+import flag
+import os
+
+fn test_main() {
+	mut fp := flag.new_flag_parser(os.args)
+	test := fp.string('test', 0, '', 'this is a test')
+	fp.finalize() or { return }
+	println(test)
+}
