ebf484364aba6447f78baacb8778b035a17619c8
谢克 <1023052645@qq.com>
2025-09-20 20:17:07 +0800

    fix

--- vlib/v/gen/c/cmain.v
+++ vlib/v/gen/c/cmain.v
@@ -134,10 +134,12 @@ fn (mut g Gen) gen_c_main_function_only_header() {
 fn (mut g Gen) gen_c_main_function_header() {
 	g.gen_c_main_function_only_header()
 
-	g.writeln('\tif (fileno(stdout) < 0) {')
-	g.writeln('\t\tfreopen("NUL", "w", stdout);')
-	g.writeln('\t\tfreopen("NUL", "w", stderr);')
-	g.writeln('\t}')
+	$if !freestanding {
+		g.writeln('\tif (fileno(stdout) < 0) {')
+		g.writeln('\t\tfreopen("NUL", "w", stdout);')
+		g.writeln('\t\tfreopen("NUL", "w", stderr);')
+		g.writeln('\t}')
+	}
 
 	g.gen_c_main_trace_calls_hook()
 	if !g.pref.no_builtin {
