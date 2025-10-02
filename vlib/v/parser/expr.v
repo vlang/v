1a20e4e1d5562f3442dcd8b0ce1f5d2f87a5b1cb
Swastik <swstkbaranwal@gmail.com>
2025-10-03 03:32:38 +0530

    parser: allow smart detect `or {}` inside loop when using `c <-val`

--- vlib/v/parser/expr.v
+++ vlib/v/parser/expr.v
@@ -827,6 +827,16 @@ fn (mut p Parser) infix_expr(left ast.Expr) ast.Expr {
 	mut or_scope := &ast.Scope(unsafe { nil })
 	// allow `x := <-ch or {...}` to handle closed channel
 	if op == .arrow {
+		if mut right is ast.SelectorExpr {
+			or_kind = right.or_block.kind
+			or_stmts = right.or_block.stmts.clone()
+			right.or_block = ast.OrExpr{}
+		}
+		if mut right is ast.CallExpr {
+			or_kind = right.or_block.kind
+			or_stmts = right.or_block.stmts.clone()
+			right.or_block = ast.OrExpr{}
+		}
 		if p.tok.kind == .key_orelse {
 			or_kind = .block
 			or_stmts, or_pos, or_scope = p.or_block(.with_err_var)
