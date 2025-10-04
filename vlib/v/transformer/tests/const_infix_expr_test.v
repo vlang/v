6834c1e8476a21f15758f0c59cd567b9f7af7563
kbkpbot <kbkpbot@sina.com>
2025-10-04 13:33:46 +0800

    transformer: eval par expr for literal and ident

--- vlib/v/transformer/tests/const_infix_expr_test.v
+++ vlib/v/transformer/tests/const_infix_expr_test.v
@@ -4,12 +4,12 @@ import v.parser
 import v.checker
 import v.transformer
 
-fn test_const_infix_expr() {
+fn test_const_par_expr_and_infix_expr() {
 	println(@LOCATION)
 	source_text := '
 const k = 2
 fn main() {
-	x := [(k-1)*2]int{}
+	x := [(k+1)-2+(2*(k))]int{}
 }
 '
 	mut table := ast.new_table()
@@ -19,15 +19,15 @@ fn main() {
 	checker_.check(mut prog)
 	mut t := transformer.new_transformer_with_table(table, vpref)
 
-	// get the `InfixExpr`(`(k-1)*2`) from table
-	main_fn := unsafe { table.cur_fn[0] }
+	// get the `InfixExpr`(`(k+1)-2+(2*(k))`) from table
+	main_fn := table.cur_fn
 	assign_stmt := main_fn.stmts[0] as ast.AssignStmt
 	array_init_expr := assign_stmt.right[0] as ast.ArrayInit
 	mut dim_expr := array_init_expr.exprs[0] as ast.InfixExpr
 	dump(dim_expr)
 
-	// verify `infix_expr` work as expected
+	// verify `infix_expr` and `par_expr` work as expected
 	folded_expr := t.infix_expr(mut dim_expr)
 	dump(folded_expr)
-	assert '${folded_expr}' == '(1) * 2'
+	assert '${folded_expr}' == '5'
 }
