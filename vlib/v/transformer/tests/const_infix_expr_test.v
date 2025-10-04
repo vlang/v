1153f420885db539e09a3381a08d76d5d07982e5
kbkpbot <kbkpbot@sina.com>
2025-10-04 09:55:14 +0800

    transformer: add test

---
+++ vlib/v/transformer/tests/const_infix_expr_test.v
@@ -0,0 +1,33 @@
+import v.ast
+import v.pref
+import v.parser
+import v.checker
+import v.transformer
+
+fn test_const_infix_expr() {
+	println(@LOCATION)
+	source_text := '
+const k = 2
+fn main() {
+	x := [(k-1)*2]int{}
+}
+'
+	mut table := ast.new_table()
+	vpref := &pref.Preferences{}
+	mut prog := parser.parse_text(source_text, '', mut table, .skip_comments, vpref)
+	mut checker_ := checker.new_checker(table, vpref)
+	checker_.check(mut prog)
+	mut t := transformer.new_transformer_with_table(table, vpref)
+
+	// get the `InfixExpr`(`(k-1)*2`) from table
+	main_fn := unsafe { table.cur_fn[0] }
+	assign_stmt := main_fn.stmts[0] as ast.AssignStmt
+	array_init_expr := assign_stmt.right[0] as ast.ArrayInit
+	mut dim_expr := array_init_expr.exprs[0] as ast.InfixExpr
+	dump(dim_expr)
+
+	// verify `infix_expr` work as expected
+	folded_expr := t.infix_expr(mut dim_expr)
+	dump(folded_expr)
+	assert '${folded_expr}' == '(1) * 2'
+}
