3a95b6f89636a53e62b7889871464fd38d769b5e
Felipe Pena <felipensp@gmail.com>
2025-10-11 08:26:57 -0300

    fix

--- vlib/v/tests/generic_calls/bug_test.v
+++ vlib/v/tests/generic_calls/bug_test.v
@@ -1,4 +1,3 @@
-// import faker { Faker, Currency }
 module main
 
 fn test_main() {
@@ -6,16 +5,5 @@ fn test_main() {
 
 	code := fake.currency_code()
 	codes := Currency.cases().map(it.to_code())
-	// dump(code)
-	// dump(codes)
-	assert codes.contains(code)
-}
-
-fn test_it_generates_currency_code() {
-	mut fake := Faker{}
-
-	code := fake.currency_code()
-	codes := Currency.cases().map(it.to_code())
-
 	assert codes.contains(code)
 }
