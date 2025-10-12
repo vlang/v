ee283f399ee620c0f28ef8b042c84dab9d0cc896
Dylan Donnell <dylan.donnell@student.griffith.ie>
2025-10-12 15:31:22 +0100

    x.json2.decoder2: stub module

--- vlib/x/json2/decoder_deprecated.v
+++ vlib/x/json2/decoder_deprecated.v
@@ -1,5 +1,6 @@
 module json2
 
+// Decodes a JSON string into an `Any` type. Returns an option.
 @[deprecated: 'use `decode[json2.Any]` instead']
 @[deprecated_after: '2025-10-10']
 pub fn raw_decode(src string) !Any {
