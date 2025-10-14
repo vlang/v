56256d05797e656cdad965bf9456b69d5e06476a
ge <gechandev@gmail.com>
2025-10-14 13:53:35 +0300

    json2: make decoder2 fns public

--- vlib/x/json2/decoder2/stub.v
+++ vlib/x/json2/decoder2/stub.v
@@ -4,12 +4,12 @@ import x.json2
 
 @[deprecated: '`decode` has been moved to `x.json2`, use `decode` from `x.json2` instead']
 @[deprecated_after: '2025-10-12']
-fn decode[T](val string) !T {
+pub fn decode[T](val string) !T {
 	return json2.decode[T](val)
 }
 
 @[deprecated: '`decode` has been moved to `x.json2`, use `decode` from `x.json2` instead']
 @[deprecated_after: '2025-03-18']
-fn decode_array[T](src string) !T {
+pub fn decode_array[T](src string) !T {
 	return json2.decode[T](src)
 }
