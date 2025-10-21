848cdb71162ac420fb282ce4bc41e13ee5a14640
kbkpbot <kbkpbot@sina.com>
2025-10-21 18:38:48 +0800

    parser: fix vls mode strut mut keyword (fix #25548) (#25551)

--- vlib/v/parser/struct.v
+++ vlib/v/parser/struct.v
@@ -132,7 +132,7 @@ fn (mut p Parser) struct_decl(is_anon bool) ast.StructDecl {
 		}
 		p.check(.lcbr)
 		// if p.is_vls && p.tok.kind == .key_struct { // p.tok.is_key() {
-		if p.is_vls && p.tok.is_key() {
+		if p.is_vls && p.tok.is_key() && p.tok.kind != .key_mut {
 			// End parsing after `struct Foo {` in vls mode to avoid lots of junk errors
 			// If next token after { is a key, the struct wasn't finished
 			p.error('expected `}` to finish a struct definition')
