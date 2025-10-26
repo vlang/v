b89000f9a6ca4e448ec1556aa8e7dd1c5b16ceaf
Swastik <swstkbaranwal@gmail.com>
2025-10-26 13:49:16 +0530

    fix check Co-authored-by: StunxFS <56417208+StunxFS@users.noreply.github.com>

--- vlib/v/checker/checker.v
+++ vlib/v/checker/checker.v
@@ -2342,9 +2342,8 @@ fn (mut c Checker) stmt(mut node ast.Stmt) {
 					if node.expr.op == .arrow {
 						if mut node.expr.right is ast.CallExpr {
 							if node.expr.right.return_type.has_flag(.result) {
+								node.expr.or_block.err_used = node.expr.or_block.scope.known_var('err')
 								node.expr.right.or_block = node.expr.or_block
-								node.expr.right.or_block.err_used = node.expr.or_block.scope.known_var('err')
-								node.expr.or_block = ast.OrExpr{}
 							}
 						}
 					}
