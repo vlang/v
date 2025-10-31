1335c365d7590500df83721474c9f667b1d76bb6
JMD <56417208+StunxFS@users.noreply.github.com>
2025-10-31 18:39:11 +0000

    extract scoped defer test from defer_test.v to scoped_defer_test.v

--- vlib/v/tests/defer/defer_test.v
+++ vlib/v/tests/defer/defer_test.v
@@ -187,26 +187,3 @@ fn test_defer_inside_comptime_if_else() {
 	}
 	assert true
 }
-
-fn test_scoped_defer() {
-	mut res := 0
-
-	defer {
-		res++
-		assert res == 5
-	}
-	{
-		res++
-		defer {
-			res++
-			assert res == 4
-		}
-		{
-			res += 1
-			defer {
-				res++
-				assert res == 3
-			}
-		} // <- Block 2 ends. Defer 3 executes. res = 3.
-	} // <- Block 1 ends. Defer 2 executes. res = 4.
-} // <- 'test_scoped_defer' ends. Defer 1 executes. res = 5.
