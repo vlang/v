c00a2e9932cc5d456e2ef3e49489e4335eb9ae65
Delyan Angelov <delian66@gmail.com>
2025-10-31 10:07:34 +0200

    minimize the test for clarity, and to avoid dependencies on actively developed modules

--- vlib/v/tests/fns/return_multi_fixed_arr_test.v
+++ vlib/v/tests/fns/return_multi_fixed_arr_test.v
@@ -1,20 +1,11 @@
-import rand
-import x.crypto.poly1305
+struct Abc {}
 
-fn bundle(x []u8) !([12]u8, &poly1305.Poly1305) {
-	mut x12 := [12]u8{}
-	mut x32 := []u8{len: 32}
-
-	unsafe {
-		vmemcpy(x12, x.data, 12)
-		vmemcpy(x32.data, x.data, 32)
-	}
-	po := poly1305.new(x32)!
-	return x12, po
+fn bundle(x []u8) !([12]u8, Abc) {
+	x12 := [12]u8{}
+	return x12, Abc{}
 }
 
 fn test_main() {
-	x := rand.bytes(45)!
-	a, _ := bundle(x)!
-	assert a[0..12] == x[0..12]
+	a, _ := bundle([])!
+	assert a.len == 12
 }
