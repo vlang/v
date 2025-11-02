43e682e5f520d258e365e4fe7305e8b7acb66ae1
Delyan Angelov <delian66@gmail.com>
2025-11-02 07:47:52 +0200

    add concrete assertions to the test too

--- vlib/json/tests/json_mut_map_test.v
+++ vlib/json/tests/json_mut_map_test.v
@@ -1,10 +1,12 @@
 import json
 
 fn q_and_a(mut db_json map[string][]string) {
-	json.encode(db_json)
+	x := json.encode(db_json)
+	assert x == '{}'
 }
 
 fn test_main() {
 	mut db_json := json.decode(map[string][]string, '{}')!
+	assert db_json == {}
 	q_and_a(mut db_json)
 }
