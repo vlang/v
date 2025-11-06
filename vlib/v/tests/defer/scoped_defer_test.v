46489f203bad0754cc4d70b302edcbf8458c96cc
Jose Mendoza <56417208+StunxFS@users.noreply.github.com>
2025-11-06 03:06:44 -0400

    fix defer generation within comptime if/match/for

--- vlib/v/tests/defer/scoped_defer_test.v
+++ vlib/v/tests/defer/scoped_defer_test.v
@@ -69,3 +69,51 @@ fn test_scoped_defer() {
 		} // <- Block 2 ends. Defer 3 executes. res = 3.
 	} // <- Block 1 ends. Defer 2 executes. res = 4.
 } // <- 'test_scoped_defer' ends. Defer 1 executes. res = 5.
+
+fn test_defer_with_comptime_if() {
+	mut c := 0
+	defer { assert c == 3 }
+	defer { c++ }
+	$if tinyc || gcc || clang {
+		defer { c++ }
+		c++
+	} $else {
+		c = 0
+	}
+}
+
+fn test_defer_with_comptime_match() {
+	mut c := 0
+	defer { assert c == 3 }
+	defer { c++ }
+	$match @CCOMPILER {
+		'tinyc', 'gcc', 'clang' {
+			defer { c++ }
+			c++
+		}
+		$else {
+			c = 0
+		}
+	}
+}
+
+fn test_defer_with_comptime_for() {
+	mut c := 0
+	$for f in Data.fields {
+		defer {
+			if f.name == 'counter' {
+				c++
+			}
+		}
+	}
+	assert c == 1
+
+	$for m in Data.methods {
+		defer {
+			if m.name in ['operation', 'run_operation'] {
+				c++
+			}
+		}
+	}
+	assert c == 3
+}
