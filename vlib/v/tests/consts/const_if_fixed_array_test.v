1d842bd77bfa31d2ec29e0f4add9b09865952d4c
Felipe Pena <felipensp@gmail.com>
2025-11-09 13:22:57 -0300

    fix

--- vlib/v/tests/consts/const_if_fixed_array_test.v
+++ vlib/v/tests/consts/const_if_fixed_array_test.v
@@ -1,14 +1,15 @@
-pub const fixed_sized_comptime_array_const = $if amd64 {
+// vtest vflags: -d some_other_define
+pub const fixed_sized_comptime_array_const = $if some_other_define ? {
 	[u8(0x01), 0x02]!
-} $else $if i386 {
+} $else $if some_other_define_2 ? {
 	[u8(0x01), 0x02, 0x03]!
 } $else {
 	[u8(0)]!
 }
 
-pub const fixed_sized_comptime_array_const2 = $if i386 {
+pub const fixed_sized_comptime_array_const2 = $if some_other_define_2 ? {
 	[u8(0x01), 0x02, 0x03]!
-} $else $if amd64 {
+} $else $if some_other_define ? {
 	[u8(0x01), 0x02]!
 } $else {
 	[u8(0)]!
