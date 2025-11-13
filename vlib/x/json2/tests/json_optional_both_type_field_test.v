35ead23a0ac516ac144d3f78be75730c08647a02
Delyan Angelov <delian66@gmail.com>
2025-11-14 00:04:28 +0200

    simplify test

--- vlib/x/json2/tests/json_optional_both_type_field_test.v
+++ vlib/x/json2/tests/json_optional_both_type_field_test.v
@@ -1,6 +1,3 @@
-import x.json2
-import rand
-
 struct Response[T] {
 	code int
 	msg  string
@@ -9,20 +6,18 @@ struct Response[T] {
 
 struct UserInfo {
 	name string
-	age  int @[json: 'type']
+	age  int
 }
 
-fn get_optional_data() ?UserInfo {
-	return if rand.u8() >= 0 { UserInfo{'Jay Chou', 46} } else { none }
+fn g[T](x T) string {
+	return 'abc'
 }
 
 fn response_to_string[T](response Response[T]) string {
-	return json2.encode(response)
+	return g(response)
 }
 
 fn test_main() {
-	data := get_optional_data()
-	response := Response{1, 'success', data}
-	res := response_to_string(response)
-	assert res == '{"code":1,"msg":"success","data":{"name":"Jay Chou","type":46}}'
+	res := response_to_string(Response{1, 'success', ?UserInfo{'Jay Chou', 46}})
+	assert res == 'abc'
 }
