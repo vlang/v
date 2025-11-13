f33caca7eb611a388f1e852e67556612a0657b51
CreeperFace <165158232+dy-tea@users.noreply.github.com>
2025-11-13 08:07:29 +0000

    cgen: correct function definitions for callbacks in imported modules (fix #25700) (#25719)

---
+++ vlib/v/tests/modules/callback_consumer_module/main.v
@@ -0,0 +1,8 @@
+module main
+
+import mod1
+import mod2
+
+fn main() {
+	dump('main')
+}
---
+++ vlib/v/tests/modules/callback_consumer_module/mod1/faulty.v
@@ -0,0 +1,15 @@
+module mod1
+
+pub fn function() ! {
+	callback_consumer(callback_fn)!
+}
+
+type CallbackType = fn () ![4]u8
+
+fn callback_consumer(callback CallbackType) ! {
+	callback()!
+}
+
+fn callback_fn() ![4]u8 {
+	return [u8(1), 2, 3, 255]!
+}
