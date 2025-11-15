f4a8e8478cb9a3f0bd10e9ec3566b37252192e20
Delyan Angelov <delian66@gmail.com>
2025-11-15 07:38:40 +0200

    os: fix `v test pipe_test.c.v`, uncomment more tests (#25733)

--- vlib/os/pipe.c.v
+++ vlib/os/pipe.c.v
@@ -92,16 +92,18 @@ pub fn (mut p Pipe) slurp() []string {
 @[noinit]
 pub struct IOCapture {
 pub mut:
-	stdout &Pipe = unsafe { nil }
-	stderr &Pipe = unsafe { nil }
+	stdout Pipe
+	stderr Pipe
 mut:
 	original_stdout_fd int = -1
 	original_stderr_fd int = -1
 }
 
 // stdio_capture starts capturing stdout and stderr by redirecting them to pipes
-// example:
+// Example:
 // ```v
+//    	import os
+//    	unbuffer_stdout()
 //    	mut cap := os.stdio_capture()!
 //    	println('hello println')
 //    	eprintln('hello eprintln')
@@ -109,13 +111,21 @@ mut:
 //    	sout := cap.stdout.slurp()
 //    	serr := cap.stderr.slurp()
 //    	cap.close()
+//    	assert sout == ['hello println\n']
+//    	assert serr == ['hello eprintln\n']
 // ```
 // or
 // ```v
+//    	import os
+//    	unbuffer_stdout()
 //    	mut cap := os.stdio_capture()!
 //    	println('hello println')
 //    	eprintln('hello eprintln')
+//    	eprintln('World eprintln')
+//    	println('World println')
 //    	sout, serr := cap.finish()
+//    	assert sout == ['hello println\nWorld println\n']
+//    	assert serr == ['hello eprintln\nWorld eprintln\n']
 // ```
 pub fn stdio_capture() !IOCapture {
 	mut c := IOCapture{}
@@ -149,8 +159,8 @@ pub fn stdio_capture() !IOCapture {
 	pipe_stderr.write_fd = -1
 
 	// Store pipes for later reading
-	c.stdout = &pipe_stdout
-	c.stderr = &pipe_stderr
+	c.stdout = pipe_stdout
+	c.stderr = pipe_stderr
 	return c
 }
 
