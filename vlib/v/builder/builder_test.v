73159541a6322f1382a50729cc86a7145de968aa
kbkpbot <kbkpbot@sina.com>
2025-11-16 22:22:08 +0800

    builder,pref: add  support

--- vlib/v/builder/builder_test.v
+++ vlib/v/builder/builder_test.v
@@ -43,3 +43,41 @@ fn test_conditional_executable_removal() {
 	dump(after_second_run___)
 	assert executable in after_second_run___
 }
+
+fn test_file_list() {
+	os.chdir(test_path)!
+	os.mkdir_all('filelist')!
+	os.write_file('filelist/main.v', 'module main
+fn main() {
+        part_a := AS{}
+        part_b := BS{}
+        println("\${part_a}=>\${part_b}")
+}')!
+	os.write_file('filelist/part_a.v', 'module main
+pub struct AS{}
+')!
+
+	os.write_file('filelist/part_b.v', 'module main
+pub struct BS{}
+')!
+
+	// `part_c.v` is not included in the compilation
+	// or there will be a conflit definition of `struct BS`
+	os.write_file('filelist/part_c.v', 'module main
+pub struct BS{}
+')!
+	mut executable := 'filelist_check'
+	$if windows {
+		executable += '.exe'
+	}
+
+	original_file_list_ := os.ls(test_path)!
+	dump(original_file_list_)
+	assert executable !in original_file_list_
+
+	os.execute('${os.quoted_path(vexe)} -o ${executable} filelist/main.v -file-list "filelist/part_a.v;filelist/part_b.v;"')
+	after_compile_file_list := os.ls(test_path)!.filter(os.exists(it))
+	dump(after_compile_file_list)
+	assert executable in after_compile_file_list
+	assert os.execute('./${executable}').output.trim_space() == 'AS{}=>BS{}'
+}
