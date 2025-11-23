09821129372faba4f72025d03f1e34eb2575becd
Linklancien <123779978+Linklancien@users.noreply.github.com>
2025-11-23 11:27:40 +0100

    Specify the differents type in gg.v

--- vlib/gg/gg.v
+++ vlib/gg/gg.v
@@ -5,20 +5,26 @@ module gg
 
 pub type FNCb = fn (data voidptr)
 
+// Define the type of function that can be given to a Config and that will be called on every events
 pub type FNEvent = fn (e &Event, data voidptr)
 
 pub type FNEvent2 = fn (data voidptr, e &Event)
 
 pub type FNFail = fn (msg string, data voidptr)
 
+// Define the type of function that can be given to a Config and that will be called every time a key is pressed down
 pub type FNKeyDown = fn (c KeyCode, m Modifier, data voidptr)
 
+// Define the type of function that can be given to a Config and that will be called every time a key come back tp the up position
 pub type FNKeyUp = fn (c KeyCode, m Modifier, data voidptr)
 
+// Define the type of function that can be given to a Config and that will be called every time the mouse move on the screen
 pub type FNMove = fn (x f32, y f32, data voidptr)
 
+// Define the type of function that can be given to a Config and that will be called every time the mouse is pressed
 pub type FNClick = fn (x f32, y f32, button MouseButton, data voidptr)
 
+// Define the type of function that can be given to a Config and that will be called every time the mouse is released
 pub type FNUnClick = fn (x f32, y f32, button MouseButton, data voidptr)
 
 pub type FNChar = fn (c u32, data voidptr)
