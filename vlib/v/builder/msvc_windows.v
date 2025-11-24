d0dc13ee63884aa7a573dac5b12f3fbd0c30bf62
Delyan Angelov <delian66@gmail.com>
2025-11-24 20:50:26 +0200

    v.builder: bump the default stack size for executables, when compiling with msvc on windows, to 32MB (#25824)

--- vlib/v/builder/msvc_windows.v
+++ vlib/v/builder/msvc_windows.v
@@ -277,8 +277,10 @@ pub fn (mut v Builder) cc_msvc() {
 	// `-w` no warnings
 	// `/we4013` 2 unicode defines, see https://docs.microsoft.com/en-us/cpp/error-messages/compiler-warnings/compiler-warning-level-3-c4013?redirectedfrom=MSDN&view=msvc-170
 	// `/volatile:ms` enables atomic volatile (gcc _Atomic)
-	// `/F 16777216` changes the stack size to 16MB, see https://docs.microsoft.com/en-us/cpp/build/reference/f-set-stack-size?view=msvc-170
-	a << ['-w', '/we4013', '/volatile:ms', '/F 16777216']
+	// `/F33554432` changes the stack size to 32MB, see https://docs.microsoft.com/en-us/cpp/build/reference/f-set-stack-size?view=msvc-170
+	// Note: passing `/FNUMBER` is preferable to `/F NUMBER` for unix shells like bash or in cygwin, that otherwise may treat the `/F` as a folder,
+	// if there is an F: drive in the system (they map c: as /c/, d: as /d/ etc)
+	a << ['-w', '/we4013', '/volatile:ms', '/F33554432']
 	if v.pref.is_prod && !v.pref.no_prod_options {
 		a << '/O2'
 	}
@@ -387,7 +389,7 @@ pub fn (mut v Builder) cc_msvc() {
 		a << v.pref.ldflags.trim_space()
 	}
 	v.dump_c_options(a)
-	args := '\xEF\xBB\xBF' + a.join(' ')
+	args := '\xEF\xBB\xBF' + a.join(' ') // write a BOM to indicate the utf8 encoding of the file
 	// write args to a file so that we dont smash createprocess
 	os.write_file(out_name_cmd_line, args) or {
 		verror('Unable to write response file to "${out_name_cmd_line}"')
