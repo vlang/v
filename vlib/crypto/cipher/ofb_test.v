473c8e8ff268c52d1a948a180b2345249bff5ae6
Mike <45243121+tankf33der@users.noreply.github.com>
2025-11-30 12:57:21 +0200

    crypto.cipher: fix xor_key_stream() for OFB mode, add test (#25844)

--- vlib/crypto/cipher/ofb_test.v
+++ vlib/crypto/cipher/ofb_test.v
@@ -1,11 +1,12 @@
 import crypto.cipher
+import crypto.aes
 import crypto.des
 
 struct StreamCipher {
 	cipher cipher.Stream
 }
 
-fn test_ctr_stream_cipher() ! {
+fn test_ofb_stream_cipher() ! {
 	key := '123456789012345678901234'.bytes()
 	iv := 'abcdegfh'.bytes()
 
@@ -15,4 +16,18 @@ fn test_ctr_stream_cipher() ! {
 	s := StreamCipher{
 		cipher: c
 	}
+}
+
+fn test_ofb_byte_by_byte() {
+	key := []u8{len: 16, init: index}
+	iv := []u8{len: 16, init: index}
+	txt := []u8{len: 32, init: index}
+	mut out := []u8{len: 32}
+
+	mut ofb := cipher.new_ofb(aes.new_cipher(key), iv)
+	for i in 0 .. 32 {
+		ofb.xor_key_stream(mut out[i..i + 1], txt[i..i + 1])
+	}
+	assert out == [u8(10), 149, 9, 182, 69, 107, 246, 66, 249, 202, 158, 83, 202, 94, 228, 85,
+		190, 246, 12, 182, 85, 194, 184, 92, 243, 121, 164, 215, 69, 34, 168, 124]
 }
