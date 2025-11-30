e07eb549e4a6a1c827bccafdc69d66e66490c7d7
Mike <45243121+tankf33der@users.noreply.github.com>
2025-11-30 16:24:39 +0200

    math.complex: fix handling of the argument to the log() method, replace tests (#25863)

--- vlib/math/complex/complex.v
+++ vlib/math/complex/complex.v
@@ -138,7 +138,7 @@ pub fn (c Complex) ln() Complex {
 // Based on
 // http://www.milefoot.com/math/complex/summaryops.htm
 pub fn (c Complex) log(base Complex) Complex {
-	return base.ln().divide(c.ln())
+	return c.ln().divide(base.ln())
 }
 
 // Complex Argument
