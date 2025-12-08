b9f1c386251a174fc2e90c1ad33ec0c4c4d904f5
Nopana_Eliyaan <nopana1007@gmail.com>
2025-12-08 19:24:00 +0100

    not needed

--- vlib/v/gen/c/array.v
+++ vlib/v/gen/c/array.v
@@ -141,7 +141,7 @@ fn (mut g Gen) fixed_array_init(node ast.ArrayInit, array_type Type, var_name st
 
 	if (g.inside_struct_init && g.inside_cast && !g.inside_memset && !g.inside_opt_or_res
 		&& !g.inside_sumtype_cast) || (node.is_option && !is_none)
-		|| (g.inside_call && node.has_val && node.exprs.len > 0) {
+		|| (g.inside_call && node.has_val) {
 		ret_typ_str := g.styp(node.typ)
 		g.write('(${ret_typ_str})')
 	}
