b5aa2a90a119fa613c17e75d728ab02ce92f1192
krchi <krchi@outlook.com>
2025-12-14 17:37:16 +0800

    separate test to a single file

--- vlib/v/tests/fns/call_args_variadic_sumtype_test.v
+++ vlib/v/tests/fns/call_args_variadic_sumtype_test.v
@@ -14,26 +14,6 @@ fn print_names(animals ...Animal) {
 	}
 }
 
-type Any = []Any | int
-
-fn print_any(arr ...Any) {
-	if any := arr[0] {
-		match any {
-			int {
-				assert any == 1
-			}
-			[]Any {
-				assert any == [Any(1)]
-			}
-		}
-	}
-	assert true
-}
-
-fn print_int_any(_ int, arr ...Any) {
-	print_any(...arr)
-}
-
 fn test_main() {
 	cat := Cat{
 		name: 'Kitty'
@@ -41,12 +21,4 @@ fn test_main() {
 	mut animals := []Animal{}
 	animals << cat
 	print_names(...animals)
-
-	print_any(1, Any(1), [Any(1)])
-	print_any(1)
-	print_any(Any(1))
-	print_any([Any(1)])
-	print_any([Any(1)], 1)
-	print_int_any(1, 1)
-	print_int_any(1, [Any(1)])
 }
