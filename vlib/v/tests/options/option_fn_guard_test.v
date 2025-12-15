046c5e0bddc8763d5007187a4d704199d30184c8
Delyan Angelov <delian66@gmail.com>
2025-12-15 09:10:44 +0200

    add more assertions in option_fn_guard_test.v

--- vlib/v/tests/options/option_fn_guard_test.v
+++ vlib/v/tests/options/option_fn_guard_test.v
@@ -1,6 +1,8 @@
 type MyFn = fn ()
 
-fn hello() {}
+fn hello() {
+	println(@FN)
+}
 
 fn get_ptr() ?&MyFn {
 	return &hello
@@ -8,10 +10,12 @@ fn get_ptr() ?&MyFn {
 
 fn test_main() {
 	if p := get_ptr() {
+		assert voidptr(p) == voidptr(hello)
 		dump(p)
 		p()
 	}
 
 	p2 := get_ptr() or { return }
 	p2()
+	assert voidptr(p2) == voidptr(hello)
 }
