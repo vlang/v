73d6a4bd2b58cbf4cf19169101de2863b758024e
kbkpbot <kbkpbot@sina.com>
2025-12-20 22:41:43 +0800

    no cast when both side are bool

--- vlib/v/gen/c/cgen.v
+++ vlib/v/gen/c/cgen.v
@@ -5687,10 +5687,14 @@ fn (mut g Gen) cast_expr(node ast.CastExpr) {
 		if node_typ_is_option {
 			g.expr_with_opt(node.expr, expr_type, node.typ)
 		} else {
-			styp := g.styp(node.typ)
-			g.write('(${styp}[]){(')
-			g.expr(node.expr)
-			g.write(')?1:0}[0]')
+			if node.typ == ast.bool_type && expr_type == ast.bool_type {
+				g.expr(node.expr)
+			} else {
+				styp := g.styp(node.typ)
+				g.write('(${styp}[]){(')
+				g.expr(node.expr)
+				g.write(')?1:0}[0]')
+			}
 		}
 	} else {
 		styp := g.styp(node.typ)
