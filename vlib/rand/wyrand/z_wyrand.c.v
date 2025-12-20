f21cce4b6929222724c3b1097242850c58712620
Mike <45243121+tankf33der@users.noreply.github.com>
2025-12-20 18:18:36 +0200

    rand.wyrand: switch to using the original C._wymix() as a performance optimisation (#26051)

--- vlib/rand/wyrand/z_wyrand.c.v
+++ vlib/rand/wyrand/z_wyrand.c.v
@@ -1,7 +1,21 @@
 module wyrand
 
+fn C._wymix(u64, u64) u64
+
 // free should be called when the generator is no longer needed
 @[unsafe]
 pub fn (mut rng WyRandRNG) free() {
 	unsafe { free(rng) }
+}
+
+// u64 returns a pseudorandom 64bit int in range `[0, 2⁶⁴)`.
+@[ignore_overflow; inline]
+pub fn (mut rng WyRandRNG) u64() u64 {
+	unsafe {
+		mut seed1 := rng.state
+		seed1 += wyp0
+		rng.state = seed1
+		return C._wymix(seed1 ^ wyp1, seed1)
+	}
+	return 0
 }
