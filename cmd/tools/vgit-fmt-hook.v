3a435fab682583d9b816358cbdc5d6e0484b5320
Laurent Cheylus <foxy@free.fr>
2026-01-18 20:29:27 +0100

    tools: add support for vgit-fmt-hook on OpenBSD (fix #26344) (#26388)

--- cmd/tools/vgit-fmt-hook.v
+++ cmd/tools/vgit-fmt-hook.v
@@ -4,12 +4,23 @@ import crypto.sha256
 const vexe = os.getenv_opt('VEXE') or { panic('missing VEXE env variable') }
 const vroot = os.to_slash(os.real_path(os.dir(vexe)))
 const horiginal = os.to_slash(os.join_path(vroot, 'cmd/tools/git_pre_commit_hook.vsh'))
+const shoriginal = os.join_path(os.vtmp_dir(), 'git_pre_commit_hook.sh')
 
 fn get_hook_target(git_folder string) string {
 	return os.to_slash(os.join_path(git_folder, 'hooks/pre-commit'))
 }
 
 fn main() {
+	// On OS without support for 'env -S', generate shell script
+	// to run cmd/tools/git_pre_commit_hook.vsh
+	// TODO: detect other OS (BusyBox) without support for 'env -S'
+	$if openbsd {
+		os.write_file(shoriginal, '#!/bin/sh\nv run ${horiginal}') or {
+			eprintln('unable to write shell script ${shoriginal}')
+			exit(1)
+		}
+		os.chmod(shoriginal, 0o755)!
+	}
 	git_folder := find_nearest_top_level_folder_with_a_git_subfolder(os.getwd()) or {
 		eprintln('This command has to be run inside a Git repository.')
 		exit(0)
@@ -39,9 +50,15 @@ fn cmd_status(htarget string) {
 }
 
 fn cmd_install(htarget string) {
-	report_status(htarget, false)
+	if report_status(htarget, false) {
+		return
+	}
 	println('> Installing the newest version of ${horiginal} over ${htarget} ...')
-	os.cp(horiginal, htarget) or { err_exit('failed to copy to ${htarget}') }
+	$if openbsd {
+		os.cp(shoriginal, htarget) or { err_exit('failed to copy to ${htarget}') }
+	} $else {
+		os.cp(horiginal, htarget) or { err_exit('failed to copy to ${htarget}') }
+	}
 	println('> Done.')
 }
 
@@ -55,25 +72,32 @@ fn cmd_remove(htarget string) {
 	println('> Done.')
 }
 
-fn report_status(htarget string, show_instructions bool) {
-	ostat := os.stat(horiginal) or { os.Stat{} }
+// Returns true if pre-commit Git hook already exists and identical to VSH script
+fn report_status(htarget string, show_instructions bool) bool {
+	mut original := ''
+	$if openbsd {
+		original = shoriginal
+	} $else {
+		original = horiginal
+	}
+	ostat := os.stat(original) or { os.Stat{} }
 	tstat := os.stat(htarget) or { os.Stat{} }
-	ohash := hash_file(horiginal) or { '' }
+	ohash := hash_file(original) or { '' }
 	thash := hash_file(htarget) or { '' }
 	if os.exists(htarget) && os.is_file(htarget) {
 		println('>   CURRENT git repo pre-commit hook: size: ${tstat.size:6} bytes, sha256: ${thash}, ${htarget}')
 	} else {
 		println('>   CURRENT git repo pre-commit hook: missing ${htarget}')
 	}
-	if os.exists(horiginal) && os.is_file(horiginal) {
-		println('> Main V repo pre-commit hook script: size: ${ostat.size:6} bytes, sha256: ${ohash}, ${horiginal}')
+	if os.exists(original) && os.is_file(original) {
+		println('> Main V repo pre-commit hook script: size: ${ostat.size:6} bytes, sha256: ${ohash}, ${original}')
 	}
 	if ohash == thash {
 		println('> Both files are exactly the same.')
 		if show_instructions {
 			show_msg_about_removing(htarget)
 		}
-		return
+		return true
 	}
 	println('> Files have different hashes.')
 	if ohash != '' && thash != '' {
@@ -88,6 +112,7 @@ fn report_status(htarget string, show_instructions bool) {
 		println('> with the newest pre-commit formatting script from the main V repo.')
 		show_msg_about_removing(htarget)
 	}
+	return false
 }
 
 fn show_msg_about_removing(htarget string) {
