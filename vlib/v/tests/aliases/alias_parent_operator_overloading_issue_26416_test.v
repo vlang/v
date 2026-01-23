vlang/v

commit 29e60da6143da14f57e8740ed301cc3ff6f84a1a

checker,cgen: implement alias operator overloading for generic struct parent type (fix #23965) (#23967)

vlib/v/gen/c/assign.v
@@ -674,6 +674,19 @@ fn (mut g Gen) assign_stmt(node_ ast.AssignStmt) {
 						g.write(';')
 					}
 					return
+				} else if left_sym.kind == .alias && g.table.final_sym(var_type).kind == .struct {
+					struct_info := g.table.final_sym(var_type)
+					if struct_info.info is ast.Struct && struct_info.info.generic_types.len > 0 {
+						mut method_name := struct_info.cname + '_' + util.replace_op(extracted_op)
+						method_name = g.generic_fn_name(struct_info.info.concrete_types,
+							method_name)
+						g.write(' = ${method_name}(')
+						g.expr(left)
+						g.write(', ')
+						g.expr(val)
+						g.writeln(');')
+						return
+					}
 				} else {
 					if g.table.final_sym(g.unwrap_generic(var_type)).kind == .array_fixed {
 						g.go_back_to(pos)

vlib/v/checker/assign.v
@@ -797,19 +797,27 @@ or use an explicit `unsafe{ a[..] }`, if you do not want a copy of the slice.',
 			if left_sym.kind == .struct && (left_sym.info as ast.Struct).generic_types.len > 0 {
 				continue
 			}
-			if method := left_sym.find_method(extracted_op) {
+			if method := left_sym.find_method_with_generic_parent(extracted_op) {
 				if method.return_type != left_type_unwrapped {
 					c.error('operator `${extracted_op}` must return `${left_name}` to be used as an assignment operator',
 						node.pos)
 				}
 			} else {
-				if !parent_sym.is_primitive() {
-					if left_name == right_name {
-						c.error('undefined operation `${left_name}` ${extracted_op} `${right_name}`',
+				if method := parent_sym.find_method_with_generic_parent(extracted_op) {
+					if parent_sym.kind == .alias
+						&& (parent_sym.info as ast.Alias).parent_type != method.return_type {
+						c.error('operator `${extracted_op}` must return `${left_name}` to be used as an assignment operator',
 							node.pos)
-					} else {
-						c.error('mismatched types `${left_name}` and `${right_name}`',
-							node.pos)
+					}
+				} else {
+					if !parent_sym.is_primitive() {
+						if left_name == right_name {
+							c.error('undefined operation `${left_name}` ${extracted_op} `${right_name}`',
+								node.pos)
+						} else {
+							c.error('mismatched types `${left_name}` and `${right_name}`',
+								node.pos)
+						}
 					}
 				}
 			}

vlib/v/tests/aliases/alias_generic_parent_operator_overloading_test.v
@@ -1,0 +1,18 @@
+import math.vec
+
+type V2 = vec.Vec2[f32]
+
+fn test_alias_generic_parent_operator_overloading() {
+	a := V2{1, 1}
+	b := V2{10, 20}
+
+	mut c := a + b
+	assert c.x == 11.0
+	assert c.y == 21.0
+	dump(c)
+
+	c += a
+	assert c.x == 12.0
+	assert c.y == 22.0
+	dump(c)
+}

commit c2a64e2fd76d77383c9a0253b4d4876c790898e4

cgen: fix `+=` overloading for aliases, regression after 29e60da #23967 (fix #26416) (#26423)

vlib/v/gen/c/assign.v
@@ -704,6 +704,7 @@ fn (mut g Gen) assign_stmt(node_ ast.AssignStmt) {
 				}
 				pos := g.out.len
 				g.expr(left)
+				struct_info := g.table.final_sym(var_type)
 				if left_sym.info is ast.Struct && left_sym.info.generic_types.len > 0 {
 					concrete_types := left_sym.info.concrete_types
 					mut method_name := left_sym.cname + '_' + util.replace_op(extracted_op)
@@ -725,19 +726,16 @@ fn (mut g Gen) assign_stmt(node_ ast.AssignStmt) {
 						g.write(';')
 					}
 					return
-				} else if left_sym.kind == .alias && g.table.final_sym(var_type).kind == .struct {
-					struct_info := g.table.final_sym(var_type)
-					if struct_info.info is ast.Struct && struct_info.info.generic_types.len > 0 {
-						mut method_name := struct_info.cname + '_' + util.replace_op(extracted_op)
-						method_name = g.generic_fn_name(struct_info.info.concrete_types,
-							method_name)
-						g.write(' = ${method_name}(')
-						g.expr(left)
-						g.write(', ')
-						g.expr(val)
-						g.writeln(');')
-						return
-					}
+				} else if left_sym.kind == .alias && struct_info.kind == .struct
+					&& struct_info.info is ast.Struct && struct_info.info.generic_types.len > 0 {
+					mut method_name := struct_info.cname + '_' + util.replace_op(extracted_op)
+					method_name = g.generic_fn_name(struct_info.info.concrete_types, method_name)
+					g.write(' = ${method_name}(')
+					g.expr(left)
+					g.write(', ')
+					g.expr(val)
+					g.writeln(');')
+					return
 				} else {
 					if g.table.final_sym(g.unwrap_generic(var_type)).kind == .array_fixed {
 						g.go_back_to(pos)

vlib/v/tests/aliases/alias_parent_operator_overloading_issue_26416_test.v
@@ -1,0 +1,31 @@
+pub struct Vec3 {
+pub:
+	x f64
+	y f64
+	z f64
+}
+
+pub fn (u Vec3) + (v Vec3) Vec3 {
+	return Vec3{u.x + v.x, u.y + v.y, u.z + v.z}
+}
+
+pub type Color3 = Vec3
+
+pub fn (u Color3) + (v Color3) Color3 {
+	return Color3{100 + u.x + v.x, 200 + u.y + v.y, 300 + u.z + v.z}
+}
+
+fn color(x int) Color3 {
+	return Color3{1, 2, 3}
+}
+
+fn test_main() {
+	mut c := Color3{1, 2, 3}
+	for _ in 0 .. 3 {
+		c += color(123)
+	}
+	println(c)
+	assert c.x == 304.0 // make sure that the overloaded + for Color3 was called, and NOT the overloaded + for Vec3.
+	assert c.y == 608.0
+	assert c.z == 912.0
+}