ba4e96df93d84bced2dbab9aacfadeef55c68334
Delyan Angelov <delian66@gmail.com>
2026-01-23 12:39:15 +0200

    change assertions, to make sure that the correct overloaded + was called

--- vlib/v/tests/aliases/alias_parent_operator_overloading_issue_26416_test.v
+++ vlib/v/tests/aliases/alias_parent_operator_overloading_issue_26416_test.v
@@ -12,22 +12,20 @@ pub fn (u Vec3) + (v Vec3) Vec3 {
 pub type Color3 = Vec3
 
 pub fn (u Color3) + (v Color3) Color3 {
-	return Color3{u.x + v.x, u.y + v.y, u.z + v.z}
+	return Color3{100 + u.x + v.x, 200 + u.y + v.y, 300 + u.z + v.z}
 }
 
-pub const black = Color3{1, 2, 3}
-
 fn color(x int) Color3 {
-	return black
+	return Color3{1, 2, 3}
 }
 
 fn test_main() {
-	mut c := black
+	mut c := Color3{1, 2, 3}
 	for _ in 0 .. 3 {
 		c += color(123)
 	}
 	println(c)
-	assert c.x == 4.0
-	assert c.y == 8.0
-	assert c.z == 12.0
+	assert c.x == 304.0 // make sure that the overloaded + for Color3 was called, and NOT the overloaded + for Vec3.
+	assert c.y == 608.0
+	assert c.z == 912.0
 }
