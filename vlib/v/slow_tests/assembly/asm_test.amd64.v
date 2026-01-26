c5c21d07fb110c519e7e22630314d73c81919696
D.Angelov <delian66@gmail.com>
2026-01-26 22:11:22 +0000

    ci: extract tests that tcc can not compile from asm_test.amd64.v to asm_no_tcc_test.amd64.v

--- vlib/v/slow_tests/assembly/asm_test.amd64.v
+++ vlib/v/slow_tests/assembly/asm_test.amd64.v
@@ -110,26 +110,6 @@ fn test_inline_asm() {
 		; ; eax
 	}
 	assert o.str()[0].is_capital()
-
-	x := u64(100)
-	y := u64(200)
-	mut hi := u64(0)
-	mut lo := u64(0)
-	asm amd64 {
-		mov rax, x
-		mulq y
-		mov lo, rax
-		mov hi, rdx
-		; =*m (lo)
-		  =&r (hi)
-		; rm (x)
-		  r (y)
-		; rax
-		  rdx
-		  cc
-	}
-	assert hi == 0
-	assert lo == 20000
 }
 
 @[packed]
@@ -193,46 +173,6 @@ $if !macos {
 		byte_sequence:
 		.byte 0x27, 0x35, 0x0f, 0x48
 	}
-}
-
-fn test_flag_output() {
-	a, b := 4, 9
-	mut out := false
-	asm amd64 {
-		cmp a, b
-		; =@ccl (out)
-		; r (a)
-		  r (b)
-	}
-	assert out
-	asm amd64 {
-		cmp b, a
-		; =@ccl (out)
-		; r (a)
-		  r (b)
-	}
-	assert !out
-
-	zero := 0
-	asm amd64 {
-		cmp zero, zero
-		; =@ccz (out)
-		; r (zero)
-	}
-	assert out
-
-	mut maybe_four := 4
-	mut four := 4
-	asm amd64 {
-		subl four, maybe_four
-		testl four, maybe_four
-		movl maybe_four, 9
-		; +m (maybe_four)
-		  +r (four)
-		  =@ccz (out)
-	}
-	assert out
-	assert maybe_four == 9
 }
 
 fn test_asm_generic() {
