185bbd0aa0b3910d1f118187399c8ee3a915258b
Delyan Angelov <delian66@gmail.com>
2026-01-27 15:20:41 +0200

    ci: revert the changes in f665055 for vlib/v/tests/printing_c_structs/ too

--- vlib/v/tests/printing_c_structs/string_interpolation_test.c.v
+++ vlib/v/tests/printing_c_structs/string_interpolation_test.c.v
@@ -24,30 +24,3 @@ fn test_interpolation_of_v_structs_containing_c_structs() {
     }
 }'
 }
-
-// Test for C structs where voidptr fields in V have different actual C types.
-// This simulates the FreeType case where FT_Generic/FT_BBox are structs in C
-// but may be declared as voidptr in V bindings.
-pub struct C.OuterStruct {
-	id    int
-	inner voidptr // Declared as voidptr in V, but actual C type is struct InnerStruct
-}
-
-struct VStructWithCStruct {
-	outer C.OuterStruct
-}
-
-fn test_c_struct_with_voidptr_field_that_is_actually_struct() {
-	// This test verifies that printing a C struct with a voidptr field
-	// that is actually a struct in C does not cause C compilation errors.
-	outer := C.OuterStruct{
-		id: 42
-	}
-	wrapper := VStructWithCStruct{
-		outer: outer
-	}
-	s := wrapper.str()
-	// The voidptr field should be printed as <cptr> placeholder
-	assert s.contains('id: 42')
-	assert s.contains('inner: <cptr>')
-}
