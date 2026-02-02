e8ad4adb65135a1aa61320619ad599fddf3c0d45
Delyan Angelov <delian66@gmail.com>
2026-02-02 22:01:49 +0200

    checker: fix sql statement using an unwrapped option value from an if guard (fix #26496) (using codex) (#26505)

---
+++ vlib/db/sqlite/sqlite_orm_option_field_test.v
@@ -0,0 +1,26 @@
+// vtest build: present_sqlite3? && !windows && !sanitize-memory-clang && !docker-ubuntu-musl
+import db.sqlite
+
+@[table: 'foos']
+struct Foo {
+	value int
+}
+
+struct State {
+	val ?int
+}
+
+fn test_main() {
+	val := ?int(none)
+	state := State{val}
+	db := sqlite.connect(':memory:')!
+	if state.val != none {
+		v := sql db {
+			select from Foo where value < state.val
+		}!
+		println(v)
+		assert false
+	} else {
+		assert true
+	}
+}
