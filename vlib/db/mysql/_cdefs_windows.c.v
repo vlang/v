62801fcdacf72a64cfbb3f29ee928946ed0e2744
Simen Wiik <simenwiik@hotmail.com>
2026-02-07 18:14:16 +0100

    mysql: use correct link flag for both tcc and msvc (#26539)

--- vlib/db/mysql/_cdefs_windows.c.v
+++ vlib/db/mysql/_cdefs_windows.c.v
@@ -1,5 +1,11 @@
 module mysql
 
 #flag windows -I@VEXEROOT/thirdparty/mysql/include
-#flag windows @VEXEROOT/thirdparty/mysql/lib/libmysql.dll
-#include <mysql.h> # Please install https://dev.mysql.com/downloads/installer/ , then put the include/ and lib/ folders in thirdparty/mysql
+
+$if tinyc {
+	#flag windows @VEXEROOT/thirdparty/mysql/lib/libmysql.dll
+} $else {
+	#flag windows @VEXEROOT/thirdparty/mysql/lib/libmysql.lib
+}
+
+#include <mysql.h> # Please install https://dev.mysql.com/downloads/installer/ , then put the include/ and lib/ folders in thirdparty/mysql. Do not forget to add thirdparty/mysql/bin and thirdparty/mysql/lib to PATH
