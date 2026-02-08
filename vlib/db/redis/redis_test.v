57c0c59b89fa198f5ac1ef73abe24cca51656956
Delyan Angelov <delian66@gmail.com>
2026-02-08 09:20:45 +0200

    apply suggested change from last review

--- vlib/db/redis/redis_test.v
+++ vlib/db/redis/redis_test.v
@@ -397,7 +397,7 @@ fn test_fuzz_random_binary_many() ! {
 	for iter in 0 .. count {
 		// size up to ~200 KiB
 		size := int((x % 200_000) + 1)
-		mut data := []u8{len: size, init: rand.u8()}
+		mut data := []u8{len: size, init: rand.u8() + 0 * index}
 		key := 'fuzz_big_${iter}'
 		assert db.set(key, data.clone())! == 'OK'
 		got := db.get[[]u8](key)!
@@ -424,7 +424,7 @@ fn test_fuzz_crlf_random_positions() ! {
 	for t in 0 .. trials {
 		// size between 1 KiB and 32 KiB
 		size := int((x % 31_744) + 1024)
-		mut data := []u8{len: size, init: rand.u8()}
+		mut data := []u8{len: size, init: rand.u8() + 0 * index}
 		// place a CRLF at a pseudo-random position; sometimes force it near boundaries
 		pos := int(x % u64(size - 2))
 		data[pos] = `\r`
@@ -484,7 +484,7 @@ fn test_fuzz_random_binary_small() {
 	for iter := 0; iter < 20; iter++ {
 		// pseudo-random size in [1, 4096]
 		size := int((x % 4096) + 1)
-		mut data := []u8{len: size, init: rand.u32()}
+		mut data := []u8{len: size, init: rand.u32() + 0 * index}
 		k := 'fuzz_small_${iter}'
 		assert db.set(k, data.clone())! == 'OK'
 		got := db.get[[]u8](k)!
