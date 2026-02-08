d1ed08f403f65f22cdd773ace8aafa4f582b7b5f
Delyan Angelov <delian66@gmail.com>
2026-02-08 12:04:52 +0200

    regex.pcre: fix test, for systems that also have done `v install pcre`

--- vlib/regex/pcre/regex_test.v
+++ vlib/regex/pcre/regex_test.v
@@ -5,7 +5,7 @@ Copyright (c) 2026 Dario Deledda. All rights reserved.
 Use of this source code is governed by an MIT license
 that can be found in the LICENSE file.
 */
-import pcre
+import regex.pcre
 
 fn main() {
 	println('Running pcre tests...\n')
