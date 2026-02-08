ee2b5d714c4e19228a3d26b1105bda3cd1f489b0
Delyan Angelov <delian66@gmail.com>
2026-02-08 22:57:41 +0200

    ci: fix `./v test vlib/v/generics/new_generics_regression_test.v`

--- vlib/v/generics/new_generics_regression_test.v
+++ vlib/v/generics/new_generics_regression_test.v
@@ -18,8 +18,11 @@ const vroot = @VEXEROOT
 const vtrace_output = os.getenv('VTRACE_OUTPUT').int() != 0
 
 fn testsuite_begin() {
-	os.chdir(@VEXEROOT)!
+	unbuffer_stdout()
+	os.chdir(vroot)!
 	os.setenv('VJOBS', '1', true)
+	// important, when this test itself is run through `v test`; this simplify the handling of the output of the inner `v test` commands
+	os.setenv('VCOLORS', 'never', true)
 }
 
 fn test_new_generic_solver_does_not_regress_silently() {
