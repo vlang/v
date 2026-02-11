e48c15caea52e5314d499c2a205b8cbe16886aa3
Maokaman1 <maokaman@gmail.com>
2026-02-11 17:01:42 +0300

    Fix broken `trace_ssl` flag.

--- vlib/net/mbedtls/ssl_connection.c.v
+++ vlib/net/mbedtls/ssl_connection.c.v
@@ -586,7 +586,7 @@ pub fn (mut s SSLConn) socket_read_into_ptr(buf_ptr &u8, len int) !int {
 				}
 				C.MBEDTLS_ERR_SSL_TIMEOUT {
 					$if trace_ssl ? {
-						eprintln('${@METHOD} ---> res: ${err} C.MBEDTLS_ERR_SSL_TIMEOUT')
+						eprintln('${@METHOD} ---> res: C.MBEDTLS_ERR_SSL_TIMEOUT')
 					}
 					return error_with_code('net.mbedtls SSLConn.socket_read_into_ptr, did not receive any data within ${mbedtls_client_read_timeout_ms}ms. compile with `-d mbedtls_client_read_timeout_ms=100000` to set a higher timeout',
 						res)
