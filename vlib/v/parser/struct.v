3aa627339b3c8907107f5e984f711320b04b64b7
CreeperFace <165158232+dy-tea@users.noreply.github.com>
2026-02-12 06:42:55 +0000

    parser: fix `pub:` not being detected in structs in -vls-mode (#26581)

--- vlib/v/parser/struct.v
+++ vlib/v/parser/struct.v
@@ -143,7 +143,8 @@ fn (mut p Parser) struct_decl(is_anon bool) ast.StructDecl {
 		}
 		p.check(.lcbr)
 		// if p.is_vls && p.tok.kind == .key_struct { // p.tok.is_key() {
-		if p.is_vls && p.tok.is_key() && p.tok.kind != .key_mut {
+		if p.is_vls && p.tok.is_key() && !(p.tok.kind in [.key_pub, .key_mut]
+			&& p.peek_tok.kind in [.colon, .key_mut]) {
 			// End parsing after `struct Foo {` in vls mode to avoid lots of junk errors
 			// If next token after { is a key, the struct wasn't finished
 			p.error('expected `}` to finish a struct definition')
