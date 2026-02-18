vlang/v

commit 9831e7c4350d66fba2a1e500a9966422260bc703

v2: top level declarations inside $if

vlib/os/os.c.v
@@ -6,9 +6,7 @@ import strings
 #include <errno.h>
 
 $if macos {
-	#include <mach-o/dyld.h>
-	// needed for os.executable():
-	fn C._dyld_get_image_name(image u32) &char
+	#include <libproc.h>
 }
 
 $if freebsd || openbsd {
@@ -767,11 +765,14 @@ pub fn executable() string {
 		return res
 	}
 	$if macos {
-		self_path := &char(C._dyld_get_image_name(u32(0)))
-		if self_path == C.NULL {
+		pid := C.getpid()
+		ret := C.proc_pidpath(pid, &result[0], max_path_len)
+		if ret <= 0 {
+			eprintln('os.executable() failed at calling proc_pidpath with pid: ${pid} . proc_pidpath returned ${ret} ')
 			return executable_fallback()
 		}
-		return unsafe { cstring_to_vstring(self_path) }
+		res := unsafe { tos_clone(&result[0]) }
+		return res
 	}
 	$if freebsd {
 		bufsize := usize(max_path_buffer_size)

vlib/v2/gen/cleanc/types.v
@@ -1696,56 +1696,88 @@ fn (mut g Gen) get_raw_type(node ast.Expr) ?types.Type {
 // Returns none if the name is not a known primitive type.
 fn resolve_primitive_type_name(name string) ?types.Type {
 	return match name {
-		'int' { types.Type(types.int_) }
-		'i8' { types.Type(types.Primitive{
+		'int' {
+			types.Type(types.int_)
+		}
+		'i8' {
+			types.Type(types.Primitive{
 				size:  8
 				props: .integer
-			}) }
-		'i16' { types.Type(types.Primitive{
+			})
+		}
+		'i16' {
+			types.Type(types.Primitive{
 				size:  16
 				props: .integer
-			}) }
-		'i32' { types.Type(types.Primitive{
+			})
+		}
+		'i32' {
+			types.Type(types.Primitive{
 				size:  32
 				props: .integer
-			}) }
-		'i64' { types.Type(types.Primitive{
+			})
+		}
+		'i64' {
+			types.Type(types.Primitive{
 				size:  64
 				props: .integer
-			}) }
-		'u8', 'byte' { types.Type(types.Primitive{
+			})
+		}
+		'u8', 'byte' {
+			types.Type(types.Primitive{
 				size:  8
 				props: .integer | .unsigned
-			}) }
-		'u16' { types.Type(types.Primitive{
+			})
+		}
+		'u16' {
+			types.Type(types.Primitive{
 				size:  16
 				props: .integer | .unsigned
-			}) }
-		'u32' { types.Type(types.Primitive{
+			})
+		}
+		'u32' {
+			types.Type(types.Primitive{
 				size:  32
 				props: .integer | .unsigned
-			}) }
-		'u64' { types.Type(types.Primitive{
+			})
+		}
+		'u64' {
+			types.Type(types.Primitive{
 				size:  64
 				props: .integer | .unsigned
-			}) }
-		'f32' { types.Type(types.Primitive{
+			})
+		}
+		'f32' {
+			types.Type(types.Primitive{
 				size:  32
 				props: .float
-			}) }
-		'f64' { types.Type(types.Primitive{
+			})
+		}
+		'f64' {
+			types.Type(types.Primitive{
 				size:  64
 				props: .float
-			}) }
-		'bool' { types.Type(types.bool_) }
-		'string' { types.Type(types.string_) }
-		'int_literal' { types.Type(types.Primitive{
+			})
+		}
+		'bool' {
+			types.Type(types.bool_)
+		}
+		'string' {
+			types.Type(types.string_)
+		}
+		'int_literal' {
+			types.Type(types.Primitive{
 				props: .untyped | .integer
-			}) }
-		'float_literal' { types.Type(types.Primitive{
+			})
+		}
+		'float_literal' {
+			types.Type(types.Primitive{
 				props: .untyped | .float
-			}) }
-		else { none }
+			})
+		}
+		else {
+			none
+		}
 	}
 }
 

vlib/v2/parser/parser.v
@@ -216,6 +216,47 @@ fn (mut p Parser) stmt() ast.Stmt {
 		.hash {
 			return p.directive()
 		}
+		// Top-level declarations that can appear inside comptime $if blocks.
+		.key_const {
+			return p.const_decl(false)
+		}
+		.key_enum {
+			return p.enum_decl(false, [])
+		}
+		.key_fn {
+			// `fn name(...)` or `fn C.name(...)` is a declaration;
+			// `fn (...)` or `fn [captures](...)` is a literal (handled by else/expr).
+			if p.peek() == .name {
+				return p.fn_decl(false, [])
+			}
+			// fall through to expression (fn literal)
+			expr := p.expr(.lowest)
+			return p.complete_simple_stmt(expr, false)
+		}
+		.key_global {
+			return p.global_decl([])
+		}
+		.key_interface {
+			return p.interface_decl(false, [])
+		}
+		.key_pub {
+			p.next()
+			match p.tok {
+				.key_const { return p.const_decl(true) }
+				.key_enum { return p.enum_decl(true, []) }
+				.key_fn { return p.fn_decl(true, []) }
+				.key_interface { return p.interface_decl(true, []) }
+				.key_struct, .key_union { return p.struct_decl(true, []) }
+				.key_type { return p.type_decl(true) }
+				else { p.error('not implemented: pub ${p.tok}') }
+			}
+		}
+		.key_struct, .key_union {
+			return p.struct_decl(false, [])
+		}
+		.key_type {
+			return p.type_decl(false)
+		}
 		.key_asm {
 			return p.asm_stmt()
 		}

commit c468cfeee67fc646494f602da2109d84ad2a1e74

os: re-apply changes from 39d8433  (pr #26617) after regression caused by 9831e7c (#26626)

vlib/os/os.c.v
@@ -6,7 +6,9 @@ import strings
 #include <errno.h>
 
 $if macos {
-	#include <libproc.h>
+	#include <mach-o/dyld.h>
+	// needed for os.executable():
+	fn C._dyld_get_image_name(image u32) &char
 }
 
 $if freebsd || openbsd {
@@ -765,14 +767,11 @@ pub fn executable() string {
 		return res
 	}
 	$if macos {
-		pid := C.getpid()
-		ret := C.proc_pidpath(pid, &result[0], max_path_len)
-		if ret <= 0 {
-			eprintln('os.executable() failed at calling proc_pidpath with pid: ${pid} . proc_pidpath returned ${ret} ')
+		self_path := &char(C._dyld_get_image_name(u32(0)))
+		if self_path == C.NULL {
 			return executable_fallback()
 		}
-		res := unsafe { tos_clone(&result[0]) }
-		return res
+		return unsafe { cstring_to_vstring(self_path) }
 	}
 	$if freebsd {
 		bufsize := usize(max_path_buffer_size)