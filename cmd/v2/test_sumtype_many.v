705e9fc0fe2d2eb32617086578a022aec1a38c9f
Alexander Medvednikov <alexander@medvednikov.com>
2026-03-01 00:19:50 +0300

    v2: sum type test

--- cmd/v2/test_sumtype_many.v
+++ cmd/v2/test_sumtype_many.v
@@ -3,21 +3,79 @@ struct Pos {
 	y int
 }
 
-struct V0 { pos Pos; a int }
-struct V1 { pos Pos; b int }
-struct V2 { pos Pos; c int }
-struct V3 { pos Pos; d int }
-struct V4 { pos Pos; e int }
-struct V5 { pos Pos; f int }
-struct V6 { pos Pos; g int }
-struct V7 { pos Pos; h int }
-struct V8 { dummy u8 }        // EmptyExpr equivalent (tag 8)
-struct V9 { pos Pos; j int }
-struct V10 { pos Pos; k int }
-struct V11 { pos Pos; l int }
-struct V12 { pos Pos; m int }
-struct V13 { pos Pos; name string }  // Ident equivalent (tag 13)
-struct V14 { pos Pos; n int }
+struct V0 {
+	pos Pos
+	a   int
+}
+
+struct V1 {
+	pos Pos
+	b   int
+}
+
+struct V2 {
+	pos Pos
+	c   int
+}
+
+struct V3 {
+	pos Pos
+	d   int
+}
+
+struct V4 {
+	pos Pos
+	e   int
+}
+
+struct V5 {
+	pos Pos
+	f   int
+}
+
+struct V6 {
+	pos Pos
+	g   int
+}
+
+struct V7 {
+	pos Pos
+	h   int
+}
+
+struct V8 {
+	dummy u8
+} // EmptyExpr equivalent (tag 8)
+
+struct V9 {
+	pos Pos
+	j   int
+}
+
+struct V10 {
+	pos Pos
+	k   int
+}
+
+struct V11 {
+	pos Pos
+	l   int
+}
+
+struct V12 {
+	pos Pos
+	m   int
+}
+
+struct V13 {
+	pos  Pos
+	name string
+} // Ident equivalent (tag 13)
+
+struct V14 {
+	pos Pos
+	n   int
+}
 
 type Expr = V0 | V1 | V2 | V3 | V4 | V5 | V6 | V7 | V8 | V9 | V10 | V11 | V12 | V13 | V14
 
@@ -43,7 +101,10 @@ fn (e Expr) get_pos() Pos {
 
 fn make_v13() Expr {
 	return Expr(V13{
-		pos:  Pos{x: 42, y: 99}
+		pos:  Pos{
+			x: 42
+			y: 99
+		}
 		name: 'hello'
 	})
 }
