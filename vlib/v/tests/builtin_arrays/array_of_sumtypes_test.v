098f7a4b31c17c86bea0f23630f484a47f38faad
Alexander Medvednikov <alexander@medvednikov.com>
2026-03-11 12:55:58 +0300

    error: field does not exist with embedded structure and sumt... (fixes #11290)

--- vlib/v/tests/builtin_arrays/array_of_sumtypes_test.v
+++ vlib/v/tests/builtin_arrays/array_of_sumtypes_test.v
@@ -9,6 +9,16 @@ struct Text {
 
 type Content = Node | Text
 
+struct Left {
+	error string
+}
+
+struct Right {
+	value int
+}
+
+type Either = Left | Right
+
 fn test_push_prepend_insert() {
 	mut body := Node{}
 	body.content << Node{
@@ -17,4 +27,31 @@ fn test_push_prepend_insert() {
 	body.content.prepend(Node{ tag: 'b' })
 	body.content.insert(1, Node{ tag: 'c' })
 	assert body.content.len == 3
+}
+
+fn test_nested_selector_access_on_sumtype_array_elements() {
+	mut body := Node{
+		tag: 'body'
+	}
+	body.content << Node{
+		tag: 'p'
+	}
+	body.content[0].content << Node{
+		tag: 'i'
+	}
+	body.content[0].content[0].content << Node{
+		tag: 'j'
+	}
+	body.content[0].content[0].content[0].content << Node{
+		tag: 'k'
+	}
+	body.content[0].content[0].content[0].content[0].content << Text{}
+	assert ((body.content[0] as Node).content[0] as Node).content.len == 1
+}
+
+fn test_selector_access_on_indexed_sumtype_variants() {
+	items := [Either(Left{
+		error: 'boom'
+	})]
+	assert items[0].error == 'boom'
 }
