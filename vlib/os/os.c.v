cf480bb23b6291b988f175b14e579bd0dbad31e8
Alexander Medvednikov <alexander@medvednikov.com>
2026-03-12 01:18:25 +0300

    error on reading input from shell pipe (fixes #24701)

--- vlib/os/os.c.v
+++ vlib/os/os.c.v
@@ -667,22 +667,15 @@ pub fn get_raw_line() string {
 
 		mut str := ''
 		nr_chars := unsafe { C.getline(voidptr(&buf), &max, C.stdin) }
-		// On OpenBSD, buf=0 for EOF =>  panic when calling tos function
-		$if openbsd {
-			if nr_chars != -1 {
-				str = unsafe { tos(buf, nr_chars) }
-			} else {
-				if int(C.feof(C.stdin)) == 0 && int(C.ferror(C.stdin)) != 0 {
-					panic('get_raw_line(): error to read string')
-				}
-			}
-		} $else {
-			str = unsafe { tos(buf, if nr_chars < 0 { 0 } else { nr_chars }) }
+		if nr_chars >= 0 && buf != 0 {
+			str = unsafe { tos(buf, nr_chars) }
+		} else if int(C.feof(C.stdin)) == 0 && int(C.ferror(C.stdin)) != 0 {
+			panic('get_raw_line(): error reading from stdin')
 		}
 		ret := str.clone()
 		$if !autofree {
 			unsafe {
-				if nr_chars > 0 && buf != 0 {
+				if buf != 0 {
 					C.free(buf)
 				}
 			}
