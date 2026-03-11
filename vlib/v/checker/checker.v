d23a7e245259362c009b50d6cb4b49bb295f5887
Alexander Medvednikov <alexander@medvednikov.com>
2026-03-11 19:08:08 +0300

    improve perf

--- vlib/v/checker/checker.v
+++ vlib/v/checker/checker.v
@@ -6383,8 +6383,10 @@ fn (mut c Checker) ensure_type_exists(typ ast.Type, pos token.Pos) bool {
 		}
 		else {}
 	}
-	if !c.ensure_supported_map_key_types(typ, pos) {
-		return c.pref.is_vls
+	if sym.kind == .map {
+		if !c.ensure_supported_map_key_types(typ, pos) {
+			return c.pref.is_vls
+		}
 	}
 	return true
 }
