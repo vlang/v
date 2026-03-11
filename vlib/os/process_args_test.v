476e1fc1dc0d031e851277bd58bca77e720a45d7
Alexander Medvednikov <alexander@medvednikov.com>
2026-03-11 13:50:59 +0300

    os: just installed Vlang on system and when running program it's shows failed createprocess:%1 is not valid win32 application (fixes #14579)

--- vlib/os/process_args_test.v
+++ vlib/os/process_args_test.v
@@ -18,3 +18,19 @@ fn test_v_run_quoted_args_with_spaces() {
 	assert res.exit_code == 0
 	assert res.output.starts_with("['1', 'Learn V', '3']")
 }
+
+fn test_v_run_file_from_path_with_spaces() {
+	echo_os_args := os.join_path(vroot, 'cmd/tools/test_os_args.v')
+	spaced_dir := os.join_path(os.vtmp_dir(), 'v run path with spaces')
+	spaced_echo_os_args := os.join_path(spaced_dir, 'test os args.v')
+	os.rmdir_all(spaced_dir) or {}
+	os.mkdir_all(spaced_dir)!
+	defer {
+		os.rmdir_all(spaced_dir) or {}
+	}
+	os.cp(echo_os_args, spaced_echo_os_args)!
+	res := os.execute('${os.quoted_path(vexe)} run ${os.quoted_path(spaced_echo_os_args)} 1 "Learn V" 3')
+	println(res)
+	assert res.exit_code == 0
+	assert res.output.starts_with("['1', 'Learn V', '3']")
+}
