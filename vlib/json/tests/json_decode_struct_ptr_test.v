a5d9916e4fa51d320594a3a9e234ee704e7a3547
Alexander Medvednikov <alexander@medvednikov.com>
2026-03-12 11:01:44 +0300

    json.decode() segfaults on recursive struct with ?&t (fixes #26385)

--- vlib/json/tests/json_decode_struct_ptr_test.v
+++ vlib/json/tests/json_decode_struct_ptr_test.v
@@ -7,6 +7,13 @@ mut:
 	reply_to &Message
 }
 
+struct OptionalMessage {
+mut:
+	id       int
+	text     string
+	reply_to ?&OptionalMessage
+}
+
 fn test_main() {
 	mut json_data := '{"id": 1, "text": "Hello", "reply_to": {"id": 2, "text": "Hi"}}'
 	mut message := json.decode(Message, json_data)!
@@ -19,4 +26,17 @@ fn test_main() {
 	json_data = '{"id": 1, "text": "Hello", "reply_to": {"id": 2, "text": "Hi", "reply_to": {"id": 5}}}'
 	message = json.decode(Message, json_data)!
 	assert message.reply_to.reply_to.id == 5
+}
+
+fn test_recursive_optional_struct_ptr() {
+	json_data := '{"id": 1, "text": "Hello", "reply_to": {"id": 2, "text": "Hi", "reply_to": null}}'
+	message := json.decode(OptionalMessage, json_data)!
+	reply := message.reply_to or {
+		assert false
+		return
+	}
+
+	assert reply.id == 2
+	assert reply.text == 'Hi'
+	assert reply.reply_to == none
 }
