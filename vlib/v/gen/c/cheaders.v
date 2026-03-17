41948adad7f87da2a212aa9fe515c81c1682155f
Mike <tankf33der@disroot.org>
2026-03-17 10:22:20 +0200

    improve arch detection

--- vlib/v/gen/c/cheaders.v
+++ vlib/v/gen/c/cheaders.v
@@ -106,7 +106,7 @@ const c_common_macros = '
        #undef __V_architecture
        #define __V_architecture 10
 #endif
-#if defined(__PPC64__)
+#if defined(__powerpc64__) && defined(__BIG_ENDIAN__)
        #define __V_ppc64  1
        #undef __V_architecture
        #define __V_architecture 11
@@ -484,7 +484,7 @@ void * aligned_alloc(size_t alignment, size_t size) { return malloc(size); }
 
 const c_builtin_types = '
 //================================== builtin types ================================*/
-#if defined(__x86_64__) || defined(_M_AMD64) || defined(__aarch64__) || defined(__arm64__) || defined(_M_ARM64) || (defined(__riscv_xlen) && __riscv_xlen == 64) || defined(__s390x__) || (defined(__powerpc64__) && defined(__LITTLE_ENDIAN__)) || defined(__loongarch64) || defined(__sparc__) || defined(__PPC64__)
+#if defined(__x86_64__) || defined(_M_AMD64) || defined(__aarch64__) || defined(__arm64__) || defined(_M_ARM64) || (defined(__riscv_xlen) && __riscv_xlen == 64) || defined(__s390x__) || (defined(__powerpc64__) && defined(__LITTLE_ENDIAN__)) || defined(__loongarch64) || defined(__sparc__) || (defined(__powerpc64__) && defined(__BIG_ENDIAN__))
 typedef int64_t vint_t;
 #else
 typedef int32_t vint_t;
