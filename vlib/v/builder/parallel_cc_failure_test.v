1d504e15b2f5fc2013b82ddba9b2e735e1226684
Alexander Medvednikov <alexander@medvednikov.com>
2026-03-25 16:42:20 +0300

    builder: fix linking failure with -cc gcc -parallel-cc (fixes #23209)

---
+++ vlib/v/builder/parallel_cc_failure_test.v
@@ -0,0 +1,49 @@
+module main
+
+import os
+
+const parallel_cc_test_path = os.join_path(os.vtmp_dir(), 'parallel_cc_failure_test')
+const parallel_cc_vexe = @VEXE
+
+fn run_parallel_cc_failure_case(case_name string, files map[string]string) os.Result {
+	case_dir := os.join_path(parallel_cc_test_path, case_name)
+	os.rmdir_all(case_dir) or {}
+	os.mkdir_all(case_dir) or { panic(err) }
+	defer {
+		os.rmdir_all(case_dir) or {}
+	}
+	for rel_path, contents in files {
+		fpath := os.join_path(case_dir, rel_path)
+		os.mkdir_all(os.dir(fpath)) or { panic(err) }
+		os.write_file(fpath, contents) or { panic(err) }
+	}
+	return os.execute('${os.quoted_path(parallel_cc_vexe)} -parallel-cc ${os.quoted_path(os.join_path(case_dir,
+		'main.v'))}')
+}
+
+fn test_parallel_cc_fails_when_c_compilation_fails() {
+	res := run_parallel_cc_failure_case('compile_fail', {
+		'main.v': 'module main
+#include "definitely_missing_parallel_cc_header.h"
+fn main() {}
+'
+	})
+	assert res.exit_code == 1, res.output
+	assert res.output.contains('failed parallel C compilation'), res.output
+}
+
+fn test_parallel_cc_fails_when_final_linking_fails() {
+	res := run_parallel_cc_failure_case('link_fail', {
+		'parallel_cc_duplicate_symbol.h': 'int parallel_cc_duplicate_symbol(void) { return 42; }
+'
+		'main.v':                         'module main
+#include "@DIR/parallel_cc_duplicate_symbol.h"
+fn C.parallel_cc_duplicate_symbol() int
+fn main() {
+	println(C.parallel_cc_duplicate_symbol())
+}
+'
+	})
+	assert res.exit_code == 1, res.output
+	assert res.output.contains('failed to link after parallel C compilation'), res.output
+}
