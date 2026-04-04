5e7686c2a856d1c54759786fc20e42113a3203a1
Alexander Medvednikov <alexander@medvednikov.com>
2026-04-04 23:12:41 +0300

    all: disable gc on windows for now

--- vlib/v/pref/default.v
+++ vlib/v/pref/default.v
@@ -146,7 +146,7 @@ pub fn (mut p Preferences) fill_with_defaults() {
 		p.parse_define('cross') // TODO: remove when `$if cross {` works
 	}
 	if p.gc_mode == .unknown {
-		if p.backend != .c || p.building_v || p.is_bare {
+		if p.backend != .c || p.building_v || p.is_bare || p.os == .windows {
 			p.gc_mode = .no_gc
 			p.build_options << ['-gc', 'none']
 		} else {
