5b0de094e3a1e12a9e4acccc66876a4ea8291732
Alexander Medvednikov <alexander@medvednikov.com>
2026-04-13 07:08:38 +0300

    tools/vpm: remove unused test

--- cmd/tools/vpm/args_test.v
+++ cmd/tools/vpm/args_test.v
@@ -1,13 +1,5 @@
 module main
 
-fn test_parse_server_urls_supports_all_aliases() {
-	args := ['install', '-m', 'https://mirror-a.example/', '--mirror', 'https://mirror-b.example',
-		'-server-url', 'https://mirror-c.example/', '--server-url', 'https://mirror-d.example',
-		'--server-urls', 'https://mirror-e.example', '-m', 'https://mirror-a.example/']
-	assert parse_server_urls(args) == ['https://mirror-a.example', 'https://mirror-b.example',
-		'https://mirror-c.example', 'https://mirror-d.example', 'https://mirror-e.example']
-}
-
 fn test_parse_vpm_command_skips_option_values() {
 	args := ['-m', 'https://mirror.example', 'install', '--once', 'nedpals.args']
 	assert parse_vpm_command(args) == 'install'
