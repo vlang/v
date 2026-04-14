0d67e1446029ba9eec31d9d8ef2f7a862035d756
Alexander Medvednikov <alexander@medvednikov.com>
2026-04-14 23:10:44 +0300

    nesting of fixed arrays (fixes #16393)

--- vlib/v/tests/aliases/alias_fixed_array_of_struct_test.v
+++ vlib/v/tests/aliases/alias_fixed_array_of_struct_test.v
@@ -13,6 +13,14 @@ struct Empty {}
 
 type EmptyFixed = [2]Empty
 
+type Dot = [3]f64
+type Box = [2]Dot
+
+struct Tst {
+	box Box
+	val int
+}
+
 fn test_alias_fixed_array_of_struct() {
 	bar := Bar([Foo{}, Foo{}, Foo{}, Foo{}]!)
 	println(bar)
@@ -30,4 +38,16 @@ fn test_fixed_array_alias_of_empty_struct() {
 	nested := [2]EmptyFixed{}
 	assert nested.len == 2
 	assert nested[0].len == 2
+}
+
+fn test_nested_fixed_array_alias_in_struct_init() {
+	v_box := Box{}
+	assert v_box.len == 2
+	assert v_box[0].len == 3
+
+	v_tst := Tst{Box([2]Dot{}), 1}
+	println(v_tst)
+	assert v_tst.val == 1
+	assert v_tst.box[0][0] == 0.0
+	assert v_tst.box[1][2] == 0.0
 }
