86ca51ea8f32973215d072684655400ebb687e6c
Alexander Medvednikov <alexander@medvednikov.com>
2026-04-14 23:37:27 +0300

    sdl2: creating window fails (fixes #13138)

--- vlib/v/builder/cc_tcc_retry_test.v
+++ vlib/v/builder/cc_tcc_retry_test.v
@@ -1,5 +1,7 @@
 module builder
 
+import os
+
 fn test_is_tcc_compilation_failure_detects_tcc_kind() {
 	assert is_tcc_compilation_failure('cc', .tcc, '')
 }
@@ -15,4 +17,22 @@ fn test_is_tcc_compilation_failure_detects_tcc_output() {
 	assert is_tcc_compilation_failure('cc', .unknown, 'tcc: error: bad architecture')
 	assert is_tcc_compilation_failure('cc', .unknown, 'line 1\nline 2\ntcc: error: lib not found')
 	assert !is_tcc_compilation_failure('cc', .unknown, 'clang: error: unsupported option')
+}
+
+fn test_is_tcc_compilation_failure_detects_tcc_alias_compiler() {
+	if os.user_os() == 'windows' {
+		return
+	}
+	test_root := os.join_path(os.vtmp_dir(), 'v_builder_cc_tcc_retry_test_${os.getpid()}')
+	cc_path := os.join_path(test_root, 'cc')
+	old_path := os.getenv('PATH')
+	os.mkdir_all(test_root) or { panic(err) }
+	os.write_file(cc_path, '#!/bin/sh\necho "Tiny C Compiler"\n') or { panic(err) }
+	os.chmod(cc_path, 0o700) or { panic(err) }
+	os.setenv('PATH', '${test_root}${os.path_delimiter}${old_path}', true)
+	defer {
+		os.setenv('PATH', old_path, true)
+		os.rmdir_all(test_root) or {}
+	}
+	assert is_tcc_compilation_failure('cc', .unknown, '')
 }
