754f6548af06b02b7a0cf0fc95abbe802a292e61
Alexander Medvednikov <alexander@medvednikov.com>
2026-04-14 12:45:30 +0300

    net: examples/concurrency/concurrency_http.v fails on BSD with tcc (fixes #24728)

--- vlib/net/ssl/ssl_notd_use_openssl.v
+++ vlib/net/ssl/ssl_notd_use_openssl.v
@@ -1,18 +1,41 @@
 module ssl
 
-import net.mbedtls
-
-pub struct SSLConn {
-	mbedtls.SSLConn
+$if tinyc && (freebsd || openbsd) {
+	import net.openssl
+} $else {
+	import net.mbedtls
 }
 
-@[params]
-pub struct SSLConnectConfig {
-	mbedtls.SSLConnectConfig
-}
+$if tinyc && (freebsd || openbsd) {
+	// TinyCC hangs in the bundled mbedtls path on FreeBSD/OpenBSD.
+	// Prefer the OpenSSL backend there, which does not exhibit the issue.
+	pub struct SSLConn {
+		openssl.SSLConn
+	}
 
-// new_ssl_conn returns a new SSLConn with the given config.
-pub fn new_ssl_conn(config SSLConnectConfig) !&SSLConn {
-	c := mbedtls.new_ssl_conn(config.SSLConnectConfig) or { return err }
-	return &SSLConn{c}
+	pub struct SSLConnectConfig {
+		openssl.SSLConnectConfig
+	}
+
+	// new_ssl_conn returns a new SSLConn with the given config.
+	@[params]
+	pub fn new_ssl_conn(config SSLConnectConfig) !&SSLConn {
+		c := openssl.new_ssl_conn(config.SSLConnectConfig) or { return err }
+		return &SSLConn{c}
+	}
+} $else {
+	pub struct SSLConn {
+		mbedtls.SSLConn
+	}
+
+	pub struct SSLConnectConfig {
+		mbedtls.SSLConnectConfig
+	}
+
+	// new_ssl_conn returns a new SSLConn with the given config.
+	@[params]
+	pub fn new_ssl_conn(config SSLConnectConfig) !&SSLConn {
+		c := mbedtls.new_ssl_conn(config.SSLConnectConfig) or { return err }
+		return &SSLConn{c}
+	}
 }
