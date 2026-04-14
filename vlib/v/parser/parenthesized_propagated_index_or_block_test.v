30f85a6d68ebf4cb1b3ee20e516e9f1b193d30eb
Alexander Medvednikov <alexander@medvednikov.com>
2026-04-14 23:46:37 +0300

    parser: allow getting element by index or propagating an error to the caller (fixes #23452)

---
+++ vlib/v/parser/parenthesized_propagated_index_or_block_test.v
@@ -0,0 +1,19 @@
+module parser
+
+import v.ast
+import v.checker
+import v.pref
+
+fn test_parenthesized_propagated_index_can_be_followed_by_or_block() {
+	source_text := "
+fn main() {
+	values := [?string('known'), ?string(none)]
+	_ = (values[1]!) or { 'unknown' }
+}
+"
+	mut table := ast.new_table()
+	vpref := &pref.Preferences{}
+	mut prog := parse_text(source_text, '', mut table, .skip_comments, vpref)
+	mut checker_ := checker.new_checker(table, vpref)
+	checker_.check(mut prog)
+}
