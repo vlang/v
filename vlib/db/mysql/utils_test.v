be19909bd8504ca43c293b9d16f32e6e91146cc9
Alexander Medvednikov <alexander@medvednikov.com>
2026-04-15 15:56:59 +0300

    malformed error messages from db.mysql real_query fail encod... (fixes #19842)

---
+++ vlib/db/mysql/utils_test.v
@@ -0,0 +1,20 @@
+module mysql
+
+import json
+
+struct MysqlErrorPayload {
+	data string
+}
+
+fn test_clone_mysql_cstring_keeps_json_encodable_error_text() {
+	mut backing := "Incorrect string value: 'undefined' for function uuid_to_bin; code: 1411".bytes()
+	backing << u8(0)
+	msg := clone_mysql_cstring(backing.data)
+
+	backing[0] = 0xff
+
+	assert msg == "Incorrect string value: 'undefined' for function uuid_to_bin; code: 1411"
+	assert json.encode(MysqlErrorPayload{
+		data: msg
+	}) == '{"data":"Incorrect string value: \'undefined\' for function uuid_to_bin; code: 1411"}'
+}
