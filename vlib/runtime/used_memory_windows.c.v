ace1ff7ce23fc1ad889c8cde5f70a60c1a6b021d
Alexander Medvednikov <alexander@medvednikov.com>
2026-04-21 16:53:54 +0300

    builtin: fix building on windows 7 failing due to missing psapi.h (fixes #26885)

--- vlib/runtime/used_memory_windows.c.v
+++ vlib/runtime/used_memory_windows.c.v
@@ -1,22 +1,29 @@
 module runtime
 
-#flag -lpsapi
-#include <psapi.h>
-
+// Windows 7+ exports K32GetProcessMemoryInfo from kernel32.dll, so avoid
+// depending on psapi.h/psapi.lib during bootstrap builds.
 @[typedef]
-struct C.PROCESS_MEMORY_COUNTERS {
-	cb             u64
-	WorkingSetSize isize
+struct C.V_PROCESS_MEMORY_COUNTERS {
+	cb                              u32
+	page_fault_count                u32
+	peak_working_set_size           usize
+	working_set_size                usize
+	quota_peak_paged_pool_usage     usize
+	quota_paged_pool_usage          usize
+	quota_peak_non_paged_pool_usage usize
+	quota_non_paged_pool_usage      usize
+	pagefile_usage                  usize
+	peak_pagefile_usage             usize
 }
 
-fn C.GetProcessMemoryInfo(i32, &C.PROCESS_MEMORY_COUNTERS, u64) bool
+fn C.K32GetProcessMemoryInfo(voidptr, &C.V_PROCESS_MEMORY_COUNTERS, u32) bool
 
 // used_memory retrieves the current physical memory usage of the process.
 pub fn used_memory() !u64 {
-	mut pmc := C.PROCESS_MEMORY_COUNTERS{}
-	pmc.cb = u64(sizeof(pmc))
-	if C.GetProcessMemoryInfo(C.GetCurrentProcess(), &pmc, pmc.cb) {
-		return u64(pmc.WorkingSetSize)
+	mut pmc := C.V_PROCESS_MEMORY_COUNTERS{}
+	pmc.cb = u32(sizeof(pmc))
+	if C.K32GetProcessMemoryInfo(C.GetCurrentProcess(), &pmc, pmc.cb) {
+		return u64(pmc.working_set_size)
 	}
 	return 0
 }
