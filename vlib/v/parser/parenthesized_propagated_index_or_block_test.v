aab5601e1298a2af9878cab0d1082e7407e58f46
Alexander Medvednikov <alexander@medvednikov.com>
2026-04-21 15:12:57 +0300

    parser: fix comptime $else { import log } error when in one line (fixes #26527)
    (cherry picked from commit c25c0233c8baf62be411538e3d24fa4b6137a48e)

--- vlib/v/parser/parenthesized_propagated_index_or_block_test.v
+++ vlib/v/parser/parenthesized_propagated_index_or_block_test.v
@@ -17,3 +17,16 @@ fn main() {
 	mut checker_ := checker.new_checker(table, vpref)
 	checker_.check(mut prog)
 }
+
+fn test_top_level_comptime_else_allows_one_line_import() {
+	source_text := "
+$if never_defined ? {} $else { import log }
+
+log.info('42')
+"
+	mut table := ast.new_table()
+	vpref := &pref.Preferences{}
+	mut prog := parse_text(source_text, '', mut table, .skip_comments, vpref)
+	mut checker_ := checker.new_checker(table, vpref)
+	checker_.check(mut prog)
+}
