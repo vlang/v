9cfcd9c2496b0079eae1b0bbbec319da9b18a587
Alexander Medvednikov <alexander@medvednikov.com>
2026-04-21 17:09:20 +0300

    sokol: fix fontstash freetype compiler bug (fixes #17376)
    (cherry picked from commit 4ff79f9299c9f6565da4056f5476f5a3ff6ec088)

--- vlib/fontstash/a_d_use_freetype.v
+++ vlib/fontstash/a_d_use_freetype.v
@@ -1,8 +1,8 @@
 module fontstash
 
 #define FONS_USE_FREETYPE 1
-#flag windows -I @VEXEROOT/thirdparty/freetype/include
-#flag windows -L @VEXEROOT/thirdparty/freetype/win64
+// Windows fontstash forces stb_truetype in thirdparty/fontstash/fontstash.h,
+// so adding freetype link flags here only creates a stale linker dependency.
 #flag linux -I/usr/include/freetype2
 #flag darwin -I/usr/local/include/freetype2
 // brew on m1
@@ -13,7 +13,6 @@ module fontstash
 #flag darwin -L/opt/local/lib
 #flag freebsd -I/usr/local/include/freetype2
 #flag freebsd -Wl -L/usr/local/lib
-#flag windows -lfreetype
 #flag linux -lfreetype
 #flag darwin -lfreetype
 #flag darwin -lpng -lbz2 -lz
