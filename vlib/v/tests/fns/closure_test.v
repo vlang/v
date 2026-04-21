0e02d0e5257251c4be2ebe24bbfedad2422c6eea
Alexander Medvednikov <alexander@medvednikov.com>
2026-04-21 15:07:41 +0300

    tests: add test for closure-captured variables dirtied across contexts (fixes #16635)

--- vlib/v/tests/fns/closure_test.v
+++ vlib/v/tests/fns/closure_test.v
@@ -1,3 +1,5 @@
+import arrays { flat_map, flat_map_indexed, fold, map_indexed, max }
+
 fn test_decl_assignment() {
 	my_var := 12
 	c1 := fn [my_var] () int {
@@ -295,4 +297,111 @@ fn test_auto_heap_var_and_non_ptr_as_closure_arg() {
 	f := c.m()
 	f()
 	assert true
+}
+
+fn test_nested_closure_capture_context() {
+	data := nested_closure_capture_data()
+	assert nested_closure_capture_trace(data) == nested_closure_capture_trace_ref(data)
+	assert nested_closure_capture_part1(data) == 21
+	part2 := nested_closure_capture_part2(data) or { panic(err) }
+	assert part2 == 8
+}
+
+fn nested_closure_capture_data() [][]int {
+	return [
+		[3, 0, 3, 7, 3],
+		[2, 5, 5, 1, 2],
+		[6, 5, 3, 3, 2],
+		[3, 3, 5, 4, 9],
+		[3, 5, 3, 9, 0],
+	]
+}
+
+fn nested_closure_capture_trace(data [][]int) []int {
+	return flat_map_indexed[[]int, int](data, fn [data] (y int, row []int) []int {
+		return flat_map_indexed[int, int](row, fn [data, y] (x int, _ int) []int {
+			return flat_map_indexed[[]int, int](nested_closure_grid_cross_walk(data.len, x, y), fn [data, x, y] (i int, grid_walk []int) []int {
+				return grid_walk.map(fn [data, x, y, i] (pos int) int {
+					return if i < 2 {
+						data[y][pos]
+					} else {
+						data[pos][x]
+					}
+				})
+			})
+		})
+	})
+}
+
+fn nested_closure_capture_trace_ref(data [][]int) []int {
+	mut out := []int{}
+	for y, row in data {
+		for x, _ in row {
+			for i, grid_walk in nested_closure_grid_cross_walk(data.len, x, y) {
+				for pos in grid_walk {
+					out << if i < 2 {
+						data[y][pos]
+					} else {
+						data[pos][x]
+					}
+				}
+			}
+		}
+	}
+	return out
+}
+
+fn nested_closure_capture_part1(data [][]int) int {
+	return flat_map_indexed[[]int, bool](data, fn [data] (y int, row []int) []bool {
+		return map_indexed(row, fn [data, y] (x int, height int) bool {
+			return map_indexed(nested_closure_grid_cross_walk(data.len, x, y), fn [data, x, y, height] (i int, grid_walk []int) bool {
+				return grid_walk.map(fn [data, x, y, i] (pos int) int {
+					return if i < 2 {
+						data[y][pos]
+					} else {
+						data[pos][x]
+					}
+				}).all(it < height)
+			}).any(it)
+		}).filter(it)
+	}).len
+}
+
+fn nested_closure_capture_part2(data [][]int) !int {
+	return max(flat_map_indexed[[]int, int](data, fn [data] (y int, row []int) []int {
+		return map_indexed(row, fn [data, y] (x int, height int) int {
+			return fold(map_indexed(nested_closure_grid_cross_walk(data.len, x, y), fn [data, x, y, height] (i int, grid_walk []int) int {
+				mut count := 0
+				heights := grid_walk.map(fn [data, x, y, i] (pos int) int {
+					return if i < 2 {
+						data[y][pos]
+					} else {
+						data[pos][x]
+					}
+				})
+				for h in heights {
+					count++
+					if h >= height {
+						break
+					}
+				}
+				return count
+			}), 1, fn (acc int, elem int) int {
+				return acc * elem
+			})
+		})
+	}))!
+}
+
+fn nested_closure_range(size int, start int) [][]int {
+	return [
+		[]int{len: start, init: start - index - 1},
+		[]int{len: size - start - 1, init: start + 1 + index},
+	]
+}
+
+fn nested_closure_grid_cross_walk(size int, start_x int, start_y int) [][]int {
+	return flat_map[int, []int]([start_x, start_y], fn [size] (elem int) [][]int {
+		return nested_closure_range(size, elem)
+	})
 }
