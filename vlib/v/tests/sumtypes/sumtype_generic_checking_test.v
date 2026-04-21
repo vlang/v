804346c6ab0a52b638250e717cfc4f7e16c5a3cc
Alexander Medvednikov <alexander@medvednikov.com>
2026-04-21 15:22:55 +0300

    checker: fix generic function with generic sumtype compile error (fixes #25248)
    (cherry picked from commit e2f36d435f9fbf322a5150ede50b3c7dff5c9d25)

--- vlib/v/tests/sumtypes/sumtype_generic_checking_test.v
+++ vlib/v/tests/sumtypes/sumtype_generic_checking_test.v
@@ -1,4 +1,5 @@
 type Sumtype = string | int
+type DemoType[T] = T | fn () T
 
 fn generic_fn[T]() ?T {
 	a := Sumtype('123')
@@ -17,4 +18,25 @@ fn generic_fn[T]() ?T {
 fn test_main() {
 	assert '${generic_fn[string]()}' == "Option('123')"
 	assert '${generic_fn[int]()}' == 'Option(123)'
+}
+
+fn is_function_type[T](data DemoType[T]) bool {
+	_ = data
+	$if T is $function {
+		return true
+	} $else {
+		return false
+	}
+}
+
+fn test_generic_sumtype_function_inference() {
+	assert is_function_type[string]('test string') == false
+	assert is_function_type[string](fn () string {
+		return 'function test string'
+	}) == false
+
+	_ = is_function_type('test string')
+	_ = is_function_type(fn () string {
+		return 'function test string'
+	})
 }
