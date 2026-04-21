79c0a15a95aa2a2479bd9c7eef56f7bcd8960f22
Alexander Medvednikov <alexander@medvednikov.com>
2026-04-21 15:23:01 +0300

    ast: fix assignment error with recursive definition in sum types (fixes #14160)

--- vlib/v/tests/builtin_maps/map_sumtype_value_init_test.v
+++ vlib/v/tests/builtin_maps/map_sumtype_value_init_test.v
@@ -1,8 +1,15 @@
 type Child = Tree | string
 
+type RecursiveChild = int | []RecursiveChild
+
 struct Tree {
 	name  string
 	child map[string]Child
+}
+
+struct RecursiveFields {
+	a int
+	b []int
 }
 
 fn test_map_sumtype_value_init() {
@@ -15,4 +22,29 @@ fn test_map_sumtype_value_init() {
 	println(tree)
 	ret := tree.child['a'] or { Child('') }
 	assert ret == Child('value')
+}
+
+fn test_recursive_sumtype_array_variant_assignment_in_map() {
+	ints := [1, 2, 3]
+	mut got := map[string]RecursiveChild{}
+	got['nums'] = ints
+	assert got == {
+		'nums': RecursiveChild([RecursiveChild(1), RecursiveChild(2), RecursiveChild(3)])
+	}
+}
+
+fn test_recursive_sumtype_array_variant_comptime_selector_assignment_in_map() {
+	data := RecursiveFields{120, [130, 140]}
+	mut got := map[string]RecursiveChild{}
+	$for field in RecursiveFields.fields {
+		$if field.typ is int {
+			got[field.name] = data.$(field.name)
+		} $else $if field.typ is []int {
+			got[field.name] = data.$(field.name)
+		}
+	}
+	assert got == {
+		'a': RecursiveChild(120)
+		'b': RecursiveChild([RecursiveChild(130), RecursiveChild(140)])
+	}
 }
