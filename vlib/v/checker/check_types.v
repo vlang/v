330797c3b1019bf23964e890570e4e11698de394
Alexander Medvednikov <alexander@medvednikov.com>
2026-04-22 21:15:39 +0300

    checker: generics fix

--- vlib/v/checker/check_types.v
+++ vlib/v/checker/check_types.v
@@ -1263,48 +1263,44 @@ fn (mut c Checker) infer_composite_generic_type(gt_name string, generic_typ ast.
 		else {}
 	}
 
+	expected_type_args, expected_parent_idx := c.generic_type_args_and_parent_idx(generic_typ)
+	actual_type_args, actual_parent_idx := c.generic_type_args_and_parent_idx(concrete_typ)
+	if expected_parent_idx != 0 && actual_parent_idx != 0 && expected_type_args.len > 0
+		&& expected_type_args.len == actual_type_args.len {
+		if expected_parent_idx != actual_parent_idx {
+			expected_parent_sym := c.table.sym(ast.idx_to_type(expected_parent_idx))
+			actual_parent_sym := c.table.sym(ast.idx_to_type(actual_parent_idx))
+			expected_parent_name := if expected_parent_sym.ngname != '' {
+				expected_parent_sym.ngname
+			} else {
+				ast.strip_generic_params(expected_parent_sym.name)
+			}
+			actual_parent_name := if actual_parent_sym.ngname != '' {
+				actual_parent_sym.ngname
+			} else {
+				ast.strip_generic_params(actual_parent_sym.name)
+			}
+			if expected_parent_name != actual_parent_name
+				&& expected_parent_sym.parent_idx != actual_parent_idx
+				&& actual_parent_sym.parent_idx != expected_parent_idx {
+				return ast.void_type
+			}
+		}
+		for i, expected_type_arg in expected_type_args {
+			inferred_type := c.infer_composite_generic_type(gt_name, expected_type_arg,
+				actual_type_args[i])
+			if inferred_type != ast.void_type {
+				return inferred_type
+			}
+		}
+	}
+
 	// Sumtype coercion: if the expected type is a generic sumtype or a concrete
 	// generic sumtype instantiation, try to infer the generic type from its variants.
 	inferred_sumtype_type := c.infer_sumtype_variant_generic_type(gt_name, generic_typ,
 		concrete_typ)
 	if inferred_sumtype_type != ast.void_type {
 		return inferred_sumtype_type
-	}
-
-	expected_type_args, expected_parent_idx := c.generic_type_args_and_parent_idx(generic_typ)
-	actual_type_args, actual_parent_idx := c.generic_type_args_and_parent_idx(concrete_typ)
-	if expected_parent_idx == 0 || actual_parent_idx == 0 || expected_type_args.len == 0
-		|| expected_type_args.len != actual_type_args.len {
-		return ast.void_type
-	}
-	if expected_parent_idx != actual_parent_idx {
-		expected_parent_sym := c.table.sym(ast.idx_to_type(expected_parent_idx))
-		actual_parent_sym := c.table.sym(ast.idx_to_type(actual_parent_idx))
-		expected_parent_name := if expected_parent_sym.ngname != '' {
-			expected_parent_sym.ngname
-		} else {
-			ast.strip_generic_params(expected_parent_sym.name)
-		}
-		actual_parent_name := if actual_parent_sym.ngname != '' {
-			actual_parent_sym.ngname
-		} else {
-			ast.strip_generic_params(actual_parent_sym.name)
-		}
-		if expected_parent_name != actual_parent_name
-			&& expected_parent_sym.parent_idx != actual_parent_idx
-			&& actual_parent_sym.parent_idx != expected_parent_idx {
-			return ast.void_type
-		}
-	}
-	if expected_type_args.len == 0 || expected_type_args.len != actual_type_args.len {
-		return ast.void_type
-	}
-	for i, expected_type_arg in expected_type_args {
-		inferred_type := c.infer_composite_generic_type(gt_name, expected_type_arg,
-			actual_type_args[i])
-		if inferred_type != ast.void_type {
-			return inferred_type
-		}
 	}
 	return ast.void_type
 }
