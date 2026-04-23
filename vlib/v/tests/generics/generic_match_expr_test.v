267475d919454faf39dc54db1f7b802fe86698fa
Alexander Medvednikov <alexander@medvednikov.com>
2026-04-24 01:19:21 +0300

    checker: fix Regression: V can't match generics variant (fixes #26866)

--- vlib/v/tests/generics/generic_match_expr_test.v
+++ vlib/v/tests/generics/generic_match_expr_test.v
@@ -22,3 +22,41 @@ fn test_main() {
 	t('')!
 	assert true
 }
+
+type GenericMatchSumtype[T] = GenericMatchY[T] | GenericMatchZ[T]
+
+fn (mut x GenericMatchSumtype[T]) do_it[T]() string {
+	match mut x {
+		GenericMatchY[T] { return 'doing GenericMatchY' }
+		GenericMatchZ[T] { return 'doing GenericMatchZ' }
+	}
+
+	return ''
+}
+
+struct GenericMatchY[T] {
+	name  string
+	value T
+}
+
+struct GenericMatchZ[T] {
+	name  string
+	value T
+}
+
+fn test_match_generic_sumtype_variant_in_generic_method() {
+	y := GenericMatchY[int]{
+		name:  'Y'
+		value: 1
+	}
+	z := GenericMatchZ[bool]{
+		name:  'Z'
+		value: true
+	}
+
+	mut x1 := GenericMatchSumtype[int](y)
+	assert x1.do_it() == 'doing GenericMatchY'
+
+	mut x2 := GenericMatchSumtype[bool](z)
+	assert x2.do_it() == 'doing GenericMatchZ'
+}
