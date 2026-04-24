9b26b5d46a5c9092d3b2bbfb80f60622b4d04ce7
愚者 <guweigang@bullsoft.org>
2026-04-25 03:04:01 +0800

    net.websocket: add server on_attached hook after handshake attach (#26955)

--- vlib/net/websocket/websocket_server.v
+++ vlib/net/websocket/websocket_server.v
@@ -20,6 +20,7 @@ mut:
 	logger                  &log.Logger      = default_logger
 	ls                      &net.TcpListener = unsafe { nil } // listener used to get incoming connection to socket
 	accept_client_callbacks []AcceptClientFn      // accept client callback functions
+	attached_callbacks      []AttachedEventHandler
 	message_callbacks       []MessageEventHandler // new message callback functions
 	close_callbacks         []CloseEventHandler   // close message callback functions
 pub:
@@ -183,6 +184,13 @@ fn (mut s Server) attach_client(mut server_client ServerClient, handshake_respon
 		s.server_state.clients[server_client.client.id] = unsafe { server_client }
 	}
 	s.setup_callbacks(mut server_client)
+	s.send_attached_event(mut server_client) or {
+		lock s.server_state {
+			s.server_state.clients.delete(server_client.client.id)
+		}
+		server_client.client.shutdown_socket() or {}
+		return err
+	}
 }
 
 // setup_callbacks initialize all callback functions
