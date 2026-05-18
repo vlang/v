08a756f86123c261cfb48313ee2909dd1efb4ce4
JalonSolov <JalonSolov@gmail.com>
2026-05-18 08:37:17 -0400

    compress.zlib: changed to pure V RFC-compliant code

--- vlib/compress/zlib/zlib.v
+++ vlib/compress/zlib/zlib.v
@@ -1,17 +1,13 @@
 module zlib
 
-import compress as compr
+import compress.deflate
 
-// compresses an array of bytes using zlib and returns the compressed bytes in a new array
-// Example: b := 'abcdefgh'.repeat(1000).bytes(); cmpr := zlib.compress(b)!; assert cmpr.len < b.len; dc := zlib.decompress(cmpr)!; assert b == dc
+// compress compresses data using the zlib container format.
 pub fn compress(data []u8) ![]u8 {
-	// flags = TDEFL_WRITE_ZLIB_HEADER (0x01000)
-	return compr.compress(data, 0x01000)
+	return deflate.compress_zlib(data)
 }
 
-// decompresses an array of bytes using zlib and returns the decompressed bytes in a new array
-// Example: b := 'abcdefgh'.repeat(1000).bytes(); cmpr := zlib.compress(b)!; assert cmpr.len < b.len; dc := zlib.decompress(cmpr)!; assert b == dc
+// decompress decompresses zlib-compressed data.
 pub fn decompress(data []u8) ![]u8 {
-	// flags = TINFL_FLAG_PARSE_ZLIB_HEADER (0x1)
-	return compr.decompress(data, 0x1)
+	return deflate.decompress_zlib(data)
 }
