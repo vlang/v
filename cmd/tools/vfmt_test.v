7928176da306f9995a814de74e57d863d8c6fee6
Alexander Medvednikov <alexander@medvednikov.com>
2026-06-01 03:32:36 +0300

    vfmt: respect VFLAGS preferences

--- cmd/tools/vfmt_test.v
+++ cmd/tools/vfmt_test.v
@@ -23,3 +23,28 @@ fn test_fmt_keeps_invalid_assert_source_unchanged() {
 	assert res.output.contains('unexpected string `bye`, expecting `,`'), res.output
 	assert os.read_file(source_path)! == original
 }
+
+fn test_fmt_preferences_respect_vflags() {
+	source_path := os.join_path(vfmt_test_tdir, 'vflags_backend_js.v')
+	os.write_file(source_path, "fn main() {\n\tx := 'abc'.str\n}\n")!
+
+	old_vflags := os.getenv('VFLAGS')
+	defer {
+		if old_vflags == '' {
+			os.unsetenv('VFLAGS')
+		} else {
+			os.setenv('VFLAGS', old_vflags, true)
+		}
+	}
+
+	os.unsetenv('VFLAGS')
+	warmup_res := os.execute('${os.quoted_path(vexe)} fmt -help')
+	assert warmup_res.exit_code == 0, warmup_res.output
+
+	os.setenv('VFLAGS', '-b js', true)
+	res := os.execute('${os.quoted_path(vexe)} fmt ${os.quoted_path(source_path)}')
+
+	assert res.exit_code == 0, res.output
+	assert res.output.contains("x := 'abc'.str"), res.output
+	assert !res.output.contains("x := c'abc'"), res.output
+}
