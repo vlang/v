vlang/v

commit 77b4117c563692762ae5bc7bb93269cfcc25aecb

cgen: match aliases by runtime tag (#27461)

vlib/v/gen/c/cgen.v
@@ -4619,6 +4619,12 @@ fn (g &Gen) find_matching_sumtype_variant(expected_type ast.Type, got_type ast.T
 			return variant
 		}
 	}
+	got_unaliased := g.table.fully_unaliased_type(got_type)
+	for variant in variants {
+		if g.table.fully_unaliased_type(variant) == got_unaliased {
+			return variant
+		}
+	}
 	for variant in variants {
 		if g.table.can_implicit_array_cast(got_type, variant) {
 			return variant
@@ -12814,6 +12820,42 @@ fn (mut g Gen) as_cast_option_payload_expr_from_expr(typ ast.Type, expr ast.Expr
 	return g.as_cast_option_payload_expr(typ, g.expr_string(expr), false)
 }
 
+fn (mut g Gen) write_as_cast_call_start(styp string, sym ast.TypeSymbol) {
+	if sym.info is ast.FnType {
+		g.write('(${styp})')
+	} else if g.inside_smartcast {
+		g.write('(${styp}*)')
+	} else {
+		g.write('*(${styp}*)')
+	}
+}
+
+fn (mut g Gen) write_as_cast_call(obj_expr string, tag_expr string, expected_sidx string, index_exprs []string) {
+	needs_tag_condition := index_exprs.len > 1
+		|| (index_exprs.len == 1 && index_exprs[0] != expected_sidx)
+	if needs_tag_condition {
+		g.write('(')
+		g.write_type_tag_condition(tag_expr, '==', index_exprs)
+		g.write(' ? ${obj_expr} : ')
+	}
+	g.write('builtin____as_cast(${obj_expr}, ${tag_expr}, ${expected_sidx})')
+	if needs_tag_condition {
+		g.write(')')
+	}
+}
+
+fn (mut g Gen) as_cast_payload_type(target_type ast.Type, matching_variants []ast.Type) ast.Type {
+	for variant in matching_variants {
+		if g.is_exact_sumtype_variant_match(variant, target_type) {
+			return target_type
+		}
+	}
+	if matching_variants.len > 0 {
+		return matching_variants[0]
+	}
+	return target_type
+}
+
 fn (mut g Gen) as_cast(node ast.AsCast) {
 	// Make sure the sum type can be cast to this type (the types
 	// are the same), otherwise panic.
@@ -12845,6 +12887,11 @@ fn (mut g Gen) as_cast(node ast.AsCast) {
 	if mut expr_type_sym.info is ast.SumType {
 		expr_is_option := unwrapped_expr_type.has_flag(.option)
 		dot := if expr_type_without_option.is_ptr() { '->' } else { '.' }
+		matching_variants := g.matching_sumtype_variant_types(expr_type_without_option,
+			unwrapped_node_typ)
+		index_exprs := g.type_idx_exprs_for_types(matching_variants)
+		payload_sym := g.table.sym(g.as_cast_payload_type(unwrapped_node_typ, matching_variants))
+		sidx := g.type_sidx(unwrapped_node_typ)
 		if node.expr.has_fn_call() && !g.is_cc_msvc {
 			tmp_var := g.new_tmp_var()
 			expr_styp := g.styp(node.expr_type)
@@ -12856,41 +12903,21 @@ fn (mut g Gen) as_cast(node ast.AsCast) {
 			} else {
 				tmp_var
 			}
-			if sym.info is ast.FnType {
-				g.write('(${styp})builtin____as_cast(')
-			} else if g.inside_smartcast {
-				g.write('(${styp}*)builtin____as_cast(')
-			} else {
-				g.write('*(${styp}*)builtin____as_cast(')
-			}
-			g.write2('(${expr_str})', dot)
-			g.write2('_${sym.cname},', '(${expr_str})')
-			g.write(dot)
-			sidx := g.type_sidx(unwrapped_node_typ)
-			g.write('_typ, ${sidx}); })')
+			obj_expr := '(${expr_str})${dot}_${payload_sym.cname}'
+			tag_expr := '(${expr_str})${dot}_typ'
+			g.write_as_cast_call_start(styp, sym)
+			g.write_as_cast_call(obj_expr, tag_expr, sidx, index_exprs)
+			g.write('; })')
 		} else {
-			if sym.info is ast.FnType {
-				g.write('(${styp})builtin____as_cast(')
-			} else if g.inside_smartcast {
-				g.write('(${styp}*)builtin____as_cast(')
+			expr_str := if expr_is_option {
+				g.as_cast_option_payload_expr_from_expr(unwrapped_expr_type, node.expr)
 			} else {
-				g.write('*(${styp}*)builtin____as_cast(')
+				g.expr_string(node.expr)
 			}
-			if expr_is_option {
-				expr_str := g.as_cast_option_payload_expr_from_expr(unwrapped_expr_type, node.expr)
-				g.write2('(${expr_str})', dot)
-				g.write2('_${sym.cname},', '(${expr_str})')
-				g.write(dot)
-			} else {
-				g.write('(')
-				g.expr(node.expr)
-				g.write2(')', dot)
-				g.write2('_${sym.cname},', '(')
-				g.expr(node.expr)
-				g.write2(')', dot)
-			}
-			sidx := g.type_sidx(unwrapped_node_typ)
-			g.write('_typ, ${sidx})')
+			obj_expr := '(${expr_str})${dot}_${payload_sym.cname}'
+			tag_expr := '(${expr_str})${dot}_typ'
+			g.write_as_cast_call_start(styp, sym)
+			g.write_as_cast_call(obj_expr, tag_expr, sidx, index_exprs)
 		}
 
 		// fill as cast name table
@@ -12934,40 +12961,27 @@ fn (mut g Gen) as_cast(node ast.AsCast) {
 		expr_type_sym.info = info
 	} else if mut expr_type_sym.info is ast.Interface && node.expr_type != node.typ {
 		dot := if node.expr_type.is_ptr() { '->' } else { '.' }
+		matching_variants := g.matching_interface_variant_types(expr_type_sym, unwrapped_node_typ)
+		index_exprs := g.type_idx_exprs_for_types(matching_variants)
+		payload_sym := g.table.sym(g.as_cast_payload_type(unwrapped_node_typ, matching_variants))
+		sidx := g.type_sidx(unwrapped_node_typ)
 		if node.expr.has_fn_call() && !g.is_cc_msvc {
 			tmp_var := g.new_tmp_var()
 			expr_styp := g.styp(node.expr_type)
 			g.write('({ ${expr_styp} ${tmp_var} = ')
 			g.expr(node.expr)
 			g.write('; ')
-			if sym.info is ast.FnType {
-				g.write('(${styp})builtin____as_cast(')
-			} else if g.inside_smartcast {
-				g.write('(${styp}*)builtin____as_cast(')
-			} else {
-				g.write('*(${styp}*)builtin____as_cast(')
-			}
-			g.write2(tmp_var, dot)
-			g.write('_${sym.cname},v_typeof_interface_idx_${expr_type_sym.cname}(')
-			g.write2(tmp_var, dot)
-			sidx := g.type_sidx(unwrapped_node_typ)
-			g.write('_typ), ${sidx}); })')
+			obj_expr := '${tmp_var}${dot}_${payload_sym.cname}'
+			tag_expr := 'v_typeof_interface_idx_${expr_type_sym.cname}(${tmp_var}${dot}_typ)'
+			g.write_as_cast_call_start(styp, sym)
+			g.write_as_cast_call(obj_expr, tag_expr, sidx, index_exprs)
+			g.write('; })')
 		} else {
-			if sym.info is ast.FnType {
-				g.write('(${styp})builtin____as_cast(')
-			} else if g.inside_smartcast {
-				g.write('(${styp}*)builtin____as_cast(')
-			} else {
-				g.write('*(${styp}*)builtin____as_cast(')
-			}
-			g.write('(')
-			g.expr(node.expr)
-			g.write2(')', dot)
-			g.write2('_${sym.cname},v_typeof_interface_idx_${expr_type_sym.cname}(', '(')
-			g.expr(node.expr)
-			g.write2(')', dot)
-			sidx := g.type_sidx(unwrapped_node_typ)
-			g.write('_typ), ${sidx})')
+			expr_str := g.expr_string(node.expr)
+			obj_expr := '(${expr_str})${dot}_${payload_sym.cname}'
+			tag_expr := 'v_typeof_interface_idx_${expr_type_sym.cname}((${expr_str})${dot}_typ)'
+			g.write_as_cast_call_start(styp, sym)
+			g.write_as_cast_call(obj_expr, tag_expr, sidx, index_exprs)
 		}
 
 		// fill as cast name table

vlib/v/gen/c/utils.v
@@ -522,6 +522,132 @@ fn (mut g Gen) resolved_scope_var_type(expr ast.Ident) ast.Type {
 	return resolved
 }
 
+fn (mut g Gen) alias_equivalent_type(typ ast.Type) ast.Type {
+	return g.table.fully_unaliased_type(g.unwrap_generic(typ))
+}
+
+fn (mut g Gen) type_idx_exprs_for_types(types []ast.Type) []string {
+	mut matches := []string{}
+	mut seen := map[string]bool{}
+	for typ in types {
+		index_expr := g.type_sidx(typ)
+		if index_expr !in seen {
+			seen[index_expr] = true
+			matches << index_expr
+		}
+	}
+	return matches
+}
+
+fn (mut g Gen) matching_sumtype_variant_types(parent_type ast.Type, target_type ast.Type) []ast.Type {
+	variants := g.sumtype_runtime_variants(parent_type)
+	target_unaliased := g.alias_equivalent_type(target_type)
+	mut matches := []ast.Type{}
+	mut seen := map[string]bool{}
+	for variant in variants {
+		if g.alias_equivalent_type(variant) == target_unaliased {
+			index_expr := g.type_sidx(variant)
+			if index_expr !in seen {
+				seen[index_expr] = true
+				matches << variant
+			}
+		}
+	}
+	if matches.len > 0 {
+		return matches
+	}
+	for variant in variants {
+		if g.is_exact_sumtype_variant_match(variant, target_type) {
+			return [variant]
+		}
+	}
+	return [target_type]
+}
+
+fn (mut g Gen) matching_sumtype_variant_type_idx_exprs(parent_type ast.Type, target_type ast.Type) []string {
+	return g.type_idx_exprs_for_types(g.matching_sumtype_variant_types(parent_type, target_type))
+}
+
+fn (mut g Gen) matching_interface_variant_types(interface_sym ast.TypeSymbol, target_type ast.Type) []ast.Type {
+	if interface_sym.info !is ast.Interface {
+		return []
+	}
+	target_unaliased := g.alias_equivalent_type(target_type)
+	info := interface_sym.info as ast.Interface
+	mut matches := []ast.Type{}
+	mut seen := map[string]bool{}
+	for variant in g.runtime_interface_variants(info) {
+		variant_sym := g.table.sym(variant)
+		if variant_sym.kind in [.interface, .aggregate] {
+			continue
+		}
+		if variant_sym.info is ast.Struct && variant_sym.info.is_unresolved_generic() {
+			continue
+		}
+		if g.alias_equivalent_type(variant) != target_unaliased {
+			continue
+		}
+		index_expr := g.type_sidx(variant)
+		if index_expr !in seen {
+			seen[index_expr] = true
+			matches << variant
+		}
+	}
+	if matches.len > 0 {
+		return matches
+	}
+	return [target_type]
+}
+
+fn (mut g Gen) matching_interface_variant_index_exprs(interface_sym ast.TypeSymbol, target_type ast.Type) []string {
+	if interface_sym.info !is ast.Interface {
+		return []
+	}
+	matching_variants := g.matching_interface_variant_types(interface_sym, target_type)
+	mut matches := []string{}
+	mut seen := map[string]bool{}
+	for variant in matching_variants {
+		cctype := g.cc_type(ast.mktyp(variant), true)
+		index_expr := '_${interface_sym.cname}_${cctype}_index'
+		if index_expr !in seen {
+			seen[index_expr] = true
+			matches << index_expr
+		}
+	}
+	if matches.len > 0 {
+		return matches
+	}
+	cctype := g.cc_type(ast.mktyp(target_type), true)
+	return ['_${interface_sym.cname}_${cctype}_index']
+}
+
+fn (mut g Gen) matching_interface_variant_type_idx_exprs(interface_sym ast.TypeSymbol, target_type ast.Type) []string {
+	if interface_sym.info !is ast.Interface {
+		return []
+	}
+	return g.type_idx_exprs_for_types(g.matching_interface_variant_types(interface_sym, target_type))
+}
+
+fn (mut g Gen) write_type_tag_condition(tag_expr string, cmp_op string, index_exprs []string) {
+	if index_exprs.len == 0 {
+		g.write(if cmp_op == '==' { 'false' } else { 'true' })
+		return
+	}
+	if index_exprs.len > 1 {
+		g.write('(')
+	}
+	joiner := if cmp_op == '==' { ' || ' } else { ' && ' }
+	for i, index_expr in index_exprs {
+		if i > 0 {
+			g.write(joiner)
+		}
+		g.write('${tag_expr} ${cmp_op} ${index_expr}')
+	}
+	if index_exprs.len > 1 {
+		g.write(')')
+	}
+}
+
 fn (mut g Gen) resolved_scope_var_type_uncached(expr ast.Ident) ast.Type {
 	mut scope := if expr.scope != unsafe { nil } {
 		expr.scope.innermost(expr.pos.pos)

vlib/v/checker/infix.v
@@ -984,8 +984,10 @@ fn (mut c Checker) infix_expr(mut node ast.InfixExpr) ast.Type {
 				if typ_sym.kind == .placeholder {
 					c.error('${op}: type `${typ_sym.name}` does not exist', right_expr.pos())
 				}
+				mut left_sumtype_check_type := left_type
 				if mut left_sym.info is ast.Aggregate {
 					parent_left_type := left_sym.info.sum_type
+					left_sumtype_check_type = parent_left_type
 					left_sym = c.table.sym(parent_left_type)
 				}
 				if c.inside_sql {
@@ -1002,8 +1004,9 @@ fn (mut c Checker) infix_expr(mut node ast.InfixExpr) ast.Type {
 					} else {
 						typ
 					}
-					if variant_typ !in left_sym.info.variants
-						&& c.unwrap_generic(variant_typ) !in left_sym.info.variants {
+					unwrapped_variant_typ := c.unwrap_generic(variant_typ)
+					if !c.table.sumtype_has_variant_recursive(left_sumtype_check_type, variant_typ, true)
+						&& !c.table.sumtype_has_variant_recursive(left_sumtype_check_type, unwrapped_variant_typ, true) {
 						c.error('`${left_sym.name}` has no variant `${typ_sym.name}`', right_pos)
 					}
 				} else if c.is_orig_sumtype(node.left) {
@@ -1013,8 +1016,9 @@ fn (mut c Checker) infix_expr(mut node ast.InfixExpr) ast.Type {
 						orig_t := (node.left.obj as ast.Var).orig_type
 						orig_sym := c.table.sym(orig_t)
 						if orig_sym.info is ast.SumType {
-							if typ !in orig_sym.info.variants
-								&& c.unwrap_generic(typ) !in orig_sym.info.variants {
+							unwrapped_typ := c.unwrap_generic(typ)
+							if !c.table.sumtype_has_variant_recursive(orig_t, typ, true)
+								&& !c.table.sumtype_has_variant_recursive(orig_t, unwrapped_typ, true) {
 								c.error('`${orig_sym.name}` has no variant `${typ_sym.name}`',
 									right_pos)
 							}

vlib/v/gen/c/infix.v
@@ -1106,6 +1106,87 @@ fn (mut g Gen) infix_expr_in_optimization(left ast.Expr, left_type ast.Type, rig
 	}
 }
 
+fn (mut g Gen) type_tag_expr_for_is_left(node ast.InfixExpr, is_aggregate bool, is_orig_sumtype bool) string {
+	mut left_expr := ''
+	if is_aggregate {
+		left_expr = '${node.left}'
+	} else {
+		if is_orig_sumtype {
+			g.prevent_sum_type_unwrapping_once = true
+		}
+		left_expr = g.expr_string(node.left)
+	}
+	left_value_expr := if node.left_type.nr_muls() > 1 {
+		'(${'*'.repeat(node.left_type.nr_muls() - 1)}${left_expr})'
+	} else {
+		'(${left_expr})'
+	}
+	dot_or_ptr := if node.left_type.is_ptr() { '->' } else { '.' }
+	return '${left_value_expr}${dot_or_ptr}_typ'
+}
+
+fn (mut g Gen) write_type_tag_expr_for_is_left(node ast.InfixExpr, is_aggregate bool, is_orig_sumtype bool) {
+	g.write('(')
+	if node.left_type.nr_muls() > 1 {
+		g.write('*'.repeat(node.left_type.nr_muls() - 1))
+	}
+	if is_aggregate {
+		g.write('${node.left}')
+	} else if is_orig_sumtype {
+		g.prevent_sum_type_unwrapping_once = true
+		g.expr(node.left)
+	} else {
+		g.expr(node.left)
+	}
+	g.write(')')
+	if node.left_type.is_ptr() {
+		g.write('->')
+	} else {
+		g.write('.')
+	}
+	g.write('_typ')
+}
+
+fn (mut g Gen) write_is_type_tag_condition(node ast.InfixExpr, is_aggregate bool, is_orig_sumtype bool, cmp_op string, index_exprs []string) {
+	if index_exprs.len == 0 {
+		g.write(if cmp_op == '==' { 'false' } else { 'true' })
+		return
+	}
+	if index_exprs.len == 1 {
+		g.write_type_tag_expr_for_is_left(node, is_aggregate, is_orig_sumtype)
+		g.write(' ${cmp_op} ${index_exprs[0]}')
+		return
+	}
+	if is_aggregate {
+		tag_expr := g.type_tag_expr_for_is_left(node, is_aggregate, is_orig_sumtype)
+		g.write_type_tag_condition(tag_expr, cmp_op, index_exprs)
+		return
+	}
+	tag_tmp := g.new_tmp_var()
+	if !g.is_cc_msvc {
+		g.write('({ ${ast.int_type_name} ${tag_tmp} = ')
+		g.write_type_tag_expr_for_is_left(node, is_aggregate, is_orig_sumtype)
+		g.write('; ')
+		g.write_type_tag_condition(tag_tmp, cmp_op, index_exprs)
+		g.write('; })')
+		return
+	}
+	mut cur_line := if g.inside_ternary > 0 {
+		g.go_before_ternary().trim_space()
+	} else {
+		g.go_before_last_stmt().trim_space()
+	}
+	if g.inside_return && cur_line.ends_with('return') {
+		cur_line += ' '
+	}
+	g.empty_line = true
+	g.write('${ast.int_type_name} ${tag_tmp} = ')
+	g.write_type_tag_expr_for_is_left(node, is_aggregate, is_orig_sumtype)
+	g.writeln(';')
+	g.write(cur_line)
+	g.write_type_tag_condition(tag_tmp, cmp_op, index_exprs)
+}
+
 // infix_expr_is_op generates code for `is` and `!is`
 fn (mut g Gen) infix_expr_is_op(node ast.InfixExpr) {
 	mut left_sym := g.table.final_sym(g.unwrap_generic(g.type_resolver.get_type_or_default(node.left,
@@ -1143,30 +1224,10 @@ fn (mut g Gen) infix_expr_is_op(node ast.InfixExpr) {
 	}
 
 	cmp_op := if node.op == .key_is { '==' } else { '!=' }
-	g.write('(')
-	if node.left_type.nr_muls() > 1 {
-		g.write('*'.repeat(node.left_type.nr_muls() - 1))
-	}
-	if is_aggregate {
-		g.write('${node.left}')
-	} else if is_orig_sumtype {
-		g.prevent_sum_type_unwrapping_once = true
-		g.expr(node.left)
-	} else {
-		g.expr(node.left)
-	}
-	g.write(')')
-	if node.left_type.is_ptr() {
-		g.write('->')
-	} else {
-		g.write('.')
-	}
 	if left_sym.kind == .interface {
-		g.write('_typ ${cmp_op} ')
-		// `_Animal_Dog_index`
 		sub_type := match node.right {
 			ast.TypeNode {
-				g.unwrap_generic(node.right.typ)
+				right_type
 			}
 			ast.None {
 				ast.idx_to_type(g.table.type_idxs['None__'])
@@ -1176,17 +1237,57 @@ fn (mut g Gen) infix_expr_is_op(node ast.InfixExpr) {
 			}
 		}
 
-		sub_sym := g.table.sym(sub_type)
-		g.write('_${left_sym.cname}_${sub_sym.cname}_index')
+		g.write_is_type_tag_condition(node, is_aggregate, is_orig_sumtype, cmp_op, g.matching_interface_variant_index_exprs(left_sym,
+			sub_type))
 		return
 	} else if left_sym.kind == .sum_type || is_aggregate {
-		g.write('_typ ${cmp_op} ')
+		mut aggregate_parent_type := node.left_type
+		if is_aggregate {
+			aggregate_sym := g.table.sym(node.left_type)
+			if aggregate_sym.info is ast.Aggregate {
+				aggregate_parent_type = aggregate_sym.info.sum_type
+			}
+		}
+		sumtype_parent_type := if is_orig_sumtype && node.left is ast.Ident
+			&& node.left.obj is ast.Var {
+			(node.left.obj as ast.Var).orig_type
+		} else if is_aggregate {
+			aggregate_parent_type
+		} else {
+			node.left_type
+		}
+		if node.right is ast.None {
+			g.write_is_type_tag_condition(node, is_aggregate, is_orig_sumtype, cmp_op, [
+				'${ast.none_type.idx()}',
+			])
+		} else if node.right is ast.Ident && node.right.name == g.comptime.comptime_for_variant_var {
+			mut variant_idx := g.type_resolver.get_ct_type_or_default('${g.comptime.comptime_for_variant_var}.typ',
+				ast.void_type)
+			if (left_sym.kind == .sum_type || is_aggregate) && node.left_type.nr_muls() > 0
+				&& variant_idx.nr_muls() <= node.left_type.nr_muls() {
+				variant_idx = variant_idx.set_nr_muls(0)
+			}
+			g.write_is_type_tag_condition(node, is_aggregate, is_orig_sumtype, cmp_op, g.matching_sumtype_variant_type_idx_exprs(sumtype_parent_type,
+				variant_idx))
+		} else if node.right is ast.TypeNode {
+			g.write_is_type_tag_condition(node, is_aggregate, is_orig_sumtype, cmp_op, g.matching_sumtype_variant_type_idx_exprs(sumtype_parent_type,
+				right_type))
+		} else {
+			g.write_type_tag_expr_for_is_left(node, is_aggregate, is_orig_sumtype)
+			g.write(' ${cmp_op} ')
+			g.expr(node.right)
+		}
+		return
 	}
 	if node.right is ast.None {
+		g.write_type_tag_expr_for_is_left(node, is_aggregate, is_orig_sumtype)
+		g.write(' ${cmp_op} ')
 		g.write('${ast.none_type.idx()}')
 	} else if node.right is ast.Ident && node.right.name == g.comptime.comptime_for_variant_var {
 		variant_idx := g.type_resolver.get_ct_type_or_default('${g.comptime.comptime_for_variant_var}.typ',
 			ast.void_type)
+		g.write_type_tag_expr_for_is_left(node, is_aggregate, is_orig_sumtype)
+		g.write(' ${cmp_op} ')
 		if (left_sym.kind == .sum_type || is_aggregate) && node.left_type.nr_muls() > 0
 			&& variant_idx.nr_muls() <= node.left_type.nr_muls() {
 			g.write('${int(variant_idx.set_nr_muls(0))}')
@@ -1194,8 +1295,12 @@ fn (mut g Gen) infix_expr_is_op(node ast.InfixExpr) {
 			g.write('${int(variant_idx)}')
 		}
 	} else if node.right is ast.TypeNode {
+		g.write_type_tag_expr_for_is_left(node, is_aggregate, is_orig_sumtype)
+		g.write(' ${cmp_op} ')
 		g.write('${int(right_type)}')
 	} else {
+		g.write_type_tag_expr_for_is_left(node, is_aggregate, is_orig_sumtype)
+		g.write(' ${cmp_op} ')
 		g.expr(node.right)
 	}
 }

commit e211864614c6ed0a2f6b609d08b48c06223c21b4

cgen: fix sumtype alias-variant conflation and as_cast operand corruption
Two regressions from 77b4117c5 ("cgen: match aliases by runtime tag"):

1. Variant matching compared types by fully_unaliased_type, conflating
   distinct sumtype variants that share an underlying base (e.g. the v2
   `Type` sumtype's `String`/`Char`/`Void`/... all `= u8`). `x is Void`
   then matched any of them, which broke the whole `-v2` frontend.
   Match by alias-chain containment instead (respecting nr_muls/option):
   distinct `=u8` siblings stay distinct, genuine alias/original pairs
   still match by runtime tag.

2. The as_cast rewrite rendered the operand with g.expr_string(), which
   saves out.len, runs g.expr() and cut_to()s back. A hoisting operand
   (option propagation like `j['name']!`, an IndexExpr that is not an AST
   CallExpr) calls go_before_last_stmt(), cutting the builder past the
   saved offset and splicing cast text into earlier output (`_S("name")`
   -> `_S("n*(string*)builtin____as_cast((ame")`, `_t12 undeclared`).
   Evaluate hoisting operands into a temp first (non-MSVC via a statement
   expression, MSVC via a hoisted line), for both the sumtype and
   interface as_cast branches.

vlib/v/gen/c/cgen.v
@@ -4619,9 +4619,8 @@ fn (g &Gen) find_matching_sumtype_variant(expected_type ast.Type, got_type ast.T
 			return variant
 		}
 	}
-	got_unaliased := g.table.fully_unaliased_type(got_type)
 	for variant in variants {
-		if g.table.fully_unaliased_type(variant) == got_unaliased {
+		if g.alias_chain_equivalent(variant, got_type) {
 			return variant
 		}
 	}
@@ -12875,6 +12874,32 @@ fn (mut g Gen) as_cast_payload_type(target_type ast.Type, matching_variants []as
 	return target_type
 }
 
+// as_cast_operand_needs_tmp_eval reports whether emitting `expr` as the operand of
+// an `as` cast over a sum type may itself emit statements (option propagation,
+// if/match temporaries, calls). Such operands must be evaluated into a temporary
+// first: rendering them with g.expr_string() runs g.expr() into a saved builder
+// offset, but a hoisting operand calls go_before_last_stmt() which cuts the
+// builder back past that offset, so the subsequent cut_to() corrupts the output.
+// Plain values (idents, literals, field accesses) never emit statements and can be
+// rendered inline.
+fn as_cast_operand_needs_tmp_eval(expr ast.Expr) bool {
+	return match expr {
+		ast.Ident, ast.BoolLiteral, ast.IntegerLiteral, ast.FloatLiteral, ast.CharLiteral,
+		ast.StringLiteral, ast.EnumVal, ast.TypeNode {
+			false
+		}
+		ast.ParExpr {
+			as_cast_operand_needs_tmp_eval(expr.expr)
+		}
+		ast.SelectorExpr {
+			as_cast_operand_needs_tmp_eval(expr.expr)
+		}
+		else {
+			true
+		}
+	}
+}
+
 fn (mut g Gen) as_cast(node ast.AsCast) {
 	// Make sure the sum type can be cast to this type (the types
 	// are the same), otherwise panic.
@@ -12911,12 +12936,34 @@ fn (mut g Gen) as_cast(node ast.AsCast) {
 		index_exprs := g.type_idx_exprs_for_types(matching_variants)
 		payload_sym := g.table.sym(g.as_cast_payload_type(unwrapped_node_typ, matching_variants))
 		sidx := g.type_sidx(unwrapped_node_typ)
-		if node.expr.has_fn_call() && !g.is_cc_msvc {
+		if as_cast_operand_needs_tmp_eval(node.expr) {
+			// The operand emits statements while it is generated (option
+			// propagation, if/match temporaries, calls). g.expr_string() would
+			// drop those statements and corrupt the output, so evaluate the
+			// operand into a temporary first and reference it by name.
 			tmp_var := g.new_tmp_var()
 			expr_styp := g.styp(node.expr_type)
-			g.write('({ ${expr_styp} ${tmp_var} = ')
-			g.expr(node.expr)
-			g.write('; ')
+			if !g.is_cc_msvc {
+				g.write('({ ${expr_styp} ${tmp_var} = ')
+				g.expr(node.expr)
+				g.write('; ')
+			} else {
+				// MSVC has no statement-expressions; hoist the temporary onto its
+				// own line before the current statement instead.
+				mut cur_line := if g.inside_ternary > 0 {
+					g.go_before_ternary().trim_space()
+				} else {
+					g.go_before_last_stmt().trim_space()
+				}
+				if g.inside_return && cur_line.ends_with('return') {
+					cur_line += ' '
+				}
+				g.empty_line = true
+				g.write('${expr_styp} ${tmp_var} = ')
+				g.expr(node.expr)
+				g.writeln(';')
+				g.write(cur_line)
+			}
 			expr_str := if expr_is_option {
 				g.as_cast_option_payload_expr(unwrapped_expr_type, tmp_var, false)
 			} else {
@@ -12926,7 +12973,9 @@ fn (mut g Gen) as_cast(node ast.AsCast) {
 			tag_expr := '(${expr_str})${dot}_typ'
 			g.write_as_cast_call_start(styp, sym)
 			g.write_as_cast_call(obj_expr, tag_expr, sidx, index_exprs)
-			g.write('; })')
+			if !g.is_cc_msvc {
+				g.write('; })')
+			}
 		} else {
 			expr_str := if expr_is_option {
 				g.as_cast_option_payload_expr_from_expr(unwrapped_expr_type, node.expr)
@@ -12984,17 +13033,38 @@ fn (mut g Gen) as_cast(node ast.AsCast) {
 		index_exprs := g.type_idx_exprs_for_types(matching_variants)
 		payload_sym := g.table.sym(g.as_cast_payload_type(unwrapped_node_typ, matching_variants))
 		sidx := g.type_sidx(unwrapped_node_typ)
-		if node.expr.has_fn_call() && !g.is_cc_msvc {
+		if as_cast_operand_needs_tmp_eval(node.expr) {
+			// See as_cast_operand_needs_tmp_eval: a hoisting operand must be
+			// evaluated into a temporary, otherwise g.expr_string() drops the
+			// statements it emits and corrupts the output.
 			tmp_var := g.new_tmp_var()
 			expr_styp := g.styp(node.expr_type)
-			g.write('({ ${expr_styp} ${tmp_var} = ')
-			g.expr(node.expr)
-			g.write('; ')
+			if !g.is_cc_msvc {
+				g.write('({ ${expr_styp} ${tmp_var} = ')
+				g.expr(node.expr)
+				g.write('; ')
+			} else {
+				mut cur_line := if g.inside_ternary > 0 {
+					g.go_before_ternary().trim_space()
+				} else {
+					g.go_before_last_stmt().trim_space()
+				}
+				if g.inside_return && cur_line.ends_with('return') {
+					cur_line += ' '
+				}
+				g.empty_line = true
+				g.write('${expr_styp} ${tmp_var} = ')
+				g.expr(node.expr)
+				g.writeln(';')
+				g.write(cur_line)
+			}
 			obj_expr := '${tmp_var}${dot}_${payload_sym.cname}'
 			tag_expr := 'v_typeof_interface_idx_${expr_type_sym.cname}(${tmp_var}${dot}_typ)'
 			g.write_as_cast_call_start(styp, sym)
 			g.write_as_cast_call(obj_expr, tag_expr, sidx, index_exprs)
-			g.write('; })')
+			if !g.is_cc_msvc {
+				g.write('; })')
+			}
 		} else {
 			expr_str := g.expr_string(node.expr)
 			obj_expr := '(${expr_str})${dot}_${payload_sym.cname}'

vlib/v/gen/c/utils.v
@@ -522,8 +522,44 @@ fn (mut g Gen) resolved_scope_var_type(expr ast.Ident) ast.Type {
 	return resolved
 }
 
-fn (mut g Gen) alias_equivalent_type(typ ast.Type) ast.Type {
-	return g.table.fully_unaliased_type(g.unwrap_generic(typ))
+// type_alias_chain_idxs returns the idxs of `typ` and of every type it aliases,
+// transitively, down to the first non-alias type. Comparison is by idx only,
+// so flags and pointer levels are ignored.
+fn (g &Gen) type_alias_chain_idxs(typ ast.Type) []int {
+	mut idxs := [typ.idx()]
+	mut cur := typ
+	for {
+		sym := g.table.sym(cur)
+		if sym.info is ast.Alias {
+			parent := sym.info.parent_type
+			pidx := parent.idx()
+			if pidx in idxs {
+				break
+			}
+			idxs << pidx
+			cur = parent
+		} else {
+			break
+		}
+	}
+	return idxs
+}
+
+// alias_chain_equivalent reports whether `a` and `b` denote the same type modulo
+// alias naming: one type's alias chain (the type itself plus the types it
+// aliases, transitively) contains the other. This is stricter than comparing
+// fully unaliased base types, so distinct aliases that merely share an
+// underlying representation (e.g. `type String = u8` and `type Void = u8`) are
+// NOT treated as equivalent, while genuine alias/original pairs (e.g. a variant
+// `Bar` together with `type Foo = Bar`) still match by runtime tag.
+fn (g &Gen) alias_chain_equivalent(a ast.Type, b ast.Type) bool {
+	if a.nr_muls() != b.nr_muls() || a.has_flag(.option) != b.has_flag(.option) {
+		return false
+	}
+	if a.idx() == b.idx() {
+		return true
+	}
+	return b.idx() in g.type_alias_chain_idxs(a) || a.idx() in g.type_alias_chain_idxs(b)
 }
 
 fn (mut g Gen) type_idx_exprs_for_types(types []ast.Type) []string {
@@ -541,11 +577,11 @@ fn (mut g Gen) type_idx_exprs_for_types(types []ast.Type) []string {
 
 fn (mut g Gen) matching_sumtype_variant_types(parent_type ast.Type, target_type ast.Type) []ast.Type {
 	variants := g.sumtype_runtime_variants(parent_type)
-	target_unaliased := g.alias_equivalent_type(target_type)
+	target := g.unwrap_generic(target_type)
 	mut matches := []ast.Type{}
 	mut seen := map[string]bool{}
 	for variant in variants {
-		if g.alias_equivalent_type(variant) == target_unaliased {
+		if g.alias_chain_equivalent(g.unwrap_generic(variant), target) {
 			index_expr := g.type_sidx(variant)
 			if index_expr !in seen {
 				seen[index_expr] = true
@@ -572,7 +608,7 @@ fn (mut g Gen) matching_interface_variant_types(interface_sym ast.TypeSymbol, ta
 	if interface_sym.info !is ast.Interface {
 		return []
 	}
-	target_unaliased := g.alias_equivalent_type(target_type)
+	target := g.unwrap_generic(target_type)
 	info := interface_sym.info as ast.Interface
 	mut matches := []ast.Type{}
 	mut seen := map[string]bool{}
@@ -584,7 +620,7 @@ fn (mut g Gen) matching_interface_variant_types(interface_sym ast.TypeSymbol, ta
 		if variant_sym.info is ast.Struct && variant_sym.info.is_unresolved_generic() {
 			continue
 		}
-		if g.alias_equivalent_type(variant) != target_unaliased {
+		if !g.alias_chain_equivalent(g.unwrap_generic(variant), target) {
 			continue
 		}
 		index_expr := g.type_sidx(variant)