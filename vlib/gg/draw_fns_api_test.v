79f9772ec33709f75ce4eea59a547823ce17cd60
Alexander Medvednikov <alexander@medvednikov.com>
2026-07-09 19:09:39 +0300

    ci: stabilize final master checks

--- vlib/gg/draw_fns_api_test.v
+++ vlib/gg/draw_fns_api_test.v
@@ -4,6 +4,19 @@ import os
 fn test_all_samples_can_be_compiled() {
 	vexe := @VEXE
 	vroot := os.dir(vexe)
+	cache_dir := os.join_path(os.vtmp_dir(), 'draw_fns_api_vcache_${os.getpid()}')
+	os.rmdir_all(cache_dir) or {}
+	os.mkdir_all(cache_dir) or { panic(err) }
+	old_vcache := os.getenv_opt('VCACHE')
+	os.setenv('VCACHE', cache_dir, true)
+	defer {
+		if vcache := old_vcache {
+			os.setenv('VCACHE', vcache, true)
+		} else {
+			os.unsetenv('VCACHE')
+		}
+		os.rmdir_all(cache_dir) or {}
+	}
 	samples := os.walk_ext('${vroot}/vlib/gg/testdata', '.vv')
 	mut fails := []string{}
 	for program_source in samples {
