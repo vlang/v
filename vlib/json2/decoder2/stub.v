8650679babd95a6909254575025eb74b48e462ed
Alexander Medvednikov <alexander@medvednikov.com>
2026-07-13 06:24:33 +0300

    json2: remove duplicate decoder2 decode stub

--- vlib/json2/decoder2/stub.v
+++ vlib/json2/decoder2/stub.v
@@ -2,13 +2,6 @@ module decoder2
 
 import json2
 
-// decode decodes a JSON string into a specified type.
-@[deprecated: '`decode` has been moved to `json2`, use `decode` from `json2` instead']
-@[deprecated_after: '2025-10-12']
-pub fn decode[T](val string) !T {
-	return json2.decode[T](val)!
-}
-
 // decode_array decodes a JSON string into a specified type. This is the same as decode.
 @[deprecated: '`decode_array` has been moved to `json2`, use `decode` from `json2` instead']
 @[deprecated_after: '2025-03-18']
