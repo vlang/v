a5d6ed9f55e1e3cf8d1cb9478de462d062bcc5c4
Alexander Medvednikov <alexander@medvednikov.com>
2026-07-18 22:38:11 +0300

    cgen: stop recursive sum type default expansion

--- vlib/v/tests/sumtype_default_common_field_test.v
+++ vlib/v/tests/sumtype_default_common_field_test.v
@@ -38,6 +38,21 @@ struct NestedHolder {
 	o NestedOuter
 }
 
+type RecursiveDefaultSum = RecursiveDefaultBranch | RecursiveDefaultLeaf
+
+struct RecursiveDefaultBranch {
+	x     int
+	child RecursiveDefaultSum
+}
+
+struct RecursiveDefaultLeaf {
+	x int
+}
+
+struct RecursiveDefaultHolder {
+	node RecursiveDefaultSum
+}
+
 fn test_default_sumtype_common_field_is_accessible() {
 	holder := DefaultHolder{}
 	assert holder.s.x == 0
@@ -46,4 +61,15 @@ fn test_default_sumtype_common_field_is_accessible() {
 fn test_default_nested_sumtype_common_field_is_accessible() {
 	holder := NestedHolder{}
 	assert holder.o.x == 0
+}
+
+fn test_recursive_default_sumtype_common_field_is_accessible() {
+	holder := RecursiveDefaultHolder{}
+	node := holder.node
+	assert node is RecursiveDefaultBranch
+	assert node.x == 0
+	if node is RecursiveDefaultBranch {
+		assert node.child is RecursiveDefaultBranch
+		assert node.child.x == 0
+	}
 }
