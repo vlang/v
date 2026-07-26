f658ae74417d6f397c0f5a721c7b041da62fefe1
tkhuu01 <tk7315@hotmail.com>
2026-07-26 18:12:06 -0400

    format fix

--- vlib/arrays/arrays.v
+++ vlib/arrays/arrays.v
@@ -211,7 +211,7 @@ pub fn chunk_while[T](a []T, predicate fn (before T, after T) bool) [][]T {
 // WindowAttribute configures the windows produced by arrays.window
 pub struct WindowAttribute {
 pub:
-	size int     // number of elements in each window; must be positive
+	size int // number of elements in each window; must be positive
 	step int = 1 // amount added to the starting index after each window; must be positive
 }
 
