0510d303ef9e56ae6c05cb89090bd10b1fceabb5
Alexander Medvednikov <alexander@medvednikov.com>
2026-08-09 11:47:55 +0300

    v3: stream C output when mmap is unavailable

--- vlib/v3/gen/c/output_nix.c.v
+++ vlib/v3/gen/c/output_nix.c.v
@@ -5,26 +5,56 @@ import os
 #include <sys/mman.h>
 #include <unistd.h>
 
+fn write_c_output_sequential(mut file os.File, prefix []u8, segments []string, tail string) ! {
+	if prefix.len > 0 {
+		unsafe { file.write_full_buffer(prefix.data, usize(prefix.len))! }
+	}
+	for segment in segments {
+		if segment.len > 0 {
+			file.write_string(segment)!
+		}
+	}
+	if tail.len > 0 {
+		file.write_string(tail)!
+	}
+}
+
 fn write_c_output_mapped(path string, prefix []u8, segments []string, tail string) ! {
 	mut total := prefix.len + tail.len
 	for segment in segments {
 		total += segment.len
 	}
-	mut file := os.open_file(path, 'w+b')!
+	mut file := os.open_file(path, 'w+b') or {
+		mut sequential_file := os.open_file(path, 'wb')!
+		write_c_output_sequential(mut sequential_file, prefix, segments, tail) or {
+			sequential_file.close()
+			return err
+		}
+		sequential_file.close()
+		return
+	}
 	if total == 0 {
 		file.close()
 		return
 	}
 	if C.ftruncate(file.fd, u64(total)) != 0 {
+		write_c_output_sequential(mut file, prefix, segments, tail) or {
+			file.close()
+			return err
+		}
 		file.close()
-		return error('ftruncate failed (errno ${C.errno})')
+		return
 	}
 	mapped := unsafe {
 		C.mmap(nil, usize(total), C.PROT_READ | C.PROT_WRITE, C.MAP_SHARED, file.fd, 0)
 	}
 	if mapped == C.MAP_FAILED {
+		write_c_output_sequential(mut file, prefix, segments, tail) or {
+			file.close()
+			return err
+		}
 		file.close()
-		return error('mmap failed (errno ${C.errno})')
+		return
 	}
 	mut offset := 0
 	unsafe {
