018dcfabdc31eb979bc6ae41a6bff9db19811976
Alexander Medvednikov <alexander@medvednikov.com>
2026-09-05 03:18:28 +0300

    fasthttp: preserve connection response state

--- vlib/fasthttp/fasthttp_bsd_regression_test.c.v
+++ vlib/fasthttp/fasthttp_bsd_regression_test.c.v
@@ -11,51 +11,52 @@ fn bsd_reregistration_test_handler(_ HttpRequest) !HttpResponse {
 
 fn test_keep_alive_completion_rearms_kqueue_read_after_consumed_edge() ! {
 	server := new_server(ServerConfig{
-		family:                  .ip
-		port:                    0
+		family: .ip
+		port: 0
 		max_request_buffer_size: 8192
-		handler:                 bsd_reregistration_test_handler
+		handler: bsd_reregistration_test_handler
 	})!
 	kq := C.kqueue()
 	assert kq >= 0
 	defer {
 		C.close(kq)
 	}
-	mut sockets := [2]int{}
+	mut sockets := [2]i32{}
 	assert C.socketpair(C.AF_UNIX, C.SOCK_STREAM, 0, &sockets[0]) == 0
+	server_fd := int(sockets[0])
+	client_fd := int(sockets[1])
 	defer {
-		C.close(sockets[0])
-		C.close(sockets[1])
+		C.close(server_fd)
+		C.close(client_fd)
 	}
-	set_nonblocking(sockets[0])
+	set_nonblocking(server_fd)
 	mut conn := &Conn{
-		fd:             sockets[0]
+		fd: server_fd
 		request_active: true
-		file_fd:        -1
+		file_fd: -1
 	}
-	assert add_event(kq, u64(sockets[0]), i16(C.EVFILT_READ),
-		u16(C.EV_ADD | C.EV_ENABLE | C.EV_CLEAR), conn) == 0
-	assert C.write(sockets[1], c'GET ', 4) == 4
+	assert add_event(kq, u64(server_fd), i16(C.EVFILT_READ), u16(C.EV_ADD | C.EV_ENABLE | C.EV_CLEAR), conn) == 0
+	assert C.write(client_fd, c'GET ', 4) == 4
 
 	mut event := C.kevent{}
 	mut timeout := C.timespec{
 		tv_sec: 1
 	}
 	assert C.kevent(kq, unsafe { nil }, 0, &event, 1, &timeout) == 1
-	assert event.ident == u64(sockets[0])
+	assert event.ident == u64(server_fd)
 
 	server.begin_request()
 	mut clients := {
-		sockets[0]: voidptr(conn)
+		server_fd: voidptr(conn)
 	}
 	complete_response(server, kq, conn, mut clients, false)
 
 	assert server.active_request_count() == 0
-	assert clients[sockets[0]] or { unsafe { nil } } == voidptr(conn)
+	assert clients[server_fd] or { unsafe { nil } } == voidptr(conn)
 	timeout = C.timespec{
 		tv_sec: 1
 	}
 	assert C.kevent(kq, unsafe { nil }, 0, &event, 1, &timeout) == 1
-	assert event.ident == u64(sockets[0])
+	assert event.ident == u64(server_fd)
 	close_conn(server, kq, conn, mut clients)
 }
