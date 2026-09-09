b17585e802bacc94320566a3ac21610cb9141cad
Alexander Medvednikov <alexander@medvednikov.com>
2026-09-09 06:31:28 +0300

    parser: track pointer receiver alias replacement

--- vlib/v/parser/fn.v
+++ vlib/v/parser/fn.v
@@ -335,11 +335,9 @@ fn scan_receiver_reassignment(node ast.Node, name string, mut info ReceiverReass
 					reduced := left.remove_par()
 					if reduced is ast.Ident && reduced.name == name {
 						info.directly_reassigned = true
-					} else if reduced is ast.PrefixExpr && reduced.op == .mul {
-						dereferenced := reduced.right.remove_par()
-						if dereferenced is ast.Ident && dereferenced.name == name {
-							info.directly_reassigned = true
-						}
+					} else if reduced is ast.PrefixExpr && reduced.op == .mul
+						&& is_receiver_pointer_alias(reduced.right, name, info.receiver_aliases) {
+						info.directly_reassigned = true
 					} else if reduced is ast.IndexExpr
 						&& is_receiver_pointer_alias(reduced.left, name, info.receiver_aliases) {
 						info.directly_reassigned = true
