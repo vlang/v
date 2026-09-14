63d42305673350f8a9256cc0ef8af4f5b43fe87d
Alexander Medvednikov <alexander@medvednikov.com>
2026-09-14 22:54:48 +0300

    context: document custom string representation

--- vlib/context/context.v
+++ vlib/context/context.v
@@ -68,7 +68,8 @@ mut:
 	err() IError
 }
 
-// str returns the `str` method of the corresponding Context struct
+// str returns the built-in context's string representation. For a Context implementation
+// defined outside this module, it returns the concrete implementation type name.
 pub fn (ctx &Context) str() string {
 	// since `Context` is an interface we have to manually match every possible
 	// type that implements `Context` if we want to use a `Context` as a field in a struct
