61e21042485bcda56e84d352a994068aee0cb774
Alexander Medvednikov <alexander@medvednikov.com>
2026-09-15 08:55:04 +0300

    cmd/v: declare renameat in generated C (#28633)

--- cmd/v/toolcache_nix.c.v
+++ cmd/v/toolcache_nix.c.v
@@ -31,6 +31,7 @@ fn C.fstat(fd i32, information &C.stat) i32
 
 fn C.fchmod(fd i32, mode u32) i32
 
+@[c_extern]
 fn C.renameat(old_directory i32, const_old_path &char, new_directory i32, const_new_path &char) i32
 
 fn C.unlinkat(directory i32, const_path &char, flags i32) i32
