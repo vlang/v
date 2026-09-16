0576770a1abfd6aa83f7f582863c977a327781cd
Alexander Medvednikov <alexander@medvednikov.com>
2026-09-16 12:13:10 +0300

    toml: test json_any array conversion

--- vlib/toml/tests/json_test.v
+++ vlib/toml/tests/json_test.v
@@ -16,3 +16,9 @@ fn test_parse() {
 	println(toml_json)
 	assert toml_json == out_file_json
 }
+
+fn test_json_any_array() {
+	toml_doc := toml.parse_text('values = ["one", "two"]') or { panic(err) }
+	json_value := to.json_any(toml_doc.value('values'))
+	assert json_value.json_str() == '["one","two"]'
+}
