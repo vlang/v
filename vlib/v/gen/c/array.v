f76005bb3eab3f1781b0f321b63c141c2658d63e
Alexander Medvednikov <alexander@medvednikov.com>
2026-09-18 12:18:46 +0300

    cgen: unwrap shared maps in index assignment

--- vlib/v/gen/c/array.v
+++ vlib/v/gen/c/array.v
@@ -1656,7 +1656,7 @@ fn (mut g FlatGen) gen_index_assign(node flat.Node) {
 				if !is_ptr {
 					g.write('&')
 				}
-				g.gen_expr(base_id)
+				g.gen_map_value_expr(base_id)
 				g.writeln(';')
 				key_id := g.a.child(&lhs, 1)
 				mut key_ref := '&${key_tmp}'
@@ -1683,7 +1683,7 @@ fn (mut g FlatGen) gen_index_assign(node flat.Node) {
 			} else {
 				g.write('map__set(&')
 			}
-			g.gen_expr(base_id)
+			g.gen_map_value_expr(base_id)
 			g.write(', &(${c_key}[]){')
 			g.gen_expr_with_expected_type(g.a.child(&lhs, 1), clean_base.key_type)
 			g.write('}, &(${c_val}[]){')
