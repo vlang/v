55a98f6b8fa17dbc4380eb9c023bbf6692e3d2aa
Alexander Medvednikov <alexander@medvednikov.com>
2026-09-18 21:08:49 +0300

    chore: isolated pkg-config query edits [skip ci]

--- vlib/v/gen/c/cleanc.v
+++ vlib/v/gen/c/cleanc.v
@@ -4,12 +4,10 @@
 }
 
 fn c_pkgconfig_flags(raw string) []string {
-	packages := cmdexec.split_args(trimmed_space(raw)) or { return []string{} }
-	if packages.len == 0 {
+	args := pref.pkgconfig_flags_args(trimmed_space(raw)) or { return []string{} }
+	if args.len == 0 {
 		return []string{}
 	}
-	mut args := ['--cflags', '--libs']
-	args << packages
 	result := cmdexec.run('pkg-config', args)
 	if result.exit_code != 0 {
 		return []string{}
