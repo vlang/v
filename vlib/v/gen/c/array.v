7b3bf8e54b64eddff9567d299d016cbe649b74f3
Alexander Medvednikov <alexander@medvednikov.com>
2026-09-18 12:10:53 +0300

    cgen: pass shared map payloads to map methods

--- vlib/v/gen/c/array.v
+++ vlib/v/gen/c/array.v
@@ -1091,15 +1091,15 @@ fn (g &FlatGen) array_elem_type_matches(expected types.Type, actual types.Type) 
 // gen_map_ref_arg emits map ref arg output for c.
 fn (mut g FlatGen) gen_map_ref_arg(base_id flat.NodeId, base_type types.Type) {
 	if base_type is types.Pointer {
-		g.gen_expr(base_id)
+		g.gen_map_value_expr(base_id)
 	} else if !g.expr_is_addressable(base_id) || g.map_index_value_is_rvalue(base_id) {
 		ct := g.tc.c_type(base_type)
 		g.write('&((${ct}[]){')
-		g.gen_expr(base_id)
+		g.gen_map_value_expr(base_id)
 		g.write('})[0]')
 	} else {
 		g.write('&')
-		g.gen_expr(base_id)
+		g.gen_map_value_expr(base_id)
 	}
 }
 
