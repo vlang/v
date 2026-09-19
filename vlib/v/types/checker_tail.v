76909cbf3862be3b5083fec4816440dc0de13590
Alexander Medvednikov <alexander@medvednikov.com>
2026-09-20 00:27:01 +0300

    checker: reject excess explicit pointer depth

--- vlib/v/types/checker_tail.v
+++ vlib/v/types/checker_tail.v
@@ -13627,23 +13627,30 @@ fn (mut tc TypeChecker) check_call_arg_types(id flat.NodeId, node flat.Node, inf
 			pointer_value_arg = pointer_value_arg || tc.type_compatible(base, expected)
 				|| tc.pointer_value_compatible(base, expected)
 		}
-		pointer_depth_mismatch := actual_pointer_depth != expected_pointer_depth
+		explicit_address_depth_mismatch := !info.name.starts_with('C.')
+			&& arg_node.kind == .prefix && arg_node.op == .amp
+			&& actual_pointer_depth > expected_pointer_depth
 			&& expected.name() !in ['voidptr', 'byteptr', 'charptr']
-			&& !fn_param_is_voidptr_type(expected) && !(arg_node.is_mut
-			&& tc.mut_pointer_slot_arg_compatible(pointer_check_actual, expected))
-			&& !tc.fn_voidptr_expr_compatible(pointer_check_actual, expected)
-			&& !(info.name.starts_with('C.') && tc.is_zero_literal(arg_id))
-			&& !(info.name.starts_with('C.') && fn_param_is_voidptr_type(pointer_check_actual))
-			&& !tc.implicit_ref_arg_compatible(arg_id, pointer_check_actual, expected)
-			&& !(actual_pointer_depth == expected_pointer_depth + 1
-				&& tc.receiver_compatible(pointer_check_actual, expected))
-			&& !type_contains_unknown(pointer_check_actual) && !type_contains_unknown(expected)
 			&& !tc.call_arg_is_callee_receiver(node, arg_id)
 			&& !tc.call_arg_is_lowered_method_receiver(node, info, param_idx, expected)
-			&& !(arg_node.is_mut && expected is Pointer
-				&& tc.type_compatible(actual, expected.base_type)) && !pointer_value_arg
-			&& !(info.name.starts_with('C.')
-				&& c_pointer_to_voidptr_arg_compatible(pointer_check_actual, expected))
+		pointer_depth_mismatch := explicit_address_depth_mismatch
+			|| (actual_pointer_depth != expected_pointer_depth
+				&& expected.name() !in ['voidptr', 'byteptr', 'charptr']
+				&& !fn_param_is_voidptr_type(expected) && !(arg_node.is_mut
+				&& tc.mut_pointer_slot_arg_compatible(pointer_check_actual, expected))
+				&& !tc.fn_voidptr_expr_compatible(pointer_check_actual, expected)
+				&& !(info.name.starts_with('C.') && tc.is_zero_literal(arg_id))
+				&& !(info.name.starts_with('C.') && fn_param_is_voidptr_type(pointer_check_actual))
+				&& !tc.implicit_ref_arg_compatible(arg_id, pointer_check_actual, expected)
+				&& !(actual_pointer_depth == expected_pointer_depth + 1
+					&& tc.receiver_compatible(pointer_check_actual, expected))
+				&& !type_contains_unknown(pointer_check_actual) && !type_contains_unknown(expected)
+				&& !tc.call_arg_is_callee_receiver(node, arg_id)
+				&& !tc.call_arg_is_lowered_method_receiver(node, info, param_idx, expected)
+				&& !(arg_node.is_mut && expected is Pointer
+					&& tc.type_compatible(actual, expected.base_type)) && !pointer_value_arg
+				&& !(info.name.starts_with('C.')
+					&& c_pointer_to_voidptr_arg_compatible(pointer_check_actual, expected)))
 		pointer_array_mismatch := actual_pointer_depth > 0 && expected_pointer_depth > 0
 			&& unalias_type(actual_pointer_base) is Array
 			&& unalias_type(expected_pointer_base) is Array
