7ef8427b37715fd9efd4d5b3c2d665c4c538b1a8
Alexander Medvednikov <alexander@medvednikov.com>
2026-09-20 02:28:02 +0300

    checker: allow option payload alias casts

--- vlib/v/types/checker_comptime.v
+++ vlib/v/types/checker_comptime.v
@@ -4148,7 +4148,7 @@ fn (mut tc TypeChecker) check_cast_expr(id flat.NodeId, node flat.Node) {
 		tc.record_error_at(.assignment_mismatch, 'cannot cast `none` to `${target_name}`', id, node.pos)
 		return
 	}
-	if actual is OptionType && target !is OptionType {
+	if actual is OptionType && target !is OptionType && target !is Alias {
 		tc.record_error_at(.assignment_mismatch, 'cannot type cast an Option', id, node.pos)
 		return
 	}
