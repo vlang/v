4d90df56e72e55082fae647a113e7c10b1930703
Alexander Medvednikov <alexander@medvednikov.com>
2026-09-19 18:21:28 +0300

    checker: diagnose unknown expression statement names

--- vlib/v/types/checker_tail_stmt.v
+++ vlib/v/types/checker_tail_stmt.v
@@ -7345,7 +7345,7 @@ fn (mut tc TypeChecker) check_ident(id flat.NodeId, node flat.Node) {
 		parent_id := tc.direct_parent_id(id)
 		if tc.fn_context.node_id >= 0 && tc.valid_node_id(parent_id)
 			&& tc.a.node(parent_id).kind == .expr_stmt {
-			tc.record_error(.unknown_ident, '`${node.value}` evaluated but not used', id)
+			tc.record_error(.unknown_ident, 'unexpected name `${node.value}`', id)
 			tc.register_synth_type(id, Type(void_))
 			return
 		}
