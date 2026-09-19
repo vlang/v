b47ee55d199280f47ad9d28659a9d3f559c40913
Alexander Medvednikov <alexander@medvednikov.com>
2026-09-20 00:54:24 +0300

    checker: classify unused parameter captures

--- vlib/v/types/checker_comptime.v
+++ vlib/v/types/checker_comptime.v
@@ -11113,7 +11113,11 @@ fn (mut tc TypeChecker) check_fn_literal(id flat.NodeId, node flat.Node) {
 		if capture.kind == .ident && capture.value.len > 0 && !capture_has_open_generic
 			&& !tc.fn_literal_body_uses_ident(node, capture.value)
 			&& !tc.comptime_skipped_body_uses(node, capture.value) {
-			tc.record_warning_at(.unknown_ident, 'unused variable: `${capture.value}`', capture_id, tc.node_value_diagnostic_pos(capture_id))
+			if _ := tc.current_fn_param_type_text(capture.value) {
+				tc.record_notice_at(.unknown_ident, 'unused parameter: `${capture.value}`', capture_id, tc.node_value_diagnostic_pos(capture_id))
+			} else {
+				tc.record_warning_at(.unknown_ident, 'unused variable: `${capture.value}`', capture_id, tc.node_value_diagnostic_pos(capture_id))
+			}
 		}
 	}
 	for i in 0 .. node.children_count {
