69fa4efa310fc0b9b65fe3a6e685ce1ce52e2392
Alexander Medvednikov <alexander@medvednikov.com>
2026-09-20 05:13:28 +0300

    checker: preserve repeated byte deprecation errors

--- vlib/v/types/checker_parallel.v
+++ vlib/v/types/checker_parallel.v
@@ -2403,7 +2403,8 @@ fn duplicate_match_case_int(message string) ?int {
 fn type_errors_equal(a TypeError, b TypeError) bool {
 	if a.node == b.node && a.kind == b.kind && a.msg == b.msg
 		&& (is_inline_asm_instruction_error(a.msg)
-			|| a.msg.starts_with('cannot embed non-struct `')) {
+			|| a.msg.starts_with('cannot embed non-struct `')
+			|| a.msg == 'byte is deprecated, use u8 instead') {
 		return a.pos == b.pos
 	}
 	return a.node == b.node && a.kind == b.kind && a.msg == b.msg
