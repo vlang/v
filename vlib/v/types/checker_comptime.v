6280f8dd0c065c99af5d9e091f37848694e7a3c5
Alexander Medvednikov <alexander@medvednikov.com>
2026-09-20 03:23:26 +0300

    checker: reject as casts on wrapped options

--- vlib/v/types/checker_comptime.v
+++ vlib/v/types/checker_comptime.v
@@ -5599,6 +5599,16 @@ fn (mut tc TypeChecker) check_as_expr(id flat.NodeId, node flat.Node) {
 		tc.record_unhandled_result_call(child_id, child_type)
 		return
 	}
+	if child_type is OptionType {
+		child := tc.a.node(child_id)
+		message := if child.kind == .ident {
+			'variable `${child.value}` is an Option, it must be unwrapped first'
+		} else {
+			'Option expression `${tc.source_text_for_node(child_id)}` must be unwrapped first'
+		}
+		tc.record_error_at(.assignment_mismatch, message, child_id, tc.node_value_diagnostic_pos(child_id))
+		return
+	}
 	target_type_name := comptime_static_unwrap_type_text(node.value)
 	if target_type_name.contains('.') && !interface_pattern_is_collapsed_container(node.value)
 		&& !tc.type_name_known(target_type_name) {
