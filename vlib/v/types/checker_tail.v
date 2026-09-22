c874a4b9ab0bcb89ed47962bc62231a93664048c
Alexander Medvednikov <alexander@medvednikov.com>
2026-09-22 17:45:57 +0300

    checker: distinguish required branch-hint parentheses (patch preparation) [skip ci]

--- vlib/v/types/checker_tail.v
+++ vlib/v/types/checker_tail.v
@@ -1,7 +1,9 @@
 	value_context := !tc.is_statement_node(id) && tc.expression_node_used_as_value(id)
 	cond_id := tc.a.child(&node, 0)
 	condition := tc.a.node(cond_id)
-	if condition.kind == .paren && condition.value != '__v3_comptime_d' {
+	// Branch hints also use .paren nodes, but their parentheses are required.
+	if condition.kind == .paren && condition.value != '__v3_comptime_d'
+		&& tc.node_source_starts_with(cond_id, '(') {
 		tc.record_warning_at(.condition_mismatch, 'unnecessary `()` in `if` condition, use `if expr {` instead of `if (expr) {`.', cond_id, tc.if_parenthesized_condition_pos(condition))
 	}
 	saved_mut_local_owners := tc.fn_context.mut_local_owners.clone()
