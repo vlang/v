d6070ecfec9a4c36d31c4eb1c92febde70a372c2
antono2 <antono2@github>
2026-09-23 20:27:31 +0200

    test: cover negative fixed float interpolation

--- vlib/v/compiler_tests/string_interpolation_float_rounding_test.v
+++ vlib/v/compiler_tests/string_interpolation_float_rounding_test.v
@@ -1,4 +1,6 @@
 fn test_fixed_float_interpolation_rounds_half_away_from_zero_at_any_precision() {
 	assert '${6.25:.1f}' == '6.3'
 	assert '${-6.25:.1f}' == '-6.3'
+	assert '${f32(-0.526):.3f}' == '-0.526'
+	assert '${f64(-1.234):.3f}' == '-1.234'
 }
