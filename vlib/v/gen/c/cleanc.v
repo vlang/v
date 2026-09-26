45ad1ea2fae51aa54e32545fa024aeba96dccdb1
antono2 <antono2@github>
2026-09-26 07:40:51 +0200

    v3: preserve pointer indirection in callback type aliases

--- vlib/v/gen/c/cleanc.v
+++ vlib/v/gen/c/cleanc.v
@@ -13631,8 +13631,8 @@ fn (mut g FlatGen) type_name_c_type(type_name string) string {
 		return g.resolve_fn_ptr_type(type_name)
 	}
 	t := g.tc.parse_type(type_name)
-	if fn_type := fn_type_from(t) {
-		return g.resolve_fn_ptr_type(g.fn_ptr_type_key(fn_type))
+	if _ := fn_type_from(t) {
+		return g.value_c_type(t)
 	}
 	ct := if t is types.OptionType || t is types.ResultType {
 		g.optional_type_name(t)
